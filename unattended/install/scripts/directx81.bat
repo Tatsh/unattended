80c0e51c0c6fc36e0a9059e3452f6cf7242d0461
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-16 18:34:13 +0000

    Minor fixes

--- unattended/install/scripts/directx81.bat
+++ unattended/install/scripts/directx81.bat
@@ -1,6 +1,7 @@
 :: DirectX 8.1 upgrade
 
 :: Reboot when finished
-z:\scripts\todo.pl .reboot
+z:\bin\todo.pl .reboot
 
 start /wait z:\packages\directx81\dxsetup /install /silent
+if errorlevel 1 exit 1
