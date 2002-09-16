84e09c7544073f47072811728a1489b253b7fa95
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-16 21:06:30 +0000

    Initial revision.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@20 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/perl.bat
@@ -0,0 +1,7 @@
+:: "PERL_PATH=Yes" instructs installer to add Perl to the PATH
+:: "PERL_EXT=Yes" instructs installer to associate .pl files with Perl
+:: (The latter is undocumented; see
+:: http://bugs.activestate.com/show_bug.cgi?id=20364)
+
+start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i z:\packages\ActivePerl.msi PERL_PATH=Yes PERL_EXT=Yes
+if errorlevel 1 exit 1
