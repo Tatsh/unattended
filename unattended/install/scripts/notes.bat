efdd85b33aa1ba2cfbb51e6df980e18cd443a10c
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-18 17:04:21 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@50 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/notes.bat
@@ -0,0 +1,24 @@
+:: Install Notes 5.0.8, then update it to the current release.
+
+z:
+cd \packages\Lotus\notes5.0.8\Clients\W32Intel
+
+:: The magic -SMS flag permits "start /wait" to work.
+:: There is no way to learn about these flags except to search the
+:: Gold Release discussion forum on www.notes.net.  Have a nice day.
+
+start /wait .\setup.exe -s -SMS -f1 setup.iss
+if errorlevel 1 exit 1
+
+cd \packages\Lotus\incremental-installers
+if errorlevel 1 exit 1
+
+:: The incremental installers, on the other hand, respond to "/?".
+start /wait 5.0.9.exe /nouser
+if errorlevel 1 exit 1
+start /wait 5.0.9a.exe /nouser
+if errorlevel 1 exit 1
+start /wait 5.0.10.exe /nouser
+if errorlevel 1 exit 1
+start /wait 5.0.11.exe /nouser
+if errorlevel 1 exit 1
