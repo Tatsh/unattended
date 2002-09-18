6e4a0bcb0fd69df82b2f72a45a596ce99a48772b
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-18 16:56:46 +0000

    Initial revision

---
+++ trunk/unattended/install/scripts/7-zip.pl
@@ -0,0 +1,26 @@
+# Install 7-zip.
+
+use warnings;
+use strict;
+
+my $pkg = 'z:\\packages\\7z230b23.exe -y /Q';
+
+print "Running $pkg...";
+0 == system $pkg
+    or die "$pkg failed: ", ($? ? $? : $^E);
+print "done.\n";
+
+# 7-zip adds itself to the right-click menu in the Explorer by
+# default.  Most people probably don't want that, so we undo it here.
+# Commented out because I changed my mind.
+
+#  my %reg;
+#  use Win32::TieRegistry (Delimiter => '/', TiedHash => \%reg);
+
+#  foreach my $subkey ('Directory', 'Folder') {
+#      my $key = "/Classes/$subkey/shellex/ContextMenuHandlers/7-ZIP/";
+#      (delete $reg{$key})
+#          or die "Unable to delete $key: $^E";
+#  }
+
+exit 0;
