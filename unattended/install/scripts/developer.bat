8e75cd9e81032baca972630ca7627db08cb519c8
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-20 16:15:46 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@79 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/developer.bat
@@ -0,0 +1,6 @@
+:: Install a developer workstaion
+
+:: We install the reskit for shutdown.exe and maybe some other things.
+:: We install the support tools just for tlist.exe.
+
+z:\bin\todo.pl base.bat reskit.bat support-tools.bat emacs.bat cygwin.bat vc6.bat symbols.bat
