f0aaf81b391086f3cc52ed6ca8d4bdf7f3637461
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-20 20:08:41 +0000

    Initial revision

---
+++ unattended/install/scripts/sshd.bat
@@ -0,0 +1,16 @@
+:: Install sshd, set it up as a service, etc.
+
+:: Note: This script presupposes the presense of the Resource Kit (for
+:: ntrights.exe) and Cygwin (for sshd itself).
+
+:: Also note: The "curlbot" account is used internally for performing
+:: builds.  Any account could be used.
+
+:: Grant curlbot the right to log on as a service.  Create the
+:: cygwin-sshd service to run srvany.exe as curlbot.  Load the
+:: registry with the srvany arguments.
+z:\scripts\todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "instsrv.pl cygwin-sshd c:\ntreskit\srvany.exe CURL\curlbot" "regedit /s z:\scripts\sshd.reg"
+
+:: Create the sshd keys, if needed.
+set cyg=c:\cygwin
+z:\scripts\todo.pl "if not exist %cyg%\etc\ssh_host_key %cyg%\bin\ssh-keygen -t rsa1 -N '' -f /etc/ssh_host_key" "if not exist %cyg%\etc\ssh_host_rsa_key %cyg%\bin\ssh-keygen -t rsa -N '' -f /etc/ssh_host_rsa_key" "if not exist %cyg%\etc\ssh_host_dsa_key %cyg%\bin\ssh-keygen -t dsa -N '' -f /etc/ssh_host_dsa_key"
