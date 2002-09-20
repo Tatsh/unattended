2e780aef6d7cbb7cbf37dc2661c9f21d85fadef4
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-20 16:53:59 +0000

    Fix path to todo.pl
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@83 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/vc6.bat
+++ unattended/install/scripts/vc6.bat
@@ -8,7 +8,7 @@ if errorlevel 1 exit 1
 z:\bin\todo.pl mdac.bat "z:\packages\VC6\vc6_sp5\setupsp5.exe /qn1 /g c:\netinst\logs\vc6sp5.txt" .reboot
 
 :: Reboot after installing VC6
-z:\scripts\todo.pl .reboot
+z:\bin\todo.pl .reboot
 
 :: NOTE: YOU MUST PROVIDE A VALID LICENSE KEY FOR THE /k OPTION
 start /wait z:\packages\VC6\acmboot.exe /qn1 /g c:\netinst\logs\vc6.txt /k 1234567890
