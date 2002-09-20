f2a5778eee1db565e6e2335c7ab58b3dd7a07b1b
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-20 16:15:46 +0000

    Initial revision

---
+++ unattended/install/scripts/reskit.bat
@@ -0,0 +1,5 @@
+:: Install the Windows 2000 resource kit
+
+:: Force the installation directory to be C:\NTRESKIT for historical
+:: reasons
+start /wait msiexec /qb /l* c:\netinst\logs\reskit.txt /i z:\packages\reskit\W2000RKPRO.MSI NTRK50DIR=C:\NTRESKIT
