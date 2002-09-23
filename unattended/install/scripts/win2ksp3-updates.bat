5fa26b822bc819af7cf408d16b37b1c15e5a9944
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-23 22:05:09 +0000

    Add "Euro conversion tool" update

--- unattended/install/scripts/win2ksp3-updates.bat
+++ unattended/install/scripts/win2ksp3-updates.bat
@@ -1,3 +1,6 @@
 :: Install all hotfixes and updates for win2ksp3
 
+:: Euro conversion tool update
+todo.pl "z:\packages\win2ksp3\euro-setup.exe /q"
+
 todo.pl win2ksp3-hotfixes.bat ie6.bat directx81.bat
