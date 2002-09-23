818b103e63828901e6120448975d04ad4046d0a5
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-23 19:46:10 +0000

    Initial revision

---
+++ trunk/unattended/install/scripts/win2ksp3-hotfixes.bat
@@ -0,0 +1,27 @@
+:: Install post-SP3 hotfixes for Win2k SP3
+
+:: Note that since we are pushing these commands onto the to-do stack,
+:: they will be executed in the opposite order
+
+:: Q324380
+:: <http://www.microsoft.com/windows2000/downloads/security/q324380/>
+todo.pl "z:\packages\win2ksp3\Q324380.exe /u /o /z"
+
+:: Q327269
+:: <http://www.microsoft.com/windows2000/downloads/recommended/q327269/>
+todo.pl "z:\packages\win2ksp3\Q327269_W2K_SP4_X86_EN.exe /u /o /z" .reboot
+
+:: Q326886
+:: <http://www.microsoft.com/windows2000/downloads/critical/q326886/>
+todo.pl "z:\packages\win2ksp3\Q326886_W2K_SP4_X86_EN.exe /u /o /z" .reboot
+
+:: Q326830
+:: <http://www.microsoft.com/windows2000/downloads/critical/q326830/>
+todo.pl "z:\packages\win2ksp3\Q326830_W2K_SP4_X86_EN.exe /u /o /z"
+
+:: Q323172
+:: <http://www.microsoft.com/windows2000/downloads/critical/q323172/>
+todo.pl "z:\packages\win2ksp3\Q323172_W2K_SP4_X86_EN.exe /u /o /z"
+
+:: Q322842
+todo.pl "z:\packages\win2ksp3\Q322842_W2K_SP4_X86_EN.exe /u /o /z"
