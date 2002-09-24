b02ff02843b2f8f6bc410e2c7037c9e9c7b3b8b7
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-24 14:41:56 +0000

    Changes to make binary distribution feasible

--- unattended/bootdisk/template/autoexec.bat
+++ unattended/bootdisk/template/autoexec.bat
@@ -9,12 +9,27 @@ LH TCPTSR.EXE
 LH TINYRFC.EXE
 
 NET START BASIC
-NET USE Z: \\ntinstall\install
+if not errorlevel 1 goto gotnet
+@echo *** Unable to start network, probably because
+@echo *** you are using the wrong device driver.
+@goto end
 
-set PATH=Z:\dosbin;%PATH%
+:gotnet
 
+@echo.
+@echo Enter location of install share (default \\ntinstall\install):
+@nset INSTALL=$0
+@if %INSTALL%.==. set INSTALL=\\ntinstall\install
+@echo OK, using INSTALL=%INSTALL%
+
+NET USE Z: %INSTALL%
+@if not errorlevel 1 goto got_z
+@echo Unable to map Z: drive.  Try again...
+@goto gotnet
+
+:got_z
+set PATH=Z:\djgpp\bin;z:\dosbin;%PATH%
 set DJGPP=Z:\djgpp\djgpp.env
-set PATH=Z:\djgpp\bin;%PATH%
 lh cwsdpmi -p -s-
 
 bash Z:\install.sh
