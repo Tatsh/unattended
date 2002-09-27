d4ab8be1e556ea3e37da48864929964838bed7eb
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-27 20:14:01 +0000

    Add .NET framework Service Pack 2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@146 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winxpsp1-updates.bat
+++ unattended/install/scripts/winxpsp1-updates.bat
@@ -12,8 +12,14 @@
 :: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/5509.htm>
 todo.pl "z:\packages\winxpsp1\movie-maker-1.2.1.exe /q"
 
+:: Microsoft .NET framework Service Pack 2
+:: <http://msdn.microsoft.com/netframework/downloads/updates/sp/>
+:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q321884>
+todo.pl "z:\packages\winxpsp1\NDP10_SP_Q321884_En.exe /q"
+
 :: Microsoft .NET framework
 :: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/4812.htm>
+:: <http://msdn.microsoft.com/netframework/downloads/>
 :: For silent install, see:
 :: <http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dnnetdep/html/dotnetfxref.asp>
 todo.pl "z:\packages\winxpsp1\dotnetfx.exe /q /c:\"install /q\""
