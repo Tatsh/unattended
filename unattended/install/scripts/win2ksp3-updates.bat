fce6722f9a07d3ff7f815d9f2e6dd3388a122d29
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-09-27 15:27:30 +0000

    Add URL for Euro Conversion Tool
    Rename its setup.exe to euro-tool.exe

--- unattended/install/scripts/win2ksp3-updates.bat
+++ unattended/install/scripts/win2ksp3-updates.bat
@@ -1,6 +1,7 @@
 :: Install all hotfixes and updates for win2ksp3
 
 :: Euro conversion tool update
-todo.pl "z:\packages\win2ksp3\euro-setup.exe /q"
+:: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/5026.htm>
+todo.pl "z:\packages\win2ksp3\euro-tool.exe /q"
 
 todo.pl win2ksp3-hotfixes.bat ie6.bat directx81.bat
