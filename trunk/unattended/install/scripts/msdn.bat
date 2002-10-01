af340601907292fcec66210faee9a89a5f9d7dbd
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-10-01 20:17:43 +0000

    Slight change to comments

--- trunk/unattended/install/scripts/msdn.bat
+++ trunk/unattended/install/scripts/msdn.bat
@@ -4,7 +4,7 @@
 todo.pl .reboot
 
 :: Use UNC path to installer, because MSDN library depends on the
-:: network path from which it was installed, and we do not normally
+:: network path from which it was installed, and users do not normally
 :: map the Z: drive.
 start /wait msiexec /qb /l* c:\netinst\logs\msdn.txt /i %Z_PATH%\packages\msdn\Apr2002\msdn.msi SETUP_EXE=yes
 if errorlevel 1 exit 1
