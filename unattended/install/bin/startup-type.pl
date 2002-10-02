8883aaa5abb942d9190b58bf4460d4e96ab43cf4
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-10-02 15:50:50 +0000

    Rewrite completely to use WMI interface instead of hacking registry.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@182 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/bin/startup-type.pl
+++ unattended/install/bin/startup-type.pl
@@ -1,29 +1,54 @@
-# Set startup type for service
+# Set startup type (automatic, manual, etc.) for a service
 
 use warnings;
 use strict;
+use Win32::OLE;
 
 @ARGV == 2
     or die "Usage: $0 <type> <service>\n";
 
-my ($type, $service) = @ARGV;
+sub to_lower ($) {
+    my ($arg) = @_;
+    $arg =~ tr/A-Z/a-z/;
+    return $arg;
+}
 
-my %types = ( 'automatic' => 2,
-              'manual' => 3,
-              'disabled' => 4 );
+my ($type, $service_name) = @ARGV;
+# Convert to lower-case
+$type = to_lower ($type);
+$service_name = to_lower ($service_name);
+
+my %types = map { (to_lower ($_) => $_) } ('Boot', 'System', 'Automatic',
+                                           'Manual', 'Disabled');
 
 (exists $types{$type})
     or die '<type> must be one of ', join ' ', keys %types;
 
-my %reg;
-use Win32::TieRegistry (Delimiter => '/', TiedHash => \%reg,
-                        qw (REG_DWORD));
+# Bomb out completely if COM engine encounters any trouble.
+Win32::OLE->Option ('Warn' => 3);
 
-my $services_key = '/LMachine/SYSTEM/CurrentControlSet/Services';
+# Get a handle to the SWbemServices object of the local machine.
+my $moniker = 'WinMgmts:';
+my $computer = Win32::OLE->GetObject ($moniker);
+defined $computer
+    or die "Unable to GetObject $moniker: $^E";
 
-my $val = "$services_key/$service//Start";
+# Get the SWbemObjectSet of all services.  See:
+# http://msdn.microsoft.com/library/en-us/wmisdk/wmi/win32_service.asp
+my $services_set = $computer->InstancesOf ('Win32_Service');
 
-(exists $reg{$val})
-    or die "Unable to read $val: $^E";
-$reg{$val} = [ pack ('L', $types{$type}), REG_DWORD ]
-    or die "Unable to set $val to $types{$type}: $^E";
+# Convert set to a Perl array.
+my @services = Win32::OLE::Enum->All ($services_set);
+
+foreach my $service (@services) {
+    my $name = $service->{'Name'};
+    my $display_name = $service->{'DisplayName'};
+    if ($service_name eq to_lower($name)
+        || $service_name eq to_lower ($display_name)) {
+        print "Setting mode for $name ($display_name) to $types{$type}...";
+        my $ret = $service->ChangeStartMode ($types{$type});
+        $ret == 0
+            or die "Unable to ChangeStartMode to $types{$type}: $ret";
+        print "done.\n";
+    }
+}
