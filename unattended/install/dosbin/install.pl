f3953956309f745cc4883873574c2bcb2e1c1fab
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-10-21 20:23:33 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@193 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/dosbin/install.pl
@@ -0,0 +1,420 @@
+
+use warnings;
+use strict;
+use Carp;
+
+require 'unattend.pl';
+
+## Handy general-purpose subroutines for asking questions.
+
+# Yes/no question
+sub yes_no_choice ($) {
+    my ($question) = @_;
+    print "\n";
+    system 'choice', $question;
+    return ($? == 1 ? 1 : 0);
+}
+
+# Menu of options.  Takes an even number of arguments which are
+# display / return pairs.  For example:
+#
+#     menu_choice ('option X' => 'foo', 'option Y' => 'bar')
+#
+# ...returns 'foo' the user selects option X and 'bar' if the user
+# selects option Y.
+sub menu_choice (@) {
+    my @args = @_;
+    my @choice_map;
+
+    scalar @args % 2 == 0
+        or croak "menu_choice called with odd number of arguments";
+
+    scalar @args > 18
+        and croak "menu_choice called with too many arguments";
+
+    print "\n";
+
+    my $i = 1;
+    my $choices = '';
+    while (exists $args[0]) {
+        my $option = shift @args;
+        my $value = shift @args;
+        print "$i) $option\n";
+        $choices .= $i;
+        $choice_map[$i] = $value;
+        $i++;
+    }
+
+    print "X) Exit this program\n";
+    $choices .= 'x';
+    my $exit_choice = $i++;
+    system 'choice', "/c:$choices", "Select:";
+    my $ret = ($? >> 8);
+    if ($ret == $exit_choice) {
+        print "Exiting.\n";
+        exit 1;
+    }
+    return $choice_map[$ret];
+}
+
+# Canonicalize a username with respect to a domain.  If username is
+# already in fully-qualified form DOMAIN\USER, do nothing.
+sub canonicalize_user ($$) {
+    my ($domain, $user) = @_;
+    $user =~ /\\/
+        or $user = "$domain\\$user";
+    return $user;
+}
+
+# Convert from short OS name (like win2k, win2ksp3, or winxpsp1) to
+# full human-readable name.
+sub full_os_name ($) {
+    my ($arg) = @_;
+    my $ret;
+    if ($arg =~ /^win2k(?:sp(\d+))?$/) {
+        $ret = "Windows 2000 Professional";
+    }
+    elsif ($arg =~ /^winxp(?:sp(\d+))$/) {
+        $ret = "Windows XP Professional";
+    }
+    defined $1
+        and $ret .= " Service Pack $1";
+    return $ret;
+}
+
+## Set defaults.
+
+sub ask_fdisk_lba () {
+    return menu_choice
+        ('Large (>8G) disk support (normal)' => 1,
+         'No large disk support (required for some broken BIOSes)' => 0);
+}
+
+sub ask_fdisk_cmds () {
+    print "Choose partitioning scheme.\n";
+    print "NOTE: Any choice other than 'Do nothing' will require a reboot.\n";
+    my @pre_cmds = [ 'fdisk /clearall 1' ];
+    my @post_cmds = [ 'fdisk /prio:2000',
+                      'fdisk /activate:1',
+                      'fdisk /mbr',
+                      'fdisk /reboot'];
+
+    while (1) {
+        my $cmds = menu_choice
+            ('Do nothing (continue)' => undef,
+             'Whole disk C:', =>
+             [ @pre_cmds, @post_cmds],
+             '4G C:, rest D:' =>
+             [ @pre_cmds,
+               'fdisk /pri:4096',
+               'fdisk /pri:100,100 /spec:7',
+               'fdisk /delete /pri:1',
+               @post_cmds ],
+             '4G C:, 4G D:, rest E:' =>
+             [ @pre_cmds,
+               'fdisk /pri:4096',
+               'fdisk /pri:4096 /spec:7',
+               'fdisk /pri:100,100 /spec:7',
+               @post_cmds ],
+             'Run fdisk interactively' =>
+             [ 'fdisk /xo', @post_cmds ]);
+        if (defined $cmds) {
+            print "WARNING: This operation destroys the disk!";
+            yes_no_choice ("Are you sure")
+                or next;
+        }
+        return $cmds;
+    }
+}
+
+sub ask_os () {
+    my $os_dir = get_value ('_config', 'os_dir');
+    opendir OSDIR, $os_dir
+        or die "Unable to opendir $os_dir: $^E";
+    my @ents = map { lc; } readdir OSDIR;
+    closedir OSDIR, $os_dir
+        or die "Unable to closedir $os_dir: $^E";
+    my @oses = grep { defined full_os_name ($_); } @ents;
+    exists $oses[0]
+        or die "Unable to find any OS directories under $os_dir; bailing";
+    unless (exists $oses[1]) {
+        print "$oses[0] is the only OS directory under $os_dir; using it.\n";
+        return $oses[0];
+    }
+    # Remove trailing backslash
+    $os_dir =~ s/\\*$//;
+    print "Please choose the OS to install:\n";
+    return menu_choice (map { full_os_name ($_) . " ($os_dir\\$_)"
+                                  => $_ }
+                        @oses);
+}
+
+sub simple_q ($) {
+    my ($question) = @_;
+    print "\n", $question;
+    my $answer = <STDIN>;
+    chomp $answer;
+    $answer eq ''
+        and undef $answer;
+    return $answer;
+}
+
+set_comments ('_config', '',
+              "; This section is for informational purposes;\n"
+              . "; Windows Setup does not use it.\n");
+
+set_comments ('_config', 'fdisk_lba',
+              "    ; Use extended INT13 BIOS calls for fdisk?\n");
+
+set_value ('_config', 'fdisk_lba', \&ask_fdisk_lba);
+
+set_value ('_config', 'fdisk_cmds', \&ask_fdisk_cmds);
+
+set_value ('_config', 'format_cmd',
+           sub {
+               return (yes_no_choice ('Format C: drive')
+                       ? 'format /q /v: c:'
+                       : undef);
+           });
+
+set_comments ('_config', 'OS_dir',
+              "    ; Directory holding OS media directories\n");
+set_value ('_config', 'OS_dir', 'z:\\');
+
+set_value ('_config', 'OS', \&ask_os);
+
+set_comments ('_config', 'post_install_script',
+              "    ; Script run by postinst.bat\n");
+
+set_value ('_config', 'post_install_script',
+           sub {
+               print "Choose post-installation script to run:\n";
+               my @scripts = ('base.bat', 'sales.bat', 'developer.bat',
+                              'build-server.bat', 'training.bat');
+               my @choices = map { my $full = "Z:\\scripts\\$_";
+                                   return ($full => $full);
+                               } @scripts;
+               return menu_choice (@choices, 'none' => undef);
+           });
+
+set_comments ('_config', 'local_admins',
+              "    ; Accounts added to local Administrators group\n");
+set_value ('_config', 'local_admins',
+           sub {
+               # Bogus dependency to enforce question order
+               get_value ('Identification', 'DomainAdmin');
+               my $dom = get_value ('Identification', 'JoinDomain');
+               defined $dom
+                   or return undef;
+               print "Enter users to add to local Administrators group.\n";
+               my $users = simple_q
+                   ("Type 0 or more usernames, separated by spaces:\n");
+               my @users = (map { canonicalize_user ($dom, $_); }
+                            split / /, $users);
+               return (exists $users[0] ? \@users : undef);
+           });
+
+set_value ('_config', 'netinst', 'c:\\netinst');
+
+set_value ('_config', 'doit_cmd',
+           sub {
+               my $unattend_txt = (get_value ('_config', 'netinst')
+                                   . '\\unattend.txt');
+               my $src_tree = get_value ('_config', 'OS_dir');
+               $src_tree =~ /\\$/
+                   or $src_tree .= '\\';
+               $src_tree .= get_value ('_config', 'OS');
+               return "$src_tree\\winnt /s:$src_tree /u:$unattend_txt";
+           });
+
+set_value ('UserData', 'FullName',
+           sub {
+               return simple_q
+                   ("Enter the user's full name for this machine:\n");
+           });
+
+set_value ('UserData', 'OrgName',
+           sub {
+               return simple_q
+                   ("Enter the organization name for this machine:\n");
+           });
+
+set_value ('UserData', 'ComputerName',
+           sub {
+               my $name = simple_q
+                   ("Enter computer name (default = autogenerate):\n");
+               $name eq ''
+                   and $name = '*';
+               return $name;
+           });
+
+# Default time zone US/Eastern (FIXME)
+set_value ('GuiUnattended', 'TimeZone', 35);
+
+set_value ('Identification', 'JoinDomain',
+           sub {
+               return simple_q
+                   ('Join workstation to what domain (default = none)? ');
+           });
+
+set_value ('Identification', 'DomainAdmin',
+           sub {
+               my $dom = get_value ('Identification', 'JoinDomain');
+               defined $dom or return undef;
+               return simple_q
+                   ('DomainAdmin account for joining domain? ');
+           });
+
+set_value ('Identification', 'DomainAdminPassword',
+           sub {
+               my $admin = get_value ('Identification', 'DomainAdmin');
+               defined $admin
+                   or return undef;
+               return simple_q
+                   ('DomainAdminPassword for joining domain? ');
+           });
+
+my $product_key_q =
+    "Enter your product key now.\n"
+    . "This a 25-character code found on your software license,\n"
+    . "like 12345-6789AB-CDEFG-HIJKL-MNOPQ\n"
+    . "Enter key: ";
+
+set_value ('UserData', 'ProductID',
+           sub {
+               my $os = get_value ('_config', 'OS');
+               # ProductID is used by win2k and winnt
+               $os =~ /^win2k/ || $os =~ /^winnt/
+                   or return undef;
+               return simple_q ($product_key_q);
+           });
+
+set_value ('UserData', 'ProductKey',
+           sub {
+               my $product_id = get_value ('UserData', 'ProductID');
+               # Only ask for ProductKey if we lack a ProductID
+               defined $product_id
+                   and return undef;
+               return simple_q ($product_key_q);
+           });
+
+## Now the meat of the script.
+
+# Read master unattend.txt.
+#read_unattend_txt ('z:\\unattend.txt');
+
+# Read site-specific unattend.txt, if it exists.
+my $site_unattend_txt = 'z:\\site\\unattend.txt';
+-e ($site_unattend_txt)
+    and read_unattend_txt ($site_unattend_txt);
+
+# Read site-specific Perl configuration file.
+my $site_conf = 'z:\\site\\dosconf.pl';
+if (-e $site_conf) {
+    my $result = do $site_conf;
+    $@
+        and die "do $site_conf failed: $@";
+    defined $result
+        or die "Could not do $site_conf: $^E";
+}
+
+# Set environment variable controlling fdisk's use of INT13 extensions.
+get_value ('_config', 'fdisk_lba')
+    or $ENV{'FFD_VERSION'}=6;
+
+# Display the partition table.
+print "\nCurrent partition table:\n\n";
+system 'fdisk', '/info';
+print "\n";
+
+# Partition the disk and reboot, if required.
+my $fdisk_cmds = get_value ('_config', 'fdisk_cmds');
+if (defined $fdisk_cmds) {
+    foreach my $cmd (@$fdisk_cmds) {
+        system $cmd;
+    }
+}
+
+# Run formatting command, if any.
+my $format_cmd = get_value ('_config', 'format_cmd');
+defined $format_cmd
+    and system $format_cmd;
+
+# Create C:\netinst and subdirectories.
+my $netinst = get_value ('_config', 'netinst');
+foreach my $dir ($netinst, "$netinst\\logs") {
+    -d $dir
+        and next;
+    print "Creating $dir...";
+    mkdir $dir
+        or die "FAILED: $^E";
+    print "done.\n";
+}
+
+# Create unattend.txt file.
+my $unattend_txt = get_value ('_config', 'netinst') . '\\unattend.txt';
+
+my $unattend_contents = generate_unattend_txt ();
+
+print "Creating $unattend_txt...";
+
+open UNATTEND, ">$unattend_txt"
+    or die "Unable to open $unattend_txt for writing: $^E";
+print UNATTEND $unattend_contents
+    or die "Unable to write to $unattend_txt: $^E";
+close UNATTEND
+    or die "Unable to close $unattend_txt: $^E";
+
+print "done.\n";
+
+# Create postinst.bat script.
+my $postinst = "$netinst\\postinst.bat";
+print "Creating $postinst...";
+
+my $top = get_value ('_config', 'post_install_script');
+my @local_admins = @{get_value ('_config', 'local_admins')};
+
+open POSTINST, ">$postinst"
+    or die "Unable to open $postinst for writing: $^E";
+
+foreach my $line ("net use z: $ENV{'INSTALL'} /persistent:yes",
+                  "call z:\\scripts\\perl.bat",
+                  "PATH=z:\\bin;$%PATH%",
+                  "todo.pl $top \"autolog.pl --disable\" .reboot",
+                  (map { "todo.pl \"net localgroup Administrators $_ /add" }
+                   @local_admins),
+                  "\ntodo.pl --go") {
+    print POSTINST $line, "\n"
+        or die "Unable to write to $postinst: $^E";
+}
+
+close POSTINST
+    or die "Unable to close $postinst: $^E";
+
+print "done.\n";
+
+# While DJGPP is running, there is not enough conventional memory
+# available for winnt.exe to work.  So we just drop the command in a
+# .bat script (doit.bat) and run it.
+my $doit = "$netinst\\doit.bat";
+print "Creating $doit...";
+open DOIT, ">$doit"
+    or die "Unable to open $doit for writing: $^E";
+
+print DOIT get_value ('_config', 'doit_cmd'), "\n"
+    or die "Unable to write to $doit: $^E";
+
+close DOIT
+    or die "Unable to close $doit: $^E";
+
+print "done.\n";
+
+while (1) {
+    my $file = menu_choice ("Edit $unattend_txt" => $unattend_txt,
+                            "Edit $postinst" => $postinst,
+                            "Edit $doit" => $doit,
+                            'Continue' => undef);
+    defined $file
+        or last;
+    system 'edit', $file;
+}
---
+++ unattended/install/doslib/unattend.pl
@@ -0,0 +1,249 @@
+# Routines for parsing and generate unattend.txt files.
+
+use warnings;
+use strict;
+
+# Master hash holding values for unattend.txt.
+my $unattend = { };
+
+# Get the hash corresponding to a section+key pair.
+# Not intended for external use.
+sub get_info ($$) {
+    my ($sect, $key) = @_;
+    my ($l_sect, $l_key) = map { lc } ($sect, $key);
+    my $sect_hash = $unattend->{$l_sect};
+    # If this section does not exist yet, create it.
+    if (!defined $sect_hash) {
+        $sect_hash = { };
+        $unattend->{$l_sect} = $sect_hash;
+    }
+
+    my $key_hash = $sect_hash->{$l_key};
+    # If this key does not exist yet, create it.
+    if (!defined $key_hash) {
+        $key_hash = { };
+        $sect_hash->{$l_key} = $key_hash;
+    }
+
+    return $key_hash;
+}
+
+# Get the value associated with a section+key in the answer file.
+# Example:
+#   $name = get_value ('UserData', 'FullName');
+sub get_value ($$;$);
+sub get_value ($$;$) {
+    my ($sect, $key, $noforce) = @_;
+    my $info = get_info ($sect, $key);
+    my $val = $info->{'value'};
+    if (ref $val eq 'CODE' && !$noforce) {
+        # It is a promise, not a value, so force it.
+        $val = &$val ();
+        $info->{'value'} = $val;
+    }
+    return $val;
+}
+
+# Assign a value to a section+key in the answer file.
+# Example:
+#    set_value ('GuiUnattended', 'AdminPassword', 'sekrit');
+sub set_value ($$$);
+sub set_value ($$$) {
+    my ($sect, $key, $val) = @_;
+    my $sect_info = get_info ($sect, '');
+    # Handle "array" keys, a little invention of our own.
+    if ($key =~ /^(.*)\[(\d+)\]$/) {
+        my ($container_key, $index) = ($1, $2);
+        my $container_val = get_value ($sect, $container_key);
+        if (!defined $container_val) {
+            # Entry does not exist yet, so create it.
+            $container_val = [ ];
+        }
+
+        ref $container_val eq 'ARRAY'
+            or die "Cannot set array index $index of scalar key $sect/$container_key";
+
+        $container_val->[$index] = $val;
+        $key = $container_key;
+        $val = $container_val;
+    }
+
+    my $key_info = get_info ($sect, $key);
+    $key_info->{'value'} = $val;
+    # While we are here, set display name of section and key.
+    $sect_info->{'dname'} = $sect;
+    $key_info->{'dname'} = $key;
+}
+
+# Get the "priority" of a section+key.  This is a pair (major, minor).
+# Lower priorities sort later in the output file.
+sub get_priority ($$) {
+    my ($sect, $key) = @_;
+    my @ret = (0, 0);
+    my $pri = get_info ($sect, $key)->{'priority'};
+    defined $pri
+        and @ret = @$pri;
+
+    return @ret;
+}
+
+# Set the priority of a section+key.
+sub set_priority ($$$$) {
+    my ($sect, $key, $pri_major, $pri_minor) = @_;
+    get_info ($sect, $key)->{'priority'} = [ $pri_major, $pri_minor ];
+}
+
+sub get_comments ($$) {
+    my ($sect, $key) = @_;
+    my $comments = get_info ($sect, $key)->{'comments'};
+    defined $comments
+        or $comments = '';
+    return $comments;
+}
+
+sub set_comments ($$$) {
+    my ($sect, $key, $comments) = @_;
+    get_info ($sect, $key)->{'comments'} = $comments;
+}
+
+sub merge_comments ($$$) {
+    my ($sect, $key, $new_comments) = @_;
+    my $old_comments = get_comments ($sect, $key);
+    # If the new comments are non-trivial or the old comments are
+    # trivial, replace the old with the new.
+    if ($new_comments =~ /\S/ || $old_comments !~ /\S/) {
+        set_comments ($sect, $key, $new_comments);
+    }
+}
+
+sub get_display_name ($$) {
+    my ($sect, $key) = @_;
+    my $dname = get_info ($sect, $key)->{'dname'};
+    return $dname;
+}
+
+# Compare two section+key pairs by priority.
+sub cmp_pri ($$$$) {
+    my ($sec1, $key1, $sec2, $key2) = @_;
+    my ($pri1_major, $pri1_minor) = get_priority ($sec1, $key1);
+    my ($pri2_major, $pri2_minor) = get_priority ($sec2, $key2);
+
+    $pri1_major != $pri2_major
+        and return $pri1_major <=> $pri2_major;
+    return $pri1_minor <=> $pri2_minor;
+}
+
+# Read and parse an unattend.txt file, and accumulate its information
+# into the master hash.  Later files have higher priority.
+my $current_pri = -1;
+sub read_unattend_txt ($) {
+    my ($file) = @_;
+    my $cur_section;
+    my $cur_comments = '';
+
+    open FILE, $file
+        or die "Unable to open $file: $^E";
+
+    while (my $line = <FILE>) {
+        $line =~ s/\r//;        # For testing on Unix...
+        if ($line =~ /^\[(\S+)\]$/) {
+            # New section header
+            $cur_section = $1;
+            my ($major_pri, $minor_pri) = get_priority ($cur_section, '');
+            defined $major_pri && $major_pri == $current_pri
+                and (die "Duplicate [$cur_section] sections in $file, ",
+                     "lines $minor_pri and $.\n");
+            set_priority ($cur_section, '', $current_pri, $.);
+            merge_comments ($cur_section, '', $cur_comments);
+            $cur_comments = '';
+        }
+        elsif ($line =~ /^\s*(\w+)\s*=\s*(.*)$/) {
+            # key=value setting
+            my ($key, $val) = ($1, $2);
+            defined $cur_section
+                or die "$key=$val outside any section in $file";
+            my ($major_pri, $minor_pri) = get_priority ($cur_section, $key);
+            defined $major_pri && $major_pri == $current_pri
+                and (die "Duplicate $key settings in $file, ",
+                     "lines $minor_pri and $.\n");
+            set_priority ($cur_section, $key, $current_pri, $.);
+            set_value ($cur_section, $key, $val);
+            merge_comments ($cur_section, $key, $cur_comments);
+            $cur_comments = '';
+        }
+        elsif ($line =~ /^(\s*;.*|\s*)$/) {
+            # Comment
+            $cur_comments .= $line;
+        }
+        else {
+            die "Unrecognized line:\n  $line\n...in $file, ";
+        }
+    }
+
+    close FILE
+        or die "Unable to close $file: $^E";
+
+    $current_pri--;
+}
+
+# sub dump_hash ($;$);
+# sub dump_hash ($;$) {
+#     my ($hash, $indent) = @_;
+#     defined $indent
+#         or $indent = 0;
+
+#     foreach my $key (keys %$hash) {
+#         print ' ' x $indent;
+#         my $val = $hash->{$key};
+#         defined $val
+#             or $val = "UNDEF";
+#         print "$key -> $val\n";
+#         ref $val eq 'HASH'
+#             and dump_hash ($val, $indent + 4);
+#     }
+# }
+
+# Create string for "name=val"
+sub name_val_str ($$);
+sub name_val_str ($$) {
+    my ($name, $val) = @_;
+    my $ret = '';
+
+    if (!defined $val) {
+        # do nothing
+    }
+    elsif (ref $val eq 'ARRAY') {
+        foreach my $i (0 .. $#$val) {
+            $ret .= name_val_str ("$name\[$i\]", $val->[$i]);
+        }
+    }
+    else {
+        $ret = sprintf("    %s=%s\n", $name, $val);
+    }
+
+    return $ret;
+}
+
+sub generate_unattend_txt () {
+    my $ret = '';
+
+    foreach my $sect (sort { cmp_pri ($a, '', $b, '') } keys %$unattend) {
+        my $comments = get_comments ($sect, '');
+        $comments =~ /^\n/
+            or $comments = "\n$comments";
+        $ret .= $comments;
+        $ret .= sprintf "[%s]\n", get_display_name ($sect, '');
+        foreach my $key (sort { cmp_pri ($sect, $a, $sect, $b) }
+                         keys %{$unattend->{$sect}}) {
+            # Skip info for section itself
+            $key eq ''
+                and next;
+            my $val = get_value ($sect, $key);
+            my $dname = get_display_name ($sect, $key);
+            $ret .= name_val_str ($dname, $val);
+        }
+    }
+
+    $ret =~ s/\A\n//;
+    return $ret;
+}
