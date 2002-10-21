74c38c6a9757fc655e2273e154c53be874982f4a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-10-21 20:37:41 +0000

    Use DISABLEADVTSHORTCUTS instead of DISABLEMEDIA

--- trunk/unattended/install/scripts/office.bat
+++ trunk/unattended/install/scripts/office.bat
@@ -3,5 +3,5 @@
 :: FIXME: Do we really need to reboot?
 todo.pl .reboot office1.bat
 
-start /wait msiexec /qb /l* c:\netinst\logs\office.txt /i z:\office2k\data1.msi ADDLOCAL=ALL REBOOT=ReallySuppress NOUSERNAME=TRUE ALLUSERS=1 DISABLEMEDIA=1
+start /wait msiexec /qb /l* c:\netinst\logs\office.txt /i z:\office2k\data1.msi ADDLOCAL=ALL REBOOT=ReallySuppress NOUSERNAME=1 ALLUSERS=1 DISABLEADVTSHORTCUTS=1
 if errorlevel 1 exit 1
