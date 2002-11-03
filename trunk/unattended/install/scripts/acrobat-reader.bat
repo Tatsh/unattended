18d3925c5cb858c88f4e0f6e406c35244ae8b09a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-11-03 18:26:05 +0000

    Add /f2c:\netinst\logs\acrobat-reader.txt switch

--- trunk/unattended/install/scripts/acrobat-reader.bat
+++ trunk/unattended/install/scripts/acrobat-reader.bat
@@ -1,5 +1,5 @@
 :: Install Adobe Acrobat reader
 
-start /wait z:\packages\AcroReader51_ENU_full.exe /a /s /sms /f1z:\packages\acrobat-reader.iss
+start /wait z:\packages\AcroReader51_ENU_full.exe /a /s /sms /f1z:\packages\acrobat-reader.iss /f2c:\netinst\logs\acrobat-reader.txt
 
 if errorlevel 1 exit 1
