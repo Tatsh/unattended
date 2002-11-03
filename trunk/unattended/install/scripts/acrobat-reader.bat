9ca4e068451cdeafb99cb3fd4d4c286904df3d45
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-11-03 18:28:24 +0000

    Rename acrobat-reader.iss to acrobat.iss, because stupid InstallShield cannot handle dashes in the file name.

--- trunk/unattended/install/scripts/acrobat-reader.bat
+++ trunk/unattended/install/scripts/acrobat-reader.bat
@@ -1,5 +1,5 @@
 :: Install Adobe Acrobat reader
 
-start /wait z:\packages\AcroReader51_ENU_full.exe /a /s /sms /f1z:\packages\acrobat-reader.iss /f2c:\netinst\logs\acrobat-reader.txt
+start /wait z:\packages\AcroReader51_ENU_full.exe /a /s /sms /f1z:\packages\acrobat.iss /f2c:\netinst\logs\acrobat.txt
 
 if errorlevel 1 exit 1
