e248232d59c4fd225306f09be436399a3791667b
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-11-08 19:30:06 +0000

    Use todo.pl, not z:\scripts\todo.pl (which no longer exists...)

--- unattended/install/scripts/sshd.bat
+++ unattended/install/scripts/sshd.bat
@@ -9,8 +9,8 @@
 :: Grant curlbot the right to log on as a service.  Create the
 :: cygwin-sshd service to run srvany.exe as curlbot.  Load the
 :: registry with the srvany arguments.
-z:\scripts\todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "instsrv.pl cygwin-sshd c:\ntreskit\srvany.exe CURL\curlbot" "regedit /s z:\scripts\sshd.reg"
+todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "instsrv.pl cygwin-sshd c:\ntreskit\srvany.exe CURL\curlbot" "regedit /s z:\scripts\sshd.reg"
 
 :: Create the sshd keys, if needed.
 set cyg=c:\cygwin
-z:\scripts\todo.pl "if not exist %cyg%\etc\ssh_host_key %cyg%\bin\ssh-keygen -t rsa1 -N '' -f /etc/ssh_host_key" "if not exist %cyg%\etc\ssh_host_rsa_key %cyg%\bin\ssh-keygen -t rsa -N '' -f /etc/ssh_host_rsa_key" "if not exist %cyg%\etc\ssh_host_dsa_key %cyg%\bin\ssh-keygen -t dsa -N '' -f /etc/ssh_host_dsa_key"
+todo.pl "if not exist %cyg%\etc\ssh_host_key %cyg%\bin\ssh-keygen -t rsa1 -N '' -f /etc/ssh_host_key" "if not exist %cyg%\etc\ssh_host_rsa_key %cyg%\bin\ssh-keygen -t rsa -N '' -f /etc/ssh_host_rsa_key" "if not exist %cyg%\etc\ssh_host_dsa_key %cyg%\bin\ssh-keygen -t dsa -N '' -f /etc/ssh_host_dsa_key"
