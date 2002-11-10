92f07ff5989f9350ecd4914a8db704f37049de64
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-11-10 18:17:55 +0000

    Eliminate explicit paths
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@245 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/cygwin.bat
+++ unattended/install/scripts/cygwin.bat
@@ -2,7 +2,7 @@
 
 :: After installing Cygwin package, configure it.
 :: (ESHELL is used by Emacs.)
-z:\bin\todo.pl "setenv.pl ESHELL c:\cygwin\bin\bash.exe" cygwin-1.bat
+todo.pl "setenv.pl ESHELL c:\cygwin\bin\bash.exe" cygwin-1.bat
 
-z:\bin\AutoIt.exe z:\scripts\cygwin.aut
+AutoIt.exe z:\scripts\cygwin.aut
 
