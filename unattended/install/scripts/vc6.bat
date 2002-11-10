5ab03a3517fdc3a534c9faff509c25b3ccb7b88a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-11-10 18:17:55 +0000

    Eliminate explicit paths

--- unattended/install/scripts/vc6.bat
+++ unattended/install/scripts/vc6.bat
@@ -5,10 +5,10 @@ start /wait regedit /s z:\packages\VC6\key.dat
 if errorlevel 1 exit 1
 
 :: After VC6 is done, install MDAC, then SP5, then reboot
-z:\bin\todo.pl mdac.bat "z:\packages\VC6\vc6_sp5\setupsp5.exe /qn1 /g c:\netinst\logs\vc6sp5.txt" .reboot
+todo.pl mdac.bat "z:\packages\VC6\vc6_sp5\setupsp5.exe /qn1 /g c:\netinst\logs\vc6sp5.txt" .reboot
 
 :: Reboot after installing VC6
-z:\bin\todo.pl .reboot
+todo.pl .reboot
 
 :: NOTE: You must create Z:\site\vc6_key.bat to set the vc6_key
 :: variable to your license key
