c720fc2307470eb2cdbde857214798fa46a5d082
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-11-27 01:26:34 +0000

    Add critical update Q318203 back in

--- trunk/unattended/install/scripts/ie6.bat
+++ trunk/unattended/install/scripts/ie6.bat
@@ -3,16 +3,16 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
-:: Critical update Q318203/Q317244
-:: (not required for IE6 SP1?)
-:: <http://support.microsoft.com/?kbid=318203>
-::todo.pl .reboot q317244.bat
-
 :: Critical Update Q328970
 :: "November 2002, Cumulative Patch for Internet Explorer"
 :: <http://www.microsoft.com/windows/ie/downloads/critical/Q328970/>
 :: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q328970>
-todo.pl .reboot ".ignore-err 194 z:\packages\win2ksp3\q328970.exe /q /r:n" .reboot
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\q328970.exe /q /r:n" .reboot
+
+:: Critical update Q318203/Q317244
+:: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
+:: <http://support.microsoft.com/?kbid=318203>
+todo.pl .reboot "z:\packages\win2ksp3\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
 
 z:
 cd \packages\ie6\flat\win32\en
