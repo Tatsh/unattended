a121bb2681288f88a3604e73ac09a7a4e17b3db7
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-05 20:24:49 +0000

    Uncomment hotfix Q318203, since it is not included in the "cumulative update"

--- unattended/install/scripts/ie6.bat
+++ unattended/install/scripts/ie6.bat
@@ -9,15 +9,9 @@
 :: <http://www.microsoft.com/windows/ie/downloads/critical/q324929/>
 todo.pl ".ignore-err 194 z:\packages\win2ksp3\q324929.exe /q:a" .reboot
 
-:: Critical Update Q328970
-:: "November 2002, Cumulative Patch for Internet Explorer"
-:: <http://www.microsoft.com/windows/ie/downloads/critical/Q328970/>
-:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q328970>
-::todo.pl ".ignore-err 194 z:\packages\win2ksp3\q328970.exe /q /r:n" .reboot
-
 :: Critical update Q318203/Q317244
 :: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
 :: <http://support.microsoft.com/?kbid=318203>
-::todo.pl .reboot "z:\packages\win2ksp3\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
+todo.pl .reboot "z:\packages\win2ksp3\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
 
 todo.pl ".ignore-err 194 z:\packages\ie6\flat\win32\en\IE6setup.exe /q /r:n" .reboot
