ada202e2301c08aa83c958be6979fe2ef67cea99
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-05 17:23:22 +0000

    Clean up, and add hotfix Q324929
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@279 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/ie6.bat
+++ unattended/install/scripts/ie6.bat
@@ -3,20 +3,21 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Cumulative patch for Internet Explorer (324929)
+:: <http://www.microsoft.com/technet/security/bulletin/MS02-068.asp>
+:: <http://support.microsoft.com/?kbid=324929>
+:: <http://www.microsoft.com/windows/ie/downloads/critical/q324929/>
+todo.pl "z:\packages\win2ksp3\q324929.exe /q:a"
+
 :: Critical Update Q328970
 :: "November 2002, Cumulative Patch for Internet Explorer"
 :: <http://www.microsoft.com/windows/ie/downloads/critical/Q328970/>
 :: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q328970>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\q328970.exe /q /r:n" .reboot
+::todo.pl ".ignore-err 194 z:\packages\win2ksp3\q328970.exe /q /r:n" .reboot
 
 :: Critical update Q318203/Q317244
 :: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
 :: <http://support.microsoft.com/?kbid=318203>
-todo.pl .reboot "z:\packages\win2ksp3\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
+::todo.pl .reboot "z:\packages\win2ksp3\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
 
-z:
-cd \packages\ie6\flat\win32\en
-start /wait IE6setup.exe /q /r:n
-
-:: Commented out, because stupid IE setup always exits non-zero
-:: if errorlevel 1 exit 1
+todo.pl "start /wait z:\packages\ie6\flag\win32\en\IE6setup.exe /q /r:n"
