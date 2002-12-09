80cfbe46a3c03a82715e13c23d6605dd942114d1
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-09 18:17:28 +0000

    Add Symantec anti-virus
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@287 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sales.bat
+++ unattended/install/scripts/sales.bat
@@ -1,3 +1,4 @@
 :: Install a salesperson laptop
 
-todo.pl base.bat office.bat notes.bat att.bat shiva.bat
+:: Do antivirus software last so it picks up Office and Notes
+todo.pl base.bat office.bat notes.bat att.bat shiva.bat symantec-antivirus.bat
