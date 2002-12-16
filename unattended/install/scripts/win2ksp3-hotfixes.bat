c0f9d24290dd74d9801d2655cbaf237f396f4735
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-16 17:39:08 +0000

    Add hotfixes 328310, 810030, and 329170/309376
    Do not reboot so much, because current hotfixes include QCHAIN
    technology (TM).
    
    
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@308 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/win2ksp3-hotfixes.bat
+++ unattended/install/scripts/win2ksp3-hotfixes.bat
@@ -6,11 +6,28 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Critical Update 329170/309376
+:: "Flaw in SMB Signing Could Enable Group Policy to be Modified"
+:: <http://support.microsoft.com/?kbid=329170>
+:: <http://support.microsoft.com/?kbid=309376>
+todo.pl "z:\packages\win2ksp3\Q329170_W2K_SP4_X86_EN.exe /u /o /z"
+
+:: Critical Update 810030
+:: "Flaw in Microsoft VM May Compromise Windows"
+:: <http://support.microsoft.com/?kbid=810030>
+:: (only available from <http://windowsupdate.microsoft.com/catalog/>)
+todo.pl "z:\packages\win2ksp3\Q810030_W2K.exe /u /o /z"
+
+:: Critical Update Q328310
+:: "Flaw in Windows WM_TIMER Message Handling Could Enable Privilege Elevation"
+:: <http://support.microsoft.com/?kbid=322913>
+todo.pl "z:\packages\win2ksp3\Q328310_W2K_SP4_X86_EN.exe /u /o /z"
+
 :: Recommended update Q322913
 :: "WM_TIMER Messages Not Delivered to Programs"
 :: <http://www.microsoft.com/windows2000/downloads/recommended/q322913/>
 :: <http://support.microsoft.com/?kbid=322913>
-todo.pl "z:\packages\win2ksp3\Q322913_W2K_SP4_X86_EN.exe /u /o /z" .reboot
+todo.pl "z:\packages\win2ksp3\Q322913_W2K_SP4_X86_EN.exe /u /o /z"
 
 :: Critical Update Q329414
 :: "Buffer Overrun in MDAC Can Lead to Code Execution (MDAC 2.6)"
@@ -32,7 +49,7 @@ todo.pl "z:\packages\win2ksp3\Q328523_W2K_SP4_X86_EN.exe /u /o /z"
 :: Critical Update Q329834
 :: "Unchecked Buffer in PPTP Implementation Could Enable Denial of Service"
 :: <http://www.microsoft.com/Windows2000/downloads/critical/q329834/>
-todo.pl "z:\packages\win2ksp3\Q329834_W2K_SP4_X86_EN.exe /u /o /z" .reboot
+todo.pl "z:\packages\win2ksp3\Q329834_W2K_SP4_X86_EN.exe /u /o /z"
 
 :: IIS security Roll-up package Q327696
 :: <http://www.microsoft.com/Windows2000/downloads/security/q327696/>
@@ -57,11 +74,11 @@ todo.pl "z:\packages\win2ksp3\Q324380.exe /u /o /z"
 
 :: Q327269
 :: <http://www.microsoft.com/windows2000/downloads/recommended/q327269/>
-todo.pl "z:\packages\win2ksp3\Q327269_W2K_SP4_X86_EN.exe /u /o /z" .reboot
+todo.pl "z:\packages\win2ksp3\Q327269_W2K_SP4_X86_EN.exe /u /o /z"
 
 :: Q326886
 :: <http://www.microsoft.com/windows2000/downloads/critical/q326886/>
-todo.pl "z:\packages\win2ksp3\Q326886_W2K_SP4_X86_EN.exe /u /o /z" .reboot
+todo.pl "z:\packages\win2ksp3\Q326886_W2K_SP4_X86_EN.exe /u /o /z"
 
 :: Q326830
 :: <http://www.microsoft.com/windows2000/downloads/critical/q326830/>
