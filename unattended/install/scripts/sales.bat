c54af88d3e75bd71ba58f4a8ec8279384ed1e67e
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-17 22:59:32 +0000

    Rename office.bat to office2k.bat
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@315 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sales.bat
+++ unattended/install/scripts/sales.bat
@@ -1,4 +1,4 @@
 :: Install a salesperson laptop
 
 :: Do antivirus software last so it picks up Office and Notes
-todo.pl base.bat office.bat notes.bat att.bat shiva.bat symantec-antivirus.bat
+todo.pl base.bat office2k.bat notes.bat att.bat shiva.bat symantec-antivirus.bat
