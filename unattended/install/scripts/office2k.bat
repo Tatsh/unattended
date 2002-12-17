0cf6b99d9ff3ccb991d968e0f5b4b2ae81fa1daa
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-17 22:58:47 +0000

    Rename from office.bat.
    Modify to install hotfixes on client, instead of installation point,
    because it works better with OfficeUpdate Web site.

---
+++ unattended/install/scripts/office2k.bat
@@ -0,0 +1,20 @@
+:: Install Office 2000 and its updates
+
+:: Reboot after installing everything (superstition)
+todo.pl .reboot
+
+:: Install Service Pack 3
+:: See <http://support.microsoft.com/?kbid=326585>
+:: Download from:
+:: <http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/EN-US/o2ksp3.exe>
+todo.pl "z:\packages\o2ksp3.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\o2ksp3.txt /p MAINSP3.msp REBOOT=ReallySuppress\""
+
+:: Install SR-1a
+:: See <http://support.microsoft.com/?kbid=245025>
+:: and <http://support.microsoft.com/?kbid=258323>
+:: Download O2KSR1aDL.exe from:
+:: <http://www.microsoft.com/office/ork/2000/appndx/toolbox.htm#o2sr1au>
+:: Run it to extract the package.
+todo.pl "msiexec /qb /l* c:\netinst\logs\o2ksr1a.txt /p z:\packages\o2ksr1a\data1.msp REBOOT=ReallySuppress"
+
+todo.pl "msiexec /qb /l* c:\netinst\logs\office.txt /i z:\packages\office2k\data1.msi ADDLOCAL=ALL REBOOT=ReallySuppress NOUSERNAME=1 ALLUSERS=1 DISABLEADVTSHORTCUTS=1"
