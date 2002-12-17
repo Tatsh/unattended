1924037b2bd85f2f342dd0396963ae6be858f414
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-17 22:59:13 +0000

    Renamed to office2k.bat

--- unattended/install/scripts/office.bat
+++
@@ -1,7 +0,0 @@
-:: Install Office 2000 and security fixes
-
-:: FIXME: Do we really need to reboot?
-todo.pl .reboot office1.bat
-
-start /wait msiexec /qb /l* c:\netinst\logs\office.txt /i z:\office2k\data1.msi ADDLOCAL=ALL REBOOT=ReallySuppress NOUSERNAME=1 ALLUSERS=1 DISABLEADVTSHORTCUTS=1
-if errorlevel 1 exit 1
