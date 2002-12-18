c0c33abfeb9e8ce705c90d69b9611b33955c2648
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-18 22:40:37 +0000

    Remove openoffice.bat (for now)

--- unattended/install/scripts/base.bat
+++ unattended/install/scripts/base.bat
@@ -1,6 +1,6 @@
 :: Perform a basic workstation installation
 
-todo.pl hidepw.pl %WINVER%-notips.pl bootini.pl %WINVER%-updates.bat mozilla.bat vnc.bat acrobat-reader.bat 7-zip.pl openoffice.bat
+todo.pl hidepw.pl %WINVER%-notips.pl bootini.pl %WINVER%-updates.bat mozilla.bat vnc.bat acrobat-reader.bat 7-zip.pl
 
 :: Set IIS startup type to manual
 todo.pl "startup-type.pl manual IISADMIN" "startup-type.pl manual W3SVC"
