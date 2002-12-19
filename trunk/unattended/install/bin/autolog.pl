ae5b46b250f6496fd95a185208ae8a706ee1c856
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2002-12-19 17:40:24 +0000

    Fix horrible bug and documentation

--- trunk/unattended/install/bin/autolog.pl
+++ trunk/unattended/install/bin/autolog.pl
@@ -33,7 +33,7 @@ my %new_values = ('/DefaultUserName' => $opts{'user'},
 foreach my $name (sort keys %new_values) {
     my $val = $new_values{$name};
     if (defined $val) {
-        $winlogon_key->{$name} = [ REG_SZ, $val ]
+        $winlogon_key->{$name} = [ $val, REG_SZ ]
             or die "Unable to set $winlogon_key_name$name to $val: $^E";
     }
     else {
@@ -50,7 +50,7 @@ autolog.pl - Set defaults for Windows logon
 
 =head1 SYNOPSIS
 
-autolog.pl [ options ] <username> <password>
+autolog.pl [ options ]
 
 Options:
 
@@ -62,13 +62,44 @@ Options:
 
 =head1 NOTES
 
-This script patches the registry to set the values in the Windows
-logon panel, logging in automatically if the AutoAdminLogon key has a
-non-zero value.
+This script modifies the registry entries controlling automatic logon.
+These also control the default user name and domain name in the logon
+panel, even when automatic logon is disabled.
 
-Note that all of these values, including the password, are stored in
-the clear in the registry.
+Failing to specify an argument removes the corresponding registry key.
+You probably at least want to specify --logon=0, because otherwise
+Windows will not even set the defaults to be the last user who logged
+on.
 
-Also note that if the password is not set, the logon can still proceed
-if the account has an empty password.  However, Windows will
-"helpfully" set the AutoAdminLogon key back to 0 in this case.
+Note that all of these values, most notably the password, are stored
+as cleartext.
+
+Also note that if the password is not set, the automatic logon will
+still work if the account has an empty password.  However, Windows
+will "helpfully" set the AutoAdminLogon key back to 0 in this case,
+disabling automatic logon for next time.
+
+=head1 EXAMPLES
+
+To automatically log on to the local machine as user "Administrator"
+with password "sekrit":
+
+  autolog.pl --user=Administrator --password=sekrit --domain=%COMPUTERNAME% --logon=1
+
+To set the defaut user name and domain to MYDOM\someuser:
+
+  autolog.pl --user=someuser --domain=MYDOM --logon=0
+
+To disable automatic logon and clear the defaults, but retain the
+memory of who logged on last to set the defaults in future:
+
+  autolog.pl --logon=0
+
+To disable all memory of who logged on last, so the the user name must
+be typed every time:
+
+  autolog.pl
+
+=head1 SEE ALSO
+
+  <http://support.microsoft.com/?kbid=234562>
