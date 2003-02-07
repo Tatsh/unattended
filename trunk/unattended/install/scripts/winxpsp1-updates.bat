1a54e82b636903876644ebc82ac18256f40b05a3
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-02-07 22:38:46 +0000

    Soup-to-nuts refresh.

--- trunk/unattended/install/scripts/winxpsp1-updates.bat
+++ trunk/unattended/install/scripts/winxpsp1-updates.bat
@@ -1,43 +1,110 @@
 :: Install all updates and hotfixes for Windows XP SP1
 
-:: Most of these were downloaded using the Windows Update Catalog,
-:: <http://windowsupdate.microsoft.com/catalog/>.  Some of them were
-:: just named "setup.exe", so I renamed them to something more
-:: descriptive.  You'll figure it out.
+:: Some of these can only be downloaded using the Windows Update
+:: Catalog, <http://windowsupdate.microsoft.com/catalog/>.
 
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
-:: Critical update Q323255
-:: <http://www.microsoft.com/Downloads/Release.asp?ReleaseID=41834>
+:: Critical update 811630
+:: "HTML Help Update to Limit Functionality ..."
+:: <http://support.microsoft.com/?kbid=811630>
+todo.pl "z:\packages\winxpsp1\Q811630_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Critical update 810833
+:: "Unchecked Buffer in the Locator Service Might Permit Code to Run"
+:: <http://support.microsoft.com/?kbid=810833>
+todo.pl "z:\packages\winxpsp1\Q810833_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Critical update 810577
+:: "Unchecked Buffer in Windows Redirector May Permit Privilege Elevation"
+:: <http://support.microsoft.com/?kbid=810577>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q810577_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+
+:: Critical update 810565
+:: "Better support for default Web browsers other than Internet Explorer"
+:: <http://support.microsoft.com/?kbid=810565>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q810565_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+
+:: Recommended update 810243
+:: "DirectShow Playback Support for Files Recorded with Windows XP Media Center Edition"
+:: <http://support.microsoft.com/?kbid=810243>
+:: (only available from Windows Update Catalog)
+todo.pl "z:\packages\winxpsp1\Q810243_WXP_SP2.exe /u /o /z"
+
+:: Critical update 810030
+:: "Flaw in Microsoft VM May Compromise Windows"
+:: <http://support.microsoft.com/?kbid=810030>
+:: <http://support.microsoft.com/?kbid=304930>
+:: (only available from Windows Update Catalog)
+todo.pl "z:\packages\winxpsp1\msjavwu.exe /c:\"javatrig.exe /exe_install /l /q\" /q:a /r:n" .reboot
+
+:: Critical update 329834
+:: "Unchecked Buffer in PPTP Implementation May Permit Denial-of-Service"
+:: <http://support.microsoft.com/?kbid=329834>
+todo.pl "z:\packages\winxpsp1\Q329834_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Critical update 329390
+:: "Unchecked Buffer in Windows Shell Might Permit System Compromise"
+:: <http://support.microsoft.com/?kbid=329390>
+todo.pl "z:\packages\winxpsp1\Q329390_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Critical update 329170
+:: "Flaw in SMB Signing May Permit Group Policy to Be Modified"
+:: <http://support.microsoft.com/?kbid=329170>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q329170_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+
+:: Critical update 329115
+:: "Certificate Validation Flaw Might Permit Identity Spoofing"
+:: <http://support.microsoft.com/?kbid=329115>
+todo.pl "z:\packages\winxpsp1\Q329115_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Critical update 329048
+:: "Unchecked Buffer in File Decompression Functions May Allow Attacker ..."
+:: <http://support.microsoft.com/?kbid=329048>
+todo.pl "z:\packages\winxpsp1\Q329048_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Critical update 328310
+:: "Flaw in Windows WM_TIMER Message Handling Can Enable Privilege Elevation"
+:: <http://support.microsoft.com/?kbid=328310>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q328310_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+
+:: Recommended update 327979
+:: "Game Stops Responding (Hangs) or Quits Unexpectedly ..."
+:: <http://support.microsoft.com/?kbid=327979>
+todo.pl "z:\packages\winxpsp1\Q327979_WXP_SP2.exe /u /o /z"
+
+:: Critical update 327696
+:: "October 2002 Cumulative Patch for Internet Information Services"
+:: <http://support.microsoft.com/?kbid=327696>
+todo.pl "z:\packages\winxpsp1\Q327696_WXP_SP2_x86_ENU.exe /u /o /z"
+
+:: Recommended update 327405
+:: "Windows XP Documentation Update Can Help You Understand How to Make Your Compute
+:: <http://support.microsoft.com/?kbid=327405>
+:: (only available from Windows Update Catalog)
+todo.pl "z:\packages\winxpsp1\hu1002_pro.exe /q"
+
+:: Critical update 323255
+:: "Unchecked Buffer in Windows Help Facility May Allow Attacker to Run Code"
+:: <http://support.microsoft.com/?kbid=323255>
 todo.pl "z:\packages\winxpsp1\Q323255_WXP_SP2_x86_ENU.exe /u /o /z"
 
-:: Movie Maker 1.2.1 update
-:: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/5509.htm>
-todo.pl "z:\packages\winxpsp1\movie-maker-1.2.1.exe /q"
+:: Recommended update 322011
+:: "You Cannot Preview a Fax in the Fax Console"
+:: <http://support.microsoft.com/?kbid=323255>
+todo.pl "z:\packages\winxpsp1\Q322011_WXP_SP2_x86_ENU.exe /u /o /z"
 
-:: Microsoft .NET framework Service Pack 2
-:: <http://msdn.microsoft.com/netframework/downloads/updates/sp/>
-:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q321884>
-:: (Always seems to exit with status 194)
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\NDP10_SP_Q321884_En.exe /q"
+:: IE updates
 
-:: Microsoft .NET framework
-:: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/4812.htm>
-:: <http://msdn.microsoft.com/netframework/downloads/>
-:: For silent install, see:
-:: <http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dnnetdep/html/dotnetfxref.asp>
-todo.pl "z:\packages\winxpsp1\dotnetfx.exe /q /c:\"install /q\""
+:: Critical update 810847
+:: "February, 2003, Cumulative Patch for Internet Explorer"
+:: <http://support.microsoft.com/?kbid=810847>
+:: <http://www.microsoft.com/windows/ie/downloads/critical/810847/>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\q810847.exe /q /r:n" .reboot
 
-:: Euro conversion tool update
-:: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/5026.htm>
-todo.pl "z:\packages\winxpsp1\euro-tool.exe /q"
-
-:: Recommended Update Q327405
-:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q327405>
-todo.pl "Z:\packages\winxpsp1\hu1002_pro.exe /Q /R:N" .reboot
-
-:: Critical Update Q329077
-:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q329077>
-:: (security bulletin says reboot required)
-todo.pl "Z:\packages\winxpsp1\vm-sfix3.exe /Q /R:N" .reboot
+:: Critical update 318089
+:: "Incorrect VBScript Handling in Internet Explorer Can Allow Web Pages ..."
+:: <http://support.microsoft.com/?kbid=318089>
+:: <http://www.microsoft.com/windows/ie/downloads/critical/q318089/>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\vbs56nen.exe /q /r:n" .reboot
