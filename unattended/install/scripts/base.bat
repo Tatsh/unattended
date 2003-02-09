4ddc034359ccbcf54fcf4f4bb44dfeb57841a39a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-02-09 22:27:25 +0000

    Add PHP to base install.

--- unattended/install/scripts/base.bat
+++ unattended/install/scripts/base.bat
@@ -1,6 +1,6 @@
 :: Perform a basic workstation installation
 
-todo.pl hidepw.pl %WINVER%-notips.pl bootini.pl %WINVER%-updates.bat mozilla.bat vnc.bat acrobat-reader.bat 7-zip.pl
+todo.pl hidepw.pl %WINVER%-notips.pl bootini.pl %WINVER%-updates.bat mozilla.bat vnc.bat acrobat-reader.bat 7-zip.pl php.bat
 
 :: Set IIS startup type to manual
 todo.pl "startup-type.pl manual IISADMIN" "startup-type.pl manual W3SVC"
