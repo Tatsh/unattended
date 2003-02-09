d5c887b6e6246b49fa6d4fd3a61caf2ae385c073
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-02-09 22:29:21 +0000

    Upgrade to Office XP, and use with-env.pl to allow calculation of license key.

--- unattended/install/scripts/sales.bat
+++ unattended/install/scripts/sales.bat
@@ -1,4 +1,10 @@
 :: Install a salesperson laptop
 
 :: Do antivirus software last so it picks up Office and Notes
-todo.pl base.bat office2k.bat notes.bat att.bat shiva.bat symantec-antivirus.bat
+todo.pl notes.bat att.bat shiva.bat symantec-antivirus.bat
+
+:: Install Office XP with automatically determined license key
+todo.pl "with-env.pl -n z:\site\officexp-key.pl officexp.bat"
+
+:: Start with base install
+todo.pl base.bat
