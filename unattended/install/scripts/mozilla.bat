b0d7c6565b01d09575a259068f4040d8a90cf99e
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-02-10 20:13:57 +0000

    Add -ira flag (thanks to Marcus Greil)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@386 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/mozilla.bat
+++ unattended/install/scripts/mozilla.bat
@@ -1,6 +1,3 @@
 :: Install Mozilla
 
-:: The Mozilla installer exits before it is really finished.
-todo.pl ".sleep 120"
-
-todo.pl "z:\packages\mozilla-win32-1.2.1-installer.exe -ma"
+todo.pl "z:\packages\mozilla-win32-1.2.1-installer.exe -ma -ira"
