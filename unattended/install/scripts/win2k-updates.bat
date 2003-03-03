65711cfc1ac8cee9a94d3177d490d34dab4afc4b
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-03-03 13:52:25 +0000

    Er, win2ksp3-updates.bat, not winxpsp3-updates.bat

--- unattended/install/scripts/win2k-updates.bat
+++ unattended/install/scripts/win2k-updates.bat
@@ -1,7 +1,7 @@
 :: Install all updates for Windows 2000
 
 :: After installing SP3, install updates for winxpsp3.
-todo.pl winxpsp3-updates.bat
+todo.pl win2ksp3-updates.bat
 
 :: Install Service Pack 3.
 :: See <http://support.microsoft.com/?kbid=262839> for a description
