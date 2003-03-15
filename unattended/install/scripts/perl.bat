021919d118a72175f9cc1455eb6c4e528678cfca
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-03-15 22:46:43 +0000

    Rev to build 805

--- unattended/install/scripts/perl.bat
+++ unattended/install/scripts/perl.bat
@@ -3,5 +3,5 @@
 :: (The latter is undocumented; see
 :: http://bugs.activestate.com/show_bug.cgi?id=20364)
 
-start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i z:\packages\ActivePerl-5.8.0.804-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
+start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i z:\packages\ActivePerl-5.8.0.805-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
 if errorlevel 1 exit 1
