faef39176ed8b6aceac0830145b4dd78b32e65bf
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-03-15 22:18:47 +0000

    Use todo.pl exclusively.  Rev to Jan 2003 version.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@437 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/msdn.bat
+++ unattended/install/scripts/msdn.bat
@@ -6,5 +6,4 @@ todo.pl .reboot
 :: Use UNC path to installer, because MSDN library depends on the
 :: network path from which it was installed, and users do not normally
 :: map the Z: drive.
-start /wait msiexec /qb /l* c:\netinst\logs\msdn.txt /i %Z_PATH%\packages\msdn\Apr2002\msdn.msi SETUP_EXE=yes
-if errorlevel 1 exit 1
+todo.pl "msiexec /qb /l* c:\netinst\logs\msdn.txt /i %Z_PATH%\packages\msdn\Jan2003\msdn.msi SETUP_EXE=yes"
