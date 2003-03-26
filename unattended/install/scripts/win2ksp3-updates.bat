097ea609d8b890198dc7f846a539c3d712c573ec
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-03-26 21:45:58 +0000

    Install IE6 before most updates, because some of the updates are for IE.  (Yuck.  Need to clean this up.)

--- unattended/install/scripts/win2ksp3-updates.bat
+++ unattended/install/scripts/win2ksp3-updates.bat
@@ -4,4 +4,4 @@
 :: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/5026.htm>
 todo.pl "z:\packages\win2ksp3\euro-tool.exe /q"
 
-todo.pl win2ksp3-hotfixes.bat ie6.bat directx81.bat
+todo.pl ie6.bat win2ksp3-hotfixes.bat directx81.bat
