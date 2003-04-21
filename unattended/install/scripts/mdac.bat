64229f225e517e27b422a530ff72d77062565465
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-04-21 16:44:29 +0000

    Add a comment

--- unattended/install/scripts/mdac.bat
+++ unattended/install/scripts/mdac.bat
@@ -1,3 +1,5 @@
 :: Install latest version of Microsoft Data Access Components
 
+:: Download from <http://www.microsoft.com/data/>.  Rename
+:: mdac_typ.exe to something more descriptive.
 todo.pl "z:\packages\mdac-2.7sp1.exe /q /c:\"setup /qn1\""
