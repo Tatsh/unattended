c64a804382520553f3f984940faf7520d7f8fce0
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-04-21 22:46:19 +0000

    Get rid of /o ("overwrite OEM files without prompting") flag, because I now believe it does more harm than good.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@464 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/win2ksp3-hotfixes.bat
+++ unattended/install/scripts/win2ksp3-hotfixes.bat
@@ -12,12 +12,12 @@ todo.pl .reboot
 :: Critical update 816093
 :: "Flaw in the Microsoft VM Could Enable System Compromise"
 :: <http://support.microsoft.com/?kbid=816093>
-todo.pl "z:\packages\win2ksp3\Q816093_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q816093_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical update 815021
 :: "Unchecked Buffer in Windows Component May Cause Web Server Compromise"
 :: <http://support.microsoft.com/?kbid=815021>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q815021_W2K_SP4_x86_EN.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q815021_W2K_SP4_x86_EN.exe /u /z" .reboot
 
 :: Critical update 814078
 :: "Flaw in Windows Script Engine May Allow Code to Run"
@@ -28,12 +28,12 @@ todo.pl ".ignore-err 194 z:\packages\win2ksp3\js56nen.exe /q /r:n" .reboot
 :: "Cannot Install Driver Updates from the Windows Update Web Site"
 :: (only available from <http://windowsupdate.microsoft.com/catalog/>)
 :: <http://support.microsoft.com/?kbid=814033>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q814033_W2K_SP4_x86_EN.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q814033_W2K_SP4_x86_EN.exe /u /z" .reboot
 
 :: Critical Update 811630
 :: "HTML Help Update to Limit Functionality ..."
 :: <http://support.microsoft.com/?kbid=811630>
-todo.pl "z:\packages\win2ksp3\Q811630_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q811630_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical Update 813951
 :: "February 2003, Update for Internet Explorer 6 SP1"
@@ -50,41 +50,41 @@ todo.pl ".ignore-err 194 z:\packages\win2ksp3\q810847.exe /q /r:n" .reboot
 :: Critical Update 810833
 :: "Unchecked Buffer in the Locator Service Might Permit Code to Run"
 :: <http://support.microsoft.com/?kbid=810833>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q810833_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q810833_W2K_SP4_X86_EN.exe /u /z"
 
 :: Recommended Update 329251
 :: "Performance Improvement Fix for Windows NAT Solutions"
 :: <http://support.microsoft.com/?kbid=329251>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q329251_W2K_SP4.exe /u /o /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q329251_W2K_SP4.exe /u /z"
 
 :: Critical Update 810649 (funny)
 :: "Hyperlinks Open in Internet Explorer Instead of in Default Browser"
 :: (only available from <http://windowsupdate.microsoft.com/catalog/>)
 :: <http://support.microsoft.com/?kbid=810649>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q810649_W2K_SP4.exe /u /o /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q810649_W2K_SP4.exe /u /z"
 
 :: Critical Update 329170/309376
 :: "Flaw in SMB Signing Could Enable Group Policy to be Modified"
 :: <http://support.microsoft.com/?kbid=329170>
 :: <http://support.microsoft.com/?kbid=309376>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q329170_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q329170_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical Update 810030
 :: "Flaw in Microsoft VM May Compromise Windows"
 :: <http://support.microsoft.com/?kbid=810030>
 :: (only available from <http://windowsupdate.microsoft.com/catalog/>)
-todo.pl "z:\packages\win2ksp3\Q810030_W2K.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q810030_W2K.exe /u /z"
 
 :: Critical Update Q328310
 :: "Flaw in Windows WM_TIMER Message Handling Could Enable Privilege Elevation"
 :: <http://support.microsoft.com/?kbid=322913>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q328310_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q328310_W2K_SP4_X86_EN.exe /u /z"
 
 :: Recommended update Q322913
 :: "WM_TIMER Messages Not Delivered to Programs"
 :: <http://www.microsoft.com/windows2000/downloads/recommended/q322913/>
 :: <http://support.microsoft.com/?kbid=322913>
-todo.pl "z:\packages\win2ksp3\Q322913_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q322913_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical Update Q329414
 :: "Buffer Overrun in MDAC Can Lead to Code Execution (MDAC 2.6)"
@@ -95,30 +95,30 @@ todo.pl "z:\packages\win2ksp3\q329414_mdacall_x86.exe /q /c:\"dahotfix /q /n\"" 
 :: "Certificate Validation Flaw Might Permit Identity Spoofing"
 :: <http://support.microsoft.com/?kbid=329115>
 :: <http://www.microsoft.com/windows2000/downloads/critical/q329115/>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q329115_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\Q329115_W2K_SP4_X86_EN.exe /u /z"
 
 :: Not-so-critical update Q328523
 :: "default invocation of IE from the Internet Connection Wizard"
 :: <http://support.microsoft.com/?kbid=328523>
 :: <http://www.microsoft.com/Downloads/Release.asp?ReleaseID=42457>
-todo.pl "z:\packages\win2ksp3\Q328523_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q328523_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical Update Q329834
 :: "Unchecked Buffer in PPTP Implementation Could Enable Denial of Service"
 :: <http://support.microsoft.com/?kbid=329834>
 :: <http://www.microsoft.com/Windows2000/downloads/critical/q329834/>
-todo.pl "z:\packages\win2ksp3\Q329834_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q329834_W2K_SP4_X86_EN.exe /u /z"
 
 :: IIS security Roll-up package Q327696
 :: <http://support.microsoft.com/?kbid=327696>
 :: <http://www.microsoft.com/Windows2000/downloads/security/q327696/>
-todo.pl "z:\packages\win2ksp3\Q327696_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q327696_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical Update Q323255
 :: "Unchecked Buffer in Windows Help Facility May Allow Attacker to Run Code"
 :: <http://support.microsoft.com/?kbid=323255>
 :: <http://www.microsoft.com/Windows2000/downloads/critical/q323255/>
-todo.pl "z:\packages\win2ksp3\Q323255_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q323255_W2K_SP4_X86_EN.exe /u /z"
 
 :: Critical Update Q329077
 :: "Flaw in Microsoft VM JDBC Classes Might Permit Code to Be Run"
@@ -128,28 +128,28 @@ todo.pl "Z:\packages\win2ksp3\vm-sfix3.exe /Q /R:N" .reboot
 
 :: Q324096
 :: <http://www.microsoft.com/Windows2000/downloads/security/q324096/>
-todo.pl "z:\packages\win2ksp3\Q324096_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q324096_W2K_SP4_X86_EN.exe /u /z"
 
 :: Q324380
 :: <http://www.microsoft.com/windows2000/downloads/security/q324380/>
-todo.pl "z:\packages\win2ksp3\Q324380.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q324380.exe /u /z"
 
 :: Q327269
 :: <http://www.microsoft.com/windows2000/downloads/recommended/q327269/>
-todo.pl "z:\packages\win2ksp3\Q327269_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q327269_W2K_SP4_X86_EN.exe /u /z"
 
 :: Q326886
 :: <http://www.microsoft.com/windows2000/downloads/critical/q326886/>
-todo.pl "z:\packages\win2ksp3\Q326886_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q326886_W2K_SP4_X86_EN.exe /u /z"
 
 :: Q326830
 :: <http://www.microsoft.com/windows2000/downloads/critical/q326830/>
-todo.pl "z:\packages\win2ksp3\Q326830_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q326830_W2K_SP4_X86_EN.exe /u /z"
 
 :: Q323172
 :: <http://www.microsoft.com/windows2000/downloads/critical/q323172/>
-todo.pl "z:\packages\win2ksp3\Q323172_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q323172_W2K_SP4_X86_EN.exe /u /z"
 
 :: Q322842
 :: <http://www.microsoft.com/windows2000/downloads/recommended/q322842/>
-todo.pl "z:\packages\win2ksp3\Q322842_W2K_SP4_X86_EN.exe /u /o /z"
+todo.pl "z:\packages\win2ksp3\Q322842_W2K_SP4_X86_EN.exe /u /z"
--- unattended/install/scripts/winxpsp1-updates.bat
+++ unattended/install/scripts/winxpsp1-updates.bat
@@ -10,7 +10,7 @@
 :: "Windows Update 643 Error and the Catalog Database"
 :: <http://support.microsoft.com/?kbid=817287>
 :: (download only available from Windows Catalog)
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q817287_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q817287_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Critical update 816093
 :: "Flaw in the Microsoft VM Could Enable System Compromise"
@@ -21,17 +21,17 @@ todo.pl "z:\packages\winxpsp1\msjavwu.exe /q:a /r:n"
 :: Critical update 331953
 :: "Flaw in RPC Endpoint Mapper Could Allow Denial of Service Attacks"
 :: <http://support.microsoft.com/?kbid=331953>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q331953_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q331953_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Recommended update 815485
 :: WPA Wireless Security Update for Windows XP
 :: <http://support.microsoft.com/?kbid=815485>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q815485_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q815485_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Critical update 811493
 :: "Buffer Overrun ... Could Lead to Elevated Privileges"
 :: <http://support.microsoft.com/?kbid=811493>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q811493_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q811493_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Critical update 814078
 :: "Flaw in Windows Script Engine May Allow Code to Run"
@@ -41,13 +41,13 @@ todo.pl ".ignore-err 194 z:\packages\winxpsp1\js56nen.exe /q /r:n" .reboot
 :: Critical update 814033
 :: "Cannot Install Driver Updates from the Windows Update Web Site"
 :: <http://support.microsoft.com/?kbid=814033>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q814033_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q814033_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Recommended update 814995
 :: "Some ... Fixes Stop Working After You Install the 328310 Update"
 :: <http://support.microsoft.com/?kbid=814995>
 :: (download only available from Windows Update Catalog)
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q814995_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q814995_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Critical update 813951
 :: "You Cannot Access ... or Authenticate with a Web Site in Various Programs"
@@ -57,74 +57,74 @@ todo.pl ".ignore-err 194 z:\packages\winxpsp1\q813951.exe /q /r:n" .reboot
 :: Critical update 811630
 :: "HTML Help Update to Limit Functionality ..."
 :: <http://support.microsoft.com/?kbid=811630>
-todo.pl "z:\packages\winxpsp1\Q811630_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q811630_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Critical update 810833
 :: "Unchecked Buffer in the Locator Service Might Permit Code to Run"
 :: <http://support.microsoft.com/?kbid=810833>
-todo.pl "z:\packages\winxpsp1\Q810833_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q810833_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Critical update 810577
 :: "Unchecked Buffer in Windows Redirector May Permit Privilege Elevation"
 :: <http://support.microsoft.com/?kbid=810577>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q810577_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q810577_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Critical update 810565
 :: "Better support for default Web browsers other than Internet Explorer"
 :: <http://support.microsoft.com/?kbid=810565>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q810565_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q810565_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Recommended update 810243
 :: "DirectShow Playback Support for Files Recorded with Windows XP Media Center Edition"
 :: <http://support.microsoft.com/?kbid=810243>
 :: (only available from Windows Update Catalog)
-todo.pl "z:\packages\winxpsp1\Q810243_WXP_SP2.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q810243_WXP_SP2.exe /u /z"
 
 :: Critical update 329834
 :: "Unchecked Buffer in PPTP Implementation May Permit Denial-of-Service"
 :: <http://support.microsoft.com/?kbid=329834>
-todo.pl "z:\packages\winxpsp1\Q329834_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q329834_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Critical update 329441
 :: "You Cannot Create a Network Connection After You Restore Windows XP"
 :: <http://support.microsoft.com/?kbid=329441>
 :: (download only available from Windows Update Catalog)
-todo.pl "z:\packages\winxpsp1\Q329441_WXP_SP2.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q329441_WXP_SP2.exe /u /z"
 
 :: Critical update 329390
 :: "Unchecked Buffer in Windows Shell Might Permit System Compromise"
 :: <http://support.microsoft.com/?kbid=329390>
-todo.pl "z:\packages\winxpsp1\Q329390_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q329390_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Critical update 329170
 :: "Flaw in SMB Signing May Permit Group Policy to Be Modified"
 :: <http://support.microsoft.com/?kbid=329170>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q329170_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q329170_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Critical update 329115
 :: "Certificate Validation Flaw Might Permit Identity Spoofing"
 :: <http://support.microsoft.com/?kbid=329115>
-todo.pl "z:\packages\winxpsp1\Q329115_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q329115_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Critical update 329048
 :: "Unchecked Buffer in File Decompression Functions May Allow Attacker ..."
 :: <http://support.microsoft.com/?kbid=329048>
-todo.pl "z:\packages\winxpsp1\Q329048_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q329048_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Critical update 328310
 :: "Flaw in Windows WM_TIMER Message Handling Can Enable Privilege Elevation"
 :: <http://support.microsoft.com/?kbid=328310>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q328310_WXP_SP2_x86_ENU.exe /u /o /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\Q328310_WXP_SP2_x86_ENU.exe /u /z" .reboot
 
 :: Recommended update 327979
 :: "Game Stops Responding (Hangs) or Quits Unexpectedly ..."
 :: <http://support.microsoft.com/?kbid=327979>
-todo.pl "z:\packages\winxpsp1\Q327979_WXP_SP2.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q327979_WXP_SP2.exe /u /z"
 
 :: Critical update 327696
 :: "October 2002 Cumulative Patch for Internet Information Services"
 :: <http://support.microsoft.com/?kbid=327696>
-todo.pl "z:\packages\winxpsp1\Q327696_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q327696_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Recommended update 327405
 :: "Windows XP Documentation Update Can Help You Understand How to Make Your Compute
@@ -135,12 +135,12 @@ todo.pl "z:\packages\winxpsp1\hu1002_pro.exe /q"
 :: Critical update 323255
 :: "Unchecked Buffer in Windows Help Facility May Allow Attacker to Run Code"
 :: <http://support.microsoft.com/?kbid=323255>
-todo.pl "z:\packages\winxpsp1\Q323255_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q323255_WXP_SP2_x86_ENU.exe /u /z"
 
 :: Recommended update 322011
 :: "You Cannot Preview a Fax in the Fax Console"
 :: <http://support.microsoft.com/?kbid=322011>
-todo.pl "z:\packages\winxpsp1\Q322011_WXP_SP2_x86_ENU.exe /u /o /z"
+todo.pl "z:\packages\winxpsp1\Q322011_WXP_SP2_x86_ENU.exe /u /z"
 
 :: IE updates
 
