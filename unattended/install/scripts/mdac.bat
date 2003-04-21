f4ddaefdd10c312bdc2b44a41a741e52216c4ac2
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-04-21 16:43:33 +0000

    Rev to 2.7sp1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@460 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/mdac.bat
+++ unattended/install/scripts/mdac.bat
@@ -1,4 +1,3 @@
 :: Install latest version of Microsoft Data Access Components
 
-start /wait z:\packages\mdac-2.7.exe /q /c:"setup /qn1"
-if errorlevel 1 exit 1
+todo.pl "z:\packages\mdac-2.7sp1.exe /q /c:\"setup /qn1\""
