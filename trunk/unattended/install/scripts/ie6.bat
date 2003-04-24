e60c46dc810fda9016ba1df6b4e131ae012aaadc
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-04-24 19:46:01 +0000

    Add latest round of hotfixes.  Comment out obsolete (?) hotfixes.

--- trunk/unattended/install/scripts/ie6.bat
+++ trunk/unattended/install/scripts/ie6.bat
@@ -3,11 +3,22 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Critical update 813489
+:: "April, 2003, Cumulative Patch for Internet Explorer"
+:: <http://support.microsoft.com/?kbid=813489>
+:: <http://www.microsoft.com/windows/ie/downloads/critical/813489/>
+todo.pl "z:\packages\win2ksp3\q813489.exe /q /r:n"
+
+:: Critical update 330994
+:: "April, 2003, Cumulative patch for Outlook Express"
+:: <http://support.microsoft.com/?kbid=330994>
+todo.pl "z:\packages\win2ksp3\q330994.exe /q /r:n"
+
 :: Cumulative patch for Internet Explorer (324929)
 :: <http://www.microsoft.com/technet/security/bulletin/MS02-068.asp>
 :: <http://support.microsoft.com/?kbid=324929>
 :: <http://www.microsoft.com/windows/ie/downloads/critical/q324929/>
-todo.pl ".ignore-err 194 z:\packages\win2ksp3\q324929.exe /q:a" .reboot
+:todo.pl ".ignore-err 194 z:\packages\win2ksp3\q324929.exe /q:a" .reboot
 
 :: Critical update Q318203/Q317244
 :: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
