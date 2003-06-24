ae51efdf1486254af570284e62b94542204efa8d
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-06-24 20:58:49 +0000

    Delete C:\Perl\bin\HEAD, since it conflicts with C:\Cygwin\bin\head.exe

--- unattended/install/scripts/developer.bat
+++ unattended/install/scripts/developer.bat
@@ -1,3 +1,7 @@
 :: Install a developer workstaion
 
+:: C:\Perl\bin\HEAD conflicts with Cygwin version (because Windows is
+:: case-insensitive), so delete it.
+del C:\Perl\bin\HEAD
+
 todo.pl base.bat emacs.bat cygwin.bat vc6.bat msdn.bat %WINVER%-symbols.bat symantec-antivirus.bat
