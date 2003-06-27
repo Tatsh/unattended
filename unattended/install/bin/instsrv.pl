b5cd28fe23b3d7feec68d872024968ad08a34804
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-06-27 19:27:57 +0000

    Use WMI instead of instsrv.exe ResKit tool.

--- unattended/install/bin/instsrv.pl
+++ unattended/install/bin/instsrv.pl
@@ -2,23 +2,53 @@
 
 use warnings;
 use strict;
+use Win32::OLE;
+#use Term::ReadKey;
 
 @ARGV == 3
-    or die "usage: $0 <service> <command> <account name>";
+    or die "usage: $0 <service> <command> <account name>\n";
 
 my ($service, $cmd, $acct) = @ARGV;
 
 print "*** Now configuring $service\n";
-print "Enter password for $acct: ";
-my $pass = <STDIN>;
-chomp $pass;
 
-if (0 != system 'instsrv', $service, $cmd, '-a', $acct, '-p', $pass) {
-    # instsrv always exits 1
-    # But it prints a success message, which we could check
-    # here... FIXME
-    $? == 256
-        or die "instsrv failed: ", ($? ? $? : $^E);
+#ReadMode 2;
+
+my $pass;
+while (1) {
+    print "Enter password for $acct: ";
+    $pass = <STDIN>;
+    chomp $pass;
+#    print "\nRe-type password: ";
+#    my $pass2 = <STDIN>;
+    my $pass2 = $pass;
+#    chomp $pass2;
+    $pass eq $pass2
+        and last;
+    print "Passwords do not match!\nTry again.\n\n";
 }
+
+#ReadMode 0;
+
+# Bomb out completely if COM engine encounters any trouble.
+Win32::OLE->Option ('Warn' => 3);
+
+# Get the Win32_Service class.
+# <http://msdn.microsoft.com/library/en-us/wmisdk/wmi/win32_service.asp>
+my $srv_class = Win32::OLE->GetObject ('WinMgmts:Win32_Service');
+my $ret = $srv_class->Create
+    ($service,
+     $service,
+     $cmd,
+     16,
+     1,
+     'Manual',
+     0,
+     $acct,
+     $pass
+     );
+
+$ret == 0
+    or die "Win32_Service::Create failed; code == $ret";
 
 exit 0;
