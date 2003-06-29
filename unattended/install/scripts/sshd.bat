1af2e1ae33bbdd97a03a38f8b533bf677bd64cc8
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-06-29 17:09:15 +0000

    Clean up file
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@510 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sshd.bat
+++ unattended/install/scripts/sshd.bat
@@ -10,8 +10,4 @@
 :: cygwin-sshd service to run srvany.exe as curlbot.  Load the
 :: registry with the srvany arguments.  Set some useful environment
 :: variables.
-todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "instsrv.pl cygwin-sshd c:\ntreskit\srvany.exe CURL\curlbot" "regedit /s z:\scripts\sshd.reg" "setenv.pl CYGWIN tty"
-
-:: Create the sshd keys, if needed.
-set cyg=c:\cygwin
-todo.pl "if not exist %cyg%\etc\ssh_host_key %cyg%\bin\ssh-keygen -t rsa1 -N '' -f /etc/ssh_host_key" "if not exist %cyg%\etc\ssh_host_rsa_key %cyg%\bin\ssh-keygen -t rsa -N '' -f /etc/ssh_host_rsa_key" "if not exist %cyg%\etc\ssh_host_dsa_key %cyg%\bin\ssh-keygen -t dsa -N '' -f /etc/ssh_host_dsa_key"
+todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "instsrv.pl cygwin-sshd c:\ntreskit\srvany.exe CURL\curlbot" "regedit /s z:\scripts\sshd.reg"
