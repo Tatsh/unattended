cfaffcb9a59136a13c70c0d03feb366e4f80abfb
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-06-30 21:59:17 +0000

    Use c:\cygwin\cygwin.bat for ESHELL, since it works better (by spawning a "login" shell).

--- trunk/unattended/install/scripts/cygwin.bat
+++ trunk/unattended/install/scripts/cygwin.bat
@@ -2,6 +2,6 @@
 
 :: After installing Cygwin package, configure it.
 :: (ESHELL is used by Emacs.)
-todo.pl "setenv.pl ESHELL c:\cygwin\bin\bash.exe" "setenv.pl CYGWIN tty" cygwin-1.bat
+todo.pl "setenv.pl ESHELL c:\cygwin\cygwin.bat" "setenv.pl CYGWIN tty" cygwin-1.bat
 
 todo.pl "AutoIt.exe z:\scripts\cygwin.aut"
