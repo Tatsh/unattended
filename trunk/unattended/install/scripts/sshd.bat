0677d537d8dfb60fd75eae64e362f5f65d2e852e
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-07 17:48:48 +0000

    Put call to "ntrights" back in.  (Must eliminate this ResKit dependency someday.)

--- trunk/unattended/install/scripts/sshd.bat
+++ trunk/unattended/install/scripts/sshd.bat
@@ -1,14 +1,11 @@
 :: Install sshd, set it up as a service, etc.
 
 :: Note: This script presupposes the presense of the Resource Kit (for
-:: ntrights.exe) and Cygwin (for sshd itself).
+:: ntrights.exe) and Cygwin (for cygrunsrv and sshd).
 
 :: Also note: The "curlbot" account is used internally for performing
 :: builds.  Any account could be used.
 
 :: Grant curlbot the right to log on as a service.  Create the
-:: cygwin-sshd service to run srvany.exe as curlbot.  Load the
-:: registry with the srvany arguments.
-::todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "instsrv.pl cygwin-sshd c:\ntreskit\srvany.exe CURL\curlbot" "regedit /s z:\scripts\sshd.reg"
-
-todo.pl "c:\cygwin\bin\bash --login -c 'cygrunsrv -I cygwin-sshd -p /usr/local/etc/cygwin-sshd.sh -o -c / -u CURL\\\\curlbot'"
+:: cygwin-sshd service to run as curlbot.
+todo.pl "ntrights -u CURL\curlbot +r SeServiceLogonRight" "c:\cygwin\bin\bash --login -c 'cygrunsrv -I cygwin-sshd -p /usr/local/etc/cygwin-sshd.sh -o -c / -u CURL\\\\curlbot'"
