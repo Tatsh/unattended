66bd812f93b2726a5328e92b39301e46c1ff61b2
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-10 18:35:29 +0000

    Update for Win2k SP4
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@600 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/win2k-updates.bat
+++ unattended/install/scripts/win2k-updates.bat
@@ -1,9 +1,9 @@
 :: Install all updates for Windows 2000
 
-:: After installing SP3, install updates for winxpsp3.
-todo.pl win2ksp3-updates.bat
+:: After installing SP4, install updates for win2ksp4
+todo.pl win2ksp4-updates.bat
 
-:: Install Service Pack 3.
+:: Install Service Pack 4.
 :: See <http://support.microsoft.com/?kbid=262839> for a description
 :: of the switches.
-todo.pl "z:\packages\win2k\sp3\i386\update\update.exe /n /o /q /u /z" .reboot
+todo.pl "z:\packages\win2k\sp4\i386\update\update.exe /n /o /q /u /z" .reboot
