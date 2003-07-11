9975e087fe8795b22cdb0e8bb5d1781479b60fd1
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-11 19:09:30 +0000

    Add some required fixes back in.  (I wish "cumulative" updates were cumulative.)

--- unattended/install/scripts/ie6.bat
+++ unattended/install/scripts/ie6.bat
@@ -8,9 +8,15 @@
 :: <http://support.microsoft.com/?kbid=818529>
 todo.pl ".ignore-err 194 z:\packages\ie6sp1\q818529.exe /q /r:n" .reboot
 
+:: Critical update 330994
+:: "April 2003, Cumulative Patch for Outlook Express"
+:: <http://support.microsoft.com/?kbid=330994>
+:: <http://www.microsoft.com/windows/ie/downloads/critical/330994/>
+todo.pl "z:\packages\ie6sp1\q330994.exe /q /r:n"
+
 :: Critical update Q318203/Q317244
 :: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
 :: <http://support.microsoft.com/?kbid=318203>
-::todo.pl .reboot "z:\packages\win2ksp4\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
+todo.pl "z:\packages\win2ksp4\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
 
 todo.pl ".ignore-err 194 z:\packages\ie6\flat\win32\en\IE6setup.exe /q /r:n" .reboot
