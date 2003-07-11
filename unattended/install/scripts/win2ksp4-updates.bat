7d0ab6049d70f10810b93270ae3ab22049090270
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-11 15:08:24 +0000

    Populate with current fixes. Include IE6SP1 and DirectX 8.1.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@601 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/win2ksp4-updates.bat
+++ unattended/install/scripts/win2ksp4-updates.bat
@@ -1,1 +1,15 @@
+:: Install updates for Win2k SP4
 
+:: Some of these can only be downloaded using the Windows Update
+:: Catalog, <http://windowsupdate.microsoft.com/catalog/>.
+
+:: Note that since we are pushing these commands onto the to-do stack,
+:: they will be executed in the opposite order.
+
+:: Critical update 823559
+:: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
+:: <http://support.microsoft.com/?kbid=823559>
+todo.pl "z:\packages\win2ksp4\Windows2000-KB823559-x86-ENU.exe /u /z"
+
+:: Install IE6 and DirectX 8.1
+todo.pl ie6.bat directx81.bat
