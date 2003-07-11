a756bf1b737cf9e92b37b75dff0b73654ee6df38
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-11 19:08:45 +0000

    Latest round of fixes

--- unattended/install/scripts/win2ksp4-updates.bat
+++ unattended/install/scripts/win2ksp4-updates.bat
@@ -11,5 +11,16 @@
 :: <http://support.microsoft.com/?kbid=823559>
 todo.pl "z:\packages\win2ksp4\Windows2000-KB823559-x86-ENU.exe /u /z"
 
+:: Recommended update 818043
+:: "L2TP/IPSec NAT-T Update for Windows XP and Windows 2000"
+:: <http://support.microsoft.com/?kbid=818043>
+:: (download only available from Windows Catalog)
+todo.pl "z:\packages\win2ksp4\Q818043_W2K_SP5_x86_EN.EXE /u /z"
+
+:: Critical update 814078
+:: "Flaw in Windows Script Engine May Allow Code to Run"
+:: <http://support.microsoft.com/?kbid=814078>
+todo.pl ".ignore-err 194 z:\packages\win2ksp3\js56nen.exe /q /r:n" .reboot
+
 :: Install IE6 and DirectX 8.1
 todo.pl ie6.bat directx81.bat
