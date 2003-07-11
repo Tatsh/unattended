ba80ad3c28da88670a4bf0c7a389c8444c16ee88
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-11 19:10:04 +0000

    Add .ignore-err/.reboot for 818043

--- trunk/unattended/install/scripts/win2ksp4-updates.bat
+++ trunk/unattended/install/scripts/win2ksp4-updates.bat
@@ -15,7 +15,7 @@ todo.pl "z:\packages\win2ksp4\Windows2000-KB823559-x86-ENU.exe /u /z"
 :: "L2TP/IPSec NAT-T Update for Windows XP and Windows 2000"
 :: <http://support.microsoft.com/?kbid=818043>
 :: (download only available from Windows Catalog)
-todo.pl "z:\packages\win2ksp4\Q818043_W2K_SP5_x86_EN.EXE /u /z"
+todo.pl ".ignore-err 194 z:\packages\win2ksp4\Q818043_W2K_SP5_x86_EN.EXE /u /z" .reboot
 
 :: Critical update 814078
 :: "Flaw in Windows Script Engine May Allow Code to Run"
