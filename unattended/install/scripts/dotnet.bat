8e44f6e47142da60f79445fd6ee3facb1c922254
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-24 15:19:02 +0000

    Update .NET to include SP2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@639 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/dotnet.bat
+++ unattended/install/scripts/dotnet.bat
@@ -1,3 +1,9 @@
 :: Install .NET framework
 
-todo.pl "Z:\packages\dotnetfx.exe /q /c:\"install /q\""
+:: Microsoft .NET framework Service Pack 2
+:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q321884>
+todo.pl ".ignore-err 194 z:\packages\winxpsp1\NDP10_SP_Q321884_En.exe /q" .reboot
+
+:: Microsoft .NET framework
+:: <http://msdn.microsoft.com/netframework/downloads/>
+todo.pl "z:\packages\winxpsp1\dotnetfx.exe /q /c:\"install /q\""
