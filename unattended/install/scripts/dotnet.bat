e4f7827b8f0a2504b89b6fef6b43b6bf12d61dcc
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-25 16:12:31 +0000

    Use shorter URL for KB article :-)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@643 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/dotnet.bat
+++ unattended/install/scripts/dotnet.bat
@@ -1,7 +1,7 @@
 :: Install .NET framework
 
 :: Microsoft .NET framework Service Pack 2
-:: <http://support.microsoft.com/default.aspx?scid=KB;EN-US;q321884>
+:: <http://support.microsoft.com/?kbid=321884>
 todo.pl ".ignore-err 194 z:\packages\winxpsp1\NDP10_SP_Q321884_En.exe /q" .reboot
 
 :: Microsoft .NET framework
