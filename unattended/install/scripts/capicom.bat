cc625167c14884af801cfbee71e2570d4fcb8171
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-26 20:04:48 +0000

    Fix typo

--- unattended/install/scripts/capicom.bat
+++ unattended/install/scripts/capicom.bat
@@ -8,7 +8,7 @@
 :: capicom.dll to Z:\packages.
 
 :: It sounds like a mess, but it is just following the instructions:
-q:: <http://msdn.microsoft.com/library/en-us/security/Security/getting_ready_to_use_capicom.asp>
+:: <http://msdn.microsoft.com/library/en-us/security/Security/getting_ready_to_use_capicom.asp>
 
 :: Copy DLL to system32 directory and register it
 todo.pl "copy Z:\packages\capicom.dll %SystemRoot%\system32" "regsvr32 /s %SystemRoot%\system32\capicom.dll"
