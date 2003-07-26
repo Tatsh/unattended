1a953ddef2cf8c774a8796c7df553c594e813135
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-26 13:40:16 +0000

    Fix order of operations

--- trunk/unattended/install/scripts/capicom.bat
+++ trunk/unattended/install/scripts/capicom.bat
@@ -1,5 +1,5 @@
-:: Install CryptoAPI COM interface (CAPICOM).  Useful for doing crypto
-:: operations from scripts.
+:: Install CryptoAPI COM interface (CAPICOM).  Useful for doing
+:: cryptographic operations from scripts.
 
 :: But you have to download it first.  Grab the package from
 :: <http://www.microsoft.com/downloads/results.aspx?freetext=capicom>.
@@ -8,10 +8,7 @@
 :: capicom.dll to Z:\packages.
 
 :: It sounds like a mess, but it is just following the instructions:
-:: <http://msdn.microsoft.com/library/en-us/security/Security/getting_ready_to_use_capicom.asp>
+q:: <http://msdn.microsoft.com/library/en-us/security/Security/getting_ready_to_use_capicom.asp>
 
-:: Copy DLL to system32 directory
-todo.pl "copy Z:\packages\capicom.dll %SystemRoot%\system32"
-
-:: Register DLL
-todo.pl "regsvr32 /s %SystemRoot%\system32\capicom.dll"
+:: Copy DLL to system32 directory and register it
+todo.pl "copy Z:\packages\capicom.dll %SystemRoot%\system32" "regsvr32 /s %SystemRoot%\system32\capicom.dll"
