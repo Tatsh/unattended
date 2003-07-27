d5b76aff5bcd771636a821095ba31a2cabf85ad8
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-27 06:31:26 +0000

    Windows Power Toys

---
+++ trunk/unattended/install/scripts/powertoys.bat
@@ -0,0 +1,16 @@
+:: Install Windows PowerToys
+
+:: Windows Power Toys for XP
+:: <http://www.microsoft.com/windowsxp/pro/downloads/powertoys.asp>
+:: (Only install useful powertoys.  At least useful to me)
+todo.pl "z:\packages\powertoys\tweakuipowertoysetup.exe /S /v/qn"
+todo.pl "z:\packages\powertoys\timershotpowertoysetup.exe /S /v/qn"
+todo.pl "z:\packages\powertoys\slideshowpowertoysetup.exe /S /v/qn"
+todo.pl "z:\packages\powertoys\powercalcpowertoysetup.exe /S /v/qn"
+todo.pl "z:\packages\powertoys\imageresizerpowertoysetup.exe /S /v/qn"
+todo.pl "z:\packages\powertoys\cmdherepowertoysetup.exe /S /v/qn"
+
+:: ISO Recorder Power Toy
+:: "Nice tool to burn ISO images with XP"
+:: <http://isorecorder.alexfeinman.com/isorecorder.htm>
+todo.pl "msiexec /qb /i z:\packages\powertoys\isorecordersetup.msi"
