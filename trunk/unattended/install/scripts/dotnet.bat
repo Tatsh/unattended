48639cc20e05a053c99b77a67c8ba933ebcf92ff
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-07-27 04:19:28 +0000

    remove SP2.  This was for v1.0 and we now have v1.1

--- trunk/unattended/install/scripts/dotnet.bat
+++ trunk/unattended/install/scripts/dotnet.bat
@@ -1,9 +1,5 @@
 :: Install .NET framework
 
-:: Microsoft .NET framework Service Pack 2
-:: <http://support.microsoft.com/?kbid=321884>
-todo.pl ".ignore-err 194 z:\packages\winxpsp1\NDP10_SP_Q321884_En.exe /q" .reboot
-
 :: Microsoft .NET framework
 :: <http://msdn.microsoft.com/netframework/downloads/>
 todo.pl "z:\packages\winxpsp1\dotnetfx.exe /q /c:\"install /q\""
