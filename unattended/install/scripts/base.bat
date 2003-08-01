6d11b5eb0be7e3436425ddb48748e993c3dd2e7d
Shad L. Lords <slords@mail.com>
2003-08-01 04:29:11 +0000

    Clean up order of installation.

--- unattended/install/scripts/base.bat
+++ unattended/install/scripts/base.bat
@@ -1,19 +1,22 @@
 :: Perform a basic workstation installation
 
-todo.pl hidepw.pl %%WINVER%%-notips.pl bootini.pl %%WINVER%%-updates.bat mozilla.bat vnc.bat adobe-reader.bat 7-zip.pl php.bat openoffice.bat
-
-:: Set IIS startup type to manual
-todo.pl "startup-type.pl manual IISADMIN" "startup-type.pl manual W3SVC"
-
-:: Turn off stupid Windows Messenger service
-todo.pl "startup-type.pl manual Messenger"
-
-:: Set Automatic Updates to download automatically but prompt before
-:: install
+:: Set Automatic Updates to download automatically but prompt before install.
 todo.pl "auconfig.pl 3"
 
-:: Work around daylight saving bug in XP unattended installation.
-todo.pl fixtz.pl
+:: Install popular applications.
+todo.pl adobe-reader.bat vnc.bat mozilla.bat 7-zip.pl php.bat openoffice.bat
 
-:: First things first: Defragment the drive to collect the free space.
+:: Update windows and turn off annoying stuff.
+todo.pl %%WINVER%%-updates.bat %%WINVER%%-notips.pl
+
+:: Defragment the drive to collect the free space.
 todo.pl "AutoIt z:\scripts\defrag.aut"
+
+:: Set IIS startup type to manual.
+todo.pl "startup-type.pl manual IISADMIN" "startup-type.pl manual W3SVC"
+
+:: Turn off stupid Windows Messenger service.
+todo.pl "startup-type.pl manual Messenger"
+
+:: Cleanup messes left over from installing.
+todo.pl hidepw.pl bootini.pl fixtz.pl 
