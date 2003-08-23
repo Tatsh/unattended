f5decc93c4bf505a156a8797c312bbefb579de5b
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-08-23 19:12:08 +0000

    Little rearrangements and stuff

--- unattended/install/scripts/cygwin-1.bat
+++ unattended/install/scripts/cygwin-1.bat
@@ -1,7 +1,14 @@
 :: Perform post-install Cygwin configuration.
 
-:: Untar our local additions to the tree
-todo.pl "c:\cygwin\bin\bash.exe --login -c 'tar -C / -xvzf /cygdrive/z/packages/curlstuff.tar.gz'"
+:: Set up mounts (/projects, /releases, etc.)
+todo.pl "c:\cygwin\bin\mount.exe -b -f -s P: /projects" "c:\cygwin\bin\mount.exe -b -f -s R: /releases" "c:\cygwin\bin\mount.exe -b -f -s S: /software" "c:\cygwin\bin\mount.exe -b -f -s U: /u"
+
+:: Give ownership of everything to Administrators *group*.  This is
+:: legal on Windows.
+todo.pl "C:\cygwin\bin\bash.exe --login -c 'cd / && chown -R Administrators .'"
+
+:: Copy over our local additions to the tree
+todo.pl "c:\cygwin\bin\bash.exe --login -c 'cp -a /cygdrive/z/packages/curlstuff/. /.'"
 
 :: Configure sshd
 todo.pl "c:\cygwin\bin\bash.exe --login -c 'ssh-host-config --no'"
@@ -15,6 +22,3 @@ todo.pl "c:\cygwin\bin\bash.exe --login -c 'grep -q curlbot /etc/passwd || echo 
 
 :: Populate /etc/group similarly (mkgroup -d)
 todo.pl "c:\cygwin\bin\bash.exe --login -c 'grep -q 10513 /etc/group|| echo Domain Users:S-1-5-21-223763145-1258644291-1338337383-513:10513: >> /etc/group"'
-
-:: Set up mounts (/projects, /releases, etc.)
-todo.pl "c:\cygwin\bin\mount.exe -b -f -s P: /projects" "c:\cygwin\bin\mount.exe -b -f -s R: /releases" "c:\cygwin\bin\mount.exe -b -f -s S: /software" "c:\cygwin\bin\mount.exe -b -f -s U: /u"
