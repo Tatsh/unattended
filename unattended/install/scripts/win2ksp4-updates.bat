760cae1a69f96de80ab232624524094b31d7fdba
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-09-07 15:20:27 +0000

    Add 823718 and 824105

--- unattended/install/scripts/win2ksp4-updates.bat
+++ unattended/install/scripts/win2ksp4-updates.bat
@@ -6,22 +6,34 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Critical update 824105
+:: "Flaw in NetBIOS Could Lead to Information Disclosure"
+:: <http://support.microsoft.com/?kbid=824105>
+todo.pl ".ignore-err 194 z:\packages\win2ksp4\Windows2000-KB824105-x86-ENU.exe /u /n /z" .reboot
+
+:: Critical update 823718
+:: "Security Update for Microsoft Data Access Components"
+:: (This will be superseded if MDAC 2.8 is installed later, but no
+:: harm done.)
+:: <http://support.microsoft.com/?kbid=823718>
+todo.pl ".ignore-err 194 z:\packages\win2ksp4\Q823718_MDAC_SecurityPatch.exe /q /c:\"dahotfix.exe /q /n\"" .reboot
+
 :: Critical update 823980
 :: "Buffer Overrun in RPC Interface May Allow Code Execution"
 :: (So anyone can take over any Windows computer anywhere.  Wow.)
 :: <http://support.microsoft.com/?kbid=823980>
-todo.pl ".ignore-err 194 z:\packages\win2ksp4\Windows2000-KB823980-x86-ENU.exe /u /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\win2ksp4\Windows2000-KB823980-x86-ENU.exe /u /n /z" .reboot
 
 :: Critical update 823559
 :: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
 :: <http://support.microsoft.com/?kbid=823559>
-todo.pl "z:\packages\win2ksp4\Windows2000-KB823559-x86-ENU.exe /u /z"
+todo.pl "z:\packages\win2ksp4\Windows2000-KB823559-x86-ENU.exe /u /n /z"
 
 :: Recommended update 818043
 :: "L2TP/IPSec NAT-T Update for Windows XP and Windows 2000"
 :: <http://support.microsoft.com/?kbid=818043>
 :: (download only available from Windows Catalog)
-todo.pl ".ignore-err 194 z:\packages\win2ksp4\Q818043_W2K_SP5_x86_EN.EXE /u /z" .reboot
+todo.pl ".ignore-err 194 z:\packages\win2ksp4\Q818043_W2K_SP5_x86_EN.EXE /u /n /z" .reboot
 
 :: Critical update 814078
 :: "Flaw in Windows Script Engine May Allow Code to Run"
