245b030b3d8421064d7b015a17354e485aa02945
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-09-12 19:15:23 +0000

    Initial revision

---
+++ unattended/install/bin/mapznrun.bat
@@ -0,0 +1,43 @@
+@if exist c:\netinst\permcreds.bat call c:\netinst\permcreds.bat
+@if exist c:\netinst\tempcreds.bat call c:\netinst\tempcreds.bat
+
+@if not %Z%. == . goto got_drive
+@set Z=Z:
+:got_drive
+
+net use %Z% /delete
+
+@if not %Z_PATH%. == . goto got_path
+@set Z_PATH=\\ntinstall\install
+:got_path
+
+@if %Z_USER%. == . goto no_user
+@set USER_ARG=/user:%Z_USER%
+:no_user
+
+:try_again
+@set COUNT=x%COUNT%
+@if not %COUNT% == xxxxx goto mapit
+@echo Too many retries; giving up
+@goto hang
+
+:mapit
+net use %Z% %Z_PATH% %Z_PASS% %USER_ARG%
+@if not exist %Z%\ goto try_again
+
+:: Clean up environment, except for %Z%
+@set Z_PATH=
+@set Z_USER=
+@set Z_PASS=
+@set USER_ARG=
+@set COUNT=
+
+:: Invoke command passed as argument
+%1 %2 %3 %4 %5 %6 %7 %8 %9
+if not errorlevel 1 goto end
+@echo FAILURE in mapznrun.bat
+
+:hang
+@goto hang
+
+:end
