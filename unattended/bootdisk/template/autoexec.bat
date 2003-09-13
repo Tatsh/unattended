6b0773a9052e1e9d79d58439515911f49b02e258
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-09-13 21:49:02 +0000

    Clean up and simplify.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@739 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/bootdisk/template/autoexec.bat
+++ unattended/bootdisk/template/autoexec.bat
@@ -1,44 +1,20 @@
 @echo off
+
 :: Set these variables to your defaults
 
-:: Lock Setting YES or NO
-SET LOCK_SETTINGS=NO
-
-:: Debug mode YES or NO
-SET DEBUG_MODE=YES
-
-:: Prompt user for input YES or NO
-SET MANUAL_INPUT=YES
-
 :: Install share
-SET INSTALL=\\ntinstall\install
+SET Z_PATH=\\ntinstall\install
 
 :: Username to logon to install share
-SET USERNAME=GUEST
+SET Z_USER=GUEST
 
 :: Password for install share
-SET PASSWD=
+SET Z_PASS=GUEST
 
-:: End of user setable variables
+:: End of user settable variables
 
-if %DEBUG_MODE%.==YES. echo on
 echo Unattended Boot Disk Starting....
-if %LOCK_SETTINGS%.==YES. goto endsetup
-if %DEBUG_MODE%.==YES. goto checkman
-CHOICE /C:YN /T:N,5 Debug Mode?
-if errorlevel 2 goto checkman
-if errorlevel 1 SET DEBUG_MODE=YES
-if %DEBUG_MODE%.==YES. echo on
 
-:checkman
-if %MANUAL_INPUT%.==YES. goto endsetup
-CHOICE /C:YN /T:N,5 Manual Input?
-if errorlevel 2 goto endsetup
-if errorlevel 1 SET MANUAL_INPUT=YES
-echo Enter location of install share (default %INSTALL%):
-nset INPUT=$0
-if not %INPUT%.==. set INSTALL=%INPUT%
-:endsetup
 set PATH=\;\NET
 PROMPT $P$G
 :: Randomize hostname
@@ -51,73 +27,45 @@ NETBIND.COM
 TCPTSR.EXE
 TINYRFC.EXE
 
+:retry
+CHOICE /C:YN /T:N,10 Override defaults (if unsure, say yes)
+if errorlevel 2 goto endsetup
+echo Enter location of install share (default %Z_PATH%):
+nset INPUT=$0
+if not %INPUT%. == . set Z_PATH=%INPUT%
+echo Enter username for mapping install share (default %Z_USER%):
+nset INPUT=$0
+if not %INPUT%. == . set Z_USER=%INPUT%
+echo Enter password for mapping install share (default %Z_PASS%):
+nset INPUT=$0
+if not %INPUT%. == . set Z_PASS=%INPUT%
+
+:endsetup
+
 :: LOGON
-if %USERNAME%.==. goto no_default
-:: Do we have a password
-if %PASSWD%.==. goto blank_pwd
-:: Password provided, logon using USERNAME and PASSWORD
 echo Starting network....
-NET LOGON %USERNAME% %PASSWD% /YES /SAVEPW:NO > \devnull
+NET LOGON %Z_USER% %Z_PASS% /YES /SAVEPW:NO
 if not errorlevel 1 goto gotnet
-goto default_failed
-
-:blank_pwd
-:: No password provided logon using provided USERNAME
-:: and provide a return when prompted for PASSWORD
-echo Starting network....
-type \net\areturn|NET LOGON %USERNAME% /YES /SAVEPW:NO > \devnull
-if not errorlevel 1 goto gotnet
-
-:default_failed
-echo *** Unable to start network, with defaults
-if %MANUAL_INPUT%.==YES. echo *** Retrying ...
-if %MANUAL_INPUT%.==YES. goto no_default
-echo *** probably because you are using the wrong device driver.
-goto reboot
-
-:no_default
-if %MANUAL_INPUT%.==NO. goto reboot
-:: Let the user try other options
-echo Starting network....
-NET LOGON /YES /SAVEPW:NO
-if not errorlevel 1 goto gotnet
-echo *** Unable to start network, probably because
-echo *** you are using the wrong device driver.
-goto reboot
+echo *** Unable to start network (are you using the right driver?)
+echo *** retrying...
+goto retry
 
 :gotnet
 :: Save MAC address in file (to be parsed later).
-net diag /status < \net\areturn > \netdiag.txt
-if %INSTALL%.==. goto man_share
+net diag /status < nul > \netdiag.txt
+
 echo Mapping Z:....
-NET USE Z: %INSTALL% > \devnull
+NET USE Z: %Z_PATH%
 if not errorlevel 1 goto got_z
-if %MANUAL_INPUT%.==NO. goto reboot
-echo Unable to map Z: drive.  Trying again...
-
-:man_share
-if %MANUAL_INPUT%.==NO. goto reboot
-
-:: Three tries at mapping share
-set COUNT=
-
-:map_share
-if %COUNT%.==111. goto reboot
-echo.
-echo Enter location of install share (default %INSTALL%):
-nset INPUT=$0
-if not %INPUT%.==. set INSTALL=%INPUT%
-echo Mapping Z:....
-NET USE Z: %INSTALL%
-if not errorlevel 1 goto got_z
-echo Unable to map Z: drive.  Try again...
-set COUNT=%COUNT%1
-goto map_share
+echo Unable to map Z: drive.  Retrying...
+goto retry
 
 :got_z
 set PATH=Z:\djgpp\bin;Z:\dosbin;%PATH%
+
 :: Save IP address in file (to be parsed later).
 ipconfig \net > \ipconfig.txt
+
 set DJGPP=Z:\djgpp\djgpp.env
 lh cwsdpmi -p -s-
 
@@ -133,11 +81,9 @@ smartdrv
 C:\netinst\doit.bat
 
 :reboot
-if %MANUAL_INPUT%.==NO. goto restart
 CHOICE /C:YN /T:Y,5 Reboot?
 if errorlevel 2 goto end
 
-:restart
 fdapm WARMBOOT
 
 :end
