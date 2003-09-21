248849f8370426d7f71df05c8d024d592b6f52d7
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-09-21 18:02:15 +0000

    Remove old cruft
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@779 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/office1.bat
+++
@@ -1,8 +0,0 @@
-:: Outlook 2000 SR-1 View Control Security Update (2001-08-16-0)
-
-:: Not available as an administrative update, so we apply it after
-:: installing Office reboots.
-
-:: /q means "quiet", /r:n means "do not reboot"
-start /wait z:\packages\outlctlx.exe /q /r:n
-if errorlevel 1 exit 1
