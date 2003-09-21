5b44d8cb58b19cb9c628b8b8781a507c9fc34799
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-09-21 18:02:15 +0000

    Remove old cruft

--- unattended/install/scripts/win2ksp3-updates.bat
+++
@@ -1,7 +0,0 @@
-:: Install all hotfixes and updates for win2ksp3
-
-:: Euro conversion tool update
-:: <http://www.download.windowsupdate.com/msdownload/update/v3/static/RTF/en/5026.htm>
-todo.pl "z:\packages\win2ksp3\euro-tool.exe /q"
-
-todo.pl ie6.bat win2ksp3-hotfixes.bat directx81.bat
