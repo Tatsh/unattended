c47150a025f6f5f1b1e845af8af4c0941243caa6
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-09-30 05:55:38 +0000

    More updates for language (ENU => %WINLANG%)

--- trunk/unattended/install/scripts/winxpsp1-recommended.bat
+++ trunk/unattended/install/scripts/winxpsp1-recommended.bat
@@ -3,7 +3,7 @@
 :: Movie Maker 2.0 update
 :: <http://www.microsoft.com/windowsxp/moviemaker/>
 :: (Works better if Media Player 9 is already installed)
-todo.pl "%Z%\packages\winxpsp1\mm20enu.exe /q /c:\"msiexec /i mm20.msi /qn\""
+todo.pl "%Z%\packages\winxpsp1\mm20%WINLANG%.exe /q /c:\"msiexec /i mm20.msi /qn\""
 
 :: Windows Messenger Update
 :: <http://www.microsoft.com/windowsxp/windowsmessenger/>
