994e2b1a0ba5b6247f83e11cd8e452f15085688b
Shad L. Lords <slords@mail.com>
2003-09-30 14:59:59 +0000

    Hide credentials from user Make batch file less noisy

--- unattended/install/bin/mapznrun.bat
+++ unattended/install/bin/mapznrun.bat
@@ -1,51 +1,52 @@
-@if exist c:\netinst\permcred.bat call c:\netinst\permcred.bat
-@if exist c:\netinst\tempcred.bat call c:\netinst\tempcred.bat
+@echo off
+if exist c:\netinst\permcred.bat call c:\netinst\permcred.bat
+if exist c:\netinst\tempcred.bat call c:\netinst\tempcred.bat
 
-@if not %Z%. == . goto got_drive
-@set Z=Z:
+if not %Z%. == . goto got_drive
+set Z=Z:
 :got_drive
 
 :: If drive is already mapped, do nothing
 if exist %Z%\ goto mapped
 
-@if not %Z_PATH%. == . goto got_path
-@set Z_PATH=\\ntinstall\install
+if not %Z_PATH%. == . goto got_path
+set Z_PATH=\\ntinstall\install
 :got_path
 
-@if %Z_USER%. == . goto no_user
-@set USER_ARG=/user:%Z_USER%
+if %Z_USER%. == . goto no_user
+set USER_ARG=/user:%Z_USER%
 :no_user
 
 :try_again
-@set COUNT=x%COUNT%
-@if not %COUNT% == xxxxxxxxxxxxxxxxxxxx goto mapit
-@echo Too many retries; giving up
-@goto hang
+set COUNT=x%COUNT%
+if not %COUNT% == xxxxxxxxxxxxxxxxxxxx goto mapit
+echo Too many retries; giving up
+goto hang
 
 :mapit
-@echo Mapping %Z_PATH% on %Z%...
-@net use %Z% /delete
-@net use %Z% %Z_PATH% %Z_PASS% %USER_ARG% /persistent:no
-@if exist %Z%\ goto mapped
-@echo Failed; retrying.
+echo Mapping %Z_PATH% on %Z%...
+net use %Z% /delete
+net use %Z% %Z_PATH% %Z_PASS% %USER_ARG% /persistent:no
+if exist %Z%\ goto mapped
+echo Failed; retrying.
 if exist C:\Perl\bin\perl.exe C:\Perl\bin\perl.exe -e "sleep 10;"
-@goto try_again
+goto try_again
 
 :mapped
-@echo ...done.
+echo ...done.
 :: Clean up environment, except for %Z%
-@set Z_PATH=
-@set Z_USER=
-@set Z_PASS=
-@set USER_ARG=
-@set COUNT=
+set Z_PATH=
+set Z_USER=
+set Z_PASS=
+set USER_ARG=
+set COUNT=
 
 :: Invoke command passed as argument
 %1 %2 %3 %4 %5 %6 %7 %8 %9
-@if not errorlevel 1 goto end
-@echo FAILURE in mapznrun.bat
+if not errorlevel 1 goto end
+echo FAILURE in mapznrun.bat
 
 :hang
-@goto hang
+goto hang
 
 :end
