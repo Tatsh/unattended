87796d1716bc958c006f84884d8b7a904be1e21e
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-10-15 20:36:01 +0000

    Add MS03-041, -043, -044, and -045 (untested)

--- trunk/unattended/install/scripts/winxpsp1-updates.bat
+++ trunk/unattended/install/scripts/winxpsp1-updates.bat
@@ -6,6 +6,26 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Critical update 824141 (MS03-045)
+:: "Buffer Overrun in the ListBox and in the ComboBox Control Could Allow Code Execution"
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-045.asp>
+todo.pl "%Z%\packages\win2ksp4\WindowsXP-KB824141-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 825119 (MS03-044)
+:: "Buffer Overrun in Windows Help and Support Center Could Lead to System Compromise"
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-044.asp>
+todo.pl "%Z%\packages\win2ksp4\WindowsXP-KB825119-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 828035 (MS03-043)
+:: "Buffer Overrun in Messenger Service Could Allow Code Execution"
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-043.asp>
+todo.pl "%Z%\packages\win2ksp4\WindowsXP-KB828035-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 823182 (MS03-041)
+:: "Vulnerability in Authenticode Verification Could Allow Remote Code Execution"
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-041.asp>
+todo.pl "%Z%\packages\win2ksp4\WindowsXP-KB823182-x86-%WINLANG%.exe /u /n /z"
+
 :: Recommended update 822603
 :: "Windows XP SP1 USB 1.1 and 2.0 Update"
 :: <http://support.microsoft.com/?kbid=822603>
