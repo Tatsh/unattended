7cbf361bfe16a832e009fb814f2ac36ec3f3fad6
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-10-18 18:51:00 +0000

    MDAC needs reboot
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@842 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/mdac.bat
+++ unattended/install/scripts/mdac.bat
@@ -2,4 +2,4 @@
 
 :: Download from <http://www.microsoft.com/data/>.  Rename
 :: mdac_typ.exe to something more descriptive.
-todo.pl "%Z%\packages\mdac-2.8.exe /q /c:\"setup /qn1\""
+todo.pl ".ignore-err 194 %Z%\packages\mdac-2.8.exe /q /c:\"setup /qn1\"" .reboot
