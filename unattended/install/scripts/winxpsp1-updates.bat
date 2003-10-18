eb77f30fd795e14d466e540c35b64d292fcee076
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-10-18 17:36:33 +0000

    Add Update Rollup 1 (826939), which supersedes 22 other updates.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@840 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winxpsp1-updates.bat
+++ unattended/install/scripts/winxpsp1-updates.bat
@@ -31,12 +31,6 @@ todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\WindowsXP-KB823182-x86-%WINLANG%.e
 :: <http://support.microsoft.com/?kbid=822603>
 todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\WindowsXP-KB822603-x86-%WINLANG%.exe /u /n /z" .reboot
 
-:: Critical update 824146
-:: "A Buffer Overrun in RPCSS May Allow Code Execution"
-:: (Worst Windows security hole, ever.  Again.)
-:: <http://support.microsoft.com/?kbid=824146>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\WindowsXP-KB824146-x86-%WINLANG%.exe /u /n /z" .reboot
-
 :: Critical update 824105
 :: "Flaw in NetBIOS Could Lead to Information Disclosure"
 :: <http://support.microsoft.com/?kbid=824105>
@@ -62,11 +56,6 @@ todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\WindowsXP-KB817778-x86-%WINLANG%.e
 :: (Requires .NET to be installed first for managed DX)
 todo.pl ".expect-reboot AutoIt %Z%\scripts\directx9.aut"
 
-:: Critical update 821557
-:: "An Unchecked Buffer in the Windows Shell Could Permit Your System to Be Compromised"
-:: <http://support.microsoft.com/default.aspx?kbid=821557>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\WindowsXP-KB821557-x86-%WINLANG%.exe /u /n /z" .reboot
-
 :: Windows Journal Viewer update
 :: <http://download.windowsupdate.com/msdownload/update/v3/static/rtf/en/5850.htm>
 todo.pl "%Z%\updates\winxpsp1\journal_viewer_%WINLANG%.exe /q /c:\"msiexec /i \"\"Microsoft Windows Journal Viewer.msi\"\" /qn\""
@@ -75,16 +64,6 @@ todo.pl "%Z%\updates\winxpsp1\journal_viewer_%WINLANG%.exe /q /c:\"msiexec /i \"
 :: "Update For Windows Error Reporting"
 :: <http://support.microsoft.com/?kbid=821253>
 todo.pl "%Z%\updates\winxpsp1\WindowsXP-KB821253-x86-%WINLANG%.exe /u /n /z"
-
-:: Critical update 817606
-:: "Buffer Overrun in Windows Could Lead to Data Corruption"
-:: <http://support.microsoft.com/?kbid=817606>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q817606_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
-:: Critical update 823559
-:: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
-:: <http://support.microsoft.com/?kbid=823559>
-todo.pl "%Z%\updates\winxpsp1\WindowsXP-KB823559-x86-%WINLANG%.exe /u /n /z"
 
 :: Recommended update 282010
 :: "Updated Version of Microsoft Jet 4.0"
@@ -113,21 +92,10 @@ todo.pl "regedit /s %Z%\scripts\mediaplayer9.reg"
 :: <http://www.microsoft.com/windows/windowsmedia/>
 todo.pl "%Z%\updates\winxpsp1\MPSetupXP.EXE /Q /R:N /C:\"setup_wm.exe /DisallowSystemRestore /NoPID /SetWMPAsDefault /Q /R:N\""
 
-:: Critical update 815021
-:: "Unchecked Buffer in Windows Component May Cause Web Server Compromise"
-:: <http://support.microsoft.com/?kbid=815021>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q815021_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
 :: Critical update 811114
 :: "May 2003 Cumulative Patch for Internet Information Services (IIS)"
 :: <http://support.microsoft.com/?kbid=811114>
 todo.pl "%Z%\updates\winxpsp1\Q811114_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 817287
-:: "Windows Update 643 Error and the Catalog Database"
-:: <http://support.microsoft.com/?kbid=817287>
-:: (download only available from Windows Catalog)
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q817287.exe /q /r:n" .reboot
 
 :: Critical update 816093
 :: "Flaw in the Microsoft VM Could Enable System Compromise"
@@ -140,11 +108,6 @@ todo.pl "%Z%\updates\winxpsp1\msjavwu.exe /q /r:n"
 :: <http://support.microsoft.com/?kbid=815485>
 todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q815485_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
 
-:: Critical update 811493
-:: "Buffer Overrun ... Could Lead to Elevated Privileges"
-:: <http://support.microsoft.com/?kbid=811493>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q811493_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
 :: Critical update 814078
 :: "Flaw in Windows Script Engine May Allow Code to Run"
 :: <http://support.microsoft.com/?kbid=814078>
@@ -153,78 +116,18 @@ todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q811493_WXP_SP2_x86_%WINLANG%.exe 
 :: rename js56nen.exe to js56nenu.exe.
 todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\js56n%WINLANG%.exe /q /r:n" .reboot
 
-:: Critical update 814033
-:: "Cannot Install Driver Updates from the Windows Update Web Site"
-:: <http://support.microsoft.com/?kbid=814033>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q814033_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
 :: Recommended update 814995
 :: "Some ... Fixes Stop Working After You Install the 328310 Update"
 :: <http://support.microsoft.com/?kbid=814995>
 :: (download only available from Windows Update Catalog)
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q814995_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
-:: Critical update 811630
-:: "HTML Help Update to Limit Functionality ..."
-:: <http://support.microsoft.com/?kbid=811630>
-todo.pl "%Z%\updates\winxpsp1\Q811630_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 810833
-:: "Unchecked Buffer in the Locator Service Might Permit Code to Run"
-:: <http://support.microsoft.com/?kbid=810833>
-todo.pl "%Z%\updates\winxpsp1\Q810833_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 810577
-:: "Unchecked Buffer in Windows Redirector May Permit Privilege Elevation"
-:: <http://support.microsoft.com/?kbid=810577>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q810577_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
-:: Critical update 810565
-:: "Better support for default Web browsers other than Internet Explorer"
-:: <http://support.microsoft.com/?kbid=810565>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q810565_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
+:: (fixed in rollup?  Check this! FIXME)
+::todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q814995_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
 
 :: Recommended update 810243
 :: "DirectShow Playback Support for Files Recorded with Windows XP Media Center Edition"
 :: <http://support.microsoft.com/?kbid=810243>
 :: (only available from Windows Update Catalog)
 todo.pl "%Z%\updates\winxpsp1\Q810243_WXP_SP2.exe /u /n /z"
-
-:: Critical update 329834
-:: "Unchecked Buffer in PPTP Implementation May Permit Denial-of-Service"
-:: <http://support.microsoft.com/?kbid=329834>
-todo.pl "%Z%\updates\winxpsp1\Q329834_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 329441
-:: "You Cannot Create a Network Connection After You Restore Windows XP"
-:: <http://support.microsoft.com/?kbid=329441>
-:: (download only available from Windows Update Catalog)
-todo.pl "%Z%\updates\winxpsp1\Q329441_WXP_SP2_en.exe /u /n /z"
-
-:: Critical update 329390
-:: "Unchecked Buffer in Windows Shell Might Permit System Compromise"
-:: <http://support.microsoft.com/?kbid=329390>
-todo.pl "%Z%\updates\winxpsp1\Q329390_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 329170
-:: "Flaw in SMB Signing May Permit Group Policy to Be Modified"
-:: <http://support.microsoft.com/?kbid=329170>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q329170_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
-
-:: Critical update 329115
-:: "Certificate Validation Flaw Might Permit Identity Spoofing"
-:: <http://support.microsoft.com/?kbid=329115>
-todo.pl "%Z%\updates\winxpsp1\Q329115_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 329048
-:: "Unchecked Buffer in File Decompression Functions May Allow Attacker ..."
-:: <http://support.microsoft.com/?kbid=329048>
-todo.pl "%Z%\updates\winxpsp1\Q329048_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
-
-:: Critical update 328310
-:: "Flaw in Windows WM_TIMER Message Handling Can Enable Privilege Elevation"
-:: <http://support.microsoft.com/?kbid=328310>
-todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\Q328310_WXP_SP2_x86_%WINLANG%.exe /u /n /z" .reboot
 
 :: Recommended update 327979
 :: "Game Stops Responding (Hangs) or Quits Unexpectedly ..."
@@ -238,15 +141,10 @@ todo.pl "%Z%\updates\winxpsp1\Q327979_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
 todo.pl "%Z%\updates\winxpsp1\Q327696_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
 
 :: Recommended update 327405
-:: "Windows XP Documentation Update Can Help You Understand How to Make Your Compute
+:: "Windows XP Documentation Update Can Help You Understand How to Make Your Computer More Secure"
 :: <http://support.microsoft.com/?kbid=327405>
 :: (only available from Windows Update Catalog)
 todo.pl "%Z%\updates\winxpsp1\hu1002_pro.exe /q"
-
-:: Critical update 323255
-:: "Unchecked Buffer in Windows Help Facility May Allow Attacker to Run Code"
-:: <http://support.microsoft.com/?kbid=323255>
-todo.pl "%Z%\updates\winxpsp1\Q323255_WXP_SP2_x86_%WINLANG%.exe /u /n /z"
 
 :: Recommended update 322011
 :: "You Cannot Preview a Fax in the Fax Console"
@@ -278,3 +176,7 @@ todo.pl ".ignore-err 194 %Z%\updates\ie6sp1\q330994.exe /q /r:n" .reboot
 :: abbreviation instead of two.  For example, for English you would
 :: rename vbs56nen.exe to vbs56nenu.exe.
 todo.pl ".ignore-err 194 %Z%\updates\winxpsp1\vbs56n%WINLANG%.exe /q /r:n" .reboot
+
+:: Start with Update Rollup 1
+:: <http://support.microsoft.com/?kbid=826939>
+todo.pl "%Z%\updates\winxpsp1\WindowsXP-KB826939-x86-%WINLANG%.exe"
