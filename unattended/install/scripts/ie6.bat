99a8947282218dcbe23257c103357bbdf7fac94d
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-10-30 06:51:46 +0000

    Don't add todo to the registry if there are no more commands. Add .reboot-on to reboot on specific errors. Update *-update scripts to use new .reboot-on Add recommended update 829558 to 2000, xp and 2003
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@867 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/ie6.bat
+++ unattended/install/scripts/ie6.bat
@@ -6,17 +6,17 @@
 :: Critical update 828750
 :: "October, 2003, Cumulative Patch for Internet Explorer"
 :: <http://support.microsoft.com/?kbid=828750>
-todo.pl ".ignore-err 194 %Z%\updates\ie6sp1\q828750.exe /q /r:n" .reboot
+todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\q828750.exe /q /r:n"
 
 :: Critical update 330994
 :: "April 2003, Cumulative Patch for Outlook Express"
 :: <http://support.microsoft.com/?kbid=330994>
 :: <http://www.microsoft.com/windows/ie/downloads/critical/330994/>
-todo.pl ".ignore-err 194 %Z%\updates\ie6sp1\q330994.exe /q /r:n" .reboot
+todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\q330994.exe /q /r:n"
 
 :: Critical update Q318203/Q317244
 :: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
 :: <http://support.microsoft.com/?kbid=318203>
-todo.pl "%Z%\updates\ie6sp1\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\"" .reboot
+todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\""
 
-todo.pl ".ignore-err 194 %Z%\packages\ie6sp1\IE6setup.exe /q /r:n" .reboot
+todo.pl ".reboot-on 194 %Z%\packages\ie6sp1\IE6setup.exe /q /r:n"
