fda11fccf00a8866d9cc73121cb4e4f3905d4a3c
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-06 07:32:44 +0000

    Add Media Player 9 to Windows 2000 Add 820888 and instructions for 818043
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@884 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/packages/mediaplayer9/README.txt
@@ -0,0 +1,3 @@
+This directory holds the install files for Internet Explorer
+v9.  You need to download it from Microsoft. You might alsoi
+need to modify the script in install/scripts/ to install it.
--- unattended/install/scripts/win2ksp4-updates.bat
+++ unattended/install/scripts/win2ksp4-updates.bat
@@ -6,10 +6,114 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Extra Updates
+
+:: Microsoft DirectX 9.0b update
+:: <http://www.microsoft.com/directx/>
+:: Download the redistributable for developers (dx90b_redist.exe) and
+:: extract it to %Z%\packages\DirectX.
+:: <http://www.microsoft.com/downloads/details.aspx?familyid=a6dee0db-dcce-43ea-87bb-7c7e1fd1eaa2>
+:: (Requires .NET to be installed first for managed DX)
+todo.pl ".expect-reboot AutoIt %Z%\scripts\directx9.aut"
+
+:: Windows Journal Viewer update
+:: <http://download.windowsupdate.com/msdownload/update/v3/static/rtf/en/5850.htm>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\journal_viewer_%WINLANG%.exe /q /c:\"msiexec /i \"\"Microsoft Windows Journal Viewer.msi\"\" /qn\""
+
+:: "Update for Windows Media Player Script Commands"
+:: <http://support.microsoft.com/?kbid=828026>
+todo.pl ".reboot-on 194 %Z%\updates\mediaplayer9\WindowsMedia-KB828026-x86-%WINLANG%.exe /passive /n /norestart"
+
+:: Flaw In Windows Media Player May Allow Media Library Access (819639)
+:: <http://support.microsoft.com/?kbid=819639>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-021.asp>
+todo.pl ".reboot-on 194 %Z%\updates\mediaplayer9\WindowsMedia9-KB819639-x86-%WINLANG%.exe /Q /R:N"
+
+:: Trick to skip EULA on Media Player 9 
+todo.pl "regedit /s %Z%\packages\mediaplayer9\mediaplayer9.reg"
+
+:: Media Player 9.0
+:: <http://www.microsoft.com/windows/windowsmedia/>
+todo.pl ".reboot-on 194 %Z%\packages\mediaplayer9\MPSetup.EXE /Q /R:N /C:\"setup_wm.exe /DisallowSystemRestore /NoPID /SetWMPAsDefault /Q /R:N\""
+
+:: Microsoft .NET framework
+:: <http://msdn.microsoft.com/netframework/downloads/>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\dotnetfx.exe /q /c:\"install /q\""
+
+:: Recommended Updates
+
+:: Root Certificates Update
+:: <http://download.windowsupdate.com/msdownload/update/v3/static/rtf/en/4702.htm>
+:: (download only available from Windows Catalog)
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\rootsupd.exe /q /r:n"
+
 :: Recommended update 829558
 :: "Update for Jet 4.0 Service Pack 8"
 :: <http://support.microsoft.com/?kbid=829558>
 todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB829558-x86-%WINLANG%.exe /u /n /z"
+
+:: Recommended update 822831
+:: "BUG: Driver Installation Program Does Not Install Device Drivers"
+:: <http://support.microsoft.com/?kbid=822831>
+:: (download only available from Windows Catalog)
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB822831-x86-%WINLANG%.exe /u /n /z"
+
+:: Recommended update 820888
+:: "Computer Stops Responding (Hangs) When It Tries to Mount an NTFS Volume After You Restart the Computer"
+:: <http://support.microsoft.com/?kbid=820888>
+:: (download only available from Windows Catalog)
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB820888-x86-%WINLANG%.exe /u /n /z"
+
+:: Recommended update 818043
+:: "L2TP/IPSec NAT-T Update for Windows XP and Windows 2000"
+:: <http://support.microsoft.com/?kbid=818043>
+:: (download only available from Windows Catalog)
+:: NOTE: You must rename this to use the three-letter language
+:: abbreviation instead of two.  For example, for English you would
+:: rename Q818043_W2K_SP5_x86_EN.EXE to Q818043_W2K_SP5_x86_ENU.EXE.
+::
+:: As of 2003-10-19, this update was still not available from
+:: Windows Catalog.  See:
+:: <http://groups.google.com/groups?threadm=uJILwu%24kDHA.3612%40TK2MSFTNGP11.phx.gbl>
+:: Until Microsoft gets their act together, you will have to use
+:: Windows Update to install this one by hand. Or download like
+:: wget http://download.windowsupdate.com/msdownload/update/v3-19990518/cabpool/Q818043_W2K_SP5_x86_EN_8771f25008d3c5f8c7aa577b615f619.EXE -O q818043_w2k_sp5_x86_enu.exe
+:: You can also move from c:\documents and settings\administrator\local settings\temporary internet files\q818043*.exe 
+:: and rename appropriately.
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Q818043_W2K_SP5_x86_%WINLANG%.EXE /u /n /z"
+
+:: Install IE6
+todo.pl ie6.bat
+
+:: Critical Updates
+
+:: Critical update 823718
+:: "Security Update for Microsoft Data Access Components"
+:: (This will be superseded if MDAC 2.8 is installed later, but no
+:: harm done.)
+:: <http://support.microsoft.com/?kbid=823718>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Q823718_MDAC_SecurityPatch.exe /q /c:\"dahotfix.exe /q /n\""
+
+:: Critical update 824105
+:: "Flaw in NetBIOS Could Lead to Information Disclosure"
+:: <http://support.microsoft.com/?kbid=824105>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB824105-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 826232 (MS03-042)
+:: "Buffer Overflow in Windows Troubleshooter ActiveX Control Could Allow Code Execution"
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-042.asp>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB826232-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 823559
+:: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
+:: <http://support.microsoft.com/?kbid=823559>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB823559-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 824146
+:: "A Buffer Overrun in RPCSS May Allow Code Execution"
+:: (Worst Windows security hole, ever.  Again.)
+:: <http://support.microsoft.com/?kbid=824146>
+todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB824146-x86-%WINLANG%.exe /u /n /z"
 
 :: Critical update 824141 (MS03-045)
 :: "Buffer Overrun in the ListBox and in the ComboBox Control Could Allow Code Execution"
@@ -26,77 +130,10 @@ todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB825119-x86-%WINLANG%.
 :: <http://www.microsoft.com/technet/security/bulletin/MS03-043.asp>
 todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB828035-x86-%WINLANG%.exe /u /n /z"
 
-:: Critical update 826232 (MS03-042)
-:: "Buffer Overflow in Windows Troubleshooter ActiveX Control Could Allow Code Execution"
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-042.asp>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB826232-x86-%WINLANG%.exe /u /n /z"
-
 :: Critical update 823182 (MS03-041)
 :: "Vulnerability in Authenticode Verification Could Allow Remote Code Execution"
 :: <http://www.microsoft.com/technet/security/bulletin/MS03-041.asp>
 todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB823182-x86-%WINLANG%.exe /u /n /z"
-
-:: Root Certificates Update
-:: <http://download.windowsupdate.com/msdownload/update/v3/static/rtf/en/4702.htm>
-:: (download only available from Windows Catalog)
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\rootsupd.exe /q /r:n"
-
-:: Windows Journal Viewer update
-:: <http://download.windowsupdate.com/msdownload/update/v3/static/rtf/en/5850.htm>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\journal_viewer_%WINLANG%.exe /q /c:\"msiexec /i \"\"Microsoft Windows Journal Viewer.msi\"\" /qn\""
-
-:: Critical update 824146
-:: "A Buffer Overrun in RPCSS May Allow Code Execution"
-:: (Worst Windows security hole, ever.  Again.)
-:: <http://support.microsoft.com/?kbid=824146>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB824146-x86-%WINLANG%.exe /u /n /z"
-
-:: Critical update 824105
-:: "Flaw in NetBIOS Could Lead to Information Disclosure"
-:: <http://support.microsoft.com/?kbid=824105>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB824105-x86-%WINLANG%.exe /u /n /z"
-
-:: Critical update 823718
-:: "Security Update for Microsoft Data Access Components"
-:: (This will be superseded if MDAC 2.8 is installed later, but no
-:: harm done.)
-:: <http://support.microsoft.com/?kbid=823718>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Q823718_MDAC_SecurityPatch.exe /q /c:\"dahotfix.exe /q /n\""
-
-:: Microsoft DirectX 9.0b update
-:: <http://www.microsoft.com/directx/>
-:: Download the redistributable for developers (dx90b_redist.exe) and
-:: extract it to %Z%\packages\DirectX.
-:: <http://www.microsoft.com/downloads/details.aspx?familyid=a6dee0db-dcce-43ea-87bb-7c7e1fd1eaa2>
-:: (Requires .NET to be installed first for managed DX)
-todo.pl ".expect-reboot AutoIt %Z%\scripts\directx9.aut"
-
-:: Critical update 823559
-:: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
-:: <http://support.microsoft.com/?kbid=823559>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB823559-x86-%WINLANG%.exe /u /n /z"
-
-:: Recommended update 822831
-:: "BUG: Driver Installation Program Does Not Install Device Drivers"
-:: <http://support.microsoft.com/?kbid=822831>
-:: (download only available from Windows Catalog)
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB822831-x86-%WINLANG%.exe /u /n /z"
-
-:: Recommended update 818043
-:: "L2TP/IPSec NAT-T Update for Windows XP and Windows 2000"
-:: <http://support.microsoft.com/?kbid=818043>
-:: (download only available from Windows Catalog)
-:: NOTE: You must rename this to use the three-letter language
-:: abbreviation instead of two.  For example, for English you would
-:: rename Q818043_W2K_SP5_x86_EN.EXE to Q818043_W2K_SP5_x86_ENU.EXE.
-::
-:: As of 2003-10-19, this update was still not available from
-:: Windows Catalog.  See:
-:: <http://groups.google.com/groups?threadm=uJILwu%24kDHA.3612%40TK2MSFTNGP11.phx.gbl>
-:: Until Microsoft gets their act together, you will have to use
-:: Windows Update to install this one by hand.
-:: Commented out for now.
-:: todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Q818043_W2K_SP5_x86_%WINLANG%.EXE /u /n /z"
 
 :: Critical update 814078
 :: "Flaw in Windows Script Engine May Allow Code to Run"
@@ -105,10 +142,3 @@ todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\Windows2000-KB822831-x86-%WINLANG%.
 :: abbreviation instead of two.  For example, for English you would
 :: rename js56nen.exe to js56nenu.exe.
 todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\js56n%WINLANG%.exe /q /r:n"
-
-:: Microsoft .NET framework
-:: <http://msdn.microsoft.com/netframework/downloads/>
-todo.pl ".reboot-on 194 %Z%\updates\win2ksp4\dotnetfx.exe /q /c:\"install /q\""
-
-:: Install IE6
-todo.pl ie6.bat
