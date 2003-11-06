e05fc2788f683226e6e08285135b833cfa041eee
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-06 17:00:30 +0000

    Move q828750.exe to %Z%\updates\ie6sp1\%WINLANG%\ folder

--- unattended/install/scripts/winxpsp1-updates.bat
+++ unattended/install/scripts/winxpsp1-updates.bat
@@ -112,7 +112,7 @@ todo.pl ".reboot-on 194 %Z%\updates\winxpsp1\Q322011_WXP_SP2_x86_%WINLANG%.exe /
 :: Critical update 828750
 :: "October, 2003, Cumulative Patch for Internet Explorer"
 :: <http://support.microsoft.com/?kbid=828750>
-todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\q828750.exe /q /r:n"
+todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\%WINLANG%\q828750.exe /q /r:n"
 
 :: Critical update 330994
 :: "April 2003, Cumulative Patch for Outlook Express"
