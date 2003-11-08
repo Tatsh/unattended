71e4f215c13672de4ee1444b3b5a1a3815db5d5c
Shad L. Lords <slords@mail.com>
2003-11-08 16:01:03 +0000

    Make IE install on Celeron

--- unattended/install/scripts/ie6.bat
+++ unattended/install/scripts/ie6.bat
@@ -3,11 +3,6 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
-:: Critical update 828750
-:: "October, 2003, Cumulative Patch for Internet Explorer"
-:: <http://support.microsoft.com/?kbid=828750>
-todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\%WINLANG%\q828750.exe /q /r:n"
-
 :: Critical update 330994
 :: "April 2003, Cumulative Patch for Outlook Express"
 :: <http://support.microsoft.com/?kbid=330994>
@@ -18,5 +13,19 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\%WINLANG%\q330994.exe /q /r:n"
 :: "XMLHTTP Control in MSXML 3.0 Can Allow Access to Local Files"
 :: <http://support.microsoft.com/?kbid=318203>
 todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\Q318203_MSXML30_x86.exe /q:a /c:\"dahotfix /q /n\""
+
+:: Critical update 828750
+:: "October, 2003, Cumulative Patch for Internet Explorer"
+:: <http://support.microsoft.com/?kbid=828750>
+todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\%WINLANG%\q828750.exe /q /r:n"
+
+:: Critical update 318089
+:: "Incorrect VBScript Handling in Internet Explorer
+::  Can Allow Web Pages to Read Local Files"
+:: <http://support.microsoft.com/?kbid=318089>
+:: NOTE: You must rename this to use the three-letter language
+:: abbreviation instead of two.  For example, for English you would
+:: rename vbs56nen.exe to vbs56nenu.exe.
+todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\vbs56n%WINLANG%.exe /q /r:n"
 
 todo.pl ".reboot-on 194 %Z%\packages\ie6sp1\%WINLANG%\IE6setup.exe /q /r:n"
