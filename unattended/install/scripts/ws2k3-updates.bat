3c7576e6d51d63ce18cc4e6185d05422894f785b
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-12 20:33:41 +0000

    Added prepare scripts to download all files that are available freely  - This script will download ~1Gb of stuff from the internet
    Lots of changes and cleanups (needs good testing)
    Moved adobe-reader and acrobat-reader into /adobereader
    Updated AT&T Dialer to latest version v5.0.9.1
    Try to install Office 2000 SR1a from download
    Try to install Office XP updates from downloads
    Update php to v4.3.4
    Made note that powertoys are only for XP
    Install Windows 2000 SP4 from download
    Install Windows XP SP1 from download
    Make win2ksp4-symbols i18n compliant
    Rearrange updates to install most critical first
    Move OpenOffice txt file to /packages/openoffice
    Make all msiexec installs log installs
    
    Added instructions to cygwin, ie6, support-tools
      win2k-symbols, directx9
    
    
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@964 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/ws2k3-updates.bat
+++ unattended/install/scripts/ws2k3-updates.bat
@@ -6,10 +6,59 @@
 :: Note that since we are pushing these commands onto the to-do stack,
 :: they will be executed in the opposite order.
 
+:: Extra Updates
+
+:: "Update for Windows Media Player Script Commands"
+:: <http://support.microsoft.com/?kbid=828026>
+todo.pl ".reboot-on 194 %Z%\updates\mediaplayer9\WindowsMedia-KB828026-x86-%WINLANG%.exe /passive /n /norestart"
+
+:: Flaw In Windows Media Player May Allow Media Library Access (819639)
+:: <http://support.microsoft.com/?kbid=819639>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-021.asp>
+:: (Even though this is named the same as the one in mediaplayer9 it
+::  is different for 2003.  So this one goes in ws2k3 directory)
+todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsMedia9-KB819639-x86-%WINLANG%.exe /u /n /z"
+
+:: Microsoft DirectX 9.0b update
+:: <http://www.microsoft.com/directx/>
+:: Download the redistributable for developers (dx90b_redist.exe) and
+:: extract it to %Z%\packages\DirectX.
+:: <http://www.microsoft.com/downloads/details.aspx?familyid=a6dee0db-dcce-43ea-87bb-7c7e1fd1eaa2>
+:: (Requires .NET to be installed first for managed DX)
+todo.pl ".expect-reboot AutoIt %Z%\scripts\directx9.aut"
+
+:: Recommended Updates
+
 :: Recommended update 829558
 :: "Update for Jet 4.0 Service Pack 8"
 :: <http://support.microsoft.com/?kbid=829558>
 todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB829558-x86-%WINLANG%.exe /u /n /z"
+
+:: Recommended update 282010
+:: "Updated Version of Microsoft Jet 4.0"
+:: (is this still needed)
+:: <http://support.microsoft.com/?kbid=282010>
+:: (download only available from Windows Catalog)
+:: todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB282010-x86-%WINLANG%.exe /u /n /z"
+
+:: IE Critical Updates
+
+:: Critical update 824145
+:: "November, 2003, Cumulative Patch for Internet Explorer"
+:: <http://support.microsoft.com/?kbid=824145>
+todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB824145-x86-%WINLANG%.exe /passive /n /norestart"
+
+:: Critical Updates
+
+:: Critical update 824105
+:: "Flaw in NetBIOS Could Lead to Information Disclosure"
+:: <http://support.microsoft.com/?kbid=824105>
+todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB824105-x86-%WINLANG%.exe /u /n /z"
+
+:: Critical update 823559
+:: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
+:: <http://support.microsoft.com/?kbid=823559>
+todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB823559-x86-%WINLANG%.exe /u /n /z"
 
 :: Critical update 824141 (MS03-045)
 :: "Buffer Overrun in the ListBox and in the ComboBox Control Could Allow Code Execution"
@@ -36,45 +85,3 @@ todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB823182-x86-%WINLAN
 :: (Worst Windows security hole, ever.  Again.)
 :: <http://support.microsoft.com/?kbid=824146>
 todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB824146-x86-%WINLANG%.exe /u /n /z"
-
-:: Critical update 824105
-:: "Flaw in NetBIOS Could Lead to Information Disclosure"
-:: <http://support.microsoft.com/?kbid=824105>
-todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB824105-x86-%WINLANG%.exe /u /n /z"
-
-:: Critical update 823559
-:: "Buffer Overrun in the HTML Converter Could Allow Code Execution"
-:: <http://support.microsoft.com/?kbid=823559>
-todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB823559-x86-%WINLANG%.exe /u /n /z"
-
-:: Recommended update 282010
-:: "Updated Version of Microsoft Jet 4.0"
-:: <http://support.microsoft.com/default.aspx?kbid=282010>
-:: (download only available from Windows Catalog)
-todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB282010-x86-%WINLANG%.exe /u /n /z"
-
-:: "Update for Windows Media Player Script Commands"
-:: <http://support.microsoft.com/?kbid=828026>
-todo.pl ".reboot-on 194 %Z%\updates\mediaplayer9\WindowsMedia-KB828026-x86-%WINLANG%.exe /passive /n /norestart"
-
-:: Flaw In Windows Media Player May Allow Media Library Access (819639)
-:: <http://support.microsoft.com/?kbid=819639>
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-021.asp>
-:: (Even though this is named the same as the one in mediaplayer9 it
-::  is different for 2003.  So this one goes in ws2k3 directory)
-todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsMedia9-KB819639-x86-%WINLANG%.exe /u /n /z"
-
-:: Critical update 828750
-:: "October, 2003, Cumulative Patch for Internet Explorer"
-:: (Note: You must click through to get the special version of this
-:: update for Windows Server 2003)
-:: <http://support.microsoft.com/?kbid=828750>
-todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB828750-x86-%WINLANG%.exe /passive /n /norestart"
-
-:: Microsoft DirectX 9.0b update
-:: <http://www.microsoft.com/directx/>
-:: Download the redistributable for developers (dx90b_redist.exe) and
-:: extract it to %Z%\packages\DirectX.
-:: <http://www.microsoft.com/downloads/details.aspx?familyid=a6dee0db-dcce-43ea-87bb-7c7e1fd1eaa2>
-:: (Requires .NET to be installed first for managed DX)
-todo.pl ".expect-reboot AutoIt %Z%\scripts\directx9.aut"
