9ba2e634875c16b8f69b6e2d8208a1c2fa2dcfd1
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-12 20:03:36 +0000

    If command-line defrag.exe utility is present (Win XP and higher), use it.

--- unattended/install/scripts/defrag.bat
+++ unattended/install/scripts/defrag.bat
@@ -1,3 +1,12 @@
 :: Defragment the C: drive
 
+if exist %SystemRoot%\system32\defrag.exe goto got_defrag
+
+:: If we have no command-line defragmenter, use AutoIT script
 todo.pl "AutoIt %Z%\scripts\%WINLANG%\defrag.aut"
+goto :end
+
+:got_defrag
+todo.pl "%SystemRoot%\system32\defrag.exe %SystemDrive% -f -v"
+
+:end
