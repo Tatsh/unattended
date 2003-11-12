188ffbe15a5b83d36b50b888328b171d81926adc
Shad L. Lords <slords@mail.com>
2003-11-12 21:17:11 +0000

    Start Windows installer with start /wait

--- unattended/install/scripts/perl.bat
+++ unattended/install/scripts/perl.bat
@@ -4,7 +4,7 @@
 :: http://bugs.activestate.com/show_bug.cgi?id=20364)
 
 :: This will gracefully exit on both 2000, XP, and 2003 if already installed
-%Z%\packages\windowsinstaller\InstMsiW.exe /q /c:"msiinst.exe /i instmsi.msi /q"
+start /wait %Z%\packages\windowsinstaller\InstMsiW.exe /q /c:"msiinst.exe /i instmsi.msi /q"
 
 start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i %Z%\packages\ActivePerl-5.8.0.806-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
 @if errorlevel 1 exit 1
