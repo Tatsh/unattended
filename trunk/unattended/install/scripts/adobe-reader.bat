1049ef0f52e964c81b03bc874289870f5c35d8aa
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-13 23:22:46 +0000

    Include switches to create a log file

--- trunk/unattended/install/scripts/adobe-reader.bat
+++ trunk/unattended/install/scripts/adobe-reader.bat
@@ -1,4 +1,4 @@
 :: Install Adobe Reader
 
 :: Download from <http://www.adobe.com/products/acrobat/alternate.html>
-todo.pl "%Z%\packages\AdobeReader\AdbeRdr60_%WINLANG%_full.exe -p\"-s /v\\\"/qn\\\"\""
+todo.pl "%Z%\packages\AdobeReader\AdbeRdr60_%WINLANG%_full.exe -p\"-s /v\\\"/qb /l* c:\netinst\logs\adobe-reader.txt\\\"\""
