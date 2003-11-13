e8c978bfc966c84728ed80b53217d0658fc36071
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-13 23:15:15 +0000

    Add latest collection of security fixes for Office 2000.  Untested, but wanted to commit this ahead of Shad :-).
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@986 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/office2k.bat
+++ unattended/install/scripts/office2k.bat
@@ -3,6 +3,31 @@
 :: Reboot after installing everything (superstition)
 todo.pl .reboot
 
+:: "Excel 2002 Security Patch: November 11, 2003"
+:: <http://support.microsoft.com/?kbid=830349>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-050.asp>
+todo.pl "%Z%\updates\office2k\Office2000-kb830349-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\excelop.txt /p EXCELop.msp REBOOT=ReallySuppress\""
+
+:: "Word 2002 Security Patch: November 11, 2003"
+:: <http://support.microsoft.com/?kbid=830347>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-050.asp>
+todo.pl "%Z%\updates\office2k\Office2000-kb830347-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\winwordop.txt /p WINWORDop.msp REBOOT=ReallySuppress\""
+
+:: "Access 2002 Snapshot Viewer Security Patch: September 3, 2003"
+:: <http://support.microsoft.com/?kbid=826292>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-038.asp>
+todo.pl "%Z%\updates\office2k\office2000-kb826292-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\snapviewop.txt /p SNAPVIEWop.msp REBOOT=ReallySuppress\""
+
+:: "WordPerfect 5.x Converter Security Patch: September 3, 2003"
+:: <http://support.microsoft.com/?kbid=824993>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-036.asp>
+todo.pl "%Z%\updates\office2k\office2000-kb824993-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\msconvop.txt /p MSCONVop.msp REBOOT=ReallySuppress\""
+
+:: "Office 2000 Security Patch: September 3, 2003"
+:: <http://support.microsoft.com/?kbid=822035>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-037.asp>
+todo.pl "%Z%\updates\office2k\Office2000-KB822035-Client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\vbe6op.txt /p VBE6op.msp REBOOT=ReallySuppress\""
+
 :: Install the "Outlook 2000 Update: December 18, 2002"
 :: <http://support.microsoft.com/?kbid=811167>
 todo.pl "%Z%\updates\office2k\Olk0901.exe /r:n /q /c:\"msiexec /qb /l* c:\netinst\logs\Olk0901.txt /p OMIop.msp REBOOT=ReallySuppress\""
