36b9fcc22bef040be4e3fadd41fc8bb0adf04946
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-13 19:40:39 +0000

    Use %WINLANG% in filename

--- trunk/unattended/install/scripts/acrobat-reader.bat
+++ trunk/unattended/install/scripts/acrobat-reader.bat
@@ -1,5 +1,5 @@
 :: Install Adobe Acrobat reader
 
-start /wait %Z%\packages\AdobeReader\AcroReader51_ENU_full.exe /a /s /sms /f1%Z%\packages\AdobeReader\acrobat.iss /f2c:\netinst\logs\acrobat.txt
+start /wait %Z%\packages\AdobeReader\AcroReader51_%WINLANG%_full.exe /a /s /sms /f1%Z%\packages\AdobeReader\acrobat.iss /f2c:\netinst\logs\acrobat.txt
 
 if errorlevel 1 exit 1
