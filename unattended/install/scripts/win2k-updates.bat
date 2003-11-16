26666f899dea42bfdcbf3a73f8d3d78e1f851fa2
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-11-16 16:34:25 +0000

    Add URL's to each script for future download script
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1002 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/win2k-updates.bat
+++ unattended/install/scripts/win2k-updates.bat
@@ -4,6 +4,7 @@
 todo.pl win2ksp4-updates.bat
 
 :: Install Service Pack 4.
+:: URL|ENU|http://download.microsoft.com/download/E/6/A/E6A04295-D2A8-40D0-A0C5-241BFECD095E/W2KSP4_EN.EXE|updates/win2k/w2ksp4_enu.exe
 :: See <http://support.microsoft.com/?kbid=262839> for a description
 :: of the switches.
 todo.pl ".ignore-err 194 %Z%\updates\win2k\w2ksp4_%WINLANG%.exe /u /n /q /z" .reboot
