a3a5fe7ff76ca9e6b4e5bf25db237682935bed56
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-12-15 17:04:39 +0000

    Upgrade to 7-zip 3.13

--- unattended/install/scripts/7-zip.bat
+++ unattended/install/scripts/7-zip.bat
@@ -1,4 +1,4 @@
 :: OPTIONAL: Install 7-Zip
-:: URL|ALL|http://www.7-zip.org/dl/7z311.exe|packages/7z311.exe
+:: URL|ALL|http://www.7-zip.org/dl/7z313.exe|packages/7z313.exe
 
-todo.pl "%Z%\packages\7z311.exe -y /q /r:n"
+todo.pl "%Z%\packages\7z313.exe -y /q /r:n"
