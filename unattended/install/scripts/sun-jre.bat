d8afe03958f45accf18d025e9225654525e102c8
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2003-12-16 15:40:35 +0000

    Install Sun JVM (Java 2 Runtime Environment)

---
+++ unattended/install/scripts/sun-jre.bat
@@ -0,0 +1,9 @@
+:: OPTIONAL: Install Sun Java Virtual Machine
+
+:: Download from <http://java.sun.com/j2se/downloads/>
+
+:: See
+:: <http://java.sun.com/j2se/1.4.2/docs/guide/plugin/developer_guide/silent.html>
+:: for installation switches
+
+todo.pl "%Z%\packages\j2re-1_4_2_03-windows-i586-p.exe /s /v\"/qb IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress\""
