0bde4ce71764f08bff51d30c588cc81bb54c9791
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-01-14 19:32:21 +0000

    Upgrade to January 2004 edition
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1036 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/msdn.bat
+++ unattended/install/scripts/msdn.bat
@@ -6,4 +6,4 @@ todo.pl .reboot
 :: Use UNC path to installer, because MSDN library depends on the
 :: network path from which it was installed, and users do not normally
 :: map the Z: drive.
-todo.pl "msiexec /qb /l* c:\netinst\logs\msdn.txt /i %Z_PATH%\packages\msdn\Jul2003\msdn.msi SETUP_EXE=yes"
+todo.pl "msiexec /qb /l* c:\netinst\logs\msdn.txt /i %Z_PATH%\packages\msdn\Jan2004\msdn.msi SETUP_EXE=yes"
