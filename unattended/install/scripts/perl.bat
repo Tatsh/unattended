5dde1f75d5d92fecafc238d37e834eaf45e69adf
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-01-14 23:27:36 +0000

    Upgrade to ActivePerl 5.8.2 build 808

--- unattended/install/scripts/perl.bat
+++ unattended/install/scripts/perl.bat
@@ -9,6 +9,6 @@ if not exist %Z%\packages\windowsinstaller\instmsiw.exe goto nomsi
 :: (The latter is undocumented; see
 :: http://bugs.activestate.com/show_bug.cgi?id=20364)
 
-:: URL|ALL|http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.1.807-MSWin32-x86.msi|packages/activeperl-5.8.1.807-mswin32-x86.msi
-start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i %Z%\packages\ActivePerl-5.8.1.807-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
+:: URL|ALL|http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.2.808-MSWin32-x86.msi|packages/activeperl-5.8.2.808-mswin32-x86.msi
+start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i %Z%\packages\ActivePerl-5.8.2.808-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
 @if errorlevel 1 exit 1
