05c670a593f83d581c24dc3acc25b6c74d7f0165
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-01-16 17:27:28 +0000

    Update for Mozilla 1.6

--- unattended/install/scripts/mozilla.bat
+++ unattended/install/scripts/mozilla.bat
@@ -1,4 +1,4 @@
 :: OPTIONAL: Install Mozilla
-:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.5/mozilla-win32-1.5-installer.exe|packages/mozilla-win32-1.5-installer.exe
+:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.6/mozilla-win32-1.6-installer.exe|packages/mozilla-win32-1.6-installer.exe
 
-todo.pl "%Z%\packages\mozilla-win32-1.5-installer.exe -ma -ira"
+todo.pl "%Z%\packages\mozilla-win32-1.6-installer.exe -ma -ira"
