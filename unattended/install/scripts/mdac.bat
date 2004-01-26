69001a6a2e1c5d9ebcb3370a2242ce53a1ffd93d
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-01-26 17:42:54 +0000

    Stupid SourceForge broken CVS

--- unattended/install/scripts/mdac.bat
+++
@@ -1,5 +0,0 @@
-:: Install latest version of Microsoft Data Access Components
-
-:: Download from <http://www.microsoft.com/data/>.  Rename
-:: mdac_typ.exe to something more descriptive.
-todo.pl ".ignore-err 194 %Z%\packages\mdac-2.8.exe /q /c:\"setup /qn1\"" .reboot
