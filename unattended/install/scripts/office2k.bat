f2b212d68589f5e44a0b260e0e716499aa42048f
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-03-14 20:49:31 +0000

    added url's for russian files
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1220 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/office2k.bat
+++ unattended/install/scripts/office2k.bat
@@ -8,61 +8,70 @@ if not %WINVER%. == winxpsp1. goto didfpse
 :: Critical update 810217 (MS03-051)
 :: URL|ENU|http://download.microsoft.com/download/e/7/1/e71674de-9f52-4b5a-86c8-5499c359f89d/WindowsXP-KB810217-x86-ENU.exe|updates/winxpsp1/windowsxp-kb810217-x86-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/e/b/d/ebd43dd9-9cc8-4ea9-b6e1-dfc75a78984f/WindowsXP-KB810217-x86-NLD.exe|updates/winxpsp1/windowsxp-kb810217-x86-nld.exe
+:: URL|RUS|http://download.microsoft.com/download/a/a/d/aad356bb-ca20-4f89-a5bc-3a9b474c0d4c/WindowsXP-KB810217-x86-RUS.exe|updates/winxpsp1/windowsxp-kb810217-x86-rus.exe
 :: "Buffer Overrun in Microsoft FrontPage Server Extensions Could Allow Code Execution"
 :: <http://support.microsoft.com/?kbid=810217>
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp1\WindowsXP-KB810217-x86-%WINLANG%.exe /u /n /z"
 :didfpse
 
-:: "Excel 2002 Security Patch: November 11, 2003"
+:: "Excel 2000 Security Patch: November 11, 2003"
 :: URL|ENU|http://download.microsoft.com/download/3/4/e/34e4d08b-f015-4bc8-84e6-f827ecbc4107/office2000-kb830349-client-enu.exe|updates/office2k/office2000-kb830349-client-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/e/e/7/ee74120e-cbb7-4f27-bc1f-933a4c4d6576/office2000-kb830349-client-nld.exe|updates/office2k/office2000-kb830349-client-nld.exe
+:: URL|RUS|http://download.microsoft.com/download/a/b/f/abf7c3a2-296f-4219-ad58-653d334d7b51/office2000-kb830349-client-rus.exe|updates/office2k/office2000-kb830349-client-rus.exe
 :: <http://support.microsoft.com/?kbid=830349>
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-050.asp>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-050.mspx>
 todo.pl "%Z%\updates\office2k\Office2000-kb830349-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\excelop.txt /p EXCELop.msp REBOOT=ReallySuppress\""
 
-:: "Word 2002 Security Patch: November 11, 2003"
+:: "Word 2000 Security Patch: November 11, 2003"
 :: URL|ENU|http://download.microsoft.com/download/f/2/2/f225f9c6-87ef-45bd-a4f7-cc08bb14f4da/Office2000-kb830347-client-enu.exe|updates/office2k/office2000-kb830347-client-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/c/e/8/ce8709c4-f1e6-4de6-933f-07b0c7c6ab39/office2000-kb830347-client-nld.exe|updates/office2k/office2000-kb830347-client-nld.exe
+:: URL|RUS|http://download.microsoft.com/download/8/b/f/8bfb329a-743b-4efa-bcd5-a020133fd71a/office2000-kb830347-client-rus.exe|updates/office2k/office2000-kb830347-client-rus.exe
 :: <http://support.microsoft.com/?kbid=830347>
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-050.asp>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-050.mspx>
 todo.pl "%Z%\updates\office2k\Office2000-kb830347-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\winwordop.txt /p WINWORDop.msp REBOOT=ReallySuppress\""
 
-:: "Access 2002 Snapshot Viewer Security Patch: September 3, 2003"
+:: "Access 2000 Snapshot Viewer Security Patch: September 3, 2003"
 :: URL|ENU|http://download.microsoft.com/download/2/d/5/2d5a89c9-972f-4135-a8d9-3747188552b2/office2000-kb826292-client-enu.exe|updates/office2k/office2000-kb826292-client-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/8/2/9/829f8dcc-ed43-4054-a3c5-ea630db63f63/office2000-kb826292-client-nld.exe|updates/office2k/office2000-kb826292-client-nld.exe
+:: URL|RUS|http://download.microsoft.com/download/1/e/c/1ecb33e7-bc4e-46d0-ac80-d9bfb6c04cb5/office2000-kb826292-client-rus.exe|updates/office2k/office2000-kb826292-client-rus.exe
 :: <http://support.microsoft.com/?kbid=826292>
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-038.asp>
+:: <http://www.microsoft.com/technet/security/bulletin/ms03-038.mspx>
 todo.pl "%Z%\updates\office2k\office2000-kb826292-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\snapviewop.txt /p SNAPVIEWop.msp REBOOT=ReallySuppress\""
 
 :: "WordPerfect 5.x Converter Security Patch: September 3, 2003"
 :: URL|ENU|http://download.microsoft.com/download/8/7/8/8785bb28-b4e3-401d-9aef-c23e7af0e347/office2000-kb824993-client-enu.exe|updates/office2k/office2000-kb824993-client-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/9/b/1/9b1d2c2f-7261-4040-ac27-abb796897655/office2000-kb824993-client-nld.exe|updates/office2k/office2000-kb824993-client-nld.exe
+:: URL|RUS|http://download.microsoft.com/download/5/e/2/5e21412f-aa5f-4efc-abf6-8e918f854061/office2000-kb824993-client-rus.exe|updates/office2k/office2000-kb824993-client-rus.exe
 :: <http://support.microsoft.com/?kbid=824993>
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-036.asp>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-036.mspx>
 todo.pl "%Z%\updates\office2k\office2000-kb824993-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\msconvop.txt /p MSCONVop.msp REBOOT=ReallySuppress\""
 
 :: "Office 2000 Security Patch: September 3, 2003"
 :: URL|ENU|http://download.microsoft.com/download/5/E/D/5ED56804-6577-46E4-88EE-9EB47AC6AA0E/Office2000-KB822035-Client-ENU.exe|updates/office2k/office2000-kb822035-client-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/2/3/7/2376a700-6512-4f4c-b5ec-ff24018376bc/office2000-kb822035-client-nld.exe|updates/office2k/office2000-kb822035-client-nld.exe
+:: URL|RUS|http://download.microsoft.com/download/a/6/6/a6608caa-6aff-499a-ae77-2eef52ec92cf/office2000-kb822035-client-rus.exe|updates/office2k/office2000-kb822035-client-rus.exe
 :: <http://support.microsoft.com/?kbid=822035>
-:: <http://www.microsoft.com/technet/security/bulletin/MS03-037.asp>
+:: <http://www.microsoft.com/technet/security/bulletin/MS03-037.mspx>
 todo.pl "%Z%\updates\office2k\Office2000-KB822035-Client-%WINLANG%.exe /q /c:\"msiexec /qb /l* c:\netinst\logs\vbe6op.txt /p VBE6op.msp REBOOT=ReallySuppress\""
 
 :: Install the "Outlook 2000 Update: December 18, 2002"
 :: URL|ENU|http://download.microsoft.com/download/f/1/d/f1d8421a-2b76-4603-808b-39d8dcc74038/Olk0901.exe|updates/office2k/olk0901.exe
 :: URL|NLD|http://download.microsoft.com/download/9/3/e/93e84ec0-9f9e-466e-8e1b-9a4f3ac53c0c/Olk0901.exe|updates/office2k/olk0901.exe
+:: URL|RUS|http://download.microsoft.com/download/c/f/0/cf0e4961-c166-4200-b346-bb52873f70a3/Olk0901.exe|updates/office2k/olk0901.exe
 :: <http://support.microsoft.com/?kbid=811167>
 todo.pl "%Z%\updates\office2k\Olk0901.exe /r:n /q /c:\"msiexec /qb /l* c:\netinst\logs\Olk0901.txt /p OMIop.msp REBOOT=ReallySuppress\""
 
 :: Install View Control Security Update
 :: URL|ENU|http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/EN-US/O2kSp3.exe|updates/office2k/o2ksp3.exe
 :: URL|NLD|http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/NL/O2kSp3.exe|updates/office2k/o2ksp3.exe
+:: URL|RUS|http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/RU/O2kSp3.exe|updates/office2k/o2ksp3.exe
 :: (part of SP3, but not installed via MSI)
 todo.pl "%Z%\updates\office2k\o2ksp3.exe /r:n /q /c:\"outlctlx.exe /q /r:n\""
 
 :: Install Service Pack 3
 :: URL|ENU|http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/EN-US/O2kSp3.exe|updates/office2k/o2ksp3.exe
 :: URL|NLD|http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/NL/O2kSp3.exe|updates/office2k/o2ksp3.exe
+:: URL|RUS|http://download.microsoft.com/download/office2000/SP/3/WIN98MeXP/RU/O2kSp3.exe|updates/office2k/o2ksp3.exe
 :: See <http://support.microsoft.com/?kbid=326585>
 :: Download from:
 :: <http://office.microsoft.com/downloads/2000/o2ksp3.aspx>
@@ -71,6 +80,7 @@ todo.pl "%Z%\updates\office2k\o2ksp3.exe /r:n /q /c:\"msiexec /qb /l* c:\netinst
 :: Install SR-1a
 :: URL|ENU|http://download.microsoft.com/download/office2000prem/adminup2/SR-1a/WIN98/EN-US/O2KSR1aDL.EXE|updates/office2k/o2ksr1adl.exe
 :: URL|NLD|http://download.microsoft.com/download/office2000prem/SP/SR-1a/Win98/NL/O2KSR1ADL.EXE|updates/office2k/o2ksr1adl.exe
+:: URL|RUS|http://download.microsoft.com/download/office2000prem/SP/SR-1a/Win98/RU/O2KSR1ADL.EXE|updates/office2k/o2ksr1adl.exe
 :: See <http://support.microsoft.com/?kbid=245025>
 :: and <http://support.microsoft.com/?kbid=258323>
 :: Download O2KSR1aDL.exe from:
--- unattended/install/scripts/win2k-updates.bat
+++ unattended/install/scripts/win2k-updates.bat
@@ -6,6 +6,7 @@ todo.pl win2ksp4-updates.bat
 :: Install Service Pack 4.
 :: URL|ENU|http://download.microsoft.com/download/E/6/A/E6A04295-D2A8-40D0-A0C5-241BFECD095E/W2KSP4_EN.EXE|updates/win2k/w2ksp4_enu.exe
 :: URL|NLD|http://download.microsoft.com/download/1/b/7/1b7b823b-e760-4aa3-99d6-d9843cf71e7f/w2ksp4_nl.exe|updates/win2k/w2ksp4_nld.exe
+:: URL|RUS|http://download.microsoft.com/download/6/1/1/611e63b8-95ec-40f4-b96c-e676f96d1a95/w2ksp4_ru.exe|updates/win2k/w2ksp4_rus.exe
 :: See <http://support.microsoft.com/?kbid=262839> for a description
 :: of the switches.
 todo.pl ".ignore-err 194 %Z%\updates\win2k\w2ksp4_%WINLANG%.exe /u /n /q /z" .reboot
