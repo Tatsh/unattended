6d995be71e60ecc6c3b690109f9f858b01e39a2d
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-03-14 23:15:17 +0000

    German updates and downloads.  Thanks to Gerhard Heift.

--- trunk/unattended/install/scripts/ie6.bat
+++ trunk/unattended/install/scripts/ie6.bat
@@ -5,6 +5,7 @@
 
 :: Critical update 832894 (MS04-004)
 :: URL|ENU|http://download.microsoft.com/download/9/b/3/9b3f16b4-a907-468b-8283-c050d1ea4948/Q832894.exe|updates/ie6sp1/enu/q832894.exe
+:: URL|DEU|http://download.microsoft.com/download/0/f/1/0f134842-5111-4f87-a2b3-e01c8f80e5b9/Q832894.exe|updates/ie6sp1/deu/q832894.exe
 :: URL|NLD|http://download.microsoft.com/download/2/e/d/2ed7c7e7-4922-48c6-bf4f-c32b76cba519/Q832894.exe|updates/ie6sp1/nld/q832894.exe
 :: URL|RUS|http://download.microsoft.com/download/c/f/d/cfd3fc45-d53b-4c6d-878d-de4282069a26/Q832894.exe|updates/ie6sp1/rus/q832894.exe
 :: "February, 2004, Cumulative Patch for Internet Explorer"
@@ -14,6 +15,7 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\%WINLANG%\Q832894.exe /q /r:n"
 
 :: Critical update 330994
 :: URL|ENU|http://download.microsoft.com/download/1/5/f/15f23ca7-965e-4a10-95a5-5b3d6a83f218/q330994.exe|updates/ie6sp1/enu/q330994.exe
+:: URL|DEU|http://download.microsoft.com/download/f/5/6/f5628ef3-76cd-45e4-966a-e93c43bf8256/q330994.exe|updates/ie6sp1/deu/q330994.exe
 :: URL|NLD|http://download.microsoft.com/download/b/4/6/b4667035-5df2-420a-bfd7-b8ab5542eb80/q330994.exe|updates/ie6sp1/nld/q330994.exe
 :: URL|RUS|http://download.microsoft.com/download/6/5/6/656e2c57-570f-460f-90f6-bcd542f3fe73/q330994.exe|upsates/ie6sp1/rus/q330994.exe
 :: "April 2003, Cumulative Patch for Outlook Express"
@@ -29,6 +31,7 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\Q318203_MSXML30_x86.exe /q:a /c:\"dah
 
 :: Critical update 318089
 :: URL|ENU|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/EN-US/vbs56nen.exe|updates/ie6sp1/vbs56nenu.exe
+:: URL|DEU|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/DE/vbs56nde.exe|updates/ie6sp1/vbs56ndeu.exe
 :: URL|NLD|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/NL/vbs56nnl.exe|updates/ie6sp1/vbs56nnld.exe
 :: URL|RUS|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/RU/vbs56nru.exe|updates/ie6sp1/vbs56nrus.exe
 :: "Incorrect VBScript Handling in Internet Explorer
@@ -42,6 +45,7 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\vbs56n%WINLANG%.exe /q /r:n"
 
 :: Download Internet Explorer 6 SP1 installer from Microsoft then issue:
 :: URL|ENU|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/EN-US/ie6setup.exe|packages/ie6sp1/enu/ie6setup.exe
+:: URL|DEU|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/DE/ie6setup.exe|packages/ie6sp1/deu/ie6setup.exe
 :: URL|NLD|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/NL/ie6setup.exe|packages/ie6sp1/nld/ie6setup.exe
 :: URL|RUS|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/RU/ie6setup.exe|packages/ie6sp1/rus/ie6setup.exe
 :: ie6setup.exe /c:"ie6wzd.exe /d /s:""#E"
--- trunk/unattended/install/scripts/win2k-updates.bat
+++ trunk/unattended/install/scripts/win2k-updates.bat
@@ -5,6 +5,7 @@ todo.pl win2ksp4-updates.bat
 
 :: Install Service Pack 4.
 :: URL|ENU|http://download.microsoft.com/download/E/6/A/E6A04295-D2A8-40D0-A0C5-241BFECD095E/W2KSP4_EN.EXE|updates/win2k/w2ksp4_enu.exe
+:: URL|DEU|http://download.microsoft.com/download/c/3/5/c35591e3-52b6-4be0-95d3-ec82fa01ce12/W2KSP4_DE.EXE|updates/win2k/w2ksp4_deu.exe
 :: URL|NLD|http://download.microsoft.com/download/1/b/7/1b7b823b-e760-4aa3-99d6-d9843cf71e7f/w2ksp4_nl.exe|updates/win2k/w2ksp4_nld.exe
 :: URL|RUS|http://download.microsoft.com/download/6/1/1/611e63b8-95ec-40f4-b96c-e676f96d1a95/w2ksp4_ru.exe|updates/win2k/w2ksp4_rus.exe
 :: See <http://support.microsoft.com/?kbid=262839> for a description
