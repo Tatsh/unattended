d4b8ff8923c04fe1b8100f62044f96f12255c150
sfaivre <sfaivre@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-03-18 11:31:55 +0000

    added french urls

--- trunk/unattended/install/scripts/ie6.bat
+++ trunk/unattended/install/scripts/ie6.bat
@@ -8,6 +8,7 @@
 :: URL|DEU|http://download.microsoft.com/download/0/f/1/0f134842-5111-4f87-a2b3-e01c8f80e5b9/Q832894.exe|updates/ie6sp1/deu/q832894.exe
 :: URL|NLD|http://download.microsoft.com/download/2/e/d/2ed7c7e7-4922-48c6-bf4f-c32b76cba519/Q832894.exe|updates/ie6sp1/nld/q832894.exe
 :: URL|RUS|http://download.microsoft.com/download/c/f/d/cfd3fc45-d53b-4c6d-878d-de4282069a26/Q832894.exe|updates/ie6sp1/rus/q832894.exe
+:: URL|FRA|http://download.microsoft.com/download/a/7/c/a7cf4dc1-b1a0-4273-8b42-5e2a4139a478/Q832894.exe|updates/ie6sp1/fra/q832894.exe
 :: "February, 2004, Cumulative Patch for Internet Explorer"
 :: <http://support.microsoft.com/?kbid=832894>
 :: <http://www.microsoft.com/technet/security/bulletin/MS04-004.asp>
@@ -17,7 +18,8 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\%WINLANG%\Q832894.exe /q /r:n"
 :: URL|ENU|http://download.microsoft.com/download/1/5/f/15f23ca7-965e-4a10-95a5-5b3d6a83f218/q330994.exe|updates/ie6sp1/enu/q330994.exe
 :: URL|DEU|http://download.microsoft.com/download/f/5/6/f5628ef3-76cd-45e4-966a-e93c43bf8256/q330994.exe|updates/ie6sp1/deu/q330994.exe
 :: URL|NLD|http://download.microsoft.com/download/b/4/6/b4667035-5df2-420a-bfd7-b8ab5542eb80/q330994.exe|updates/ie6sp1/nld/q330994.exe
-:: URL|RUS|http://download.microsoft.com/download/6/5/6/656e2c57-570f-460f-90f6-bcd542f3fe73/q330994.exe|upsates/ie6sp1/rus/q330994.exe
+:: URL|RUS|http://download.microsoft.com/download/6/5/6/656e2c57-570f-460f-90f6-bcd542f3fe73/q330994.exe|updates/ie6sp1/rus/q330994.exe
+:: URL|FRA|http://download.microsoft.com/download/d/7/2/d727f4ed-92a4-4d60-8be2-8e42e891cc8e/q330994.exe|updates/ie6sp1/fra/q330994.exe
 :: "April 2003, Cumulative Patch for Outlook Express"
 :: <http://support.microsoft.com/?kbid=330994>
 :: <http://www.microsoft.com/windows/ie/downloads/critical/330994/>
@@ -34,6 +36,7 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\Q318203_MSXML30_x86.exe /q:a /c:\"dah
 :: URL|DEU|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/DE/vbs56nde.exe|updates/ie6sp1/vbs56ndeu.exe
 :: URL|NLD|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/NL/vbs56nnl.exe|updates/ie6sp1/vbs56nnld.exe
 :: URL|RUS|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/RU/vbs56nru.exe|updates/ie6sp1/vbs56nrus.exe
+:: URL|FRA|http://download.microsoft.com/download/IE60/Patch/Q318089/NT5XP/FR/vbs56nfr.exe|updates/ie6sp1/vbs56nfra.exe
 :: "Incorrect VBScript Handling in Internet Explorer
 ::  Can Allow Web Pages to Read Local Files"
 :: <http://support.microsoft.com/?kbid=318089>
@@ -48,6 +51,7 @@ todo.pl ".reboot-on 194 %Z%\updates\ie6sp1\vbs56n%WINLANG%.exe /q /r:n"
 :: URL|DEU|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/DE/ie6setup.exe|packages/ie6sp1/deu/ie6setup.exe
 :: URL|NLD|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/NL/ie6setup.exe|packages/ie6sp1/nld/ie6setup.exe
 :: URL|RUS|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/RU/ie6setup.exe|packages/ie6sp1/rus/ie6setup.exe
+:: URL|FRA|http://download.microsoft.com/download/ie6sp1/finrel/6_sp1/W98NT42KMeXP/FR/ie6setup.exe|packages/ie6sp1/fra/ie6setup.exe
 :: ie6setup.exe /c:"ie6wzd.exe /d /s:""#E"
 :: This will allow you to download all necessary files for install.
 todo.pl ".reboot-on 194 %Z%\packages\ie6sp1\%WINLANG%\IE6setup.exe /q /r:n"
