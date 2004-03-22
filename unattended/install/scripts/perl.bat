10ff8d646829082b0e5e9b08bb352a9c1837b71f
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-03-22 13:58:23 +0000

    Replace C: with %SystemDrive%.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1293 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/perl.bat
+++ unattended/install/scripts/perl.bat
@@ -16,5 +16,5 @@ if not exist %Z%\packages\windowsinstaller\instmsiw.exe goto nomsi
 :: http://bugs.activestate.com/show_bug.cgi?id=20364)
 
 :: URL|ALL|http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.3.809-MSWin32-x86.msi|packages/activeperl-5.8.3.809-mswin32-x86.msi
-start /wait msiexec /qb /l* c:\netinst\logs\perl.txt /i %Z%\packages\ActivePerl-5.8.3.809-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
+start /wait msiexec /qb /l* %SystemDrive%\netinst\logs\perl.txt /i %Z%\packages\ActivePerl-5.8.3.809-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
 @if errorlevel 1 exit 1
--- unattended/install/scripts/vs6.bat
+++ unattended/install/scripts/vs6.bat
@@ -5,13 +5,13 @@ start /wait regedit /s %Z%\scripts\vs6.reg
 if errorlevel 1 exit 1
 
 :: Install latest service pack
-todo.pl "%Z%\updates\VS6\setupsp5.exe /qn1 /g c:\netinst\logs\vs6sp5.txt" .reboot
+todo.pl "%Z%\updates\VS6\setupsp5.exe /qn1 /g %SystemDrive%\netinst\logs\vs6sp5.txt" .reboot
 
 :: NOTE: You must create %Z%\site\vs6_key.bat to set the vs6_key
 :: variable to your license key
 call %Z%\site\vs6_key.bat
 if %vs6_key%.==. goto nokey
-todo.pl "%Z%\packages\VS6\acmboot.exe /qn1 /g c:\netinst\logs\vs6.txt /k %vs6_key%" .reboot
+todo.pl "%Z%\packages\VS6\acmboot.exe /qn1 /g %SystemDrive%\netinst\logs\vs6.txt /k %vs6_key%" .reboot
 if errorlevel 1 exit 1
 exit 0
 
