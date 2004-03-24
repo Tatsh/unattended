c5b363fea14662d37104d59301fbb2dbe54c91ea
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-03-24 14:04:27 +0000

    Add German download URLs.  Thanks to Jan Brauer.

--- trunk/unattended/install/scripts/winxp-updates.bat
+++ trunk/unattended/install/scripts/winxp-updates.bat
@@ -7,6 +7,7 @@ todo.pl winxpsp1-updates.bat
 :: URL|ENU|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/en-us/xpsp1_en_x86.exe|updates/winxp/xpsp1_enu_x86.exe
 :: URL|NLD|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/nl/xpsp1_nl_x86.exe|updates/winxp/xpsp1_nld_x86.exe
 :: URL|RUS|http://download.microsoft.com/download/f/d/7/fd7a4d93-1cf6-40ac-93c9-c99338aa95ec/xpsp1a_ru_x86.exe|updates/winxp/xpsp1_rus_x86.exe
+:: URL|DEU|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/de/xpsp1_de_x86.exe|updates/winxp/xpsp1_deu_x86.exe
 :: See <http://support.microsoft.com/?kbid=262839> for a description
 :: of the switches.
 todo.pl ".ignore-err 194 %Z%\updates\winxp\xpsp1_%WINLANG%_x86.exe /n /q /u /z" .reboot
