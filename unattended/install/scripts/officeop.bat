b0f399950cae59b36ebd2048078a1f8b976b566d
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-03-31 13:34:08 +0000

    Add WINLANG to download/install path.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1323 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/officeop.bat
+++ unattended/install/scripts/officeop.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: Install OpenOffice.org
-:: URL|ENU|http://www.ibiblio.org/pub/mirrors/openoffice/stable/1.1.0/OOo_1.1.0_Win32Intel_install.zip|packages/openoffice/ooo_1.1.0_win32intel_install_enu.zip
-:: URL|DEU|http://sunsite.informatik.rwth-aachen.de/ftp/pub/mirror/OpenOffice/localized/de/1.1.0/OOo_1.1.0_Win32Intel_install_de.zip|packages/openoffice/ooo_1.1.0_win32intel_install_deu.zip
+:: URL|ENU|http://www.ibiblio.org/pub/mirrors/openoffice/stable/1.1.0/OOo_1.1.0_Win32Intel_install.zip|packages/openoffice/enu/ooo_1.1.0_win32intel_install_enu.zip
+:: URL|DEU|http://sunsite.informatik.rwth-aachen.de/ftp/pub/mirror/OpenOffice/localized/de/1.1.0/OOo_1.1.0_Win32Intel_install_de.zip|packages/openoffice/deu/ooo_1.1.0_win32intel_install_deu.zip
 todo.pl "shortcut.pl \"%ProgramFiles%\"\OpenOffice.org\program\soffice.exe special:AllUsersDesktop\OpenOffice"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\OpenOffice.org\program\soffice.exe special:AllUsersPrograms\OpenOffice"
 
-todo.pl "%Z_PATH%\packages\OpenOffice\setup.exe -r:%Z%\packages\OpenOffice\officeop.txt -debug"
+todo.pl "%Z_PATH%\packages\OpenOffice\%WINLANG%\setup.exe -r:%Z%\packages\OpenOffice\%WINLANG%\officeop.txt -debug"
