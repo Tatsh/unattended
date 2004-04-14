4dc736c48c14e110e9d8b0ae91b706619f29b446
Shad L. Lords <slords@mail.com>
2004-04-14 17:39:58 +0000

    permierdownload site stopped working.  Guess we need to get SP1a for now

--- unattended/install/scripts/winxp-updates.bat
+++ unattended/install/scripts/winxp-updates.bat
@@ -4,11 +4,11 @@
 todo.pl winxpsp1-updates.bat
 
 :: Install Service Pack 1.
-:: URL|ENU|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/en-us/xpsp1_en_x86.exe|updates/winxp/xpsp1_enu_x86.exe
-:: URL|NLD|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/nl/xpsp1_nl_x86.exe|updates/winxp/xpsp1_nld_x86.exe
-:: URL|RUS|http://download.microsoft.com/download/f/d/7/fd7a4d93-1cf6-40ac-93c9-c99338aa95ec/xpsp1a_ru_x86.exe|updates/winxp/xpsp1_rus_x86.exe
+:: URL|ENU|http://download.microsoft.com/download/5/4/f/54f8bcf8-bb4d-4613-8ee7-db69d01735ed/xpsp1a_en_x86.exe|updates/winxp/xpsp1a_enu_x86.exe
+:: URL|NLD|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/nl/xpsp1_nl_x86.exe|updates/winxp/xpsp1a_nld_x86.exe
+:: URL|RUS|http://download.microsoft.com/download/f/d/7/fd7a4d93-1cf6-40ac-93c9-c99338aa95ec/xpsp1a_ru_x86.exe|updates/winxp/xpsp1a_rus_x86.exe
 :: URL|DEU|http://premierdownload.microsoft.com/download/whistler/SP/SP1/WXP/de/xpsp1_de_x86.exe|updates/winxp/xpsp1_deu_x86.exe
-:: URL|ITA|http://download.microsoft.com/download/6/a/7/6a727e5c-c84b-45ef-b943-b0080acfe352/xpsp1a_it_x86.exe|updates/winxp/xpsp1_ita_x86.exe
+:: URL|ITA|http://download.microsoft.com/download/6/a/7/6a727e5c-c84b-45ef-b943-b0080acfe352/xpsp1a_it_x86.exe|updates/winxp/xpsp1a_ita_x86.exe
 :: See <http://support.microsoft.com/?kbid=262839> for a description
 :: of the switches.
-todo.pl ".ignore-err 194 %Z%\updates\winxp\xpsp1_%WINLANG%_x86.exe /n /q /u /z" .reboot
+todo.pl ".ignore-err 194 %Z%\updates\winxp\xpsp1a_%WINLANG%_x86.exe /n /q /u /z" .reboot
