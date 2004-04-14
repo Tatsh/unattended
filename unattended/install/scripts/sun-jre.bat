5a0f0e2cb3356fbb563128f07c3718ea68004f8b
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-04-14 16:51:06 +0000

    Add link to page with direct download which I finally found.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1378 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -1,10 +1,12 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 
 :: URL|ALL|http://java.sun.com/webapps/download/AutoDL?BundleId=9567|packages/j2re-1_4_2_04-windows-i586-p.exe
-:: Download from <http://java.sun.com/j2se/downloads/>
+
+:: Download from <http://java.sun.com/j2se/downloads/>.  Direct link
+:: at <http://www.java.com/en/download/windows_manual.jsp>.
 
 :: See
 :: <http://java.sun.com/j2se/1.4.2/docs/guide/plugin/developer_guide/silent.html>
-:: for installation switches
+:: for installation switches.
 
 todo.pl "%Z%\packages\j2re-1_4_2_04-windows-i586-p.exe /s /v\"/qb IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress\""
