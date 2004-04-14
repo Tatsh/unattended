5657448a467181b96b55a01fca97f5b5a2a04b1a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-04-14 16:49:07 +0000

    Update to 1.4.2_04.

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -1,10 +1,10 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 
-:: URL|ALL|http://java.sun.com/webapps/download/AutoDL?BundleId=9505|packages/j2re-1_4_2_03-windows-i586-p.exe
+:: URL|ALL|http://java.sun.com/webapps/download/AutoDL?BundleId=9567|packages/j2re-1_4_2_04-windows-i586-p.exe
 :: Download from <http://java.sun.com/j2se/downloads/>
 
 :: See
 :: <http://java.sun.com/j2se/1.4.2/docs/guide/plugin/developer_guide/silent.html>
 :: for installation switches
 
-todo.pl "%Z%\packages\j2re-1_4_2_03-windows-i586-p.exe /s /v\"/qb IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress\""
+todo.pl "%Z%\packages\j2re-1_4_2_04-windows-i586-p.exe /s /v\"/qb IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress\""
