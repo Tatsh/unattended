1b469ec1388822d241f0a19bcbccc692865e5827
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-04-14 17:41:34 +0000

    update location of sshclient program
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1380 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sshclient.bat
+++ unattended/install/scripts/sshclient.bat
@@ -1,5 +1,5 @@
 :: Install SSH Client (http://www.ssh.com)
-:: URL|ENU|http://www.icrew.org/ssh/SSHSecureShellClient-3.2.9.exe|packages/sshsecureshellclient/sshsecureshellclient-3.2.9.exe
+:: URL|ENU|http://www.icrew.org/mirror/ssh/SSHSecureShellClient-3.2.9.exe|packages/sshsecureshellclient/sshsecureshellclient-3.2.9.exe
 
 start /wait %Z%\packages\SSHSecureShellClient\SSHSecureShellClient-3.2.9.exe /s /a /s /sms /f1%Z%\packages\SSHSecureShellClient\SSHSecureShellClient.iss /f2c:\netinst\logs\SSHSecureShellClient.txt
 
