a5f750ecf1fb63ccd248c17d609f3da5f64a53b7
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-04-18 17:41:35 +0000

    Upgrade to PHP 4.3.6.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1399 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/php.bat
+++ unattended/install/scripts/php.bat
@@ -1,4 +1,4 @@
 :: Install PHP
-:: URL|ALL|http://us3.php.net/get/php-4.3.4-installer.exe/from/www.php.net/mirror|packages/php-4.3.4-installer.exe
+:: URL|ALL|http://www.php.net/get/php-4.3.6-installer.exe/from/www.php.net/mirror|packages/php-4.3.6-installer.exe
 
-todo.pl "%Z%\packages\php-4.3.4-installer.exe /s"
+todo.pl "%Z%\packages\php-4.3.6-installer.exe /s"
