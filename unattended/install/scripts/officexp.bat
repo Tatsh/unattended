25eacb7bd859b1783586b83d4c6506d53fe7fd62
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-04-21 14:31:12 +0000

    Add German URL for 833858.  Thanks to Jan Brauer.

--- unattended/install/scripts/officexp.bat
+++ unattended/install/scripts/officexp.bat
@@ -7,6 +7,7 @@ todo.pl .reboot
 
 :: "Office XP Update: March 23, 2004"
 :: ("exploitable security issues in Microsoft Script Editor")
+:: URL|DEU|http://download.microsoft.com/download/5/4/2/542c935d-3fd5-40d4-b6e1-b430de25c907/officexp-kb833858-client-deu.exe|updates/officexp/officexp-kb833858-client-deu.exe
 :: URL|ENU|http://download.microsoft.com/download/2/5/7/25750dd8-e66e-4099-95b4-216c0542d2f1/officexp-kb833858-client-enu.exe|updates/officexp/officexp-kb833858-client-enu.exe
 :: URL|NLD|http://download.microsoft.com/download/2/3/e/23eccd28-e7f8-4a18-8446-2507fb075f2a/officexp-kb833858-client-nld.exe|updates/officexp/officexp-kb833858-client-nld.exe
 :: <http://support.microsoft.com/?id=833858>
