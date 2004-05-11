9fa3021fa8c3b80442e6732a16e2cea2353bd267
jbus <jbus@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-05-11 14:23:06 +0000

    Added winamp.bat

---
+++ unattended/install/scripts/winamp.bat
@@ -0,0 +1,4 @@
+:: OPTIONAL: Install WinAmp 5.03
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp503_full.exe|packages/winamp/winamp503-full.exe
+
+todo.pl "%Z%\packages\winamp\winamp503-full.exe /S /install=SFQR" 
