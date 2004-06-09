e404c1e711fc881c0ac1b19f16596ec1cefdb96e
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-06-09 18:27:05 +0000

    added url's for russian files

--- trunk/unattended/install/scripts/ws2k3-updates.bat
+++ trunk/unattended/install/scripts/ws2k3-updates.bat
@@ -10,7 +10,7 @@
 
 :: "Update for Windows Media Player 9 Series"
 :: URL|ENU|http://download.microsoft.com/download/7/0/D/70D0BAC6-A68B-4CB6-94AB-E0DF8C0EFCAF/WindowsMedia9-KB837272-ENU.exe|updates/mediaplayer9/windowsmedia9-kb837272-enu.exe
-:: URL|RUS|http://download.windowsupdate.com/msdownload/update/v3-19990518/cabpool/WindowsMedia9-KB837272-RUS_b6fe29ce412032622728fa1eb4cc7a0.exe|updates/mediaplayer9/windowsmedia9-kb837272-rus.exe
+:: URL|RUS|http://download.microsoft.com/download/3/c/6/3c6b5375-05af-4fef-8dd0-595506d9e59b/WindowsMedia9-KB837272-RUS.exe|updates/mediaplayer9/windowsmedia9-kb837272-rus.exe
 :: <http://support.microsoft.com/?id=837272>
 todo.pl ".reboot-on 194 %Z%\updates\mediaplayer9\windowsmedia9-kb837272-%WINLANG%.exe /passive /n /norestart"
 
