38e044b088a58f3a5c87b1ade2a3d9346ee054ed
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-07-02 18:04:20 +0000

    Add 870669.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1588 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/ws2k3-updates.bat
+++ unattended/install/scripts/ws2k3-updates.bat
@@ -64,6 +64,13 @@ todo.pl ".reboot-on 194 %Z%\updates\ws2k3\WindowsServer2003-KB832894-x86-%WINLAN
 
 :: Critical Updates
 
+:: Critical update 870669
+:: "How to disable the ADODB.Stream object from Internet Explorer"
+:: (aka. "the zero-day exploit")
+:: <http://support.microsoft.com/?kbid=818043>
+:: (download URLs in win2ksp4-updates.bat)
+todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB870669-x86-%WINLANG%.exe /q /r:n"
+
 :: MS04-015
 :: "Vulnerability in Help and Support Center Could Allow Remote Code Execution"
 :: <http://www.microsoft.com/technet/security/bulletin/ms04-015.mspx>
