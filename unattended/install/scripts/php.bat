ab84c60653324972fcfbb98d1f92cb14446d0275
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-07-14 18:19:28 +0000

    Update to 4.3.8.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1608 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/php.bat
+++ unattended/install/scripts/php.bat
@@ -1,4 +1,4 @@
 :: Install PHP
-:: URL|ALL|http://www.php.net/get/php-4.3.6-installer.exe/from/www.php.net/mirror|packages/php-4.3.6-installer.exe
+:: URL|ALL|http://www.php.net/get/php-4.3.8-installer.exe/from/www.php.net/mirror|packages/php-4.3.8-installer.exe
 @Echo off
-todo.pl "%Z%\packages\php-4.3.6-installer.exe /s"
+todo.pl "%Z%\packages\php-4.3.8-installer.exe /s"
