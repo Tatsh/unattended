ec14b6c8bc423390fe8ed97561b05e129bfd7c38
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-07-25 21:16:26 +0000

    added: language packs for (deu, esn, fra,) ita, nld, rus
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1643 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,4 +1,19 @@
 :: OPTIONAL: Install WinAmp 5.03
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp503_full.exe|packages/winamp/winamp503-full.exe
 
-todo.pl "%Z%\packages\winamp\winamp503-full.exe /S /install=SFQR" 
+:: user contributed translation packages
+:: URL|DEU|http://download.nullsoft.com/customize/component/2004/3/18/P/Deutsche_Sprachdatei_v14_Winamp502.exe|packages/winamp/langpack-deu_.exe
+:: URL|ESN|http://download.nullsoft.com/customize/component/2004/2/15/P/Espannol.exe|packages/winamp/langpack-esn_.exe
+:: URL|FRA|http://download.nullsoft.com/customize/component/2003/12/18/P/translation_in_french.exe|packages/winamp/langpack-fra.exe
+:: URL|ITA|http://download.nullsoft.com/customize/component/2004/2/27/P/Traduzione_Italiana_per_Winamp_5.exe|packages/winamp/langpack-ita.exe
+:: URL|NLD|http://download.nullsoft.com/customize/component/2004/4/14/P/Dutch_languagepack_5-03.exe|packages/winamp/langpack-nld.exe
+:: URL|RUS|http://download.nullsoft.com/customize/component/2004/4/26/P/Winamp_5v03_RUS.exe|packages/winamp/langpack-rus.exe
+::
+:: warning: DEU, ESN and FRA do not (completely) install silently. Please  the underscore "_"
+:: from the filename, if you want to the package to be called anyway.
+::
+if not exist %Z%\packages\winamp\langpack-%WINLANG%.exe goto nolangpack
+todo.pl "%Z%\packages\winamp\langpack-%WINLANG%.exe /S"
+:nolangpack
+
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp503_full.exe|packages/winamp/winamp503-full.exe
+todo.pl "%Z%\packages\winamp\winamp503-full.exe /S /install=SFQR"
