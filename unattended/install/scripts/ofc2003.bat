77309c023adfbe43719624ea889dd926ad3cc094
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-07-27 14:55:28 +0000

    Add Service Pack 1 (deu, eni, esn, fra, nld), remove included updates

--- unattended/install/scripts/ofc2003.bat
+++ unattended/install/scripts/ofc2003.bat
@@ -11,50 +11,13 @@
 :: Reboot after installing everything (superstition).
 todo.pl .reboot
 
-:: "Excel 2003 Update: March 23, 2004"
-:: URL|ENU|http://download.microsoft.com/download/1/2/0/12029EF7-BFD5-433F-B9CD-058E7388A1E2/office2003-kb834691-client-enu.exe|updates/office2003/office2003-kb834691-client-enu.exe
-:: URL|NLD|http://download.microsoft.com/download/7/a/d/7ad3b80c-24a4-4e42-a6b7-5cff9ead225d/office2003-kb834691-client-nld.exe|updates/office2003/office2003-kb834691-client-nld.exe
-:: <http://support.microsoft.com/?id=834691>
-todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb834691-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\o2003-kb834691.txt /p excel-Binary-GLB.msp REBOOT=ReallySuppress\""
-
-
-:: "Outloook 2003 Junk E-mail filter Update: March 23, 2004"
-:: URL|ENU|http://download.microsoft.com/download/8/7/a/87ab64c5-33fe-470b-a5a6-8ae046b3adda/office2003-kb835235-client-enu.exe|updates/office2003/office2003-kb835235-client-enu.exe
-:: URL|NLD|http://download.microsoft.com/download/7/2/6/72699c7d-cc56-4827-8e53-62977456c845/office2003-kb835235-client-nld.exe|updates/office2003/office2003-kb835235-client-nld.exe
-:: <http://support.microsoft.com/?id=835235>
-todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb835235-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\o2003-kb835235.txt /p outlfltr-Binary-GLB.msp REBOOT=ReallySuppress\""
-
-:: "Unacceptable symbols in Bookshelf Symbol 7 font" (?)
-:: URL|ENU|http://download.microsoft.com/download/1/3/c/13c13f98-0d59-4722-b804-5b29e0320b23/Windows-KB833407-x86-enu.exe|updates/office2003/windows-kb833407-x86-enu.exe
-:: URL|NLD|http://download.microsoft.com/download/b/4/f/b4f32f95-1d60-4cde-97df-c0f0d41066a6/Windows-KB833407-x86-NLD.exe|updates/office2003/windows-kb833407-x86-nld.exe
-:: <http://support.microsoft.com/?id=833407>
-todo.pl ".reboot-on 194 %Z%\updates\office2003\windows-kb833407-x86-enu.exe /passive /n /norestart"
-
-
-:: Word 2003 Update: KB830000
-:: URL|ENU|http://download.microsoft.com/download/3/3/0/33067852-1400-4BEB-ADFE-8ABA8661F16A/Office2003-kb830000-client-enu.exe|updates/office2003/office2003-kb830000-client-enu.exe
-:: URL|NLD|http://download.microsoft.com/download/e/2/c/e2cdc6fa-3fed-4694-981b-cdebc435aa68/office2003-kb830000-client-nld.exe|updates/office2003/office2003-kb830000-client-nld.exe
-:: URL|ITA|http://download.microsoft.com/download/5/b/4/5b49b939-695b-458e-af46-bd1245752200/office2003-kb830000-client-ita.exe|updates/office2003/office2003-kb830000-client-ita.exe
-:: <http://support.microsoft.com/?id=830000>
-::todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb830000-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\o2003-kb830000.txt /p winword-Binary-GLB.msp REBOOT=ReallySuppress\""
-::not yet working, returns error 1328!
-
-:: Outlook 2003 Junk E-mail Filter Update: KB832333
-:: URL|ENU|http://download.microsoft.com/download/A/D/D/ADDC1A61-68BE-45A1-A780-8B4D668BDC09/Office2003-kb832333-client-enu.exe|updates/office2003/office2003-kb832333-client-enu.exe
-:: URL|NLD|http://download.microsoft.com/download/1/d/c/1dc8edef-9d22-4341-ab10-cade0e594390/office2003-kb832333-client-nld.exe|updates/office2003/office2003-kb832333-client-nld.exe
-:: URL|ITA|http://download.microsoft.com/download/2/3/b/23b6baf3-db9f-466b-b0f3-2dbe0df95e60/office2003-kb832333-client-ita.exe|updates/office2003/office2003-kb832333-client-ita.exe
-:: <http://support.microsoft.com/?id=832333>
-:: See <http://www.microsoft.com/office/ork/2003/tools/locddl/>
-:: for localized version
-todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb832333-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\o2003-kb832333.txt /p outlfltr-Binary-GLB.msp REBOOT=ReallySuppress\""
-
-:: "Office 2003 Critical Update: November 4, 2003"
-:: URL|ENU|http://download.microsoft.com/download/e/1/9/e19746da-c20c-43ff-b824-90a55b5386ad/Office2003-kb828041-client-enu.exe|updates/office2003/office2003-kb828041-client-enu.exe
-:: URL|NLD|http://download.microsoft.com/download/b/4/e/b4e204d5-70fe-4125-a264-ac768406e493/Office2003-kb828041-client-NLD.exe|updates/office2003/office2003-kb828041-client-nld.exe
-:: URL|ITA|http://download.microsoft.com/download/c/f/5/cf5c26fd-5278-4811-9fef-a3be9652dbaa/Office2003-kb828041-client-ITA.exe|updates/office2003/office2003-kb828041-client-ita.exe
-:: See <http://www.microsoft.com/downloads/details.aspx?FamilyId=722C7A55-E541-44CC-97CB-572859346DEE>
-:: <http://support.microsoft.com/?id=828041>
-todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb828041-client-%WINLANG%.exe /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\o2003-kb828041.txt /p mso-Binary-GLB.msp REBOOT=ReallySuppress\""
+:: Office 2003 Service Pack 1 (842532)
+:: <http://support.microsoft.com/?kbid=842532>
+:: URL|DEU|http://download.microsoft.com/download/c/8/0/c809c51b-490b-4738-b4da-6e3aee891c47/Office2003SP1-kb842532-fullfile-deu.exe|updates/office2003/Office2003SP1-kb842532-fullfile-deu.exe
+:: URL|ENU|http://download.microsoft.com/download/1/a/9/1a9a2039-70e3-4e92-b977-756fe884f731/Office2003SP1-kb842532-fullfile-enu.exe|updates/office2003/Office2003SP1-kb842532-fullfile-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/6/1/f/61f62739-74fc-480e-9c30-99d001561f52/Office2003SP1-kb842532-fullfile-esn.exe|updates/office2003/Office2003SP1-kb842532-fullfile-esn.exe
+:: URL|FRA|http://download.microsoft.com/download/d/d/2/dd218791-3d92-4eab-a27a-afce9b06f105/Office2003SP1-kb842532-fullfile-fra.exe|updates/office2003/Office2003SP1-kb842532-fullfile-fra.exe
+:: URL|NLD|http://download.microsoft.com/download/b/f/b/bfb399e8-9954-4b39-854a-98b0cde40ee6/Office2003SP1-kb842532-fullfile-nld.exe|updates/office2003/Office2003SP1-kb842532-fullfile-nld.exe
+todo.pl ".reboot-on 194 %Z%\updates\office2003\Office2003SP1-kb842532-fullfile-%WINLANG%.exe /r:n /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\ofc2003sp1.txt /p MAINSP1ff.msp REBOOT=ReallySuppress\""
 
 todo.pl ".reboot-on 194 %Z%\packages\office2003\setup.exe /qb /l* %SystemDrive%\netinst\logs\office2003.txt ADDLOCAL=ALL NOUSERNAME=1 PIDKEY=%PIDKEY%"
-
