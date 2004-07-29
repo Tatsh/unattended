8d433dc5ff155bcdb3a60b7a5ef33b64127756b9
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-07-29 18:07:19 +0000

    Produce better diagnostic if Perl MSI is not present.

--- unattended/install/scripts/perl.bat
+++ unattended/install/scripts/perl.bat
@@ -12,11 +12,20 @@ if not exist %Z%\packages\windowsinstaller\instmsiw.exe goto nomsi
 %Z%\packages\windowsinstaller\InstMsiW.exe /q /c:"msiinst.exe /i instmsi.msi /q"
 :nomsi
 
+set perl_msi=%Z%\packages\ActivePerl-5.8.4.810-MSWin32-x86.msi
+
+if exist %perl_msi% goto have_perl
+echo %perl_msi% not found; halting.
+halt:
+goto halt
+
+:have_perl
+
 :: "PERL_PATH=Yes" instructs installer to add Perl to the PATH
 :: "PERL_EXT=Yes" instructs installer to associate .pl files with Perl
 :: (The latter is undocumented; see
 :: http://bugs.activestate.com/show_bug.cgi?id=20364)
 
 :: URL|ALL|http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.4.810-MSWin32-x86.msi|packages/activeperl-5.8.4.810-mswin32-x86.msi
-start /wait msiexec /qb /l* %SystemDrive%\netinst\logs\perl.txt /i %Z%\packages\ActivePerl-5.8.4.810-MSWin32-x86.msi PERL_PATH=Yes PERL_EXT=Yes
+start /wait msiexec /qb /l* %SystemDrive%\netinst\logs\perl.txt %perl_msi% /i  PERL_PATH=Yes PERL_EXT=Yes
 @if errorlevel 1 exit 1
