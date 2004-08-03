6b7f8c1b2eba4b41c5401e5a474caf6c94da7a98
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-03 14:38:14 +0000

    Use %ProgramFiles% instead of C:\Program Files.  Thanks to Steffen Kaiser and Jeff Black.

--- unattended/install/scripts/tightvnc.bat
+++ unattended/install/scripts/tightvnc.bat
@@ -1,4 +1,4 @@
 :: OPTIONAL: Install TightVNC
 :: URL|ALL|http://dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.2.9-setup.exe|packages/tightvnc-1.2.9-setup.exe
 @Echo off
-todo.pl "%Z%\packages\tightvnc-1.2.9-setup.exe /silent" "\"C:\Program Files\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
+todo.pl "%Z%\packages\tightvnc-1.2.9-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
