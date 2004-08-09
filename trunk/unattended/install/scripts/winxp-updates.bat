10fa72c4f8349fec263771f032b41f626bc1e6f4
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-09 20:03:40 +0000

    Substitute SP1 with SP2, add urls for deu, enu

--- trunk/unattended/install/scripts/winxp-updates.bat
+++ trunk/unattended/install/scripts/winxp-updates.bat
@@ -1,15 +1,12 @@
 :: Install all updates for Windows XP
 @Echo off
-:: After installing SP1, install updates for winxpsp1.
-todo.pl winxpsp1-updates.bat
+:: After installing SP2, install updates for winxpsp2.
+todo.pl winxpsp2-updates.bat
 
-:: Install Service Pack 1.
-:: URL|DEU|http://download.microsoft.com/download/a/4/c/a4ce1a3b-fac8-4597-be33-c10ced905c3b/xpsp1a_de_x86.exe|updates/winxp/xpsp1a_deu_x86.exe
-:: URL|ENU|http://download.microsoft.com/download/5/4/f/54f8bcf8-bb4d-4613-8ee7-db69d01735ed/xpsp1a_en_x86.exe|updates/winxp/xpsp1a_enu_x86.exe
-:: URL|ITA|http://download.microsoft.com/download/6/a/7/6a727e5c-c84b-45ef-b943-b0080acfe352/xpsp1a_it_x86.exe|updates/winxp/xpsp1a_ita_x86.exe
-:: URL|NLD|http://download.microsoft.com/download/8/2/8/828e2216-e87c-4a0e-a056-c83da3e781f2/xpsp1a_nl_x86.exe|updates/winxp/xpsp1a_nld_x86.exe
-:: URL|RUS|http://download.microsoft.com/download/f/d/7/fd7a4d93-1cf6-40ac-93c9-c99338aa95ec/xpsp1a_ru_x86.exe|updates/winxp/xpsp1a_rus_x86.exe
+:: Install Service Pack 2.
+:: URL|DEU|http://download.microsoft.com/download/9/6/4/96442257-721a-4cd5-9006-10a40cbb45cb/WindowsXP-KB835935-SP2-DEU.exe|updates/winxp/xpsp2_deu_x86.exe
+:: URL|ENU|http://download.microsoft.com/download/1/6/5/165b076b-aaa9-443d-84f0-73cf11fdcdf8/WindowsXP-KB835935-SP2-ENU.exe|updates/winxp/xpsp2_enu_x86.exe
 
-:: See <http://support.microsoft.com/?id=262839> for a description of
+:: See <http://support.microsoft.com/?kbid=262841> for a description of
 :: the switches.
-todo.pl ".ignore-err 194 %Z%\updates\winxp\xpsp1a_%WINLANG%_x86.exe /n /q /u /z" .reboot
+todo.pl ".ignore-err 194 %Z%\updates\winxp\xpsp2_%WINLANG%_x86.exe /n /q /u /z" .reboot
