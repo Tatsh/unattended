b3b8d2f675d8ea3c78acea98bcc4d11804745813
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-10 22:10:53 +0000

    languages updated: deu, esn, kor, nor, plk, sve
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1705 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/opera.bat
+++ unattended/install/scripts/opera.bat
@@ -8,22 +8,22 @@
 :: URL|CAT|ftp://ftp.opera.com/pub/opera/win/lng/723/ca/ow723_3227ca.lng|packages/opera/languages/opera-cat.lng
 :: URL|CHS|ftp://ftp.opera.com/pub/opera/win/lng/723/zh-cn/ow723_3227zh-cn.lng|packages/opera/languages/opera-chs.lng
 :: URL|DAN|ftp://ftp.opera.com/pub/opera/win/lng/723/da/ow723_3227da.lng|packages/opera/languages/opera-dan.lng
-:: URL|DEU|ftp://ftp.opera.com/pub/opera/win/lng/753/de/ow753_3850de.lng|packages/opera/languages/opera-deu.lng
+:: URL|DEU|ftp://ftp.opera.com/pub/opera/win/lng/754/de/ow754_3865de.lng|packages/opera/languages/opera-deu.lng
 :: URL|ELL|ftp://ftp.opera.com/pub/opera/win/lng/711/el/ow711_2887el.lng|packages/opera/languages/opera-ell.lng
 :: URL|ENG|ftp://ftp.opera.com/pub/opera/win/lng/723/en-GB/ow723_3227en-GB.lng|packages/opera/languages/opera-eng.lng
-:: URL|ESN|ftp://ftp.opera.com/pub/opera/win/lng/752/es-ES/ow752_3834es-ES.lng|packages/opera/languages/opera-esn.lng
+:: URL|ESN|ftp://ftp.opera.com/pub/opera/win/lng/754/es-ES/ow754_3865es-ES.lng|packages/opera/languages/opera-esn.lng
 :: URL|FIN|ftp://ftp.opera.com/pub/opera/win/lng/752/fi/ow752_3834fi.lng|packages/opera/languages/opera-fin.lng
 :: URL|FRA|ftp://ftp.opera.com/pub/opera/win/lng/752/fr/ow752_3834fr.lng|packages/opera/languages/opera-fra.lng
 :: URL|ITA|ftp://ftp.opera.com/pub/opera/win/lng/752/it/ow752_3834it.lng|packages/opera/languages/opera-ita.lng
 :: URL|JPN|ftp://ftp.opera.com/pub/opera/win/lng/723/ja/ow723_3227ja.lng|packages/opera/languages/opera-jpn.lng
-:: URL|KOR|ftp://ftp.opera.com/pub/opera/win/lng/752/ko/ow752_3834ko.lng|packages/opera/languages/opera-kor.lng
+:: URL|KOR|ftp://ftp.opera.com/pub/opera/win/lng/754/ko/ow754_3865ko.lng|packages/opera/languages/opera-kor.lng
 :: URL|NLD|ftp://ftp.opera.com/pub/opera/win/lng/722/nl/ow722_3221nl.lng|packages/opera/languages/opera-nld.lng
 :: URL|NON|ftp://ftp.opera.com/pub/opera/win/lng/723/nn/ow723_3227nn.lng|packages/opera/languages/opera-non.lng
-:: URL|NOR|ftp://ftp.opera.com/pub/opera/win/lng/752/nb/ow752_3834nb.lng|packages/opera/languages/opera-nor.lng
-:: URL|PLK|ftp://ftp.opera.com/pub/opera/win/lng/723/pl/ow723_3227pl.lng|packages/opera/languages/opera-plk.lng
+:: URL|NOR|ftp://ftp.opera.com/pub/opera/win/lng/754/nb/ow754_3865nb.lng|packages/opera/languages/opera-nor.lng
+:: URL|PLK|ftp://ftp.opera.com/pub/opera/win/lng/754/pl/ow754_3865pl.lng|packages/opera/languages/opera-plk.lng
 :: URL|PTB|ftp://ftp.opera.com/pub/opera/win/lng/722/pt-BR/ow722_3221pt-BR.lng|packages/opera/languages/opera-ptb.lng
 :: URL|RUS|ftp://ftp.opera.com/pub/opera/win/lng/723/ru/ow723_3227ru.lng|packages/opera/languages/opera-rus.lng
-:: URL|SVE|ftp://ftp.opera.com/pub/opera/win/lng/723/sv/ow723_3227sv.lng|packages/opera/languages/opera-sve.lng
+:: URL|SVE|ftp://ftp.opera.com/pub/opera/win/lng/754/sv/ow754_3865sv.lng|packages/opera/languages/opera-sve.lng
 :: URL|TRK|ftp://ftp.opera.com/pub/opera/win/lng/723/tr/ow723_3227tr.lng|packages/opera/languages/opera-trk.lng
 
 :: if you like to do special things (e.g. define custom searches), save your customized ini's in packages/opera/special
