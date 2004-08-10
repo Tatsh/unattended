21a3c781dc2aa66b8afdf8bb1c6d558a18aaba24
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-10 21:11:54 +0000

    Install Windows XP SP2 debugging symbols.

---
+++ unattended/install/scripts/winxpsp2-symbols.bat
@@ -0,0 +1,4 @@
+:: Install Windows XP SP2 debugging symbols
+:: URL|ALL|http://msdl.microsoft.com/download/symbols/packages/windowsxp/WindowsXP-KB835935-SP2-slp-Symbols.exe|updates/winxpsp2/windowsxp-kb835935-sp2-slp-symbols.exe
+
+todo.pl "%Z%\updates\winxpsp2\windowsxp-kb835935-sp2-slp-symbols.exe /q /c:\"symbols.exe /u\""
