660d2a28dcdbc18d5537bd6f1b5ae9f3509f16be
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-11 17:48:25 +0000

    fix paths

--- trunk/unattended/install/scripts/sshclient.bat
+++ trunk/unattended/install/scripts/sshclient.bat
@@ -1,6 +1,6 @@
 :: Install SSH Client (http://www.ssh.com)
 :: URL|ALL|http://www.icrew.org/mirror/ssh/SSHSecureShellClient-3.2.9.exe|packages/sshclient/sshsecureshellclient-3.2.9.exe
 @Echo off
-start /wait %Z%\packages\sshclient\SSHSecureShellClient-3.2.9.exe /s /a /s /sms /f1%Z%\packages\SSHSecureShellClient\SSHSecureShellClient.iss /f2c:\netinst\logs\SSHSecureShellClient.txt
+start /wait %Z%\packages\sshclient\sshsecureshellclient-3.2.9.exe /s /a /s /sms /f1%Z%\packages\sshclient\sshclient.iss /f2c:\netinst\logs\sshclient.txt
 
 if errorlevel 1 exit 1
