bacce3b59ade3823d79fd5414c95aa202dfd9925
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-11 08:40:30 +0000

    Install Windows 2003 Server debugging symbols.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1706 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/ws2k3-symbols.bat
@@ -0,0 +1,4 @@
+:: Install Windows 2003 Server debugging symbols
+:: URL|ALL|http://msdl.microsoft.com/download/symbols/packages/windows2003/Windows2003.x86.fre.rtm.symbols.exe|updates/ws2k3/Windows2003.x86.fre.rtm.symbols.exe
+
+todo.pl "%Z%\updates\ws2k3\Windows2003.x86.fre.rtm.symbols.exe /q /c:\"symbols.exe /u\""
