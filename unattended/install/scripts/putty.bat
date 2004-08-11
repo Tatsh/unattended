f8bd1430dc7f72e85159656e771c80bfaf1b55d4
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-11 18:27:26 +0000

    initial version

---
+++ unattended/install/scripts/putty.bat
@@ -0,0 +1,4 @@
+:: Optional: Install PuTTY
+:: URL|ALL|http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.55-installer.exe|packages/putty/putty-0.55-installer.exe
+@echo off
+%Z%\packages\putty\putty-0.55-installer.exe /sp- /silent /norestart
