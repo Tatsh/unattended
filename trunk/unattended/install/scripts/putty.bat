af68e0d2ce44dee03f60f6fa6a1c3b0eb25ad94c
jbus <jbus@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-12 11:53:51 +0000

    Added Putty

--- trunk/unattended/install/scripts/putty.bat
+++ trunk/unattended/install/scripts/putty.bat
@@ -1,4 +1,6 @@
-:: Optional: Install PuTTY
-:: URL|ALL|http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.55-installer.exe|packages/putty/putty-0.55-installer.exe
-@echo off
-%Z%\packages\putty\putty-0.55-installer.exe /sp- /silent /norestart
+:: OPTIONAL: Install Putty 0.55 
+:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.55-installer.exe|packages/putty/putty-0.55-installer.exe
+@Echo off
+start /wait %Z%\packages\putty\putty-0.55-installer.exe /sp- /silent  
+
+if errorlevel 1 exit 1
