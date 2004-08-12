ac44c37d866ec41802ced53c572cbbbca33fe9bc
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-12 20:04:06 +0000

    Use todo.pl.

--- unattended/install/scripts/reskit.bat
+++ unattended/install/scripts/reskit.bat
@@ -2,4 +2,4 @@
 @Echo off
 :: Force the installation directory to be C:\NTRESKIT for historical
 :: reasons
-start /wait msiexec /qb /l* %SystemDrive%\netinst\logs\reskit.txt /i %Z%\packages\reskit\W2000RK.MSI NTRK50DIR=%SystemDrive%\NTRESKIT
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\reskit.txt /i %Z%\packages\reskit\W2000RK.MSI NTRK50DIR=%SystemDrive%\NTRESKIT"
