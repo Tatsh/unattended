bc4db69fcfdf3d8a3073b9ef44375527ca00a427
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-12 19:58:35 +0000

    Get rid of silly %PIDKEY% reference.

--- unattended/install/scripts/officexp.bat
+++ unattended/install/scripts/officexp.bat
@@ -37,4 +37,6 @@ todo.pl ".reboot-on 194 %Z%\updates\officexp\officexp-kb832668-client-%WINLANG%.
 :: <http://support.microsoft.com/?id=832671>
 todo.pl ".reboot-on 194 %Z%\updates\officexp\officexpsp3-kb832671-fullfile-%WINLANG%.exe /r:n /q /c:\"msiexec /qb /l* %SystemDrive%\netinst\logs\oxpsp3.txt /p MAINSP3ff.msp REBOOT=ReallySuppress\""
 
-todo.pl ".reboot-on 194 %Z%\packages\officexp\setuppls.exe /qb /l* %SystemDrive%\netinst\logs\officexp.txt ADDLOCAL=ALL NOUSERNAME=1 PIDKEY=%PIDKEY%"
+:: Add "PIDKEY=<key>" to this command line, where <key> is your
+:: product key without hyphens.
+todo.pl ".reboot-on 194 %Z%\packages\officexp\setuppls.exe /qb /l* %SystemDrive%\netinst\logs\officexp.txt ADDLOCAL=ALL NOUSERNAME=1"
