150961140c59de8c1e02c5e7d168b5625cd6d11a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-14 18:32:33 +0000

    Install 7-zip because emacs.bat depends on it.

--- unattended/install/scripts/develop.bat
+++ unattended/install/scripts/develop.bat
@@ -4,6 +4,6 @@
 :: case-insensitive), so delete it.
 del C:\Perl\bin\HEAD
 
-todo.pl php.bat emacs.bat cygwin.bat vs6.bat vsnet.bat msdn.bat %%WINVER%%-symbols.bat
+todo.pl php.bat 7-zip.bat emacs.bat cygwin.bat vs6.bat vsnet.bat msdn.bat %%WINVER%%-symbols.bat
 
 todo.pl base.bat 
