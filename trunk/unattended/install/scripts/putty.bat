b904c22979ee55c28c7250d08f28cdeec93ae365
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-14 19:14:52 +0000

    Remove trailing spaces.
    Use todo.pl to watch installer's exit status.

--- trunk/unattended/install/scripts/putty.bat
+++ trunk/unattended/install/scripts/putty.bat
@@ -1,6 +1,5 @@
-:: OPTIONAL: Install Putty 0.55 
+:: OPTIONAL: Install Putty 0.55
 :: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.55-installer.exe|packages/putty/putty-0.55-installer.exe
 @Echo off
-start /wait %Z%\packages\putty\putty-0.55-installer.exe /sp- /silent  
 
-if errorlevel 1 exit 1
+todo.pl "%Z%\packages\putty\putty-0.55-installer.exe /sp- /silent"
