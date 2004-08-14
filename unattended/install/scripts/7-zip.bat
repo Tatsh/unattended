7898f20b3a28b0d7a49a552128ea5b740055bfd0
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-14 18:31:57 +0000

    Do not install 7-zip if it is already installed.  Needed because develop.bat runs this as a required script, so this will run again if selected as an optional script.

--- unattended/install/scripts/7-zip.bat
+++ unattended/install/scripts/7-zip.bat
@@ -1,4 +1,7 @@
 :: OPTIONAL: Install 7-Zip
 :: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z313.exe|packages/7z313.exe
 @Echo off
+
+if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
 todo.pl "%Z%\packages\7z313.exe -y /q /r:n"
+:done
