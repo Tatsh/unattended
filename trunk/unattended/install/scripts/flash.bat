dc7cdb670fc6b9731190ff1e57c489643f3c4768
dolphinbofh <dolphinbofh@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-24 20:46:58 +0000

    Add flash/shockwave (thanks Paul Griffith)

---
+++ trunk/unattended/install/scripts/flash.bat
@@ -0,0 +1,8 @@
+:: OPTIONAL: Install Flash 7.0.19.0 
+::
+::URL|ALL|http://fpdownload.macromedia.com/pub/shockwave/flash/english/win95nt/7.0.19.0/flashplayer7installer.exe|packages/macromedia/flashplayer7installer.exe
+::
+:: Download from <http://www.macromedia.com/downloads/>
+@Echo off
+
+todo.pl "%Z%\packages\macromedia\flashplayer7installer.exe /s"
