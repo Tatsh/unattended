d94367d2958aea0f1aef5f958327505b257eebb0
dolphinbofh <dolphinbofh@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-24 20:46:58 +0000

    Add flash/shockwave (thanks Paul Griffith)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1766 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/shockwave.bat
@@ -0,0 +1,8 @@
+:: Install Shockwave10 player
+::URL|ALL|http://fpdownload.macromedia.com/pub/shockwave/default/english/win95nt/10.0.0.210/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full.exe
+::
+:: Download from <http://www.macromedia.com/downloads/>
+
+@Echo off
+
+todo.pl "%Z%\packages\macromedia\shockwave_installer_full.exe /s"
