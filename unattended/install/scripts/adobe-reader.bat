d040c7f4a041d18844e729a175f9b35f14373538
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-08-28 19:39:02 +0000

    added url's for russian files
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1777 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/adobe-reader.bat
+++ unattended/install/scripts/adobe-reader.bat
@@ -11,4 +11,5 @@ todo.pl "%Z%\packages\AdobeReader\update_602.exe /S /v/qn"
 :: URL|NLD|http://ardownload.adobe.com/pub/adobe/reader/win/6.x/6.0/nld/AdbeRdr60_nld_full.exe|packages/adobereader/adberdr60_nld_full.exe
 :: URL|FRA|http://ardownload.adobe.com/pub/adobe/reader/win/6.x/6.0/fra/AdbeRdr60_fra_full.exe|packages/adobereader/adberdr60_fra_full.exe
 :: URL|ITA|http://ardownload.adobe.com/pub/adobe/reader/win/6.x/6.0/ita/AdbeRdr60_ita_full.exe|packages/adobereader/adberdr60_ita_full.exe
+:: URL|RUS|http://ardownload.adobe.com/pub/adobe/reader/win/6.x/6.0/enu/AdbeRdr60_enu_full.exe|packages/adobereader/adberdr60_rus_full.exe
 todo.pl "%Z%\packages\AdobeReader\AdbeRdr60_%WINLANG%_full.exe -p\"-s /v\\\"/qb /l* %SystemDrive%\netinst\logs\adobe-reader.txt\\\"\""
