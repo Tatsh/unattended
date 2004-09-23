d2667385836a67f3a38f44c450d1dfebb47d5d52
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-09-23 14:20:31 +0000

    Update to Mozilla 1.7.3. Thanks to Peter Baumann

--- unattended/install/scripts/mozilla.bat
+++ unattended/install/scripts/mozilla.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Mozilla
-:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.2/mozilla-win32-1.7.2-installer.exe|packages/mozilla-win32-1.7.2-installer.exe
+:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.3/mozilla-win32-1.7.3-installer.exe|packages/mozilla-win32-1.7.3-installer.exe
 
 @Echo off
-todo.pl "%Z%\packages\mozilla-win32-1.7.2-installer.exe -ma -ira"
+todo.pl "%Z%\packages\mozilla-win32-1.7.3-installer.exe -ma -ira"
