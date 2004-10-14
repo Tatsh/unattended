6177e62053865697c19364439f0be349ca8fc714
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-10-14 19:10:50 +0000

    update to jre 1.5.0

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -1,12 +1,11 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 
-:: URL|ALL|http://java.sun.com/webapps/download/AutoDL?BundleId=9723|packages/j2re-1_4_2_05-windows-i586-p.exe
+:: URL|ALL|http://192.18.97.251/ECom/EComTicketServlet/BEGINjsecom15c.sun.com-1be8%3A416eb294%3A155baba62d444d22/-2147483648/561906963/1/531950/531830/561906963/2ts+/westCoastFSEND/jre-1.5.0-oth-JPR/jre-1.5.0-oth-JPR:1/jre-1_5_0-windows-i586.exe|packages/jre-1_5_0-windows-i586.exe
 
-:: Download from <http://java.sun.com/j2se/downloads/>.  Direct link
-:: at <http://www.java.com/en/download/windows_manual.jsp>.
+:: Download from <http://java.sun.com/j2se/1.5.0/download.jsp>.
 
 :: See
-:: <http://java.sun.com/j2se/1.4.2/docs/guide/plugin/developer_guide/silent.html>
+:: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/silent.html>
 :: for installation switches.
 @Echo off
-todo.pl "%Z%\packages\j2re-1_4_2_05-windows-i586-p.exe /s /v\"/qb IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0\""
+todo.pl "%Z%\packages\jre-1_5_0-windows-i586.exe /s /v\"/qb IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0\""
