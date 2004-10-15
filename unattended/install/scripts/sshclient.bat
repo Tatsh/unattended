3b4c0ff917d526bf02d98622fb880f9351e9fde2
Shad L. Lords <slords@mail.com>
2004-10-15 16:46:20 +0000

    Let todo handle the errorcode on exit

--- unattended/install/scripts/sshclient.bat
+++ unattended/install/scripts/sshclient.bat
@@ -1,6 +1,4 @@
 :: Install SSH Client (http://www.ssh.com)
 :: URL|ALL|http://www.icrew.org/mirror/ssh/SSHSecureShellClient-3.2.9.exe|packages/sshclient/sshsecureshellclient-3.2.9.exe
 @Echo off
-start /wait %Z%\packages\sshclient\sshsecureshellclient-3.2.9.exe /s /a /s /sms /f1%Z%\packages\sshclient\sshclient.iss /f2c:\netinst\logs\sshclient.txt
-
-if errorlevel 1 exit 1
+todo.pl "start /wait %Z%\packages\sshclient\sshsecureshellclient-3.2.9.exe /s /a /s /sms /f1%Z%\packages\sshclient\sshclient.iss /f2c:\netinst\logs\sshclient.txt"
