124e0c4d06d6c7d0a44a51ed4a5190a080014349
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-10-17 19:24:55 +0000

    added french url

--- unattended/install/scripts/officeop.bat
+++ unattended/install/scripts/officeop.bat
@@ -1,7 +1,8 @@
 :: OPTIONAL: Install OpenOffice.org
 
-:: URL|ENU|http://www.ibiblio.org/pub/mirrors/openoffice/stable/1.1.2/OOo_1.1.2_Win32Intel_install.zip|packages/openoffice/enu/ooo_1.1.2_win32intel_install_enu.zip
 :: URL|DEU|http://www.ibiblio.org/pub/mirrors/openoffice/localized/de/1.1.2/OOo_1.1.2_Win32Intel_install_de.zip|packages/openoffice/deu/ooo_1.1.2_win32intel_install_deu.zip
+:: URL|ENU|http://www.ibiblio.org/pub/mirrors/openoffice/stable/1.1.2/OOo_1.1.2_Win32Intel_install.zip|packages/openoffice/enu/ooo_1.1.2_win32intel_install_enu.zip
+:: URL|FRA|http://www.ibiblio.org/pub/mirrors/openoffice/localized/fr/1.1.2/OOo_1.1.2_Win32Intel_install_fr.zip|packages/openoffice/fra/ooo_1.1.2_win32intel_install_fra.zip
 :: URL|ITA|http://www.ibiblio.org/pub/mirrors/openoffice/localized/it/1.1.2/OOo_1.1.2_Win32Intel_install_it.zip|packages/openoffice/ita/ooo_1.1.2_win32intel_install_ita.zip
 
 @Echo off
