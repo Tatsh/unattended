523385aa457bb6e130529b1726f7fcdfefd3bac0
Shad L. Lords <slords@mail.com>
2004-10-26 01:28:51 +0000

    Add familyid links Add NOR links Fix Portuguese links

--- unattended/install/scripts/ofc2003.bat
+++ unattended/install/scripts/ofc2003.bat
@@ -7,6 +7,7 @@ todo.pl .reboot
 
 :: Update for Outlook 2003: Junk E-mail Filter (KB870765)
 :: <http://support.microsoft.com/?kbid=870765>
+:: <http://www.microsoft.com/downloads/details.aspx?familyid=d8ded71e-89ed-4473-9640-13a2b799564e&displaylang=en>
 :: URL|ARA|http://download.microsoft.com/download/b/e/7/be7e1918-4b3b-4954-80a8-677afc40a81e/office2003-kb870765-fullfile-ara.exe|updates/office2003/office2003-kb870765-fullfile-ara.exe
 :: URL|CSY|http://download.microsoft.com/download/0/6/0/06014eed-0960-4d07-b03d-a9cf1abf1653/office2003-kb870765-fullfile-csy.exe|updates/office2003/office2003-kb870765-fullfile-csy.exe
 :: URL|DAN|http://download.microsoft.com/download/d/3/e/d3e84b9b-71eb-4fbd-8fdd-e315e0db5df4/office2003-kb870765-fullfile-dan.exe|updates/office2003/office2003-kb870765-fullfile-dan.exe
@@ -22,9 +23,10 @@ todo.pl .reboot
 :: URL|JPN|http://download.microsoft.com/download/f/6/d/f6d6f42f-38a1-4d70-b8eb-74f65e9ef18e/office2003-kb870765-fullfile-jpn.exe|updates/office2003/office2003-kb870765-fullfile-jpn.exe
 :: URL|KOR|http://download.microsoft.com/download/c/3/b/c3b5f798-665c-44ff-8d7d-d8d8ba5f2a0a/office2003-kb870765-fullfile-kor.exe|updates/office2003/office2003-kb870765-fullfile-kor.exe
 :: URL|NLD|http://download.microsoft.com/download/b/b/6/bb647828-78c2-4fb3-8966-4f22c426dc3a/office2003-kb870765-fullfile-nld.exe|updates/office2003/office2003-kb870765-fullfile-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/f/5/e/f5e29eca-2cbb-492f-8b33-bdb35aa170be/office2003-kb870765-fullfile-nor.exe|updates/office2003/office2003-kb870765-fullfile-nor.exe
 :: URL|PLK|http://download.microsoft.com/download/4/c/a/4ca2a8a9-3d5f-45bf-be40-810c99b1d9ef/office2003-kb870765-fullfile-plk.exe|updates/office2003/office2003-kb870765-fullfile-plk.exe
-:: URL|PRG|http://download.microsoft.com/download/3/5/4/3542f9d3-6ce3-408a-8f66-8526aa421f72/office2003-kb870765-fullfile-ptg.exe|updates/office2003/office2003-kb870765-fullfile-ptg.exe
 :: URL|PTB|http://download.microsoft.com/download/6/f/7/6f70db5d-373b-4445-94ec-623e1f9aebce/office2003-kb870765-fullfile-ptb.exe|updates/office2003/office2003-kb870765-fullfile-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/3/5/4/3542f9d3-6ce3-408a-8f66-8526aa421f72/office2003-kb870765-fullfile-ptg.exe|updates/office2003/office2003-kb870765-fullfile-ptg.exe
 :: URL|ROM|http://download.microsoft.com/download/1/2/9/129ae0f2-0987-4080-acec-ce2748c59445/office2003-kb870765-fullfile-rom.exe|updates/office2003/office2003-kb870765-fullfile-rom.exe
 :: URL|RUS|http://download.microsoft.com/download/1/5/8/1581f369-f54c-4fed-9fce-79fd6b622115/office2003-kb870765-fullfile-rus.exe|updates/office2003/office2003-kb870765-fullfile-rus.exe
 :: URL|SVE|http://download.microsoft.com/download/2/4/8/2483f581-51e6-428f-8a56-d940359f966f/office2003-kb870765-fullfile-sve.exe|updates/office2003/office2003-kb870765-fullfile-sve.exe
@@ -33,6 +35,7 @@ todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb870765-fullfile-%WIN
 
 :: Office 2003 Service Pack 1
 :: <http://support.microsoft.com/?kbid=842532>
+:: <http://www.microsoft.com/downloads/details.aspx?familyid=9c51d3a6-7cb1-4f61-837e-5f938254fc47&displaylang=en>
 :: URL|ARA|http://download.microsoft.com/download/e/e/5/ee5c68b0-6783-41a1-a948-c2f192acc057/Office2003SP1-kb842532-client-ara.exe|updates/office2003/office2003sp1-kb842532-client-ara.exe
 :: URL|CSY|http://download.microsoft.com/download/b/d/1/bd14e601-3983-4c49-b16b-71a671a89a69/Office2003SP1-kb842532-client-csy.exe|updates/office2003/office2003sp1-kb842532-client-csy.exe
 :: URL|DAN|http://download.microsoft.com/download/a/2/9/a291ea50-a083-4b3a-884a-4de911733eea/Office2003SP1-kb842532-client-dan.exe|updates/office2003/office2003sp1-kb842532-client-dan.exe
@@ -48,9 +51,10 @@ todo.pl ".reboot-on 194 %Z%\updates\office2003\office2003-kb870765-fullfile-%WIN
 :: URL|JPN|http://download.microsoft.com/download/0/3/d/03d47c29-483e-4174-81fb-e34bdc843c81/Office2003SP1-kb842532-client-jpn.exe|updates/office2003/office2003sp1-kb842532-client-jpn.exe
 :: URL|KOR|http://download.microsoft.com/download/e/5/c/e5caa028-ed53-4687-9bce-6631569a027b/Office2003SP1-kb842532-client-kor.exe|updates/office2003/office2003sp1-kb842532-client-kor.exe
 :: URL|NLD|http://download.microsoft.com/download/b/f/b/bfb399e8-9954-4b39-854a-98b0cde40ee6/Office2003SP1-kb842532-client-nld.exe|updates/office2003/office2003sp1-kb842532-client-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/f/e/b/feb5a983-7548-4294-9d6e-c4d17c175abe/Office2003SP1-kb842532-client-nor.exe|updates/office2003/office2003sp1-kb842532-client-nor.exe
 :: URL|PLK|http://download.microsoft.com/download/7/4/6/7466c5d6-027f-495f-9f7c-ae607bf1d390/Office2003SP1-kb842532-client-plk.exe|updates/office2003/office2003sp1-kb842532-client-plk.exe
-:: URL|PRG|http://download.microsoft.com/download/8/0/1/8012d31d-db05-41cd-8397-38954028af13/Office2003SP1-kb842532-client-ptg.exe|updates/office2003/office2003sp1-kb842532-client-ptg.exe
 :: URL|PTB|http://download.microsoft.com/download/9/d/4/9d4b1e82-ccb7-45ab-a5e8-8834738c807d/Office2003SP1-kb842532-client-ptb.exe|updates/office2003/office2003sp1-kb842532-client-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/8/0/1/8012d31d-db05-41cd-8397-38954028af13/Office2003SP1-kb842532-client-ptg.exe|updates/office2003/office2003sp1-kb842532-client-ptg.exe
 :: URL|ROM|http://download.microsoft.com/download/c/e/7/ce7afce3-5189-4268-a86a-967efc9e8782/Office2003SP1-kb842532-client-rom.exe|updates/office2003/office2003sp1-kb842532-client-rom.exe
 :: URL|RUS|http://download.microsoft.com/download/9/b/1/9b1e99d3-c605-40ae-953d-95250265d75a/Office2003SP1-kb842532-client-rus.exe|updates/office2003/office2003sp1-kb842532-client-rus.exe
 :: URL|SVE|http://download.microsoft.com/download/5/f/9/5f97ecdc-fcec-41d3-9a85-37f37efc6ace/Office2003SP1-kb842532-client-sve.exe|updates/office2003/office2003sp1-kb842532-client-sve.exe
