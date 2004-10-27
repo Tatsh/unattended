ad59ce78147cb58f82f1509d9b877de51b1bf2f8
dolphinbofh <dolphinbofh@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-10-27 11:27:23 +0000

    Upgrade to 0.56

--- unattended/install/scripts/putty.bat
+++ unattended/install/scripts/putty.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install Putty 0.55
-:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.55-installer.exe|packages/putty/putty-0.55-installer.exe
+:: OPTIONAL: Install Putty 0.56
+:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.56-installer.exe|packages/putty/putty-0.56-installer.exe
 @Echo off
 
-todo.pl "%Z%\packages\putty\putty-0.55-installer.exe /sp- /silent"
+todo.pl "%Z%\packages\putty\putty-0.56-installer.exe /sp- /silent"
