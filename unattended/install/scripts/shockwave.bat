83363ac71aad16404fc2b37a7655a25a02b59c84
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-04 19:29:48 +0000

    Update to 10.1.0.011
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1995 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/shockwave.bat
+++ unattended/install/scripts/shockwave.bat
@@ -1,5 +1,5 @@
 :: Install Shockwave10 player
-::URL|ALL|http://fpdownload.macromedia.com/pub/shockwave/default/english/win95nt/10.0.0.210/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full.exe
+::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.1.0.011/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full.exe
 ::
 :: Download from <http://www.macromedia.com/downloads/>
 
