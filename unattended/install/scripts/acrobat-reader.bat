f2de57267c49c30d115e824bcb946c4924a83f90
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-04 17:12:20 +0000

    Add all available additional languages (dan, jpn, kor, nor, ptb, sve) and let todo handle the errorcode on exit
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@1992 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/acrobat-reader.bat
+++ unattended/install/scripts/acrobat-reader.bat
@@ -1,10 +1,14 @@
 :: Install Adobe Acrobat reader
-:: URL|ENU|http://ardownload.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_ENU_full.exe|packages/adobereader/acroreader51_enu_full.exe
+:: URL|DAN|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_DAN_full.exe|packages/adobereader/acroreader51_dan_full.exe
 :: URL|DEU|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_DEU_full.exe|packages/adobereader/acroreader51_deu_full.exe
-:: URL|NLD|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_NLD_full.exe|packages/adobereader/acroreader51_nld_full.exe
+:: URL|ENU|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_ENU_full.exe|packages/adobereader/acroreader51_enu_full.exe
 :: URL|FRA|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_FRA_full.exe|packages/adobereader/acroreader51_fra_full.exe
 :: URL|ITA|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_ITA_full.exe|packages/adobereader/acroreader51_ita_full.exe
+:: URL|JPN|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_JPN_full.exe|packages/adobereader/acroreader51_jpn_full.exe
+:: URL|KOR|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_KOR_full.exe|packages/adobereader/acroreader51_kor_full.exe
+:: URL|NLD|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_NLD_full.exe|packages/adobereader/acroreader51_nld_full.exe
+:: URL|NOR|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_NOR_full.exe|packages/adobereader/acroreader51_nor_full.exe
+:: URL|PTB|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_PTB_full.exe|packages/adobereader/acroreader51_ptb_full.exe
+:: URL|SVE|http://download.adobe.com/pub/adobe/acrobatreader/win/5.x/5.1/AcroReader51_SVE_full.exe|packages/adobereader/acroreader51_sve_full.exe
 @Echo off
-start /wait %Z%\packages\AdobeReader\AcroReader51_%WINLANG%_full.exe /a /s /sms /f1%Z%\packages\AdobeReader\acrobat.iss /f2%SystemDrive%\netinst\logs\acrobat.txt
-
-if errorlevel 1 exit 1
+todo.pl "start /wait %Z%\packages\AdobeReader\AcroReader51_%WINLANG%_full.exe /a /s /sms /f1%Z%\packages\AdobeReader\acrobat.iss /f2%SystemDrive%\netinst\logs\acrobat.txt"
