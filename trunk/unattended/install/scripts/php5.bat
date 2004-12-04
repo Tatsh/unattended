5eca08953db8bd96855d8f448cfa88b26fcaa8fa
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-04 19:14:11 +0000

    Initial revision.

---
+++ trunk/unattended/install/scripts/php5.bat
@@ -0,0 +1,4 @@
+:: Install PHP5
+:: URL|ALL|http://www.php.net/get/php-5.0.2-installer.exe/from/www.php.net/mirror|packages/php-5.0.2-installer.exe
+@Echo off
+todo.pl "%Z%\packages\php-5.0.2-installer.exe /s"
