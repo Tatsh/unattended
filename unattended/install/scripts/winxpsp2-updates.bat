ee8dac875d51a3a61dc0eeda8aafdd5956dd1948
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-15 17:21:39 +0000

    Add MS04-041, MS04-043, and MS04-044.

--- unattended/install/scripts/winxpsp2-updates.bat
+++ unattended/install/scripts/winxpsp2-updates.bat
@@ -177,9 +177,87 @@ todo.pl ".reboot-on 194 %Z%\updates\common\%WINLANG%\dotnetfx.exe /q /c:\"instal
 
 :: Recommended Updates
 
+:: Microsoft Security Bulletin MS04-044
+:: "Vulnerabilities in Windows Kernel and LSASS Could Allow Elevation of Privilege (885835)"
+:: <http://www.microsoft.com/technet/security/bulletin/MS04-044.mspx>
+:: URL|ARA|http://download.microsoft.com/download/7/7/4/77434035-5f83-4b5a-884a-aed86d6d5403/WindowsXP-KB885835-x86-ARA.exe|updates/winxpsp2/windowsxp-kb885835-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/4/5/4/454b4216-d5c3-44b9-ae8a-bf0d58fa090e/WindowsXP-KB885835-x86-CSY.exe|updates/winxpsp2/windowsxp-kb885835-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/c/4/2/c42841e8-6ee8-40c7-a192-344f7dd1b1d8/WindowsXP-KB885835-x86-DAN.exe|updates/winxpsp2/windowsxp-kb885835-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/5/2/1/521c415a-0f62-4da3-b974-531c2ea227be/WindowsXP-KB885835-x86-DEU.exe|updates/winxpsp2/windowsxp-kb885835-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/5/d/8/5d8c468f-18e0-44f0-8500-c9ccc6f98d7b/WindowsXP-KB885835-x86-ELL.exe|updates/winxpsp2/windowsxp-kb885835-x86-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/c/c/3/cc385a85-3a2e-4632-ad43-72e48480e69c/WindowsXP-KB885835-x86-ENU.exe|updates/winxpsp2/windowsxp-kb885835-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/2/4/1/241b08c4-8c39-47ac-97a6-e08d76c3b670/WindowsXP-KB885835-x86-ESN.exe|updates/winxpsp2/windowsxp-kb885835-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/4/f/5/4f5544f2-fbd7-4a7b-8f77-88a57e3a3894/WindowsXP-KB885835-x86-FIN.exe|updates/winxpsp2/windowsxp-kb885835-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/9/d/6/9d6d74f7-d0ea-4db7-b631-3dfed1be30ec/WindowsXP-KB885835-x86-FRA.exe|updates/winxpsp2/windowsxp-kb885835-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/c/f/3/cf3ed3a5-130d-4b84-a261-4ad47c929494/WindowsXP-KB885835-x86-HEB.exe|updates/winxpsp2/windowsxp-kb885835-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/8/c/6/8c638e89-8da8-44e9-8942-50f320422128/WindowsXP-KB885835-x86-HUN.exe|updates/winxpsp2/windowsxp-kb885835-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/7/e/a/7ea1b1e8-54c1-498d-b272-9a09d9caaa04/WindowsXP-KB885835-x86-ITA.exe|updates/winxpsp2/windowsxp-kb885835-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/e/7/9/e798d344-4539-4ede-8a62-925cd22ef067/WindowsXP-KB885835-x86-JPN.exe|updates/winxpsp2/windowsxp-kb885835-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/3/2/7/32759a6f-b003-4361-9864-1a369a7b2e9a/WindowsXP-KB885835-x86-KOR.exe|updates/winxpsp2/windowsxp-kb885835-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/7/9/9/7997fe91-799f-45bb-89a6-ccc1283d1a36/WindowsXP-KB885835-x86-NLD.exe|updates/winxpsp2/windowsxp-kb885835-x86-nld.exe
+:: URL|PLK|http://download.microsoft.com/download/8/f/7/8f732d0d-4835-4fb0-bbfd-546c6ddcfc84/WindowsXP-KB885835-x86-PLK.exe|updates/winxpsp2/windowsxp-kb885835-x86-plk.exe
+:: URL|PRG|http://download.microsoft.com/download/9/a/b/9abdd184-acbc-4fa1-83b5-29ee49c05c6c/WindowsXP-KB885835-x86-PTG.exe|updates/winxpsp2/windowsxp-kb885835-x86-ptg.exe
+:: URL|PTB|http://download.microsoft.com/download/3/6/a/36a46cf9-3ae2-4065-b09d-38782cba4c1d/WindowsXP-KB885835-x86-PTB.exe|updates/winxpsp2/windowsxp-kb885835-x86-ptb.exe
+:: URL|RUS|http://download.microsoft.com/download/0/c/e/0ce6286b-7baf-4592-8c70-354cfcd0ea38/WindowsXP-KB885835-x86-RUS.exe|updates/winxpsp2/windowsxp-kb885835-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/9/1/d/91d6e290-31ba-4b66-86ae-1b03ae6ea6d9/WindowsXP-KB885835-x86-SVE.exe|updates/winxpsp2/windowsxp-kb885835-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/a/d/7/ad7672e1-71cb-43e0-8f12-71ef95537696/WindowsXP-KB885835-x86-TRK.exe|updates/winxpsp2/windowsxp-kb885835-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp2\WindowsXP-KB885835-x86-%WINLANG%.exe /passive /n /norestart"
+
+:: Microsoft Security Bulletin MS04-043
+:: "Vulnerability in HyperTerminal Could Allow Code Execution (873339)"
+:: <http://www.microsoft.com/technet/security/bulletin/MS04-043.mspx>
+:: URL|ARA|http://download.microsoft.com/download/5/0/5/505d24d2-c8cd-4cb6-aec1-05b63b50a89b/WindowsXP-KB873339-x86-ARA.exe|updates/winxpsp2/windowsxp-kb873339-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/c/f/f/cff9eef1-e9f4-483e-8958-9d835cbe87c7/WindowsXP-KB873339-x86-CSY.exe|updates/winxpsp2/windowsxp-kb873339-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/4/9/4/4948657c-3c9c-409b-b7f9-dcbec0bd466f/WindowsXP-KB873339-x86-DAN.exe|updates/winxpsp2/windowsxp-kb873339-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/9/e/0/9e00feee-075c-4674-9a34-be28ceb6f546/WindowsXP-KB873339-x86-DEU.exe|updates/winxpsp2/windowsxp-kb873339-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/a/c/9/ac9d6c1c-6cd0-4be9-b974-3500dbe890a9/WindowsXP-KB873339-x86-ELL.exe|updates/winxpsp2/windowsxp-kb873339-x86-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/8/3/e/83e7e311-f8ea-4e59-9b50-64dbfdcb0f1f/WindowsXP-KB873339-x86-ENU.exe|updates/winxpsp2/windowsxp-kb873339-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/a/d/8/ad88e37e-7711-4227-8a8d-fd13c47919e9/WindowsXP-KB873339-x86-ESN.exe|updates/winxpsp2/windowsxp-kb873339-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/1/4/6/146875b0-0c37-40c2-863c-b451bd7f0569/WindowsXP-KB873339-x86-FIN.exe|updates/winxpsp2/windowsxp-kb873339-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/a/d/9/ad9f3764-3e77-41ea-8301-21a74e2cba24/WindowsXP-KB873339-x86-FRA.exe|updates/winxpsp2/windowsxp-kb873339-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/b/3/5/b35b1b82-5cc6-403c-bbff-591481188990/WindowsXP-KB873339-x86-HEB.exe|updates/winxpsp2/windowsxp-kb873339-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/2/2/f/22ff0948-a3d0-4b9e-8b89-720f584e16b1/WindowsXP-KB873339-x86-HUN.exe|updates/winxpsp2/windowsxp-kb873339-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/e/2/0/e20c6a78-32f1-490e-bd17-683e58daced2/WindowsXP-KB873339-x86-ITA.exe|updates/winxpsp2/windowsxp-kb873339-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/b/6/9/b699023d-0258-4a65-80fe-1ef7fed55acc/WindowsXP-KB873339-x86-JPN.exe|updates/winxpsp2/windowsxp-kb873339-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/0/a/7/0a7ce1cc-0475-492d-9eb8-4eda5fac66ca/WindowsXP-KB873339-x86-KOR.exe|updates/winxpsp2/windowsxp-kb873339-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/a/7/c/a7c6f7f5-0009-477d-a2e5-6c86d8430612/WindowsXP-KB873339-x86-NLD.exe|updates/winxpsp2/windowsxp-kb873339-x86-nld.exe
+:: URL|PLK|http://download.microsoft.com/download/0/c/5/0c5f611e-7ffd-446e-ab8a-76435fda2edb/WindowsXP-KB873339-x86-PLK.exe|updates/winxpsp2/windowsxp-kb873339-x86-plk.exe
+:: URL|PRG|http://download.microsoft.com/download/3/a/5/3a5db46d-4d1c-4b01-a255-9fbd7ca494de/WindowsXP-KB873339-x86-PTG.exe|updates/winxpsp2/windowsxp-kb873339-x86-ptg.exe
+:: URL|PTB|http://download.microsoft.com/download/4/5/2/45208561-6e5e-41e7-b7b1-fae92d944698/WindowsXP-KB873339-x86-PTB.exe|updates/winxpsp2/windowsxp-kb873339-x86-ptb.exe
+:: URL|RUS|http://download.microsoft.com/download/9/3/0/93074d28-ef15-4719-9455-e6132d8f4dc6/WindowsXP-KB873339-x86-RUS.exe|updates/winxpsp2/windowsxp-kb873339-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/3/7/8/378a134b-a440-494f-be21-2e2ea5054530/WindowsXP-KB873339-x86-SVE.exe|updates/winxpsp2/windowsxp-kb873339-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/7/0/c/70cc17ab-c441-4ab6-b20a-14d3b53d837f/WindowsXP-KB873339-x86-TRK.exe|updates/winxpsp2/windowsxp-kb873339-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp2\WindowsXP-KB873339-x86-%WINLANG%.exe /passive /n /norestart"
+
+:: Microsoft Security Bulletin MS04-041
+:: "Vulnerability in WordPad Could Allow Code Execution (885836)"
+:: <http://www.microsoft.com/technet/security/bulletin/MS04-041.mspx>
+:: URL|ARA|http://download.microsoft.com/download/d/a/5/da5ba6a4-9aa3-4475-be73-4448d9537be7/WindowsXP-KB885836-x86-ARA.exe|updates/winxpsp2/windowsxp-kb885836-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/0/1/e/01e25635-1c98-46f4-9aed-efebbd8fbdba/WindowsXP-KB885836-x86-CSY.exe|updates/winxpsp2/windowsxp-kb885836-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/6/6/a/66a7c2fa-0bc2-4deb-a3eb-618289ac1f4f/WindowsXP-KB885836-x86-DAN.exe|updates/winxpsp2/windowsxp-kb885836-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/2/a/b/2ab2e80e-4b78-4f08-9c4c-6b8430d07eeb/WindowsXP-KB885836-x86-DEU.exe|updates/winxpsp2/windowsxp-kb885836-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/e/8/d/e8d7a5a2-317e-4ebe-b5fb-61373b5119d7/WindowsXP-KB885836-x86-ELL.exe|updates/winxpsp2/windowsxp-kb885836-x86-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/3/d/a/3dafd665-ac57-4abe-9ddb-63d4398b7fd2/WindowsXP-KB885836-x86-ENU.exe|updates/winxpsp2/windowsxp-kb885836-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/c/1/f/c1fad355-94de-4142-bbf4-13a57fcff27c/WindowsXP-KB885836-x86-ESN.exe|updates/winxpsp2/windowsxp-kb885836-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/0/1/a/01a0391c-b2af-4c09-96dd-26bd8c136533/WindowsXP-KB885836-x86-FIN.exe|updates/winxpsp2/windowsxp-kb885836-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/2/4/7/2479f2e6-a35e-495d-b083-a0672873fa0a/WindowsXP-KB885836-x86-FRA.exe|updates/winxpsp2/windowsxp-kb885836-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/a/1/5/a1590f07-3a13-4917-8394-c8f9b4e4916e/WindowsXP-KB885836-x86-HEB.exe|updates/winxpsp2/windowsxp-kb885836-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/4/e/9/4e9d15d8-fa96-401c-bd92-a6d672e2c0d3/WindowsXP-KB885836-x86-HUN.exe|updates/winxpsp2/windowsxp-kb885836-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/b/f/2/bf24b6ae-1ef6-494a-b1f1-821b815528ea/WindowsXP-KB885836-x86-ITA.exe|updates/winxpsp2/windowsxp-kb885836-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/f/a/a/faaf8a53-7adf-4e4a-82fd-7d5d24224b3e/WindowsXP-KB885836-x86-JPN.exe|updates/winxpsp2/windowsxp-kb885836-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/6/e/3/6e3ce2cf-5f3e-43de-9f93-1baefe2d39e5/WindowsXP-KB885836-x86-KOR.exe|updates/winxpsp2/windowsxp-kb885836-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/4/9/2/4922593f-9a8a-4525-ba61-fd4b12ddb366/WindowsXP-KB885836-x86-NLD.exe|updates/winxpsp2/windowsxp-kb885836-x86-nld.exe
+:: URL|PLK|http://download.microsoft.com/download/d/7/5/d75d2db8-6324-431b-ae90-a5d31407aa3e/WindowsXP-KB885836-x86-PLK.exe|updates/winxpsp2/windowsxp-kb885836-x86-plk.exe
+:: URL|PRG|http://download.microsoft.com/download/b/e/7/be78a62d-3976-446c-a56d-07676858b5fe/WindowsXP-KB885836-x86-PTG.exe|updates/winxpsp2/windowsxp-kb885836-x86-ptg.exe
+:: URL|PTB|http://download.microsoft.com/download/6/8/6/6862e977-a379-4185-8326-6726855c6a85/WindowsXP-KB885836-x86-PTB.exe|updates/winxpsp2/windowsxp-kb885836-x86-ptb.exe
+:: URL|RUS|http://download.microsoft.com/download/c/8/e/c8e6f231-6810-4cd0-a397-e6df09cf2c77/WindowsXP-KB885836-x86-RUS.exe|updates/winxpsp2/windowsxp-kb885836-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/3/a/0/3a00f15b-4d81-4686-b773-7ef494cffb53/WindowsXP-KB885836-x86-SVE.exe|updates/winxpsp2/windowsxp-kb885836-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/8/a/e/8ae4a3a2-ff3a-4f89-a8a8-5045858b4669/WindowsXP-KB885836-x86-TRK.exe|updates/winxpsp2/windowsxp-kb885836-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp2\WindowsXP-KB885836-x86-%WINLANG%.exe /passive /n /norestart"
+
+
 :: IE Critical Updates
 
-:: Cumulative Security Update for Internet Explorer for XP Service Pack 2 (KB834707)
 :: Microsoft Security Bulletin MS04-038
 :: "Cumulative Security Update for Internet Explorer (834707)"
 :: <http://www.microsoft.com/technet/security/bulletin/ms04-038.mspx>
