089a18ed3a31d5b54045babbf824194e350dcb2a
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-21 21:28:02 +0000

    Upgrade to 4.3.10 and 5.0.3 (security fixes).

--- unattended/install/scripts/php.bat
+++ unattended/install/scripts/php.bat
@@ -1,4 +1,4 @@
 :: Install PHP4
-:: URL|ALL|http://www.php.net/get/php-4.3.9-installer.exe/from/www.php.net/mirror|packages/php-4.3.9-installer.exe
+:: URL|ALL|http://www.php.net/get/php-4.3.10-installer.exe/from/www.php.net/mirror|packages/php-4.3.10-installer.exe
 @Echo off
-todo.pl "%Z%\packages\php-4.3.9-installer.exe /s"
+todo.pl "%Z%\packages\php-4.3.10-installer.exe /s"
