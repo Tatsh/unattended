186f5133befe8cd7bfc4582550b4e3ef822e6645
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-22 16:03:43 +0000

    Upgrade to Java Runtime Environment 1.5.0 Update 1.

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 
-:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=9833|packages/jre1.5.0.msi
+:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=9934|packages/jre1.5.0_01.msi
 
 :: Download from <http://java.sun.com/j2se/1.5.0/download.jsp>.
 
@@ -9,4 +9,4 @@
 :: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/silent.html>
 
 @Echo off
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0.msi IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress JAVAUPDATE=0"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_01.msi IEXPLORER=1 MOZILLA=1 NETSCAPE6=1 REBOOT=ReallySuppress JAVAUPDATE=0"
