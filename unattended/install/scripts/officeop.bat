ffa238c288b10d12a0e697d502a796133ea8712d
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2004-12-23 16:20:05 +0000

    Updated all urls; added dan, jpn, nld.

--- unattended/install/scripts/officeop.bat
+++ unattended/install/scripts/officeop.bat
@@ -1,9 +1,12 @@
 :: OPTIONAL: Install OpenOffice.org
 
-:: URL|DEU|http://www.ibiblio.org/pub/mirrors/openoffice/localized/de/1.1.2/OOo_1.1.2_Win32Intel_install_de.zip|packages/openoffice/deu/ooo_1.1.2_win32intel_install_deu.zip
-:: URL|ENU|http://www.ibiblio.org/pub/mirrors/openoffice/stable/1.1.2/OOo_1.1.2_Win32Intel_install.zip|packages/openoffice/enu/ooo_1.1.2_win32intel_install_enu.zip
-:: URL|FRA|http://www.ibiblio.org/pub/mirrors/openoffice/localized/fr/1.1.2/OOo_1.1.2_Win32Intel_install_fr.zip|packages/openoffice/fra/ooo_1.1.2_win32intel_install_fra.zip
-:: URL|ITA|http://www.ibiblio.org/pub/mirrors/openoffice/localized/it/1.1.2/OOo_1.1.2_Win32Intel_install_it.zip|packages/openoffice/ita/ooo_1.1.2_win32intel_install_ita.zip
+:: URL|DAN|http://www.ibiblio.org/pub/mirrors/openoffice/localized/da/1.1.3/OOo_1.1.3_Win32Intel_install_da.zip|packages/openoffice/dan/ooo_1.1.4_dan.zip
+:: URL|DEU|http://www.ibiblio.org/pub/mirrors/openoffice/localized/de/1.1.4/OOo_1.1.4_Win32Intel_install_de.zip|packages/openoffice/deu/ooo_1.1.4_deu.zip
+:: URL|ENU|http://www.ibiblio.org/pub/mirrors/openoffice/stable/1.1.4/OOo_1.1.4_Win32Intel_install.zip|packages/openoffice/enu/ooo_1.1.4_enu.zip
+:: URL|FRA|http://www.ibiblio.org/pub/mirrors/openoffice/localized/fr/1.1.4/OOo_1.1.4_Win32Intel_install_fr.zip|packages/openoffice/fra/ooo_1.1.4_fra.zip
+:: URL|ITA|http://www.ibiblio.org/pub/mirrors/openoffice/localized/it/1.1.3/OOo_1.1.3_Win32Intel_install_it.zip|packages/openoffice/ita/ooo_1.1.3_ita.zip
+:: URL|JPN|http://www.ibiblio.org/pub/mirrors/openoffice/localized/ja/1.1.3/OOo_1.1.3_Win32Intel_install_ja.zip|packages/openoffice/jpn/ooo_1.1.3_jpn.zip
+:: URL|NLD|http://www.ibiblio.org/pub/mirrors/openoffice/localized/nl/1.1.4/OOo_1.1.4_Win32Intel_install_nl.zip|packages/openoffice/nld/ooo_1.1.4_nld.zip
 
 @Echo off
 todo.pl "shortcut.pl \"%ProgramFiles%\"\OpenOffice.org\program\soffice.exe special:AllUsersDesktop\OpenOffice"
