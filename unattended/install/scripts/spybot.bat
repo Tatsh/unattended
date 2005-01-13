fe140efba18fdb41e8a91b59d8c30741869c7b06
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-01-13 23:08:53 +0000

    Fix download URL.

--- unattended/install/scripts/spybot.bat
+++ unattended/install/scripts/spybot.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Spybot - Search & Destroy
 
-:: URL|ALL|ftp://ftp.download.com/pub/win95/utilities/spybotsd13.exe|packages/spybotsd13.exe
+:: URL|ALL|http://software-files.download.com/c1/dl/software/cache/spybotsd13.exe|packages/spybotsd13.exe
 @Echo off
 todo.pl "%Z%\packages\spybotsd13.exe /sp- /silent /norestart"
