b46e4aa6dc768307caa3ad9789c0b7881d574c52
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-02-17 21:18:54 +0000

    Add (commented-out) command to auto-update. Thanks to Ty Moffet.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2114 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/spybot.bat
+++ unattended/install/scripts/spybot.bat
@@ -2,4 +2,9 @@
 
 :: URL|ALL|http://software-files.download.com/c1/dl/software/cache/spybotsd13.exe|packages/spybotsd13.exe
 @Echo off
+
+:: Un-comment the following line, if you want to run Spybot, check for updates, auto fix found issues,
+:: and auto-immunize. The machine *will* need public internet access for that.
+:: todo.pl "%ProgramFiles%\Spybot~1\SpybotSD.exe /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
+
 todo.pl "%Z%\packages\spybotsd13.exe /sp- /silent /norestart"
