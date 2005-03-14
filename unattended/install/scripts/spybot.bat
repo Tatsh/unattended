69d940c5650cb96cf575af6447cf6795a7b00325
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-03-14 23:13:07 +0000

    Fix problem with auto-update call
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2126 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/spybot.bat
+++ unattended/install/scripts/spybot.bat
@@ -5,6 +5,6 @@
 
 :: Un-comment the following line, if you want to run Spybot, check for updates, auto fix found issues,
 :: and auto-immunize. The machine *will* need public internet access for that.
-:: todo.pl "%ProgramFiles%\Spybot~1\SpybotSD.exe /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
+:: todo.pl "\"%ProgramFiles%\Spybot~1\SpybotSD.exe\" /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
 
 todo.pl "%Z%\packages\spybotsd13.exe /sp- /silent /norestart"
