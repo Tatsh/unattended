a03e0ec5ffbdec0c23fdb4b4a3bf06db44d96a28
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-03-22 13:44:28 +0000

    Thunderbird 1.0.2 released (currently only ENU)

--- unattended/install/scripts/thunderbird.bat
+++ unattended/install/scripts/thunderbird.bat
@@ -1,8 +1,11 @@
 :: OPTIONAL: Install Thunderbird
 ::
-:: PLEASE NOTE: these release contains an installer with a bug that prevents
+:: PLEASE NOTE: the 1.0 release contains an installer with a bug that prevents
 :: a silent installation. Please follow the howto below to work around that
 :: issue. Thank you.
+::
+:: The 1.0.2 release (currently only ENU) does not contain this bug. There is
+:: no further preparation necessary.
 ::
 :: URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/ca-AD/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-cat.exe
 :: URL|CHS|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/zh-CN/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-chs.exe
@@ -10,7 +13,7 @@
 :: URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/da-DK/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-dan.exe
 :: URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/de-DE/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-deu.exe
 :: URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/el-GR/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-ell.exe
-:: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/en-US/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-enu.exe
+:: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0.2/win32/en-US/Thunderbird%20Setup%201.0.2.exe|packages/mozilla/thunderbird-102-enu.exe
 :: URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/es-ES/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-esn.exe
 :: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/es-AR/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-ess.exe
 :: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/1.0/win32/fi-FI/Thunderbird%20Setup%201.0.exe|packages/mozilla/thunderbird-100-fin.exe
@@ -36,4 +39,4 @@
 ::
 @echo off
 
-todo.pl "%Z%\packages\mozilla\thunderbird-100-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-10?-%WINLANG%.exe -ms -ira"
