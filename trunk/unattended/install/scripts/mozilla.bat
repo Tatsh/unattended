746f440c8c1d604578b8842efe022adb60af444a
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-03-22 13:53:37 +0000

    Update to version 1.7.6 (security fixes)

--- trunk/unattended/install/scripts/mozilla.bat
+++ trunk/unattended/install/scripts/mozilla.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Mozilla
-:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.5/mozilla-win32-1.7.5-installer.exe|packages/mozilla-win32-1.7.5-installer.exe
+:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.6/mozilla-win32-1.7.6-installer.exe|packages/mozilla/mozilla-176-all.exe
 
 @Echo off
-todo.pl "%Z%\packages\mozilla-win32-1.7.5-installer.exe -ma -ira"
+todo.pl "%Z%\packages\mozilla\mozilla-176-all.exe -ma -ira"
