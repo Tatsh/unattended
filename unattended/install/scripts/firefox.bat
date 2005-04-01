72427d5671622452b6336b166662a212f5b97c8e
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-01 21:20:50 +0000

    Update to 1.0.2 (for HEB, NOR)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2154 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/firefox.bat
+++ unattended/install/scripts/firefox.bat
@@ -13,14 +13,14 @@
 :: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/es-AR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-ess.exe
 :: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/fi-FI/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-fin.exe
 :: URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/fr-FR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-fra.exe
-:: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.1/win32/he-IL/Firefox%20Setup%201.0.1.exe|packages/mozilla/firefox-101-heb.exe
+:: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/he-IL/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-heb.exe
 :: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/hu-HU/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-hun.exe
 :: URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/it-IT/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-ita.exe
 :: URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/ja-JP/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-jpn.exe
 :: URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/ko-KR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-kor.exe
 :: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/mk-MK/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-mki.exe
 :: URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/nl-NL/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-nld.exe
-:: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.1/win32/nb-NO/Firefox%20Setup%201.0.1.exe|packages/mozilla/firefox-101-nor.exe
+:: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/nb-NO/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-nor.exe
 :: URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/pl-PL/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-plk.exe
 :: URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/pt-BR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-ptb.exe
 :: URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/pt-PT/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-ptg.exe
