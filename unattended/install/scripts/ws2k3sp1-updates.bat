73ad0245e33a066fa5310ccd4b5d953250c4fb7f
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-04 19:19:11 +0000

    Initial version.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2155 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/ws2k3sp1-updates.bat
@@ -0,0 +1,14 @@
+:: Install all updates and hotfixes for Windows Server 2003 SP1
+@Echo off
+
+:: Note that since we are pushing these commands onto the to-do stack,
+:: they will be executed in the opposite order.
+
+:: Extra Updates
+
+:: Recommended Updates
+
+:: IE Critical Updates
+
+:: Critical Updates
+
