524529eddf9e77d6fb435a1bab93ef29511df13c
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-06 14:23:37 +0000

    Update to version 0.58

--- unattended/install/scripts/putty.bat
+++ unattended/install/scripts/putty.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install Putty 0.57
-:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.57-installer.exe|packages/putty/putty-0.57-installer.exe
+:: OPTIONAL: Install Putty 0.58
+:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.58-installer.exe|packages/putty/putty-0.58-installer.exe
 @Echo off
 
-todo.pl "%Z%\packages\putty\putty-0.57-installer.exe /sp- /silent"
+todo.pl "%Z%\packages\putty\putty-0.58-installer.exe /sp- /silent"
