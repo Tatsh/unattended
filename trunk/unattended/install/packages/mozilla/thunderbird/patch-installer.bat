4b3cd691f2d7a1c763827d60a271133628093bf0
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-11 21:20:44 +0000

    Remove file because bug in thunderbird-installer is fixed

--- trunk/unattended/install/packages/mozilla/thunderbird/patch-installer.bat
+++
@@ -1,6 +0,0 @@
-:: Please use this script to patch your Thunderbird-installer
-::
-:: You must have 7-Zip installed on your computer.
-::
-@echo off
-FOR %%A IN (..\thunderbird-*.exe) DO "%ProgramFiles%\7-Zip\7z.exe" a %%A setup*.*
