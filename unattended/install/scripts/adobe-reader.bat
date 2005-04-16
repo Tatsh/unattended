95eb26b7209ecd0b005d659982328d9b9a9d95fd
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-16 16:16:13 +0000

    Use version 6.0.2 instead of 6.0.1 + update

--- unattended/install/scripts/adobe-reader.bat
+++ unattended/install/scripts/adobe-reader.bat
@@ -2,7 +2,7 @@
 :: Download from <http://www.adobe.com/products/acrobat/alternate.html>
 @Echo off
 
-:: Download version 7 if available; otherwise, download 6.0.1.
+:: Download version 7 if available; otherwise, download 6.0.2.
 :: URL|DEU|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0/deu/AdbeRdr70_deu_full.exe|packages/adobereader/adberdr70_deu_full.exe
 :: URL|ENU|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0/enu/AdbeRdr70_enu_full.exe|packages/adobereader/adberdr70_enu_full.exe
 :: URL|FRA|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0/fra/AdbeRdr70_fra_full.exe|packages/adobereader/adberdr70_fra_full.exe
@@ -10,7 +10,7 @@
 :: URL|JPN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0/jpn/AdbeRdr70_jpn_full.exe|packages/adobereader/adberdr70_jpn_full.exe
 :: URL|NLD|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0/nld/AdbeRdr70_nld_full.exe|packages/adobereader/adberdr70_nld_full.exe
 :: URL|NOR|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0/nor/AdbeRdr70_nor_full.exe|packages/adobereader/adberdr70_nor_full.exe
-:: URL|RUS|http://ardownload.adobe.com/pub/adobe/reader/win/6.x/6.0/enu/AdbeRdr60_enu_full.exe|packages/adobereader/adberdr60_rus_full.exe
+:: URL|RUS|http://ardownload.adobe.com/pub/adobe/reader/win/6.x/6.0/rus/AdbeRdr602_rus_full.exe|packages/adobereader/adberdr602_rus_full.exe
 
 if not exist %Z%\packages\adobereader\adberdr70_%WINLANG%_full.exe goto no_v7
 
@@ -24,9 +24,7 @@ goto done
 
 :no_v7
 
-:: No version 7 for this language, so install 6.0.1 and update to 6.0.2
-:: URL|ALL|http://download.adobe.com/pub/adobe/acrobat/win/6.x/6.0.1/misc/Acro-Reader_6.0.2_Update.exe|packages/adobereader/update_602.exe
-todo.pl "%Z%\packages\adobereader\update_602.exe /S /v/qn"
-todo.pl "%Z%\packages\adobereader\adberdr60_%WINLANG%_full.exe -p\"-s /v\\\"/qb /l* %SystemDrive%\netinst\logs\adobe-reader.txt\\\"\""
+:: No version 7 for this language, so install 6.0.2
+todo.pl "%Z%\packages\adobereader\adberdr602_%WINLANG%_full.exe -p\"-s /v\\\"/qb /l* %SystemDrive%\netinst\logs\adobe-reader.txt\\\"\""
 
 :done
