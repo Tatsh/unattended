2717ab0c37b544452ebfa7cdafd49cf0942435fd
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-19 20:18:31 +0000

    Try REBOOT=Suppress instead of ReallySuppress, since the former is documented and the latter does not appear to be working.
    Add ".reboot-on 194".
    
    
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2194 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -9,4 +9,4 @@
 :: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/silent.html>
 
 @Echo off
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_02.msi IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0"
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_02.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
