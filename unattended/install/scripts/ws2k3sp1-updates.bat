2489c22cbc7e65d147ff443e79846fceb0050f0b
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-04-26 19:30:03 +0000

    Fixed typo with kb892313 update
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2202 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/ws2k3sp1-updates.bat
+++ unattended/install/scripts/ws2k3sp1-updates.bat
@@ -9,7 +9,7 @@
 :: FIX: Updates for Windows Media Player 9 Series and for Windows Media Player 10
 :: <http://support.microsoft.com/kb/892313>
 :: URL|ALL|http://download.microsoft.com/download/c/a/c/cac342d1-db54-4620-8af2-b7f52d48632e/windowsmedia9-kb892313-x86-enu.exe|updates/mediaplayer9/windowsmedia9-kb892313-x86-enu.exe
-todo.pl ".reboot-on 194 %Z%\mediaplayer9\windowsmedia9-kb892313-x86-enu.exe /passive /n /norestart"
+todo.pl ".reboot-on 194 %Z%\updates\mediaplayer9\windowsmedia9-kb892313-x86-enu.exe /passive /n /norestart"
 
 :: Recommended Updates
 
