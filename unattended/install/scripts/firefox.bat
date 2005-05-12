6bbfdb3d18ed162bb098a8852a09ba5f8c8fe1f8
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-05-12 14:22:40 +0000

    Update to version 1.0.4 (security fix)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2211 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/firefox.bat
+++ unattended/install/scripts/firefox.bat
@@ -1,47 +1,47 @@
 :: OPTIONAL: Install Firefox
-:: URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/ca-AD/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-cat.exe
-:: URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/zh-TW/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-cht.exe
-:: URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/cs-CZ/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-csy.exe
-:: URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/da-DK/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-dan.exe
-:: URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/de-DE/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-deu.exe
-:: URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/el-GR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-ell.exe
-:: URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/en-GB/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-eng.exe
-:: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/en-US/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-enu.exe
+:: URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/ca-AD/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-cat.exe
+:: URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/zh-TW/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-cht.exe
+:: URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/cs-CZ/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-csy.exe
+:: URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/da-DK/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-dan.exe
+:: URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/de-DE/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-deu.exe
+:: URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/el-GR/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-ell.exe
+:: URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/en-GB/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-eng.exe
+:: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/en-US/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-enu.exe
 :: URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/es-ES/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-esn.exe
-:: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/es-AR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-ess.exe
-:: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/fi-FI/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-fin.exe
-:: URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/fr-FR/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-fra.exe
-:: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/he-IL/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-heb.exe
-:: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/hu-HU/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-hun.exe
-:: URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/it-IT/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-ita.exe
+:: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/es-AR/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-ess.exe
+:: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/fi-FI/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-fin.exe
+:: URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/fr-FR/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-fra.exe
+:: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/he-IL/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-heb.exe
+:: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/hu-HU/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-hun.exe
+:: URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/it-IT/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-ita.exe
 :: URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/ja-JP/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-jpn.exe
-:: URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/ko-KR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-kor.exe
-:: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/mk-MK/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-mki.exe
+:: URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/ko-KR/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-kor.exe
+:: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/mk-MK/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-mki.exe
 :: URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/nl-NL/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-nld.exe
-:: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/nb-NO/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-nor.exe
-:: URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/pl-PL/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-plk.exe
-:: URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/pt-BR/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-ptb.exe
+:: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/nb-NO/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-nor.exe
+:: URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/pl-PL/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-plk.exe
+:: URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/pt-BR/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-ptb.exe
 :: URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/pt-PT/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-ptg.exe
 :: URL|ROM|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/ro-RO/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-rom.exe
 :: URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/ru-RU/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-rus.exe
 :: URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/sl-SI/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-slv.exe
 :: URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.3/win32/sv-SE/Firefox%20Setup%201.0.3.exe|packages/mozilla/firefox-103-sve.exe
-:: URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.2/win32/tr-TR/Firefox%20Setup%201.0.2.exe|packages/mozilla/firefox-102-trk.exe
+:: URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.4/win32/tr-TR/Firefox%20Setup%201.0.4.exe|packages/mozilla/firefox-104-trk.exe
 
 
 @Echo off
 
 :: detect current patchlevel for your language
+if exist "%Z%\packages\mozilla\firefox-104-%WINLANG%.exe" goto v104
 if exist "%Z%\packages\mozilla\firefox-103-%WINLANG%.exe" goto v103
-if exist "%Z%\packages\mozilla\firefox-102-%WINLANG%.exe" goto v102
+goto end_of_script
+
+:v104
+todo.pl "%Z%\packages\mozilla\firefox-104-%WINLANG%.exe -ms -ira"
 goto end_of_script
 
 :v103
 todo.pl "%Z%\packages\mozilla\firefox-103-%WINLANG%.exe -ms -ira"
-goto end_of_script
-
-:v102
-todo.pl "%Z%\packages\mozilla\firefox-102-%WINLANG%.exe -ms -ira"
 goto end_of_script
 
 :end_of_script
