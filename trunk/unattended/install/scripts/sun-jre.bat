67c5dc3d77562766ee78004eb3024b20602b29c7
patl <patl@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-05-14 20:33:15 +0000

    1.5.0 Update 3

--- trunk/unattended/install/scripts/sun-jre.bat
+++ trunk/unattended/install/scripts/sun-jre.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 
-:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=9994|packages/jre1.5.0_02.msi
+:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=10070|packages/jre1.5.0_03.msi
 
 :: Download from <http://java.sun.com/j2se/1.5.0/download.jsp>.
 
@@ -9,4 +9,4 @@
 :: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/silent.html>
 
 @Echo off
-todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_02.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_03.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
