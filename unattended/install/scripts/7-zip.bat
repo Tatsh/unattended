aec8af835a30e0d0c987fbc01683f870b93e8e62
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-06-16 16:52:03 +0000

    Update to 7-zip v4.20

--- unattended/install/scripts/7-zip.bat
+++ unattended/install/scripts/7-zip.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install 7-Zip
-:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z313.exe|packages/7z313.exe
+:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z420.exe|packages/7z420.exe
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
-todo.pl "%Z%\packages\7z313.exe -y /q /r:n"
+todo.pl "%Z%\packages\7z420.exe /S /install=SFQR"
 :done
