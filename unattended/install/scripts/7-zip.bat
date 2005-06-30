9dfbfda2172fc9f1a580a2d4883b7661131afb16
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-06-30 04:26:18 +0000

    Update to 7-zip 4.23
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2252 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/7-zip.bat
+++ unattended/install/scripts/7-zip.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install 7-Zip
-:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z420.exe|packages/7z420.exe
+:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z423.exe|packages/7z423.exe
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
-todo.pl "%Z%\packages\7z420.exe /S /install=SFQR"
+todo.pl "%Z%\packages\7z423.exe /S /install=SFQR"
 :done
