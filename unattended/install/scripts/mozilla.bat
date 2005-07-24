31d6f48f0575a1ca7b65379ce8d453e860885c8d
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-07-24 07:17:09 +0000

    Updated to v1.7.10

--- unattended/install/scripts/mozilla.bat
+++ unattended/install/scripts/mozilla.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Mozilla
-:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.8/mozilla-win32-1.7.8-installer.exe|packages/mozilla/mozilla-178-all.exe
+:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.10/mozilla-win32-1.7.10-installer.exe|packages/mozilla/mozilla-1710-all.exe
 
 @Echo off
-todo.pl "%Z%\packages\mozilla\mozilla-178-all.exe -ma -ira"
+todo.pl "%Z%\packages\mozilla\mozilla-1710-all.exe -ma -ira"
