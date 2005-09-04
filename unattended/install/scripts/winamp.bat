774d8040bcbd2f2ebcaed88b6653f0c04a222295
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-09-04 19:52:11 +0000

    Fix small bug with %ProgramFiles%
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2284 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install WinAmp 5.1
 
 :: URL|ALL|http://download.nullsoft.com/winamp/client/winamp51_full_emusic-7plus.exe|packages/winamp/winamp5100-full.exe
-todo.pl "%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe /S"
+todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
 todo.pl "%Z%\packages\winamp\winamp5100-full.exe /S /install=SFQR"
