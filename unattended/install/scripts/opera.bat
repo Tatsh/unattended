8744c92c2ffd38afe9b8b615b64f9003d86a55dd
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-09-20 21:19:08 +0000

    Update to version 8.5

--- unattended/install/scripts/opera.bat
+++ unattended/install/scripts/opera.bat
@@ -1,37 +1,32 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|ftp://ftp.opera.com/pub/opera/win/802/en/ow32enen802.exe|packages/opera/ow32enen802.exe
+:: URL|ENU|http://get.opera.com/pub/opera/win/850/en/ow32enen850.exe|packages/opera/opera-850-enu.exe
+:: URL|DEU|http://get.opera.com/pub/opera/win/850/de/ow32dede850.exe|packages/opera/opera-850-deu.exe
+:: URL|PLK|http://get.opera.com/pub/opera/win/850/pl/ow32plpl850.exe|packages/opera/opera-850-plk.exe
+:: URL|CHS|http://get.opera.com/pub/opera/win/850/zh-cn/ow32zh-cnzh-cn850.exe|packages/opera/opera-850-chs.exe
+:: URL|DAN|http://get.opera.com/pub/opera/win/850/da/ow32dada850.exe|packages/opera/opera-850-dan.exe
+:: URL|NLD|http://get.opera.com/pub/opera/win/850/nl/ow32nlnl850.exe|packages/opera/opera-850-nld.exe
+:: URL|FIN|http://get.opera.com/pub/opera/win/850/fi/ow32fifi850.exe|packages/opera/opera-850-fin.exe
+:: URL|FRA|http://get.opera.com/pub/opera/win/850/fr/ow32frfr850.exe|packages/opera/opera-850-fra.exe
+:: URL|HIN|http://get.opera.com/pub/opera/win/850/hi/ow32hihi850.exe|packages/opera/opera-850-hin.exe
+:: URL|ITA|http://get.opera.com/pub/opera/win/850/it/ow32itit850.exe|packages/opera/opera-850-ita.exe
+:: URL|JPN|http://get.opera.com/pub/opera/win/850/ja/ow32jaja850.exe|packages/opera/opera-850-jpn.exe
+:: URL|KOR|http://get.opera.com/pub/opera/win/850/ko/ow32koko850.exe|packages/opera/opera-850-kor.exe
+:: URL|NOR|http://get.opera.com/pub/opera/win/850/nb/ow32nbnb850.exe|packages/opera/opera-850-nor.exe
+:: URL|NON|http://get.opera.com/pub/opera/win/850/nn/ow32nnnn850.exe|packages/opera/opera-850-non.exe
+:: URL|PTG|http://get.opera.com/pub/opera/win/850/pt/ow32ptpt850.exe|packages/opera/opera-850-ptg.exe
+:: URL|PTB|http://get.opera.com/pub/opera/win/850/pt-BR/ow32pt-BRpt-BR850.exe|packages/opera/opera-850-ptb.exe
+:: URL|RUS|http://get.opera.com/pub/opera/win/850/ru/ow32ruru850.exe|packages/opera/opera-850-rus.exe
+:: URL|ESN|http://get.opera.com/pub/opera/win/850/es-ES/ow32es-ESes-ES850.exe|packages/opera/opera-850-esn.exe
+:: URL|SVE|http://get.opera.com/pub/opera/win/850/sv/ow32svsv850.exe|packages/opera/opera-850-sve.exe
 
 :: note: we need the installer WITHOUT java (installer with java cannot install silently)
 @Echo off
 
-:: Language files:
-:: URL|BGR|http://www.opera.com/download/lng/802/ouw802_bg.lng|packages/opera/languages/opera-bgr.lng
-:: URL|CAT|http://www.opera.com/download/lng/802/ouw802_ca.lng|packages/opera/languages/opera-cat.lng
-:: URL|CHS|http://www.opera.com/download/lng/802/ouw802_zh-cn.lng|packages/opera/languages/opera-chs.lng
-:: URL|DAN|http://www.opera.com/download/lng/802/ouw802_da.lng|packages/opera/languages/opera-dan.lng
-:: URL|DEU|http://www.opera.com/download/lng/802/ouw802_de.lng|packages/opera/languages/opera-deu.lng
-:: URL|ENG|http://www.opera.com/download/lng/802/ouw802_en-GB.lng|packages/opera/languages/opera-eng.lng
-:: URL|ESN|http://www.opera.com/download/lng/802/ouw802_es-ES.lng|packages/opera/languages/opera-esn.lng
-:: URL|FIN|http://www.opera.com/download/lng/802/ouw802_fi.lng|packages/opera/languages/opera-fin.lng
-:: URL|FRA|http://www.opera.com/download/lng/802/ouw802_fr.lng|packages/opera/languages/opera-fra.lng
-:: URL|HUN|http://www.opera.com/download/lng/802/ouw802_hu.lng|packages/opera/languages/opera-hun.lng
-:: URL|ITA|http://www.opera.com/download/lng/802/ouw802_it.lng|packages/opera/languages/opera-ita.lng
-:: URL|JPN|http://www.opera.com/download/lng/802/ouw802_ja.lng|packages/opera/languages/opera-jpn.lng
-:: URL|KOR|http://www.opera.com/download/lng/802/ouw802_ko.lng|packages/opera/languages/opera-kor.lng
-:: URL|NLD|http://www.opera.com/download/lng/802/ouw802_nl.lng|packages/opera/languages/opera-nld.lng
-:: URL|NOR|http://www.opera.com/download/lng/802/ouw802_nn.lng|packages/opera/languages/opera-nor.lng
-:: URL|PLK|http://www.opera.com/download/lng/802/ouw802_pl.lng|packages/opera/languages/opera-plk.lng
-:: URL|PTB|http://www.opera.com/download/lng/802/ouw802_pt-BR.lng|packages/opera/languages/opera-ptb.lng
-:: URL|RUS|http://www.opera.com/download/lng/802/ouw802_ru.lng|packages/opera/languages/opera-rus.lng
-:: URL|SVE|http://www.opera.com/download/lng/802/ouw802_sv.lng|packages/opera/languages/opera-sve.lng
-:: URL|TRK|http://www.opera.com/download/lng/802/ouw802_tr.lng|packages/opera/languages/opera-trk.lng
-
 :: if you like to do special things (e.g. define custom searches), save your customized ini's in packages/opera/special
 
-%Z%\packages\opera\ow32enen802.exe /s
+%Z%\packages\opera\opera-850-%winlang%.exe /s
 if exist "%ProgramFiles%\Opera\Opera.exe" set opera="%ProgramFiles%\Opera"
 if exist "%ProgramFiles%\Opera7\Opera.exe" set opera="%ProgramFiles%\Opera7"
 if exist "%ProgramFiles%\Opera75\Opera.exe" set opera="%ProgramFiles%\Opera75"
 if exist "%ProgramFiles%\Opera8\Opera.exe" set opera="%ProgramFiles%\Opera8"
-if exist "%Z%\packages\opera\languages\opera-%winlang%.lng" copy "%Z%\packages\opera\languages\opera-%winlang%.lng" "%opera%"
 if exist "%Z%\packages\opera\special\*" copy "%Z%\packages\opera\special\*" "%opera%"
