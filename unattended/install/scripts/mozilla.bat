e283f72b961991730436969aab595661ddcee656
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-09-30 20:21:43 +0000

    Updated to v1.7.12
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2300 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/mozilla.bat
+++ unattended/install/scripts/mozilla.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Mozilla
-:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.10/mozilla-win32-1.7.10-installer.exe|packages/mozilla/mozilla-1710-all.exe
+:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.12/mozilla-win32-1.7.12-installer.exe|packages/mozilla/mozilla-1712-all.exe
 
 @Echo off
-todo.pl "%Z%\packages\mozilla\mozilla-1710-all.exe -ma -ira"
+todo.pl "%Z%\packages\mozilla\mozilla-1712-all.exe -ma -ira"
