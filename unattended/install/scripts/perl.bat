aafb7fc48ea37d41ace6a88178e47e8c723f64f8
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-09-30 18:26:24 +0000

    Updated perl to v5.8.7.813

--- unattended/install/scripts/perl.bat
+++ unattended/install/scripts/perl.bat
@@ -27,8 +27,8 @@ if "%WINVER%" == "win2k" goto skipinstaller3
 %Z%\packages\windowsinstaller\windowsinstaller-kb893803-v2-x86.exe /passive /norestart
 :skipinstaller3
 
-:: URL|ALL|http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.6.811-MSWin32-x86-122208.msi|packages/activeperl-5.8.6.811-mswin32-x86-122208.msi
-set perl_msi=%Z%\packages\activeperl-5.8.6.811-mswin32-x86-122208.msi
+:: URL|ALL|http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.7.813-MSWin32-x86-148120.msi|packages/activeperl-5.8.7.813-mswin32-x86-148120.msi
+set perl_msi=%Z%\packages\activeperl-5.8.7.813-mswin32-x86-148120.msi
 
 :retry
 if exist %perl_msi% goto have_perl
