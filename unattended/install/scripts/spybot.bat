91c7405048497395fb88c357b8d0d8291a98968e
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-10-03 17:27:28 +0000

    Updated to v1.4

--- unattended/install/scripts/spybot.bat
+++ unattended/install/scripts/spybot.bat
@@ -1,10 +1,10 @@
 :: OPTIONAL: Install Spybot - Search & Destroy
 
-:: URL|ALL|http://software-files.download.com/c1/dl/software/cache/spybotsd13.exe|packages/spybotsd13.exe
+:: URL|ALL|http://fileforum.betanews.com/sendfile/1043809773/1/spybotsd14.exe|packages/spybotsd14.exe
 @Echo off
 
 :: Un-comment the following line, if you want to run Spybot, check for updates, auto fix found issues,
 :: and auto-immunize. The machine *will* need public internet access for that.
 :: todo.pl "\"%ProgramFiles%\Spybot~1\SpybotSD.exe\" /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
 
-todo.pl "%Z%\packages\spybotsd13.exe /sp- /silent /norestart"
+todo.pl "%Z%\packages\spybotsd14.exe /sp- /silent /norestart"
