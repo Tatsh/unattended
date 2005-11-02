30cefdf6e5bb8a02890227c7db50da3ed957c375
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-11-02 19:10:38 +0000

    Updated download URL

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -5,6 +5,7 @@
 :: See:
 :: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/upgrade-guide/deployment.html>
 :: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/silent.html>
+:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=10209|packages/jre1.5.0_05.msi
 
 @Echo off
 todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_05.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
