06505b1096da294b7cbb8ceaf516ddb86e7078fc
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-12-04 09:28:19 +0000

    Updated to v5.09.7
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2315 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/att.bat
+++ unattended/install/scripts/att.bat
@@ -1,4 +1,4 @@
 :: Install AT&T Global Network Dialer
-:: URL|ALL|ftp://ftp.attglobal.net/pub/client/win32/d5091.exe|packages/d5091.exe
+:: URL|ALL|ftp://ftp.attglobal.net/pub/client/win32/d5097.exe|packages/d5097.exe
 @Echo off
-todo.pl "%Z%\packages\d5091.exe /s"
+todo.pl "%Z%\packages\d5097.exe /s"
