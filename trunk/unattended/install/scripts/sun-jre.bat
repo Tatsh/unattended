e0d98b9939ee8189d8f58aabf385f4fdeb45bbe2
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2005-12-08 17:49:59 +0000

    Updated to version 1.5.0_06

--- trunk/unattended/install/scripts/sun-jre.bat
+++ trunk/unattended/install/scripts/sun-jre.bat
@@ -3,9 +3,18 @@
 :: Download from <http://java.sun.com/j2se/1.5.0/download.jsp>.
 
 :: See:
-:: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/upgrade-guide/deployment.html>
-:: <http://java.sun.com/j2se/1.5.0/docs/guide/deployment/deployment-guide/silent.html>
-:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=10209|packages/jre1.5.0_05.msi
+:: <http://www.java.com/en/download/manual.jsp>
+:: <http://java.com/en/download/help/silent_install.xml>
+:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=10351|packages/jre1.5.0_06.msi
 
 @Echo off
-todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_05.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre1.5.0_06.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
+
+:: Deinstall older Versions if you wish
+:: (last numbers schema according to above mentioned pages: 1.5.0_04 -> 15004 etc.):
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150010} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150020} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150030} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150040} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150050} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150060} REBOOT=Suppress"
