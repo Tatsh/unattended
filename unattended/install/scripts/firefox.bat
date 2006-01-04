b2b71c793c7affa39a6a83f826a84e41132c250d
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-01-04 17:08:58 +0000

    Updated some language versions to v1.5

--- unattended/install/scripts/firefox.bat
+++ unattended/install/scripts/firefox.bat
@@ -10,14 +10,17 @@
 :: URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/de/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-deu.exe
 :: URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/el/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-ell.exe
 :: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/en-US/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-enu.exe
+:: URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/en-GB/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-eng.exe
 :: URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/es-ES/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-esn.exe
 :: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/es-AR/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-ess.exe
 :: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/fi/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-fin.exe
 :: URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/fr/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-fra.exe
 :: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/he/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-heb.exe
+:: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/hu/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-hun.exe
 :: URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/it/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-ita.exe
 :: URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/ja/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-jpn.exe
 :: URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/ko/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-kor.exe
+:: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/mk/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-mki.exe
 :: URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/nl/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-nld.exe
 :: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/nb-NO/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-nor.exe
 :: URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/pl/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-plk.exe
@@ -26,14 +29,10 @@
 :: URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/ru/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-rus.exe
 :: URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/sl/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-slv.exe
 :: URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/sv-SE/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-sve.exe
+:: URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5/win32/tr/Firefox%20Setup%201.5.exe|packages/mozilla/firefox-15-trk.exe
 
 :: Firefox 1.0.7
-:: URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.7/win32/en-GB/Firefox%20Setup%201.0.7.exe|packages/mozilla/firefox-107-eng.exe
-:: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.7/win32/hu-HU/Firefox%20Setup%201.0.7.exe|packages/mozilla/firefox-107-hun.exe
-:: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.7/win32/mk-MK/Firefox%20Setup%201.0.7.exe|packages/mozilla/firefox-107-mki.exe
 :: URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.7/win32/pt-PT/Firefox%20Setup%201.0.7.exe|packages/mozilla/firefox-107-ptg.exe
-:: URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.7/win32/tr-TR/Firefox%20Setup%201.0.7.exe|packages/mozilla/firefox-107-trk.exe
-
 
 @Echo off
 
