c218955d2fca5d8416b0f574e0d6d2fa8c0a6f9d
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-02-02 21:00:46 +0000

    Updated to v5.13
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2342 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install WinAmp 5.11
+:: OPTIONAL: Install WinAmp 5.13
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp5111_full_emusic-7plus.exe|packages/winamp/winamp5111-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp513_full_emusic-7plus.exe|packages/winamp/winamp513-full.exe
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp5111-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp513-full.exe /S /install=SFQR"
