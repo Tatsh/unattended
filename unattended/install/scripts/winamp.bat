ef78f313c9cbf1536905a5af8057d7a78c41ac02
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-02-22 21:09:19 +0000

    Update to winamp 5.20

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install WinAmp 5.13
+:: OPTIONAL: Install WinAmp 5.2
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp513_full_emusic-7plus.exe|packages/winamp/winamp513-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp52_full_emusic-7plus.exe|packages/winamp/winamp520-full.exe
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp513-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp520-full.exe /S /install=SFQR"
