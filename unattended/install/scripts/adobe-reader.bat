b3d52787882df4f439aeed33dca1d49ff19d6356
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-03-19 17:29:11 +0000

    Added links for version 7.07
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2361 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/adobe-reader.bat
+++ unattended/install/scripts/adobe-reader.bat
@@ -1,23 +1,30 @@
-:: OPTIONAL: Install Adobe Reader 7.0.5
+:: OPTIONAL: Install Adobe Reader 7.0.x
 @Echo Off
 
-:: Download Adobe Reader 7.0.5 full version
-::URL|CHS|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/chs/AdbeRdr705_chs_full.exe|packages/adobereader/AdbeRdr705_chs_full.exe
-::URL|CHT|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/cht/AdbeRdr705_cht_full.exe|packages/adobereader/AdbeRdr705_cht_full.exe
-::URL|DAN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/dan/AdbeRdr705_dan_full.exe|packages/adobereader/AdbeRdr705_dan_full.exe
-::URL|DEU|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/deu/AdbeRdr705_deu_full.exe|packages/adobereader/AdbeRdr705_deu_full.exe
-::URL|ENU|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/enu/AdbeRdr705_enu_full.exe|packages/adobereader/AdbeRdr705_enu_full.exe
-::URL|ESP|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/esp/AdbeRdr705_esp_full.exe|packages/adobereader/AdbeRdr705_esp_full.exe
-::URL|FRA|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/fra/AdbeRdr705_fra_full.exe|packages/adobereader/AdbeRdr705_fra_full.exe
-::URL|ITA|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/ita/AdbeRdr705_ita_full.exe|packages/adobereader/AdbeRdr705_ita_full.exe
-::URL|JPN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/jpn/AdbeRdr705_jpn_full.exe|packages/adobereader/AdbeRdr705_jpn_full.exe
-::URL|KOR|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/kor/AdbeRdr705_kor_full.exe|packages/adobereader/AdbeRdr705_kor_full.exe
-::URL|NLD|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/nld/AdbeRdr705_nld_full.exe|packages/adobereader/AdbeRdr705_nld_full.exe
-::URL|NOR|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/nor/AdbeRdr705_nor_full.exe|packages/adobereader/AdbeRdr705_nor_full.exe
-::URL|PTB|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/ptb/AdbeRdr705_ptb_full.exe|packages/adobereader/AdbeRdr705_ptb_full.exe
-::URL|RUS|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/rus/AdbeRdr705_rus_full.exe|packages/adobereader/AdbeRdr705_rus_full.exe
-::URL|SUO|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/suo/AdbeRdr705_suo_full.exe|packages/adobereader/AdbeRdr705_suo_full.exe
-::URL|SVE|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/sve/AdbeRdr705_sve_full.exe|packages/adobereader/AdbeRdr705_sve_full.exe
+:: Download Adobe Reader 7.0.x full version
+:: URL|ARA|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/ara/AdbeRdr705_ara_full.exe|packages/adobereader/adberdr7_ara.exe
+:: URL|CHS|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/chs/AdbeRdr707_zh_CN.exe|packages/adobereader/adberdr7_chs.exe
+:: URL|CHT|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/cht/AdbeRdr707_zh_TW.exe|packages/adobereader/adberdr7_cht.exe
+:: URL|CSY|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/cze/AdbeRdr705_cze_full.exe|packages/adobereader/adberdr7_csy.exe
+:: URL|DAN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/dan/AdbeRdr707_da_DK.exe|packages/adobereader/adberdr7_dan.exe
+:: URL|NLD|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/nld/AdbeRdr707_nl_NL.exe|packages/adobereader/adberdr7_nld.exe
+:: URL|ENU|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/enu/AdbeRdr707_en_US.exe|packages/adobereader/adberdr7_enu.exe
+:: URL|FIN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/suo/AdbeRdr707_fi_FI.exe|packages/adobereader/adberdr7_fin.exe
+:: URL|FRA|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/fra/AdbeRdr707_fr_FR.exe|packages/adobereader/adberdr7_fra.exe
+:: URL|DEU|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/deu/AdbeRdr707_de_DE.exe|packages/adobereader/adberdr7_deu.exe
+:: URL|ELL|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/gre/AdbeRdr705_gre_full.exe|packages/adobereader/adberdr7_ell.exe
+:: URL|HEB|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/heb/AdbeRdr705_heb_full.exe|packages/adobereader/adberdr7_heb.exe
+:: URL|HUN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/hun/AdbeRdr705_hun_full.exe|packages/adobereader/adberdr7_hun.exe
+:: URL|ITA|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/ita/AdbeRdr707_it_IT.exe|packages/adobereader/adberdr7_ita.exe
+:: URL|JPN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/jpn/AdbeRdr707_ja_JP.exe|packages/adobereader/adberdr7_jpn.exe
+:: URL|KOR|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/kor/AdbeRdr707_ko_KR.exe|packages/adobereader/adberdr7_kor.exe
+:: URL|NOR|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/nor/AdbeRdr707_no_NO.exe|packages/adobereader/adberdr7_nor.exe
+:: URL|PLK|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/pol/AdbeRdr705_pol_full.exe|packages/adobereader/adberdr7_plk.exe
+:: URL|PTB|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/ptb/AdbeRdr707_pt_BR.exe|packages/adobereader/adberdr7_ptb.exe
+:: URL|RUS|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/rus/AdbeRdr705_rus_full.exe|packages/adobereader/adberdr7_rus.exe
+:: URL|ESN|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/esp/AdbeRdr707_es_ES.exe|packages/adobereader/adberdr7_esn.exe
+:: URL|SVE|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.7/sve/AdbeRdr707_sv_SE.exe|packages/adobereader/adberdr7_sve.exe
+:: URL|TRK|http://ardownload.adobe.com/pub/adobe/reader/win/7x/7.0.5/tur/AdbeRdr705_tur_full.exe|packages/adobereader/adberdr7_trk.exe
 
 :: Download Adobe Reader 7.0.5 Language Support
 ::URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/7x/7.0.5/misc/Acrd705SD.exe|packages/adobereader/misc/Acrd705SD.exe
@@ -32,8 +39,13 @@
 :: Download Adobe Reader Korean Fonts
 ::URL|KOR|ftp://ftp.adobe.com/pub/adobe/reader/win/7x/7.0.5/misc/alf_kor.exe|packages/adobereader/misc/alf_kor.exe
 
-if not exist %Z%\packages\adobereader\AdbeRdr705_%WINLANG%_full.exe goto noadberdr
+set adobe_install=%Z%\packages\adobereader\AdbeRdr707_%WINLANG%_full.exe
+if exist %adobe_install% goto found_install
 
+set adobe_install=%Z%\packages\adobereader\AdbeRdr705_%WINLANG%_full.exe
+if not exist %adobe_install% goto noadberdr
+
+:found_install
 ::--------------------------------------------------------------------------
 :: N.B. Edit this section for a correct selection of Adobe Reader Fonts
 ::--------------------------------------------------------------------------
@@ -74,11 +86,11 @@ todo.pl "%Z%\packages\adobereader\misc\Acrd705SD.exe /S /v/qb"
 :: delete the temporary folder.
 todo.pl "rmdir /s /q \"%TEMP%\adberdr\""
 todo.pl "for %%m in (\"%TEMP%\adberdr\Adobe Reader 7.0*.msi\") do msiexec /qb /l* %SystemDrive%\netinst\logs\adobe-reader.txt /i \"%%m\" REBOOT=ReallySuppress EULA_ACCEPT=YES"
-todo.pl ".ignore-err 131 %Z%\packages\adobereader\AdbeRdr705_%WINLANG%_full.exe /S -nos_ne -nos_o\"%TEMP%\adberdr\""
+todo.pl ".ignore-err 131 %adobe_install% /S -nos_ne -nos_o\"%TEMP%\adberdr\""
 goto done
 
 :noadberdr
-@echo adobe-reader.bat: No Installation File found for Adobe Reader 7.0
-@echo adobe-reader.bat: No Installation File found for Adobe Reader 7.0 1>>%SystemDrive%\netinst\logs\not-installed.txt
+@echo adobe-reader.bat: No Installation File found for Adobe Reader 7.0x
+@echo adobe-reader.bat: No Installation File found for Adobe Reader 7.0x 1>>%SystemDrive%\netinst\logs\not-installed.txt
 
 :done
