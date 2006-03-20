435f99f9f291489ebac87c03171d49687ad9f26b
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-03-20 19:01:46 +0000

    Updated to v5.21

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install WinAmp 5.2
+:: OPTIONAL: Install WinAmp 5.21
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp52_full_emusic-7plus.exe|packages/winamp/winamp520-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp521_full_emusic-7plus.exe|packages/winamp/winamp521-full.exe
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp520-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp521-full.exe /S /install=SFQR"
