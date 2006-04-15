b050098b07ebed587d7227e67106dd79fcb846fd
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-04-15 14:56:41 +0000

    Updated languages to v0.9
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2367 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/pdfcreator.bat
+++ unattended/install/scripts/pdfcreator.bat
@@ -2,22 +2,20 @@
 @echo off
 
 :: Install language files
-:: URL|CAT|http://dl.sourceforge.net/sourceforge/pdfcreator/catalan.ini|packages/pdfcreator/lang-cat-0_8_0.ini
-:: URL|CHS|http://dl.sourceforge.net/sourceforge/pdfcreator/simplified_chinese.ini|packages/pdfcreator/lang-chs-0_8_0.ini
-:: URL|CSY|http://dl.sourceforge.net/sourceforge/pdfcreator/czech.ini|packages/pdfcreator/lang-csy-0_8_0.ini
-:: URL|DAN|http://dl.sourceforge.net/sourceforge/pdfcreator/danish.ini|packages/pdfcreator/lang-dan-0_8_0.ini
-:: URL|ESN|http://dl.sourceforge.net/sourceforge/pdfcreator/espanol.ini|packages/pdfcreator/lang-esn-0_8_0.ini
-:: URL|FRA|http://dl.sourceforge.net/sourceforge/pdfcreator/french.ini|packages/pdfcreator/lang-fra-0_8_0.ini
-:: URL|HUN|http://dl.sourceforge.net/sourceforge/pdfcreator/hungarian.ini|packages/pdfcreator/lang-hun-0_8_0.ini
-:: URL|ITA|http://dl.sourceforge.net/sourceforge/pdfcreator/italiano.ini|packages/pdfcreator/lang-ita-0_8_0.ini
-:: URL|NLD|http://dl.sourceforge.net/sourceforge/pdfcreator/dutch.ini|packages/pdfcreator/lang-nld-0_8_0.ini
-:: URL|NOR|http://dl.sourceforge.net/sourceforge/pdfcreator/norwegian.ini|packages/pdfcreator/lang-nor-0_8_0.ini
-:: URL|PLK|http://dl.sourceforge.net/sourceforge/pdfcreator/polish.ini|packages/pdfcreator/lang-plk-0_8_0.ini
-:: URL|RUS|http://dl.sourceforge.net/sourceforge/pdfcreator/russian.ini|packages/pdfcreator/lang-rus-0_8_0.ini
-:: URL|SKY|http://dl.sourceforge.net/sourceforge/pdfcreator/slovensko.ini|packages/pdfcreator/lang-sky-0_8_0.ini
-:: URL|UKR|http://dl.sourceforge.net/sourceforge/pdfcreator/ukrainian.ini|packages/pdfcreator/lang-ukr-0_8_0.ini
-if not exist %Z%\packages\pdfcreator\lang-%WINLANG%-0_8_0.ini goto nolangpack
-todo.pl "copy %Z%\packages\pdfcreator\lang-%WINLANG%-0_8_0.ini \"%ProgramFiles%\PDFCreator\languages\""
+:: URL|CSY|http://dl.sourceforge.net/sourceforge/pdfcreator/czech.ini|packages/pdfcreator/lang-csy-0_9_0.ini
+:: URL|DEU|http://dl.sourceforge.net/sourceforge/pdfcreator/german.ini|packages/pdfcreator/lang-deu-0_9_0.ini
+:: URL|DAN|http://dl.sourceforge.net/sourceforge/pdfcreator/danish.ini|packages/pdfcreator/lang-dan-0_9_0.ini
+:: URL|FRA|http://dl.sourceforge.net/sourceforge/pdfcreator/french.ini|packages/pdfcreator/lang-fra-0_9_0.ini
+:: URL|HUN|http://dl.sourceforge.net/sourceforge/pdfcreator/hungarian.ini|packages/pdfcreator/lang-hun-0_9_0.ini
+:: URL|ITA|http://dl.sourceforge.net/sourceforge/pdfcreator/italiano.ini|packages/pdfcreator/lang-ita-0_9_0.ini
+:: URL|NLD|http://dl.sourceforge.net/sourceforge/pdfcreator/dutch.ini|packages/pdfcreator/lang-nld-0_9_0.ini
+:: URL|NOR|http://dl.sourceforge.net/sourceforge/pdfcreator/norwegian.ini|packages/pdfcreator/lang-nor-0_9_0.ini
+:: URL|PLK|http://dl.sourceforge.net/sourceforge/pdfcreator/polish.ini|packages/pdfcreator/lang-plk-0_9_0.ini
+:: URL|PTB|http://dl.sourceforge.net/sourceforge/pdfcreator/portuguesebr.ini|packages/pdfcreator/lang-ptb-0_9_0.ini
+:: URL|RUS|http://dl.sourceforge.net/sourceforge/pdfcreator/russian.ini|packages/pdfcreator/lang-rus-0_9_0.ini
+:: URL|SKY|http://dl.sourceforge.net/sourceforge/pdfcreator/slovensko.ini|packages/pdfcreator/lang-sky-0_9_0.ini
+if not exist %Z%\packages\pdfcreator\lang-%WINLANG%-0_9_0.ini goto nolangpack
+todo.pl "copy %Z%\packages\pdfcreator\lang-%WINLANG%-0_9_0.ini \"%ProgramFiles%\PDFCreator\languages\""
 :nolangpack
 
 :: Install PDFCreator
