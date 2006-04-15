79e85b282fb1e7aac4621154690735ace4d10c00
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-04-15 14:43:24 +0000

    Updated to v1.5.0.2

--- unattended/install/scripts/firefox.bat
+++ unattended/install/scripts/firefox.bat
@@ -1,54 +1,54 @@
 :: OPTIONAL: Install Firefox
 
-:: Firefox 1.5.0.1
-:: URL|ARA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/ar/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-ara.exe
-:: URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/ca/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-cat.exe
-:: URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/cs/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-csy.exe
-:: URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/zh-TW/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-cht.exe
-:: URL|CHS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/zh-CN/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-chs.exe
-:: URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/da/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-dan.exe
-:: URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/de/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-deu.exe
-:: URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/el/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-ell.exe
-:: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/en-US/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-enu.exe
-:: URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/en-GB/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-eng.exe
-:: URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/es-ES/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-esn.exe
-:: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/es-AR/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-ess.exe
-:: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/fi/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-fin.exe
-:: URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/fr/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-fra.exe
-:: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/he/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-heb.exe
-:: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/hu/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-hun.exe
-:: URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/it/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-ita.exe
-:: URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/ja/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-jpn.exe
-:: URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/ko/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-kor.exe
-:: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/mk/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-mki.exe
-:: URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/nl/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-nld.exe
-:: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/nb-NO/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-nor.exe
-:: URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/pl/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-plk.exe
-:: URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/pt-BR/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-ptb.exe
-:: URL|ROM|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/ro/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-rom.exe
-:: URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/ru/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-rus.exe
-:: URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/sl/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-slv.exe
-:: URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/sv-SE/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-sve.exe
-:: URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.1/win32/tr/Firefox%20Setup%201.5.0.1.exe|packages/mozilla/firefox-1501-trk.exe
+:: Firefox 1.5.0.2
+:: URL|ARA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/ar/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-ara.exe
+:: URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/ca/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-cat.exe
+:: URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/cs/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-csy.exe
+:: URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/zh-TW/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-cht.exe
+:: URL|CHS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/zh-CN/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-chs.exe
+:: URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/da/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-dan.exe
+:: URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/de/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-deu.exe
+:: URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/el/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-ell.exe
+:: URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/en-US/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-enu.exe
+:: URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/en-GB/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-eng.exe
+:: URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/es-ES/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-esn.exe
+:: URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/es-AR/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-ess.exe
+:: URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/fi/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-fin.exe
+:: URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/fr/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-fra.exe
+:: URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/he/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-heb.exe
+:: URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/hu/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-hun.exe
+:: URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/it/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-ita.exe
+:: URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/ja/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-jpn.exe
+:: URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/ko/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-kor.exe
+:: URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/mk/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-mki.exe
+:: URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/nl/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-nld.exe
+:: URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/nb-NO/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-nor.exe
+:: URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/pl/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-plk.exe
+:: URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/pt-BR/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-ptb.exe
+:: URL|ROM|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/ro/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-rom.exe
+:: URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/ru/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-rus.exe
+:: URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/sl/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-slv.exe
+:: URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/sv-SE/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-sve.exe
+:: URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.5.0.2/win32/tr/Firefox%20Setup%201.5.0.2.exe|packages/mozilla/firefox-1502-trk.exe
 
-:: Firefox 1.0.7
-:: URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.7/win32/pt-PT/Firefox%20Setup%201.0.7.exe|packages/mozilla/firefox-107-ptg.exe
+:: Firefox 1.0.8
+:: URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/1.0.8/win32/pt-PT/Firefox%20Setup%201.0.8.exe|packages/mozilla/firefox-108-ptg.exe
 
 @Echo off
 
 :: detect current patchlevel for your language
-if exist "%Z%\packages\mozilla\firefox-1501-%WINLANG%.exe" goto v1501
-goto v1501
-if exist "%Z%\packages\mozilla\firefox-107-%WINLANG%.exe" goto v107
-goto v107
+if exist "%Z%\packages\mozilla\firefox-1502-%WINLANG%.exe" goto v1502
+goto v1502
+if exist "%Z%\packages\mozilla\firefox-108-%WINLANG%.exe" goto v108
+goto v108
 
 
-:v1501
-todo.pl "%Z%\packages\mozilla\firefox-1501-%WINLANG%.exe -ms -ira"
+:v1502
+todo.pl "%Z%\packages\mozilla\firefox-1502-%WINLANG%.exe -ms -ira"
 goto end_of_script
 
-:v107
-todo.pl "%Z%\packages\mozilla\firefox-107-%WINLANG%.exe -ms -ira"
+:v108
+todo.pl "%Z%\packages\mozilla\firefox-108-%WINLANG%.exe -ms -ira"
 goto end_of_script
 
 
