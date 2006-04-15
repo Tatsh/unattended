af384b021b692aae41d2c93bfb8fe4f2bc0a814d
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-04-15 14:32:15 +0000

    Updated to v8.0.24.0

--- unattended/install/scripts/flash.bat
+++ unattended/install/scripts/flash.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL: Install Flash 8.0.22.0 
+:: OPTIONAL: Install Flash 8.0.24.0 
 ::
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_8_0_24.exe
 ::
 :: Download from <http://www.macromedia.com/downloads/>
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player.exe /s"
+todo.pl "%Z%\packages\macromedia\install_flash_player_8_0_24.exe /s"
