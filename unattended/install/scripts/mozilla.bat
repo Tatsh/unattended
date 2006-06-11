49b6d1f26f57ba39e914a5e5f016a1656c8ace0e
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-06-11 06:59:15 +0000

    Updated to v1.7.13

--- unattended/install/scripts/mozilla.bat
+++ unattended/install/scripts/mozilla.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Mozilla
-:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.12/mozilla-win32-1.7.12-installer.exe|packages/mozilla/mozilla-1712-all.exe
+:: URL|ALL|http://ftp.mozilla.org/pub/mozilla.org/mozilla/releases/mozilla1.7.13/mozilla-win32-1.7.13-installer.exe|packages/mozilla/mozilla-1713-all.exe
 
 @Echo off
-todo.pl "%Z%\packages\mozilla\mozilla-1712-all.exe -ma -ira"
+todo.pl "%Z%\packages\mozilla\mozilla-1713-all.exe -ma -ira"
