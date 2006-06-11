ce68de374429b3b1ae5173de24bd769835fd31f3
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-06-11 07:05:36 +0000

    Updated to v5.1.4
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2381 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/php5.bat
+++ unattended/install/scripts/php5.bat
@@ -1,4 +1,4 @@
 :: Install PHP5
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.1.2-installer.exe|packages/php-5.1.2-installer.exe
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.1.4-installer.exe|packages/php-5.1.4-installer.exe
 @Echo off
-todo.pl "%Z%\packages\php-5.1.2-installer.exe /s"
+todo.pl "%Z%\packages\php-5.1.4-installer.exe /s"
