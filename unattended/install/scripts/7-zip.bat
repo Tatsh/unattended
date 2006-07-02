dcdc8076bdfdf96bb45d019e05d0b9fc063869f1
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-07-02 12:07:47 +0000

    Move package to appropriate subdirectory.

--- unattended/install/scripts/7-zip.bat
+++ unattended/install/scripts/7-zip.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install 7-Zip
-:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z442.msi|packages/7z442.msi
+:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z442.msi|packages/sevenzip/7z442.msi
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\7z442.msi REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\sevenzip\7z442.msi REBOOT=ReallySuppress"
 :done
--- unattended/install/scripts/att.bat
+++ unattended/install/scripts/att.bat
@@ -1,4 +1,4 @@
 :: Install AT&T Global Network Dialer
-:: URL|ALL|ftp://ftp.attglobal.net/pub/client/win32/d5097.exe|packages/d5097.exe
+:: URL|ALL|ftp://ftp.attglobal.net/pub/client/win32/d5097.exe|packages/att/d5097.exe
 @Echo off
-todo.pl "%Z%\packages\d5097.exe /s"
+todo.pl "%Z%\packages\att\d5097.exe /s"
