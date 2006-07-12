5e5a0823f9325e679e6cf9c0aaa544728bb75249
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-07-12 14:57:39 +0000

    Updated KB890830 to v1.18.

--- unattended/install/scripts/winxpsp1-updates.bat
+++ unattended/install/scripts/winxpsp1-updates.bat
@@ -1401,8 +1401,8 @@ todo.pl ".reboot-on 194 %Z%\updates\winxpsp1\WindowsXP-KB896423-x86-%WINLANG%.ex
 :: Microsoft Windows Malicious Software Removal Tool (KB890830)
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.17.exe|updates/common/windows-kb890830-v1.17.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V1.17.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.18.exe|updates/common/windows-kb890830-v1.18.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V1.18.exe /Q"
 
 :: Security Update for JView Profiler (KB903235)
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=2A506C16-01EF-4060-BCF8-6993C55840A9>
