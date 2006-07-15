36ad3720f25396889aef6b7c452cb89446822334
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-07-15 06:38:53 +0000

    Updated to v9.0.16.0

--- unattended/install/scripts/flash.bat
+++ unattended/install/scripts/flash.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL: Install Flash 8.0.24.0 
+:: OPTIONAL: Install Flash 9.0.16.0 
 ::
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_8_0_24.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_9_0_16.exe
 ::
 :: Download from <http://www.macromedia.com/downloads/>
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_8_0_24.exe /s"
+todo.pl "%Z%\packages\macromedia\install_flash_player_9_0_16.exe /s"
