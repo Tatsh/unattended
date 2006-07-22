7ec7e7eb45e62226cef965b1265e99660fd340dd
slords <slords@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-07-22 20:16:59 +0000

    Update to 2.0.3

--- trunk/unattended/install/scripts/officeop.bat
+++ trunk/unattended/install/scripts/officeop.bat
@@ -1,15 +1,11 @@
 :: OPTIONAL: Install OpenOffice.org
 
-:: URL|CHS|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/zh-cn/1.1.5/OOo_1.1.5_Win32Intel_install_zh-CN.zip|packages/openoffice/chs/ooo_1.1.5_chs.zip
-:: URL|CHT|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/zh-tw/1.1.5/OOo_1.1.5_Win32Intel_install_zh-TW.zip|packages/openoffice/cht/ooo_1.1.5_cht.zip
-:: URL|DAN|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/da/1.1.3/OOo_1.1.3_Win32Intel_install_da.zip|packages/openoffice/dan/ooo_1.1.3_dan.zip
-:: URL|DEU|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/de/1.1.5/OOo_1.1.5_Win32Intel_install_de.zip|packages/openoffice/deu/ooo_1.1.5_deu.zip
-:: URL|ENU|http://mirrors.ibiblio.org/pub/mirrors/openoffice/stable/1.1.5/OOo_1.1.5_Win32Intel_install.zip|packages/openoffice/enu/ooo_1.1.5_enu.zip
-:: URL|FRA|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/fr/1.1.5/OOo_1.1.5_Win32Intel_install_fr.zip|packages/openoffice/fra/ooo_1.1.5_fra.zip
-:: URL|ITA|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/it/1.1.3/OOo_1.1.3_Win32Intel_install_it.zip|packages/openoffice/ita/ooo_1.1.3_ita.zip
-:: URL|JPN|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/ja/1.1.5/OOo_1.1.5_Win32Intel_install_ja.zip|packages/openoffice/jpn/ooo_1.1.5_jpn.zip
-:: URL|NLD|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/nl/1.1.5/OOo_1.1.5_Win32Intel_install_nl.zip|packages/openoffice/nld/ooo_1.1.5_nld.zip
-:: URL|RUS|http://download.i-rs.ru/pub/openoffice/1.1.5/ru/OOo_1.1.5_ru_RU_WinIntel_install.zip|packages/openoffice/rus/ooo_1.1.5_rus.zip
+:: URL|DEU|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/de/2.0.3/OOo_2.0.3_Win32Intel_install_de_wJRE.exe|packages/openoffice/ooo_2.0.3_win32intel_install_deu_wjre.exe
+:: URL|ENU|http://mirrors.ibiblio.org/pub/mirrors/openoffice/stable/2.0.3/OOo_2.0.3_Win32Intel_install_wJRE.exe|packages/openoffice/ooo_2.0.3_win32intel_install_enu_wjre.exe
+:: URL|FRA|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/fr/2.0.3/OOo_2.0.3_Win32Intel_install_fr_wJRE.exe|packages/openoffice/ooo_2.0.3_win32intel_install_fra_wjre.exe
+:: URL|ITA|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/it/2.0.3/OOo_2.0.3_Win32Intel_install_it_wJRE.exe|packages/openoffice/ooo_2.0.3_win32intel_install_ita_wjre.exe
+:: URL|JPN|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/ja/2.0.3/OOo_2.0.3_Win32Intel_install_ja_wJRE.exe|packages/openoffice/ooo_2.0.3_win32intel_install_jpn_wjre.exe
+:: URL|NLD|http://mirrors.ibiblio.org/pub/mirrors/openoffice/localized/nl/2.0.3/OOo_2.0.3_Win32Intel_install_nl_wJRE.exe|packages/openoffice/ooo_2.0.3_win32intel_install_nl_wjre.exe
 
 
 @Echo off
@@ -17,4 +13,4 @@ todo.pl "shortcut.pl \"%ProgramFiles%\"\OpenOffice.org\program\soffice.exe speci
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\OpenOffice.org\program\soffice.exe special:AllUsersPrograms\OpenOffice"
 
-todo.pl "%Z%\packages\OpenOffice\%WINLANG%\setup.exe -r:%Z%\packages\OpenOffice\officeop.txt -D:\"%ProgramFiles%\OpenOffice.org\" -debug -net"
+todo.pl "%Z%\packages\OpenOffice\ooo_2.0.3_win32intel_install_%WINLANG%_wjre.exe -r:%Z%\packages\OpenOffice\officeop.txt -D:\"%ProgramFiles%\OpenOffice.org\" -debug -net"
