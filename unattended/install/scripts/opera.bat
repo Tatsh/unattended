2c3d13c0f3d985a23f210f580532322ed5bde2d5
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-08-14 19:43:22 +0000

    Updated to v9.01

--- unattended/install/scripts/opera.bat
+++ unattended/install/scripts/opera.bat
@@ -1,9 +1,9 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/900/int/Opera%209%20International%20Setup.exe|packages/opera/opera-900-int.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/901/int/Opera_9.01_International_Setup.exe|packages/opera/opera-901-int.exe
 @Echo off
 
 :: if you like to do special things (e.g. define custom searches), save your customized ini's in packages/opera/special
 
-%Z%\packages\opera\opera-900-int.exe /S /v/qn
+%Z%\packages\opera\opera-901-int.exe /S /v/qn
 
 if exist "%Z%\packages\opera\special\*" copy "%Z%\packages\opera\special\*" "%ProgramFiles%\Opera"
