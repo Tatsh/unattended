c8925cbbc30a225b554cf4e445f60a0c6fabeeaf
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-08-14 19:49:48 +0000

    Updated to v4.4.3

--- unattended/install/scripts/php.bat
+++ unattended/install/scripts/php.bat
@@ -1,4 +1,4 @@
 :: Install PHP4
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-4.4.2-installer.exe|packages/php/php-4.4.2-installer.exe
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-4.4.3-installer.exe|packages/php/php-4.4.3-installer.exe
 @Echo off
-todo.pl "%Z%\packages\php\php-4.4.2-installer.exe /s"
+todo.pl "%Z%\packages\php\php-4.4.3-installer.exe /s"
