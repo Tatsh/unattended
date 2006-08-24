1880646820edd3dc74500cd30662e7cdd737efb6
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-08-24 19:25:51 +0000

    Added support for ini file values containing ( and ) characters

--- trunk/unattended/install/lib/Unattend/IniFile.pm
+++ trunk/unattended/install/lib/Unattend/IniFile.pm
@@ -247,7 +247,7 @@ sub merge ($$) {
 # Characters needing no quotes on output
 my $nq_out_chars = qr{[a-zA-Z0-9_.,<>:/~%*\-\\\$]};
 # Characters needing no quotes on input
-my $nq_in_chars = qr{(?:$nq_out_chars|[ \x80-\xFF])};
+my $nq_in_chars = qr{(?:$nq_out_chars|[() \x80-\xFF])};
 
 # Regexp matching a single token (key or value)
 my $token = qr{(?:\".*?\"|$nq_in_chars+?)};
