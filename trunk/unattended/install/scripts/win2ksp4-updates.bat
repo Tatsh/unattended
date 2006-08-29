904010938bc2224a549458ac3d512a788e90c8a1
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-08-29 18:02:25 +0000

    Updated KB890830 to v1.19.

--- trunk/unattended/install/scripts/win2ksp4-updates.bat
+++ trunk/unattended/install/scripts/win2ksp4-updates.bat
@@ -34,8 +34,8 @@ todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB909520-v1.000-x86-%WINLANG%
 :: Microsoft Windows Malicious Software Removal Tool (KB890830)
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.18.exe|updates/common/windows-kb890830-v1.18.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V1.18.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.19.exe|updates/common/windows-kb890830-v1.19.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V1.19.exe /Q"
 
 :: Security Update for Windows Media Player 9 (KB917734)
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=c00be4c3-34ba-4858-90d7-520b7d240e33>
