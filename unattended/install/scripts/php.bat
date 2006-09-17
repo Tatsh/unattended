1f03bfadb3c4877432c5fd3bc34970390009127d
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-09-17 20:39:31 +0000

    Updated to v4.4.4
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2460 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/php.bat
+++ unattended/install/scripts/php.bat
@@ -1,4 +1,4 @@
 :: Install PHP4
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-4.4.3-installer.exe|packages/php/php-4.4.3-installer.exe
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-4.4.4-installer.exe|packages/php/php-4.4.4-installer.exe
 @Echo off
-todo.pl "%Z%\packages\php\php-4.4.3-installer.exe /s"
+todo.pl "%Z%\packages\php\php-4.4.4-installer.exe /s"
