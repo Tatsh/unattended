2d33d9ad526a23f6af751e9aa7f50cf3ca58eff2
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-09-17 20:24:05 +0000

    Update to v5.1.5

--- unattended/install/scripts/php5.bat
+++ unattended/install/scripts/php5.bat
@@ -1,4 +1,4 @@
 :: Install PHP5
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.1.4-installer.exe|packages/php/php-5.1.4-installer.exe
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.1.5-installer.exe|packages/php/php-5.1.5-installer.exe
 @Echo off
-todo.pl "%Z%\packages\php\php-5.1.4-installer.exe /s"
+todo.pl "%Z%\packages\php\php-5.1.5-installer.exe /s"
