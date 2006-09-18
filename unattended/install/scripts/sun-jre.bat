0ab15881e96c0c10b5f7b55e92032f3da00655c0
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-09-18 16:35:15 +0000

    Updated to v1.5.0_08
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2461 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sun-jre.bat
+++ unattended/install/scripts/sun-jre.bat
@@ -5,10 +5,10 @@
 :: See:
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://java.com/en/download/help/silent_install.xml>
-:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=10550|packages/jre/jre1.5.0_07.msi
+:: URL|ALL|http://jdl.sun.com/webapps/download/AutoDL?BundleId=10650|packages/jre/jre1.5.0_08.msi
 
 @Echo off
-todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre\jre1.5.0_07.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre\jre1.5.0_08.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
 
 :: Deinstall older Versions if you wish
 :: (last numbers schema according to above mentioned pages: 1.5.0_04 -> 15004 etc.):
@@ -19,3 +19,4 @@ todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /
 :: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150050} REBOOT=Suppress"
 :: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150060} REBOOT=Suppress"
 :: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150070} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150080} REBOOT=Suppress"
