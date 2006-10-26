d8cc0b5eafb15fd3fdbca959b1d37edc79ce6cc6
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-10-26 14:10:22 +0000

    Update to v5.31.

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install WinAmp 5.24
+:: OPTIONAL: Install WinAmp 5.31
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp524_full_emusic-7plus.exe|packages/winamp/winamp524-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp531_full_emusic-7plus.exe|packages/winamp/winamp531-full.exe
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp524-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp531-full.exe /S /install=SFQR"
