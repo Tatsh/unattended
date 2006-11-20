d8118263704c2b57f84360eb6403c35fe6cf14a7
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2006-11-20 17:18:49 +0000

    Updated to v9.0.28, thanks to Andreas Heinlein
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2494 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/flash.bat
+++ unattended/install/scripts/flash.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL: Install Flash 9.0.16.0 
+:: OPTIONAL: Install Flash 9.0.28.0 
 ::
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_9_0_16.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_9_0_28.exe
 ::
 :: Download from <http://www.macromedia.com/downloads/>
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_9_0_16.exe /s"
+todo.pl "%Z%\packages\macromedia\install_flash_player_9_0_28.exe /s"
