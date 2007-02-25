fb41d79cf35b950544dbf90b287bc278c685df30
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-02-25 16:57:54 +0000

    Updated KB890830 to v.1.26
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2511 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winxpsp2-updates.bat
+++ unattended/install/scripts/winxpsp2-updates.bat
@@ -2377,7 +2377,7 @@ todo.pl ".reboot-on 194 %Z%\updates\common\WindowsXP-KB888302-x86-%WINLANG%.exe 
 :: Microsoft Windows Malicious Software Removal Tool (KB890830)
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.24.exe|updates/common/windows-kb890830-v1.24.exe
+:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.26.exe|updates/common/windows-kb890830-v1.26.exe
 todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V1.24.exe /Q"
 
 :: Security Update for Windows XP (KB890175)
