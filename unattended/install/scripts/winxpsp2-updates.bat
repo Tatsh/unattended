7b5537de23d495604e461a35a6cf48ffefeff88b
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-02-26 23:17:27 +0000

    Fix bug affecting KB890830 v1.26. Thanks to Matthias Bertschy.

--- unattended/install/scripts/winxpsp2-updates.bat
+++ unattended/install/scripts/winxpsp2-updates.bat
@@ -2378,7 +2378,7 @@ todo.pl ".reboot-on 194 %Z%\updates\common\WindowsXP-KB888302-x86-%WINLANG%.exe 
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
 :: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V1.26.exe|updates/common/kb890830/windows-kb890830-v1.26.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\kb890830\Windows-KB890830-V1.24.exe /Q"
+todo.pl ".reboot-on 194 %Z%\updates\common\kb890830\Windows-KB890830-V1.26.exe /Q"
 
 :: Security Update for Windows XP (KB890175)
 :: Microsoft Security Bulletin MS05-001
