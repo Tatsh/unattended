53d215ef21d974ec052e85054fd3e80fa37e8195
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-02-28 19:26:05 +0000

    Updated to v5.2.1

--- trunk/unattended/install/scripts/php5.bat
+++ trunk/unattended/install/scripts/php5.bat
@@ -1,4 +1,4 @@
 :: Install PHP5
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.2.0-win32-installer.msi|packages/php/php-5.2.0-win32-installer.msi
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.2.1-win32-installer.msi|packages/php/php-5.2.1-win32-installer.msi
 @Echo off
-todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\php5.txt /i %Z%\packages\php\php-5.2.0-win32-installer.msi"
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\php5.txt /i %Z%\packages\php\php-5.2.1-win32-installer.msi"
