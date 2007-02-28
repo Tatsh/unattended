8c24d6dd1a7709a9288101cdbc0d40257c6191ff
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-02-28 19:31:19 +0000

    Updated to v0.59
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2516 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/putty.bat
+++ unattended/install/scripts/putty.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install Putty 0.58
-:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.58-installer.exe|packages/putty/putty-0.58-installer.exe
+:: OPTIONAL: Install Putty 0.59
+:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.59-installer.exe|packages/putty/putty-0.59-installer.exe
 @Echo off
 
-todo.pl "%Z%\packages\putty\putty-0.58-installer.exe /sp- /silent"
+todo.pl "%Z%\packages\putty\putty-0.59-installer.exe /sp- /silent"
