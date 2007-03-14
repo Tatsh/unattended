9b560b8816e92a0f1a2875bea3509d899f051014
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-03-14 21:15:47 +0000

    Updated to v1.5.0_11

--- trunk/unattended/install/scripts/sun-jre.bat
+++ trunk/unattended/install/scripts/sun-jre.bat
@@ -5,18 +5,12 @@
 :: See:
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://java.com/en/download/help/silent_install.xml>
-:: URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=10881|packages/jre/jre1.5.0_10.msi
+:: URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=11029|packages/jre/jre1.5.0_11.msi
 
 @Echo off
-todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre\jre1.5.0_10.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\sun-jre.txt /i %Z%\packages\jre\jre1.5.0_11.msi IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
 
 :: Deinstall older Versions if you wish
-:: (last numbers schema according to above mentioned pages: 1.5.0_04 -> 15004 etc.):
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150010} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150020} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150030} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150040} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150050} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150060} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150070} REBOOT=Suppress"
-:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150080} REBOOT=Suppress"
+:: (last numbers schema according to above mentioned pages: 1.5.0_09 -> 15009 etc.):
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150090} REBOOT=Suppress"
+:: todo.pl ".ignore-err 69 msiexec.exe /qn /x {3248F0A8-6813-11D6-A77B-00B0D0150100} REBOOT=Suppress"
