6e4e6e97b734615e497826338b084dca87dad758
nrichthof <nrichthof@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-05-07 18:17:51 +0000

    Update to v. 5.34, include patch for in_mp4.dll
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2554 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,5 +1,7 @@
-:: OPTIONAL: Install WinAmp 5.31
+:: OPTIONAL: Install WinAmp 5.34
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp531_full_emusic-7plus.exe|packages/winamp/winamp531-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp534_full_emusic-7plus.exe|packages/winamp/winamp534-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/wa5update.exe|packages/winamp/wa5update-20070507.exe
+todo.pl "%Z%\packages\winamp\wa5update-20070507.exe /S /install=SFQR"
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
 todo.pl "%Z%\packages\winamp\winamp531-full.exe /S /install=SFQR"
