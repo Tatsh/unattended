064795073c85f7a55eb730964207b2a5d0d09405
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-06-23 10:39:35 +0000

    Updated to v5.35

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,7 +1,6 @@
-:: OPTIONAL: Install WinAmp 5.34
+:: OPTIONAL: Install WinAmp 5.35
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp534_full_emusic-7plus.exe|packages/winamp/winamp534-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp535_full_emusic-7plus.exe|packages/winamp/winamp535-full.exe
 :: URL|ALL|http://download.nullsoft.com/winamp/client/wa5update.exe|packages/winamp/wa5update-20070507.exe
-todo.pl "%Z%\packages\winamp\wa5update-20070507.exe /S /install=SFQR"
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp531-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp535-full.exe /S /install=SFQR"
