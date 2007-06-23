16c48f4b6c8ff167cac9c29a87df8e266fd03724
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-06-23 10:08:48 +0000

    Update to v9.21

--- trunk/unattended/install/scripts/opera.bat
+++ trunk/unattended/install/scripts/opera.bat
@@ -1,9 +1,9 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/920/int/Opera_9.20_International_Setup.exe|packages/opera/opera-920-int.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/921/int/Opera_9.21_International_Setup.exe|packages/opera/opera-921-int.exe
 @Echo off
 
 :: if you like to do special things (e.g. define custom searches), save your customized ini's in packages/opera/special
 
-%Z%\packages\opera\opera-920-int.exe /S /v/qn
+%Z%\packages\opera\opera-921-int.exe /S /v/qn
 
 if exist "%Z%\packages\opera\special\*" copy "%Z%\packages\opera\special\*" "%ProgramFiles%\Opera"
