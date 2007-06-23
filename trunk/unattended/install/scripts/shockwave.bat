b7930fbd61d6dbe506a05ba74af443fcf0ffcb01
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-06-23 11:06:09 +0000

    Update to v10.2.0.021

--- trunk/unattended/install/scripts/shockwave.bat
+++ trunk/unattended/install/scripts/shockwave.bat
@@ -1,5 +1,5 @@
 :: Install Shockwave10 player
-::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.1.3.018/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full.exe
+::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.2.0.021/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full.exe
 
 @Echo off
 
