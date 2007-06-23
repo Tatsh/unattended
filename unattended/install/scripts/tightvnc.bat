311e8da0bc8ddffe6162c87da73190468279bfbb
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-06-23 10:57:37 +0000

    Updated to v1.3.9

--- unattended/install/scripts/tightvnc.bat
+++ unattended/install/scripts/tightvnc.bat
@@ -1,4 +1,4 @@
 :: OPTIONAL: Install TightVNC
-:: URL|ALL|http://dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.2.9-setup.exe|packages/tightvnc-1.2.9-setup.exe
+:: URL|ALL|http://dl.sourceforge.net/sourceforge/vnc-tight/tightvnc-1.3.9-setup.exe|packages/tightvnc-1.3.9-setup.exe
 @Echo off
-todo.pl "%Z%\packages\tightvnc-1.2.9-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
+todo.pl "%Z%\packages\tightvnc-1.3.9-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
