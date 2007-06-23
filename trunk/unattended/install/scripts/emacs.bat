35b9bfacf8c7c9e9ef72ba891fa7dd9f6f182b6d
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-06-23 10:33:57 +0000

    Update to v22.1

--- trunk/unattended/install/scripts/emacs.bat
+++ trunk/unattended/install/scripts/emacs.bat
@@ -1,19 +1,15 @@
 :: OPTIONAL: Install Emacs for Windows
 ::
-:: URL|ALL|http://mirrors.usc.edu/pub/gnu/windows/emacs/emacs-21.3-fullbin-i386.tar.gz|packages/emacs/emacs-21.3-fullbin-i386.tar.gz
+:: URL|ALL|http://mirrors.usc.edu/pub/gnu/windows/emacs/emacs-22.1-bin-i386.zip|packages/emacs/emacs-22.1-bin-i386.zip
 :: 
 @Echo off
 set zip="%ProgramFiles%\7-ZIP\7z.exe"
-set prog=emacs-21.3
+set prog=emacs-22.1
 
 c:
 cd \
 
-%zip% x %Z%\packages\emacs\%prog%-fullbin-i386.tar.gz
-if errorlevel 1 exit 1
-%zip% x %prog%-fullbin-i386.tar
-if errorlevel 1 exit 1
-del %prog%-fullbin-i386.tar
+%zip% x %Z%\packages\emacs\%prog%-bin-i386.zip
 if errorlevel 1 exit 1
 cd %prog%
 if errorlevel 1 exit 1
