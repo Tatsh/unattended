1e87d6ec4efe067b16933ea1e088b479171f92d1
ekot <ekot@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-07-21 19:39:41 +0000

    Updated to v2.0.0.5, thanks to Juan Jose Pablos
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2578 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/thunderbird.bat
+++ unattended/install/scripts/thunderbird.bat
@@ -1,29 +1,29 @@
-:: OPTIONAL: Install Thunderbird 2.0.0.4
+:: OPTIONAL: Install Thunderbird 2.0.0.5
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/ca/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/cs/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/da/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/de/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/el/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/en-GB/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/en-US/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/es-ES/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/es-AR/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/fi/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/fr/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/he/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/hu/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/it/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/ja/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/ko/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/nl/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-nld.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/pl/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/pt-BR/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/ru/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/sl/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/sv-SE/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.4/win32/tr/Thunderbird%20Setup%202.0.0.4.exe|packages/mozilla/thunderbird-2004-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/ca/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/cs/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/da/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/de/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/el/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/en-GB/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/en-US/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/es-ES/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/es-AR/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/fi/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/fr/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/he/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/hu/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/it/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/ja/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/ko/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/nl/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-nld.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/pl/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/pt-BR/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/ru/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/sl/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/sv-SE/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.5/win32/tr/Thunderbird%20Setup%202.0.0.5.exe|packages/mozilla/thunderbird-2005-trk.exe
 
 @echo off
 
-todo.pl "%Z%\packages\mozilla\thunderbird-2004-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-2005-%WINLANG%.exe -ms -ira"
