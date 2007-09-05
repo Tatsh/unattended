9c722e97aee3bb57ef06137286b67b7361559a65
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-09-05 11:17:27 +0000

    update version 1.5

--- trunk/unattended/install/scripts/spybot.bat
+++ trunk/unattended/install/scripts/spybot.bat
@@ -1,10 +1,11 @@
 :: OPTIONAL: Install Spybot - Search & Destroy
-
-:: URL|ALL|http://fileforum.betanews.com/sendfile/1043809773/1/spybotsd14.exe|packages/spybotsd/spybotsd14.exe
+:: HOME: http://www.safer-networking.org/
+:: URL|ALL|http://www.spybotupdates.biz/files/spybotsd15.exe|packages/spybotsd/spybotsd15.exe
 @Echo off
 
 :: Un-comment the following line, if you want to run Spybot, check for updates, auto fix found issues,
 :: and auto-immunize. The machine *will* need public internet access for that.
-:: todo.pl "\"%ProgramFiles%\Spybot~1\SpybotSD.exe\" /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
+:: todo.pl "at 11:00 /every:5,10,15,20,25,30 \"%ProgramFiles%\Spybot~1\SpybotSD.exe\" /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
 
-todo.pl "%Z%\packages\spybotsd\spybotsd14.exe /sp- /silent /norestart"
+:: By default does not try to update or startup the teatimer task (as in 1.4)
+todo.pl "%Z%\packages\spybotsd\spybotsd15.exe /sp- /silent /norestart /components=\"main\" /tasks=\"!lauchteatimer\""
