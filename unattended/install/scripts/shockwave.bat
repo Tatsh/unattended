2c76dcc8c875f9fb594f31dd916913e62319d56c
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-10-17 21:44:42 +0000

    Update to 10.0.2.23
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2629 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/shockwave.bat
+++ unattended/install/scripts/shockwave.bat
@@ -1,6 +1,6 @@
 :: Install Shockwave10 player
-::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.2.0.021/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full.exe
+::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.2.0.023/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full_10.2.0.023.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\shockwave_installer_full.exe /s"
+todo.pl "%Z%\packages\macromedia\shockwave_installer_full_10.2.0.023.exe /s"
