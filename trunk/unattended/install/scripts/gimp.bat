afe6c6c63f4b975641b84b8ddb9ed2ecbefe88b3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-10-28 12:41:20 +0000

    update Gimp to 2.4.0 version

--- trunk/unattended/install/scripts/gimp.bat
+++ trunk/unattended/install/scripts/gimp.bat
@@ -1,22 +1,10 @@
-::OPTIONAL: Install GIMP (2.2.17)
+::OPTIONAL: Install GIMP (2.4.0)
 ::HOME: http://gimp-win.sourceforge.net/
-::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gtk+-2.10.13-setup.exe|packages/gtk/gtk+-2.10.13-setup.exe
-::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-2.2.17-i586-setup.exe|packages/gimp/gimp-2.2.17-i586-setup.exe
-::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-gap-2.2.0-setup.zip|packages/gimp/gimp-gap-2.2.0-setup.zip
+::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-2.4.0-i586-setup.exe|packages/gimp/gimp-2.4.0-i586-setup.exe
 ::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-help-2-0.13-setup.exe|packages/gimp/gimp-help-2-0.13-setup.exe
 @echo off
 
-:: Run the unzipped installers
+:: Run the installers
 
-todo.pl "start /wait %TEMP%\gimp-gap-2.2.0-setup.exe SP- /SILENT /NORESTART"
 todo.pl "start /wait %Z%\packages\gimp\gimp-help-2-0.13-setup.exe SP- /SILENT /NORESTART"
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.2.17-i586-setup.exe SP- /SILENT /NORESTART"
-todo.pl "start /wait %Z%\packages\gtk\gtk+-2.10.13-setup.exe /SP- /VERYSILENT /NORESTART"
-
-
-:: Unzip all of the files to the current users temp folder
-
-todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\" %Z%\packages\gimp\gimp-gap-2.2.0-setup.zip"
-
-:: Install 7zip if it isn't already
-todo.pl 7-zip.bat
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.4.0-i586-setup.exe SP- /SILENT /NORESTART"
