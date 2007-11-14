e87a313aaaf90abe9ff7dedd5ef1b9f4ad68f1da
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-11-14 19:18:48 +0000

    upsss... forgot to add file for november update

---
+++ trunk/unattended/install/scripts/winxpsp2-up0711.bat
@@ -0,0 +1,26 @@
+
+:: Security Update for Windows XP (KB943460)
+:: <http://www.microsoft.com/downloads/details.aspx?FamilyId=8ba1c2f9-1bde-4e97-b327-21259c5e5104>
+:: URL|ARA|http://download.microsoft.com/download/6/5/d/65d44181-3418-4598-ab2d-cad5d4f0e20c/WindowsXP-KB943460-x86-ARA.exe|updates/winxpsp2/windowsxp-kb943460-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/d/b/0/db0bb04f-f34c-4b51-9605-cb037ae20967/WindowsXP-KB943460-x86-CSY.exe|updates/winxpsp2/windowsxp-kb943460-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/9/d/a/9daa4cf4-7136-4394-aee4-235bfe135744/WindowsXP-KB943460-x86-DAN.exe|updates/winxpsp2/windowsxp-kb943460-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/a/6/1/a61e3d0e-6cf1-4108-9699-2e0459ef38ed/WindowsXP-KB943460-x86-DEU.exe|updates/winxpsp2/windowsxp-kb943460-x86-deu.exe
+:: No Download found for ELL.
+:: URL|ENU|http://download.microsoft.com/download/0/e/b/0eb29ece-fb34-4eb6-ae49-a6e816a9fa5b/WindowsXP-KB943460-x86-ENU.exe|updates/winxpsp2/windowsxp-kb943460-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/e/b/2/eb2a1ce9-5cc8-4dd2-ae38-fa3afa0f230a/WindowsXP-KB943460-x86-ESN.exe|updates/winxpsp2/windowsxp-kb943460-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/6/e/f/6ef013e5-653e-4913-8ea5-c74937f8ed7f/WindowsXP-KB943460-x86-FIN.exe|updates/winxpsp2/windowsxp-kb943460-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/3/8/a/38aa5ebb-d616-4e69-a37a-04b558fbec7e/WindowsXP-KB943460-x86-FRA.exe|updates/winxpsp2/windowsxp-kb943460-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/b/6/0/b607c519-be77-4d3b-bd81-13c0476c5e9e/WindowsXP-KB943460-x86-HEB.exe|updates/winxpsp2/windowsxp-kb943460-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/7/0/3/70365399-ad26-4b51-8a81-50eb63baa0a6/WindowsXP-KB943460-x86-HUN.exe|updates/winxpsp2/windowsxp-kb943460-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/f/0/1/f0143056-4f13-4d16-834b-f510e4633628/WindowsXP-KB943460-x86-ITA.exe|updates/winxpsp2/windowsxp-kb943460-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/8/d/2/8d2a688c-71b6-4f9d-9d30-428038534ec7/WindowsXP-KB943460-x86-JPN.exe|updates/winxpsp2/windowsxp-kb943460-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/5/0/e/50eea68a-7e5e-4cdf-ba6d-e7b9739794a1/WindowsXP-KB943460-x86-KOR.exe|updates/winxpsp2/windowsxp-kb943460-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/a/5/7/a57ae8fa-612a-4ef5-b0be-6ab7ee9de380/WindowsXP-KB943460-x86-NLD.exe|updates/winxpsp2/windowsxp-kb943460-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/9/a/1/9a118b03-858e-4f75-8e04-9d02d7b897dc/WindowsXP-KB943460-x86-NOR.exe|updates/winxpsp2/windowsxp-kb943460-x86-nor.exe
+:: URL|PLK|http://download.microsoft.com/download/1/7/7/177b95d8-af14-479e-99b9-fba6f04731be/WindowsXP-KB943460-x86-PLK.exe|updates/winxpsp2/windowsxp-kb943460-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/c/d/b/cdb3f5cd-c3a1-4bf6-a458-66810e44ca8c/WindowsXP-KB943460-x86-PTB.exe|updates/winxpsp2/windowsxp-kb943460-x86-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/a/a/6/aa6a3e6a-2dbd-4f13-883e-eca69acd72a0/WindowsXP-KB943460-x86-PTG.exe|updates/winxpsp2/windowsxp-kb943460-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/c/8/1/c818545a-1a53-4e84-bc0b-626063c463b8/WindowsXP-KB943460-x86-RUS.exe|updates/winxpsp2/windowsxp-kb943460-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/d/8/0/d8044378-1275-426e-8eeb-75e9dfec5af9/WindowsXP-KB943460-x86-SVE.exe|updates/winxpsp2/windowsxp-kb943460-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/9/4/1/9416eb2e-24b6-4cb6-8a9c-46322d8d7942/WindowsXP-KB943460-x86-TRK.exe|updates/winxpsp2/windowsxp-kb943460-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp2\WindowsXP-KB943460-x86-%WINLANG%.exe /passive /n /norestart"
