b0a409314aa5c73f230d881a7a9e0c349cc6f1c5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-11-27 12:37:21 +0000

    update to 2.4.2

--- trunk/unattended/install/scripts/gimp.bat
+++ trunk/unattended/install/scripts/gimp.bat
@@ -1,10 +1,10 @@
 ::OPTIONAL: Install GIMP (2.4.0)
 ::HOME: http://gimp-win.sourceforge.net/
-::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-2.4.0-i586-setup.exe|packages/gimp/gimp-2.4.0-i586-setup.exe
+::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-2.4.2-i686-setup.exe|packages/gimp/gimp-2.4.2-i686-setup.exe
 ::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-help-2-0.13-setup.exe|packages/gimp/gimp-help-2-0.13-setup.exe
 @echo off
 
 :: Run the installers
 
 todo.pl "start /wait %Z%\packages\gimp\gimp-help-2-0.13-setup.exe SP- /SILENT /NORESTART"
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.4.0-i586-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.4.2-i686-setup.exe SP- /SILENT /NORESTART"
