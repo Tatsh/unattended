e40dad5bdf0ea3ab1976fef7418a724cef84fe29
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-12-07 01:28:32 +0000

    upgrade version 5.5

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,6 +1,8 @@
-:: OPTIONAL: Install WinAmp 5.35
+:: OPTIONAL: Install WinAmp 5.5
+:: HOME: http://www.winamp.com/
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp535_full_emusic-7plus.exe|packages/winamp/winamp535-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp55_full_emusic-7plus.exe|packages/winamp/winamp55-full.exe
 :: URL|ALL|http://download.nullsoft.com/winamp/client/wa5update.exe|packages/winamp/wa5update-20070507.exe
+
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp535-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp55-full.exe /S /install=SFQR"
