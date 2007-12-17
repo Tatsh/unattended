4bbf2da04cc6f09d287cd75d46eb0369c410bc11
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-12-17 01:30:58 +0000

    fix path

--- unattended/install/scripts/spybot.bat
+++ unattended/install/scripts/spybot.bat
@@ -12,12 +12,12 @@
 :: and auto-immunize. The machine *will* need public internet access for that.
 todo.pl "at 11:00 /every:5,10,15,20,25,30 \"%ProgramFiles%\Spybot~1\SpybotSD.exe\" /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
 
-todo.pl "copy "%Z%\packages\spybotsd\download.ini" "%ProgramFiles%\Spybot~1\Updates\"
-todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -y -o\"%ProgramFiles%\Spybot~1\Includes\" %Z%\packages\spybotsd\startup.zip" 
-todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -y -o\"%ProgramFiles%\Spybot~1\Includes\" %Z%\packages\spybotsd\clsid.zip" 
 todo.pl "%Z%\packages\spybotsd\spybotsd_advcheck.exe /S /D=\"%ProgramFiles%\Spybot~1\" 
 todo.pl "%Z%\packages\spybotsd\spybotsd_tools.exe /S /D=\"%ProgramFiles%\Spybot~1\"
 todo.pl "%Z%\packages\spybotsd\spybotsd_includes-071128.exe /S /D=\"%ProgramFiles%\Spybot~1\"
+todo.pl "copy %Z%\packages\spybotsd\downloaded.ini \"%ProgramFiles%\Spybot~1\Updates\" "
+todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -y -o\"%ProgramFiles%\Spybot~1\Includes\" %Z%\packages\spybotsd\startup.zip" 
+todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -y -o\"%ProgramFiles%\Spybot~1\Includes\" %Z%\packages\spybotsd\clsid.zip" 
 
 :: By default does not try to update or startup the teatimer task (as in 1.4)
 todo.pl "%Z%\packages\spybotsd\spybotsd15.exe /sp- /silent /norestart /components=\"main\" /tasks=\"!lauchteatimer\""
