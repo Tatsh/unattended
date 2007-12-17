86b79dd08a15593f93177a5ad0fb8c65a03ea827
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-12-17 13:47:35 +0000

    Initial version

---
+++ unattended/install/scripts/vlc.bat
@@ -0,0 +1,8 @@
+:: OPTIONAL:  Install VideoLAN - VLC media player
+:: HOME: http://www.videolan.org/
+
+:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/0.8.6d/win32/vlc-0.8.6d-win32.exe|packages/vlc-0.8.6d-win32.exe
+
+@Echo off
+
+todo.pl "%Z%\packages\vlc-0.8.6d-win32.exe /S"
