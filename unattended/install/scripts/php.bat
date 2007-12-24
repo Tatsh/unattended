8a02c0e99a1a9d6f9dd53d0241f30b4758f1aa04
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2007-12-24 08:53:46 +0000

    php4 is not longer supported.

--- unattended/install/scripts/php.bat
+++ unattended/install/scripts/php.bat
@@ -1,4 +1,6 @@
-:: Install PHP4
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-4.4.4-installer.exe|packages/php/php-4.4.4-installer.exe
-@Echo off
-todo.pl "%Z%\packages\php\php-4.4.4-installer.exe /s"
+:: Install PHP
+:: HOME: http://www.php.net/
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.2.5-win32-installer.msi|packages/php/php-5.2.5-win32-installer.msi^M
+
+@Echo off^M
+todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\php5.txt /i %Z%\packages\php\php-5.2.5-win32-installer.msi"
