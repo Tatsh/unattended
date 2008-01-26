53380ba4356f44366f12219feedf721d2c0aa5fd
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-01-26 10:56:48 +0000

    added HOME

--- trunk/unattended/install/scripts/putty.bat
+++ trunk/unattended/install/scripts/putty.bat
@@ -1,4 +1,5 @@
 :: OPTIONAL: Install Putty 0.60
+:: HOME: http://www.chiark.greenend.org.uk/~sgtatham/putty/
 :: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.60-installer.exe|packages/putty/putty-0.60-installer.exe
 @Echo off
 
