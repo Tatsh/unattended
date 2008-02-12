99c74d9567c8c095cfb3bec38cb88eac59dcb0f6
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-02-12 18:38:08 +0000

    update winamp 5.5.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2714 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winamp.bat
+++ unattended/install/scripts/winamp.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: Install WinAmp 5.5
 :: HOME: http://www.winamp.com/
 
-:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp55_full_emusic-7plus.exe|packages/winamp/winamp55-full.exe
+:: URL|ALL|http://download.nullsoft.com/winamp/client/winamp552_full_emusic-7plus_en-us.exe|packages/winamp/winamp552-full.exe
 :: URL|ALL|http://download.nullsoft.com/winamp/client/wa5update.exe|packages/winamp/wa5update-20070507.exe
 
 todo.pl "\"%ProgramFiles%\Winamp\eMusic\Uninst-eMusic-promotion.exe\" /S"
-todo.pl "%Z%\packages\winamp\winamp55-full.exe /S /install=SFQR"
+todo.pl "%Z%\packages\winamp\winamp552-full.exe /S /install=SFQR"
