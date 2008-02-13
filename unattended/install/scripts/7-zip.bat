1653b60147b21bc4ef8d6444313eb5623e5babd8
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-02-13 14:08:03 +0000

    Update to version 457. Thanks to Lars Reinecke

--- unattended/install/scripts/7-zip.bat
+++ unattended/install/scripts/7-zip.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: Install 7-Zip
 :: HOME: http://sevenzip.sourceforge.net/
-:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z442.msi|packages/sevenzip/7z442.msi
+:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z457.msi|packages/sevenzip/7z442.msi
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\sevenzip\7z442.msi REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\sevenzip\7z457.msi REBOOT=ReallySuppress"
 :done
