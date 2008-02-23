2631ae803a4609810bcc91e9e6e05fc135afd30b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-02-23 09:03:04 +0000

    typo on the version number from last commit. Thanks Jon Wilson for spotted!

--- trunk/unattended/install/scripts/7-zip.bat
+++ trunk/unattended/install/scripts/7-zip.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install 7-Zip
 :: HOME: http://sevenzip.sourceforge.net/
-:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z457.msi|packages/sevenzip/7z442.msi
+:: URL|ALL|http://dl.sourceforge.net/sourceforge/sevenzip/7z457.msi|packages/sevenzip/7z457.msi
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
