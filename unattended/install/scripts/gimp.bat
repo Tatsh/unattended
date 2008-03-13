ec2a57b34735a509d7eec51789719da8b98e45d7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-03-13 22:05:09 +0000

    update to version 2.4.5
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2722 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/gimp.bat
+++ unattended/install/scripts/gimp.bat
@@ -1,10 +1,10 @@
-::OPTIONAL: Install GIMP (2.4.3)
+::OPTIONAL: Install GIMP (2.4.5)
 ::HOME: http://gimp-win.sourceforge.net/
-::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-2.4.3-i686-setup-1.exe|packages/gimp/gimp-2.4.3-i686-setup-1.exe
+::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-2.4.5-i686-setup.exe|packages/gimp/gimp-2.4.5-i686-setup.exe
 ::URL|ALL|http://dl.sourceforge.net/sourceforge/gimp-win/gimp-help-2-2.4.0-setup.exe|packages/gimp/gimp-help-2-2.4.0-setup.exe
 @echo off
 
 :: Run the installers
 
 todo.pl "start /wait %Z%\packages\gimp\gimp-help-2-2.4.0-setup.exe SP- /SILENT /NORESTART"
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.4.3-i686-setup-1.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.4.5-i686-setup.exe SP- /SILENT /NORESTART"
