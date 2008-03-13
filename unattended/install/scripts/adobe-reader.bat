e31fd4ef7ddfc3d76406fbd7a38e576bfd02748e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-03-13 22:04:49 +0000

    Update to version 8.1.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2721 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/adobe-reader.bat
+++ unattended/install/scripts/adobe-reader.bat
@@ -2,31 +2,31 @@
 @Echo Off
 
 :: Download Adobe Reader 8.x.x full version
-:: URL|ARA|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/ar_AE/AdbeRdr80_ar_AE.exe|packages/adobereader/adberdr80_ara.exe
-:: URL|CHS|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/chs/AdbeRdr810_zh_CN.exe|packages/adobereader/adberdr810_chs.exe
-:: URL|CHT|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/cht/AdbeRdr810_zh_TW.exe|packages/adobereader/adberdr810_cht.exe
-:: URL|CSY|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/cs_CZ/AdbeRdr80_cs_CZ.exe|packages/adobereader/adberdr80_csy.exe
-:: URL|DAN|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/dan/AdbeRdr810_da_DK.exe|packages/adobereader/adberdr810_dan.exe
-:: URL|NLD|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/nld/AdbeRdr810_nl_NL.exe|packages/adobereader/adberdr810_nld.exe
-:: URL|ENU|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1.1/enu/AdbeRdr811_en_US.exe|packages/adobereader/adberdr811_enu.exe
-:: URL|FIN|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/suo/AdbeRdr810_fi_FI.exe|packages/adobereader/adberdr810_fin.exe
-:: URL|FRA|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/fra/AdbeRdr810_fr_FR.exe|packages/adobereader/adberdr810_fra.exe
-:: URL|DEU|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/deu/AdbeRdr810_de_DE.exe|packages/adobereader/adberdr810_deu.exe
-:: URL|ELL|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/el_GR/AdbeRdr80_el_GR.exe|packages/adobereader/adberdr80_ell.exe
-:: URL|HEB|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/he_IL/AdbeRdr80_he_IL.exe|packages/adobereader/adberdr80_heb.exe
-:: URL|HUN|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/hu_HU/AdbeRdr80_hu_HU.exe|packages/adobereader/adberdr80_hun.exe
-:: URL|ITA|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/ita/AdbeRdr810_it_IT.exe|packages/adobereader/adberdr810_ita.exe
-:: URL|JPN|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/jpn/AdbeRdr810_ja_JP.exe|packages/adobereader/adberdr810_jpn.exe
-:: URL|KOR|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/kor/AdbeRdr810_ko_KR.exe|packages/adobereader/adberdr810_kor.exe
-:: URL|NOR|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/nor/AdbeRdr810_nb_NO.exe|packages/adobereader/adberdr810_nor.exe
-:: URL|PLK|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/pl_PL/AdbeRdr80_pl_PL.exe|packages/adobereader/adberdr80_plk.exe
-:: URL|PTB|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/ptb/AdbeRdr810_pt_BR.exe|packages/adobereader/adberdr810_ptb.exe
-:: URL|RUS|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.0/ru_RU/AdbeRdr80_ru_RU.exe|packages/adobereader/adberdr80_rus.exe
-:: URL|ESN|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/esp/AdbeRdr810_es_ES.exe|packages/adobereader/adberdr810_esn.exe
-:: URL|SVE|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/sve/AdbeRdr810_sv_SE.exe|packages/adobereader/adberdr810_sve.exe
-:: URL|TRK|http://ardownload.adobe.com/pub/adobe/reader/win/8.x/8.1/sve/AdbeRdr810_sv_SE.exe|packages/adobereader/adberdr810_trk.exe
+:: URL|ARA|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/ar_AE/AdbeRdr810_ar_AE.exe|packages/adobereader/adberdr810_ara.exe
+:: URL|CHS|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/chs/AdbeRdr812_zh_CN.exe|packages/adobereader/adberdr812_chs.exe
+:: URL|CHT|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/cht/AdbeRdr812_zh_TW.exe|packages/adobereader/adberdr812_cht.exe
+:: URL|CSY|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/cs_CZ/AdbeRdr810_cs_CZ.exe|packages/adobereader/adberdr810_csy.exe
+:: URL|DAN|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/dan/AdbeRdr812_da_DK.exe|packages/adobereader/adberdr812_dan.exe
+:: URL|NLD|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/nld/AdbeRdr812_nl_NL.exe|packages/adobereader/adberdr812_nld.exe
+:: URL|ENU|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/enu/AdbeRdr812_en_US.exe|packages/adobereader/adberdr812_enu.exe
+:: URL|FIN|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/suo/AdbeRdr812_fi_FI.exe|packages/adobereader/adberdr812_fin.exe
+:: URL|FRA|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/fra/AdbeRdr812_fr_FR.exe|packages/adobereader/adberdr812_fra.exe
+:: URL|DEU|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/deu/AdbeRdr812_de_DE.exe|packages/adobereader/adberdr812_deu.exe
+:: URL|ELL|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/el_GR/AdbeRdr810_el_GR.exe|packages/adobereader/adberdr810_ell.exe
+:: URL|HEB|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/he_IL/AdbeRdr810_he_IL.exe|packages/adobereader/adberdr810_heb.exe
+:: URL|HUN|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/hu_HU/AdbeRdr810_hu_HU.exe|packages/adobereader/adberdr810_hun.exe
+:: URL|ITA|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/ita/AdbeRdr812_it_IT.exe|packages/adobereader/adberdr812_ita.exe
+:: URL|JPN|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/jpn/AdbeRdr812_ja_JP.exe|packages/adobereader/adberdr812_jpn.exe
+:: URL|KOR|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/kor/AdbeRdr812_ko_KR.exe|packages/adobereader/adberdr812_kor.exe
+:: URL|NOR|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/nor/AdbeRdr812_nb_NO.exe|packages/adobereader/adberdr812_nor.exe
+:: URL|PLK|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/pl_PL/AdbeRdr810_pl_PL.exe|packages/adobereader/adberdr810_plk.exe
+:: URL|PTB|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/ptb/AdbeRdr812_pt_BR.exe|packages/adobereader/adberdr812_ptb.exe
+:: URL|RUS|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/ru_RU/AdbeRdr810_ru_RU.exe|packages/adobereader/adberdr810_rus.exe
+:: URL|ESN|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/esp/AdbeRdr812_es_ES.exe|packages/adobereader/adberdr812_esn.exe
+:: URL|SVE|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/sve/AdbeRdr812_sv_SE.exe|packages/adobereader/adberdr812_sve.exe
+:: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1.2/sve/AdbeRdr812_sv_SE.exe|packages/adobereader/adberdr812_trk.exe
 
-:: Download Adobe Reader 8.1.x Language Support
+:: Download Adobe Reader 8.1.2.x Language Support
 :: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/misc/AdbeRdrSD810_all.msi|packages/adobereader/misc/AdbeRdrSD810_all.msi
 :: Download Extended Language Support Fonts Package
 :: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/misc/FontPack810_Xtd_Lang.msi|packages/adobereader/misc/fontpack810_xtd_lang.msi
@@ -39,13 +39,10 @@
 :: Download Adobe Reader Taiwanese Fonts
 :: ULR|TWS|ftp://ftp.adobe.com/pub/adobe/reader/win/8.x/8.1/misc/FontPack810_zh_TW.msi|packages/adobereader/misc/fontpack810_nan.msi
 
-set adobe_install=%Z%\packages\adobereader\AdbeRdr811_%WINLANG%.exe
+set adobe_install=%Z%\packages\adobereader\AdbeRdr812_%WINLANG%.exe
 if exist %adobe_install% goto found_install
 
 set adobe_install=%Z%\packages\adobereader\AdbeRdr810_%WINLANG%.exe
-if exist %adobe_install% goto found_install
-
-set adobe_install=%Z%\packages\adobereader\AdbeRdr80_%WINLANG%.exe
 if not exist %adobe_install% goto noadberdr
 
 :found_install
@@ -74,7 +71,7 @@ if not exist %adobe_install% goto noadberdr
 :: End of Adobe Reader Fonts installation section
 ::--------------------------------------------------------------------------------
 
-if not exist %Z%\packages\adobereader\misc\fontpack810_xtd_lang.msi goto nolangsupport
+if not exist %Z%\packages\adobereader\misc\fontpack812_xtd_lang.msi goto nolangsupport
 :: Install Extended Language Support Fonts Package
 todo.pl "msiexec /qb /i %Z%\packages\adobereader\misc\fontpack810_xtd_lang.msi"
 
