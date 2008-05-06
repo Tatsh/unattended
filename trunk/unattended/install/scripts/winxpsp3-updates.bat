670cd0cfb402134113780b9251851ae082119279
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-05-06 23:13:37 +0000

    Add the Windows XP Service Pack Â3

---
+++ trunk/unattended/install/scripts/winxpsp3-updates.bat
@@ -0,0 +1,8 @@
+:: Install all updates and hotfixes for Windows XP SP3
+@Echo off
+:: Note that since we are pushing these commands onto the to-do stack,
+:: they will be executed in the opposite order.
+
+todo.pl "%Z%\scripts\winxpsp3-extras.bat"
+:: Install all updates for Windows XP SP3
+@Echo off
