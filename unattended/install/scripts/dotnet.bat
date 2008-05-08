ba058c3dd24447992a224845b398dd1fb7e3c7d9
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-05-08 23:18:33 +0000

    First version for the 3.5 dotnet package. Thanks to Jon Wilson
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2796 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ unattended/install/scripts/dotnet.bat
@@ -0,0 +1,5 @@
+:: INSTALL :: Microsoft .NET 3.5 Redistributable Package (x86)
+:: http://msdn.microsoft.com/en-us/netframework/default.aspx
+:: URL|ALL|http://download.microsoft.com/download/6/0/f/60fc5854-3cb8-4892-b6db-bd4f42510f28/dotnetfx35.exe|updates/common/dotnetfx35.exe
+
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx35.exe /q /norestart"
