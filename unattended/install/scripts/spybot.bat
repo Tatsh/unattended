1f11910f9fb36b6bcd2c74e07beece2a3ea87af2
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-06-02 00:45:34 +0000

    update spybot definitions up to jun 2008

--- unattended/install/scripts/spybot.bat
+++ unattended/install/scripts/spybot.bat
@@ -1,9 +1,10 @@
 :: OPTIONAL: Install Spybot - Search & Destroy
 :: HOME: http://www.safer-networking.org/
 :: URL|ALL|http://www.spybotupdates.com/files/spybotsd152.exe|packages/spybotsd/spybotsd152.exe
-:: URL|ALL|http://www.spybotupdates.com/updates/files/spybotsd_includes.exe|packages/spybotsd/spybotsd_includes-080417.exe
+:: URL|ALL|http://www.spybotupdates.com/updates/files/spybotsd_includes.exe|packages/spybotsd/spybotsd_includes-080528.exe
 :: URL|ALL|http://www.spybotupdates.com/updates/files/spybotsd_tools.exe|packages/spybotsd/spybotsd_tools-070801.exe
-:: URL|ALL|http://www.spybotupdates.com/updates/files/spybotsd_advcheck.exe|packages/spybotsd/spybotsd_advcheck.exe
+:: URL|ALL|http://www.spybotupdates.com/updates/files/spybotsd_advcheck.exe|packages/spybotsd/spybotsd_advcheck-070119.exe
+:: URL|ALL|http://www.spybotupdates.com/files/spybotsd_plugins.exe|packages/spybotsd/spybotsd_plugins.exe
 :: URL|ALL|http://www.spybotupdates.com/updates/files/startup.zip|packages/spybotsd/startup.zip
 :: URL|ALL|http://www.spybotupdates.com/updates/files/clsid.zip|packages/spybotsd/clsid.zip
 @Echo off
@@ -12,9 +13,10 @@
 :: and auto-immunize. The machine *will* need public internet access for that.
 todo.pl "at 11:00 /every:5,10,15,20,25,30 \"%ProgramFiles%\Spybot~1\SpybotSD.exe\" /taskbarhide /autoclose /autoupdate /autoimmunize /autofix /autocheck"
 
-todo.pl "%Z%\packages\spybotsd\spybotsd_advcheck.exe /S /D=\"%ProgramFiles%\Spybot~1\" 
+todo.pl "%Z%\packages\spybotsd\spybotsd_plugins.exe /S /D=\"%ProgramFiles%\Spybot~1\" 
+todo.pl "%Z%\packages\spybotsd\spybotsd_advcheck-070119.exe /S /D=\"%ProgramFiles%\Spybot~1\" 
 todo.pl "%Z%\packages\spybotsd\spybotsd_tools-070801.exe /S /D=\"%ProgramFiles%\Spybot~1\"
-todo.pl "%Z%\packages\spybotsd\spybotsd_includes-080417.exe /S /D=\"%ProgramFiles%\Spybot~1\"
+todo.pl "%Z%\packages\spybotsd\spybotsd_includes-080528.exe /S /D=\"%ProgramFiles%\Spybot~1\"
 todo.pl "copy %Z%\packages\spybotsd\downloaded.ini \"%ProgramFiles%\Spybot~1\Updates\" "
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -y -o\"%ProgramFiles%\Spybot~1\Includes\" %Z%\packages\spybotsd\startup.zip" 
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -y -o\"%ProgramFiles%\Spybot~1\Includes\" %Z%\packages\spybotsd\clsid.zip" 
