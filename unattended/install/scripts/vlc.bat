93625df1e82acac15e3740ad9c55f45f7f8dac0e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-06-13 17:07:36 +0000

    new version 8.6h
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2810 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/vlc.bat
+++ unattended/install/scripts/vlc.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
 
-:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/0.8.6h/win32/vlc-0.8.6h-win32.exe|packages/vlc-0.8.6f-win32.exe
+:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/0.8.6h/win32/vlc-0.8.6h-win32.exe|packages/vlc-0.8.6h-win32.exe
 
 @Echo off
 
