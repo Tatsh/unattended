3115fb04da492baa9bd8ae8733a85642e02814b2
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-06-13 17:01:34 +0000

    script not longer used
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2809 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/sshclient.bat
+++
@@ -1,4 +0,0 @@
-:: Install SSH Client (http://www.ssh.com)
-:: URL|ALL|http://ftp.ssh.org/pub/ssh/SSHSecureShellClient-3.2.9.exe|packages/sshclient/sshsecureshellclient-3.2.9.exe
-@Echo off
-todo.pl "start /wait %Z%\packages\sshclient\sshsecureshellclient-3.2.9.exe /s /a /s /sms /f1%Z%\packages\sshclient\sshclient.iss /f2c:\netinst\logs\sshclient.txt"
