61d3094e8469a6eeabe12ab9b3689aab366413cc
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-08-16 17:42:17 +0000

    update version

--- unattended/install/scripts/gsview.bat
+++ unattended/install/scripts/gsview.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Ghostscript and GSview
-:: URL|ALL|http://mirror.cs.wisc.edu/pub/mirrors/ghost/GPL/gs862/gs862w32.exe|packages/ghostscript/gs862w32.exe
+:: URL|ALL|http://mirror.cs.wisc.edu/pub/mirrors/ghost/GPL/gs863/gs863w32.exe|packages/ghostscript/gs863w32.exe
 :: URL|ALL|http://mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv49w32.exe|packages/ghostscript/gsv49w32.exe
 :: Then manually extract to gs and gv folders in the same path
 
@@ -7,5 +7,5 @@ todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv49w32.exe"
 
 todo.pl " start /D%TEMP%\gs /wait %TEMP%\gs\setupgs.exe \"%ProgramFiles%\gs"
-todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gs %Z%\packages\ghostscript\gs862w32.exe"
+todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gs %Z%\packages\ghostscript\gs863w32.exe"
 
