0bfbf43b98a2cf951bdb3936802ddec153abdedf
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-08-16 17:10:30 +0000

    update to version 951

--- unattended/install/scripts/opera.bat
+++ unattended/install/scripts/opera.bat
@@ -1,9 +1,11 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/922/int/Opera_9.22_International_Setup.exe|packages/opera/opera-922-int.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/951/int/Opera_951_in_Setup.exe|packages/opera/opera-951-int.exe
 @Echo off
 
+todo.pl "shortcut.pl  \"%ProgramFiles%\Opera\opera.exe\" special:AllUsersDesktop\Opera"
 :: if you like to do special things (e.g. define custom searches), save your customized ini's in packages/opera/special
 
-%Z%\packages\opera\opera-922-int.exe /S /v/qn
-
 if exist "%Z%\packages\opera\special\*" copy "%Z%\packages\opera\special\*" "%ProgramFiles%\Opera"
+
+todo.pl "%Z%\packages\opera\opera-951-int.exe /S /v/qn"
+
