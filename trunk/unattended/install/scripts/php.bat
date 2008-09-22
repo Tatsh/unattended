585eea0057c1d92f582875b710ff6e7a9e4bed8a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-09-22 18:25:04 +0000

    wrong character at the end of line

--- trunk/unattended/install/scripts/php.bat
+++ trunk/unattended/install/scripts/php.bat
@@ -1,6 +1,5 @@
 :: Install PHP
 :: HOME: http://www.php.net/
-:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.2.6-win32-installer.msi|packages/php/php-5.2.6-win32-installer.msi^M
-
+:: URL|ALL|http://static.php.net/www.php.net/distributions/php-5.2.6-win32-installer.msi|packages/php/php-5.2.6-win32-installer.msi
 @Echo off^M
 todo.pl ".reboot-on 194 msiexec /qb /l* %SystemDrive%\netinst\logs\php5.txt /i %Z%\packages\php\php-5.2.6-win32-installer.msi"
