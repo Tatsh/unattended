72f1fac2d79585b4e4523d72cfe4a2590bc25b6f
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-09-22 18:25:43 +0000

    update to 0.9.6

--- trunk/unattended/install/scripts/pdfcreator.bat
+++ trunk/unattended/install/scripts/pdfcreator.bat
@@ -2,22 +2,22 @@
 :: HOME: http://www.pdfforge.org/
 @echo off
 
-:: URL|ALL|http://downloads.sourceforge.net/pdfcreator/PDFCreator-0_9_5_setup.exe|packages/pdfcreator/pdfcreator-0.9.5.setup.exe
+:: URL|ALL|http://downloads.sourceforge.net/pdfcreator/PDFCreator-0_9_6_setup.exe|packages/pdfcreator/pdfcreator-0.9.6.setup.exe
 
 :: Install language files
-:: URL|CHS|http://www.pdfforge.org/files/translations/0.9.5/chinese_simplified.ini|packages/pdfcreator/lang-chs-0.9.5.ini
-:: URL|DEU|http://www.pdfforge.org/files/translations/0.9.5/german.ini|packages/pdfcreator/lang-deu-0.9.5.ini
-:: URL|DAN|http://www.pdfforge.org/files/translations/0.9.5/danish.ini|packages/pdfcreator/lang-dan-0.9.5.ini
-:: URL|FRA|http://www.pdfforge.org/files/translations/0.9.5/french.ini|packages/pdfcreator/lang-fra-0.9.5.ini
-:: URL|ITA|http://www.pdfforge.org/files/translations/0.9.5/italian.ini|packages/pdfcreator/lang-ita-0.9.5.ini
-:: URL|NLD|http://www.pdfforge.org/files/translations/0.9.5/dutch.ini|packages/pdfcreator/lang-nld-0.9.5.ini
-:: URL|RUS|http://www.pdfforge.org/files/translations/0.9.5/russian.ini|packages/pdfcreator/lang-rus-0.9.5.ini
-:: URL|SKY|http://www.pdfforge.org/files/translations/0.9.5/slovenian.ini|packages/pdfcreator/lang-sky-0.9.5.ini
-:: URL|SVE|http://www.pdfforge.org/files/translations/0.9.5/swedish.ini|packages/pdfcreator/lang-sve-0.9.5.ini
-:: URL|TRK|http://www.pdfforge.org/files/translations/0.9.5/turkish.ini|packages/pdfcreator/lang-trk-0.9.5.ini
-if not exist %Z%\packages\pdfcreator\lang-%WINLANG%-0.9.5.ini goto nolangpack
-todo.pl "copy %Z%\packages\pdfcreator\lang-%WINLANG%-0.9.5.ini \"%ProgramFiles%\PDFCreator\languages\""
+:: URL|CHS|http://www.pdfforge.org/files/translations/0.9.6/chinese_simplified.ini|packages/pdfcreator/lang-chs-0.9.6.ini
+:: URL|DEU|http://www.pdfforge.org/files/translations/0.9.6/german.ini|packages/pdfcreator/lang-deu-0.9.6.ini
+:: URL|DAN|http://www.pdfforge.org/files/translations/0.9.6/danish.ini|packages/pdfcreator/lang-dan-0.9.6.ini
+:: URL|FRA|http://www.pdfforge.org/files/translations/0.9.6/french.ini|packages/pdfcreator/lang-fra-0.9.6.ini
+:: URL|ITA|http://www.pdfforge.org/files/translations/0.9.6/italian.ini|packages/pdfcreator/lang-ita-0.9.6.ini
+:: URL|NLD|http://www.pdfforge.org/files/translations/0.9.6/dutch.ini|packages/pdfcreator/lang-nld-0.9.6.ini
+:: URL|RUS|http://www.pdfforge.org/files/translations/0.9.6/russian.ini|packages/pdfcreator/lang-rus-0.9.6.ini
+:: URL|SKY|http://www.pdfforge.org/files/translations/0.9.6/slovenian.ini|packages/pdfcreator/lang-sky-0.9.6.ini
+:: URL|SVE|http://www.pdfforge.org/files/translations/0.9.6/swedish.ini|packages/pdfcreator/lang-sve-0.9.6.ini
+:: URL|TRK|http://www.pdfforge.org/files/translations/0.9.6/turkish.ini|packages/pdfcreator/lang-trk-0.9.6.ini
+if not exist %Z%\packages\pdfcreator\lang-%WINLANG%-0.9.6.ini goto nolangpack
+todo.pl "copy %Z%\packages\pdfcreator\lang-%WINLANG%-0.9.6.ini \"%ProgramFiles%\PDFCreator\languages\""
 :nolangpack
 
 :: Install PDFCreator
-todo.pl "%Z%\packages\pdfcreator\pdfcreator-0.9.5.setup.exe /sp- /silent /norestart"
+todo.pl "%Z%\packages\pdfcreator\pdfcreator-0.9.6.setup.exe /sp- /silent /norestart"
