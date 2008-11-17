39b6f3086b5f89b7abf88b560c67489372b34256
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-11-17 08:30:05 +0000

    update to 0.9.6
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2880 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/vlc.bat
+++ unattended/install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
 
-:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/0.9.4/win32/vlc-0.9.4-win32.exe|packages/vlc-0.9.4-win32.exe
+:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/0.9.6/win32/vlc-0.9.6-win32.exe|packages/vlc-0.9.6-win32.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc-0.9.4-win32.exe /S"
+todo.pl "%Z%\packages\vlc-0.9.6-win32.exe /S"
