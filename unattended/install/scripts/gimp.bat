3e669b70b9e2e751a89f203ee537954f8efda5a4
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-12-30 09:17:57 +0000

    update pakacke for 2.6.3

--- unattended/install/scripts/gimp.bat
+++ unattended/install/scripts/gimp.bat
@@ -1,10 +1,10 @@
 ::OPTIONAL: Install GIMP (2.6)
 ::HOME: http://gimp-win.sourceforge.net/
-::URL|ALL|http://downloads.sourceforge.net/gimp-win/gimp-2.6.3-i686-setup.exe|packages/gimp/gimp-2.6.3-i686-setup.exe
+::URL|ALL|http://downloads.sourceforge.net/gimp-win/gimp-2.6.3-i686-setup-1.exe|packages/gimp/gimp-2.6.3-i686-setup-1.exe
 ::URL|ALL|http://downloads.sourceforge.net/gimp-win/gimp-help-2-2.4.0-setup.exe|packages/gimp/gimp-help-2-2.4.0-setup.exe
 @echo off
 
 :: Run the installers
 
 todo.pl "start /wait %Z%\packages\gimp\gimp-help-2-2.4.0-setup.exe SP- /SILENT /NORESTART"
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.6.3-i686-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.6.3-i686-setup-1.exe SP- /SILENT /NORESTART"
