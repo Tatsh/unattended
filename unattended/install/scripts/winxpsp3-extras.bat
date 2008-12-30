4242db205fc985c720e1eafa2878c7dbe46214cb
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-12-30 09:13:08 +0000

    update software remover tool to 2.5

--- unattended/install/scripts/winxpsp3-extras.bat
+++ unattended/install/scripts/winxpsp3-extras.bat
@@ -34,6 +34,6 @@ todo.pl ".reboot-on 194 %Z%\updates\winxpsp2\WindowsXP-KB898461-x86-%WINLANG%.ex
 :: Microsoft Windows Malicious Software Removal Tool (KB890830)
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V2.4.exe|updates/common/kb890830/windows-kb890830-v2.4.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\kb890830\Windows-KB890830-V2.4.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/a/a/4aa524c6-239d-47ff-860b-5b397199cbf8/Windows-KB890830-V2.5.exe|updates/common/kb890830/windows-kb890830-v2.5.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\kb890830\Windows-KB890830-V2.5.exe /Q"
 
