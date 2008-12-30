6972683598129e3a17fbaeba3776b782be275d0d
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2008-12-30 09:19:03 +0000

    update to 3.5 SP1

--- unattended/install/scripts/dotnet.bat
+++ unattended/install/scripts/dotnet.bat
@@ -1,5 +1,5 @@
 :: INSTALL :: Microsoft .NET 3.5 Redistributable Package (x86)
 :: http://msdn.microsoft.com/en-us/netframework/default.aspx
-:: URL|ALL|http://download.microsoft.com/download/6/0/f/60fc5854-3cb8-4892-b6db-bd4f42510f28/dotnetfx35.exe|updates/common/dotnetfx35.exe
+:: URL|ALL|http://download.microsoft.com/download/2/0/e/20e90413-712f-438c-988e-fdaa79a8ac3d/dotnetfx35.exe|updates/common/dotnetfx35-sp1.exe
 
-todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx35.exe /q /norestart"
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx35-sp1.exe /q /norestart"
