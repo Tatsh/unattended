d497761221fe1ad0e0189653a8d47313c9179621
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-03-16 15:54:27 +0000

    upss binary is 9.10 instead of 9.1

--- unattended/install/scripts/adobe-reader.bat
+++ unattended/install/scripts/adobe-reader.bat
@@ -2,29 +2,29 @@
 @Echo Off
 
 :: Download Adobe Reader full version
-:: URL|ARA|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/ar_AE/AdbeRdr9.1_ar_AE.exe|packages/adobereader/adberdr91_ara.exe
-:: URL|CHS|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/chs/AdbeRdr9.1_zh_CN.exe|packages/adobereader/adberdr91_chs.exe
-:: URL|CHT|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/cht/AdbeRdr9.1_zh_TW.exe|packages/adobereader/adberdr91_cht.exe
-:: URL|CSY|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/cs_CZ/AdbeRdr9.1_cs_CZ.exe|packages/adobereader/adberdr91_csy.exe
-:: URL|DAN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/dan/AdbeRdr9.1_da_DK.exe|packages/adobereader/adberdr91_dan.exe
-:: URL|NLD|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/nld/AdbeRdr9.1_nl_NL.exe|packages/adobereader/adberdr91_nld.exe
-:: URL|ENU|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/enu/AdbeRdr9.1_en_US.exe|packages/adobereader/adberdr91_enu.exe
-:: URL|FIN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/suo/AdbeRdr9.1_fi_FI.exe|packages/adobereader/adberdr91_fin.exe
-:: URL|FRA|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/fra/AdbeRdr9.1_fr_FR.exe|packages/adobereader/adberdr91_fra.exe
-:: URL|DEU|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/deu/AdbeRdr9.1_de_DE.exe|packages/adobereader/adberdr91_deu.exe
-:: URL|ELL|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/el_GR/AdbeRdr9.1_el_GR.exe|packages/adobereader/adberdr91_ell.exe
-:: URL|HEB|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/he_IL/AdbeRdr9.1_he_IL.exe|packages/adobereader/adberdr91_heb.exe
-:: URL|HUN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/hu_HU/AdbeRdr9.1_hu_HU.exe|packages/adobereader/adberdr91_hun.exe
-:: URL|ITA|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/ita/AdbeRdr9.1_it_IT.exe|packages/adobereader/adberdr91_ita.exe
-:: URL|JPN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/jpn/AdbeRdr9.1_ja_JP.exe|packages/adobereader/adberdr91_jpn.exe
-:: URL|KOR|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/kor/AdbeRdr9.1_ko_KR.exe|packages/adobereader/adberdr91_kor.exe
-:: URL|NOR|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/nor/AdbeRdr9.1_nb_NO.exe|packages/adobereader/adberdr91_nor.exe
-:: URL|PLK|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/pl_PL/AdbeRdr9.1_pl_PL.exe|packages/adobereader/adberdr91_plk.exe
-:: URL|PTB|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/ptb/AdbeRdr9.1_pt_BR.exe|packages/adobereader/adberdr91_ptb.exe
-:: URL|RUS|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/ru_RU/AdbeRdr9.1_ru_RU.exe|packages/adobereader/adberdr91_rus.exe
-:: URL|ESN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/esp/AdbeRdr9.1_es_ES.exe|packages/adobereader/adberdr91_esn.exe
-:: URL|SVE|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/sve/AdbeRdr9.1_sv_SE.exe|packages/adobereader/adberdr91_sve.exe
-:: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.1/sve/AdbeRdr9.1_sv_SE.exe|packages/adobereader/adberdr91_trk.exe
+:: URL|ARA|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/ar_AE/AdbeRdr9.1_ar_AE.exe|packages/adobereader/adberdr91_ara.exe
+:: URL|CHS|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/chs/AdbeRdr9.1_zh_CN.exe|packages/adobereader/adberdr91_chs.exe
+:: URL|CHT|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/cht/AdbeRdr9.1_zh_TW.exe|packages/adobereader/adberdr91_cht.exe
+:: URL|CSY|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/cs_CZ/AdbeRdr9.1_cs_CZ.exe|packages/adobereader/adberdr91_csy.exe
+:: URL|DAN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/dan/AdbeRdr9.1_da_DK.exe|packages/adobereader/adberdr91_dan.exe
+:: URL|NLD|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/nld/AdbeRdr9.1_nl_NL.exe|packages/adobereader/adberdr91_nld.exe
+:: URL|ENU|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/enu/AdbeRdr9.1_en_US.exe|packages/adobereader/adberdr91_enu.exe
+:: URL|FIN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/suo/AdbeRdr9.1_fi_FI.exe|packages/adobereader/adberdr91_fin.exe
+:: URL|FRA|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/fra/AdbeRdr9.1_fr_FR.exe|packages/adobereader/adberdr91_fra.exe
+:: URL|DEU|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/deu/AdbeRdr9.1_de_DE.exe|packages/adobereader/adberdr91_deu.exe
+:: URL|ELL|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/el_GR/AdbeRdr9.1_el_GR.exe|packages/adobereader/adberdr91_ell.exe
+:: URL|HEB|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/he_IL/AdbeRdr9.1_he_IL.exe|packages/adobereader/adberdr91_heb.exe
+:: URL|HUN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/hu_HU/AdbeRdr9.1_hu_HU.exe|packages/adobereader/adberdr91_hun.exe
+:: URL|ITA|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/ita/AdbeRdr9.1_it_IT.exe|packages/adobereader/adberdr91_ita.exe
+:: URL|JPN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/jpn/AdbeRdr9.1_ja_JP.exe|packages/adobereader/adberdr91_jpn.exe
+:: URL|KOR|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/kor/AdbeRdr9.1_ko_KR.exe|packages/adobereader/adberdr91_kor.exe
+:: URL|NOR|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/nor/AdbeRdr9.1_nb_NO.exe|packages/adobereader/adberdr91_nor.exe
+:: URL|PLK|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/pl_PL/AdbeRdr9.1_pl_PL.exe|packages/adobereader/adberdr91_plk.exe
+:: URL|PTB|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/ptb/AdbeRdr9.1_pt_BR.exe|packages/adobereader/adberdr91_ptb.exe
+:: URL|RUS|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/ru_RU/AdbeRdr9.1_ru_RU.exe|packages/adobereader/adberdr91_rus.exe
+:: URL|ESN|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/esp/AdbeRdr9.1_es_ES.exe|packages/adobereader/adberdr91_esn.exe
+:: URL|SVE|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/sve/AdbeRdr9.1_sv_SE.exe|packages/adobereader/adberdr91_sve.exe
+:: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.10/sve/AdbeRdr9.1_sv_SE.exe|packages/adobereader/adberdr91_trk.exe
 
 
 :: Download Adobe Reader 9.0.x Language Support
@@ -40,8 +40,8 @@
 :: Download Adobe Reader Taiwanese Fonts
 :: ULR|TWS|ftp://ftp.adobe.com/pub/adobe/reader/win/9.x/9.0/misc/FontPack90_zh_TW.msi|packages/adobereader/misc/fontpack90_nan.msi
 
-:: 9.1 Version
-set adobe_install=%Z%\packages\adobereader\AdbeRdr9.1_%WINLANG%.exe
+:: 9.10 Version
+set adobe_install=%Z%\packages\adobereader\AdbeRdr9.10_%WINLANG%.exe
 if not exist %adobe_install% goto noadberdr
 
 :found_install
