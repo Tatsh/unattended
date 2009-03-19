10cd2765e0a219c8e3043fbb7048b8e851c9debe
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-03-19 22:09:11 +0000

    update version 8.64 and change mirror reported by Steven Blackery
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2959 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/gsview.bat
+++ unattended/install/scripts/gsview.bat
@@ -1,11 +1,11 @@
 :: OPTIONAL: Install Ghostscript and GSview
-:: URL|ALL|http://mirror.cs.wisc.edu/pub/mirrors/ghost/GPL/gs863/gs863w32.exe|packages/ghostscript/gs863w32.exe
-:: URL|ALL|http://mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv49w32.exe|packages/ghostscript/gsv49w32.exe
+:: URL|ALL|http://downloads.sourceforge.net//ghostscript/gs864w32.exe|packages/ghostscript/gs864w32.exe
+:: URL|ALL|http://www.mirrorservice.org/sites/mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv49w32.exe|packages/ghostscript/gsv49w32.exe
 :: Then manually extract to gs and gv folders in the same path
 
 todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv49w32.exe"
 
 todo.pl " start /D%TEMP%\gs /wait %TEMP%\gs\setupgs.exe \"%ProgramFiles%\gs"
-todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gs %Z%\packages\ghostscript\gs863w32.exe"
+todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gs %Z%\packages\ghostscript\gs864w32.exe"
 
