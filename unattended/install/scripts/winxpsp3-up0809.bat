b221d3f1f7f9c43224326868f11161ab0e24f9be
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-03-30 17:54:47 +0000

    update to version 2. Thanks to Steve Kojoukhine for spot it that out.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@2971 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- unattended/install/scripts/winxpsp3-up0809.bat
+++ unattended/install/scripts/winxpsp3-up0809.bat
@@ -1,26 +1,26 @@
+
 :: Security Update for Windows XP (KB938464)
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=e0bd6fbe-f46e-4961-9a79-49ec77d39439>
-:: URL|ARA|http://download.microsoft.com/download/e/a/7/ea7593d8-7420-403d-b096-a26db4e1cb38/WindowsXP-KB938464-x86-ARA.exe|updates/winxpsp3/windowsxp-kb938464-x86-ara.exe
-:: URL|CSY|http://download.microsoft.com/download/5/0/a/50ae20b7-9a0d-4508-9473-d8c76d7d2a8b/WindowsXP-KB938464-x86-CSY.exe|updates/winxpsp3/windowsxp-kb938464-x86-csy.exe
-:: URL|DAN|http://download.microsoft.com/download/f/4/5/f4541c3f-2dd8-428c-8d3a-44d249d88e62/WindowsXP-KB938464-x86-DAN.exe|updates/winxpsp3/windowsxp-kb938464-x86-dan.exe
-:: URL|DEU|http://download.microsoft.com/download/0/9/4/094444e4-b136-4dfd-8fe4-793afc390b08/WindowsXP-KB938464-x86-DEU.exe|updates/winxpsp3/windowsxp-kb938464-x86-deu.exe
-:: URL|ELL|http://download.microsoft.com/download/0/5/1/051527e8-a50c-4464-9fda-31a1c0960339/WindowsXP-KB938464-x86-ELL.exe|updates/winxpsp3/windowsxp-kb938464-x86-ell.exe
-:: URL|ENU|http://download.microsoft.com/download/7/8/1/78141dc7-3b1d-42f3-9aa2-4dca79ad739b/WindowsXP-KB938464-x86-ENU.exe|updates/winxpsp3/windowsxp-kb938464-x86-enu.exe
-:: URL|ESN|http://download.microsoft.com/download/d/4/9/d49d89e1-ea29-4ecf-b6e1-b988970e3db0/WindowsXP-KB938464-x86-ESN.exe|updates/winxpsp3/windowsxp-kb938464-x86-esn.exe
-:: URL|FIN|http://download.microsoft.com/download/0/e/a/0eae4756-7085-46f3-a4df-b962826546d6/WindowsXP-KB938464-x86-FIN.exe|updates/winxpsp3/windowsxp-kb938464-x86-fin.exe
-:: URL|FRA|http://download.microsoft.com/download/8/0/2/802210a3-ace1-4e96-a6f6-117b98e55715/WindowsXP-KB938464-x86-FRA.exe|updates/winxpsp3/windowsxp-kb938464-x86-fra.exe
-:: URL|HEB|http://download.microsoft.com/download/e/4/1/e41e561d-a856-4110-a754-5526a475522e/WindowsXP-KB938464-x86-HEB.exe|updates/winxpsp3/windowsxp-kb938464-x86-heb.exe
-:: URL|HUN|http://download.microsoft.com/download/b/8/9/b8907276-a52f-44e9-b04c-28366e0190f6/WindowsXP-KB938464-x86-HUN.exe|updates/winxpsp3/windowsxp-kb938464-x86-hun.exe
-:: URL|ITA|http://download.microsoft.com/download/d/e/9/de9d886b-d173-48d7-b169-13119436dbf7/WindowsXP-KB938464-x86-ITA.exe|updates/winxpsp3/windowsxp-kb938464-x86-ita.exe
-:: URL|JPN|http://download.microsoft.com/download/b/6/8/b6809d4b-7d97-44c4-a16b-1334b5b7b0b6/WindowsXP-KB938464-x86-JPN.exe|updates/winxpsp3/windowsxp-kb938464-x86-jpn.exe
-:: URL|KOR|http://download.microsoft.com/download/a/1/4/a14f8b50-37dc-435d-bbc7-378a3cc092ab/WindowsXP-KB938464-x86-KOR.exe|updates/winxpsp3/windowsxp-kb938464-x86-kor.exe
-:: URL|NLD|http://download.microsoft.com/download/4/b/9/4b930fd5-193e-4e98-8b24-e13eb3bb1181/WindowsXP-KB938464-x86-NLD.exe|updates/winxpsp3/windowsxp-kb938464-x86-nld.exe
-:: URL|NOR|http://download.microsoft.com/download/7/a/d/7ad32a4d-58f2-4872-9881-dfeb98ff8f10/WindowsXP-KB938464-x86-NOR.exe|updates/winxpsp3/windowsxp-kb938464-x86-nor.exe
-:: URL|PLK|http://download.microsoft.com/download/0/3/1/031ad15c-e3ab-4679-9387-bcb4dc93b8bd/WindowsXP-KB938464-x86-PLK.exe|updates/winxpsp3/windowsxp-kb938464-x86-plk.exe
-:: URL|PTB|http://download.microsoft.com/download/f/6/4/f64a96ee-57a9-4607-a250-89601dba6605/WindowsXP-KB938464-x86-PTB.exe|updates/winxpsp3/windowsxp-kb938464-x86-ptb.exe
-:: URL|PTG|http://download.microsoft.com/download/3/3/f/33f30ff2-0368-4423-a3b7-00b615cd55c6/WindowsXP-KB938464-x86-PTG.exe|updates/winxpsp3/windowsxp-kb938464-x86-ptg.exe
-:: URL|RUS|http://download.microsoft.com/download/f/e/c/fec45297-41a6-4b25-aa41-a60c309f85ab/WindowsXP-KB938464-x86-RUS.exe|updates/winxpsp3/windowsxp-kb938464-x86-rus.exe
-:: URL|SVE|http://download.microsoft.com/download/8/a/4/8a48db02-bd85-4447-b895-f16d7fabfcd9/WindowsXP-KB938464-x86-SVE.exe|updates/winxpsp3/windowsxp-kb938464-x86-sve.exe
-:: URL|TRK|http://download.microsoft.com/download/5/a/9/5a9bbe2d-958b-4be6-be10-4b3ec7e8b9e4/WindowsXP-KB938464-x86-TRK.exe|updates/winxpsp3/windowsxp-kb938464-x86-trk.exe
-todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB938464-x86-%WINLANG%.exe /norestart /passive /nobackup"
-
+:: URL|ARA|http://download.microsoft.com/download/E/A/7/EA7593D8-7420-403D-B096-A26DB4E1CB38/WindowsXP-KB938464-v2-x86-ARA.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/5/0/A/50AE20B7-9A0D-4508-9473-D8C76D7D2A8B/WindowsXP-KB938464-v2-x86-CSY.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/F/4/5/F4541C3F-2DD8-428C-8D3A-44D249D88E62/WindowsXP-KB938464-v2-x86-DAN.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/0/9/4/094444E4-B136-4DFD-8FE4-793AFC390B08/WindowsXP-KB938464-v2-x86-DEU.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/0/5/1/051527E8-A50C-4464-9FDA-31A1C0960339/WindowsXP-KB938464-v2-x86-ELL.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/7/8/1/78141DC7-3B1D-42F3-9AA2-4DCA79AD739B/WindowsXP-KB938464-v2-x86-ENU.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/D/4/9/D49D89E1-EA29-4ECF-B6E1-B988970E3DB0/WindowsXP-KB938464-v2-x86-ESN.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/0/E/A/0EAE4756-7085-46F3-A4DF-B962826546D6/WindowsXP-KB938464-v2-x86-FIN.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/8/0/2/802210A3-ACE1-4E96-A6F6-117B98E55715/WindowsXP-KB938464-v2-x86-FRA.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/E/4/1/E41E561D-A856-4110-A754-5526A475522E/WindowsXP-KB938464-v2-x86-HEB.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/B/8/9/B8907276-A52F-44E9-B04C-28366E0190F6/WindowsXP-KB938464-v2-x86-HUN.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/D/E/9/DE9D886B-D173-48D7-B169-13119436DBF7/WindowsXP-KB938464-v2-x86-ITA.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/B/6/8/B6809D4B-7D97-44C4-A16B-1334B5B7B0B6/WindowsXP-KB938464-v2-x86-JPN.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/A/1/4/A14F8B50-37DC-435D-BBC7-378A3CC092AB/WindowsXP-KB938464-v2-x86-KOR.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/4/B/9/4B930FD5-193E-4E98-8B24-E13EB3BB1181/WindowsXP-KB938464-v2-x86-NLD.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/7/A/D/7AD32A4D-58F2-4872-9881-DFEB98FF8F10/WindowsXP-KB938464-v2-x86-NOR.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-nor.exe
+:: URL|PLK|http://download.microsoft.com/download/0/3/1/031AD15C-E3AB-4679-9387-BCB4DC93B8BD/WindowsXP-KB938464-v2-x86-PLK.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/F/6/4/F64A96EE-57A9-4607-A250-89601DBA6605/WindowsXP-KB938464-v2-x86-PTB.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/3/3/F/33F30FF2-0368-4423-A3B7-00B615CD55C6/WindowsXP-KB938464-v2-x86-PTG.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/F/E/C/FEC45297-41A6-4B25-AA41-A60C309F85AB/WindowsXP-KB938464-v2-x86-RUS.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/8/A/4/8A48DB02-BD85-4447-B895-F16D7FABFCD9/WindowsXP-KB938464-v2-x86-SVE.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/5/A/9/5A9BBE2D-958B-4BE6-BE10-4B3EC7E8B9E4/WindowsXP-KB938464-v2-x86-TRK.exe|updates/winxpsp3/windowsxp-kb938464-v2-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB938464-v2-x86-%WINLANG%.exe /norestart /passive /nobackup"
