c0535880a36488da8502d4aa485eca126c76ae3a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-06-29 00:30:26 +0000

    update to version 10.0.22.87

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL: Install Flash 9.0.124.0 
+:: OPTIONAL: Install Flash 10.0.22.87 
 ::
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10_0_12_36.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10_0_22_87.exe
 ::
 :: Download from <http://www.macromedia.com/downloads/>
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_10_0_12_36.exe /s"
+todo.pl "%Z%\packages\macromedia\install_flash_player_10_0_22_87.exe /s"
