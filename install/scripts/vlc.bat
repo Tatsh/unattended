e82e58c64d1a740118f3ffb4a1eb3d0d921aabde
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-07-08 08:47:29 +0000

    update version 1.0.0

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/0.9.8a/win32/vlc-0.9.8a-win32.exe|packages/vlc-0.9.8a-win32.exe
+:: URL|All|http://downloads.videolan.org/pub/videolan/vlc/1.0.0/win32/vlc-1.0.0-win32.exe|packages/vlc-1.0.0-win32.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc-0.9.8a-win32.exe /S"
+todo.pl "%Z%\packages\vlc-1.0.0-win32.exe /S"
