f3bd8f7146ad75234c25ffefea8294729764a07c
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-11-18 16:47:14 +0000

    Malicious tool version 3.1

--- trunk/install/scripts/ws2k3sp1-updates.bat
+++ trunk/install/scripts/ws2k3sp1-updates.bat
@@ -1280,8 +1280,8 @@ todo.pl ".reboot-on 194 %Z%\updates\ws2k3sp1\WindowsServer2003-KB896423-x86-%WIN
 :: Microsoft Windows Malicious Software Removal Tool (KB890830)
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/windows-kb890830-v2.14.exe|updates/common/kb890830/windows-kb890830-v2.14.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\kb890830\Windows-KB890830-V2.14.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/windows-kb890830-v3.1.exe|updates/common/kb890830/windows-kb890830-v3.1.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\kb890830\Windows-KB890830-V3.1.exe /Q"
 
 :: Security Update for JView Profiler (KB903235)
 :: <http://www.microsoft.com/downloads/details.aspx?FamilyId=F368E231-9918-4881-9F17-60312F82183F>
