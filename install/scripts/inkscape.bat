7258b3b7cbc962309b19848bff287aa83ef36d20
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-11-25 10:45:10 +0000

    update to version 0.47

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
 
-:: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.46.win32.exe|packages/inkscape/inkscape-0.46.win32.exe
+:: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.47-3.exe|packages/inkscape/inkscape-0.47-3.win32.exe
 :: URL|All|http://download.openclipart.org/downloads/openclipart-clipart-2007-02-05.tar.gz|packages/inkscape/openclipart-clipart-2007-02-05.tar.gz
 :: URL|All|http://www.openclipart.org/downloads/0.18/openclipart-0.18-win32.exe|packages/inkscape/openclipart-0.18-win32.exe
 @Echo off
@@ -11,4 +11,4 @@ todo.pl "shortcut.pl  \"%ProgramFiles%\Open Clip Art Library\clipart\" special:A
 todo.pl "%Z%\packages\inkscape\openclipart-0.18-win32.exe /S"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
-todo.pl "%Z%\packages\inkscape\inkscape-0.46.win32.exe /S"
+todo.pl "%Z%\packages\inkscape\inkscape-0.47-3.win32.exe /S"
