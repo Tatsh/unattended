3b3db8c5143e3929d76e66a72b2256d380909a0e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-12-06 02:39:10 +0000

    update to patch version 17
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3071 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/sun-jre.bat
+++ install/scripts/sun-jre.bat
@@ -3,11 +3,11 @@
 :: See:
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://java.com/en/download/help/silent_install.xml>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=33889|packages/jre/jre-6u16-windows-i586-s.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=35684|packages/jre/jre-6u17-windows-i586-s.exe
 
 @Echo off
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableAutoUpdateCheck\" /t REG_DWORD /d 0"
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJavaUpdate\" /t REG_DWORD /d 0"
 
-todo.pl "%Z%\packages\jre\jre-6u16-windows-i586-s.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+todo.pl "%Z%\packages\jre\jre-6u17-windows-i586-s.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
 
