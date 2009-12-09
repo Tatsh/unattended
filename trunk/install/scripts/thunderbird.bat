00f35c4da8cdadb85a1874d46d805c3ce2c74302
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2009-12-09 00:02:55 +0000

    Update to version 3.0

--- trunk/install/scripts/thunderbird.bat
+++ trunk/install/scripts/thunderbird.bat
@@ -1,29 +1,29 @@
-:: OPTIONAL: Install Thunderbird 2.0.0.23
+:: OPTIONAL: Install Thunderbird 3.0
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/ca/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/cs/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/da/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/de/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/el/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/en-GB/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/en-US/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/es-ES/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/es-AR/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/fi/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/fr/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/he/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/hu/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/it/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/ja/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/ko/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/nl/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-nld.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/pl/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/pt-BR/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/ru/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/sl/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/sv-SE/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/2.0.0.23/win32/tr/Thunderbird%20Setup%202.0.0.23.exe|packages/mozilla/thunderbird-20023-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/ca/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/cs/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/da/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/de/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/el/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/en-GB/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/en-US/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/es-ES/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/es-AR/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/fi/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/fr/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/he/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/hu/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/it/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/ja/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/ko/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/nl/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-nld.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/pl/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/pt-BR/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/ru/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/sl/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/sv-SE/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/3.0/win32/tr/Thunderbird%20Setup%203.0.exe|packages/mozilla/thunderbird-30-trk.exe
 
 @echo off
 
-todo.pl "%Z%\packages\mozilla\thunderbird-20023-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-30-%WINLANG%.exe -ms -ira"
