b38281daa8f07338aa788cf8c2609dc99ac15949
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-01-13 10:27:57 +0000

    Update to version 10.0.42.34
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3093 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,8 +1,7 @@
-:: OPTIONAL: Install Flash 10.0.22.87 
+:: OPTIONAL: Install Flash 10.0.42.34 
+:: HOME: http://get.adobe.com/es/flashplayer/otherversions/
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10_0_42.34.exe
 ::
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10_0_22_87.exe
-::
-:: Download from <http://www.macromedia.com/downloads/>
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_10_0_22_87.exe /s"
+todo.pl "%Z%\packages\macromedia\install_flash_player_10_0_42.34.exe /s"
