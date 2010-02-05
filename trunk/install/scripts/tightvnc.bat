1c17bd9e8dd1b523faa421ae578681c42bc6f514
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-02-05 18:25:59 +0000

    update to version 1.3.10

--- trunk/install/scripts/tightvnc.bat
+++ trunk/install/scripts/tightvnc.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install TightVNC
 :: HOME: http://www.tightvnc.com/
-:: URL|ALL|http://downloads.sourceforge.net/vnc-tight/tightvnc-1.3.9-setup.exe|packages/tightvnc-1.3.9-setup.exe
+:: URL|ALL|http://downloads.sourceforge.net/vnc-tight/tightvnc-1.3.10-setup.exe|packages/tightvnc-1.3.10-setup.exe
 @Echo off
-todo.pl "%Z%\packages\tightvnc-1.3.9-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
+todo.pl "%Z%\packages\tightvnc-1.3.10-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
