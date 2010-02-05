2c9cbb31529333e0256d6f434a28d6b69129acb7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-02-05 18:28:01 +0000

    update drivers up to jan 2010

--- install/scripts/driverpacks.bat
+++ install/scripts/driverpacks.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Download Bâshrat the Sneaky drivers from [WWW] DriverPacks.net 
-:: HOME: http://driverpacks.net/DriverPacks/overview.php
+:: HOME: http://driverpacks.net/driverpacks/windows/xp/x86
 
 :: URL|All|http://downloads.driverpacks.net/xp/x86/DP_Chipset_wnt5_x86-32_902.7z|drivers/DP_Chipset_wnt5_x86-32_902.7z
 :: URL|All|http://downloads.driverpacks.net/xp/x86/DP_CPU_wnt5_x86-32_912.7z|drivers/DP_CPU_wnt5_x86-32_912.7z
@@ -13,19 +13,20 @@
 :: URL|All|http://driverpacks.sytes.net/driverpacks/Nightlies/DP_Sound_B_wnt5_x86-32_907rc1.7z|drivers/DP_Sound_B_wnt5_x86-32_907rc1.7z
 :: URL|All|http://downloads.driverpacks.net/DP_WLAN_wnt5_x86-32_806.7z|drivers/DP_WLAN_wnt5_x86-32_806.7z
 
-:: 3RD Party June-2008
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Bluetooth_wnt5_x86-32_081028.7z|drivers/DP_Bluetooth_wnt5_x86-32_081028.7z
+:: 3RD Party January-2010
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Bluetooth_wnt5_x86-32_910.7z|drivers/DP_Bluetooth_wnt5_x86-32_910.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Broadband_wnt5_x86-32_70416.7z|drivers/DP_Broadband_wnt5_x86-32_70416.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_HID_wnt5_x86-32_90204.7z|drivers/DP_HID_wnt5_x86-32_90204.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Misc_wnt5_x86-32_90322.7z|drivers/DP_Misc_wnt5_x86-32_90322.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Modem_wnt5_x86-32_90328.7z|drivers/DP_Modem_wnt5_x86-32_90328.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Monitor_wnt5_x86-32_81227.7z|drivers/DP_Monitor_wnt5_x86-32_081227.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_HID_wnt5_x86-32_91101.7z|drivers/DP_HID_wnt5_x86-32_91101.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Misc_wnt5_x86-32_90909.7z|drivers/DP_Misc_wnt5_x86-32_90909.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Modem_wnt5_x86-32_100101.7z|drivers/DP_Modem_wnt5_x86-32_100101.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Monitor_wnt5_x86-32_91101.7z|drivers/DP_Monitor_wnt5_x86-32_91101.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Phone_wnt5_x86-32_80207.7z|drivers/DP_Phone_wnt5_x86-32_80207.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_PrintersCanonInkjet_wnt5_x86-32_61105.7z|drivers/DP_PrintersCanonInkjet_wnt5_x86-32_61105.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_PrintersCanonLaserjet_wnt5_x86-32_61020.7z|drivers/DP_PrintersCanonLaserjet_wnt5_x86-32_61020.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_PrintersHP_wnt5_x86-32_71126.7z|drivers/DP_PrintersHP_wnt5_x86-32_71126.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Scanner_wnt5_x86-32_60831.7z|drivers/DP_Scanner_wnt5_x86-32_60831.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_TV_wnt5_x86-32_80908.7z|drivers/DP_TV_wnt5_x86-32_80908.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_TV_wnt5_x86-32_90915.7z|drivers/DP_TV_wnt5_x86-32_90915.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_USB_wnt5_x86-32_80911.7z|drivers/DP_USB_wnt5_x86-32_80911.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_WebCam_wnt5_x86-32_81127.7z|drivers/DP_WebCam_wnt5_x86-32_81127.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Virtual_wnt5_x86-32_90225.7z|drivers/DP_Virtual_wnt5_x86-32_90225.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_WebCam_wnt5_x86-32_100104.7z|drivers/DP_WebCam_wnt5_x86-32_100104.7z
 
