f8dd57640c6956fcc7219eb0d77c7ddf64a3ee5b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-02-06 17:44:04 +0000

    Missing files from previous commits
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3156 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ install/scripts/winxpsp3-up1001.bat
@@ -0,0 +1,52 @@
+
+:: Security Update for Windows XP (KB972270)
+:: <http://www.microsoft.com/downloads/details.aspx?familyid=793a6b3f-7660-40be-b7d5-7b0eec55e1cd>
+:: URL|ARA|http://download.microsoft.com/download/2/E/B/2EBF47F8-944A-4742-88FB-9952AD0C3811/WindowsXP-KB972270-x86-ARA.exe|updates/winxpsp3/windowsxp-kb972270-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/E/7/B/E7BBD84E-91FD-492D-B0AF-D9E26FC67DAC/WindowsXP-KB972270-x86-CSY.exe|updates/winxpsp3/windowsxp-kb972270-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/7/F/E/7FE778D5-7CD7-41E7-88A7-C1E44B86746E/WindowsXP-KB972270-x86-DAN.exe|updates/winxpsp3/windowsxp-kb972270-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/6/7/6/67637E7F-827A-4317-B37F-9B5C48953C0C/WindowsXP-KB972270-x86-DEU.exe|updates/winxpsp3/windowsxp-kb972270-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/7/2/0/72051CCB-5689-4298-B165-4735A65D1A39/WindowsXP-KB972270-x86-ELL.exe|updates/winxpsp3/windowsxp-kb972270-x86-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/E/1/3/E13622B6-84F8-40D3-9D7A-958512BD4D52/WindowsXP-KB972270-x86-ENU.exe|updates/winxpsp3/windowsxp-kb972270-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/D/D/8/DD8D4E33-BA01-4F52-B252-8D3E96EE1042/WindowsXP-KB972270-x86-ESN.exe|updates/winxpsp3/windowsxp-kb972270-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/7/6/B/76BBB91D-48A2-4A11-AE0D-0CA071BF9577/WindowsXP-KB972270-x86-FIN.exe|updates/winxpsp3/windowsxp-kb972270-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/B/A/C/BACBB668-F751-4F13-80B4-E1961F6FC772/WindowsXP-KB972270-x86-FRA.exe|updates/winxpsp3/windowsxp-kb972270-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/B/A/3/BA33BEAB-D23B-4AE9-A8AD-0A810EF84EEE/WindowsXP-KB972270-x86-HEB.exe|updates/winxpsp3/windowsxp-kb972270-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/B/C/5/BC590735-3310-47C2-912C-829E4A9A9DBD/WindowsXP-KB972270-x86-HUN.exe|updates/winxpsp3/windowsxp-kb972270-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/B/6/2/B6266E90-72A0-499F-B8B1-A8AB495EB6D6/WindowsXP-KB972270-x86-ITA.exe|updates/winxpsp3/windowsxp-kb972270-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/4/6/2/4625666A-16C0-468C-A09C-2ACD44516CCE/WindowsXP-KB972270-x86-JPN.exe|updates/winxpsp3/windowsxp-kb972270-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/0/A/E/0AE21747-F5B9-4005-A016-46C9D9F0B896/WindowsXP-KB972270-x86-KOR.exe|updates/winxpsp3/windowsxp-kb972270-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/8/8/E/88ED83F0-9D42-414C-A4C2-59E9F91D1836/WindowsXP-KB972270-x86-NLD.exe|updates/winxpsp3/windowsxp-kb972270-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/A/9/1/A91CEC95-4C6B-4685-B0F3-8588311CC7F5/WindowsXP-KB972270-x86-NOR.exe|updates/winxpsp3/windowsxp-kb972270-x86-nor.exe
+:: URL|PLK|http://download.microsoft.com/download/0/F/C/0FC7CD92-AE5F-4AC5-B83B-365B3B478A14/WindowsXP-KB972270-x86-PLK.exe|updates/winxpsp3/windowsxp-kb972270-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/0/0/8/0087501A-DA5D-4E46-848E-E32B7B7D2553/WindowsXP-KB972270-x86-PTB.exe|updates/winxpsp3/windowsxp-kb972270-x86-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/2/C/9/2C9C2D15-B3E9-4ADB-9446-1C5B0A18CA37/WindowsXP-KB972270-x86-PTG.exe|updates/winxpsp3/windowsxp-kb972270-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/8/4/3/843DCE8B-8A3E-4BFA-AF16-99E8A66326F9/WindowsXP-KB972270-x86-RUS.exe|updates/winxpsp3/windowsxp-kb972270-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/D/7/8/D78B12FA-2CE9-43B1-9D3A-2D0B52B61C24/WindowsXP-KB972270-x86-SVE.exe|updates/winxpsp3/windowsxp-kb972270-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/D/B/9/DB9A91B8-FCB0-4CB6-8A69-61B557E7F1E3/WindowsXP-KB972270-x86-TRK.exe|updates/winxpsp3/windowsxp-kb972270-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB972270-x86-%WINLANG%.exe /norestart /passive /nobackup"
+
+:: Cumulative Security Update for Internet Explorer for Windows XP (KB978207)
+:: <http://www.microsoft.com/downloads/details.aspx?FamilyID=207eecad-6e84-48e6-ae18-6794a3618ee0>
+:: URL|ARA|http://download.microsoft.com/download/C/6/5/C6530A86-ED43-4E63-A38E-5455B149D317/WindowsXP-KB978207-x86-ARA.exe|updates/winxpsp3/windowsxp-kb978207-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/5/F/A/5FA8FFB2-A7B7-4A8F-B2E7-01828299383C/WindowsXP-KB978207-x86-CSY.exe|updates/winxpsp3/windowsxp-kb978207-x86-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/C/1/B/C1BA0A26-54E8-4902-AC78-F9E2D78B8CC7/WindowsXP-KB978207-x86-DAN.exe|updates/winxpsp3/windowsxp-kb978207-x86-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/F/D/7/FD7CDC68-1CBB-4EB6-B8E5-D0988C4EF1A8/WindowsXP-KB978207-x86-DEU.exe|updates/winxpsp3/windowsxp-kb978207-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/8/B/0/8B0669F0-2E22-41EC-A6B4-C81C71C7150B/WindowsXP-KB978207-x86-ELL.exe|updates/winxpsp3/windowsxp-kb978207-x86-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/E/F/C/EFC57934-BFC3-42A9-B17B-62AB3D5662D5/WindowsXP-KB978207-x86-ENU.exe|updates/winxpsp3/windowsxp-kb978207-x86-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/F/7/B/F7BC621F-7878-42E2-A672-0E1B55E4EDE9/WindowsXP-KB978207-x86-ESN.exe|updates/winxpsp3/windowsxp-kb978207-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/7/B/8/7B81389F-AFF8-4E0B-84B5-60BA0F721DD4/WindowsXP-KB978207-x86-FIN.exe|updates/winxpsp3/windowsxp-kb978207-x86-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/A/2/2/A22121AB-E4CA-4A1D-8A2A-5CC59C020CF6/WindowsXP-KB978207-x86-FRA.exe|updates/winxpsp3/windowsxp-kb978207-x86-fra.exe
+:: URL|HEB|http://download.microsoft.com/download/E/D/5/ED580FF9-15D6-4918-AEA0-6B40DE6CDD87/WindowsXP-KB978207-x86-HEB.exe|updates/winxpsp3/windowsxp-kb978207-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/8/7/3/873CABA0-498D-4780-B7BA-886DE544DC8B/WindowsXP-KB978207-x86-HUN.exe|updates/winxpsp3/windowsxp-kb978207-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/3/7/2/372AD0CA-BD9E-4EDB-8A2A-59608065F2D6/WindowsXP-KB978207-x86-ITA.exe|updates/winxpsp3/windowsxp-kb978207-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/1/C/E/1CE308A6-6878-45F2-B815-391F9ED41A32/WindowsXP-KB978207-x86-JPN.exe|updates/winxpsp3/windowsxp-kb978207-x86-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/C/7/E/C7E50FA8-DA5E-4296-9794-C4043F7180CB/WindowsXP-KB978207-x86-KOR.exe|updates/winxpsp3/windowsxp-kb978207-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/1/E/9/1E9BB524-4245-4A89-8DC6-CB2155F1A22B/WindowsXP-KB978207-x86-NLD.exe|updates/winxpsp3/windowsxp-kb978207-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/8/E/A/8EA11063-3D6C-4ED7-AAD7-D8ED9AC00BF5/WindowsXP-KB978207-x86-NOR.exe|updates/winxpsp3/windowsxp-kb978207-x86-nor.exe
+:: URL|PLK|http://download.microsoft.com/download/9/7/8/97835247-4F3B-4856-9EE2-32DA2E1BCE68/WindowsXP-KB978207-x86-PLK.exe|updates/winxpsp3/windowsxp-kb978207-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/5/B/B/5BB0220D-EAFC-4694-8E38-E937098E4F9B/WindowsXP-KB978207-x86-PTB.exe|updates/winxpsp3/windowsxp-kb978207-x86-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/C/F/C/CFC18C69-AAA8-497D-8571-AFE1FCE3ACDA/WindowsXP-KB978207-x86-PTG.exe|updates/winxpsp3/windowsxp-kb978207-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/4/6/4/464BD20A-5766-4A4E-B0D2-4184C1C3A23F/WindowsXP-KB978207-x86-RUS.exe|updates/winxpsp3/windowsxp-kb978207-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/F/0/9/F097E8DC-D1CA-4AB9-805B-6543BC7E20CC/WindowsXP-KB978207-x86-SVE.exe|updates/winxpsp3/windowsxp-kb978207-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/8/D/0/8D052142-701E-4180-B99E-F1462C80264A/WindowsXP-KB978207-x86-TRK.exe|updates/winxpsp3/windowsxp-kb978207-x86-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB978207-x86-%WINLANG%.exe /norestart /passive /nobackup"
