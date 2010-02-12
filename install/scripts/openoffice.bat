dbb5aff965119bec9dfa7a719fcfd19847d73c49
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-02-12 01:22:37 +0000

    update to version 3.2

--- install/scripts/openoffice.bat
+++ install/scripts/openoffice.bat
@@ -1,21 +1,21 @@
 :: OPTIONAL: Install OpenOffice.org 
 
-:: Full install sets Localized Binaries for 3.1.1
+:: Full install sets Localized Binaries for 3.2
 :: http://download.openoffice.org/other.html
 
-:: URL|ENU|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=en-US&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_enu.exe
-:: URL|DAN|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=da&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_dan.exe
-:: URL|DEU|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=de&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_deu.exe
-:: URL|ESN|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=es&version=3.1.0|packages/openoffice/OOo_3.1.0_Win32Intel_install_esn.exe
-:: URL|FRA|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=fr&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_fra.exe
-:: URL|ITA|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=it&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_ita.exe
-:: URL|JPN|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=ja&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_jpn.exe
-:: URL|NOR|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=nb&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_nor.exe
-:: URL|NON|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=nn&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_non.exe
-:: URL|NLD|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=nl&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_nld.exe
-:: URL|KOR|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=ko&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_kor.exe
-:: URL|PLK|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=pl&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_plk.exe
-:: URL|RUS|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=ru&version=3.1.1|packages/openoffice/OOo_3.1.1_Win32Intel_install_rus.exe
+:: URL|ENU|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=en-US&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_enu.exe
+:: URL|DAN|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=da&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_dan.exe
+:: URL|DEU|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=de&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_deu.exe
+:: URL|ESN|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=es&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_esn.exe
+:: URL|FRA|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=fr&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_fra.exe
+:: URL|ITA|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=it&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_ita.exe
+:: URL|JPN|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=ja&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_jpn.exe
+:: URL|NOR|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=nb&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_nor.exe
+:: URL|NON|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=nn&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_non.exe
+:: URL|NLD|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=nl&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_nld.exe
+:: URL|KOR|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=ko&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_kor.exe
+:: URL|PLK|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=pl&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_plk.exe
+:: URL|RUS|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=ru&version=3.2|packages/openoffice/OOo_3.2_Win32Intel_install_rus.exe
 :: URL|SVE|http://openoffice.bouncer.osuosl.org/?product=OpenOffice.org&os=win&lang=sv&version=3.1.0|packages/openoffice/OOo_3.1.0_Win32Intel_install_sve.exe
 :: URL|PTB|ftp://ftp.broffice.org/stable/3.1.1/BrOOo_3.1.1_Win32Intel_install_pt-BR.exe|packages/openoffice/OOo_3.1.1_Win32Intel_install_ptb.exe
 :: URL|CHS|http://download.services.openoffice.org/files/localized/zh-cn/3.1.1/OOo_3.1.1_Win32Intel_install_zh-CN.exe|packages/openoffice/OOo_3.1.1_Win32Intel_install_chs.exe
@@ -33,7 +33,7 @@
 
 :: ==== TEMPLATES ====
 :: FIXME: some template pack has several version. We forget about those to keep it simple
-:: URL|ESN|http://extensions.services.openoffice.org/files/301/1/Sun_ODF_Template_Pack_es.oxt|packages/openoffice/ext/sun-odf-template-pack-esn.oxt
+:: URL|ESN|http://extensions.services.openoffice.org/files/3.2/Sun_ODF_Template_Pack_es.oxt|packages/openoffice/ext/sun-odf-template-pack-esn.oxt
 :: URL|ENU|http://extensions.services.openoffice.org/files/950/0/Sun_ODF_Template_Pack2_en-US.oxt|packages/openoffice/ext/sun-odf-template-pack-enu.oxt
 :: URL|DEU|http://extensions.services.openoffice.org/files/955/0/Sun_ODF_Template_Pack2_de.oxt|packages/openoffice/ext/sun-odf-template-pack-deu.oxt
 :: URL|FRA|http://extensions.services.openoffice.org/files/297/0/Sun_ODF_Template_Pack_fr.oxt|packages/openoffice/ext/sun-odf-template-pack-fra.oxt
@@ -51,11 +51,12 @@
 todo.pl "start /D\"%ProgramFiles%\OpenOffice.org 3\program\" unopkg.com add --shared %Z%\packages\openoffice\ext\DisableFirstStartWzd_ooo31.oxt"
 
 :: MAIN PROGRAM - Sometimes i18n version does not have the most up to date.
-if not exist "%Z%\packages\openoffice\OOo_3.1.1_Win32Intel_install_%WINLANG%.exe" goto oldversion
-set ooo_install=%Z%\packages\openoffice\OOo_3.1.1_Win32Intel_install_%WINLANG%.exe
+if not exist "%Z%\packages\openoffice\OOo_3.2_Win32Intel_install_%WINLANG%.exe" goto oldversion
+set ooo_install=%Z%\packages\openoffice\OOo_3.2_Win32Intel_install_%WINLANG%.exe
+set ooo_link=special:AllUsersDesktop\"\OpenOffice.org 3.2.lnk\"
 
 :oldversion
-set ooo_install=%Z%\packages\openoffice\OOo_3.1.0_Win32Intel_install_%WINLANG%.exe
+set ooo_install=%Z%\packages\openoffice\OOo_3.1.1_Win32Intel_install_%WINLANG%.exe
 set ooo_link=special:AllUsersDesktop\"\OpenOffice.org 3.1.lnk\"
 
 :: Remove link from desktop 
