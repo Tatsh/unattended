9c8f4594f98f7ba23d4b819f943e88dc11321293
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-02-13 18:29:32 +0000

    add firewall rule
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3170 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/tightvnc.bat
+++ install/scripts/tightvnc.bat
@@ -2,4 +2,7 @@
 :: HOME: http://www.tightvnc.com/
 :: URL|ALL|http://downloads.sourceforge.net/vnc-tight/tightvnc-1.3.10-setup.exe|packages/tightvnc-1.3.10-setup.exe
 @Echo off
+
+:: Add rule to the firewall to allow port 5900 
+todo.pl "netsh firewall add portopening protocol = TCP port = 5900 name = TightVNC mode = enable"
 todo.pl "%Z%\packages\tightvnc-1.3.10-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install" "startup-type.pl manual winvnc"
