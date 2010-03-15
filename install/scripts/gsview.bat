e550162dde3cc215835c3220e007a19eda6a2112
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-03-15 23:42:00 +0000

    update url

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -1,4 +1,5 @@
 :: OPTIONAL: Install Ghostscript and GSview
+:: HOME: http://pages.cs.wisc.edu/~ghost/
 :: URL|ALL|http://downloads.sourceforge.net/ghostscript/gs864w32.exe|packages/ghostscript/gs864w32.exe
 :: URL|ALL|http://www.mirrorservice.org/sites/mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv49w32.exe|packages/ghostscript/gsv49w32.exe
 :: Then manually extract to gs and gv folders in the same path
