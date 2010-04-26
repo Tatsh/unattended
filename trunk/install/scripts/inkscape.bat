e7ff1e675c46222503b82125e22da39b7199d01f
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-04-26 14:05:35 +0000

    use executable instead of zip file

--- trunk/install/scripts/inkscape.bat
+++ trunk/install/scripts/inkscape.bat
@@ -2,12 +2,12 @@
 :: HOME: http://www.inkscape.org/
 
 :: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.47-3.exe|packages/inkscape/inkscape-0.47-3.win32.exe
-:: URL|All|http://www.openclipart.org/downloads/2.0/openclipart-2.0.zip|packages/inkscape/openclipart-2.0.zip
+:: URL|All|http://download.openclipart.org/downloads/2.0/openclipart-2.0-win32.exe|packages/inkscape/openclipart-2.0-win32.exe
 @Echo off
 
 
 ::todo.pl "shortcut.pl  \"%ProgramFiles%\Open Clip Art Library\clipart\" special:AllUsersDesktop\OpenClipArt"
-::todo.pl "%Z%\packages\inkscape\openclipart-0.18-win32.exe /S"
+todo.pl "%Z%\packages\inkscape\openclipart-2.0-win32.exe /S"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
 todo.pl "%Z%\packages\inkscape\inkscape-0.47-3.win32.exe /S"
