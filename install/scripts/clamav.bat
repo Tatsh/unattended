d0e690b0753e7ee134334677dbbdf8e7423ac431
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-04-26 14:18:03 +0000

    Add Clam Av for windows·
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3199 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ install/scripts/clamav.bat
@@ -0,0 +1,7 @@
+:: OPTIONAL: install clamav
+:: HOME: http://www.clamav.net/lang/en/about/win32/ 
+
+:: URL|ALL|http://www.clamav.net/win32/clam-latest-32.exe|packages/clamwin/clam-latest-32.exe
+@echo off
+
+todo.pl "%Z%\packages\clamwin\clam-latest-32.exe /S"
