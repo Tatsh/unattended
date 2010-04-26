9648976c41ce509290ef3c0ea602bb2497e076e5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-04-26 14:03:46 +0000

    add openclip art version 2.0

--- trunk/install/scripts/inkscape.bat
+++ trunk/install/scripts/inkscape.bat
@@ -2,13 +2,12 @@
 :: HOME: http://www.inkscape.org/
 
 :: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.47-3.exe|packages/inkscape/inkscape-0.47-3.win32.exe
-:: URL|All|http://download.openclipart.org/downloads/openclipart-clipart-2007-02-05.tar.gz|packages/inkscape/openclipart-clipart-2007-02-05.tar.gz
-:: URL|All|http://www.openclipart.org/downloads/0.18/openclipart-0.18-win32.exe|packages/inkscape/openclipart-0.18-win32.exe
+:: URL|All|http://www.openclipart.org/downloads/2.0/openclipart-2.0.zip|packages/inkscape/openclipart-2.0.zip
 @Echo off
 
 
-todo.pl "shortcut.pl  \"%ProgramFiles%\Open Clip Art Library\clipart\" special:AllUsersDesktop\OpenClipArt"
-todo.pl "%Z%\packages\inkscape\openclipart-0.18-win32.exe /S"
+::todo.pl "shortcut.pl  \"%ProgramFiles%\Open Clip Art Library\clipart\" special:AllUsersDesktop\OpenClipArt"
+::todo.pl "%Z%\packages\inkscape\openclipart-0.18-win32.exe /S"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
 todo.pl "%Z%\packages\inkscape\inkscape-0.47-3.win32.exe /S"
