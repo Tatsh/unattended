3022d812c903f6d7fb94101411417223968322b1
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-04-26 14:14:57 +0000

    security updates February 2010. Thanks to Johannes Singer.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3198 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/winxpsp3-updates.bat
+++ install/scripts/winxpsp3-updates.bat
@@ -8,6 +8,7 @@ todo.pl "%Z%\scripts\winxpsp3-extras.bat"
 :: Get a list from Microsoft Security Bulletins
 :: http://www.microsoft.com/technet/security/current.aspx
 
+todo.pl "%Z%\scripts\winxpsp3-up1002.bat"
 todo.pl "%Z%\scripts\winxpsp3-up1001.bat"
 todo.pl "%Z%\scripts\winxpsp3-up0912.bat"
 todo.pl "%Z%\scripts\winxpsp3-up0911.bat"
