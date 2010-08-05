4a2c5cdf160f3cec583668932fe263460409e18a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-08-05 22:50:12 +0000

    add clamav url for 64 binary

--- install/scripts/clamav.bat
+++ install/scripts/clamav.bat
@@ -2,6 +2,7 @@
 :: HOME: http://www.clamav.net/lang/en/about/win32/ 
 
 :: URL|ALL|http://www.clamav.net/win32/clam-latest-32.exe|packages/clamwin/clam-latest-32.exe
+:: URL|ALL|http://www.clamav.net/win32/clam-latest-64.exe|packages/clamwin/clam-latest-64.exe
 @echo off
 
 todo.pl "%Z%\packages\clamwin\clam-latest-32.exe /S"
