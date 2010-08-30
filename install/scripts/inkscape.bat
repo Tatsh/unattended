616d610497ed93102964d9a59a8bd882b4191844
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-08-30 18:58:43 +0000

    inkscape: version 0.48
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3247 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
 
-:: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.47-3.exe|packages/inkscape/inkscape-0.47-3.win32.exe
+:: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.48.0-1.exe|packages/inkscape/inkscape-0.48.0-1.win32.exe
 :: URL|All|http://download.openclipart.org/downloads/2.0/openclipart-2.0-win32.exe|packages/inkscape/openclipart-2.0-win32.exe
 @Echo off
 
@@ -10,4 +10,4 @@
 todo.pl "%Z%\packages\inkscape\openclipart-2.0-win32.exe /S"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
-todo.pl "%Z%\packages\inkscape\inkscape-0.47-3.win32.exe /S"
+todo.pl "%Z%\packages\inkscape\inkscape-0.48.0-1.win32.exe /S"
