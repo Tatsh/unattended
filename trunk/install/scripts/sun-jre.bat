a8877d214def57e369c006d8cddaeb576592ecdb
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-10-18 12:18:09 +0000

    JRE: update patch 22

--- trunk/install/scripts/sun-jre.bat
+++ trunk/install/scripts/sun-jre.bat
@@ -3,12 +3,13 @@
 :: See:
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://java.sun.com/javase/6/webnotes/install/jre/silent.html#running>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=41290|packages/jre/jre-6u21-windows-i586-s.exe
+http://javadl.sun.com/webapps/download/AutoDL?BundleId=42751
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=42732|packages/jre/jre-6u22-windows-i586-s.exe
 
 @Echo off
 todo.pl "reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"SunJavaUpdateSched\""
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableAutoUpdateCheck\" /t REG_DWORD /d 0"
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJavaUpdate\" /t REG_DWORD /d 0"
 
-todo.pl "%Z%\packages\jre\jre-6u21-windows-i586-s.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+todo.pl "%Z%\packages\jre\jre-6u22-windows-i586-s.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
 
