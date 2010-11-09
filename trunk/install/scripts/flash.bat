77f72ff0f6aa3c3ca853f421e107cdc724588f5b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-11-09 14:09:47 +0000

    update flash version.

--- trunk/install/scripts/flash.bat
+++ trunk/install/scripts/flash.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Flash 10.1.53.64 
+:: OPTIONAL: Install Flash 10.1.85.3 
 :: HOME: http://get.adobe.com/es/flashplayer/otherversions/
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10.1.53.64.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10.1.85.3.exe
 ::
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_10.1.53.64.exe -install"
+todo.pl "%Z%\packages\macromedia\install_flash_player_10.1.85.3.exe -install"
