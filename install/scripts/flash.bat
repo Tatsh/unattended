964bc38b0b181db52a566ede184aa327a6fdb08c
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-11-29 13:01:48 +0000

    Adobe Flash: change names to reflect current player. Fix #46

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Flash 10.1.85.3 
+:: OPTIONAL: Install Flash 10.1.102.64 
 :: HOME: http://get.adobe.com/es/flashplayer/otherversions/
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10.1.85.3.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10.1.102.64.exe
 ::
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_10.1.85.3.exe -install"
+todo.pl "%Z%\packages\macromedia\install_flash_player_10.1.102.64.exe -install"
