c7f29a0f323a242f73756a2e4296cd18834a0dd1
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-11-29 12:57:33 +0000

    7zip: update to v9.20. Fix bug #47
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3277 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/7-zip.bat
+++ install/scripts/7-zip.bat
@@ -1,11 +1,9 @@
 :: OPTIONAL: Install 7-Zip
 :: HOME: http://sevenzip.sourceforge.net/
-:: URL|ALL|http://downloads.sourceforge.net/sevenzip/7z465.msi|packages/sevenzip/7z465.msi
+:: URL|ALL|http://downloads.sourceforge.net/sevenzip/7z920.msi|packages/sevenzip/7z920.msi
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
-
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\sevenzip\7z465.msi REBOOT=ReallySuppress"
 
 REM Set 7zip as default archived file manager
 SETLOCAL
@@ -20,5 +18,7 @@ FOR %%j IN (%Extn%) DO (
 )
 REG ADD %SC%\7-Zip.001\DefaultIcon /VE /D "\"%PROGRAMFILES%\7-Zip\Formats\split.dll\"" /F
 ENDLOCAL
+
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\sevenzip\7z920.msi REBOOT=ReallySuppress"
 
 :done
