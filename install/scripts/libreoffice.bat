5e3f0f1adbbcb57de1909168fc2f212b3725dc93
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2010-12-23 05:46:39 +0000

    LibreOffice: RC2 Release

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,9 +1,9 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.documentfoundation.org/download/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/testing/3.3.0-rc1/win/x86/LibO_3.3.0rc1_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.0rc1_Win_x86_install_multi.exe
+::URL|ALL|http://download.documentfoundation.org/libreoffice/testing/3.3.0-rc2/win/x86/LibO_3.3.0rc2_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.0rc2_Win_x86_install_multi.exe
 
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\LibO_3.3.0rc1_Win_x86_install_multi.exe /S"
+todo.pl "%Z%\packages\libreoffice\LibO_3.3.0rc2_Win_x86_install_multi.exe /S"
 
