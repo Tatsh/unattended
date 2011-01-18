074fbf5369fa77822e703665370cf64a4256c65e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-18 01:52:50 +0000

    update to version 2.0.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3289 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/tightvnc.bat
+++ install/scripts/tightvnc.bat
@@ -1,10 +1,9 @@
 :: OPTIONAL: Install TightVNC
 :: HOME: http://www.tightvnc.com/
-:: URL|ALL|http://downloads.sourceforge.net/vnc-tight/tightvnc-1.3.10-setup.exe|packages/tightvnc-1.3.10-setup.exe
+:: URL|ALL|http://www.tightvnc.com/download/2.0.2/tightvnc-2.0.2-setup.exe|packages/tightvnc-2.0.2-setup.exe
 @Echo off
 
 :: Add rule to the firewall to allow port 5900 
 todo.pl "\"netsh firewall add portopening protocol = TCP port = 5900 name = TightVNC mode = enable\""
-todo.pl "startup-type.pl manual winvnc"
-todo.pl "%Z%\packages\tightvnc-1.3.10-setup.exe /silent" "\"%ProgramFiles%\TightVNC\WinVNC.exe\" -install"
+todo.pl "%Z%\packages\tightvnc-2.0.2-setup.exe /S" 
 
