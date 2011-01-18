eecec8ada6ef6d1f5bf1f0044da52a244a64e194
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-18 09:07:58 +0000

    Add security bulletin march 2010

---
+++ trunk/install/scripts/winxpsp3-up1003.bat
@@ -0,0 +1,54 @@
+
+:: <http://support.microsoft.com/?kbid=975561>
+:: <http://www.microsoft.com/downloads/details.aspx?FamilyID=6301E462-02BE-4B9A-BAE9-7C4821B42D2D>
+:: URL|ARA|http://download.microsoft.com/download/3/D/0/3D0B070E-94D6-4F5B-AA47-23D45EEE166C/WindowsXP-KB975561-x86-ARA.exe|updates/winxpsp3/windowsxp-kb975561-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/D/7/1/D7176D46-D0AA-4019-91B9-27E0A6CB238E/WindowsXP-KB975561-x86-CSY.exe|updates/winxpsp3/windowsxp-kb975561-x86-csy.exe
+:: No Download found for DAN.
+:: URL|DEU|http://download.microsoft.com/download/D/3/9/D39F7EB4-1B9E-4ABD-B3FD-C2858D7E7E7E/WindowsXP-KB975561-x86-DEU.exe|updates/winxpsp3/windowsxp-kb975561-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/6/0/8/608C6F9D-2A3C-4829-B178-9D399BE90323/WindowsXP-KB975561-x86-ELL.exe|updates/winxpsp3/windowsxp-kb975561-x86-ell.exe
+:: No Download found for ENU.
+:: URL|ESN|http://download.microsoft.com/download/B/6/0/B60D9C92-E040-43CC-8F2B-A635DADF07A4/WindowsXP-KB975561-x86-ESN.exe|updates/winxpsp3/windowsxp-kb975561-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/5/5/9/5590EE92-240D-41D7-8C37-E53CF2C4297D/WindowsXP-KB975561-x86-FIN.exe|updates/winxpsp3/windowsxp-kb975561-x86-fin.exe
+:: No Download found for FRA.
+:: URL|HEB|http://download.microsoft.com/download/1/7/1/17146CC6-5B36-4823-B382-6118CCFB8A16/WindowsXP-KB975561-x86-HEB.exe|updates/winxpsp3/windowsxp-kb975561-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/D/4/D/D4D71039-1754-4BAA-BD45-52ACE68B2179/WindowsXP-KB975561-x86-HUN.exe|updates/winxpsp3/windowsxp-kb975561-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/5/E/5/5E5DFBF7-F3F6-4DF5-A301-6EE5468BD1DA/WindowsXP-KB975561-x86-ITA.exe|updates/winxpsp3/windowsxp-kb975561-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/4/6/F/46F3FCA4-FDBF-4EEA-973F-DCEF089AF811/WindowsXP-KB975561-x86-JPN.exe|updates/winxpsp3/windowsxp-kb975561-x86-jpn.exe
+:: No Download found for KOR.
+:: No Download found for NLD.
+:: No Download found for NOR.
+:: URL|PLK|http://download.microsoft.com/download/0/C/0/0C079209-7FCB-4F4A-9D05-FD6CF28DA338/WindowsXP-KB975561-x86-PLK.exe|updates/winxpsp3/windowsxp-kb975561-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/E/9/C/E9C813D5-A4A5-46C9-B15A-9674A4F510B3/WindowsXP-KB975561-x86-PTB.exe|updates/winxpsp3/windowsxp-kb975561-x86-ptb.exe
+:: No Download found for PTG.
+:: No Download found for RUS.
+:: No Download found for SVE.
+:: No Download found for TRK.
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB975561-x86-%WINLANG%.exe /norestart /passive /nobackup"
+
+:: Microsoft Security Bulletin MS10-018 - Critical
+:: "Cumulative Security Update for Internet Explorer (980182)"
+:: <http://www.microsoft.com/technet/security/bulletin/MS10-018.mspx>
+:: <http://www.microsoft.com/downloads/details.aspx?FamilyID=2f2caa01-5cd1-45cb-9995-e34d933920d4>
+:: URL|ARA|http://download.microsoft.com/download/B/4/3/B43F9235-FABA-4DA1-812D-5B36161C3AA6/WindowsXP-KB980182-x86-ARA.exe|updates/winxpsp3/windowsxp-kb980182-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/7/1/3/713B7000-C455-4CF1-8608-A1649F88EE0B/WindowsXP-KB980182-x86-CSY.exe|updates/winxpsp3/windowsxp-kb980182-x86-csy.exe
+:: No Download found for DAN.
+:: URL|DEU|http://download.microsoft.com/download/A/1/C/A1CB3DC0-8258-4654-82C1-F8474FD920B1/WindowsXP-KB980182-x86-DEU.exe|updates/winxpsp3/windowsxp-kb980182-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/A/B/B/ABBC9741-D10C-4399-B477-61F2FB29024A/WindowsXP-KB980182-x86-ELL.exe|updates/winxpsp3/windowsxp-kb980182-x86-ell.exe
+:: No Download found for ENU.
+:: URL|ESN|http://download.microsoft.com/download/C/E/8/CE895B12-F1D6-4AA3-917F-46CA2882C401/WindowsXP-KB980182-x86-ESN.exe|updates/winxpsp3/windowsxp-kb980182-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/8/3/0/830C8267-3E64-4CAA-B3C5-43C502750BAF/WindowsXP-KB980182-x86-FIN.exe|updates/winxpsp3/windowsxp-kb980182-x86-fin.exe
+:: No Download found for FRA.
+:: URL|HEB|http://download.microsoft.com/download/B/F/4/BF476D29-670A-4F35-A8E2-881E71680C2C/WindowsXP-KB980182-x86-HEB.exe|updates/winxpsp3/windowsxp-kb980182-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/B/4/6/B46BA4C1-F70A-4247-A912-3BCA590B005C/WindowsXP-KB980182-x86-HUN.exe|updates/winxpsp3/windowsxp-kb980182-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/7/3/B/73B00A83-E02C-43BE-A8E0-40586B9A60D6/WindowsXP-KB980182-x86-ITA.exe|updates/winxpsp3/windowsxp-kb980182-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/3/B/3/3B382013-9144-44AA-9536-67F3C4F56B10/WindowsXP-KB980182-x86-JPN.exe|updates/winxpsp3/windowsxp-kb980182-x86-jpn.exe
+:: No Download found for KOR.
+:: No Download found for NLD.
+:: No Download found for NOR.
+:: URL|PLK|http://download.microsoft.com/download/8/A/4/8A40190C-26A3-4FAB-ADDD-422D59EE93F4/WindowsXP-KB980182-x86-PLK.exe|updates/winxpsp3/windowsxp-kb980182-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/9/E/5/9E5294C2-1C62-4E71-ABC1-AB98AB39D96A/WindowsXP-KB980182-x86-PTB.exe|updates/winxpsp3/windowsxp-kb980182-x86-ptb.exe
+:: No Download found for PTG.
+:: No Download found for RUS.
+:: No Download found for SVE.
+:: No Download found for TRK.
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB980182-x86-%WINLANG%.exe /norestart /passive /nobackup"
