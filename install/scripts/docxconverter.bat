a249c4423e67a35d68a1ff0130e50219a5e09fb5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-18 13:41:31 +0000

    File converter is not only a office2003 update.

--- install/scripts/docxconverter.bat
+++ install/scripts/docxconverter.bat
@@ -3,26 +3,26 @@
 :: How to use earlier versions of Excel, PowerPoint, and Word to open and save files from 2007 Office programs
 :: <http://support.microsoft.com/kb/924074>
 :: <http://www.microsoft.com/downloads/details.aspx?FamilyId=941B3470-3AE9-4AEE-8F43-C6BB74CD1466>
-:: URL|ARA|http://download.microsoft.com/download/d/b/0/db0104fc-c1ea-4674-b7c6-89963c959fde/FileFormatConverters.exe|updates/office2003/ara/fileformatconverters.exe
-:: URL|CSY|http://download.microsoft.com/download/5/3/5/535f7ac9-a715-4332-9faf-e770293ca084/FileFormatConverters.exe|updates/office2003/csy/fileformatconverters.exe
-:: URL|DAN|http://download.microsoft.com/download/6/e/b/6ebc7d70-ed5f-4acf-8aba-347567dd2aed/FileFormatConverters.exe|updates/office2003/dan/fileformatconverters.exe
-:: URL|DEU|http://download.microsoft.com/download/e/4/9/e4996d8f-838e-4b75-8685-1e6134762992/FileFormatConverters.exe|updates/office2003/deu/fileformatconverters.exe
-:: URL|ELL|http://download.microsoft.com/download/d/7/4/d74c2b13-62b1-45d1-ac03-3359674d0f23/FileFormatConverters.exe|updates/office2003/ell/fileformatconverters.exe
-:: URL|ENU|http://download.microsoft.com/download/9/2/2/9222d67f-7630-4f49-bd26-476b51517fc1/FileFormatConverters.exe|updates/office2003/enu/fileformatconverters.exe
-:: URL|ESN|http://download.microsoft.com/download/b/7/2/b7277fac-80e8-4046-ac6c-34d998e2e5c8/FileFormatConverters.exe|updates/office2003/esn/fileformatconverters.exe
-:: URL|FIN|http://download.microsoft.com/download/4/b/2/4b22b252-bc02-484e-8bfc-7ec376e3d425/FileFormatConverters.exe|updates/office2003/fin/fileformatconverters.exe
+:: URL|ARA|http://download.microsoft.com/download/d/b/0/db0104fc-c1ea-4674-b7c6-89963c959fde/FileFormatConverters.exe|updates/common/ara/fileformatconverters.exe
+:: URL|CSY|http://download.microsoft.com/download/5/3/5/535f7ac9-a715-4332-9faf-e770293ca084/FileFormatConverters.exe|updates/common/csy/fileformatconverters.exe
+:: URL|DAN|http://download.microsoft.com/download/6/e/b/6ebc7d70-ed5f-4acf-8aba-347567dd2aed/FileFormatConverters.exe|updates/common/dan/fileformatconverters.exe
+:: URL|DEU|http://download.microsoft.com/download/e/4/9/e4996d8f-838e-4b75-8685-1e6134762992/FileFormatConverters.exe|updates/common/deu/fileformatconverters.exe
+:: URL|ELL|http://download.microsoft.com/download/d/7/4/d74c2b13-62b1-45d1-ac03-3359674d0f23/FileFormatConverters.exe|updates/common/ell/fileformatconverters.exe
+:: URL|ENU|http://download.microsoft.com/download/9/2/2/9222d67f-7630-4f49-bd26-476b51517fc1/FileFormatConverters.exe|updates/common/enu/fileformatconverters.exe
+:: URL|ESN|http://download.microsoft.com/download/b/7/2/b7277fac-80e8-4046-ac6c-34d998e2e5c8/FileFormatConverters.exe|updates/common/esn/fileformatconverters.exe
+:: URL|FIN|http://download.microsoft.com/download/4/b/2/4b22b252-bc02-484e-8bfc-7ec376e3d425/FileFormatConverters.exe|updates/common/fin/fileformatconverters.exe
 :: No Download found for FRA.
-:: URL|HEB|http://download.microsoft.com/download/0/d/4/0d40bae8-2d3a-48e0-8fa6-075c16960364/FileFormatConverters.exe|updates/office2003/heb/fileformatconverters.exe
-:: URL|HUN|http://download.microsoft.com/download/d/1/4/d14016de-a788-4bf8-a533-4af48b2e9b3c/FileFormatConverters.exe|updates/office2003/hun/fileformatconverters.exe
-:: URL|ITA|http://download.microsoft.com/download/0/d/8/0d84a525-261d-476c-a6bd-ab7595897f32/FileFormatConverters.exe|updates/office2003/ita/fileformatconverters.exe
-:: URL|JPN|http://download.microsoft.com/download/8/b/9/8b97798b-00f8-4523-8ca6-8c0c5867883f/FileFormatConverters.exe|updates/office2003/jpn/fileformatconverters.exe
-:: URL|KOR|http://download.microsoft.com/download/f/6/2/f628cd19-819a-4106-ad85-8eec2f58b338/FileFormatConverters.exe|updates/office2003/kor/fileformatconverters.exe
-:: URL|NLD|http://download.microsoft.com/download/5/3/c/53cb28f9-d6c1-4f2c-9d83-a2c4b29ffb49/FileFormatConverters.exe|updates/office2003/nld/fileformatconverters.exe
-:: URL|NOR|http://download.microsoft.com/download/f/d/c/fdc51c8c-b44f-433d-9cc2-d0ff06067db2/FileFormatConverters.exe|updates/office2003/nor/fileformatconverters.exe
-:: URL|PLK|http://download.microsoft.com/download/7/8/f/78f25e6d-7ac3-4003-88c3-e7db2837caef/FileFormatConverters.exe|updates/office2003/plk/fileformatconverters.exe
-:: URL|PTB|http://download.microsoft.com/download/3/9/0/390be975-5a4a-4f89-a03c-c296511cfba3/FileFormatConverters.exe|updates/office2003/ptb/fileformatconverters.exe
-:: URL|PTG|http://download.microsoft.com/download/3/5/c/35ccbdde-ac45-499d-bd1b-793ab26e95d0/FileFormatConverters.exe|updates/office2003/ptg/fileformatconverters.exe
-:: URL|RUS|http://download.microsoft.com/download/b/c/4/bc41c3a1-c243-4440-a771-8260515e4cc7/FileFormatConverters.exe|updates/office2003/rus/fileformatconverters.exe
-:: URL|SVE|http://download.microsoft.com/download/0/d/4/0d40b572-44b3-43f0-bf14-4ad3a51c63dd/FileFormatConverters.exe|updates/office2003/sve/fileformatconverters.exe
-:: URL|TRK|http://download.microsoft.com/download/8/6/c/86c94b8e-294d-4d5d-867f-8e5c1409b964/FileFormatConverters.exe|updates/office2003/trk/fileformatconverters.exe
-todo.pl ".reboot-on 194 %Z%\updates\office2003\%WINLANG%\FileFormatConverters.exe /quiet"
+:: URL|HEB|http://download.microsoft.com/download/0/d/4/0d40bae8-2d3a-48e0-8fa6-075c16960364/FileFormatConverters.exe|updates/common/heb/fileformatconverters.exe
+:: URL|HUN|http://download.microsoft.com/download/d/1/4/d14016de-a788-4bf8-a533-4af48b2e9b3c/FileFormatConverters.exe|updates/common/hun/fileformatconverters.exe
+:: URL|ITA|http://download.microsoft.com/download/0/d/8/0d84a525-261d-476c-a6bd-ab7595897f32/FileFormatConverters.exe|updates/common/ita/fileformatconverters.exe
+:: URL|JPN|http://download.microsoft.com/download/8/b/9/8b97798b-00f8-4523-8ca6-8c0c5867883f/FileFormatConverters.exe|updates/common/jpn/fileformatconverters.exe
+:: URL|KOR|http://download.microsoft.com/download/f/6/2/f628cd19-819a-4106-ad85-8eec2f58b338/FileFormatConverters.exe|updates/common/kor/fileformatconverters.exe
+:: URL|NLD|http://download.microsoft.com/download/5/3/c/53cb28f9-d6c1-4f2c-9d83-a2c4b29ffb49/FileFormatConverters.exe|updates/common/nld/fileformatconverters.exe
+:: URL|NOR|http://download.microsoft.com/download/f/d/c/fdc51c8c-b44f-433d-9cc2-d0ff06067db2/FileFormatConverters.exe|updates/common/nor/fileformatconverters.exe
+:: URL|PLK|http://download.microsoft.com/download/7/8/f/78f25e6d-7ac3-4003-88c3-e7db2837caef/FileFormatConverters.exe|updates/common/plk/fileformatconverters.exe
+:: URL|PTB|http://download.microsoft.com/download/3/9/0/390be975-5a4a-4f89-a03c-c296511cfba3/FileFormatConverters.exe|updates/common/ptb/fileformatconverters.exe
+:: URL|PTG|http://download.microsoft.com/download/3/5/c/35ccbdde-ac45-499d-bd1b-793ab26e95d0/FileFormatConverters.exe|updates/common/ptg/fileformatconverters.exe
+:: URL|RUS|http://download.microsoft.com/download/b/c/4/bc41c3a1-c243-4440-a771-8260515e4cc7/FileFormatConverters.exe|updates/common/rus/fileformatconverters.exe
+:: URL|SVE|http://download.microsoft.com/download/0/d/4/0d40b572-44b3-43f0-bf14-4ad3a51c63dd/FileFormatConverters.exe|updates/common/sve/fileformatconverters.exe
+:: URL|TRK|http://download.microsoft.com/download/8/6/c/86c94b8e-294d-4d5d-867f-8e5c1409b964/FileFormatConverters.exe|updates/common/trk/fileformatconverters.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\%WINLANG%\FileFormatConverters.exe /quiet"
