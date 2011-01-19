4c081da2184f2fed769fb70f5dbcc53adfdfe59c
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-19 00:05:39 +0000

    Security Updates May 2010

---
+++ trunk/install/scripts/winxpsp3-up1005.bat
@@ -0,0 +1,29 @@
+
+:: <a class="dsp-page-title" href="http://www.microsoft.com/downloads/" title="Microsoft Download Center" cpgn="DLC Header-Microsoft Download Center Link" cid="30-00-111SXX02958"></a>
+:: Microsoft Security Bulletin MS10-030 - Critical
+:: "Vulnerability in Outlook Express and Windows Mail Could Allow Remote Code Execution (978542)"
+:: <http://www.microsoft.com/technet/security/bulletin/MS10-030.mspx>
+:: <http://www.microsoft.com/downloads/details.aspx?FamilyID=99707C3D-A3CB-47DA-B38E-8AE0227FD703>
+:: URL|ARA|http://download.microsoft.com/download/2/9/E/29EB741F-D594-4425-BDCE-A252C3354571/WindowsXP-KB978542-x86-ARA.exe|updates/winxpsp3/windowsxp-kb978542-x86-ara.exe
+:: URL|CSY|http://download.microsoft.com/download/3/7/6/3760B703-08B4-43E7-97E6-72329F979C06/WindowsXP-KB978542-x86-CSY.exe|updates/winxpsp3/windowsxp-kb978542-x86-csy.exe
+:: No Download found for DAN.
+:: URL|DEU|http://download.microsoft.com/download/5/7/6/5763A79D-F32A-431A-B09B-389068F98C0A/WindowsXP-KB978542-x86-DEU.exe|updates/winxpsp3/windowsxp-kb978542-x86-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/3/3/E/33E07AF6-0766-4264-9621-77E2CE9BE5BF/WindowsXP-KB978542-x86-ELL.exe|updates/winxpsp3/windowsxp-kb978542-x86-ell.exe
+:: No Download found for ENU.
+:: URL|ESN|http://download.microsoft.com/download/0/7/F/07F39DA1-168D-4C39-A0D9-8988AE4E8BFD/WindowsXP-KB978542-x86-ESN.exe|updates/winxpsp3/windowsxp-kb978542-x86-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/F/1/7/F17AC859-7BAC-42F5-B6EA-59C44834DEB9/WindowsXP-KB978542-x86-FIN.exe|updates/winxpsp3/windowsxp-kb978542-x86-fin.exe
+:: No Download found for FRA.
+:: URL|HEB|http://download.microsoft.com/download/E/D/A/EDA3C5A5-EE2B-4877-9741-11E5A4B72C1C/WindowsXP-KB978542-x86-HEB.exe|updates/winxpsp3/windowsxp-kb978542-x86-heb.exe
+:: URL|HUN|http://download.microsoft.com/download/5/7/7/577AD06F-DD7F-471E-98BC-F79C00017B7E/WindowsXP-KB978542-x86-HUN.exe|updates/winxpsp3/windowsxp-kb978542-x86-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/6/5/B/65BC5D46-8597-4546-B51A-2ED36D3A8BC4/WindowsXP-KB978542-x86-ITA.exe|updates/winxpsp3/windowsxp-kb978542-x86-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/5/2/1/521D676F-A901-4B7D-8ECC-4CF7CAB70CF9/WindowsXP-KB978542-x86-JPN.exe|updates/winxpsp3/windowsxp-kb978542-x86-jpn.exe
+:: No Download found for KOR.
+:: No Download found for NLD.
+:: No Download found for NOR.
+:: URL|PLK|http://download.microsoft.com/download/6/E/D/6ED39B9C-D12F-45AC-9A8B-006513A92A9A/WindowsXP-KB978542-x86-PLK.exe|updates/winxpsp3/windowsxp-kb978542-x86-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/5/B/0/5B008B0D-6C04-4731-868C-37DBEF72B2AC/WindowsXP-KB978542-x86-PTB.exe|updates/winxpsp3/windowsxp-kb978542-x86-ptb.exe
+:: No Download found for PTG.
+:: No Download found for RUS.
+:: No Download found for SVE.
+:: No Download found for TRK.
+todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB978542-x86-%WINLANG%.exe /norestart /passive /nobackup"
