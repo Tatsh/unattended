127add69217dc1c3f680a09a39f029c7fcc1723b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-20 19:37:39 +0000

    Libreoffice: upodate to RC4

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -1,9 +1,9 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.documentfoundation.org/download/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/testing/3.3.0-rc3/win/x86/LibO_3.3.0rc3_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.0rc3_Win_x86_install_multi.exe
+::URL|ALL|http://download.documentfoundation.org/libreoffice/testing/3.3.0-rc4/win/x86/LibO_3.3.0rc4_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.0rc4_Win_x86_install_multi.exe
 
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\LibO_3.3.0rc3_Win_x86_install_multi.exe /S"
+todo.pl "%Z%\packages\libreoffice\LibO_3.3.0rc4_Win_x86_install_multi.exe /S"
 
