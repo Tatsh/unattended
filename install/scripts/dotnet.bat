c78a882c107907b0906f2b6b7738c0fa5a0e4f87
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-21 09:01:27 +0000

    fix #51: dotnet.bat should install previous versions of .NET

--- install/scripts/dotnet.bat
+++ install/scripts/dotnet.bat
@@ -1,26 +1,5 @@
+:: INSTALL :: Microsoft .NET 3.5 Redistributable Package (x86)
+:: http://msdn.microsoft.com/en-us/netframework/default.aspx
+:: URL|ALL|http://download.microsoft.com/download/2/0/e/20e90413-712f-438c-988e-fdaa79a8ac3d/dotnetfx35.exe|updates/common/dotnetfx35-sp1.exe
 
-:: Microsoft .NET Framework 4 (Standalone Installer)
-:: <http://www.microsoft.com/downloads/details.aspx?familyid=0A391ABD-25C1-4FC0-919F-B21F31AB88B7>
-:: URL|ARA|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/ara/dotnetfx40_full_x86_x64.exe
-:: URL|CSY|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/csy/dotnetfx40_full_x86_x64.exe
-:: URL|DAN|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/dan/dotnetfx40_full_x86_x64.exe
-:: URL|DEU|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/deu/dotnetfx40_full_x86_x64.exe
-:: URL|ELL|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/ell/dotnetfx40_full_x86_x64.exe
-:: URL|ENU|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/enu/dotnetfx40_full_x86_x64.exe
-:: URL|ESN|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/esn/dotnetfx40_full_x86_x64.exe
-:: URL|FIN|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/fin/dotnetfx40_full_x86_x64.exe
-:: URL|FRA|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/fra/dotnetfx40_full_x86_x64.exe
-:: URL|HEB|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/heb/dotnetfx40_full_x86_x64.exe
-:: URL|HUN|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/hun/dotnetfx40_full_x86_x64.exe
-:: URL|ITA|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/ita/dotnetfx40_full_x86_x64.exe
-:: URL|JPN|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/jpn/dotnetfx40_full_x86_x64.exe
-:: URL|KOR|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/kor/dotnetfx40_full_x86_x64.exe
-:: URL|NLD|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/nld/dotnetfx40_full_x86_x64.exe
-:: URL|NOR|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/nor/dotnetfx40_full_x86_x64.exe
-:: URL|PLK|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/plk/dotnetfx40_full_x86_x64.exe
-:: URL|PTB|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/ptb/dotnetfx40_full_x86_x64.exe
-:: URL|PTG|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/ptg/dotnetfx40_full_x86_x64.exe
-:: URL|RUS|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/rus/dotnetfx40_full_x86_x64.exe
-:: URL|SVE|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/sve/dotnetfx40_full_x86_x64.exe
-:: URL|TRK|http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe|updates/common/trk/dotnetfx40_full_x86_x64.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\%WINLANG%\dotNetFx40_Full_x86_x64.exe /q /norestart"
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx35-sp1.exe /q /norestart"
