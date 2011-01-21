dbf6867b64b237d437b72ab8d426ce9b49fe8eb5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-21 19:44:55 +0000

    MS Security updates September 2010

--- trunk/install/scripts/winxpsp3-up0906.bat
+++ trunk/install/scripts/winxpsp3-up0906.bat
@@ -25,29 +25,3 @@
 :: URL|TRK|http://download.microsoft.com/download/A/A/3/AA3C4D60-F8AB-435D-AA82-53675C41C6E1/WindowsXP-KB961501-x86-TRK.exe|updates/winxpsp3/windowsxp-kb961501-x86-trk.exe
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB961501-x86-%WINLANG%.exe /norestart /passive /nobackup"
 
-:: Security Update for Windows XP (KB970238)
-:: <http://www.microsoft.com/downloads/details.aspx?familyid=f033fa78-c451-44f8-aa6c-a49622c37f40>
-:: URL|ARA|http://download.microsoft.com/download/9/A/B/9ABC941B-7C91-48A6-B401-4EDEBAD0F025/WindowsXP-KB970238-x86-ARA.exe|updates/winxpsp3/windowsxp-kb970238-x86-ara.exe
-:: URL|CSY|http://download.microsoft.com/download/F/2/0/F207A3AC-743D-4BF7-9B7D-4C1BEAF2E204/WindowsXP-KB970238-x86-CSY.exe|updates/winxpsp3/windowsxp-kb970238-x86-csy.exe
-:: URL|DAN|http://download.microsoft.com/download/E/D/F/EDF5743A-98A7-4C12-A318-E768426CE699/WindowsXP-KB970238-x86-DAN.exe|updates/winxpsp3/windowsxp-kb970238-x86-dan.exe
-:: URL|DEU|http://download.microsoft.com/download/E/D/8/ED8BA979-A4B1-41D8-8AF3-5D9B21463CE1/WindowsXP-KB970238-x86-DEU.exe|updates/winxpsp3/windowsxp-kb970238-x86-deu.exe
-:: URL|ELL|http://download.microsoft.com/download/D/9/8/D98D97C8-FFAE-4804-AE85-CE66FF495ECB/WindowsXP-KB970238-x86-ELL.exe|updates/winxpsp3/windowsxp-kb970238-x86-ell.exe
-:: URL|ENU|http://download.microsoft.com/download/E/3/B/E3B1DD89-D72F-4802-A044-D0D117039F3C/WindowsXP-KB970238-x86-ENU.exe|updates/winxpsp3/windowsxp-kb970238-x86-enu.exe
-:: URL|ESN|http://download.microsoft.com/download/4/1/A/41A4055B-5C38-47A1-B7BB-F1EC8A7F71FD/WindowsXP-KB970238-x86-ESN.exe|updates/winxpsp3/windowsxp-kb970238-x86-esn.exe
-:: URL|FIN|http://download.microsoft.com/download/A/D/0/AD0BFD2C-6565-4B79-B7EE-C0A4E63754B0/WindowsXP-KB970238-x86-FIN.exe|updates/winxpsp3/windowsxp-kb970238-x86-fin.exe
-:: URL|FRA|http://download.microsoft.com/download/0/6/8/0682013F-033B-4185-B95C-EABC74E93AFF/WindowsXP-KB970238-x86-FRA.exe|updates/winxpsp3/windowsxp-kb970238-x86-fra.exe
-:: URL|HEB|http://download.microsoft.com/download/8/1/9/8198CE6C-B213-481A-B945-5B89C5327817/WindowsXP-KB970238-x86-HEB.exe|updates/winxpsp3/windowsxp-kb970238-x86-heb.exe
-:: URL|HUN|http://download.microsoft.com/download/A/8/B/A8B33F9B-F04D-48B0-88FD-05186AF75E0F/WindowsXP-KB970238-x86-HUN.exe|updates/winxpsp3/windowsxp-kb970238-x86-hun.exe
-:: URL|ITA|http://download.microsoft.com/download/E/0/9/E09E2D47-EA56-4D85-A53A-B1C7962FBBC3/WindowsXP-KB970238-x86-ITA.exe|updates/winxpsp3/windowsxp-kb970238-x86-ita.exe
-:: URL|JPN|http://download.microsoft.com/download/E/0/0/E00513E2-C090-422D-947C-B318B4949493/WindowsXP-KB970238-x86-JPN.exe|updates/winxpsp3/windowsxp-kb970238-x86-jpn.exe
-:: URL|KOR|http://download.microsoft.com/download/A/9/0/A90F0CDA-A71C-438B-BB73-DEDD737E5AB6/WindowsXP-KB970238-x86-KOR.exe|updates/winxpsp3/windowsxp-kb970238-x86-kor.exe
-:: URL|NLD|http://download.microsoft.com/download/B/7/E/B7ED1F0A-DAAA-4736-9528-6ECFD63F026E/WindowsXP-KB970238-x86-NLD.exe|updates/winxpsp3/windowsxp-kb970238-x86-nld.exe
-:: URL|NOR|http://download.microsoft.com/download/8/C/C/8CC2066B-BF2B-45A8-AD2F-865ACB4F7704/WindowsXP-KB970238-x86-NOR.exe|updates/winxpsp3/windowsxp-kb970238-x86-nor.exe
-:: URL|PLK|http://download.microsoft.com/download/B/3/1/B3135CDE-12EA-4306-A169-A4E5D518D201/WindowsXP-KB970238-x86-PLK.exe|updates/winxpsp3/windowsxp-kb970238-x86-plk.exe
-:: URL|PTB|http://download.microsoft.com/download/E/4/C/E4C5F4D4-C613-471D-AAF0-FBC401BA7478/WindowsXP-KB970238-x86-PTB.exe|updates/winxpsp3/windowsxp-kb970238-x86-ptb.exe
-:: URL|PTG|http://download.microsoft.com/download/6/F/A/6FAC2C1E-50B3-42B9-8B00-F53830CD2C98/WindowsXP-KB970238-x86-PTG.exe|updates/winxpsp3/windowsxp-kb970238-x86-ptg.exe
-:: URL|RUS|http://download.microsoft.com/download/C/7/8/C78B2219-59D2-43E5-A106-31C7DA8EF37B/WindowsXP-KB970238-x86-RUS.exe|updates/winxpsp3/windowsxp-kb970238-x86-rus.exe
-:: URL|SVE|http://download.microsoft.com/download/2/7/D/27DFB9BF-3556-4FDA-A6CA-01401C04333A/WindowsXP-KB970238-x86-SVE.exe|updates/winxpsp3/windowsxp-kb970238-x86-sve.exe
-:: URL|TRK|http://download.microsoft.com/download/A/0/3/A03F13F3-5A56-419F-92F2-3DA50D89CB02/WindowsXP-KB970238-x86-TRK.exe|updates/winxpsp3/windowsxp-kb970238-x86-trk.exe
-todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB970238-x86-%WINLANG%.exe /norestart /passive /nobackup"
-
