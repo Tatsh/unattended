bf46ef9f67783820fe50b4f7c95ebdf1be098e5b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-23 15:09:18 +0000

    Add missing binaries

--- install/scripts/winxpsp3-up1006.bat
+++ install/scripts/winxpsp3-up1006.bat
@@ -1,109 +1,112 @@
 
+:: Download details: Security Update for Windows Media Format Runtime 9, 9.5 & 11 for Windows XP SP3 (KB978695)
 :: <http://support.microsoft.com/?kbid=978695>
 :: <http://www.microsoft.com/downloads/details.aspx?FamilyID=EBBCCD82-C637-4C88-86EA-D39AE713C085>
 :: URL|ARA|http://download.microsoft.com/download/5/1/4/514D6FB5-E029-4378-8387-8DCEE80E1983/WindowsXP-WindowsMedia-KB978695-x86-ARA.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-ara.exe
 :: URL|CSY|http://download.microsoft.com/download/6/9/B/69B8457F-2F58-4599-BC5F-06EB17A6D431/WindowsXP-WindowsMedia-KB978695-x86-CSY.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-csy.exe
-:: No Download found for DAN.
+:: URL|DAN|http://download.microsoft.com/download/9/9/2/9929B567-05F5-49B2-80E1-5360E30E4AC4/WindowsXP-WindowsMedia-KB978695-x86-DAN.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-dan.exe
 :: URL|DEU|http://download.microsoft.com/download/B/8/2/B82A3B95-E103-4773-9F27-796C0C2B5DC4/WindowsXP-WindowsMedia-KB978695-x86-DEU.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-deu.exe
 :: URL|ELL|http://download.microsoft.com/download/0/8/D/08D7EB02-BF0E-4DF7-9575-0F0374D32EC8/WindowsXP-WindowsMedia-KB978695-x86-ELL.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-ell.exe
-:: No Download found for ENU.
+:: URL|ENU|http://download.microsoft.com/download/0/6/9/0690D342-A296-4888-AFCC-2E2DEF041A70/WindowsXP-WindowsMedia-KB978695-x86-ENU.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-enu.exe
 :: URL|ESN|http://download.microsoft.com/download/D/2/A/D2AC6BF6-A42F-4DAA-BD8F-902360FA0752/WindowsXP-WindowsMedia-KB978695-x86-ESN.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-esn.exe
 :: URL|FIN|http://download.microsoft.com/download/3/9/1/391BB568-7379-47B2-B2BF-5DB1620BE9FC/WindowsXP-WindowsMedia-KB978695-x86-FIN.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-fin.exe
-:: No Download found for FRA.
+:: URL|FRA|http://download.microsoft.com/download/1/6/8/168161BF-92EB-4A42-B94A-1F82396422E2/WindowsXP-WindowsMedia-KB978695-x86-FRA.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-fra.exe
 :: URL|HEB|http://download.microsoft.com/download/5/C/B/5CB2C9D5-7A08-4AAD-8BD9-66A7566D3A98/WindowsXP-WindowsMedia-KB978695-x86-HEB.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-heb.exe
 :: URL|HUN|http://download.microsoft.com/download/F/3/9/F39F841B-D77B-45D8-98F1-67CF076C0E72/WindowsXP-WindowsMedia-KB978695-x86-HUN.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-hun.exe
 :: URL|ITA|http://download.microsoft.com/download/F/C/1/FC16D88F-B646-4D56-B031-6C20F0B1350E/WindowsXP-WindowsMedia-KB978695-x86-ITA.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-ita.exe
 :: URL|JPN|http://download.microsoft.com/download/5/2/6/52608D49-99F3-4646-8D86-D8CEA4830D23/WindowsXP-WindowsMedia-KB978695-x86-JPN.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-jpn.exe
-:: No Download found for KOR.
-:: No Download found for NLD.
-:: No Download found for NOR.
+:: URL|KOR|http://download.microsoft.com/download/F/F/F/FFFB9128-F831-4CA6-A866-E216F26B3C30/WindowsXP-WindowsMedia-KB978695-x86-KOR.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/0/7/E/07EB7819-82F3-4739-A718-5AF86A5B93F1/WindowsXP-WindowsMedia-KB978695-x86-NLD.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/E/E/E/EEE449B6-4247-4DBA-90AD-F6DDE2E71447/WindowsXP-WindowsMedia-KB978695-x86-NOR.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-nor.exe
 :: URL|PLK|http://download.microsoft.com/download/4/5/B/45B4D350-069E-4F32-A446-1D59B54AF306/WindowsXP-WindowsMedia-KB978695-x86-PLK.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-plk.exe
 :: URL|PTB|http://download.microsoft.com/download/9/8/2/9824F618-7FCE-4A2B-B13F-0E48A5FD5537/WindowsXP-WindowsMedia-KB978695-x86-PTB.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-ptb.exe
-:: No Download found for PTG.
-:: No Download found for RUS.
-:: No Download found for SVE.
-:: No Download found for TRK.
+:: URL|PTG|http://download.microsoft.com/download/3/B/3/3B371253-61F7-4976-8051-1610C9FF2EB3/WindowsXP-WindowsMedia-KB978695-x86-PTG.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/0/B/F/0BF98C04-D9D2-450B-B8A2-B12428CF4C1A/WindowsXP-WindowsMedia-KB978695-x86-RUS.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/4/9/C/49CFB59A-5A6F-4F1D-9516-513CF92CE6CB/WindowsXP-WindowsMedia-KB978695-x86-SVE.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/A/1/8/A183D2E3-8AC1-43C1-BF19-BD6D5629C08C/WindowsXP-WindowsMedia-KB978695-x86-TRK.exe|updates/winxpsp3/windowsxp-windowsmedia-kb978695-x86-trk.exe
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-WindowsMedia-KB978695-x86-%WINLANG%.exe /norestart /passive /nobackup"
 
+:: Download details: Security Update for Windows Media Encoder 9 Series for Windows 2000, Windows XP and Windows Server 2003 (KB979332)
 :: <http://support.microsoft.com/?kbid=979332>
 :: <http://www.microsoft.com/downloads/details.aspx?FamilyID=5B5398C1-5B30-4162-95B6-948D9BE103BF>
 :: URL|ARA|http://download.microsoft.com/download/3/3/6/336FC23E-1F75-4E2E-9A30-2D67BD75FCAE/WindowsMedia9-KB979332-x86-ARA.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-ara.exe
 :: URL|CSY|http://download.microsoft.com/download/F/1/3/F134E6DF-19EA-4582-BCAD-66B19902A2E0/WindowsMedia9-KB979332-x86-CSY.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-csy.exe
-:: No Download found for DAN.
+:: URL|DAN|http://download.microsoft.com/download/5/4/B/54BDE57F-B2E9-4C8D-BB7F-772EF583554A/WindowsMedia9-KB979332-x86-DAN.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-dan.exe
 :: URL|DEU|http://download.microsoft.com/download/1/C/9/1C99720D-43AF-4A01-9293-4D7461DE3331/WindowsMedia9-KB979332-x86-DEU.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-deu.exe
 :: URL|ELL|http://download.microsoft.com/download/2/3/E/23E5E7EE-86C9-421A-BC26-3EA58AB656C8/WindowsMedia9-KB979332-x86-ELL.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-ell.exe
-:: No Download found for ENU.
+:: URL|ENU|http://download.microsoft.com/download/2/9/7/2975D8D2-F50E-47EC-AA91-D3DF22762138/WindowsMedia9-KB979332-x86-ENU.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-enu.exe
 :: URL|ESN|http://download.microsoft.com/download/A/9/4/A94D40DE-6A2A-4CE3-BACE-4F4A8DA6E9F0/WindowsMedia9-KB979332-x86-ESN.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-esn.exe
 :: URL|FIN|http://download.microsoft.com/download/8/0/4/80438091-6FC5-4896-8F34-2C91E0436ECE/WindowsMedia9-KB979332-x86-FIN.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-fin.exe
-:: No Download found for FRA.
+:: URL|FRA|http://download.microsoft.com/download/8/8/6/88619207-8674-4C84-AD60-D6D939B99408/WindowsMedia9-KB979332-x86-FRA.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-fra.exe
 :: URL|HEB|http://download.microsoft.com/download/4/2/C/42CCCA44-AEA1-4949-922E-07CB2E05EA2B/WindowsMedia9-KB979332-x86-HEB.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-heb.exe
 :: URL|HUN|http://download.microsoft.com/download/5/7/F/57F1C7EF-18AE-429E-9917-0791A2556893/WindowsMedia9-KB979332-x86-HUN.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-hun.exe
 :: URL|ITA|http://download.microsoft.com/download/9/F/2/9F28E9E1-0900-4855-80FC-2A2DA2AFD92C/WindowsMedia9-KB979332-x86-ITA.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-ita.exe
 :: URL|JPN|http://download.microsoft.com/download/8/9/2/8920F25D-11FF-4E32-9E96-BF52BEE5B4DB/WindowsMedia9-KB979332-x86-JPN.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-jpn.exe
-:: No Download found for KOR.
-:: No Download found for NLD.
-:: No Download found for NOR.
+:: URL|KOR|http://download.microsoft.com/download/D/7/E/D7E9FAD8-E5FA-4436-8066-636FFDA7D79B/WindowsMedia9-KB979332-x86-KOR.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/2/5/8/25864185-932B-4A4E-AB3A-82990B4787E1/WindowsMedia9-KB979332-x86-NLD.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/3/4/F/34F1AFA4-EA89-4369-B198-6748753B1B66/WindowsMedia9-KB979332-x86-NOR.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-nor.exe
 :: URL|PLK|http://download.microsoft.com/download/D/8/D/D8D9EF8B-CC17-4383-A4BA-488AC7C31ECA/WindowsMedia9-KB979332-x86-PLK.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-plk.exe
 :: URL|PTB|http://download.microsoft.com/download/5/5/0/55085F11-7099-4642-8518-B5B9958A4CBB/WindowsMedia9-KB979332-x86-PTB.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-ptb.exe
-:: No Download found for PTG.
-:: No Download found for RUS.
-:: No Download found for SVE.
-:: No Download found for TRK.
+:: URL|PTG|http://download.microsoft.com/download/C/A/7/CA7D547B-88F8-4C4F-8352-E1E1992E380C/WindowsMedia9-KB979332-x86-PTG.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/3/4/3/343E4AEE-F3E7-495B-B1F8-BF6F005A986E/WindowsMedia9-KB979332-x86-RUS.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/3/9/E/39E61B1A-E7A6-485E-BD03-7DD7AE69DC17/WindowsMedia9-KB979332-x86-SVE.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/C/0/C/C0CD9259-32AC-4D0E-A38F-B2788C360F1B/WindowsMedia9-KB979332-x86-TRK.exe|updates/winxpsp3/windowsmedia9-kb979332-x86-trk.exe
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsMedia9-KB979332-x86-%WINLANG%.exe /norestart /passive /nobackup"
 
+:: Download details: Security Update for Windows XP (KB979482)
 :: Microsoft Security Bulletin MS10-033 - Critical
 :: "Vulnerabilities in Media Decompression Could Allow Remote Code Execution (979902)"
 :: <http://www.microsoft.com/technet/security/bulletin/MS10-033.mspx>
 :: <http://www.microsoft.com/downloads/details.aspx?FamilyID=55C05CB8-AA6C-460B-9AA7-084842DAB280>
 :: URL|ARA|http://download.microsoft.com/download/6/8/6/686EEFDA-599E-407A-B60E-056FE98123A7/WindowsXP-KB979482-x86-ARA.exe|updates/winxpsp3/windowsxp-kb979482-x86-ara.exe
 :: URL|CSY|http://download.microsoft.com/download/1/3/D/13D96A23-0B90-4B2E-BF08-8726B183C5AA/WindowsXP-KB979482-x86-CSY.exe|updates/winxpsp3/windowsxp-kb979482-x86-csy.exe
-:: No Download found for DAN.
+:: URL|DAN|http://download.microsoft.com/download/E/2/5/E25E64C9-E660-424A-B16C-0E1DF59F481D/WindowsXP-KB979482-x86-DAN.exe|updates/winxpsp3/windowsxp-kb979482-x86-dan.exe
 :: URL|DEU|http://download.microsoft.com/download/E/6/0/E603336E-65A7-41D7-AAFF-A72B6A1301D7/WindowsXP-KB979482-x86-DEU.exe|updates/winxpsp3/windowsxp-kb979482-x86-deu.exe
 :: URL|ELL|http://download.microsoft.com/download/A/1/2/A1287151-FD4C-4E00-81FD-98DAD30CD3D6/WindowsXP-KB979482-x86-ELL.exe|updates/winxpsp3/windowsxp-kb979482-x86-ell.exe
-:: No Download found for ENU.
+:: URL|ENU|http://download.microsoft.com/download/3/A/6/3A6855B3-664A-4B8F-B2CF-99E203C7161A/WindowsXP-KB979482-x86-ENU.exe|updates/winxpsp3/windowsxp-kb979482-x86-enu.exe
 :: URL|ESN|http://download.microsoft.com/download/5/5/4/55407548-F89D-4450-869C-99D3209ACCCC/WindowsXP-KB979482-x86-ESN.exe|updates/winxpsp3/windowsxp-kb979482-x86-esn.exe
 :: URL|FIN|http://download.microsoft.com/download/C/3/A/C3A8A57B-E2B7-4218-A390-2A94430B4260/WindowsXP-KB979482-x86-FIN.exe|updates/winxpsp3/windowsxp-kb979482-x86-fin.exe
-:: No Download found for FRA.
+:: URL|FRA|http://download.microsoft.com/download/8/D/9/8D9A0177-F5E2-44A6-9DC2-44DCD3857E74/WindowsXP-KB979482-x86-FRA.exe|updates/winxpsp3/windowsxp-kb979482-x86-fra.exe
 :: URL|HEB|http://download.microsoft.com/download/9/9/F/99FDB36B-2724-4259-8E6F-C874379CB704/WindowsXP-KB979482-x86-HEB.exe|updates/winxpsp3/windowsxp-kb979482-x86-heb.exe
 :: URL|HUN|http://download.microsoft.com/download/5/A/4/5A45427B-E94B-4F24-8BAE-EB82BEA4D9ED/WindowsXP-KB979482-x86-HUN.exe|updates/winxpsp3/windowsxp-kb979482-x86-hun.exe
 :: URL|ITA|http://download.microsoft.com/download/B/B/E/BBE04B1D-FF64-44F6-832E-4D0CC20625FD/WindowsXP-KB979482-x86-ITA.exe|updates/winxpsp3/windowsxp-kb979482-x86-ita.exe
 :: URL|JPN|http://download.microsoft.com/download/3/3/3/33376527-B610-41A0-92FA-DA88524A2243/WindowsXP-KB979482-x86-JPN.exe|updates/winxpsp3/windowsxp-kb979482-x86-jpn.exe
-:: No Download found for KOR.
-:: No Download found for NLD.
-:: No Download found for NOR.
+:: URL|KOR|http://download.microsoft.com/download/2/E/1/2E1A27E6-691E-4FD1-9E65-8F7273237B79/WindowsXP-KB979482-x86-KOR.exe|updates/winxpsp3/windowsxp-kb979482-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/5/F/6/5F698B38-2E6F-489F-B915-2C30F851F284/WindowsXP-KB979482-x86-NLD.exe|updates/winxpsp3/windowsxp-kb979482-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/2/D/2/2D26F20A-FD4F-4F4E-B26F-4AAEC15109A3/WindowsXP-KB979482-x86-NOR.exe|updates/winxpsp3/windowsxp-kb979482-x86-nor.exe
 :: URL|PLK|http://download.microsoft.com/download/0/2/4/0242A294-E08F-4DAA-9845-7AFF69FC9E79/WindowsXP-KB979482-x86-PLK.exe|updates/winxpsp3/windowsxp-kb979482-x86-plk.exe
 :: URL|PTB|http://download.microsoft.com/download/9/6/D/96DFD3FA-8C3F-4497-AD13-F8017AE94219/WindowsXP-KB979482-x86-PTB.exe|updates/winxpsp3/windowsxp-kb979482-x86-ptb.exe
-:: No Download found for PTG.
-:: No Download found for RUS.
-:: No Download found for SVE.
-:: No Download found for TRK.
+:: URL|PTG|http://download.microsoft.com/download/F/E/F/FEFA8091-034C-434D-B52A-6483357EFBFB/WindowsXP-KB979482-x86-PTG.exe|updates/winxpsp3/windowsxp-kb979482-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/9/5/0/95039543-3B0A-45EF-B599-B3214F608CE5/WindowsXP-KB979482-x86-RUS.exe|updates/winxpsp3/windowsxp-kb979482-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/1/A/0/1A00C460-EB84-41A1-A84A-C6967DA4E74D/WindowsXP-KB979482-x86-SVE.exe|updates/winxpsp3/windowsxp-kb979482-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/F/5/3/F53F29DC-C8D6-484A-93FA-061041977E1D/WindowsXP-KB979482-x86-TRK.exe|updates/winxpsp3/windowsxp-kb979482-x86-trk.exe
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB979482-x86-%WINLANG%.exe /norestart /passive /nobackup"
 
+:: Download details: Cumulative Security Update for ActiveX Killbits for Windows XP (KB980195)
 :: Microsoft Security Bulletin MS10-034 - Critical
 :: "Cumulative Security Update of ActiveX Kill Bits (980195)"
 :: <http://www.microsoft.com/technet/security/bulletin/MS10-034.mspx>
 :: <http://www.microsoft.com/downloads/details.aspx?FamilyID=8c3f2e81-c0ea-494a-a47c-4f8982effb49>
 :: URL|ARA|http://download.microsoft.com/download/8/4/5/845EC64A-1A6C-4B11-9820-AC1977B4AEE3/WindowsXP-KB980195-x86-ARA.exe|updates/winxpsp3/windowsxp-kb980195-x86-ara.exe
 :: URL|CSY|http://download.microsoft.com/download/4/6/1/461FEC8C-E269-42F7-AE6F-3529E96930A7/WindowsXP-KB980195-x86-CSY.exe|updates/winxpsp3/windowsxp-kb980195-x86-csy.exe
-:: No Download found for DAN.
+:: URL|DAN|http://download.microsoft.com/download/C/6/1/C610C19E-5985-4474-8765-008B49F55777/WindowsXP-KB980195-x86-DAN.exe|updates/winxpsp3/windowsxp-kb980195-x86-dan.exe
 :: URL|DEU|http://download.microsoft.com/download/C/B/4/CB42B50A-0AD1-4FEE-B6A0-308B0C921EB2/WindowsXP-KB980195-x86-DEU.exe|updates/winxpsp3/windowsxp-kb980195-x86-deu.exe
 :: URL|ELL|http://download.microsoft.com/download/2/A/9/2A98EB11-0D83-42B7-9BB1-FB13B8A0D259/WindowsXP-KB980195-x86-ELL.exe|updates/winxpsp3/windowsxp-kb980195-x86-ell.exe
-:: No Download found for ENU.
+:: URL|ENU|http://download.microsoft.com/download/0/7/A/07AB5BDC-10C5-4716-B6B4-CC85BE71B8E9/WindowsXP-KB980195-x86-ENU.exe|updates/winxpsp3/windowsxp-kb980195-x86-enu.exe
 :: URL|ESN|http://download.microsoft.com/download/C/B/B/CBB0AD9C-C1F1-4033-BB2A-D7A80F0BE160/WindowsXP-KB980195-x86-ESN.exe|updates/winxpsp3/windowsxp-kb980195-x86-esn.exe
 :: URL|FIN|http://download.microsoft.com/download/E/A/A/EAA24535-338B-4921-A81D-94F65524FA12/WindowsXP-KB980195-x86-FIN.exe|updates/winxpsp3/windowsxp-kb980195-x86-fin.exe
-:: No Download found for FRA.
+:: URL|FRA|http://download.microsoft.com/download/6/8/7/687B8996-DB04-404F-B224-78DED405B4C9/WindowsXP-KB980195-x86-FRA.exe|updates/winxpsp3/windowsxp-kb980195-x86-fra.exe
 :: URL|HEB|http://download.microsoft.com/download/B/E/3/BE38BED4-7566-4F64-95F0-8A539C1D476E/WindowsXP-KB980195-x86-HEB.exe|updates/winxpsp3/windowsxp-kb980195-x86-heb.exe
 :: URL|HUN|http://download.microsoft.com/download/5/8/7/58718461-CDEC-4FAD-8A0D-463D62F59983/WindowsXP-KB980195-x86-HUN.exe|updates/winxpsp3/windowsxp-kb980195-x86-hun.exe
 :: URL|ITA|http://download.microsoft.com/download/2/B/2/2B29164E-26F6-4844-9473-0E616232DA1B/WindowsXP-KB980195-x86-ITA.exe|updates/winxpsp3/windowsxp-kb980195-x86-ita.exe
 :: URL|JPN|http://download.microsoft.com/download/F/B/3/FB3114EC-050D-4EBA-88D2-B136DBDCB1BA/WindowsXP-KB980195-x86-JPN.exe|updates/winxpsp3/windowsxp-kb980195-x86-jpn.exe
-:: No Download found for KOR.
-:: No Download found for NLD.
-:: No Download found for NOR.
+:: URL|KOR|http://download.microsoft.com/download/C/4/6/C468D8CD-0D03-4305-AA59-B6219B053146/WindowsXP-KB980195-x86-KOR.exe|updates/winxpsp3/windowsxp-kb980195-x86-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/D/5/D/D5DAE49F-B7D3-4C6F-9752-BFF0B82DAE2C/WindowsXP-KB980195-x86-NLD.exe|updates/winxpsp3/windowsxp-kb980195-x86-nld.exe
+:: URL|NOR|http://download.microsoft.com/download/1/0/3/10317CF0-10C0-4301-8335-FB1B8B48086A/WindowsXP-KB980195-x86-NOR.exe|updates/winxpsp3/windowsxp-kb980195-x86-nor.exe
 :: URL|PLK|http://download.microsoft.com/download/4/9/D/49D7153D-6ABA-4016-B38C-40A89339720D/WindowsXP-KB980195-x86-PLK.exe|updates/winxpsp3/windowsxp-kb980195-x86-plk.exe
 :: URL|PTB|http://download.microsoft.com/download/0/B/D/0BD33D7C-FD77-4EFD-BAF7-E47DF1C3A8E2/WindowsXP-KB980195-x86-PTB.exe|updates/winxpsp3/windowsxp-kb980195-x86-ptb.exe
-:: No Download found for PTG.
-:: No Download found for RUS.
-:: No Download found for SVE.
-:: No Download found for TRK.
+:: URL|PTG|http://download.microsoft.com/download/7/D/4/7D452135-4A09-4251-8DC6-D7B8D27AF752/WindowsXP-KB980195-x86-PTG.exe|updates/winxpsp3/windowsxp-kb980195-x86-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/C/4/E/C4E49016-4A2B-46D2-980C-A4A4CD04CAB0/WindowsXP-KB980195-x86-RUS.exe|updates/winxpsp3/windowsxp-kb980195-x86-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/5/F/8/5F8B0357-D2AD-4D1B-8E22-357A8EC0F7A8/WindowsXP-KB980195-x86-SVE.exe|updates/winxpsp3/windowsxp-kb980195-x86-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/A/8/3/A832C04C-56C4-4457-A969-0BE9FE8DA9F6/WindowsXP-KB980195-x86-TRK.exe|updates/winxpsp3/windowsxp-kb980195-x86-trk.exe
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB980195-x86-%WINLANG%.exe /norestart /passive /nobackup"
-
