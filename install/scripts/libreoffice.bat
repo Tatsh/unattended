bd454a6f3298382ba6a5962587d407e9fc194e90
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-01-25 11:44:02 +0000

    First Stable versio of LibreOffice

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,9 +1,10 @@
 ::OPTIONAL: Install LibreOffice
-::HOME: http://www.documentfoundation.org/download/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/testing/3.3.0-rc4/win/x86/LibO_3.3.0rc4_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.0rc4_Win_x86_install_multi.exe
+::HOME: http://www.libreoffice.org/
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.3.0/win/x86/LibO_3.3.0_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.0_Win_x86_install_multi.exe
+
 
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\LibO_3.3.0rc4_Win_x86_install_multi.exe /S"
+todo.pl "%Z%\packages\libreoffice\LibO_3.3.0_Win_x86_install_multi.exe /S"
 
