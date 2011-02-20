cc3531d479f28ad2641091100063aad5715173ff
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-02-20 02:32:06 +0000

    flash update to version 10.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3327 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install Flash 10.1.102.64 
 :: HOME: http://get.adobe.com/es/flashplayer/otherversions/
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10.1.102.64.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/macromedia/install_flash_player_10.2.152.26.exe
 ::
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\install_flash_player_10.1.102.64.exe -install"
+todo.pl "%Z%\packages\macromedia\install_flash_player_10.2.152.26.exe -install"
