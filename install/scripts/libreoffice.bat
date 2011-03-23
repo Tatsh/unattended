d77b62133e98e6f4f00909e3d88813ea67955874
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-03-23 18:02:54 +0000

    Libre office update to version 3.3.2

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,10 +1,10 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.3.1/win/x86/LibO_3.3.1_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.1_Win_x86_install_multi.exe
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.3.2/win/x86/LibO_3.3.2_Win_x86_install_multi.exe|packages/libreoffice/LibO_3.3.2_Win_x86_install_multi.exe
 
 
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\LibO_3.3.1_Win_x86_install_multi.exe /S"
+todo.pl "%Z%\packages\libreoffice\LibO_3.3.2_Win_x86_install_multi.exe /S"
 
