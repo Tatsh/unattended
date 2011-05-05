05dbd68306e7f24413570cc5d46cf169928012db
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-05-05 07:34:50 +0000

    Change folder to be more clear.

--- install/scripts/shockwave.bat
+++ install/scripts/shockwave.bat
@@ -1,6 +1,6 @@
 :: Install Shockwave10 player
-::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.2.0.023/Shockwave_Installer_Full.exe|packages/macromedia/shockwave_installer_full_10.2.0.023.exe
+::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.2.0.023/Shockwave_Installer_Full.exe|packages/shockwave/shockwave_installer_full_10.2.0.023.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\macromedia\shockwave_installer_full_10.2.0.023.exe /s"
+todo.pl "%Z%\packages\shockwave\shockwave_installer_full_10.2.0.023.exe /s"
