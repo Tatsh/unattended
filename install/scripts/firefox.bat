e14a1146b4a9b78e3c9178c7da44a2c30bcedd31
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-05-06 11:59:06 +0000

    Updated Firefox to 4.0.1, Shockwave to 11.5.9.620. Shockwave now uses the MSI installer, because I never managed to get the EXE installer to skip the Google toolbar dialogue window even with the silent installation flag (/s).
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3360 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox
 
-:: Firefox 4.0
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/ar/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/ca/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/cs/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/zh-TW/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/zh-CN/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/da/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/de/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/el/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/en-US/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/en-GB/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/es-ES/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/es-AR/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/fi/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/fr/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/he/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/hu/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/it/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/ja/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/ko/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/mk/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/nl/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/nb-NO/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/pl/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/pt-BR/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/pt-PT/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/ro/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/ru/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/sl/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/sv-SE/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/win32/tr/Firefox%20Setup%204.0.exe|packages/mozilla/firefox-40-trk.exe
+:: Firefox 4.0.1
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/ar/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/ca/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/cs/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/zh-TW/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/zh-CN/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/da/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/de/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/el/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/en-US/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/en-GB/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/es-ES/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/es-AR/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/fi/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/fr/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/he/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/hu/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/it/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/ja/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/ko/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/mk/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/nl/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/nb-NO/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/pl/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/pt-BR/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/pt-PT/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/ro/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/ru/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/sl/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/sv-SE/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0.1/win32/tr/Firefox%20Setup%204.0.1.exe|packages/mozilla/firefox-401-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-40-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-401-%WINLANG%.exe -ms -ira"
