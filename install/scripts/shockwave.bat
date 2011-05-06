6d502a5f042663858f28fdc26ac284ccddb47d2b
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-05-06 11:59:06 +0000

    Updated Firefox to 4.0.1, Shockwave to 11.5.9.620. Shockwave now uses the MSI installer, because I never managed to get the EXE installer to skip the Google toolbar dialogue window even with the silent installation flag (/s).

--- install/scripts/shockwave.bat
+++ install/scripts/shockwave.bat
@@ -1,6 +1,7 @@
-:: Install Shockwave10 player
-::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/10.2.0.023/Shockwave_Installer_Full.exe|packages/shockwave/shockwave_installer_full_10.2.0.023.exe
+:: Install Shockwave player
+::
+::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_11.5.9.620.msi
 
 @Echo off
 
-todo.pl "%Z%\packages\shockwave\shockwave_installer_full_10.2.0.023.exe /s"
+todo.pl "%Z%\packages\shockwave\shockwave_installer_full_11.5.9.620.msi /quiet /qb /norestart"
