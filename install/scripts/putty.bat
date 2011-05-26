c677d9d6fdbabfae67be6224ea67ee95ef72c99f
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-05-26 21:31:23 +0000

    Fix Download url for putty. Fix #62

--- install/scripts/putty.bat
+++ install/scripts/putty.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install Putty 0.60
 :: HOME: http://www.chiark.greenend.org.uk/~sgtatham/putty/
-:: URL|ALL|ftp://ftp.chiark.greenend.org.uk/users/sgtatham/putty-latest/x86/putty-0.60-installer.exe|packages/putty/putty-0.60-installer.exe
+:: URL|ALL|http://putty.bemirror.org/latest/x86/putty-0.60-installer.exe|packages/putty/putty-0.60-installer.exe
 @Echo off
 
 todo.pl "%Z%\packages\putty\putty-0.60-installer.exe /sp- /silent"
