ceebb7c7ad7c7f2a96b1c05989c52aef44cd8cfd
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-06-16 12:41:11 +0000

    Fixed some errors in vlc.bat.

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://downloads.sourceforge.net/vlc/1.1.10/win32/vlc-1.1.9-win32.exe|packages/vlc/vlc-1.1.9-win32.exe
+:: URL|All|http://downloads.sourceforge.net/vlc/1.1.10/win32/vlc-1.1.10-win32.exe|packages/vlc/vlc-1.1.10-win32.exe
 
 @Echo off
 
