6b447f28dd139eb30cec28633d7ea2e5edac286d
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-06-27 10:30:47 +0000

    update openoffice to version 3.3.0. Thanks Timothy J Massey
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3388 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/openoffice.bat
+++ install/scripts/openoffice.bat
@@ -1,23 +1,23 @@
 :: OPTIONAL: Install OpenOffice.org 
 
-:: Full install sets Localized Binaries for 3.2.1
+:: Full install sets Localized Binaries for 3.3.0
 :: http://download.openoffice.org/other.html
 
-:: URL|ENU|http://download.services.openoffice.org/files/stable/3.2.1/OOo_3.2.1_Win_x86_install_en-US.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_enu.exe
-:: URL|DAN|http://download.services.openoffice.org/files/localized/da/3.2.1/OOo_3.2.1_Win_x86_install_da.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_dan.exe
-:: URL|DEU|http://download.services.openoffice.org/files/localized/de/3.2.1/OOo_3.2.1_Win_x86_install_de.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_deu.exe
-:: URL|ESN|http://download.services.openoffice.org/files/localized/es/3.2.1/OOo_3.2.1_Win_x86_install_es.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_esn.exe
-:: URL|FRA|http://download.services.openoffice.org/files/localized/fr/3.2.1/OOo_3.2.1_Win_x86_install_fr.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_fra.exe
-:: URL|ITA|http://download.services.openoffice.org/files/localized/it/3.2.1/OOo_3.2.1_Win_x86_install_it.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_ita.exe
-:: URL|JPN|http://download.services.openoffice.org/files/localized/ja/3.2.1/OOo_3.2.1_Win_x86_install_ja.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_jpn.exe
-:: URL|NOR|http://download.services.openoffice.org/files/localized/nb/3.2.1/OOo_3.2.1_Win_x86_install_nb.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_nor.exe
-:: URL|NON|http://download.services.openoffice.org/files/localized/nn/3.2.0/OOo_3.2.0_Win_x86_install_nn.exe|packages/openoffice/ooo_3.2.0_Win_x86_install_non.exe
-:: URL|NLD|http://download.services.openoffice.org/files/localized/nl/3.2.1/OOo_3.2.1_Win_x86_install_nl.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_nld.exe
-:: URL|KOR|http://download.services.openoffice.org/files/localized/ko/3.2.0/OOo_3.2.0_Win_x86_install_ko.exe|packages/openoffice/ooo_3.2.0_Win_x86_install_kor.exe
-:: URL|PLK|http://download.services.openoffice.org/files/localized/pl/3.2.1/OOo_3.2.1_Win_x86_install_pl.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_plk.exe
-:: URL|RUS|http://download.services.openoffice.org/files/localized/ru/3.2.1/OOo_3.2.1_Win_x86_install_ru.exe|packages/openoffice/ooo_3.2.1_Win_x86_install_rus.exe
-:: URL|SVE|http://download.services.openoffice.org/files/localized/sv/3.2.0/OOo_3.2.0_Win_x86_install_sv.exe|packages/openoffice/ooo_3.2.0_Win_x86_install_sve.exe
-:: URL|PTB|ftp://ftp.broffice.org/stable/3.2.0/BrOOo_3.2.0_Win_x86_install_pt-BR.exe|packages/openoffice/OOo_3.2.0_Win_x86_install_ptb.exe
+:: URL|ENU|http://download.services.openoffice.org/files/stable/3.3.0/OOo_3.3.0_Win_x86_install_en-US.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_enu.exe
+:: URL|DAN|http://download.services.openoffice.org/files/localized/da/3.3.0/OOo_3.3.0_Win_x86_install_da.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_dan.exe
+:: URL|DEU|http://download.services.openoffice.org/files/localized/de/3.3.0/OOo_3.3.0_Win_x86_install_de.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_deu.exe
+:: URL|ESN|http://download.services.openoffice.org/files/localized/es/3.3.0/OOo_3.3.0_Win_x86_install_es.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_esn.exe
+:: URL|FRA|http://download.services.openoffice.org/files/localized/fr/3.3.0/OOo_3.3.0_Win_x86_install_fr.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_fra.exe
+:: URL|ITA|http://download.services.openoffice.org/files/localized/it/3.3.0/OOo_3.3.0_Win_x86_install_it.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_ita.exe
+:: URL|JPN|http://download.services.openoffice.org/files/localized/ja/3.3.0/OOo_3.3.0_Win_x86_install_ja.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_jpn.exe
+:: URL|NOR|http://download.services.openoffice.org/files/localized/nb/3.3.0/OOo_3.3.0_Win_x86_install_nb.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_nor.exe
+:: URL|NON|http://download.services.openoffice.org/files/localized/nn/3.3.0/OOo_3.3.0_Win_x86_install_nn.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_non.exe
+:: URL|NLD|http://download.services.openoffice.org/files/localized/nl/3.3.0/OOo_3.3.0_Win_x86_install_nl.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_nld.exe
+:: URL|KOR|http://download.services.openoffice.org/files/localized/ko/3.3.0/OOo_3.3.0_Win_x86_install_ko.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_kor.exe
+:: URL|PLK|http://download.services.openoffice.org/files/localized/pl/3.3.0/OOo_3.3.0_Win_x86_install_pl.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_plk.exe
+:: URL|RUS|http://download.services.openoffice.org/files/localized/ru/3.3.0/OOo_3.3.0_Win_x86_install_ru.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_rus.exe
+:: URL|SVE|http://download.services.openoffice.org/files/localized/sv/3.3.0/OOo_3.3.0_Win_x86_install_sv.exe|packages/openoffice/ooo_3.3.0_Win_x86_install_sve.exe
+:: URL|PTB|http://download.services.openoffice.org/files/localized/pt-BR/3.3.0/OOo_3.3.0_Win_x86_install_pt-BR.exe|packages/openoffice/OOo_3.3.0_Win_x86_install_ptb.exe
 :: URL|CHS|http://download.services.openoffice.org/files/localized/zh-CN/3.2.0/OOo_3.2.0_Win_x86_install_zh-CN.exe|packages/openoffice/ooo_3.2.0_Win_x86_install_chs.exe
 :: URL|CHT|http://download.services.openoffice.org/files/localized/zh-TW/3.2.0/OOo_3.2.0_Win_x86_install_zh-TW.exe|packages/openoffice/ooo_3.2.0_Win_x86_install_cht.exe
 
@@ -43,7 +43,7 @@
 
 :: VERSION - Sometimes i18n version does not have the most up to date.
 set VERSION=3.2.0
-if exist "%Z%\packages\openoffice\ooo_3.2.1_Win_x86_install_%WINLANG%.exe" set VERSION=3.2.1
+if exist "%Z%\packages\openoffice\ooo_3.3.0_Win_x86_install_%WINLANG%.exe" set VERSION=3.3.0
 
 :: Copy templates if they exists
 :: FIXME: It ask for confirmation
