fc70f40cbdfc43ddeee0cad14dba4334af1fcb26
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-08-31 13:16:01 +0000

    update to version 3.4.3

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,10 +1,10 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.4.2/win/x86/LibO_3.4.2_Win_x86_install_multi.exe|packages/libreoffice/libo_3.4.2_win_x86_install_multi.exe
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.4.3/win/x86/LibO_3.4.3_Win_x86_install_multi.exe|packages/libreoffice/libo_3.4.3_win_x86_install_multi.exe
 
 
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\libo_3.4.2_win_x86_install_multi.exe /S"
+todo.pl "%Z%\packages\libreoffice\libo_3.4.3_win_x86_install_multi.exe /S"
 
