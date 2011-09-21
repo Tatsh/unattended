d1bb530fb979d735d25c4c09f560239eb7119a51
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-09-21 12:22:24 +0000

    New versions of Flash (10.3.183.7) and Shockwave (11.6.1.629), also fixed a minor bug in perl.bat and added "ignore errors" to the registry commands in adobe-reader.bat.

--- trunk/install/scripts/flash.bat
+++ trunk/install/scripts/flash.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Flash 10.3.181.34
+:: OPTIONAL: Install Flash 10.3.183.7
 :: HOME: http://get.adobe.com/flashplayer/otherversions/ http://www.adobe.com/software/flash/about/
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/flash/install_flash_player_10.3.181.34.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_10.3.181.34.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player.exe|packages/flash/install_flash_player_10.3.183.7.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_10.3.183.7.exe
 ::
 @Echo off
 
@@ -9,7 +9,7 @@
 :: todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_10.3.181.34.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_10.3.183.7.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_10.3.181.34.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_10.3.183.7.exe -install"
