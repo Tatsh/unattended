fbf4ebd34bc13e35ad75e9fa778ff13200cf3cd1
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-09-26 06:39:13 +0000

    Flash updated to 10.3.183.10, Thunderbird to 6.0.2 and Opera to 11.51. The problem with previous new Opera installers is that they set Opera as default browser no matter what parameters we used. But I *think* this is fixed in version 11...

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,11 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/951/int/Opera_951_in_Setup.exe|packages/opera/opera-951-int.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/1151/int/Opera_1151_int_Setup.exe|packages/opera/opera_1151_int_setup.exe
 @Echo off
 
-todo.pl "shortcut.pl  \"%ProgramFiles%\Opera\opera.exe\" special:AllUsersDesktop\Opera"
-:: if you like to do special things (e.g. define custom searches), save your customized ini's in packages/opera/special
-
-if exist "%Z%\packages\opera\special\*" copy "%Z%\packages\opera\special\*" "%ProgramFiles%\Opera"
-
-todo.pl "%Z%\packages\opera\opera-951-int.exe /S /v/qn"
-
+todo.pl "%Z%\packages\opera\opera_1151_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
