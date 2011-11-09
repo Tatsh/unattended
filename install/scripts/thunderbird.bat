311ea0e8ba152b3ae4bccbcb0e4d7db835525778
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-11-09 09:40:48 +0000

    update version to 8.0

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,30 +1,33 @@
-:: OPTIONAL: Install Thunderbird 7.0.1
+:: OPTIONAL: Install Thunderbird 8.0
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/ca/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/cs/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/da/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/de/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/el/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/en-GB/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/en-US/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/es-ES/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/es-AR/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/fi/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/fr/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/he/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/hu/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/it/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/ja/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/ko/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/nl/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-nld.exe
-:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/nb-NO/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-nor.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/pl/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/pt-BR/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/ru/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/sl/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/sv-SE/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/7.0.1/win32/tr/Thunderbird%20Setup%207.0.1.exe|packages/mozilla/thunderbird-701-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/ca/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/cs/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/da/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/de/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/el/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/en-GB/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/en-US/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/es-ES/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/es-AR/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/fi/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/fr/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/he/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/hu/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/it/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/ja/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/ko/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/nl/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-nld.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/nb-NO/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-nor.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/pl/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/pt-BR/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/ru/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/sl/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/sv-SE/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/8.0/win32/tr/Thunderbird%20Setup%208.0.exe|packages/mozilla/thunderbird-800-trk.exe
 
 @echo off
 
-todo.pl "%Z%\packages\mozilla\thunderbird-701-%WINLANG%.exe -ms -ira"
+:: Add Thunderbird as Default Email Client
+:: todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d “Mozilla Thunderbird"
+
+todo.pl "%Z%\packages\mozilla\thunderbird-800-%WINLANG%.exe -ms -ira"
