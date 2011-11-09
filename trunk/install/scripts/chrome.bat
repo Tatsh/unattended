e7967d6d0b2e636e2cc176f325c126cbaec63229
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-11-09 09:47:56 +0000

    Chrome: Given that it holds 20% market share it shouldbe added to our repository

---
+++ trunk/install/scripts/chrome.bat
@@ -0,0 +1,7 @@
+:: OPTIONAL:  Install Chrome
+:: HOME: http://googlechromereleases.blogspot.com/
+:: URL|All|http://dl.google.com/chrome/install/874.106/chrome_installer.exe|packages/chrome/chrome_installer-15.0.874.106.exe
+@Echo off
+
+todo.pl "%Z%\packages\chrome\chrome_installer-15.0.874.106.exe --sytem-level"
+
