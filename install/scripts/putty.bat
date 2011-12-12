b570ab103a301648e508163064bab0f4aaff9128
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2011-12-12 01:08:02 +0000

    update to version 0.62
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3426 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/putty.bat
+++ install/scripts/putty.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Putty 0.61
+:: OPTIONAL: Install Putty 0.62
 :: HOME: http://www.chiark.greenend.org.uk/~sgtatham/putty/
 :: Fix %7e -> ~
-:: URL|ALL|http://the.earth.li/%7esgtatham/putty/latest/x86/putty-0.61-installer.exe|packages/putty/putty-0.61-installer.exe
+:: URL|ALL|http://the.earth.li/%7esgtatham/putty/latest/x86/putty-0.62-installer.exe|packages/putty/putty-0.62-installer.exe
 @Echo off
 
-todo.pl "%Z%\packages\putty\putty-0.61-installer.exe /sp- /silent"
+todo.pl "%Z%\packages\putty\putty-0.62-installer.exe /sp- /silent"
