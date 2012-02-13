ee84a6e5a00338e692dd88b9f820d29d6ec41248
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-02-13 21:35:39 +0000

    update to version 10.01
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3437 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox
 
-:: Firefox 10.0
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/ar/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/ca/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/cs/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/zh-TW/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/zh-CN/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/da/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/de/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/el/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/en-US/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/en-GB/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/es-ES/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/es-AR/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/fi/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/fr/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/he/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/hu/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/it/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/ja/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/ko/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/mk/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/nl/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/nb-NO/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/pl/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/pt-BR/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/pt-PT/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/ro/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/ru/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/sl/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/sv-SE/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0/win32/tr/Firefox%20Setup%2010.0.exe|packages/mozilla/firefox-100-trk.exe
+:: Firefox 10.0.1
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/ar/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/ca/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/cs/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/zh-TW/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/zh-CN/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/da/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/de/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/el/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/en-US/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/en-GB/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/es-ES/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/es-AR/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/fi/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/fr/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/he/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/hu/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/it/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/ja/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/ko/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/mk/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/nl/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/nb-NO/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/pl/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/pt-BR/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/pt-PT/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/ro/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/ru/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/sl/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/sv-SE/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/10.0.1/win32/tr/Firefox%20Setup%2010.0.1.exe|packages/mozilla/firefox-1001-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-100-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-1001-%WINLANG%.exe -ms -ira"
