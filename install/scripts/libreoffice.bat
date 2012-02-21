3df5a940522608500369e62fc84160981a17fdc5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-02-21 16:35:19 +0000

    update to versino 3.5

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,8 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.4.5/win/x86/LibO_3.4.5_Win_x86_install_multi.exe|packages/libreoffice/libo_3.4.5_win_x86_install_multi.exe
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.5.0/win/x86/LibO_3.5.0_Win_x86_install_multi.msi|packages/libreoffice/libo_3.5.0_win_x86_install_multi.msi
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\libo_3.4.5_win_x86_install_multi.exe /S"
-
+todo.pl "%Z%\packages\libreoffice\libo_3.5.0_win_x86_install_multi.msi /qb"
