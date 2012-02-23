9b9ab2049a75256501478e592d0ff19553ee7a96
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-02-23 11:54:41 +0000

    fix typo on system-level. Thanks to Joe Greco

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -3,5 +3,5 @@
 :: URL|All|http://dl.google.com/chrome/install/963.56/chrome_installer.exe|packages/chrome/chrome_installer-17.0.963.56.exe
 @Echo off
 
-todo.pl "%Z%\packages\chrome\chrome_installer-17.0.963.56.exe --sytem-level"
+todo.pl "%Z%\packages\chrome\chrome_installer-17.0.963.56.exe --system-level"
 
