a62d10f7775386a9619df5fef71789e126a29185
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-02-24 00:04:02 +0000

    patch for DotNet 3.5.1 SP1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3448 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/dotnet.bat
+++ install/scripts/dotnet.bat
@@ -1,5 +1,7 @@
 :: INSTALL :: Microsoft .NET 3.5 Redistributable Package (x86)
 :: http://msdn.microsoft.com/en-us/netframework/default.aspx
 :: URL|ALL|http://download.microsoft.com/download/2/0/e/20e90413-712f-438c-988e-fdaa79a8ac3d/dotnetfx35.exe|updates/common/dotnetfx35-sp1.exe
+:: URL|ALL|http://download.microsoft.com/download/C/6/A/C6ADC586-2518-404B-8973-E1E22C556AF4/NDP35SP1-KB958484-x86.exe|updates/common/NDP35SP1-KB958484-x86.exe
 
+todo.pl ".reboot-on 194 %Z%\updates\common\NDP35SP1-KB958484-x86.exe /q /norestart"
 todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx35-sp1.exe /q /norestart"
