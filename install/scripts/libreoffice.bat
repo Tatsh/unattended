7c8f21c3839334585fda7dfbe2685fb346625c9d
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-03-19 11:14:33 +0000

    update to version 3.5.1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3453 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.5.0/win/x86/LibO_3.5.0_Win_x86_install_multi.msi|packages/libreoffice/libo_3.5.0_win_x86_install_multi.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.5.1/win/x86/LibO_3.5.1_Win_x86_install_multi.msi|packages/libreoffice/libo_3.5.1_win_x86_install_multi.msi
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\libo_3.5.0_win_x86_install_multi.msi /qb"
+todo.pl "%Z%\packages\libreoffice\libo_3.5.1_win_x86_install_multi.msi /qb"
