6b7449e605042a54c0348cc65be115a23e73b4bf
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-04-12 12:22:43 +0000

    Fixed typo in vlc.bat, updated Flash, Opera and Shockwave.

--- trunk/install/scripts/flash.bat
+++ trunk/install/scripts/flash.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Flash 11.1.102.63
+:: OPTIONAL: Install Flash 11.2.202.228
 :: HOME: http://get.adobe.com/flashplayer/otherversions/ http://www.adobe.com/software/flash/about/
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.1.102.63/install_flash_player_32bit.exe|packages/flash/install_flash_player_11.1.102.63.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.1.102.63/install_flash_player_ax_32bit.exe|packages/flash/install_flash_player_ax_11.1.102.63.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.228/install_flash_player_32bit.exe|packages/flash/install_flash_player_11.2.202.228.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.228/install_flash_player_ax_32bit.exe|packages/flash/install_flash_player_ax_11.2.202.228.exe
 ::
 @Echo off
 
@@ -9,7 +9,7 @@
 :: todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_11.1.102.63.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_11.2.202.228.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_11.1.102.63.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_11.2.202.228.exe -install"
--- trunk/install/scripts/shockwave.bat
+++ trunk/install/scripts/shockwave.bat
@@ -1,7 +1,7 @@
 :: Install Shockwave player
 ::
-::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_11.6.1.629.msi
+::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_11.6.4.634.msi
 
 @Echo off
 
-todo.pl "%Z%\packages\shockwave\shockwave_installer_full_11.6.1.629.msi /quiet /qb /norestart"
+todo.pl "%Z%\packages\shockwave\shockwave_installer_full_11.6.4.634.msi /quiet /qb /norestart"
