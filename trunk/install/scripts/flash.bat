6083f811c30fb9412bd4e9bbb4a7ccec4f02f771
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-05-21 10:22:47 +0000

    New versions of Google Chrome, Flash Player, Opera, Shockwave Player and Thunderbird.

--- trunk/install/scripts/flash.bat
+++ trunk/install/scripts/flash.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Flash 11.2.202.233
+:: OPTIONAL: Install Flash 11.2.202.235
 :: HOME: http://get.adobe.com/flashplayer/otherversions/ http://www.adobe.com/software/flash/about/
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.233/install_flash_player_32bit.exe|packages/flash/install_flash_player_11.2.202.233.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.233/install_flash_player_ax_32bit.exe|packages/flash/install_flash_player_ax_11.2.202.233.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.235/install_flash_player_32bit.exe|packages/flash/install_flash_player_11.2.202.235.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.235/install_flash_player_ax_32bit.exe|packages/flash/install_flash_player_ax_11.2.202.235.exe
 ::
 @Echo off
 
@@ -9,7 +9,7 @@
 :: todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_11.2.202.233.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_11.2.202.235.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_11.2.202.233.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_11.2.202.235.exe -install"
