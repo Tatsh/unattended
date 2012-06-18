cc76eba8f5bbdc596cd756de74266c01f9c45901
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-06-18 17:40:33 +0000

    update to version 13.0.1

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox
 
-:: Firefox 13.0
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/ar/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/ca/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/cs/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/zh-TW/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/zh-CN/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/da/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/de/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/el/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/en-US/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/en-GB/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/es-ES/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/es-AR/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/fi/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/fr/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/he/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/hu/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/it/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/ja/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/ko/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/mk/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/nl/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/nb-NO/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/pl/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/pt-BR/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/pt-PT/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/ro/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/ru/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/sl/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/sv-SE/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0/win32/tr/Firefox%20Setup%2013.0.exe|packages/mozilla/firefox-1300-trk.exe
+:: Firefox 13.0.1
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/ar/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/ca/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/cs/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/zh-TW/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/zh-CN/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/da/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/de/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/el/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/en-US/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/en-GB/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/es-ES/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/es-AR/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/fi/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/fr/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/he/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/hu/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/it/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/ja/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/ko/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/mk/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/nl/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/nb-NO/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/pl/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/pt-BR/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/pt-PT/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/ro/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/ru/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/sl/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/sv-SE/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/13.0.1/win32/tr/Firefox%20Setup%2013.0.1.exe|packages/mozilla/firefox-1301-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-1300-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-1301-%WINLANG%.exe -ms -ira"
