aa33a8250c6d16d7e30046a4dccecb29e8c0ade6
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-06-20 23:37:04 +0000

    update scribus 1.4.1 gsview 5.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3476 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -1,11 +1,11 @@
 :: OPTIONAL: Install Ghostscript and GSview
 :: HOME: http://pages.cs.wisc.edu/~ghost/
 :: URL|ALL|http://downloads.sourceforge.net/ghostscript/gs864w32.exe|packages/ghostscript/gs864w32.exe
-:: URL|ALL|http://www.mirrorservice.org/sites/mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv49w32.exe|packages/ghostscript/gsv49w32.exe
+:: URL|ALL|http://www.mirrorservice.org/sites/mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
 :: Then manually extract to gs and gv folders in the same path
 
 todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
-todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv49w32.exe"
+todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv50w32.exe"
 
 todo.pl " start /D%TEMP%\gs /wait %TEMP%\gs\setupgs.exe \"%ProgramFiles%\gs"
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gs %Z%\packages\ghostscript\gs864w32.exe"
