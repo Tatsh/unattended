23d1689434162eceb664e360d362da029ca389d3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-06-20 23:37:04 +0000

    update scribus 1.4.1 gsview 5.0

--- install/scripts/scribus.bat
+++ install/scripts/scribus.bat
@@ -1,13 +1,13 @@
 :: OPTIONAL:  Install Scribus
 :: HOME: http://www.scribus.net/
 
-:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.0-windows.exe|packages/scribus/scribus-1.4.0-windows.exe
+:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.1-windows.exe|packages/scribus/scribus-1.4.1-windows.exe
 @Echo off
 
 
 
 :: todo.pl "shortcut.pl \"%ProgramFiles%\"\Scribus\scribus.exe special:AllUsersDesktop\Scribus"
-todo.pl "%Z%\packages\scribus\scribus-1.4.0-windows.exe /S"
+todo.pl "%Z%\packages\scribus\scribus-1.4.1-windows.exe /S"
 
 :: Install gsview first
 todo.pl gsview.bat
