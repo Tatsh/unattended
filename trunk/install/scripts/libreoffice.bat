fcc8643eeb551783b73ad55b970b83a9ec52818e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-06-22 00:14:04 +0000

    use msiexec so it does not stop

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.5.3/win/x86/LibO_3.5.3_Win_x86_install_multi.msi|packages/libreoffice/libo_3.5.3_win_x86_install_multi.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.5.4/win/x86/LibO_3.5.4_Win_x86_install_multi.msi|packages/libreoffice/libo_3.5.4_win_x86_install_multi.msi
 
 @echo off
 
-todo.pl "%Z%\packages\libreoffice\libo_3.5.3_win_x86_install_multi.msi /qb"
+todo.pl "msiexec /qb /i %Z%\packages\libreoffice\libo_3.5.4_win_x86_install_multi.msi"
