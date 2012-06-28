27a8142b8a708b7f305a851e6783045f0f149d02
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-06-28 22:50:55 +0000

    Add security updates Feb 2011

--- install/scripts/winxpsp3-up1012.bat
+++ install/scripts/winxpsp3-up1012.bat
@@ -1,62 +1,4 @@
 
-:: Download details: Cumulative Security Update for Internet Explorer for Windows XP (KB2416400)
-:: Microsoft Security Bulletin MS10-090 - Critical
-:: "Cumulative Security Update for Internet Explorer (2416400)"
-:: <http://go.microsoft.com/fwlink/?LinkId=206495>
-:: <http://www.microsoft.com/downloads/details.aspx?FamilyID=6031d98a-cd0f-4dd8-80b6-70a7167e9e55>
-:: URL|ARA|http://download.microsoft.com/download/D/E/8/DE8BF709-1A1A-48CF-9274-8C54575F3AB3/WindowsXP-KB2416400-x86-ARA.exe|updates/winxpsp3/windowsxp-kb2416400-x86-ara.exe
-:: URL|CSY|http://download.microsoft.com/download/A/1/5/A15A5413-22D5-4989-B704-DDF2FF84679D/WindowsXP-KB2416400-x86-CSY.exe|updates/winxpsp3/windowsxp-kb2416400-x86-csy.exe
-:: URL|DAN|http://download.microsoft.com/download/7/C/A/7CA51E0D-03AB-4B70-84A2-9C4CA92C7677/WindowsXP-KB2416400-x86-DAN.exe|updates/winxpsp3/windowsxp-kb2416400-x86-dan.exe
-:: URL|DEU|http://download.microsoft.com/download/E/9/B/E9B24E21-06E4-423F-8C3D-F939C9A582A9/WindowsXP-KB2416400-x86-DEU.exe|updates/winxpsp3/windowsxp-kb2416400-x86-deu.exe
-:: URL|ELL|http://download.microsoft.com/download/A/1/4/A148241E-F144-4C73-BE43-2387C90B8994/WindowsXP-KB2416400-x86-ELL.exe|updates/winxpsp3/windowsxp-kb2416400-x86-ell.exe
-:: URL|ENU|http://download.microsoft.com/download/0/8/1/081B7329-855C-4030-8203-1C5B1286B792/WindowsXP-KB2416400-x86-ENU.exe|updates/winxpsp3/windowsxp-kb2416400-x86-enu.exe
-:: URL|ESN|http://download.microsoft.com/download/3/6/F/36FE499A-CEFC-4C55-9C94-13D9568AC233/WindowsXP-KB2416400-x86-ESN.exe|updates/winxpsp3/windowsxp-kb2416400-x86-esn.exe
-:: URL|FIN|http://download.microsoft.com/download/0/C/7/0C752AAD-5078-46D0-A87B-27BE7DC4A458/WindowsXP-KB2416400-x86-FIN.exe|updates/winxpsp3/windowsxp-kb2416400-x86-fin.exe
-:: URL|FRA|http://download.microsoft.com/download/A/4/D/A4D915B6-A1AC-4670-8C16-8E6746A28AA2/WindowsXP-KB2416400-x86-FRA.exe|updates/winxpsp3/windowsxp-kb2416400-x86-fra.exe
-:: URL|HEB|http://download.microsoft.com/download/0/6/6/066E542D-3410-4C0B-8B93-DFFFD2C91D4D/WindowsXP-KB2416400-x86-HEB.exe|updates/winxpsp3/windowsxp-kb2416400-x86-heb.exe
-:: URL|HUN|http://download.microsoft.com/download/7/0/4/7044FFC8-8BCD-4784-815C-002CD3160F1C/WindowsXP-KB2416400-x86-HUN.exe|updates/winxpsp3/windowsxp-kb2416400-x86-hun.exe
-:: URL|ITA|http://download.microsoft.com/download/1/D/C/1DC4A83A-15EC-4C77-A46A-1BA06CB27C40/WindowsXP-KB2416400-x86-ITA.exe|updates/winxpsp3/windowsxp-kb2416400-x86-ita.exe
-:: URL|JPN|http://download.microsoft.com/download/2/8/1/281AC8BC-9B3F-4E57-984F-5AA6E9E1135B/WindowsXP-KB2416400-x86-JPN.exe|updates/winxpsp3/windowsxp-kb2416400-x86-jpn.exe
-:: URL|KOR|http://download.microsoft.com/download/6/B/0/6B052797-0296-4DDA-A53C-5032E36BCFB7/WindowsXP-KB2416400-x86-KOR.exe|updates/winxpsp3/windowsxp-kb2416400-x86-kor.exe
-:: URL|NLD|http://download.microsoft.com/download/F/C/5/FC5AE8B1-231F-4E02-B6D5-74A75D8B38EA/WindowsXP-KB2416400-x86-NLD.exe|updates/winxpsp3/windowsxp-kb2416400-x86-nld.exe
-:: URL|NOR|http://download.microsoft.com/download/0/6/6/0668CA5B-1AA1-49D8-98EF-BC810596B50E/WindowsXP-KB2416400-x86-NOR.exe|updates/winxpsp3/windowsxp-kb2416400-x86-nor.exe
-:: URL|PLK|http://download.microsoft.com/download/E/2/4/E2494957-344E-4B4D-9D8F-F618C78265F2/WindowsXP-KB2416400-x86-PLK.exe|updates/winxpsp3/windowsxp-kb2416400-x86-plk.exe
-:: URL|PTB|http://download.microsoft.com/download/5/2/2/522A3A62-2356-43A2-BB1F-D88C9CEC05CD/WindowsXP-KB2416400-x86-PTB.exe|updates/winxpsp3/windowsxp-kb2416400-x86-ptb.exe
-:: URL|PTG|http://download.microsoft.com/download/F/6/2/F6243B4C-BE52-474A-A4F7-D48761A84B58/WindowsXP-KB2416400-x86-PTG.exe|updates/winxpsp3/windowsxp-kb2416400-x86-ptg.exe
-:: URL|RUS|http://download.microsoft.com/download/D/9/D/D9D8CB8B-12AC-4A6E-A664-5025086CD12A/WindowsXP-KB2416400-x86-RUS.exe|updates/winxpsp3/windowsxp-kb2416400-x86-rus.exe
-:: URL|SVE|http://download.microsoft.com/download/3/4/7/347B5B80-84CB-46E7-8D0E-0FE4A819AB19/WindowsXP-KB2416400-x86-SVE.exe|updates/winxpsp3/windowsxp-kb2416400-x86-sve.exe
-:: URL|TRK|http://download.microsoft.com/download/F/3/6/F36B294E-1F8B-42C3-9C8F-477797A1872E/WindowsXP-KB2416400-x86-TRK.exe|updates/winxpsp3/windowsxp-kb2416400-x86-trk.exe
-todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB2416400-x86-%WINLANG%.exe /norestart /passive /nobackup"
-
-:: Download details: Security Update for Windows XP (KB2296199)
-:: Microsoft Security Bulletin MS10-091 - Critical
-:: "Vulnerabilities in the OpenType Font (OTF) Driver Could Allow Remote Code Execution (2296199)"
-:: <http://www.microsoft.com/technet/security/bulletin/MS10-091.mspx>
-:: <http://www.microsoft.com/downloads/details.aspx?familyid=CDEF3358-AD3E-40A6-9BA5-3BE220A56A65>
-:: URL|ARA|http://download.microsoft.com/download/6/2/3/623049E8-3112-49BD-BBCA-FD68C933C948/WindowsXP-KB2296199-x86-ARA.exe|updates/winxpsp3/windowsxp-kb2296199-x86-ara.exe
-:: URL|CSY|http://download.microsoft.com/download/6/1/2/612D95B7-71AC-446A-89B4-FFD7264DE9A3/WindowsXP-KB2296199-x86-CSY.exe|updates/winxpsp3/windowsxp-kb2296199-x86-csy.exe
-:: URL|DAN|http://download.microsoft.com/download/3/F/A/3FA75490-3162-46CE-949D-FBDE44BB7069/WindowsXP-KB2296199-x86-DAN.exe|updates/winxpsp3/windowsxp-kb2296199-x86-dan.exe
-:: URL|DEU|http://download.microsoft.com/download/E/1/E/E1E9F471-19DC-41FE-82FB-DC38C5EF2CAE/WindowsXP-KB2296199-x86-DEU.exe|updates/winxpsp3/windowsxp-kb2296199-x86-deu.exe
-:: URL|ELL|http://download.microsoft.com/download/A/0/0/A00DF166-2A85-421F-9A99-5A2A89FC2F04/WindowsXP-KB2296199-x86-ELL.exe|updates/winxpsp3/windowsxp-kb2296199-x86-ell.exe
-:: URL|ENU|http://download.microsoft.com/download/B/5/1/B518AF1B-31C9-4A19-95E6-1D682685015E/WindowsXP-KB2296199-x86-ENU.exe|updates/winxpsp3/windowsxp-kb2296199-x86-enu.exe
-:: URL|ESN|http://download.microsoft.com/download/4/8/B/48BA4619-D7EF-42E1-A761-9F476FB9D4AE/WindowsXP-KB2296199-x86-ESN.exe|updates/winxpsp3/windowsxp-kb2296199-x86-esn.exe
-:: URL|FIN|http://download.microsoft.com/download/0/F/7/0F7F80C7-620D-4B16-B2A2-77FD000E26EF/WindowsXP-KB2296199-x86-FIN.exe|updates/winxpsp3/windowsxp-kb2296199-x86-fin.exe
-:: URL|FRA|http://download.microsoft.com/download/0/F/F/0FFEDCA0-4AF9-45FC-975D-C9CCC7222C02/WindowsXP-KB2296199-x86-FRA.exe|updates/winxpsp3/windowsxp-kb2296199-x86-fra.exe
-:: URL|HEB|http://download.microsoft.com/download/3/1/1/311DD384-AE21-413E-BC0B-64B7429DD65B/WindowsXP-KB2296199-x86-HEB.exe|updates/winxpsp3/windowsxp-kb2296199-x86-heb.exe
-:: URL|HUN|http://download.microsoft.com/download/4/6/5/4653483F-60EF-4DEC-BF40-26950248662A/WindowsXP-KB2296199-x86-HUN.exe|updates/winxpsp3/windowsxp-kb2296199-x86-hun.exe
-:: URL|ITA|http://download.microsoft.com/download/8/D/7/8D703663-730B-46A0-A4AD-8B987DF4BC20/WindowsXP-KB2296199-x86-ITA.exe|updates/winxpsp3/windowsxp-kb2296199-x86-ita.exe
-:: URL|JPN|http://download.microsoft.com/download/A/F/B/AFBCB01D-21D6-4CBE-83BB-A2A09EDD2ED2/WindowsXP-KB2296199-x86-JPN.exe|updates/winxpsp3/windowsxp-kb2296199-x86-jpn.exe
-:: URL|KOR|http://download.microsoft.com/download/8/9/E/89E5885B-4F53-48B3-9AAC-53959C52FECE/WindowsXP-KB2296199-x86-KOR.exe|updates/winxpsp3/windowsxp-kb2296199-x86-kor.exe
-:: URL|NLD|http://download.microsoft.com/download/D/C/5/DC51C586-2D0A-4AC1-B551-FF090253372A/WindowsXP-KB2296199-x86-NLD.exe|updates/winxpsp3/windowsxp-kb2296199-x86-nld.exe
-:: URL|NOR|http://download.microsoft.com/download/9/D/7/9D769F26-6BCB-4F4F-893E-54FAA3E6EA53/WindowsXP-KB2296199-x86-NOR.exe|updates/winxpsp3/windowsxp-kb2296199-x86-nor.exe
-:: URL|PLK|http://download.microsoft.com/download/7/E/4/7E45A4EB-06AA-428F-9665-34B8F519671F/WindowsXP-KB2296199-x86-PLK.exe|updates/winxpsp3/windowsxp-kb2296199-x86-plk.exe
-:: URL|PTB|http://download.microsoft.com/download/0/C/2/0C279E26-A55E-4BA2-B410-0EF7A0B193D1/WindowsXP-KB2296199-x86-PTB.exe|updates/winxpsp3/windowsxp-kb2296199-x86-ptb.exe
-:: URL|PTG|http://download.microsoft.com/download/2/D/D/2DDB12D2-0A6C-4EBE-8FE0-96185595DA69/WindowsXP-KB2296199-x86-PTG.exe|updates/winxpsp3/windowsxp-kb2296199-x86-ptg.exe
-:: URL|RUS|http://download.microsoft.com/download/2/1/0/2103D1B9-23CD-43B2-AD62-58EE6B5FBF41/WindowsXP-KB2296199-x86-RUS.exe|updates/winxpsp3/windowsxp-kb2296199-x86-rus.exe
-:: URL|SVE|http://download.microsoft.com/download/F/C/4/FC4A61C1-A053-4055-A2E1-B7C409A9FAE0/WindowsXP-KB2296199-x86-SVE.exe|updates/winxpsp3/windowsxp-kb2296199-x86-sve.exe
-:: URL|TRK|http://download.microsoft.com/download/5/C/A/5CAACD50-AF92-4603-8633-1E4E14CC8B83/WindowsXP-KB2296199-x86-TRK.exe|updates/winxpsp3/windowsxp-kb2296199-x86-trk.exe
-todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB2296199-x86-%WINLANG%.exe /norestart /passive /nobackup"
-
 :: Download details: Security Update for Windows XP (KB2423089)
 :: <http://support.microsoft.com/?kbid=2423089>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=46BAA431-126C-4FA5-9A7B-525008E2817D>
@@ -110,33 +52,6 @@ todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB2423089-x86-%WINLANG%.e
 :: URL|SVE|http://download.microsoft.com/download/E/1/3/E13C7B65-7EBD-4AFC-AF1B-C939F2D51659/WindowsXP-KB2443105-x86-SVE.exe|updates/winxpsp3/windowsxp-kb2443105-x86-sve.exe
 :: URL|TRK|http://download.microsoft.com/download/4/7/E/47E7FE5E-8EE1-4416-805B-BF930A000820/WindowsXP-KB2443105-x86-TRK.exe|updates/winxpsp3/windowsxp-kb2443105-x86-trk.exe
 todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB2443105-x86-%WINLANG%.exe /norestart /passive /nobackup"
-
-:: Download details: Security Update for Windows XP (KB2436673)
-:: <http://support.microsoft.com/?kbid=2436673>
-:: <http://www.microsoft.com/downloads/details.aspx?familyid=BB9D1657-5BEB-4372-B74C-A612A6FFF5A8>
-:: URL|ARA|http://download.microsoft.com/download/5/A/5/5A539791-DA41-4409-83C5-14B1011E26B6/WindowsXP-KB2436673-x86-ARA.exe|updates/winxpsp3/windowsxp-kb2436673-x86-ara.exe
-:: URL|CSY|http://download.microsoft.com/download/C/D/E/CDE82CC2-ED8B-4523-BC4C-57C17E988B43/WindowsXP-KB2436673-x86-CSY.exe|updates/winxpsp3/windowsxp-kb2436673-x86-csy.exe
-:: URL|DAN|http://download.microsoft.com/download/F/A/8/FA87EAAB-C2C8-4200-AF25-4D4E0C148668/WindowsXP-KB2436673-x86-DAN.exe|updates/winxpsp3/windowsxp-kb2436673-x86-dan.exe
-:: URL|DEU|http://download.microsoft.com/download/4/D/C/4DC38A98-1F77-49FB-A78F-235F21A1BCC8/WindowsXP-KB2436673-x86-DEU.exe|updates/winxpsp3/windowsxp-kb2436673-x86-deu.exe
-:: URL|ELL|http://download.microsoft.com/download/9/F/0/9F046D06-156F-4808-8867-6EF0FF41C679/WindowsXP-KB2436673-x86-ELL.exe|updates/winxpsp3/windowsxp-kb2436673-x86-ell.exe
-:: URL|ENU|http://download.microsoft.com/download/0/0/5/0050ACB7-EDC8-4D93-B136-D629F915B373/WindowsXP-KB2436673-x86-ENU.exe|updates/winxpsp3/windowsxp-kb2436673-x86-enu.exe
-:: URL|ESN|http://download.microsoft.com/download/A/7/A/A7A0C3C0-BA9A-4658-8382-1F71C6C4B85F/WindowsXP-KB2436673-x86-ESN.exe|updates/winxpsp3/windowsxp-kb2436673-x86-esn.exe
-:: URL|FIN|http://download.microsoft.com/download/3/E/C/3EC61BA6-566E-42B8-A871-BD9D4AF75893/WindowsXP-KB2436673-x86-FIN.exe|updates/winxpsp3/windowsxp-kb2436673-x86-fin.exe
-:: URL|FRA|http://download.microsoft.com/download/A/7/5/A75251E4-5A65-4676-BEE6-4899D30913BC/WindowsXP-KB2436673-x86-FRA.exe|updates/winxpsp3/windowsxp-kb2436673-x86-fra.exe
-:: URL|HEB|http://download.microsoft.com/download/D/3/2/D32AA297-ABF4-4B8D-AE0E-4BC4E20CBB0D/WindowsXP-KB2436673-x86-HEB.exe|updates/winxpsp3/windowsxp-kb2436673-x86-heb.exe
-:: URL|HUN|http://download.microsoft.com/download/1/9/5/19562AA5-B1DB-4971-BC61-825B13B83650/WindowsXP-KB2436673-x86-HUN.exe|updates/winxpsp3/windowsxp-kb2436673-x86-hun.exe
-:: URL|ITA|http://download.microsoft.com/download/2/1/C/21C507C2-9E3B-48B7-9059-10C07EBF316E/WindowsXP-KB2436673-x86-ITA.exe|updates/winxpsp3/windowsxp-kb2436673-x86-ita.exe
-:: URL|JPN|http://download.microsoft.com/download/C/9/D/C9D69E5D-0421-4E07-BDA9-72C0CF73B41D/WindowsXP-KB2436673-x86-JPN.exe|updates/winxpsp3/windowsxp-kb2436673-x86-jpn.exe
-:: URL|KOR|http://download.microsoft.com/download/3/3/5/33555F4D-81F9-4C99-B1C6-5A321EB3EE98/WindowsXP-KB2436673-x86-KOR.exe|updates/winxpsp3/windowsxp-kb2436673-x86-kor.exe
-:: URL|NLD|http://download.microsoft.com/download/3/2/F/32F24985-0728-4443-A2B8-4BB41EF2F21A/WindowsXP-KB2436673-x86-NLD.exe|updates/winxpsp3/windowsxp-kb2436673-x86-nld.exe
-:: URL|NOR|http://download.microsoft.com/download/6/A/F/6AF474ED-A9A8-4796-A48A-611BBB27193C/WindowsXP-KB2436673-x86-NOR.exe|updates/winxpsp3/windowsxp-kb2436673-x86-nor.exe
-:: URL|PLK|http://download.microsoft.com/download/3/2/2/3229CCD2-9C7D-47D2-8E87-45D40407EC4A/WindowsXP-KB2436673-x86-PLK.exe|updates/winxpsp3/windowsxp-kb2436673-x86-plk.exe
-:: URL|PTB|http://download.microsoft.com/download/7/A/3/7A3AB37C-2D0A-40EA-A7BD-62FF56419D4B/WindowsXP-KB2436673-x86-PTB.exe|updates/winxpsp3/windowsxp-kb2436673-x86-ptb.exe
-:: URL|PTG|http://download.microsoft.com/download/A/B/5/AB52C5CA-4C93-46AF-AF4D-6CB3FB2A5795/WindowsXP-KB2436673-x86-PTG.exe|updates/winxpsp3/windowsxp-kb2436673-x86-ptg.exe
-:: URL|RUS|http://download.microsoft.com/download/A/E/4/AE4E0E68-19F6-4232-ACAA-6141F781554B/WindowsXP-KB2436673-x86-RUS.exe|updates/winxpsp3/windowsxp-kb2436673-x86-rus.exe
-:: URL|SVE|http://download.microsoft.com/download/5/5/D/55DFB067-6B65-427C-8D9C-8A5F542B6240/WindowsXP-KB2436673-x86-SVE.exe|updates/winxpsp3/windowsxp-kb2436673-x86-sve.exe
-:: URL|TRK|http://download.microsoft.com/download/D/C/3/DC3DC70D-4FA2-450E-969E-20C3AD865666/WindowsXP-KB2436673-x86-TRK.exe|updates/winxpsp3/windowsxp-kb2436673-x86-trk.exe
-todo.pl ".reboot-on 194 %Z%\updates\winxpsp3\WindowsXP-KB2436673-x86-%WINLANG%.exe /norestart /passive /nobackup"
 
 :: Download details: Security Update for Windows XP (KB2440591)
 :: <http://support.microsoft.com/?kbid=2440591>
