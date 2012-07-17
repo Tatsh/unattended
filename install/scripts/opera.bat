aa0252754cd209a26f4ae3911c5f06ab11dda4a9
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-07-17 13:19:29 +0000

    New versions of Google Chrome, Adobe Flash Player, Opera and VLC.

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/1164/int/Opera_1164_int_Setup.exe|packages/opera/opera_1164_int_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/1200/int/Opera_1200_int_Setup.exe|packages/opera/opera_1200_int_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_1164_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
+todo.pl "%Z%\packages\opera\opera_1200_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
