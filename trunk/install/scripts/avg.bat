637eaddb90f14ba95232bb5e453c79f0d1ef346a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-09-07 00:28:19 +0000

    update to version 2013

--- trunk/install/scripts/avg.bat
+++ trunk/install/scripts/avg.bat
@@ -1,13 +1,13 @@
 :: OPTIONAL: AVG Anti-Virus Free Edition
 :: HOME:http://free.avg.com/es-es/descargar.prd-afh#tba1
-:: URL|ALL|http://download.avgfree.com/softw/12free/update/u12iavi5098lh.bin|packages/avg/updates/u12iavi5098lh.bin
-:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x86_all_2012_2180a5034.exe|packages/avg/avg_free_x86_all_2012_2180a5034.exe
-:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x64_all_2012_2180a5034.exe|packages/avg/avg_free_AMD64_all_2012_2180a5034.exe
+:: URL|ALL|http://download.avgfree.com/softw/13free/update/u13iavi5751yh.bin|packages/avg/updates/u13iavi5751yh.bin
+:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x86_all_2013_2667a5738.exe|packages/avg/avg_free_x86_all_2013_2667a5738.exe
+:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x64_all_2013_2667a5738.exe|packages/avg/avg_free_AMD64_all_2013_2667a5738.exe
 
 @echo off
 
 :: AVG use a icon on the tray so this link is unecesary.
-todo.pl "unlink.pl special:AllUsersDesktop\"\AVG 2012.lnk\""
+todo.pl "unlink.pl special:AllUsersDesktop\"\AVG 2013.lnk\""
 
 ::  Update program 
 ::todo.pl "\"%PROGRAMFILES%\AVG\AVG2012\avgmfapx.exe\" /AppMode=UPDATE"
@@ -18,4 +18,4 @@ todo.pl "\"%PROGRAMFILES%\AVG\AVG2012\avgmfapx.exe\" /AppMode=UPDATE /source=fol
 todo.pl ".reboot"
 
 ::  HttpScanner,SafeSearch and SafeSurf are plugins for the Browser. Keep them out. 
-todo.pl "%Z%\packages\avg\avg_free_%PROCESSOR_ARCHITECTURE%_all_2012_2180a5034.exe /UILevel=Silent /REMOVE_FEATURE=fea_AVG_SafeSurf,fea_AVG_SafeSearch,fea_AVG_HttpScanner /InstallToolbar=0 /InstallSidebar=0 /DontRestart"
+todo.pl "%Z%\packages\avg\avg_free_%PROCESSOR_ARCHITECTURE%_all_2013_2667a5738.exe /UILevel=Silent /REMOVE_FEATURE=fea_AVG_SafeSurf,fea_AVG_SafeSearch,fea_AVG_HttpScanner /InstallToolbar=0 /InstallSidebar=0 /DontRestart"
