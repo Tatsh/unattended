0b1c6baaebc3b63bb8ba9a5b6ae9de7fde370883
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-09-08 17:36:06 +0000

    update to version 2.8.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3499 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/gimp.bat
+++ install/scripts/gimp.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install GIMP (2.8)
 ::HOME: http://gimp-win.sourceforge.net/
-::URL|ALL|http://downloads.sourceforge.net/gimp-win/gimp-2.8.0-setup.exe|packages/gimp/gimp-2.8.0-setup.exe
+::URL|ALL|http://downloads.sourceforge.net/gimp-win/gimp-2.8.2-setup-1.exe|packages/gimp/gimp-2.8.2-setup.exe
 @echo off
 
 :: Run the installers
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.8.0-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.8.2-setup.exe SP- /SILENT /NORESTART"
