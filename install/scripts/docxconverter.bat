9cb0611552af0fd87933f7a1b15f2115715bac1a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-09-16 10:38:23 +0000

    Servicepack update
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3501 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/docxconverter.bat
+++ install/scripts/docxconverter.bat
@@ -1,3 +1,26 @@
+:: De.scription of Office Compatibility Pack SP3
+:: <http://support.microsoft.com/kb/2526297>
+:: <http://www.microsoft.com/en-us/download/details.aspx?id=27836>
+:: URL|CSY|http://download.microsoft.com/download/8/D/1/8D163AC5-E68B-46AF-866A-1EEA63E8EE38/compatibilitypacksp3-kb2526297-fullfile-cs-cz.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-csy.exe
+:: URL|DAN|http://download.microsoft.com/download/7/1/0/7103255E-317B-48E0-A5AD-975874F57464/compatibilitypacksp3-kb2526297-fullfile-da-dk.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-dan.exe
+:: URL|DEU|http://download.microsoft.com/download/4/1/9/41917719-86E0-41CF-9CCA-AC6FE0069E4C/compatibilitypacksp3-kb2526297-fullfile-de-de.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-deu.exe
+:: URL|ELL|http://download.microsoft.com/download/C/D/A/CDA36977-624D-4B05-805D-95792FD857F0/compatibilitypacksp3-kb2526297-fullfile-el-gr.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-ell.exe
+:: URL|ENU|http://download.microsoft.com/download/D/2/9/D2906267-82B7-4F8C-B1D8-B24C461A5653/compatibilitypacksp3-kb2526297-fullfile-en-us.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/C/E/4/CE439759-6982-4BF3-9B83-22B66086248F/compatibilitypacksp3-kb2526297-fullfile-es-es.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-esn.exe
+:: URL|FIN|http://download.microsoft.com/download/B/7/1/B7159C65-5EFF-4AF2-9938-C423DBF41ADC/compatibilitypacksp3-kb2526297-fullfile-fi-fi.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-fin.exe
+:: URL|FRA|http://download.microsoft.com/download/3/8/5/385B8FD0-5A45-4853-B23F-5ABFE1E18FD0/compatibilitypacksp3-kb2526297-fullfile-fr-fr.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-fra.exe
+:: URL|HUN|http://download.microsoft.com/download/8/2/7/8273FE8A-8BDB-4736-A9FD-2A2240EC7278/compatibilitypacksp3-kb2526297-fullfile-hu-hu.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-hun.exe
+:: URL|ITA|http://download.microsoft.com/download/F/1/F/F1FCF17F-0859-4F5C-A9AA-BD76D557F378/compatibilitypacksp3-kb2526297-fullfile-it-it.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-ita.exe
+:: URL|JPN|http://download.microsoft.com/download/A/1/8/A1810723-FF82-434D-8DED-F1F4D77D11C4/compatibilitypacksp3-kb2526297-fullfile-ja-jp.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-jpn.exe
+:: URL|KOR|http://download.microsoft.com/download/2/1/3/213475BF-9404-4FC7-B472-5BE1B1F5879C/compatibilitypacksp3-kb2526297-fullfile-ko-kr.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-kor.exe
+:: URL|NLD|http://download.microsoft.com/download/C/D/6/CD69F376-80AF-4A45-91B3-7EB4C0156319/compatibilitypacksp3-kb2526297-fullfile-nl-nl.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-nld.exe
+:: URL|PLK|http://download.microsoft.com/download/0/7/A/07A57466-EEB0-49CA-9A41-7B5C8EB5E7A1/compatibilitypacksp3-kb2526297-fullfile-pl-pl.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-plk.exe
+:: URL|PTB|http://download.microsoft.com/download/B/4/C/B4CDF29F-42BA-4B63-B9F6-775C1F0D4785/compatibilitypacksp3-kb2526297-fullfile-pt-br.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-ptb.exe
+:: URL|PTG|http://download.microsoft.com/download/A/0/0/A0007A12-0B43-49F2-B6CE-CC769790C5EA/compatibilitypacksp3-kb2526297-fullfile-pt-pt.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-ptg.exe
+:: URL|RUS|http://download.microsoft.com/download/C/D/D/CDD7A4D8-AF93-473E-AB82-322AC1F4C98D/compatibilitypacksp3-kb2526297-fullfile-ru-ru.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-rus.exe
+:: URL|SVE|http://download.microsoft.com/download/8/4/8/848EDD06-4CEE-438E-8E32-9908F0315142/compatibilitypacksp3-kb2526297-fullfile-sv-se.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-sve.exe
+:: URL|TRK|http://download.microsoft.com/download/F/D/A/FDAFA0C5-A54D-451D-8090-978E9E76CCF7/compatibilitypacksp3-kb2526297-fullfile-tr-tr.exe|updates/common/compatibilitypacksp3-kb2526297-fullfile-trk.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\compatibilitypacksp3-kb2526297-fullfile-%WINLANG%.exe /quiet"
 
 :: Microsoft Office Compatibility Pack for Word, Excel, and PowerPoint 2007 File Formats
 :: How to use earlier versions of Excel, PowerPoint, and Word to open and save files from 2007 Office programs
@@ -11,7 +34,6 @@
 :: URL|ENU|http://download.microsoft.com/download/9/2/2/9222d67f-7630-4f49-bd26-476b51517fc1/FileFormatConverters.exe|updates/common/enu/fileformatconverters.exe
 :: URL|ESN|http://download.microsoft.com/download/b/7/2/b7277fac-80e8-4046-ac6c-34d998e2e5c8/FileFormatConverters.exe|updates/common/esn/fileformatconverters.exe
 :: URL|FIN|http://download.microsoft.com/download/4/b/2/4b22b252-bc02-484e-8bfc-7ec376e3d425/FileFormatConverters.exe|updates/common/fin/fileformatconverters.exe
-:: No Download found for FRA.
 :: URL|HEB|http://download.microsoft.com/download/0/d/4/0d40bae8-2d3a-48e0-8fa6-075c16960364/FileFormatConverters.exe|updates/common/heb/fileformatconverters.exe
 :: URL|HUN|http://download.microsoft.com/download/d/1/4/d14016de-a788-4bf8-a533-4af48b2e9b3c/FileFormatConverters.exe|updates/common/hun/fileformatconverters.exe
 :: URL|ITA|http://download.microsoft.com/download/0/d/8/0d84a525-261d-476c-a6bd-ab7595897f32/FileFormatConverters.exe|updates/common/ita/fileformatconverters.exe
@@ -26,3 +48,4 @@
 :: URL|SVE|http://download.microsoft.com/download/0/d/4/0d40b572-44b3-43f0-bf14-4ad3a51c63dd/FileFormatConverters.exe|updates/common/sve/fileformatconverters.exe
 :: URL|TRK|http://download.microsoft.com/download/8/6/c/86c94b8e-294d-4d5d-867f-8e5c1409b964/FileFormatConverters.exe|updates/common/trk/fileformatconverters.exe
 todo.pl ".reboot-on 194 %Z%\updates\common\%WINLANG%\FileFormatConverters.exe /quiet"
+
