6f1ea123b87cb1764520007e575056768d77c330
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-10-09 19:37:07 +0000

    Update Firefox to version 16.0

--- trunk/install/scripts/firefox.bat
+++ trunk/install/scripts/firefox.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox
 
-:: Firefox 15.0
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/ar/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/ca/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/cs/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/zh-TW/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/zh-CN/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/da/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/de/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/el/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/en-US/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/en-GB/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/es-ES/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/es-AR/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/fi/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/fr/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/he/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/hu/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/it/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/ja/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/ko/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/mk/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/nl/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/nb-NO/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/pl/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/pt-BR/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/pt-PT/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/ro/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/ru/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/sl/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/sv-SE/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/15.0/win32/tr/Firefox%20Setup%2015.0.exe|packages/mozilla/firefox-150-trk.exe
+:: Firefox 16.0
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/ar/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/ca/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/cs/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/zh-TW/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/zh-CN/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/da/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/de/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/el/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/en-US/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/en-GB/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/es-ES/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/es-AR/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/fi/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/fr/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/he/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/hu/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/it/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/ja/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/ko/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/mk/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/nl/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/nb-NO/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/pl/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/pt-BR/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/pt-PT/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/ro/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/ru/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/sl/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/sv-SE/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/16.0/win32/tr/Firefox%20Setup%2016.0.exe|packages/mozilla/firefox-160-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-150-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-160-%WINLANG%.exe -ms -ira"
