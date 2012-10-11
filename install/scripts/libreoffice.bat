65bd602a0292dd586c34b1c7518152249c2dc962
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-10-11 20:42:06 +0000

    update to version 3.6.2

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.6.1/win/x86/LibO_3.6.1_Win_x86_install_multi.msi|packages/libreoffice/libo_3.6.1_win_x86_install_multi.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.6.2/win/x86/LibO_3.6.2_Win_x86_install_multi.msi|packages/libreoffice/libo_3.6.2_win_x86_install_multi.msi
 
 @echo off
 
-todo.pl "msiexec /qb /i %Z%\packages\libreoffice\libo_3.6.1_win_x86_install_multi.msi"
+todo.pl "msiexec /qb /i %Z%\packages\libreoffice\libo_3.6.2_win_x86_install_multi.msi"
