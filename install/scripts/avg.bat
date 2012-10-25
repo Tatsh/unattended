32c6423d1cc8083fc6150618914d530b4b62c3a9
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-10-25 18:42:28 +0000

    update program and comment out update (it fails on my systems)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3511 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/avg.bat
+++ install/scripts/avg.bat
@@ -1,21 +1,21 @@
 :: OPTIONAL: AVG Anti-Virus Free Edition
 :: HOME:http://free.avg.com/es-es/descargar.prd-afh#tba1
-:: URL|ALL|http://download.avgfree.com/softw/13free/update/u13iavi5751yh.bin|packages/avg/updates/u13iavi5751yh.bin
-:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x86_all_2013_2667a5738.exe|packages/avg/avg_free_x86_all_2013_2667a5738.exe
-:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x64_all_2013_2667a5738.exe|packages/avg/avg_free_AMD64_all_2013_2667a5738.exe
+:: URL|ALL|http://download.avgfree.com/softw/13free/update/u13iavi5851ke.bin|packages/avg/updates/u13iavi5851ke.bin
+:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x86_all_2013_2741a5824.exe|packages/avg/avg_free_x86_all_2013_2741a5824.exe
+:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x64_all_2013_2741a5824.exe|packages/avg/avg_free_AMD64_all_2013_2741a5824.exe
 
 @echo off
 
 :: AVG use a icon on the tray so this link is unecesary.
 todo.pl "unlink.pl special:AllUsersDesktop\"\AVG 2013.lnk\""
 
-::  Update program 
+::  Update program (Does not seem to work)
 ::todo.pl "\"%PROGRAMFILES%\AVG\AVG2012\avgmfapx.exe\" /AppMode=UPDATE"
 ::  Install virus definitions from local network first.
-todo.pl "\"%PROGRAMFILES%\AVG\AVG2012\avgmfapx.exe\" /AppMode=UPDATE /source=folder /path=\"%Z%\packages\avg\updates\""
+::todo.pl "\"%PROGRAMFILES%\AVG\AVG2012\avgmfapx.exe\" /AppMode=UPDATE /source=folder /path=\"%Z%\packages\avg\updates\""
 
 ::  Before update lets make sure that avg is active and ok.
 todo.pl ".reboot"
 
 ::  HttpScanner,SafeSearch and SafeSurf are plugins for the Browser. Keep them out. 
-todo.pl "%Z%\packages\avg\avg_free_%PROCESSOR_ARCHITECTURE%_all_2013_2667a5738.exe /UILevel=Silent /REMOVE_FEATURE=fea_AVG_SafeSurf,fea_AVG_SafeSearch,fea_AVG_HttpScanner /InstallToolbar=0 /InstallSidebar=0 /DontRestart"
+todo.pl "%Z%\packages\avg\avg_free_%PROCESSOR_ARCHITECTURE%_all_2013_2741a5824.exe /UILevel=Silent /REMOVE_FEATURE=fea_AVG_SafeSurf,fea_AVG_SafeSearch,fea_AVG_HttpScanner /InstallToolbar=0 /InstallSidebar=0 /DontRestart"
