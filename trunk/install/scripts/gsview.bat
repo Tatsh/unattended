18f2b0ced73d9f486cf49d822e3a7a75837b39a9
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-10-25 18:40:04 +0000

    fix url gsv

--- trunk/install/scripts/gsview.bat
+++ trunk/install/scripts/gsview.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install Ghostscript and GSview
 :: HOME: http://pages.cs.wisc.edu/~ghost/
 :: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.06/gs906w32.exe|packages/ghostscript/gs906w32.exe
-:: URL|ALL|http://www.mirrorservice.org/sites/mirror.cs.wisc.edu/pub/mirrors/ghost/ghostgum/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
+:: URL|ALL|http://pages.cs.wisc.edu/~ghost/gsview/download/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
 :: Then manually extract to gs and gv folders in the same path
 
 todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
