36dd2ea722ac834f59032e8f2b34eac028145569
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-12-16 23:01:03 +0000

    update to version 3.6.4

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.6.3/win/x86/LibO_3.6.3_Win_x86_install_multi.msi|packages/libreoffice/libo_3.6.3_win_x86_install_multi.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.6.4/win/x86/LibO_3.6.4_Win_x86_install_multi.msi|packages/libreoffice/libo_3.6.4_win_x86_install_multi.msi
 
 @echo off
 
-todo.pl "msiexec /qb /i %Z%\packages\libreoffice\libo_3.6.3_win_x86_install_multi.msi"
+todo.pl "msiexec /qb /i %Z%\packages\libreoffice\libo_3.6.4_win_x86_install_multi.msi"
