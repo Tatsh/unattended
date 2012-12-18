756587cc3c217f079ea46b2ce30e5b79c7fe7fb8
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-12-18 02:39:02 +0000

    update to 2.0.5
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3526 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://downloads.sourceforge.net/vlc/2.0.4/win32/vlc-2.0.4-win32.exe|packages/vlc/vlc-2.0.4-win32.exe
+:: URL|All|http://downloads.sourceforge.net/vlc/2.0.5/win32/vlc-2.0.5-win32.exe|packages/vlc/vlc-2.0.5-win32.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.0.4-win32.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-2.0.5-win32.exe /S"
