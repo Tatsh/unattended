f04a1dbda924b8c3118e811234174bf007643411
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-12-24 10:37:43 +0000

    update links

--- install/scripts/driverpacks.bat
+++ install/scripts/driverpacks.bat
@@ -1,31 +1,32 @@
 :: OPTIONAL: Download Bâshrat the Sneaky drivers from [WWW] DriverPacks.net -- Please Donate!!!
 :: HOME: http://driverpacks.net/driverpacks/windows/xp/x86
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/chipset/12.03/download/torrent|drivers/DP_Chipset_wnt5_x86-32_1203.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/chipset/12.09/download/torrent|drivers/DP_Chipset_wnt5_x86-32_1209.7z
 :: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/cpu/10.05/download/torrent|drivers/DP_CPU_wnt5_x86-32_1005.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-languages/11.07/download/torrent|drivers/DP_Graphics_Languages_wnt5_x86-32_1107.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-a/11.07/download/torrent|drivers/DP_Graphics_A_wnt5_x86-32_1107.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-b/11.07/download/torrent|drivers/DP_Graphics_B_wnt5_x86-32_1107.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-c/11.07/download/torrent|drivers/DP_Graphics_C_wnt5_x86-32_1107.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/lan/12.02/download/torrent|drivers/DP_LAN_wnt5_x86-32_1202.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/mass-storage/12.03/download/torrent|drivers/DP_MassStorage_wnt5_x86-32_1203.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-languages/12.06/download/torrent|drivers/DP_Graphics_Languages_wnt5_x86-32_1206.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-a/12.06/download/torrent|drivers/DP_Graphics_A_wnt5_x86-32_1206.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-b/12.06/download/torrent|drivers/DP_Graphics_B_wnt5_x86-32_1206.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-c/12.06/download/torrent|drivers/DP_Graphics_C_wnt5_x86-32_1206.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/lan/12.05/download/torrent|drivers/DP_LAN_wnt5_x86-32_1205.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/mass-storage/12.09/download/torrent|drivers/DP_MassStorage_wnt5_x86-32_1209.7z
 :: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/wlan/12.02/download/torrent|drivers/DP_WLAN_wnt5_x86-32_1202.7z
 :: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/sound-a/11.11/download/torrent|drivers/DP_Sound_A_wnt5_x86-32_1111.7z
 :: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/sound-b/11.11/download/torrent|drivers/DP_Sound_B_wnt5_x86-32_1111.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/miscellaneous/12.01/download/torrent|drivers/DP_Misc_wnt5_x86-32_1201.7z
+
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/bluetooth/9.10/download/torrent|drivers/DP_Bluetooth_wnt5_x86-32_910.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/graphics-physx/12.06/download/torrent|drivers/DP_graphics-physx_wnt5_x86-32_1206.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/hid/12.09/download/torrent|drivers/DP_HID_wnt5_x86-32_1209.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/modem/12.05/download/torrent|drivers/DP_Modem_wnt5_x86-32_1205.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/tvcard/10.05/download/torrent|drivers/DP_TVCARD_wnt5_x86-32_1005.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/webcam/11.07/download/torrent|drivers/DP_webcam_wnt5_x86-32_1107.7z
 
 
-:: 3RD Party February-2010
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Bluetooth_wnt5_x86-32_910.7z|drivers/DP_Bluetooth_wnt5_x86-32_910.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Broadband_wnt5_x86-32_70416.7z|drivers/DP_Broadband_wnt5_x86-32_70416.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/hid/12.03/download/torrent|drivers/DP_HID_wnt5_x86-32_1203.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Misc_wnt5_x86-32_90909.7z|drivers/DP_Misc_wnt5_x86-32_90909.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/modem/11.01/download/torrent|drivers/DP_Modem_wnt5_x86-32_1101.7z
 :: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/monitors/10.05/download/torrent|drivers/DP_Monitor_wnt5_x86-32_1005.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Phone_wnt5_x86-32_100214.7z|drivers/DP_Phone_wnt5_x86-32_100214.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_PrintersCanonInkjet_wnt5_x86-32_61105.7z|drivers/DP_PrintersCanonInkjet_wnt5_x86-32_61105.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_PrintersCanonLaserjet_wnt5_x86-32_61020.7z|drivers/DP_PrintersCanonLaserjet_wnt5_x86-32_61020.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_PrintersHP_wnt5_x86-32_71126.7z|drivers/DP_PrintersHP_wnt5_x86-32_71126.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Scanner_wnt5_x86-32_60831.7z|drivers/DP_Scanner_wnt5_x86-32_60831.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_TV_wnt5_x86-32_90915.7z|drivers/DP_TV_wnt5_x86-32_90915.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_USB_wnt5_x86-32_80911.7z|drivers/DP_USB_wnt5_x86-32_80911.7z
 :: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Virtual_wnt5_x86-32_90225.7z|drivers/DP_Virtual_wnt5_x86-32_90225.7z
-:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_WebCam_wnt5_x86-32_100104.7z|drivers/DP_WebCam_wnt5_x86-32_100104.7z
+:: URL|All|http://3rdpartydriverpacks.thesneaky.com/wnt5_x86-32/DP_Broadband_wnt5_x86-32_70416.7z|drivers/DP_Broadband_wnt5_x86-32_70416.7z
