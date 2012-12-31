91138aeb26116e7f6e250cbb35a2220327b4b12b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2012-12-31 19:30:42 +0000

    Missing %

--- install/scripts/cdbxp.bat
+++ install/scripts/cdbxp.bat
@@ -4,7 +4,7 @@
 :: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.0.3717.msi|packages/cdbxp/cdbxp_setup_4.5.0.3717-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.0.3717-PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.0.3717-%PROCESSOR_ARCHITECTURE%.msi" 
 
 :: Requiered Dot net stuff first
 todo.pl dotnet.bat 
