39a5d22d512335e6bd9d8b4deeda715da59432ab
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-01-08 20:03:35 +0000

    update to version 4.16

--- trunk/install/scripts/win_malicious-software-removal-tool.bat
+++ trunk/install/scripts/win_malicious-software-removal-tool.bat
@@ -3,6 +3,6 @@
 
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/windows-kb890830-v4.15.exe|updates/common/windows-kb890830-v4.15.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V4.15.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/windows-kb890830-v4.16.exe|updates/common/windows-kb890830-v4.16.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\Windows-KB890830-V4.16.exe /Q"
 
