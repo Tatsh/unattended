3531d48fdc26a0db2cfdf69abb7c68226bd3eaf2
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-01-14 06:25:30 +0000

    java 1.7 update 11

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -4,7 +4,7 @@
 :: <http://www.java.com/en/download/manual.jsp>
 :: http://www.java.com/en/download/manual_v6.jsp
 :: <http://java.sun.com/javase/6/webnotes/install/jre/silent.html#running>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=71835|packages/jre/jre-7u10-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=73141|packages/jre/jre-7u11-windows-i586.exe
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=71310|packages/jre/jre-6u38-windows-i586.exe
 
 @Echo off
