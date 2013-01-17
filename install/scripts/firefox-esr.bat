e4f7656e5ad133e5df1c26b8362195c856d83aa6
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-01-17 22:14:39 +0000

    update to version 10.0.12
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3549 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
 
-:: Firefox 10.0.11esr
-::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/ar/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-ara.exe
-::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/ca/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-cat.exe
-::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/cs/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-csy.exe
-::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/zh-TW/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-cht.exe
-::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/zh-CN/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-chs.exe
-::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/da/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-dan.exe
-::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/de/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-deu.exe
-::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/el/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-ell.exe
-::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/en-US/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-enu.exe
-::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/en-GB/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-eng.exe
-::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/es-ES/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-esn.exe
-::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/es-AR/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-ess.exe
-::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/fi/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-fin.exe
-::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/fr/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-fra.exe
-::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/he/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-heb.exe
-::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/hu/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-hun.exe
-::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/it/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-ita.exe
-::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/ja/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-jpn.exe
-::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/ko/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-kor.exe
-::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/mk/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-mki.exe
-::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/nl/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-nld.exe
-::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/nb-NO/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-nor.exe
-::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/pl/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-plk.exe
-::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/pt-BR/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-ptb.exe
-::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/pt-PT/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-ptg.exe
-::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/ro/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-rom.exe
-::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/ru/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-rus.exe
-::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/sl/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-slv.exe
-::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/sv-SE/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-sve.exe
-::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.11esr/win32/tr/Firefox%20Setup%2010.0.11esr.exe|packages/mozilla/firefox-10011esr-trk.exe
+:: Firefox 10.0.12esr
+::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/ar/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-ara.exe
+::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/ca/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-cat.exe
+::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/cs/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-csy.exe
+::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/zh-TW/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-cht.exe
+::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/zh-CN/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-chs.exe
+::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/da/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-dan.exe
+::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/de/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-deu.exe
+::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/el/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-ell.exe
+::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/en-US/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-enu.exe
+::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/en-GB/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-eng.exe
+::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/es-ES/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-esn.exe
+::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/es-AR/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-ess.exe
+::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/fi/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-fin.exe
+::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/fr/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-fra.exe
+::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/he/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-heb.exe
+::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/hu/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-hun.exe
+::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/it/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-ita.exe
+::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/ja/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-jpn.exe
+::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/ko/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-kor.exe
+::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/mk/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-mki.exe
+::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/nl/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-nld.exe
+::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/nb-NO/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-nor.exe
+::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/pl/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-plk.exe
+::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/pt-BR/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-ptb.exe
+::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/pt-PT/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-ptg.exe
+::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/ro/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-rom.exe
+::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/ru/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-rus.exe
+::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/sl/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-slv.exe
+::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/sv-SE/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-sve.exe
+::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/10.0.12esr/win32/tr/Firefox%20Setup%2010.0.12esr.exe|packages/mozilla/firefox-10012esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-10.0.11esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-10012esr-%WINLANG%.exe -ms -ira"
