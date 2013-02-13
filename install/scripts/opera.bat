4e404e13a791e69adbb1b2f10483a69329bd98c8
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-02-13 14:57:41 +0000

    Updates: Flash Player 11.6.602.168 LibreOffice 4.0.0 Opera 12.14 Shockwave Player 12.0.0.112

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/1213/int/Opera_1213_int_Setup.exe|packages/opera/opera_1213_int_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/1214/int/Opera_1214_int_Setup.exe|packages/opera/opera_1214_int_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_1213_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
+todo.pl "%Z%\packages\opera\opera_1214_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
--- install/scripts/shockwave.bat
+++ install/scripts/shockwave.bat
@@ -1,7 +1,7 @@
 :: Install Shockwave player
 ::
-::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_11.6.8.638.msi
+::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_12.0.0.112.msi
 
 @Echo off
 
-todo.pl ".ignore-err 194 %Z%\packages\shockwave\shockwave_installer_full_11.6.8.638.msi /quiet /qb /norestart"
+todo.pl ".ignore-err 194 %Z%\packages\shockwave\shockwave_installer_full_12.0.0.112.msi /quiet /qb /norestart"
