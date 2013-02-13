d9e05f627981a01e42a5b73d52a7d34ca13123db
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-02-13 14:57:41 +0000

    Updates: Flash Player 11.6.602.168 LibreOffice 4.0.0 Opera 12.14 Shockwave Player 12.0.0.112
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3562 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/3.6.5/win/x86/LibO_3.6.5_Win_x86_install_multi.msi|packages/libreoffice/libo_3.6.5_win_x86_install_multi.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.0.0/win/x86/LibreOffice_4.0.0_Win_x86.msi|packages/libreoffice/libreoffice_4.0.0_win_x86_install.msi
 
 @echo off
 
-todo.pl "msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libo_3.6.5_win_x86_install_multi.msi"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.0.0_win_x86_install.msi /norestart /passive"
