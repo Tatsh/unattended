dbf95296d8be8fbe54bbb3179347943796775da8
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-02-17 23:51:03 +0000

    update vesion 9.07

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -1,11 +1,11 @@
 :: OPTIONAL: Install Ghostscript and GSview
 :: HOME: http://pages.cs.wisc.edu/~ghost/
-:: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.06/gs906w32.exe|packages/ghostscript/gs906w32.exe
+:: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.07/gs907w32.exe|packages/ghostscript/gs907w32.exe
 :: URL|ALL|http://pages.cs.wisc.edu/~ghost/gsview/download/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
 :: Then manually extract to gs and gv folders in the same path
 
 todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv50w32.exe"
 
-todo.pl "%Z%\packages\ghostscript\gs906w32.exe /S"
+todo.pl "%Z%\packages\ghostscript\gs907w32.exe /S"
 
