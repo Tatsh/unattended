10b842a4f1d4198280a01306272807aedcb6c3a0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-02-20 19:00:33 +0000

    last java6 update 41

--- trunk/install/scripts/java.bat
+++ trunk/install/scripts/java.bat
@@ -4,13 +4,13 @@
 :: <http://www.java.com/en/download/manual.jsp>
 :: http://www.java.com/en/download/manual_v6.jsp
 :: <http://java.sun.com/javase/6/webnotes/install/jre/silent.html#running>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=73857|packages/jre/jre-7u13-windows-i586.exe
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=73922|packages/jre/jre-6u39-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=74781|packages/jre/jre-7u15-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=74347|packages/jre/jre-6u41-windows-i586.exe
 
 @Echo off
 todo.pl "reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"SunJavaUpdateSched\""
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableAutoUpdateCheck\" /t REG_DWORD /d 0"
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJavaUpdate\" /t REG_DWORD /d 0"
 
-todo.pl "%Z%\packages\jre\jre-6u39-windows-i586.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+todo.pl "%Z%\packages\jre\jre-6u41-windows-i586.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
 
