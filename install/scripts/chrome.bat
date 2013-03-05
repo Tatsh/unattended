50c26402345e3605f45d9fdf1548f0d5e74725c3
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-03-05 09:33:31 +0000

    Google Chrome v25.0.1364.97 Flash v11.6.602.171
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3572 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-24.0.1312.57.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-25.0.1364.97.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-24.0.1312.57.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-25.0.1364.97.msi\" /qb REBOOT=ReallySuppress"
