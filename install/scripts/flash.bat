4cb9502590e9d4f590de3de059bcdb71b8054605
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-03-05 09:33:31 +0000

    Google Chrome v25.0.1364.97 Flash v11.6.602.171

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL: Install Flash 11.6.602.168
+:: OPTIONAL: Install Flash 11.6.602.171
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.6.602.168/install_flash_player.exe|packages/flash/install_flash_player_11.6.602.168.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.6.602.168/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.6.602.168.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.6.602.171/install_flash_player.exe|packages/flash/install_flash_player_11.6.602.171.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.6.602.171/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.6.602.171.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_11.6.602.168.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_11.6.602.171.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_11.6.602.168.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_11.6.602.171.exe -install"
