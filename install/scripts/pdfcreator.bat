a60b996bf06a6b6b6f9a30105a0ea3c10642ddce
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-03-09 16:10:08 +0000

    update to version 1.6.2

--- install/scripts/pdfcreator.bat
+++ install/scripts/pdfcreator.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install PDFCreator (PDF Printer)
 :: HOME: http://www.pdfforge.org/
 @echo off
-:: URL|ALL|http://cdn.download.pdfforge.org/pdfcreator/1.6.1/PDFCreator-1_6_1_setup.exe|packages/pdfcreator/pdfcreator-1.6.1.setup.exe
+:: URL|ALL|http://red.download.pdfforge.org/pdfcreator/1.6.2/PDFCreator-1_6_2_setup.exe|packages/pdfcreator/pdfcreator-1.6.2.setup.exe
 
 :: Install language files
 :: URL|CHS|http://www.pdfforge.org/files/translations/1.5.0/chinese_simplified.ini|packages/pdfcreator/lang-chs-1.5.0.ini
@@ -21,4 +21,4 @@ if not exist %Z%\packages\pdfcreator\lang-%WINLANG%-1.5.0.ini goto nolangpack
 :nolangpack
 
 :: Install PDFCreator
-todo.pl "%Z%\packages\pdfcreator\pdfcreator-1.6.1.setup.exe /SILENT /LOADINF="%Z%\packages\pdfcreator\PDFCreator.ini" /NORESTART"
+todo.pl "%Z%\packages\pdfcreator\pdfcreator-1.6.2.setup.exe /SILENT /LOADINF="%Z%\packages\pdfcreator\PDFCreator.ini" /NORESTART"
