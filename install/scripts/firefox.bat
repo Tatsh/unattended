c3ee269849919831b408f6c9532dfa6b4d3735fa
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-03-09 16:08:43 +0000

    update version to 19.0.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3574 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox
 ::HOME: http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/
 
-:: Firefox 19.0
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/ar/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/ca/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/cs/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/zh-TW/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/zh-CN/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/da/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/de/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/el/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/en-US/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/en-GB/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/es-ES/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/es-AR/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/fi/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/fr/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/he/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/hu/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/it/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/ja/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/ko/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/mk/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/nl/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/nb-NO/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/pl/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/pt-BR/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/pt-PT/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/ro/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/ru/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/sl/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/sv-SE/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0/win32/tr/Firefox%20Setup%2019.0.exe|packages/mozilla/firefox-1900-trk.exe
+:: Firefox 19.0.2
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/ar/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/ca/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/cs/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/zh-TW/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/zh-CN/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/da/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/de/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/el/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/en-US/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/en-GB/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/es-ES/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/es-AR/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/fi/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/fr/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/he/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/hu/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/it/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/ja/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/ko/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/mk/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/nl/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/nb-NO/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/pl/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/pt-BR/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/pt-PT/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/ro/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/ru/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/sl/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/sv-SE/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/19.0.2/win32/tr/Firefox%20Setup%2019.0.2.exe|packages/mozilla/firefox-1902-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-1900-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-19.0.2-%WINLANG%.exe -ms -ira"
