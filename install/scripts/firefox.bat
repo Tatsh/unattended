2e48360ab44b1ea783a6f8fb69da163e931caa3e
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-04-16 10:22:59 +0000

    Upgraded Firefox to v20.0.1

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox
 ::HOME: http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/
 
-:: Firefox 20.0
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/ar/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/ca/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/cs/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/zh-TW/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/zh-CN/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/da/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/de/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/el/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/en-US/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/en-GB/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/es-ES/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/es-AR/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/fi/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/fr/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/he/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/hu/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/it/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/ja/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/ko/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/mk/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/nl/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/nb-NO/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/pl/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/pt-BR/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/pt-PT/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/ro/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/ru/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/sl/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/sv-SE/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0/win32/tr/Firefox%20Setup%2020.0.exe|packages/mozilla/firefox-2000-trk.exe
+:: Firefox 20.0.1
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/ar/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/ca/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/cs/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/zh-TW/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/zh-CN/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/da/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/de/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/el/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/en-US/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/en-GB/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/es-ES/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/es-AR/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/fi/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/fr/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/he/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/hu/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/it/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/ja/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/ko/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/mk/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/nl/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/nb-NO/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/pl/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/pt-BR/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/pt-PT/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/ro/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/ru/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/sl/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/sv-SE/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/20.0.1/win32/tr/Firefox%20Setup%2020.0.1.exe|packages/mozilla/firefox-2001-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-2000-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-2001-%WINLANG%.exe -ms -ira"
