a3ff47dd65dca9eb0557fe02b9ccb768db6bb112
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-04-16 10:23:41 +0000

    Upgraded Opera to v12.15

--- trunk/install/scripts/opera.bat
+++ trunk/install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/1214/int/Opera_1214_int_Setup.exe|packages/opera/opera_1214_int_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/1215/int/Opera_1215_int_Setup.exe|packages/opera/opera_1215_int_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_1214_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
+todo.pl "%Z%\packages\opera\opera_1215_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
