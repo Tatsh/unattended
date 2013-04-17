287eb7268d4849d2926b11312001f3729cbea294
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-04-17 08:42:47 +0000

    update to version 1.7 patch 21

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -4,7 +4,7 @@
 :: <http://www.java.com/en/download/manual.jsp>
 :: http://www.java.com/en/download/manual_v6.jsp
 :: <http://java.sun.com/javase/6/webnotes/install/jre/silent.html#running>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=75259|packages/jre/jre-7u17-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76860|packages/jre/jre-7u21-windows-i586.exe
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=75085|packages/jre/jre-6u43-windows-i586.exe
 
 @Echo off
@@ -12,5 +12,5 @@ todo.pl "reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"S
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableAutoUpdateCheck\" /t REG_DWORD /d 0"
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJavaUpdate\" /t REG_DWORD /d 0"
 
-todo.pl "%Z%\packages\jre\jre-6u43-windows-i586.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+todo.pl "%Z%\packages\jre\jre-7u21-windows-i586.exe /s IEXPLORER=1 MOZILLA=1 REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
 
