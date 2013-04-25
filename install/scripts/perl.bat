c0c478c20b7659161d5a5e5f65a96f162181d0a7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-04-25 21:38:55 +0000

    Perl update to version -5.14.4.1405. Thanks to Mary Huey.

--- install/scripts/perl.bat
+++ install/scripts/perl.bat
@@ -29,10 +29,10 @@ if "%WINVER%" == "win2k" goto skipinstaller3
 %Z%\packages\windowsinstaller\windowsinstaller-kb893803-v2-x86.exe /passive /norestart
 :skipinstaller3
 
-:: URL|ALL|http://downloads.activestate.com/ActivePerl/releases/5.14.2.1402/ActivePerl-5.14.2.1402-MSWin32-x86-295342.msi|packages/perl/activeperl-5.14.2.1402-mswin32-x86-295342.msi
-:: URL|ALL|http://downloads.activestate.com/ActivePerl/releases/5.14.2.1402/ActivePerl-5.14.2.1402-MSWin32-x64-295342.msi|packages/perl/activeperl-5.14.2.1402-mswin32-AMD64-295342.msi
+:: URL|ALL|http://downloads.activestate.com/ActivePerl/releases/5.14.4.1405/ActivePerl-5.14.4.1405-MSWin32-x86-296746.msi|packages/perl/activeperl-5.14.4.1405-mswin32-x86-296746.msi
+:: URL|ALL|http://downloads.activestate.com/ActivePerl/releases/5.14.4.1405/ActivePerl-5.14.4.1405-MSWin32-x64-296746.msi|packages/perl/activeperl-5.14.4.1405-mswin32-AMD64-296746.msi
 
-set perl_msi=%Z%\packages\perl\activeperl-5.14.2.1402-mswin32-%PROCESSOR_ARCHITECTURE%-295342.msi
+set perl_msi=%Z%\packages\perl\activeperl-5.14.4.1405-mswin32-%PROCESSOR_ARCHITECTURE%-296746.msi
 
 :retry
 if exist %perl_msi% goto have_perl
