494f8f8b2f18cded98c157b3c23fa21f21206497
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-04-29 08:22:39 +0000

    tightvnc update to version 2.7.1

--- install/scripts/tightvnc.bat
+++ install/scripts/tightvnc.bat
@@ -1,9 +1,9 @@
 :: OPTIONAL: Install TightVNC
 :: HOME: http://www.tightvnc.com/
-:: URL|ALL|http://www.tightvnc.com/download/2.6.4/tightvnc-2.6.4-setup-32bit.msi|packages/tightvnc-2.6.4-setup-x86.msi
-:: URL|ALL|http://www.tightvnc.com/download/2.6.4/tightvnc-2.6.4-setup-64bit.msi|packages/tightvnc-2.6.4-setup-AMD64.msi
+:: URL|ALL|http://www.tightvnc.com/download/2.7.1/tightvnc-2.7.1-setup-32bit.msi|packages/tightvnc-2.7.1-setup-x86.msi
+:: URL|ALL|http://www.tightvnc.com/download/2.7.1/tightvnc-2.7.1-setup-64bit.msi|packages/tightvnc-2.7.1-setup-AMD64.msi
 @Echo off
 
 :: Add rule to the firewall to allow port 5900 
 todo.pl "\"netsh firewall add portopening protocol = TCP port = 5900 name = TightVNC mode = enable\""
-todo.pl "%Z%\packages\tightvnc-2.6.4-setup-%PROCESSOR_ARCHITECTURE%.msi /quiet /qb " 
+todo.pl "%Z%\packages\tightvnc-2.7.1-setup-%PROCESSOR_ARCHITECTURE%.msi /quiet /qb " 
