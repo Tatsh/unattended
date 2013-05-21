764ba7470dafe47a696b969e780e125bb9bef17d
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-05-21 12:43:02 +0000

    Windows Malicious Software Removal Tool v4.20

--- install/scripts/win_malicious-software-removal-tool.bat
+++ install/scripts/win_malicious-software-removal-tool.bat
@@ -3,6 +3,6 @@
 
 :: <http://support.microsoft.com/?kbid=890830>
 :: <http://www.microsoft.com/downloads/details.aspx?familyid=AD724AE0-E72D-4F54-9AB3-75B8EB148356>
-:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V4.19.exe|updates/common/windows-kb890830-v4.19.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-v4.19.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V4.20.exe|updates/common/windows-kb890830-v4.20.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-v4.20.exe /Q"
 
