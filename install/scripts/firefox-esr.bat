a41018fbcee2ab73cef8965228044f83f944c3a7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-05-22 09:43:03 +0000

    update 17.06esr
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3616 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
 
-:: Firefox 17.0.5esr
-::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/ar/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-ara.exe
-::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/ca/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-cat.exe
-::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/cs/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-csy.exe
-::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/zh-TW/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-cht.exe
-::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/zh-CN/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-chs.exe
-::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/da/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-dan.exe
-::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/de/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-deu.exe
-::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/el/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-ell.exe
-::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/en-US/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-enu.exe
-::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/en-GB/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-eng.exe
-::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/es-ES/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-esn.exe
-::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/es-AR/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-ess.exe
-::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/fi/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-fin.exe
-::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/fr/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-fra.exe
-::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/he/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-heb.exe
-::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/hu/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-hun.exe
-::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/it/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-ita.exe
-::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/ja/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-jpn.exe
-::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/ko/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-kor.exe
-::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/mk/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-mki.exe
-::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/nl/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-nld.exe
-::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/nb-NO/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-nor.exe
-::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/pl/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-plk.exe
-::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/pt-BR/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-ptb.exe
-::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/pt-PT/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-ptg.exe
-::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/ro/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-rom.exe
-::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/ru/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-rus.exe
-::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/sl/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-slv.exe
-::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/sv-SE/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-sve.exe
-::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.5esr/win32/tr/Firefox%20Setup%2017.0.5esr.exe|packages/mozilla/firefox-1705esr-trk.exe
+:: Firefox 17.0.6esr
+::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/ar/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-ara.exe
+::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/ca/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-cat.exe
+::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/cs/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-csy.exe
+::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/zh-TW/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-cht.exe
+::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/zh-CN/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-chs.exe
+::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/da/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-dan.exe
+::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/de/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-deu.exe
+::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/el/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-ell.exe
+::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/en-US/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-enu.exe
+::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/en-GB/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-eng.exe
+::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/es-ES/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-esn.exe
+::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/es-AR/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-ess.exe
+::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/fi/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-fin.exe
+::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/fr/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-fra.exe
+::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/he/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-heb.exe
+::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/hu/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-hun.exe
+::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/it/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-ita.exe
+::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/ja/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-jpn.exe
+::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/ko/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-kor.exe
+::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/mk/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-mki.exe
+::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/nl/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-nld.exe
+::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/nb-NO/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-nor.exe
+::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/pl/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-plk.exe
+::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/pt-BR/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-ptb.exe
+::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/pt-PT/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-ptg.exe
+::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/ro/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-rom.exe
+::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/ru/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-rus.exe
+::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/sl/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-slv.exe
+::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/sv-SE/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-sve.exe
+::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.6esr/win32/tr/Firefox%20Setup%2017.0.6esr.exe|packages/mozilla/firefox-1706esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-170.5esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-1706esr-%WINLANG%.exe -ms -ira"
