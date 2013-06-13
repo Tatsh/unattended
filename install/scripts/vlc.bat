3ba8b4b8b19dab8b5b69edc9f2ae7b8d2bb37701
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-06-13 21:54:46 +0000

    vlc: update to 2.0.7
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3625 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,7 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://downloads.sourceforge.net/vlc/2.0.6/win32/vlc-2.0.6-win32.exe|packages/vlc/vlc-2.0.6-win32.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.0.7/win32/vlc-2.0.7-win32.exe|packages/vlc/vlc-2.0.7-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.0.7/win64/vlc-2.0.7-win64.exe|packages/vlc/vlc-2.0.7-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.0.6-win32.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-2.0.7-%PROCESSOR_ARCHITECTURE%.exe /S"
