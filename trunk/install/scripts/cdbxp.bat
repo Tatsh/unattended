19597fdfe565f463fe8bccb0a17c0e55ba67ed4e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-06-14 00:13:24 +0000

    cdburnerxp version 4.5.1

--- trunk/install/scripts/cdbxp.bat
+++ trunk/install/scripts/cdbxp.bat
@@ -1,10 +1,10 @@
 :: OPTIONAL: CDBurnerXP
 :: HOME: http://www.cdburnerxp.se/
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.0.3717.msi|packages/cdbxp/cdbxp_setup_4.5.0.3717-x86.msi
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.0.3717.msi|packages/cdbxp/cdbxp_setup_4.5.0.3717-AMD64.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.1.4003.msi|packages/cdbxp/cdbxp_setup_4.5.1.4003-x86.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.1.4003.msi|packages/cdbxp/cdbxp_setup_4.5.1.4003-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.0.3717-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.1.4003-%PROCESSOR_ARCHITECTURE%.msi" 
 
 :: Requiered Dot net stuff first
 todo.pl dotnet.bat 
