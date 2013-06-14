1c722a9f56c17a98320d438c6bb2e3eefbf7c551
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-06-14 00:17:58 +0000

    AVG: update version 4-June-2013
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3629 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/avg.bat
+++ install/scripts/avg.bat
@@ -1,8 +1,9 @@
-:: OPTIONAL: AVG Anti-Virus Free Edition 29-Mar-2013
+:: OPTIONAL: AVG Anti-Virus Free Edition 4-June-2013
 :: HOME:http://free.avg.com/es-es/descargar.prd-afh#tba1
-:: URL|ALL|http://download.avgfree.com/softw/13free/update/u13iavi6105xx.bin|packages/avg/updates/u13iavi6105xx.bin
-:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x86_all_2013_3272a6212.exe|packages/avg/avg_free_x86_all_2013_3272a6212.exe
-:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x64_all_2013_3272a6212.exe|packages/avg/avg_free_AMD64_all_2013_3272a6212.exe
+http://download.avgfree.com/filedir/inst/avg_free_x86_all_2013_3345a6382.exe
+:: URL|ALL|http://download.avgfree.com/softw/13free/update/u13iavi6389jn.bin|packages/avg/updates/u13iavi6389jn.bin
+:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x86_all_2013_3345a6382.exe|packages/avg/avg_free_x86_all_2013_3345a6382.exe
+:: URL|ALL|http://download.avgfree.com/filedir/inst/avg_free_x64_all_2013_3345a6382.exe|packages/avg/avg_free_AMD64_all_2013_3345a6382.exe
 
 @echo off
 
@@ -26,4 +27,4 @@ todo.pl ".reboot"
 :: EmailPluginsFea:     Email Plugins Feature
 :: fea_Client:          AVG Remote admin
 
-todo.pl "%Z%\packages\avg\avg_free_%PROCESSOR_ARCHITECTURE%_all_2013_3272a6212.exe /UILevel=Silent /InstallToolbar=0 /InstallSidebar=0 /ChangeBrowserSearchProvider=0  /DontRestart /RemoveFeatures=fea_IDP;LinkScnFea;fea_SrchSrf__Search;fea_SrchSrf__Surf;fea_OnlnSc;EmailPluginsFea;fea_Client;"
+todo.pl "%Z%\packages\avg\avg_free_%PROCESSOR_ARCHITECTURE%_all_2013_3345a6382.exe /UILevel=Silent /InstallToolbar=0 /InstallSidebar=0 /ChangeBrowserSearchProvider=0  /DontRestart /RemoveFeatures=fea_IDP;LinkScnFea;fea_SrchSrf__Search;fea_SrchSrf__Surf;fea_OnlnSc;EmailPluginsFea;fea_Client;"
