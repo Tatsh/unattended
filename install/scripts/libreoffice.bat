5472086e5dfaac012bd8d44845fbc38b0aa15e14
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-06-24 21:55:30 +0000

    libreoffice 4.0.4
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3632 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.0.3/win/x86/LibreOffice_4.0.3_Win_x86.msi|packages/libreoffice/libreoffice_4.0.3_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.0.4/win/x86/LibreOffice_4.0.4_Win_x86.msi|packages/libreoffice/libreoffice_4.0.4_win_x86_install.msi
 
 @echo off
 
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.0.3_win_x86_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.0.4_win_x86_install.msi /norestart /passive"
