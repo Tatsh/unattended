6e484bb5ee02c86be2bce736b64efd9af4687dc3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-07-25 17:06:40 +0000

    update some info about adding path var

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -2,10 +2,12 @@
 :: HOME: http://pages.cs.wisc.edu/~ghost/
 :: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.07/gs907w32.exe|packages/ghostscript/gs907w32.exe
 :: URL|ALL|http://pages.cs.wisc.edu/~ghost/gsview/download/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
-:: Then manually extract to gs and gv folders in the same path
 
+:: Then manually extract to gs and gv folders in the same path
 todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv50w32.exe"
 
+:: some apps need ghostscript to be added on the path
+:: set PATH=%PATH%;%ProgramFiles%\gs\gs9.07\bin
 todo.pl "%Z%\packages\ghostscript\gs907w32.exe /S"
 
