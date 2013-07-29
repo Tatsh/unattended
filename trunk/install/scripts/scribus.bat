8eaa182e561c6838989bed47d5475de999add3bb
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-07-29 00:13:53 +0000

    Scribus: update version 1.4.3

--- trunk/install/scripts/scribus.bat
+++ trunk/install/scripts/scribus.bat
@@ -1,13 +1,13 @@
 :: OPTIONAL:  Install Scribus
 :: HOME: http://www.scribus.net/
 
-:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.2-windows.exe|packages/scribus/scribus-1.4.2-windows.exe
+:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.3-windows.exe|packages/scribus/scribus-1.4.3-windows.exe
 @Echo off
 
 
 
 :: todo.pl "shortcut.pl \"%ProgramFiles%\"\Scribus\scribus.exe special:AllUsersDesktop\Scribus"
-todo.pl "%Z%\packages\scribus\scribus-1.4.2-windows.exe /S"
+todo.pl "%Z%\packages\scribus\scribus-1.4.3-windows.exe /S"
 
 :: Install gsview first
 todo.pl gsview.bat
