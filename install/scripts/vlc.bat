1119de3ebfe1bed7aa8ec79b69b355a8aa59057b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-08-20 19:00:41 +0000

    update to version 2.0.8

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.0.7/win32/vlc-2.0.7-win32.exe|packages/vlc/vlc-2.0.7-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.0.7/win64/vlc-2.0.7-win64.exe|packages/vlc/vlc-2.0.7-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.0.8/win32/vlc-2.0.8-win32.exe|packages/vlc/vlc-2.0.8-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.0.8/win64/vlc-2.0.8-win64.exe|packages/vlc/vlc-2.0.8-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.0.7-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-2.0.8-%PROCESSOR_ARCHITECTURE%.exe /S"
