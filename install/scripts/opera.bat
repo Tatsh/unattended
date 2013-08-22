0d530a4b40adca7e848eb0d0f914dc24a20925c8
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-08-22 11:28:52 +0000

    Opera v12.16
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3650 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/1215/int/Opera_1215_int_Setup.exe|packages/opera/opera_1215_int_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/win/1216/int/Opera_1216_int_Setup.exe|packages/opera/opera_1216_int_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_1215_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
+todo.pl "%Z%\packages\opera\opera_1216_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
