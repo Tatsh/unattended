e7cdae70c29df4bbb3c0e0e020bbe47e9fee33fe
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-08-22 11:36:28 +0000

    Shockwave player v12.0.3.133

--- install/scripts/shockwave.bat
+++ install/scripts/shockwave.bat
@@ -1,7 +1,7 @@
 :: Install Shockwave player
 ::
-::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_12.0.2.122.msi
+::URL|ALL|http://www.adobe.com/go/sw_msi_installer|packages/shockwave/shockwave_installer_full_12.0.3.133.msi
 
 @Echo off
 
-todo.pl ".ignore-err 194 %Z%\packages\shockwave\shockwave_installer_full_12.0.2.122.msi /quiet /qb /norestart"
+todo.pl ".ignore-err 194 %Z%\packages\shockwave\shockwave_installer_full_12.0.3.133.msi /quiet /qb /norestart"
