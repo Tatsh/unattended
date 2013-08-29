3b426f527aadc929445ce885f9ec71816f8d5b98
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-08-29 17:02:53 +0000

    update to version 4.1.1

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.1.0/win/x86/LibreOffice_4.1.0_Win_x86.msi|packages/libreoffice/libreoffice_4.1.0_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.1.1/win/x86/LibreOffice_4.1.1_Win_x86.msi|packages/libreoffice/libreoffice_4.1.1_win_x86_install.msi
 
 @echo off
 
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.1.0_win_x86_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.1.1_win_x86_install.msi /norestart /passive"
