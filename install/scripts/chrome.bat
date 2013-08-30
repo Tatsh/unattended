6c70046cccd4fc9721e85cc4166bd2279d19c75e
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-08-30 12:59:28 +0000

    Google Chrome v29.0.1547.57

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-29.0.1547.57.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-29.0.1547.62.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-29.0.1547.57.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-29.0.1547.62.msi\" /qb REBOOT=ReallySuppress"
