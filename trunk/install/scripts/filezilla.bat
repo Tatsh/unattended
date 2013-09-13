f670471184b9a524397d563ee3fbd21534fd0e47
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-13 13:18:50 +0000

    filezilla 3.7.3

--- trunk/install/scripts/filezilla.bat
+++ trunk/install/scripts/filezilla.bat
@@ -1,8 +1,7 @@
 :: OPTIONAL:  Install FileZilla
 :: HOME: http://filezilla-project.org/
-
-:: URL|All|http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.3.4.1/FileZilla_3.3.4.1_win32-setup.exe|packages/filezilla/FileZilla_3.3.4.1_win32-setup.exe
+:: URL|All|http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.7.3/FileZilla_3.7.3_win32-setup.exe|packages/filezilla/FileZilla_3.7.3_win32-setup.exe
 
 
 :: Run the installer
-todo.pl "start /wait %Z%\packages\filezilla\FileZilla_3.3.4.1_win32-setup.exe /S"
+todo.pl "start /wait %Z%\packages\filezilla\FileZilla_3.7.3_win32-setup.exe /S"
