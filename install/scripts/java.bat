cb6c635a361d254702d98df2f4213451b234c5c9
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-13 09:02:02 +0000

    Java 7u40

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -6,7 +6,7 @@
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
 :: Testing early access <https://jdk7.java.net/download.html>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=78825|packages/jre/jre-7u25-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=80812|packages/jre/jre-7u40-windows-i586.exe
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76208|packages/jre/jre-6u45-windows-i586.exe
 
 @Echo off
@@ -14,5 +14,5 @@ todo.pl "reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"S
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableAutoUpdateCheck\" /t REG_DWORD /d 0"
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJavaUpdate\" /t REG_DWORD /d 0"
 
-todo.pl "%Z%\packages\jre\jre-7u25-windows-i586.exe /s /L %SystemDrive%\netinst\logs\java.log REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+todo.pl "%Z%\packages\jre\jre-7u40-windows-i586.exe /s /L %SystemDrive%\netinst\logs\java.log REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
 
