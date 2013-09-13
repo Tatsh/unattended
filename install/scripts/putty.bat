59ca659644703e43fbedd9bb01ad6e952d591bd9
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-13 13:15:31 +0000

    putty: update to 0.63

--- install/scripts/putty.bat
+++ install/scripts/putty.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install Putty 0.62
 :: HOME: http://www.chiark.greenend.org.uk/~sgtatham/putty/
 :: Fix %7e -> ~
-:: URL|ALL|http://the.earth.li/%7esgtatham/putty/latest/x86/putty-0.62-installer.exe|packages/putty/putty-0.62-installer.exe
+:: URL|ALL|http://the.earth.li/%7esgtatham/putty/latest/x86/putty-0.63-installer.exe|packages/putty/putty-0.63-installer.exe
 @Echo off
 
-todo.pl "%Z%\packages\putty\putty-0.62-installer.exe /sp- /silent"
+todo.pl "%Z%\packages\putty\putty-0.63-installer.exe /sp- /silent"
