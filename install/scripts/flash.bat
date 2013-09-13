d3b1a96875b78119432ea26597de8ee6f5bf8fad
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-13 11:15:03 +0000

    Flash v11.8.800.168

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL: Install Flash 11.8.800.94
+:: OPTIONAL: Install Flash 11.8.800.168
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.94/install_flash_player.exe|packages/flash/install_flash_player_11.8.800.94.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.94/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.8.800.94.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.168/install_flash_player.exe|packages/flash/install_flash_player_11.8.800.168.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.168/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.8.800.168.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_11.8.800.94.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_11.8.800.168.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_11.8.800.94.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_11.8.800.168.exe -install"
