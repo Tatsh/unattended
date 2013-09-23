89a2857868f710bb06ee6c2f03e40235f2d24118
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-23 20:52:49 +0000

    firefox esr: 24.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3673 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
 
-:: Firefox 17.0.8esr
-::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/ar/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-ara.exe
-::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/ca/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-cat.exe
-::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/cs/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-csy.exe
-::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/zh-TW/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-cht.exe
-::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/zh-CN/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-chs.exe
-::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/da/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-dan.exe
-::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/de/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-deu.exe
-::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/el/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-ell.exe
-::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/en-US/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-enu.exe
-::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/en-GB/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-eng.exe
-::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/es-ES/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-esn.exe
-::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/es-AR/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-ess.exe
-::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/fi/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-fin.exe
-::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/fr/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-fra.exe
-::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/he/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-heb.exe
-::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/hu/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-hun.exe
-::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/it/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-ita.exe
-::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/ja/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-jpn.exe
-::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/ko/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-kor.exe
-::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/mk/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-mki.exe
-::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/nl/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-nld.exe
-::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/nb-NO/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-nor.exe
-::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/pl/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-plk.exe
-::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/pt-BR/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-ptb.exe
-::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/pt-PT/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-ptg.exe
-::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/ro/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-rom.exe
-::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/ru/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-rus.exe
-::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/sl/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-slv.exe
-::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/sv-SE/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-sve.exe
-::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.8esr/win32/tr/Firefox%20Setup%2017.0.8esr.exe|packages/mozilla/firefox-1708esr-trk.exe
+:: Firefox 24.0esr
+::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/ar/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-ara.exe
+::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/ca/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-cat.exe
+::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/cs/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-csy.exe
+::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/zh-TW/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-cht.exe
+::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/zh-CN/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-chs.exe
+::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/da/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-dan.exe
+::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/de/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-deu.exe
+::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/el/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-ell.exe
+::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/en-US/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-enu.exe
+::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/en-GB/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-eng.exe
+::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/es-ES/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-esn.exe
+::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/es-AR/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-ess.exe
+::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/fi/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-fin.exe
+::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/fr/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-fra.exe
+::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/he/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-heb.exe
+::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/hu/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-hun.exe
+::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/it/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-ita.exe
+::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/ja/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-jpn.exe
+::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/ko/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-kor.exe
+::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/mk/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-mki.exe
+::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/nl/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-nld.exe
+::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/nb-NO/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-nor.exe
+::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/pl/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-plk.exe
+::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/pt-BR/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-ptb.exe
+::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/pt-PT/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-ptg.exe
+::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/ro/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-rom.exe
+::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/ru/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-rus.exe
+::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/sl/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-slv.exe
+::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/sv-SE/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-sve.exe
+::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.0esr/win32/tr/Firefox%20Setup%2024.0esr.exe|packages/mozilla/firefox-240esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-1708esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-240esr-%WINLANG%.exe -ms -ira"
