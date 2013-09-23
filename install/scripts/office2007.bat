93ed19f3dfb5bb2ca08b295caffbe764308067f3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-23 20:51:54 +0000

    office 2007 SP3: only ENU and ESN languages added.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3672 3671eaaa-a6c4-423f-a734-8bc38373c25a

---
+++ install/scripts/office2007.bat
@@ -0,0 +1,11 @@
+
+:: The 2007 Microsoft Office Suite Service Pack 3 (SP3)
+:: Description of the 2007 Microsoft Office suite Service Pack 3
+:: <http://support.microsoft.com/kb/2526086>
+:: <http://www.microsoft.com/en-us/download/details.aspx?id=27838>
+:: URL|ENU|http://download.microsoft.com/download/2/2/A/22AA9422-C45D-46FA-808F-179A1BEBB2A7/office2007sp3-kb2526086-fullfile-en-us.exe|updates/office2007/enu/office2007sp3-kb2526086-fullfile-enu.exe
+:: URL|ESN|http://download.microsoft.com/download/E/1/D/E1D71BF9-2D60-40D2-A473-0B06E63F113D/office2007sp3-kb2526086-fullfile-es-es.exe|updates/office2007/office2007sp3-kb2526086-fullfile-esn.exe
+todo.pl ".reboot-on 194 %Z%\updates\office2007\office2007sp3-kb2526086-fullfile-%WINLANG%.exe /quiet /norestart"
+
+
+todo.pl "%Z%\packages\office2007\setup.exe /config %Z%\site\office2007.xml"
