b44c96d5d53c2fef8c62cd3290f3d3f85c635d0b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-29 12:58:29 +0000

    Flash for explorer update to latest version

--- trunk/install/scripts/flash.bat
+++ trunk/install/scripts/flash.bat
@@ -3,7 +3,7 @@
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
 :: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.168/install_flash_player.exe|packages/flash/install_flash_player_11.8.800.168.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.168/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.8.800.168.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.8.800.175/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.8.800.175.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_11.8.800.168.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_11.8.800.175.exe -install"
 
 :: Version for every other browser
 todo.pl "%Z%\packages\flash\install_flash_player_11.8.800.168.exe -install"
