33b1da5c59fd3cdad0a8fc751a54ff97b7ab1594
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-09-29 15:07:29 +0000

    .NET split script base on the OS Version

---
+++ install/scripts/dotnet-win7sp1.bat
@@ -0,0 +1,5 @@
+:: INSTALL :: Microsoft .NET Package 
+:: MORE INFO: http://msdn.microsoft.com/en-us/library/bb822049.aspx
+:: URL|ALL|http://download.microsoft.com/download/b/a/4/ba4a7e71-2906-4b2d-a0e1-80cf16844f5f/dotnetfx45_full_x86_x64.exe|updates/common/dotnetfx45-full-x86-x64.exe
+
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx45-full-x86-x64.exe /norestart /passive"
