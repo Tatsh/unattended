18cb0a0d79f452017cc730063a9d14a409fb3529
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-10-16 11:32:06 +0000

    Opera v17.0.1241.45

--- trunk/install/scripts/opera.bat
+++ trunk/install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/win/1216/int/Opera_1216_int_Setup.exe|packages/opera/opera_1216_int_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/17.0.1241.45/win/Opera_17.0.1241.45_Setup.exe|packages/opera/opera_17.0.1241.45_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_1216_int_setup.exe /silent /launchopera 0 /desktopshortcut 0 /quicklaunchshortcut 0 /setdefaultbrowser 0 /allusers"
+todo.pl "%Z%\packages\opera\opera_17.0.1241.45_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
