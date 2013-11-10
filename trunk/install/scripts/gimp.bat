630d1fb2bae04dc40e8e2c29cb14788b7eb67737
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-11-10 23:44:07 +0000

    gimp: 2.8.8

--- trunk/install/scripts/gimp.bat
+++ trunk/install/scripts/gimp.bat
@@ -1,10 +1,10 @@
 ::OPTIONAL: Install GIMP (2.8)
 ::HOME: http://gimp-win.sourceforge.net/
-::URL|ALL|http://downloads.sourceforge.net/gimp-win/gimp-2.8.6-setup.exe|packages/gimp/gimp-2.8.6-setup.exe
+::URL|ALL|http://ftp.gimp.org/pub/gimp/v2.8/windows/gimp-2.8.8-setup.exe|packages/gimp/gimp-2.8.8-setup.exe
 @echo off
 
 :: Create a link on the desktop
 todo.pl "shortcut.pl  \"%ProgramFiles%\GIMP 2\bin\gimp-2.8.exe\" special:AllUsersDesktop\Gimp2"
 
 :: Run the installers
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.8.6-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.8.8-setup.exe SP- /SILENT /NORESTART"
