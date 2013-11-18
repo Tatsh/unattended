46f7f2f5d211086bc91f1b88601addf06d2a80f0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-11-18 14:59:43 +0000

    firefox 25.0.1

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,38 +1,37 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
-
-:: Firefox 24.1.0esr
-::URL|ARA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/ar/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-ara.exe
-::URL|CAT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/ca/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-cat.exe
-::URL|CSY|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/cs/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-csy.exe
-::URL|CHT|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/zh-TW/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-cht.exe
-::URL|CHS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/zh-CN/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-chs.exe
-::URL|DAN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/da/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-dan.exe
-::URL|DEU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/de/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-deu.exe
-::URL|ELL|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/el/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-ell.exe
-::URL|ENU|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/en-US/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-enu.exe
-::URL|ENG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/en-GB/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-eng.exe
-::URL|ESN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/es-ES/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-esn.exe
-::URL|ESS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/es-AR/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-ess.exe
-::URL|FIN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/fi/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-fin.exe
-::URL|FRA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/fr/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-fra.exe
-::URL|HEB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/he/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-heb.exe
-::URL|HUN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/hu/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-hun.exe
-::URL|ITA|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/it/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-ita.exe
-::URL|JPN|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/ja/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-jpn.exe
-::URL|KOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/ko/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-kor.exe
-::URL|MKI|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/mk/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-mki.exe
-::URL|NLD|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/nl/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-nld.exe
-::URL|NOR|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/nb-NO/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-nor.exe
-::URL|PLK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/pl/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-plk.exe
-::URL|PTB|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/pt-BR/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-ptb.exe
-::URL|PTG|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/pt-PT/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-ptg.exe
-::URL|ROM|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/ro/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-rom.exe
-::URL|RUS|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/ru/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-rus.exe
-::URL|SLV|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/sl/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-slv.exe
-::URL|SVE|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/sv-SE/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-sve.exe
-::URL|TRK|http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/24.1.0esr/win32/tr/Firefox%20Setup%2024.1.0esr.exe|packages/mozilla/firefox-2410esr-trk.exe
+:: Firefox 24.1.1esr
+::URL|ARA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/ar/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-ara.exe
+::URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/ca/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-cat.exe
+::URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/cs/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-csy.exe
+::URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/zh-TW/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-cht.exe
+::URL|CHS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/zh-CN/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-chs.exe
+::URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/da/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-dan.exe
+::URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/de/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-deu.exe
+::URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/el/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-ell.exe
+::URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/en-US/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-enu.exe
+::URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/en-GB/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-eng.exe
+::URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/es-ES/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-esn.exe
+::URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/es-AR/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-ess.exe
+::URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/fi/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-fin.exe
+::URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/fr/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-fra.exe
+::URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/he/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-heb.exe
+::URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/hu/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-hun.exe
+::URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/it/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-ita.exe
+::URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/ja/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-jpn.exe
+::URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/ko/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-kor.exe
+::URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/mk/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-mki.exe
+::URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/nl/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-nld.exe
+::URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/nb-NO/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-nor.exe
+::URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/pl/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-plk.exe
+::URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/pt-BR/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-ptb.exe
+::URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/pt-PT/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-ptg.exe
+::URL|ROM|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/ro/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-rom.exe
+::URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/ru/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-rus.exe
+::URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/sl/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-slv.exe
+::URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/sv-SE/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-sve.exe
+::URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.1.1esr/win32/tr/Firefox%20Setup%2024.1.1esr.exe|packages/mozilla/firefox-2411esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-2410esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-2411esr-%WINLANG%.exe -ms -ira"
