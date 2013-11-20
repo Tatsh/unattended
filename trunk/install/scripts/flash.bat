5a53c6ce192364ec4da617c8dce91af99e9874b7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-11-20 21:39:41 +0000

    flash: 11.9.900.152

--- trunk/install/scripts/flash.bat
+++ trunk/install/scripts/flash.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL: Install Flash 11.9.900.117
+:: OPTIONAL: Install Flash 11.9.900.152
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.9.900.117/install_flash_player.exe|packages/flash/install_flash_player_11.9.900.117.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.9.900.117/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.9.900.117.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.9.900.152/install_flash_player.exe|packages/flash/install_flash_player_11.9.900.152.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/11.9.900.152/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_11.9.900.152.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_11.9.900.117.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_11.9.900.152.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_11.9.900.117.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_11.9.900.152.exe -install"
