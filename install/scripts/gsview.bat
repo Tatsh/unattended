82fa99840e632a0b5424275f1997c2043392b6b3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2013-12-15 17:12:32 +0000

    gs 9.10

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install Ghostscript and GSview
 :: HOME: http://pages.cs.wisc.edu/~ghost/
-:: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.07/gs907w32.exe|packages/ghostscript/gs907w32.exe
+:: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.07/gs910w32.exe|packages/ghostscript/gs910w32.exe
 :: URL|ALL|http://pages.cs.wisc.edu/~ghost/gsview/download/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
 
 :: Then manually extract to gs and gv folders in the same path
@@ -9,5 +9,5 @@ todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\
 
 :: some apps need ghostscript to be added on the path
 :: set PATH=%PATH%;%ProgramFiles%\gs\gs9.07\bin
-todo.pl "%Z%\packages\ghostscript\gs907w32.exe /S"
+todo.pl "%Z%\packages\ghostscript\gs910w32.exe /S"
 
