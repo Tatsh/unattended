5434d3aebc8b523638ffeba177a55113394085cf
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-01-15 23:44:48 +0000

    iUpdate to vertion 7u51
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3728 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -3,10 +3,9 @@
 :: See:
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.java.com/en/download/manual_v6.jsp>
-:: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
 :: Testing early access <https://jdk7.java.net/download.html>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=81819|packages/jre/jre-7u45-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=83383|packages/jre/jre-7u51-windows-i586.exe
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76208|packages/jre/jre-6u45-windows-i586.exe
 
 @Echo off
@@ -15,5 +14,7 @@ todo.pl "reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"S
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableAutoUpdateCheck\" /t REG_DWORD /d 0"
 todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJavaUpdate\" /t REG_DWORD /d 0"
 
-todo.pl "%Z%\packages\jre\jre-7u45-windows-i586.exe /s /L %SystemDrive%\netinst\logs\jre-7u45.log REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+:: You can use WEB_JAVA_SECURITY_LEVEL=M 
+:: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
+todo.pl "%Z%\packages\jre\jre-7u51-windows-i586.exe /s /L %SystemDrive%\netinst\logs\jre-7u51.log REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
 
