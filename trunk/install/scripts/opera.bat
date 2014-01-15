2042a4bcef0b5c8224a636ef7b6cfe52b8f6c6f3
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-01-15 15:47:58 +0000

    Opera v18.0.1284.68

--- trunk/install/scripts/opera.bat
+++ trunk/install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/desktop/17.0.1241.45/win/Opera_17.0.1241.45_Setup.exe|packages/opera/opera_17.0.1241.45_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/18.0.1284.68/win/Opera_18.0.1284.68_Setup.exe|packages/opera/opera_18.0.1284.68_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_17.0.1241.45_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_18.0.1284.68_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
