11061dfaf0f402c79e8bdcfab8bace0ac40bd0c8
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-02-03 12:30:57 +0000

    LibreOffice v4.2.0

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,7 +1,7 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.1.4/win/x86/LibreOffice_4.1.4_Win_x86.msi|packages/libreoffice/libreoffice_4.1.4_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.2.0/win/x86/LibreOffice_4.2.0_Win_x86.msi|packages/libreoffice/libreoffice_4.2.0_win_x86_install.msi
 
 @echo off
 
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.1.4_win_x86_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.2.0_win_x86_install.msi /norestart /passive"
