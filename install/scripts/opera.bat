fc89af1e765eff4cf9b1f8f697940dddd55e63d2
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-02-03 12:31:27 +0000

    Opera v19.0.1326.56

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/desktop/18.0.1284.68/win/Opera_18.0.1284.68_Setup.exe|packages/opera/opera_18.0.1284.68_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/19.0.1326.56/win/Opera_19.0.1326.56_Setup.exe|packages/opera/opera_19.0.1326.56_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_18.0.1284.68_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_19.0.1326.56_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
