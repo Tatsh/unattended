492f052658618bd43a9b1985a596e2a039ca8f32
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-02-06 10:30:32 +0000

    Flash Player v12.0.0.44

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL: Install Flash 12.0.0.43
+:: OPTIONAL: Install Flash 12.0.0.44
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.43/install_flash_player.exe|packages/flash/install_flash_player_12.0.0.43.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.38/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_12.0.0.38.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.44/install_flash_player.exe|packages/flash/install_flash_player_12.0.0.44.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.44/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_12.0.0.44.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_12.0.0.38.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_12.0.0.44.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_12.0.0.43.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_12.0.0.44.exe -install"
