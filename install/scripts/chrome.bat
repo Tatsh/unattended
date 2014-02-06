e25af7af80667f097ebe32aa6ad8146dcf6af5af
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-02-06 11:40:50 +0000

    Google Chrome v32.0.1700.107

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-32.0.1700.102.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-32.0.1700.107.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-32.0.1700.102.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-32.0.1700.107.msi\" /qb REBOOT=ReallySuppress"
