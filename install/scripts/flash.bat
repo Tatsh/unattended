61cf001b4f20bb3a6bf77526c450e2d9691a18e8
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-03-12 23:55:28 +0000

    Flash: update 12.0.0.77
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3751 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL: Install Flash 12.0.0.70
+:: OPTIONAL: Install Flash 12.0.0.77
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.70/install_flash_player.exe|packages/flash/install_flash_player_12.0.0.70.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.70/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_12.0.0.70.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.77/install_flash_player.exe|packages/flash/install_flash_player_12.0.0.77.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/12.0.0.77/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_12.0.0.77.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_12.0.0.70.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_12.0.0.77.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_12.0.0.70.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_12.0.0.77.exe -install"
