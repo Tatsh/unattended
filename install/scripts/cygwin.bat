849aa55a3f555d403bfe6e4489a27ef4d8c4e4d0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-04-29 08:51:31 +0000

    add URL

--- install/scripts/cygwin.bat
+++ install/scripts/cygwin.bat
@@ -1,5 +1,7 @@
 :: Install Cygwin
 :: HOME: http://www.cygwin.com/
+:: URL|ALL|http://cygwin.com/setup-x86.exe|packages/cygwin/setup-x86.exe
+:: URL|ALL|http://cygwin.com/setup-x86_64.exe|packages/cygwin/setup-AMD64.exe
 
 @Echo off
 :: After installing Cygwin package, configure it.
