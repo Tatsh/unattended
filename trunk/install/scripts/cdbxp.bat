592e8fd6fd7014de067c176954a7dd1dd03afce4
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-05-06 13:31:13 +0000

    CDBurnerXP v4.5.3.4643

--- trunk/install/scripts/cdbxp.bat
+++ trunk/install/scripts/cdbxp.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: CDBurnerXP
 :: HOME: http://www.cdburnerxp.se/
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.3.4643.msi|packages/cdbxp/cdbxp_setup_4.5.3.4643-x86.msi
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.3.4643.msi|packages/cdbxp/cdbxp_setup_4.5.3.4643-AMD64.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.3.4746.msi|packages/cdbxp/cdbxp_setup_4.5.3.4746-x86.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.3.4746.msi|packages/cdbxp/cdbxp_setup_4.5.3.4746-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.3.4643-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.3.4746-%PROCESSOR_ARCHITECTURE%.msi" 
 
