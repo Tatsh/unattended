ae9dbf4e12e9023ba12ca3042a812210f66dcf6d
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-05-06 22:54:15 +0000

    shockwave up 12.1.1.151

--- install/scripts/shockwave.bat
+++ install/scripts/shockwave.bat
@@ -1,7 +1,7 @@
 :: Install Shockwave player
 :: HOME: http://get.adobe.com/es/shockwave/otherversions/
-::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.msi|packages/shockwave/shockwave_installer_full_12.0.9.149.msi
+::URL|ALL|http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.msi|packages/shockwave/shockwave_installer_full_12.1.1.151.msi
 
 @Echo off
 
-todo.pl ".ignore-err 194 %Z%\packages\shockwave\shockwave_installer_full_12.0.9.149.msi /quiet /qb /norestart"
+todo.pl ".ignore-err 194 %Z%\packages\shockwave\shockwave_installer_full_12.1.1.151.msi /quiet /qb /norestart"
