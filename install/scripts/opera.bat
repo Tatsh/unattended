fcb7e030653f97a1f5e420ea55952d5d509fd14b
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-05-06 13:32:59 +0000

    Opera v21.0.1432.57
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3775 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/desktop/19.0.1326.63/win/Opera_19.0.1326.63_Setup.exe|packages/opera/opera_19.0.1326.63_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/21.0.1432.57/win/Opera_21.0.1432.57_Setup.exe|packages/opera/opera_21.0.1432.57_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_19.0.1326.63_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_21.0.1432.57_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
