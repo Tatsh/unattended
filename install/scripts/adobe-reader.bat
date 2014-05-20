dbe7bf89470649b3241927a63874982f7c970dbb
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-05-20 08:54:37 +0000

    adobe Redader up 11.07
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3786 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/adobe-reader.bat
+++ install/scripts/adobe-reader.bat
@@ -32,7 +32,7 @@
 :: Download Extended Language Support Fonts Package
 :: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/11.x/11.0.00/misc/FontPack11000_XtdAlf_Lang.msi|packages/adobereader/misc/FontPack11000_xtdalf_lang.msi
 :: Download the 11.0x patch
-:: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/11.x/11.0.06/misc/AdbeRdrUpd11006.msp|packages/adobereader/adberdrupd11006.msp
+:: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/11.x/11.0.07/misc/AdbeRdrUpd11007.msp|packages/adobereader/adberdrupd11007.msp
 ::if not exist %Z%\packages\adobereader\misc\FontPack11000_xtd_lang.msi goto nolangsupport
 :: Install Extended Language Support Fonts Package
 ::todo.pl "msiexec /qb /i %Z%\packages\adobereader\misc\FontPack11000_xtdalf_lang.msi"
@@ -50,4 +50,4 @@ todo.pl ".ignore-err 1 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion
 todo.pl ".ignore-err 1 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"Adobe Reader Speed Launcher\""
 
 :: This is the actual installer plus patch
-todo.pl ".ignore-err 194 msiexec /qn /i \"%Z%\packages\adobereader\adberdr11000_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\adberdrupd11006.msp\" REBOOT=ReallySuppress"
+todo.pl ".ignore-err 194 msiexec /qn /i \"%Z%\packages\adobereader\adberdr11000_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\adberdrupd11007.msp\" REBOOT=ReallySuppress"
