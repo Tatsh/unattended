47ee0bb446a78462576e9baa36c229727f13ceb7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-05-29 00:08:00 +0000

    Scribus 1.4.4 and x64 version

--- trunk/install/scripts/scribus.bat
+++ trunk/install/scripts/scribus.bat
@@ -1,13 +1,14 @@
 :: OPTIONAL:  Install Scribus
 :: HOME: http://www.scribus.net/
 
-:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.3-windows.exe|packages/scribus/scribus-1.4.3-windows.exe
+:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.4-windows.exe|packages/scribus/scribus-1.4.4-windows-x86.exe
+:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.4-windows-x64.exe|packages/scribus/scribus-1.4.4-windows-AMD64.exe
 @Echo off
 
 
 
 :: todo.pl "shortcut.pl \"%ProgramFiles%\"\Scribus\scribus.exe special:AllUsersDesktop\Scribus"
-todo.pl "%Z%\packages\scribus\scribus-1.4.3-windows.exe /S"
+todo.pl "%Z%\packages\scribus\scribus-1.4.4-windows-%PROCESSOR_ARCHITECTURE%.exe /S"
 
 :: Install gsview first
 todo.pl gsview.bat
