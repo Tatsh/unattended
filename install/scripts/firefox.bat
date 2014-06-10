77edd10f031a08d9a78f102c2832f4e6050a3324
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-06-10 16:11:51 +0000

    firefox up 30.0

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox
 ::HOME: https://www.mozilla.org/en-US/firefox/all/
 
-:: Firefox 29.0.1
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/ar/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/ca/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/cs/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/zh-TW/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/zh-CN/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/da/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/de/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/el/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/en-US/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/en-GB/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/es-ES/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/es-AR/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/fi/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/fr/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/he/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/hu/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/it/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/ja/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/ko/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/mk/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/nl/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/nb-NO/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/pl/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/pt-BR/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/pt-PT/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/ro/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/ru/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/sl/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/sv-SE/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/29.0.1/win32/tr/Firefox%20Setup%2029.0.1.exe|packages/mozilla/firefox-2901-trk.exe
+:: Firefox 30.0
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/ar/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/ca/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/cs/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/zh-TW/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/zh-CN/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/da/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/de/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/el/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/en-US/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/en-GB/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/es-ES/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/es-AR/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/fi/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/fr/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/he/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/hu/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/it/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/ja/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/ko/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/mk/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/nl/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/nb-NO/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/pl/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/pt-BR/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/pt-PT/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/ro/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/ru/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/sl/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/sv-SE/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/30.0/win32/tr/Firefox%20Setup%2030.0.exe|packages/mozilla/firefox-3000-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-2901-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-3000-%WINLANG%.exe -ms -ira"
