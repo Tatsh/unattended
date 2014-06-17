230c1961be3d567b97e88666f60cadc77794f2b5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-06-17 20:49:31 +0000

    vlc revert back to version 2.1.3
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3795 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.1.4/win32/vlc-2.1.4-win32.exe|packages/vlc/vlc-2.1.4-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.1.4/win64/vlc-2.1.4-win64.exe|packages/vlc/vlc-2.1.4-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.1.3/win32/vlc-2.1.3-win32.exe|packages/vlc/vlc-2.1.3-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.1.3/win64/vlc-2.1.3-win64.exe|packages/vlc/vlc-2.1.3-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.1.4-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-2.1.3-%PROCESSOR_ARCHITECTURE%.exe /S"
