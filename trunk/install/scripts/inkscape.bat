a73fe4f33e1c2048b08fdf24e7f8261dd8ae3a59
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-07-16 00:26:11 +0000

    Inkscape up 4.8.5

--- trunk/install/scripts/inkscape.bat
+++ trunk/install/scripts/inkscape.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
-:: URL|All|http://downloads.sourceforge.net/inkscape/inkscape-0.48.4-1-win32.exe|packages/inkscape/inkscape-0.48.4-1.exe
+:: URL|All|http://downloads.sourceforge.net/inkscape/Inkscape-0.48.5-1-win32.exe|packages/inkscape/inkscape-0.48.5-1.exe
 :: URL|All|http://download.openclipart.org/downloads/2.0/openclipart-2.0-win32.exe|packages/inkscape/openclipart-2.0-win32.exe
 @Echo off
 
@@ -10,4 +10,4 @@ todo.pl "%Z%\packages\inkscape\openclipart-2.0-win32.exe /S"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
 
-todo.pl "%Z%\packages\inkscape\inkscape-0.48.4-1.exe /S"
+todo.pl "%Z%\packages\inkscape\inkscape-0.48.5-1.exe /S"
