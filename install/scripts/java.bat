b0624d54dd56a234116983670e0d380881d66826
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-07-17 11:15:08 +0000

    java fix typo
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3803 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -4,7 +4,7 @@
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
 :: Testing early access <https://jdk7.java.net/download.html>
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=86957|packages/jre/jre-8u5-windows-i586.exe
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=90223|packages/jre/jre-7u60-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=94212|packages/jre/jre-7u65-windows-i586.exe
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76208|packages/jre/jre-6u45-windows-i586.exe
 
 @Echo off
@@ -15,4 +15,4 @@ todo.pl "reg add \"HKLM\SOFTWARE\JavaSoft\Java Update\Policy\" /f /v \"EnableJav
 
 :: You can use WEB_JAVA_SECURITY_LEVEL=M 
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
-todo.pl "%Z%\packages\jre\jre-7u60-windows-i586.exe /s /L %SystemDrive%\netinst\logs\jre-7u60.log REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
+todo.pl "%Z%\packages\jre\jre-7u65-windows-i586.exe /s /L %SystemDrive%\netinst\logs\jre-7u65.log REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0"
