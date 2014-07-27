120083cbb269ea35c8e5f63917fba6e23caa99e1
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-07-27 00:01:01 +0000

    thunderbird: up 24.7.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3805 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,34 +1,34 @@
-:: OPTIONAL: Install Thunderbird 24.6.0
+:: OPTIONAL: Install Thunderbird 24.7.0
 :: HOME: http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/?C=M;O=D
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/ca/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/cs/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/da/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/de/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/el/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/en-GB/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/en-US/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/es-ES/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/es-AR/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/fi/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/fr/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/he/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/hu/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/it/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/ja/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/ko/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/nl/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-nld.exe
-:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/nb-NO/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-nor.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/pl/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/pt-BR/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/ru/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/sl/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/sv-SE/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.6.0/win32/tr/Thunderbird%20Setup%2024.6.0.exe|packages/mozilla/thunderbird-2460-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/ca/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/cs/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/da/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/de/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/el/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/en-GB/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/en-US/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/es-ES/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/es-AR/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/fi/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/fr/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/he/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/hu/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/it/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/ja/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/ko/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/nl/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-nld.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/nb-NO/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-nor.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/pl/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/pt-BR/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/ru/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/sl/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/sv-SE/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/24.7.0/win32/tr/Thunderbird%20Setup%2024.7.0.exe|packages/mozilla/thunderbird-2470-trk.exe
 
 @echo off
 
 :: Add Thunderbird as Default Email Client
 todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
-todo.pl "%Z%\packages\mozilla\thunderbird-2460-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-2470-%WINLANG%.exe -ms -ira"
