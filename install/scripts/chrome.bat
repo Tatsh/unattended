8be3be2c23785c526a090f1ae9a13419305f01a5
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-08-11 11:54:43 +0000

    Google Chrome v35.0.1916.153

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-34.0.1847.131.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-35.0.1916.153.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-34.0.1847.131.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-35.0.1916.153.msi\" /qb REBOOT=ReallySuppress"
