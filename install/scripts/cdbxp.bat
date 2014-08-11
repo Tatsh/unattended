094ad95fc2fc4653e02f92f4ae070fcca24a58a0
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-08-11 11:51:29 +0000

    CDBurner XP v4.5.4.4954
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3807 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/cdbxp.bat
+++ install/scripts/cdbxp.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: CDBurnerXP
 :: HOME: http://www.cdburnerxp.se/
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.3.4746.msi|packages/cdbxp/cdbxp_setup_4.5.3.4746-x86.msi
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.3.4746.msi|packages/cdbxp/cdbxp_setup_4.5.3.4746-AMD64.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.4.4954.msi|packages/cdbxp/cdbxp_setup_4.5.4.4954-x86.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.4.4954.msi|packages/cdbxp/cdbxp_setup_4.5.4.4954-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.3.4746-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.4.4954-%PROCESSOR_ARCHITECTURE%.msi" 
 
