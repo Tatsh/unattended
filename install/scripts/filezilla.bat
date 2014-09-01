09a916dc3d74e07cf3d262ea50e1c8a6a2e3f2e1
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-09-01 19:40:43 +0000

    filezilla: 3.9.0.3

--- install/scripts/filezilla.bat
+++ install/scripts/filezilla.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install FileZilla
 :: HOME: http://filezilla-project.org/
-:: URL|All|http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.7.3/FileZilla_3.7.3_win32-setup.exe|packages/filezilla/FileZilla_3.7.3_win32-setup.exe
+:: URL|All|http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.9.0.3/FileZilla_3.9.0.3_win32-setup.exe|packages/filezilla/FileZilla_3.9.0.3_win32-setup.exe
 
 
 :: Run the installer
-todo.pl "start /wait %Z%\packages\filezilla\FileZilla_3.7.3_win32-setup.exe /S"
+todo.pl "start /wait %Z%\packages\filezilla\FileZilla_3.9.0.3_win32-setup.exe /S"
