3f7df77ba0910e09f9c9e247a02c3e59bb21ff22
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-09-30 13:26:19 +0000

    Thunderbird v31.1.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3854 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,34 +1,34 @@
-:: OPTIONAL: Install Thunderbird 31.1.1
+:: OPTIONAL: Install Thunderbird 31.1.2
 :: HOME: http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/?C=M;O=D
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/ca/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/cs/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/da/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/de/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/el/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/en-GB/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/en-US/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/es-ES/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/es-AR/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/fi/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/fr/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/he/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/hu/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/it/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/ja/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/ko/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/nl/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-nld.exe
-:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/nb-NO/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-nor.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/pl/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/pt-BR/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/ru/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/sl/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/sv-SE/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.1/win32/tr/Thunderbird%20Setup%2031.1.1.exe|packages/mozilla/thunderbird-3111-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/ca/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/cs/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/da/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/de/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/el/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/en-GB/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/en-US/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/es-ES/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/es-AR/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/fi/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/fr/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/he/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/hu/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/it/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/ja/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/ko/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/nl/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-nld.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/nb-NO/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-nor.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/pl/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/pt-BR/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/ru/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/sl/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/sv-SE/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/31.1.2/win32/tr/Thunderbird%20Setup%2031.1.2.exe|packages/mozilla/thunderbird-3112-trk.exe
 
 @echo off
 
 :: Add Thunderbird as Default Email Client
 todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
-todo.pl "%Z%\packages\mozilla\thunderbird-3111-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-3112-%WINLANG%.exe -ms -ira"
