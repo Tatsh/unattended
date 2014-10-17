33160bf83811b1314aabde8d8b4b7cb2a3be7eea
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-10-17 13:21:11 +0000

    Google Chrome v38.0.2125.104
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3858 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-37.0.2062.120.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-38.0.2125.104.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-37.0.2062.120.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-38.0.2125.104.msi\" /qb REBOOT=ReallySuppress"
