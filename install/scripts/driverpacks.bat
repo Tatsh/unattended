c2c77094388795a6fe5be38e4fe1e6c4be7d1e29
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-10-21 21:40:57 +0000

    driverpack: use AMD64 for scripting
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3865 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/driverpacks.bat
+++ install/scripts/driverpacks.bat
@@ -21,18 +21,18 @@
 :: URL|All|http://driverpacks.net/driverpacks/windows/xp/x86/webcam/11.07/download/torrent|drivers/DP_webcam_wnt5_x86-32_1107.7z
 
 :: WIN7 64Bits http://driverpacks.net/driverpacks/windows/7/x64
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/amdfilter/11.10/download/torrent|drivers/DP_amdfilter_wnt6_x64_1110.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/audio/11.11/download/torrent|drivers/DP_audio_wnt6_x64_1111.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/cardreaders/11.04.1/download/torrent|drivers/DP_cardreaders_wnt6_x64_11041.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/chipset/12.12/download/torrent|drivers/DP_Chipset_wnt6_x64_1212.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/graphics-a/12.03/download/torrent|drivers/DP_Graphics_A_wnt6_x64_1203.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/graphics-b/12.03/download/torrent|drivers/DP_Graphics_B_wnt6_x64_1203.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/graphics-mobile/12.03/download/torrent|drivers/DP_Graphics_mobile_wnt6_x64_1203.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/lan/12.08/download/torrent|drivers/DP_LAN_wnt6_x64_1208.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/mass-storage/12.12.1/download/torrent|drivers/DP_MassStorage_wnt6_x64_12121.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/miscellaneous/12.02/download/torrent|drivers/DP_Miscellaneous_wnt6_x64_1202.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/webcam/11.03/download/torrent|drivers/DP_webcam_wnt6_x64_1103.7z
-:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/wlan/12.03/download/torrent|drivers/DP_WLAN_wnt6_x64_1203.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/amdfilter/11.10/download/torrent|drivers/DP_amdfilter_wnt6_AMD64_1110.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/audio/11.11/download/torrent|drivers/DP_audio_wnt6_AMD64_1111.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/cardreaders/11.04.1/download/torrent|drivers/DP_cardreaders_wnt6_AMD64_11041.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/chipset/12.12/download/torrent|drivers/DP_Chipset_wnt6_AMD64_1212.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/graphics-a/12.03/download/torrent|drivers/DP_Graphics_A_wnt6_AMD64_1203.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/graphics-b/12.03/download/torrent|drivers/DP_Graphics_B_wnt6_AMD64_1203.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/graphics-mobile/12.03/download/torrent|drivers/DP_Graphics_mobile_wnt6_AMD64_1203.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/lan/12.08/download/torrent|drivers/DP_LAN_wnt6_AMD64_1208.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/mass-storage/12.12.1/download/torrent|drivers/DP_MassStorage_wnt6_AMD64_12121.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/miscellaneous/12.02/download/torrent|drivers/DP_Miscellaneous_wnt6_AMD64_1202.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/webcam/11.03/download/torrent|drivers/DP_webcam_wnt6_AMD64_1103.7z
+:: URL|All|http://driverpacks.net/driverpacks/windows/7/x64/wlan/12.03/download/torrent|drivers/DP_WLAN_wnt6_AMD64_1203.7z
 
 :: WIN7 32Bits http://driverpacks.net/driverpacks/windows/7/x86
 :: URL|All|http://driverpacks.net/driverpacks/windows/7/x86/chipset/12.12/download/torrent|drivers/DP_Chipset_wnt6_x86_1212.7z
