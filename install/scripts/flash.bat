7999b6c68d8ea27cca12c489d4cd6071bfa17816
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-10-21 21:39:42 +0000

    flash: up 15.0.0.189

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -2,8 +2,8 @@
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/15.0.0.152/install_flash_player.exe|packages/flash/install_flash_player_15.0.0.152.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/15.0.0.167/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_15.0.0.167.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/15.0.0.189/install_flash_player.exe|packages/flash/install_flash_player_15.0.0.189.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/15.0.0.189/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_15.0.0.189.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_15.0.0.167.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_15.0.0.189.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_15.0.0.152.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_15.0.0.189.exe -install"
