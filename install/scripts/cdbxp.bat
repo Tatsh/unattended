72732eeb8f9b6a3052e2322bc84ebd5cf59315f0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2014-11-17 09:56:13 +0000

    CDBurnerXP: update
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3871 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/cdbxp.bat
+++ install/scripts/cdbxp.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: CDBurnerXP
 :: HOME: http://www.cdburnerxp.se/
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.4.5067.msi|packages/cdbxp/cdbxp_setup_4.5.4.5067-x86.msi
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.4.5067.msi|packages/cdbxp/cdbxp_setup_4.5.4.5067-AMD64.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.4.5143.msi|packages/cdbxp/cdbxp_setup_4.5.4.5143-x86.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.4.5143.msi|packages/cdbxp/cdbxp_setup_4.5.4.5143-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.4.5067-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.4.5143-%PROCESSOR_ARCHITECTURE%.msi" 
 
