52df7851bef177ff7179234bbdf3ceee2f1abb80
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-19 23:38:19 +0000

    Inkscape Add x64 binary.

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,6 +1,7 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
-:: URL|All|http://inkscape.global.ssl.fastly.net/media/resources/file/Inkscape-0.91-1.exe|packages/inkscape/inkscape-0.91-1.exe
+:: URL|All|http://inkscape.global.ssl.fastly.net/media/resources/file/inkscape-0.91.msi|packages/inkscape/inkscape-0.91-1-x86.msi
+:: URL|All|http://inkscape.global.ssl.fastly.net/media/resources/file/inkscape-0.91-x64.msi|packages/inkscape/inkscape-0.91-1-AMD64.msi
 :: URL|All|http://download.openclipart.org/downloads/2.0/openclipart-2.0-win32.exe|packages/inkscape/openclipart-2.0-win32.exe
 @Echo off
 
@@ -10,4 +11,4 @@ todo.pl "%Z%\packages\inkscape\openclipart-2.0-win32.exe /S"
 
 todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
 
-todo.pl "%Z%\packages\inkscape\inkscape-0.91-1.exe /S"
+todo.pl "%Z%\packages\inkscape\inkscape-0.91-1-%PROCESSOR_ARCHITECTURE%.msi /quiet /qb"
