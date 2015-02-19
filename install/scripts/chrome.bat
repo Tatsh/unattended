598c197b020c34e553788bad185f5be0e633a3c0
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-19 10:11:56 +0000

    Google Chrome v40.0.2214.111

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-39.0.2171.95.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-40.0.2214.111.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-39.0.2171.95.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-40.0.2214.111.msi\" /qb REBOOT=ReallySuppress"
