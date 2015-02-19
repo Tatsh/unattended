d55df0818167e3336c9b08d06967bf56fa5d7459
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-19 10:10:58 +0000

    Opera v27.0.1689.69
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3891 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/desktop/26.0.1656.60/win/Opera_26.0.1656.60_Setup.exe|packages/opera/opera_26.0.1656.60_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/27.0.1689.69/win/Opera_27.0.1689.69_Setup.exe|packages/opera/opera_27.0.1689.69_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_26.0.1656.60_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_27.0.1689.69_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
