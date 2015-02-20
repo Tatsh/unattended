afaaea8b5df12e0992273dfcb88de663a0257b72
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-20 09:31:24 +0000

    libreoffice 4.3.6

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -1,8 +1,8 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.4.0/win/x86/LibreOffice_4.4.0_Win_x86.msi|packages/libreoffice/libreoffice_4.4.0_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.3.5/win/x86/LibreOffice_4.3.5_Win_x86.msi|packages/libreoffice/libreoffice_4.3.5_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.3.6/win/x86/LibreOffice_4.3.6_Win_x86.msi|packages/libreoffice/libreoffice_4.3.6_win_x86_install.msi
 
 @echo off
 
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.3.5_win_x86_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_4.3.6_win_x86_install.msi /norestart /passive"
