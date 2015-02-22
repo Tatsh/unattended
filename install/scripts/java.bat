e686caedf65836f279b7d3995c091a99719b8cf4
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-22 11:44:22 +0000

    Java. Disable 64bit for now.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3898 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -2,8 +2,9 @@
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
 :: Testing early access <https://jdk7.java.net/download.html>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=98426|packages/jre/jre-8u25-windows-i586.exe
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=97807|packages/jre/jre-7u71-windows-i586.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=101406|packages/jre/jre-8u31-windows-x86.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=101408|packages/jre/jre-8u31-windows-amd64.exe
+::URL|ALL|http://download.oracle.com/otn-pub/java/jdk/7u75-b13/jre-7u75-windows-i586.exe|packages/jre/jre-7u75-windows-x86.exe
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76208|packages/jre/jre-6u45-windows-i586.exe
 
 @Echo off
@@ -12,4 +13,6 @@ todo.pl ".ignore-err 67 msiexec /qn /x {4A03706F-666A-4037-7777-5F2748764D10}"
 
 :: You can use WEB_JAVA_SECURITY_LEVEL=M 
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
-todo.pl "%Z%\packages\jre\jre-7u71-windows-i586.exe /s /L %SystemDrive%\netinst\logs\jre-7u71.log REBOOT=ReallySuppress SPONSORS=0"
+:: Some Apps and websites does not work if you are using 64bit
+:: to enable 64 change this jre-xuxx-windows-%PROCESSOR_ARCHITECTURE%.exe
+todo.pl "%Z%\packages\jre\jre-8u31-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u31.log REBOOT=ReallySuppress SPONSORS=0"
