da07a56ec56e8e8b30665d0884bfb989ee698a69
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-22 12:00:24 +0000

    inkscape: Allow 64 build. Openclipart not longer available
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3900 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -2,13 +2,6 @@
 :: HOME: http://www.inkscape.org/
 :: URL|All|http://inkscape.global.ssl.fastly.net/media/resources/file/inkscape-0.91.msi|packages/inkscape/inkscape-0.91-1-x86.msi
 :: URL|All|http://inkscape.global.ssl.fastly.net/media/resources/file/inkscape-0.91-x64.msi|packages/inkscape/inkscape-0.91-1-AMD64.msi
-:: URL|All|http://download.openclipart.org/downloads/2.0/openclipart-2.0-win32.exe|packages/inkscape/openclipart-2.0-win32.exe
 @Echo off
-
-
-::todo.pl "shortcut.pl  \"%ProgramFiles%\Open Clip Art Library\clipart\" special:AllUsersDesktop\OpenClipArt"
-todo.pl "%Z%\packages\inkscape\openclipart-2.0-win32.exe /S"
-
-todo.pl "shortcut.pl \"%ProgramFiles%\"\Inkscape\inkscape.exe special:AllUsersDesktop\Inkscape"
 
 todo.pl "%Z%\packages\inkscape\inkscape-0.91-1-%PROCESSOR_ARCHITECTURE%.msi /quiet /qb"
