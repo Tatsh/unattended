e14cdbe6ae0dba56660f9fbe1758b2045dd04b14
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-28 09:28:07 +0000

    libreoffice 4.4.1

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -1,6 +1,6 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.4.0/win/x86/LibreOffice_4.4.0_Win_x86.msi|packages/libreoffice/libreoffice_4.4.0_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.4.1/win/x86/LibreOffice_4.4.1_Win_x86.msi|packages/libreoffice/libreoffice_4.4.1_win_x86_install.msi
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/4.3.6/win/x86/LibreOffice_4.3.6_Win_x86.msi|packages/libreoffice/libreoffice_4.3.6_win_x86_install.msi
 
 @echo off
