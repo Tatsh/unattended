2155fa8443638a2fc9ea45c0f6b397db15222133
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-02-28 09:26:39 +0000

    vlc 2.2.0

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.1.5/win32/vlc-2.1.5-win32.exe|packages/vlc/vlc-2.1.5-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.1.5/win64/vlc-2.1.5-win64.exe|packages/vlc/vlc-2.1.5-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.0/win32/vlc-2.2.0-win32.exe|packages/vlc/vlc-2.2.0-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.0/win64/vlc-2.2.0-win64.exe|packages/vlc/vlc-2.2.0-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.1.5-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-2.2.0-%PROCESSOR_ARCHITECTURE%.exe /S"
