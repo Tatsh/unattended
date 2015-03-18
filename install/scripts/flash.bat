52854f07df4a1623ee924e96eddfe721f0ea708b
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-03-18 15:50:04 +0000

    Flash Player v17.0.0.134
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3909 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL: Install Flash 16.0.0.305
+:: OPTIONAL: Install Flash 17.0.0.134
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/16.0.0.305/install_flash_player.exe|packages/flash/install_flash_player_16.0.0.305.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/16.0.0.305/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_16.0.0.305.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/17.0.0.134/install_flash_player.exe|packages/flash/install_flash_player_17.0.0.134.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/17.0.0.134/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_17.0.0.134.exe
 ::
 @Echo off
 
@@ -11,7 +11,7 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_16.0.0.305.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_17.0.0.134.exe -install"
 
 :: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_16.0.0.305.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_17.0.0.134.exe -install"
