40a1a9b2ca6f0af221c50676ec30d41c0c2bd9bd
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-05-06 15:09:11 +0000

    CDBurnerXP v4.5.5.5571, also added logging

--- install/scripts/cdbxp.bat
+++ install/scripts/cdbxp.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: CDBurnerXP
 :: HOME: http://www.cdburnerxp.se/
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.4.5143.msi|packages/cdbxp/cdbxp_setup_4.5.4.5143-x86.msi
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.4.5143.msi|packages/cdbxp/cdbxp_setup_4.5.4.5143-AMD64.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.5.5571.msi|packages/cdbxp/cdbxp_setup_4.5.5.5571-x86.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.5.5571.msi|packages/cdbxp/cdbxp_setup_4.5.5.5571-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.4.5143-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\cdbxp.txt /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.5.5571-%PROCESSOR_ARCHITECTURE%.msi" 
 
