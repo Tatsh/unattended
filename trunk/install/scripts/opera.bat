eabd53874ba0efe6272eb0cbbe43b42e9017ec0c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-05-06 15:08:06 +0000

    Opera v29.0.1795.47

--- trunk/install/scripts/opera.bat
+++ trunk/install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/desktop/27.0.1689.69/win/Opera_27.0.1689.69_Setup.exe|packages/opera/opera_27.0.1689.69_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/29.0.1795.47/win/Opera_29.0.1795.47_Setup.exe|packages/opera/opera_29.0.1795.47_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_27.0.1689.69_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_29.0.1795.47_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
