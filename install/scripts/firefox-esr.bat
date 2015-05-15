e076a8e91467407e314f63637e22145c6ed32713
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-05-15 00:29:34 +0000

    firefox 3170esr
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3930 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
-:: Firefox 31.6.0esr
-::URL|ARA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/ar/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-ara.exe
-::URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/ca/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-cat.exe
-::URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/cs/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-csy.exe
-::URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/zh-TW/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-cht.exe
-::URL|CHS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/zh-CN/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-chs.exe
-::URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/da/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-dan.exe
-::URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/de/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-deu.exe
-::URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/el/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-ell.exe
-::URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/en-US/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-enu.exe
-::URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/en-GB/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-eng.exe
-::URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/es-ES/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-esn.exe
-::URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/es-AR/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-ess.exe
-::URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/fi/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-fin.exe
-::URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/fr/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-fra.exe
-::URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/he/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-heb.exe
-::URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/hu/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-hun.exe
-::URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/it/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-ita.exe
-::URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/ja/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-jpn.exe
-::URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/ko/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-kor.exe
-::URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/mk/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-mki.exe
-::URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/nl/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-nld.exe
-::URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/nb-NO/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-nor.exe
-::URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/pl/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-plk.exe
-::URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/pt-BR/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-ptb.exe
-::URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/pt-PT/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-ptg.exe
-::URL|ROM|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/ro/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-rom.exe
-::URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/ru/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-rus.exe
-::URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/sl/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-slv.exe
-::URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/sv-SE/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-sve.exe
-::URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.6.0esr/win32/tr/Firefox%20Setup%2031.6.0esr.exe|packages/mozilla/firefox-3160esr-trk.exe
+:: Firefox 31.7.0esr
+::URL|ARA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/ar/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-ara.exe
+::URL|CAT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/ca/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-cat.exe
+::URL|CSY|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/cs/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-csy.exe
+::URL|CHT|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/zh-TW/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-cht.exe
+::URL|CHS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/zh-CN/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-chs.exe
+::URL|DAN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/da/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-dan.exe
+::URL|DEU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/de/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-deu.exe
+::URL|ELL|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/el/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-ell.exe
+::URL|ENU|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/en-US/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-enu.exe
+::URL|ENG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/en-GB/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-eng.exe
+::URL|ESN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/es-ES/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-esn.exe
+::URL|ESS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/es-AR/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-ess.exe
+::URL|FIN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/fi/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-fin.exe
+::URL|FRA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/fr/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-fra.exe
+::URL|HEB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/he/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-heb.exe
+::URL|HUN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/hu/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-hun.exe
+::URL|ITA|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/it/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-ita.exe
+::URL|JPN|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/ja/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-jpn.exe
+::URL|KOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/ko/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-kor.exe
+::URL|MKI|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/mk/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-mki.exe
+::URL|NLD|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/nl/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-nld.exe
+::URL|NOR|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/nb-NO/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-nor.exe
+::URL|PLK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/pl/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-plk.exe
+::URL|PTB|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/pt-BR/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-ptb.exe
+::URL|PTG|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/pt-PT/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-ptg.exe
+::URL|ROM|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/ro/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-rom.exe
+::URL|RUS|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/ru/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-rus.exe
+::URL|SLV|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/sl/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-slv.exe
+::URL|SVE|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/sv-SE/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-sve.exe
+::URL|TRK|http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/31.7.0esr/win32/tr/Firefox%20Setup%2031.7.0esr.exe|packages/mozilla/firefox-3170esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-3160esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-3170esr-%WINLANG%.exe -ms -ira"
