07eea59a6f03c65231b77bc173a5a741fb9676e4
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-06-20 22:37:51 +0000

    update flash 18

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,9 +1,10 @@
-:: OPTIONAL: Install Flash 17.0.0.188
+:: OPTIONAL: Install Flash 18.0.0.160
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/17.0.0.188/install_flash_player.exe|packages/flash/install_flash_player_17.0.0.188.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/17.0.0.188/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_17.0.0.188.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.160/install_flash_player.exe|packages/flash/install_flash_player_18.0.0.160.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.160/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_18.0.0.160.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.160/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_18.0.0.160.exe
 ::
 @Echo off
 
@@ -11,7 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_17.0.0.188.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_18.0.0.160.exe -install"
 
-:: Version for every other browser
-todo.pl "%Z%\packages\flash\install_flash_player_17.0.0.188.exe -install"
+:: Version for Firefox NAPI
+todo.pl "%Z%\packages\flash\install_flash_player_18.0.0.160.exe -install"
+
+:: Version for Opera and Chromium
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_18.0.0.160.exe -install"
