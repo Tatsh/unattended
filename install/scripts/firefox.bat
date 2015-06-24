b432bec450c22b27ef12fff77705d6f4ed7bf2ff
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-06-24 12:41:53 +0000

    Firefox v38.0.5
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3939 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,38 +1,38 @@
 :: OPTIONAL: Install Firefox
 ::HOME: https://www.mozilla.org/en-US/firefox/all/
 
-:: Firefox 37.0.2
-::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/ar/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-ara.exe
-::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/ca/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-cat.exe
-::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/cs/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-csy.exe
-::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/zh-TW/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-cht.exe
-::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/zh-CN/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-chs.exe
-::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/da/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-dan.exe
-::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/de/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-deu.exe
-::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/el/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-ell.exe
-::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/en-US/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-enu.exe
-::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/en-GB/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-eng.exe
-::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/es-ES/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-esn.exe
-::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/es-AR/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-ess.exe
-::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/fi/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-fin.exe
-::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/fr/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-fra.exe
-::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/he/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-heb.exe
-::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/hu/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-hun.exe
-::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/it/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-ita.exe
-::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/ja/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-jpn.exe
-::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/ko/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-kor.exe
-::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/mk/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-mki.exe
-::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/nl/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-nld.exe
-::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/nb-NO/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-nor.exe
-::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/pl/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-plk.exe
-::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/pt-BR/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-ptb.exe
-::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/pt-PT/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-ptg.exe
-::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/ro/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-rom.exe
-::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/ru/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-rus.exe
-::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/sl/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-slv.exe
-::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/sv-SE/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-sve.exe
-::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/win32/tr/Firefox%20Setup%2037.0.2.exe|packages/mozilla/firefox-3702-trk.exe
+:: Firefox 38.0.5
+::URL|ARA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/ar/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-ara.exe
+::URL|CAT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/ca/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-cat.exe
+::URL|CSY|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/cs/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-csy.exe
+::URL|CHT|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/zh-TW/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-cht.exe
+::URL|CHS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/zh-CN/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-chs.exe
+::URL|DAN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/da/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-dan.exe
+::URL|DEU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/de/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-deu.exe
+::URL|ELL|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/el/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-ell.exe
+::URL|ENU|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/en-US/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-enu.exe
+::URL|ENG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/en-GB/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-eng.exe
+::URL|ESN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/es-ES/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-esn.exe
+::URL|ESS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/es-AR/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-ess.exe
+::URL|FIN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/fi/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-fin.exe
+::URL|FRA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/fr/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-fra.exe
+::URL|HEB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/he/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-heb.exe
+::URL|HUN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/hu/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-hun.exe
+::URL|ITA|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/it/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-ita.exe
+::URL|JPN|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/ja/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-jpn.exe
+::URL|KOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/ko/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-kor.exe
+::URL|MKI|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/mk/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-mki.exe
+::URL|NLD|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/nl/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-nld.exe
+::URL|NOR|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/nb-NO/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-nor.exe
+::URL|PLK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/pl/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-plk.exe
+::URL|PTB|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/pt-BR/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-ptb.exe
+::URL|PTG|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/pt-PT/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-ptg.exe
+::URL|ROM|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/ro/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-rom.exe
+::URL|RUS|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/ru/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-rus.exe
+::URL|SLV|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/sl/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-slv.exe
+::URL|SVE|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/sv-SE/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-sve.exe
+::URL|TRK|http://releases.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/win32/tr/Firefox%20Setup%2038.0.5.exe|packages/mozilla/firefox-3805-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-3702-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-3805-%WINLANG%.exe -ms -ira"
