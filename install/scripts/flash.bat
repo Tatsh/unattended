c9231177ef398c80324abaf5326f12fdd4492734
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-09-01 14:16:39 +0000

    Flash Player v18.0.0.232

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 18.0.0.160
+:: OPTIONAL: Install Flash 18.0.0.232
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.160/install_flash_player.exe|packages/flash/install_flash_player_18.0.0.160.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.160/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_18.0.0.160.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.160/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_18.0.0.160.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.232/install_flash_player.exe|packages/flash/install_flash_player_18.0.0.232.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.232/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_18.0.0.232.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.232/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_18.0.0.232.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_18.0.0.160.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_18.0.0.232.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_18.0.0.160.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_18.0.0.232.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_18.0.0.160.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_18.0.0.232.exe -install"
