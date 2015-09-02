8cc75e1a9cb99bffec7d9769650abcc863716924
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-09-02 14:39:03 +0000

    Java Runtime Environment v8u60
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3947 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -1,10 +1,11 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
-:: Testing early access <https://jdk7.java.net/download.html>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=106246|packages/jre/jre-8u45-windows-x86.exe
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=106248|packages/jre/jre-8u45-windows-amd64.exe
-::URL|ALL|http://download.oracle.com/otn-pub/java/jdk/7u75-b13/jre-7u75-windows-i586.exe|packages/jre/jre-7u75-windows-x86.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=106246|packages/jre/jre-8u60-windows-x86.exe
+::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=106248|packages/jre/jre-8u60-windows-amd64.exe
+
+:: JRE version 7 is no longer available for public download, need to login and "agree" to download it:
+:: <http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html>
 ::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76208|packages/jre/jre-6u45-windows-i586.exe
 
 @Echo off
@@ -15,4 +16,4 @@ todo.pl ".ignore-err 67 msiexec /qn /x {4A03706F-666A-4037-7777-5F2748764D10}"
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
 :: Some Apps and websites does not work if you are using 64bit
 :: to enable 64 change this jre-xuxx-windows-%PROCESSOR_ARCHITECTURE%.exe
-todo.pl "%Z%\packages\jre\jre-8u45-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u40.log REBOOT=ReallySuppress SPONSORS=0"
+todo.pl "%Z%\packages\jre\jre-8u60-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u60.log REBOOT=ReallySuppress SPONSORS=0"
