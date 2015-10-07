959526f04a114ca1cf2ab8074114473b25a15db5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-10-07 09:58:49 +0000

    Flash update 19.0.0.185

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 18.0.0.232
+:: OPTIONAL: Install Flash 19.0.0.185
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.232/install_flash_player.exe|packages/flash/install_flash_player_18.0.0.232.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.232/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_18.0.0.232.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/18.0.0.232/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_18.0.0.232.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.185/install_flash_player.exe|packages/flash/install_flash_player_19.0.0.185.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.185/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_19.0.0.185.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.185/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_19.0.0.185.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_18.0.0.232.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_19.0.0.185.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_18.0.0.232.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_19.0.0.185.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_18.0.0.232.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_19.0.0.185.exe -install"
