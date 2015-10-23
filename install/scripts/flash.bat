5e60bc702b4d8c7fe032cf16f867a470ca1401c0
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-10-23 10:59:26 +0000

    Adobe Flash Player v19.0.0.226
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3969 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 19.0.0.185
+:: OPTIONAL: Install Flash 19.0.0.226
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.185/install_flash_player.exe|packages/flash/install_flash_player_19.0.0.185.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.185/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_19.0.0.185.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.185/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_19.0.0.185.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.226/install_flash_player.exe|packages/flash/install_flash_player_19.0.0.226.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.226/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_19.0.0.226.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/19.0.0.226/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_19.0.0.226.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_19.0.0.185.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_19.0.0.226.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_19.0.0.185.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_19.0.0.226.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_19.0.0.185.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_19.0.0.226.exe -install"
