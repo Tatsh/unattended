4d7c1535e92f4bf3b34248c991aaff4be1f23086
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-11-04 10:09:30 +0000

    Adobe Reader DC v(20)15.009.20077

--- trunk/install/scripts/adobe-reader-dc.bat
+++ trunk/install/scripts/adobe-reader-dc.bat
@@ -2,35 +2,37 @@
 @Echo Off
 
 :: Download Adobe Reader DC full version
-:: URL|ARA|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_ar_AE.msi|packages/adobereader/acrordrDC1500720033_ara.msi
-:: URL|CHS|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_zh_CN.msi|packages/adobereader/acrordrDC1500720033_chs.msi
-:: URL|CHT|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_zh_TW.msi|packages/adobereader/acrordrDC1500720033_cht.msi
-:: URL|CSY|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_cs_CZ.msi|packages/adobereader/acrordrDC1500720033_csy.msi
-:: URL|DAN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_da_DK.msi|packages/adobereader/acrordrDC1500720033_dan.msi
-:: URL|DEU|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_de_DE.msi|packages/adobereader/acrordrDC1500720033_deu.msi
-:: URL|ELL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_el_GR.msi|packages/adobereader/acrordrDC1500720033_ell.msi
-:: URL|ENU|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_en_US.msi|packages/adobereader/acrordrDC1500720033_enu.msi
-:: URL|ESN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_es_ES.msi|packages/adobereader/acrordrDC1500720033_esn.msi
-:: URL|FIN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_fi_FI.msi|packages/adobereader/acrordrDC1500720033_fin.msi
-:: URL|FRA|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_fr_FR.msi|packages/adobereader/acrordrDC1500720033_fra.msi
-:: URL|HEB|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_he_IL.msi|packages/adobereader/acrordrDC1500720033_heb.msi
-:: URL|HUN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_hu_HU.msi|packages/adobereader/acrordrDC1500720033_hun.msi
-:: URL|ITA|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_it_IT.msi|packages/adobereader/acrordrDC1500720033_ita.msi
-:: URL|JPN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_ja_JP.msi|packages/adobereader/acrordrDC1500720033_jpn.msi
-:: URL|KOR|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_ko_KR.msi|packages/adobereader/acrordrDC1500720033_kor.msi
-:: URL|NLD|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_nl_NL.msi|packages/adobereader/acrordrDC1500720033_nld.msi
-:: URL|NOR|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_nb_NO.msi|packages/adobereader/acrordrDC1500720033_nor.msi
-:: URL|PLK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_pl_PL.msi|packages/adobereader/acrordrDC1500720033_plk.msi
-:: URL|PTB|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_pt_BR.msi|packages/adobereader/acrordrDC1500720033_ptb.msi
-:: URL|RUS|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_ru_RU.msi|packages/adobereader/acrordrDC1500720033_rus.msi
-:: URL|SVE|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_sv_SE.msi|packages/adobereader/acrordrDC1500720033_sve.msi
-:: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_tr_TR.msi|packages/adobereader/acrordrDC1500720033_trk.msi
+:: URL|ARA|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_ar_AE.msi|packages/adobereader/acrordrdc1500720033_ara.msi
+:: URL|CHS|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_zh_CN.msi|packages/adobereader/acrordrdc1500720033_chs.msi
+:: URL|CHT|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_zh_TW.msi|packages/adobereader/acrordrdc1500720033_cht.msi
+:: URL|CSY|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_cs_CZ.msi|packages/adobereader/acrordrdc1500720033_csy.msi
+:: URL|DAN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_da_DK.msi|packages/adobereader/acrordrdc1500720033_dan.msi
+:: URL|DEU|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_de_DE.msi|packages/adobereader/acrordrdc1500720033_deu.msi
+:: URL|ELL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_el_GR.msi|packages/adobereader/acrordrdc1500720033_ell.msi
+:: URL|ENU|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_en_US.msi|packages/adobereader/acrordrdc1500720033_enu.msi
+:: URL|ESN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_es_ES.msi|packages/adobereader/acrordrdc1500720033_esn.msi
+:: URL|FIN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_fi_FI.msi|packages/adobereader/acrordrdc1500720033_fin.msi
+:: URL|FRA|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_fr_FR.msi|packages/adobereader/acrordrdc1500720033_fra.msi
+:: URL|HEB|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_he_IL.msi|packages/adobereader/acrordrdc1500720033_heb.msi
+:: URL|HUN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_hu_HU.msi|packages/adobereader/acrordrdc1500720033_hun.msi
+:: URL|ITA|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_it_IT.msi|packages/adobereader/acrordrdc1500720033_ita.msi
+:: URL|JPN|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_ja_JP.msi|packages/adobereader/acrordrdc1500720033_jpn.msi
+:: URL|KOR|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_ko_KR.msi|packages/adobereader/acrordrdc1500720033_kor.msi
+:: URL|NLD|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_nl_NL.msi|packages/adobereader/acrordrdc1500720033_nld.msi
+:: URL|NOR|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_nb_NO.msi|packages/adobereader/acrordrdc1500720033_nor.msi
+:: URL|PLK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_pl_PL.msi|packages/adobereader/acrordrdc1500720033_plk.msi
+:: URL|PTB|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_pt_BR.msi|packages/adobereader/acrordrdc1500720033_ptb.msi
+:: URL|RUS|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_ru_RU.msi|packages/adobereader/acrordrdc1500720033_rus.msi
+:: URL|SVE|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_sv_SE.msi|packages/adobereader/acrordrdc1500720033_sve.msi
+:: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_tr_TR.msi|packages/adobereader/acrordrdc1500720033_trk.msi
 
 
 :: Download Adobe Reader Language Support
 :: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/AcroRdrSD1500720033_all_DC.msi|packages/adobereader/misc/AdbeRdrSD1500720033_all.msi
 :: Download Extended Language Support Fonts Package
 :: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/FontPack1500720033_XtdAlf_Lang_DC.msi|packages/adobereader/misc/FontPack1500720033_xtdalf_lang.msi
+:: Download the patch
+:: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500920077/AcroRdrDCUpd1500920077.msp|packages/adobereader/acrordrdcupd1500920077.msp
 
 :: Tool to create msp-file (transform) for msi available 
 :: http://www.adobe.com/support/downloads/new.jsp
@@ -52,4 +54,4 @@ todo.pl ".ignore-err 1 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion
 todo.pl ".ignore-err 1 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"Adobe Reader Speed Launcher\""
 
 :: This is the actual installer
-todo.pl ".ignore-err 194 msiexec /qn /i \"%Z%\packages\adobereader\acrordrDC1500720033_%WINLANG%.msi\" TRANSFORMS=acrordrdc1500720033_nor.mst REBOOT=ReallySuppress"
+todo.pl ".ignore-err 194 msiexec /qn /i \"%Z%\packages\adobereader\acrordrdc1500720033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\acrordrdcupd1500920077.msp\" TRANSFORMS=acrordrdc1500720033_nor.mst REBOOT=ReallySuppress"
