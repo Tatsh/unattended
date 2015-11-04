77abaa7b3b93c3e702d23bcd30ccbbe14d4b541c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-11-04 10:06:15 +0000

    Google Chrome v45.0.2454.101
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@3976 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL:  Install Chrome
 :: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-43.0.2357.130.msi
+:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-45.0.2454.101.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-43.0.2357.130.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-45.0.2454.101.msi\" /qb REBOOT=ReallySuppress"
