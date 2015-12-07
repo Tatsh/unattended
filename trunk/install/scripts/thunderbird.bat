a914d7e732b405e6fe0b35b4c5d3b6e4d18b0ee3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2015-12-07 15:05:17 +0000

    thunderbird 38.4.0

--- trunk/install/scripts/thunderbird.bat
+++ trunk/install/scripts/thunderbird.bat
@@ -1,34 +1,34 @@
-:: OPTIONAL: Install Thunderbird 38.3.0
+:: OPTIONAL: Install Thunderbird 38.4.0
 :: HOME: http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/?C=M;O=D
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/ca/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/cs/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/da/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/de/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/el/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/en-GB/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/en-US/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/es-ES/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/es-AR/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/fi/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/fr/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/he/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/hu/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/it/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/ja/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/ko/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/nl/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-nld.exe
-:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/nb-NO/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-nor.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/pl/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/pt-BR/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/ru/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/sl/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/sv-SE/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.3.0/win32/tr/Thunderbird%20Setup%2038.3.0.exe|packages/mozilla/thunderbird-3830-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/ca/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/cs/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/da/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/de/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/el/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/en-GB/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/en-US/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/es-ES/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/es-AR/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/fi/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/fr/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/he/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/hu/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/it/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/ja/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/ko/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/nl/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-nld.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/nb-NO/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-nor.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/pl/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/pt-BR/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/ru/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/sl/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/sv-SE/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/38.4.0/win32/tr/Thunderbird%20Setup%2038.4.0.exe|packages/mozilla/thunderbird-3840-trk.exe
 
 @echo off
 
 :: Add Thunderbird as Default Email Client
 todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
-todo.pl "%Z%\packages\mozilla\thunderbird-3830-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-3840-%WINLANG%.exe -ms -ira"
