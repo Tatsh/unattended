eafc63ad742dde22ef84f459ae8d012aa895a3fe
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-01-14 09:54:29 +0000

    Google Chrome For Work/Enterprise v47.0.2526.111, also added 64-bit version

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,7 +1,8 @@
-:: OPTIONAL:  Install Chrome
-:: HOME: http://googlechromereleases.blogspot.com/search/label/Stable%20updates
-:: URL|All|https://dl.google.com/tag/s/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-46.0.2490.86.msi
+:: OPTIONAL:  Install Google Chrome For Work
+:: HOME: https://www.google.no/work/chrome/browser/
+:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B834EEEB9-4D9D-8923-9464-40B2A14FFBBE%7D/dl/chrome/install/googlechromestandaloneenterprise.msi|packages/chrome/chrome_enterprise-47.0.2526.111-x86.msi
+:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B834EEEB9-4D9D-8923-9464-40B2A14FFBBE%7Dap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi|packages/chrome/chrome_enterprise-47.0.2526.111-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-46.0.2490.86.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-47.0.2526.111-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
