8bc7a24c48d5cbfd2bf4e5da123b96a600132840
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-02-05 12:05:51 +0000

    Flash Player v20.0.0.286

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 20.0.0.267 and 270
+:: OPTIONAL: Install Flash 20.0.0.286
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/20.0.0.267/install_flash_player.exe|packages/flash/install_flash_player_20.0.0.267.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/20.0.0.270/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_20.0.0.270.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/20.0.0.267/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_20.0.0.267.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/20.0.0.286/install_flash_player.exe|packages/flash/install_flash_player_20.0.0.286.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/20.0.0.286/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_20.0.0.286.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/20.0.0.286/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_20.0.0.286.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_20.0.0.270.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_20.0.0.286.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_20.0.0.267.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_20.0.0.286.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_20.0.0.267.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_20.0.0.286.exe -install"
