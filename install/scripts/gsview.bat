4e62d2046505f39ca4c0e61c6d807922baff4dd4
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-03-30 02:14:55 +0000

    GS 9.19

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -1,6 +1,6 @@
 :: OPTIONAL: Install Ghostscript and GSview
 :: HOME: http://pages.cs.wisc.edu/~ghost/
-:: URL|ALL|http://downloads.sourceforge.net/ghostscript/GPL%20Ghostscript/9.07/gs910w32.exe|packages/ghostscript/gs910w32.exe
+:: URL|ALL|https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs919/gs919w32.exe|packages/ghostscript/gs919w32.exe
 :: URL|ALL|http://pages.cs.wisc.edu/~ghost/gsview/download/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
 
 :: Then manually extract to gs and gv folders in the same path
@@ -8,8 +8,8 @@ todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv50w32.exe"
 
 :: some apps need ghostscript to be added on the path
-:: set path=%path%;%ProgramFiles%\gs\gs9.10\bin
+:: set path=%path%;%ProgramFiles%\gs\gs9.19\bin
 :: reg ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%path%" /f
 
-todo.pl "%Z%\packages\ghostscript\gs910w32.exe /S"
+todo.pl "%Z%\packages\ghostscript\gs919w32.exe /S"
 
