ff0da7b0142ffece4ba4f5da3b0d4e0260a71766
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-03-30 02:11:53 +0000

    Firefox 38.7.1esr
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4028 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
-:: Firefox 38.4.0esr
-::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/ar/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-ara.exe
-::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/ca/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-cat.exe
-::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/cs/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-csy.exe
-::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/zh-TW/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-cht.exe
-::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/zh-CN/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-chs.exe
-::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/da/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-dan.exe
-::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/de/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-deu.exe
-::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/el/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-ell.exe
-::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/en-US/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-enu.exe
-::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/en-GB/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-eng.exe
-::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/es-ES/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-esn.exe
-::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/es-AR/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-ess.exe
-::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/fi/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-fin.exe
-::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/fr/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-fra.exe
-::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/he/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-heb.exe
-::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/hu/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-hun.exe
-::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/it/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-ita.exe
-::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/ja/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-jpn.exe
-::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/ko/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-kor.exe
-::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/mk/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-mki.exe
-::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/nl/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-nld.exe
-::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/nb-NO/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-nor.exe
-::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/pl/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-plk.exe
-::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/pt-BR/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-ptb.exe
-::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/pt-PT/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-ptg.exe
-::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/ro/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-rom.exe
-::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/ru/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-rus.exe
-::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/sl/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-slv.exe
-::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/sv-SE/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-sve.exe
-::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/38.4.0esr/win32/tr/Firefox%20Setup%2038.4.0esr.exe|packages/mozilla/firefox-3840esr-trk.exe
+:: Firefox 38.7.1esr
+::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/ar/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-ara.exe
+::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/ca/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-cat.exe
+::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/cs/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-csy.exe
+::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/zh-TW/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-cht.exe
+::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/zh-CN/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-chs.exe
+::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/da/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-dan.exe
+::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/de/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-deu.exe
+::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/el/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-ell.exe
+::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/en-US/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-enu.exe
+::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/en-GB/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-eng.exe
+::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/es-ES/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-esn.exe
+::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/es-AR/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-ess.exe
+::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/fi/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-fin.exe
+::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/fr/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-fra.exe
+::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/he/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-heb.exe
+::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/hu/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-hun.exe
+::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/it/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-ita.exe
+::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/ja/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-jpn.exe
+::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/ko/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-kor.exe
+::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/mk/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-mki.exe
+::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/nl/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-nld.exe
+::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/nb-NO/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-nor.exe
+::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/pl/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-plk.exe
+::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/pt-BR/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-ptb.exe
+::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/pt-PT/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-ptg.exe
+::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/ro/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-rom.exe
+::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/ru/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-rus.exe
+::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/sl/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-slv.exe
+::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/sv-SE/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-sve.exe
+::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/38.7.1esr/win32/tr/Firefox%20Setup%2038.7.1esr.exe|packages/mozilla/firefox-3871esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-3840esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-3871esr-%WINLANG%.exe -ms -ira"
