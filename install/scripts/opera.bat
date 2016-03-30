b9d5ebd26500ab7a9bd4a373b9e133a81fb0df4c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-03-30 13:53:05 +0000

    Opera v36.0.2130.32
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4035 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|http://get.opera.com/pub/opera/desktop/35.0.2066.37/win/Opera_35.0.2066.37_Setup.exe|packages/opera/opera_35.0.2066.37_setup.exe
+:: URL|ALL|http://get.opera.com/pub/opera/desktop/36.0.2130.32/win/Opera_36.0.2130.32_Setup.exe|packages/opera/opera_36.0.2130.32_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_35.0.2066.37_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_36.0.2130.32_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
