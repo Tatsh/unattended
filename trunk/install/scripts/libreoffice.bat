ef8973de7e352d25201097e8cc8174cdb789cc5a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-07-19 18:29:09 +0000

    Libreoffice 5.1.4

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -5,8 +5,8 @@
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.1.3/win/x86/LibreOffice_5.1.3_Win_x86.msi|packages/libreoffice/libreoffice_5.1.3_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.1.3/win/x86_64/LibreOffice_5.1.3_Win_x64.msi|packages/libreoffice/libreoffice_5.1.3_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.1.4/win/x86/LibreOffice_5.1.4_Win_x86.msi|packages/libreoffice/libreoffice_5.1.4_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.1.4/win/x86_64/LibreOffice_5.1.4_Win_x64.msi|packages/libreoffice/libreoffice_5.1.4_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <https://www.libreoffice.org/download/libreoffice-still/>
@@ -17,4 +17,4 @@
 
 @echo off
 
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.1.3_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.1.4_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
