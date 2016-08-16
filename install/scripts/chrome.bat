53aee19b21518435b842567eddeaf7250526de32
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-08-16 13:19:05 +0000

    Google Chrome v52.0.2743.116

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install Google Chrome For Work
 :: HOME: https://www.google.no/work/chrome/browser/
-:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B834EEEB9-4D9D-8923-9464-40B2A14FFBBE%7D/dl/chrome/install/googlechromestandaloneenterprise.msi|packages/chrome/chrome_enterprise-51.0.2704.106-x86.msi
-:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B834EEEB9-4D9D-8923-9464-40B2A14FFBBE%7Dap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi|packages/chrome/chrome_enterprise-51.0.2704.106-AMD64.msi
+:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B834EEEB9-4D9D-8923-9464-40B2A14FFBBE%7D/dl/chrome/install/googlechromestandaloneenterprise.msi|packages/chrome/chrome_enterprise-52.0.2743.116-x86.msi
+:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B834EEEB9-4D9D-8923-9464-40B2A14FFBBE%7Dap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi|packages/chrome/chrome_enterprise-52.0.2743.116-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-51.0.2704.106-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-52.0.2743.116-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
