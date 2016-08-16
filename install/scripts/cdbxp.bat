f2df0fcaa3012bd28958946ce709854cb964882b
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-08-16 14:16:55 +0000

    CDBurnerXP v4.5.7.6282 fixed
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4086 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/cdbxp.bat
+++ install/scripts/cdbxp.bat
@@ -4,4 +4,4 @@
 :: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.7.6282.msi|packages/cdbxp/cdbxp_setup_4.5.7.6282-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\cdbxp.txt /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.6.6282-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\cdbxp.txt /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.7.6282-%PROCESSOR_ARCHITECTURE%.msi" 
