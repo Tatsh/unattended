8e5a8c6cf1bd2d63aab5e7a6b13584e7d5edf58c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-08-16 13:03:26 +0000

    Thunderbird v45.2.0

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,38 +1,38 @@
-:: OPTIONAL: Install Thunderbird 45.1.1
+:: OPTIONAL: Install Thunderbird 45.2.0
 :: HOME: http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/?C=M;O=D
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/ca/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/cs/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/da/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/de/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/el/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/en-GB/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/en-US/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/es-ES/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/es-AR/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/fi/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/fr/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/he/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/hu/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/it/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/ja/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/ko/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/nl/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-nld.exe
-:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/nb-NO/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-nor.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/pl/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/pt-BR/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/ru/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/sl/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/sv-SE/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.1.1/win32/tr/Thunderbird%20Setup%2045.1.1.exe|packages/mozilla/thunderbird-4511-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/ca/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-cat.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/cs/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-csy.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/da/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-dan.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/de/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-deu.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/el/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-ell.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/en-GB/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-eng.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/en-US/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-enu.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/es-ES/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-esn.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/es-AR/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-ess.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/fi/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-fin.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/fr/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-fra.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/he/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-heb.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/hu/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-hun.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/it/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-ita.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/ja/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-jpn.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/ko/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-kor.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/nl/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-nld.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/nb-NO/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-nor.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/pl/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-plk.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/pt-BR/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-ptb.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/ru/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-rus.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/sl/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-slv.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/sv-SE/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-sve.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/45.2.0/win32/tr/Thunderbird%20Setup%2045.2.0.exe|packages/mozilla/thunderbird-4520-trk.exe
 
 @echo off
 
 :: Delete the bundled Lightning add-on
-:: IF %PROCESSOR_ARCHITECTURE%==AMD64 todo.pl "del /S /Q \"%PROGRAMFILES(x86)%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\""
-:: IF %PROCESSOR_ARCHITECTURE%==x86 todo.pl "del /S /Q \"%PROGRAMFILES%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\""
+IF %PROCESSOR_ARCHITECTURE%==AMD64 todo.pl "del /S /Q \"%PROGRAMFILES(x86)%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\""
+IF %PROCESSOR_ARCHITECTURE%==x86 todo.pl "del /S /Q \"%PROGRAMFILES%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\""
 
 :: Add Thunderbird as Default Email Client
-:: todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
+todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
-todo.pl "%Z%\packages\mozilla\thunderbird-4511-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\thunderbird-4520-%WINLANG%.exe -ms -ira"
