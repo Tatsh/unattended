57ae61eb30276a35906504e8b3a49e34bd63e234
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-11-10 09:40:26 +0000

    Flash Player v23.0.0.207
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4094 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 22.0.0.209
+:: OPTIONAL: Install Flash 23.0.0.207
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/22.0.0.209/install_flash_player.exe|packages/flash/install_flash_player_22.0.0.209.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/22.0.0.209/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_22.0.0.209.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/22.0.0.209/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_22.0.0.209.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/23.0.0.207/install_flash_player.exe|packages/flash/install_flash_player_23.0.0.207.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/23.0.0.207/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_23.0.0.207.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/23.0.0.207/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_23.0.0.207.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_22.0.0.209.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_23.0.0.207.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_22.0.0.209.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_23.0.0.207.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_22.0.0.209.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_23.0.0.207.exe -install"
