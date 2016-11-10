aa821400e1604cbae98b895ebf3da5d9f9bd0017
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2016-11-10 10:16:11 +0000

    Opera v41.0.2353.56

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/38.0.2220.41/win/Opera_38.0.2220.41_Setup.exe|packages/opera/opera_38.0.2220.41_setup.exe
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/41.0.2353.56/win/Opera_41.0.2353.56_Setup.exe|packages/opera/opera_41.0.2353.56_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_38.0.2220.41_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_41.0.2353.56_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
