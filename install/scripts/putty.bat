4a4e2e048c300f6007d0a1701f0972b9026bf8cb
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-01-22 12:15:01 +0000

    update putty 0.67
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4116 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/putty.bat
+++ install/scripts/putty.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL: Install Putty 0.62
+:: OPTIONAL: Install Putty 0.67
 :: HOME: http://www.chiark.greenend.org.uk/~sgtatham/putty/
 :: Fix %7e -> ~
-:: URL|ALL|http://the.earth.li/%7esgtatham/putty/latest/x86/putty-0.63-installer.exe|packages/putty/putty-0.63-installer.exe
+:: URL|ALL|http://the.earth.li/%7esgtatham/putty/0.67/x86/putty-0.67-installer.msi|packages/putty/putty-0.67-installer.msi
 @Echo off
 
-todo.pl "%Z%\packages\putty\putty-0.63-installer.exe /sp- /silent"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\putty.txt /norestart /i %Z%\packages\putty\putty-0.67-installer.msi"
