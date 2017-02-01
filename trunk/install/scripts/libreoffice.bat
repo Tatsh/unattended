bf205a133aea7ce12ae9fb4dd0e34e40a64d3b3d
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-02-01 17:58:47 +0000

    Libreoffice update 5.3

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -5,15 +5,15 @@
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.5/win/x86/LibreOffice_5.2.5_Win_x86.msi|packages/libreoffice/libreoffice_5.2.5_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.5/win/x86_64/LibreOffice_5.2.5_Win_x64.msi|packages/libreoffice/libreoffice_5.2.5_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.3.0/win/x86/LibreOffice_5.3.0_Win_x86.msi|packages/libreoffice/libreoffice_5.3.0_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.3.0/win/x86_64/LibreOffice_5.3.0_Win_x64.msi|packages/libreoffice/libreoffice_5.3.0_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <https://www.libreoffice.org/download/libreoffice-still/>
 :: "LibreOffice Still is the stable version that has undergone more testing (over a longer time)
 :: It is usually recommended for more conservative use."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.1.6/win/x86/LibreOffice_5.1.6_Win_x86.msi|packages/libreoffice/libreoffice_5.1.6_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.1.6/win/x86_64/LibreOffice_5.1.6_Win_x64.msi|packages/libreoffice/libreoffice_5.1.6_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.5/win/x86/LibreOffice_5.2.5_Win_x86.msi|packages/libreoffice/libreoffice_5.2.5_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.5/win/x86_64/LibreOffice_5.2.5_Win_x64.msi|packages/libreoffice/libreoffice_5.2.5_win_AMD64_install.msi
 
 @echo off
 
