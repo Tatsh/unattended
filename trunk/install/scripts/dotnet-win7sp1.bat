cb0b96e3a1c018e2232c62d99acbad7bdb7a1d6c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-03-07 14:52:30 +0000

    Microsoft .NET Framework 4.6.2

--- trunk/install/scripts/dotnet-win7sp1.bat
+++ trunk/install/scripts/dotnet-win7sp1.bat
@@ -1,5 +1,5 @@
 :: INSTALL :: Microsoft .NET Package 
 :: MORE INFO: http://msdn.microsoft.com/en-us/library/bb822049.aspx
-:: URL|ALL|http://download.microsoft.com/download/b/a/4/ba4a7e71-2906-4b2d-a0e1-80cf16844f5f/dotnetfx45_full_x86_x64.exe|updates/common/dotnetfx45-full-x86-x64.exe
+:: URL|ALL|https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe|updates/common/dotnetfx462-full-x86-x64.exe
 
-todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx45-full-x86-x64.exe /norestart /passive"
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx462-full-x86-x64.exe /norestart /passive"
