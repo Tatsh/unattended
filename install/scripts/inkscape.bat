52a3454f073788d143072c0bae4cce78234db0b0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-03-08 07:34:29 +0000

    Inkscape 0.92.1

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
 
-:: URL|All|https://media.inkscape.org/dl/resources/file/Inkscape-0.92.0.msi|packages/inkscape/inkscape-0.92-0-x86.msi
-:: URL|All|https://media.inkscape.org/dl/resources/file/Inkscape-0.92.0-x64.msi|packages/inkscape/inkscape-0.92-0-AMD64.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/Inkscape-0.92.1.msi|packages/inkscape/inkscape-0.92.1-x86.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/Inkscape-0.92.1-x64.msi|packages/inkscape/inkscape-0.92.1-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92-0-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92.1-%PROCESSOR_ARCHITECTURE%.msi"
