5ebf93abfc8ad94d83366b067c2e034f43a7b894
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-03-08 07:38:01 +0000

    Gimp 2.8.20

--- install/scripts/gimp.bat
+++ install/scripts/gimp.bat
@@ -1,10 +1,10 @@
 ::OPTIONAL: Install GIMP (2.8)
 ::HOME: http://download.gimp.org/pub/gimp/
-::URL|ALL|http://download.gimp.org/pub/gimp/v2.8/windows/gimp-2.8.18-setup.exe|packages/gimp/gimp-2.8.18-setup.exe
+::URL|ALL|http://download.gimp.org/pub/gimp/v2.8/windows/gimp-2.8.20-setup.exe|packages/gimp/gimp-2.8.20-setup.exe
 @echo off
 
 :: Create a link on the desktop
 todo.pl "shortcut.pl  \"%ProgramFiles%\GIMP 2\bin\gimp-2.8.exe\" special:AllUsersDesktop\Gimp2"
 
 :: Run the installers
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.8.18-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.8.20-setup.exe SP- /SILENT /NORESTART"
