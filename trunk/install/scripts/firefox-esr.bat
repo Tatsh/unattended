a747be93355a3cdbf68f623c32e96106741bcbcc
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-03-22 12:32:54 +0000

    Firefox 45.8.0esr

--- trunk/install/scripts/firefox-esr.bat
+++ trunk/install/scripts/firefox-esr.bat
@@ -1,37 +1,37 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
-:: Firefox 45.7.0esr
-::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/ar/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-ara.exe
-::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/ca/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-cat.exe
-::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/cs/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-csy.exe
-::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/zh-TW/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-cht.exe
-::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/zh-CN/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-chs.exe
-::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/da/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-dan.exe
-::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/de/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-deu.exe
-::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/el/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-ell.exe
-::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/en-US/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-enu.exe
-::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/en-GB/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-eng.exe
-::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/es-ES/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-esn.exe
-::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/es-AR/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-ess.exe
-::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/fi/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-fin.exe
-::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/fr/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-fra.exe
-::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/he/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-heb.exe
-::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/hu/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-hun.exe
-::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/it/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-ita.exe
-::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/ja/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-jpn.exe
-::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/ko/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-kor.exe
-::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/mk/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-mki.exe
-::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/nl/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-nld.exe
-::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/nb-NO/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-nor.exe
-::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/pl/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-plk.exe
-::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/pt-BR/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-ptb.exe
-::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/pt-PT/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-ptg.exe
-::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/ro/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-rom.exe
-::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/ru/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-rus.exe
-::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/sl/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-slv.exe
-::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/sv-SE/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-sve.exe
-::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/45.7.0esr/win32/tr/Firefox%20Setup%2045.7.0esr.exe|packages/mozilla/firefox-4570esr-trk.exe
+:: Firefox 45.8.0esr
+::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/ar/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-ara.exe
+::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/ca/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-cat.exe
+::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/cs/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-csy.exe
+::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/zh-TW/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-cht.exe
+::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/zh-CN/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-chs.exe
+::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/da/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-dan.exe
+::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/de/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-deu.exe
+::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/el/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-ell.exe
+::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/en-US/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-enu.exe
+::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/en-GB/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-eng.exe
+::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/es-ES/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-esn.exe
+::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/es-AR/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-ess.exe
+::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/fi/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-fin.exe
+::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/fr/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-fra.exe
+::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/he/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-heb.exe
+::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/hu/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-hun.exe
+::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/it/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-ita.exe
+::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/ja/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-jpn.exe
+::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/ko/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-kor.exe
+::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/mk/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-mki.exe
+::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/nl/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-nld.exe
+::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/nb-NO/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-nor.exe
+::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/pl/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-plk.exe
+::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/pt-BR/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-ptb.exe
+::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/pt-PT/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-ptg.exe
+::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/ro/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-rom.exe
+::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/ru/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-rus.exe
+::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/sl/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-slv.exe
+::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/sv-SE/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-sve.exe
+::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/45.8.0esr/win32/tr/Firefox%20Setup%2045.8.0esr.exe|packages/mozilla/firefox-4580esr-trk.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-4570esr-%WINLANG%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-4580esr-%WINLANG%.exe -ms -ira"
