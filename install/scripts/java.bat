6e00a5b56c852158febe1536a8e1923de14299cd
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-05-04 09:24:04 +0000

    Java Runtime Environment v8u131

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -1,12 +1,11 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=218831_e9e7ea248e2c4826b92b3f075a80e441|packages/jre/jre-8u121-windows-x86.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=218833_e9e7ea248e2c4826b92b3f075a80e441|packages/jre/jre-8u121-windows-amd64.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220313_d54c1d3a095b4ff2b6607d096fa80163|packages/jre/jre-8u131-windows-x86.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220315_d54c1d3a095b4ff2b6607d096fa80163|packages/jre/jre-8u131-windows-amd64.exe
 
 :: JRE version 7 is no longer available for public download, need to login and "agree" to download it:
 :: <http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html>
-::URL|ALL|http://javadl.sun.com/webapps/download/AutoDL?BundleId=76208|packages/jre/jre-6u45-windows-i586.exe
 
 @Echo off
 :: Removes Java automatic updates:
@@ -16,4 +15,4 @@ todo.pl ".ignore-err 67 msiexec /qn /x {4A03706F-666A-4037-7777-5F2748764D10}"
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
 :: Some Apps and websites does not work if you are using 64bit
 :: to enable 64 change this jre-xuxx-windows-%PROCESSOR_ARCHITECTURE%.exe
-todo.pl "%Z%\packages\jre\jre-8u121-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u121.log REBOOT=ReallySuppress SPONSORS=0"
+todo.pl "%Z%\packages\jre\jre-8u131-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u131.log REBOOT=ReallySuppress SPONSORS=0"
