63c38fd8edaedf8dec09dc400f3547107925b80e
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-05-04 09:08:26 +0000

    Google Chrome v58.0.3029.96

--- trunk/install/scripts/chrome.bat
+++ trunk/install/scripts/chrome.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install Google Chrome For Work
 :: HOME: https://enterprise.google.com/chrome/chrome-browser/
-:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B90A30E55-AB4E-423E-9EBA-99A4BFC32BD3%7D/dl/chrome/install/googlechromestandaloneenterprise.msi|packages/chrome/chrome_enterprise-57.0.2987.98-x86.msi
-:: URL|All|https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B03FE9563-80F9-119F-DA3D-72FBBB94BC26%7Dap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi|packages/chrome/chrome_enterprise-57.0.2987.98-AMD64.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-58.0.3029.96-x86.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-58.0.3029.96-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-57.0.2987.98-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-58.0.3029.96-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
