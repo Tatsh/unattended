dacc892bbcf8c5dc0945cb494269c94102365591
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-05-13 09:49:27 +0000

    Libreoffice update 5.2.7 5.3.3

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -5,16 +5,16 @@
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.3.2/win/x86/LibreOffice_5.3.2_Win_x86.msi|packages/libreoffice/libreoffice_5.3.2_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.3.2/win/x86_64/LibreOffice_5.3.2_Win_x64.msi|packages/libreoffice/libreoffice_5.3.2_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.3.3/win/x86/LibreOffice_5.3.3_Win_x86.msi|packages/libreoffice/libreoffice_5.3.3_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.3.3/win/x86_64/LibreOffice_5.3.3_Win_x64.msi|packages/libreoffice/libreoffice_5.3.3_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
 :: "LibreOffice Still is the stable version that has undergone more testing (over a longer time)
 :: It is usually recommended for more conservative use."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.6/win/x86/LibreOffice_5.2.6_Win_x86.msi|packages/libreoffice/libreoffice_5.2.6_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.6/win/x86_64/LibreOffice_5.2.6_Win_x64.msi|packages/libreoffice/libreoffice_5.2.6_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.7/win/x86/LibreOffice_5.2.7_Win_x86.msi|packages/libreoffice/libreoffice_5.2.7_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.2.7/win/x86_64/LibreOffice_5.2.7_Win_x64.msi|packages/libreoffice/libreoffice_5.2.7_win_AMD64_install.msi
 
 @echo off
 
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.3.2_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.3.3_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
