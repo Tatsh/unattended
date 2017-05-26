c5cae04cc7c4ba3c0af01018354d4a108f71ec87
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-05-26 00:25:36 +0000

    tightvnc-2.8.8. Change firewall rule to allow all network profiles (As in windows xp)

--- trunk/install/scripts/tightvnc.bat
+++ trunk/install/scripts/tightvnc.bat
@@ -1,9 +1,11 @@
 :: OPTIONAL: Install TightVNC
 :: HOME: http://www.tightvnc.com/
-:: URL|ALL|http://www.tightvnc.com/download/2.8.2/tightvnc-2.8.2-gpl-setup-32bit.msi|packages/tightvnc-2.8.2-setup-x86.msi
-:: URL|ALL|http://www.tightvnc.com/download/2.8.2/tightvnc-2.8.2-gpl-setup-64bit.msi|packages/tightvnc-2.8.2-setup-AMD64.msi
+:: FIXME: Need to change user-agent to work
+:: URL|ALL|http://www.tightvnc.com/download/2.8.8/tightvnc-2.8.8-gpl-setup-32bit.msi|packages/tightvnc-2.8.8-setup-x86.msi
+:: URL|ALL|http://www.tightvnc.com/download/2.8.8/tightvnc-2.8.8-gpl-setup-64bit.msi|packages/tightvnc-2.8.8-setup-AMD64.msi
 @Echo off
 
 :: Add rule to the firewall to allow port 5900 
-todo.pl "\"netsh firewall add portopening protocol = TCP port = 5900 name = TightVNC mode = enable\""
-todo.pl "msiexec /qb /i %Z%\packages\tightvnc-2.8.2-setup-%PROCESSOR_ARCHITECTURE%.msi" 
+:: This change include public profile. Same behaviour as old good Windows XP 
+todo.pl "\"netsh firewall add portopening protocol=TCP port=5900 name=TightVNC profile=all\""
+todo.pl "msiexec /qb /i %Z%\packages\tightvnc-2.8.8-setup-%PROCESSOR_ARCHITECTURE%.msi" 
