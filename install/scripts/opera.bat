6888cd28316f09cfac307eda8ae886c53c531cc9
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-06-20 07:31:45 +0000

    Opera v45.0.2552.898

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/43.0.2442.1144/win/Opera_43.0.2442.1144_Setup.exe|packages/opera/opera_43.0.2442.1144_setup.exe
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/45.0.2552.898/win/Opera_45.0.2552.898_Setup.exe|packages/opera/opera_45.0.2552.898_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_43.0.2442.1144_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_45.0.2552.898_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
