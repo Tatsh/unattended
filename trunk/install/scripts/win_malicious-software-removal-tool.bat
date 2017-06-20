67ec065ebd21d71584c433663eeaf3ed4832ef78
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-06-20 07:27:00 +0000

    Windows Malicious Software Removal Tool v5.49

--- trunk/install/scripts/win_malicious-software-removal-tool.bat
+++ trunk/install/scripts/win_malicious-software-removal-tool.bat
@@ -3,7 +3,7 @@
 
 :: <http://support.microsoft.com/?kbid=890830>
 :: <https://www.microsoft.com/download/malicious-software-removal-tool-details.aspx>
-:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V5.48.exe|updates/common/windows-kb890830-x86-v5.48.exe
-:: URL|ALL|http://download.microsoft.com/download/2/C/5/2C563B99-54D9-4D85-A82B-45D3CD2F53CE/Windows-KB890830-x64-V5.48.exe|updates/common/windows-kb890830-amd64-v5.48.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.48.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V5.49.exe|updates/common/windows-kb890830-x86-v5.49.exe
+:: URL|ALL|http://download.microsoft.com/download/2/C/5/2C563B99-54D9-4D85-A82B-45D3CD2F53CE/Windows-KB890830-x64-V5.49.exe|updates/common/windows-kb890830-amd64-v5.49.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.49.exe /Q"
 
