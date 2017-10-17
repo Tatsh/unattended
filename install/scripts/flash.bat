463310a8a0841ae83a0d3e88e5c138bb7ff91978
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-10-17 09:16:18 +0000

    Flash Player v27.0.0.170

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 27.0.0.159
+:: OPTIONAL: Install Flash 27.0.0.170
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/27.0.0.159/install_flash_player.exe|packages/flash/install_flash_player_27.0.0.159.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/27.0.0.159/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_27.0.0.159.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/27.0.0.159/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_27.0.0.159.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/27.0.0.170/install_flash_player.exe|packages/flash/install_flash_player_27.0.0.170.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/27.0.0.170/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_27.0.0.170.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/27.0.0.170/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_27.0.0.170.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_27.0.0.159.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ax_27.0.0.170.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_27.0.0.159.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_27.0.0.170.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_27.0.0.159.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_27.0.0.170.exe -install"
