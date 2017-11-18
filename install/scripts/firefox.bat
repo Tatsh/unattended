5758f43fba4dfc94da9469d10eddf45d08ca6640
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-11-18 09:25:46 +0000

    Firefox Quantum 57 is here.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4200 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,69 +1,69 @@
 :: OPTIONAL: Install Firefox
 ::HOME: https://www.mozilla.org/en-US/firefox/all/
 
-:: Firefox 56.0.1 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/ar/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/ca/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/cs/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/zh-TW/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/zh-CN/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/da/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/de/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/el/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/en-US/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/en-GB/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/es-ES/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/es-AR/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/fi/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/fr/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/he/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/hu/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/it/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/ja/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/ko/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/mk/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/nl/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/nb-NO/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/pl/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/pt-BR/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/pt-PT/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/ro/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/ru/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/sl/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/sv-SE/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win32/tr/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-trk-x86.exe
-:: Firefox 56.0.1 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/ar/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/ca/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/cs/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/zh-TW/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/zh-CN/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/da/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/de/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/el/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/en-US/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/en-GB/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/es-ES/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/es-AR/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/fi/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/fr/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/he/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/hu/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/it/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/ja/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/ko/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/mk/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/nl/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/nb-NO/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/pl/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/pt-BR/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/pt-PT/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/ro/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/ru/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/sl/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/sv-SE/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/56.0.1/win64/tr/Firefox%20Setup%2056.0.1.exe|packages/mozilla/firefox-5601-trk-AMD64.exe
+:: Firefox 57.0 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/ar/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/ca/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/cs/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/zh-TW/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/zh-CN/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/da/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/de/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/el/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/en-US/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/en-GB/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/es-ES/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/es-AR/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/fi/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/fr/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/he/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/hu/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/it/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/ja/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/ko/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/mk/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/nl/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/nb-NO/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/pl/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/pt-BR/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/pt-PT/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/ro/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/ru/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/sl/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/sv-SE/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/57.0/win32/tr/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-trk-x86.exe
+:: Firefox 57.0 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/ar/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/ca/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/cs/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/zh-TW/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/zh-CN/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/da/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/de/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/el/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/en-US/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/en-GB/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/es-ES/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/es-AR/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/fi/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/fr/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/he/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/hu/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/it/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/ja/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/ko/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/mk/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/nl/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/nb-NO/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/pl/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/pt-BR/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/pt-PT/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/ro/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/ru/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/sl/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/sv-SE/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/57.0/win64/tr/Firefox%20Setup%2057.0.exe|packages/mozilla/firefox-5700-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-5601-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-5700-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira"
