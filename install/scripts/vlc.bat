21f66d38ffe0a2a06537d8da9fc5faacdd41c237
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-12-18 12:54:52 +0000

    Vlc 2.2.8
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4204 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.6/win32/vlc-2.2.6-win32.exe|packages/vlc/vlc-2.2.6-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.6/win64/vlc-2.2.6-win64.exe|packages/vlc/vlc-2.2.6-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.8/win32/vlc-2.2.8-win32.exe|packages/vlc/vlc-2.2.8-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.8/win64/vlc-2.2.8-win64.exe|packages/vlc/vlc-2.2.8-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.2.6-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-2.2.8-%PROCESSOR_ARCHITECTURE%.exe /S"
