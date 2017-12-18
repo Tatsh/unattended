c45a65bdc433a788fffb9c7d5fac8b96779f76e9
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-12-18 15:24:41 +0000

    CDBurnerXP v4.5.8.6795
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4206 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/cdbxp.bat
+++ install/scripts/cdbxp.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: CDBurnerXP
 :: HOME: http://www.cdburnerxp.se/
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.7.6521.msi|packages/cdbxp/cdbxp_setup_4.5.7.6521-x86.msi
-:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.7.6521.msi|packages/cdbxp/cdbxp_setup_4.5.7.6521-AMD64.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_4.5.8.6795.msi|packages/cdbxp/cdbxp_setup_4.5.8.6795-x86.msi
+:: URL|ALL|http://download.cdburnerxp.se/msi/cdbxp_setup_x64_4.5.8.6795.msi|packages/cdbxp/cdbxp_setup_4.5.8.6795-AMD64.msi
 
 @Echo off
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\cdbxp.txt /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.7.6521-%PROCESSOR_ARCHITECTURE%.msi" 
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\cdbxp.txt /norestart /i %Z%\packages\cdbxp\cdbxp_setup_4.5.8.6795-%PROCESSOR_ARCHITECTURE%.msi" 
