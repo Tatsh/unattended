3ad4c9679a2e61a005d4376309c5402ea256578f
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2017-12-20 12:21:53 +0000

    LibreOffice 5.4.4

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -5,8 +5,8 @@
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.3/win/x86/LibreOffice_5.4.3_Win_x86.msi|packages/libreoffice/libreoffice_5.4.3_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.3/win/x86_64/LibreOffice_5.4.3_Win_x64.msi|packages/libreoffice/libreoffice_5.4.3_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.4/win/x86/LibreOffice_5.4.4_Win_x86.msi|packages/libreoffice/libreoffice_5.4.4_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.4/win/x86_64/LibreOffice_5.4.4_Win_x64.msi|packages/libreoffice/libreoffice_5.4.4_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
@@ -19,4 +19,4 @@
 
 :: libreOffice 5.4.x is last one that works on windows XP
 :: See bug https://bugs.documentfoundation.org/show_bug.cgi?id=112928
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.4.3_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.4.4_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
