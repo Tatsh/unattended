a62862ba6f057109db2f8a4c5da536af77032528
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-01-24 09:48:44 +0000

    Firefox v58.0. Note: Java no longer works in Firefox from version 52 anyways (https://java.com/en/download/help/firefox_java.xml), and Flash seems to work fine in 64-bit version of Firefox, so I removed the comment.
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4222 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,70 +1,69 @@
 :: OPTIONAL: Install Firefox
 ::HOME: https://www.mozilla.org/en-US/firefox/all/
 
-:: Firefox 57.0.4 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/ar/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/ca/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/cs/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/zh-TW/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/zh-CN/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/da/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/de/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/el/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/en-US/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/en-GB/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/es-ES/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/es-AR/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/fi/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/fr/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/he/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/hu/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/it/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/ja/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/ko/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/mk/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/nl/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/nb-NO/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/pl/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/pt-BR/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/pt-PT/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/ro/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/ru/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/sl/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/sv-SE/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win32/tr/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-trk-x86.exe
-:: Firefox 57.0.4 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/ar/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/ca/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/cs/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/zh-TW/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/zh-CN/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/da/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/de/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/el/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/en-US/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/en-GB/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/es-ES/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/es-AR/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/fi/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/fr/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/he/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/hu/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/it/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/ja/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/ko/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/mk/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/nl/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/nb-NO/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/pl/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/pt-BR/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/pt-PT/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/ro/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/ru/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/sl/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/sv-SE/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/57.0.4/win64/tr/Firefox%20Setup%2057.0.4.exe|packages/mozilla/firefox-5704-trk-AMD64.exe
+:: Firefox 58.0 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/ar/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/ca/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/cs/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/zh-TW/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/zh-CN/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/da/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/de/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/el/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/en-US/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/en-GB/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/es-ES/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/es-AR/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/fi/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/fr/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/he/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/hu/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/it/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/ja/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/ko/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/mk/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/nl/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/nb-NO/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/pl/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/pt-BR/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/pt-PT/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/ro/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/ru/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/sl/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/sv-SE/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/58.0/win32/tr/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-trk-x86.exe
+:: Firefox 58.0 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/ar/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/ca/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/cs/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/zh-TW/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/zh-CN/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/da/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/de/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/el/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/en-US/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/en-GB/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/es-ES/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/es-AR/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/fi/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/fr/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/he/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/hu/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/it/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/ja/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/ko/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/mk/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/nl/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/nb-NO/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/pl/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/pt-BR/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/pt-PT/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/ro/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/ru/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/sl/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/sv-SE/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/58.0/win64/tr/Firefox%20Setup%2058.0.exe|packages/mozilla/firefox-5800-trk-AMD64.exe
 
 @Echo off
 
-# Java and Flash only works on 32 bit version. Maybe we need to change here.
-todo.pl "%Z%\packages\mozilla\firefox-5704-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-5800-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira"
