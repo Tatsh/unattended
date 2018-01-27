f2f6a7f2da94778b9d7460f5dd687171cd5ef80c
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-01-27 01:37:22 +0000

    Java jre-8u161
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4224 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227550_e758a0de34e24606bca991d704f6dcbf|packages/jre/jre-8u151-windows-x86.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227552_e758a0de34e24606bca991d704f6dcbf|packages/jre/jre-8u151-windows-amd64.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230540_2f38c3b165be4555a1fa6e98c45e0808|packages/jre/jre-8u161-windows-x86.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_2f38c3b165be4555a1fa6e98c45e0808|packages/jre/jre-8u161-windows-amd64.exe
 
 :: JRE version 7 is no longer available for public download, need to login and "agree" to download it:
 :: <http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html>
@@ -15,4 +15,4 @@ todo.pl ".ignore-err 67 msiexec /qn /x {4A03706F-666A-4037-7777-5F2748764D10}"
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
 :: Some Apps and websites does not work if you are using 64bit
 :: to enable 64 change this jre-xuxx-windows-%PROCESSOR_ARCHITECTURE%.exe
-todo.pl "%Z%\packages\jre\jre-8u151-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u151.log REBOOT=ReallySuppress SPONSORS=0"
+todo.pl "%Z%\packages\jre\jre-8u161-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u161.log REBOOT=ReallySuppress SPONSORS=0"
