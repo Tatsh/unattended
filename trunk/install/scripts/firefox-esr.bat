46af8a97f5517a66bd9e876a046af153a0090ef1
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-01-27 01:35:14 +0000

    Firefox ESR 5260

--- trunk/install/scripts/firefox-esr.bat
+++ trunk/install/scripts/firefox-esr.bat
@@ -1,71 +1,71 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
-:: Firefox 52.5.3esr 32Bit
-::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/ar/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ara-x86.exe
-::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/ca/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-cat-x86.exe
-::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/cs/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-csy-x86.exe
-::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/zh-TW/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-cht-x86.exe
-::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/zh-CN/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-chs-x86.exe
-::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/da/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-dan-x86.exe
-::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/de/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-deu-x86.exe
-::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/el/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ell-x86.exe
-::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/en-US/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-enu-x86.exe
-::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/en-GB/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-eng-x86.exe
-::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/es-ES/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-esn-x86.exe
-::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/es-AR/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ess-x86.exe
-::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/fi/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-fin-x86.exe
-::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/fr/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-fra-x86.exe
-::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/he/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-heb-x86.exe
-::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/hu/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-hun-x86.exe
-::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/it/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ita-x86.exe
-::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/ja/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-jpn-x86.exe
-::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/ko/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-kor-x86.exe
-::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/mk/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-mki-x86.exe
-::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/nl/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-nld-x86.exe
-::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/nb-NO/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-nor-x86.exe
-::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/pl/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-plk-x86.exe
-::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/pt-BR/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ptb-x86.exe
-::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/pt-PT/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ptg-x86.exe
-::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/ro/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-rom-x86.exe
-::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/ru/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-rus-x86.exe
-::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/sl/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-slv-x86.exe
-::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/sv-SE/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-sve-x86.exe
-::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win32/tr/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-trk-x86.exe
+:: Firefox 52.6.0esr 32Bit
+::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/ar/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ara-x86.exe
+::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/ca/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-cat-x86.exe
+::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/cs/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-csy-x86.exe
+::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/zh-TW/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-cht-x86.exe
+::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/zh-CN/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-chs-x86.exe
+::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/da/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-dan-x86.exe
+::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/de/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-deu-x86.exe
+::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/el/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ell-x86.exe
+::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/en-US/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-enu-x86.exe
+::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/en-GB/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-eng-x86.exe
+::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/es-ES/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-esn-x86.exe
+::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/es-AR/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ess-x86.exe
+::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/fi/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-fin-x86.exe
+::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/fr/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-fra-x86.exe
+::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/he/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-heb-x86.exe
+::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/hu/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-hun-x86.exe
+::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/it/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ita-x86.exe
+::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/ja/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-jpn-x86.exe
+::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/ko/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-kor-x86.exe
+::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/mk/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-mki-x86.exe
+::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/nl/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-nld-x86.exe
+::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/nb-NO/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-nor-x86.exe
+::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/pl/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-plk-x86.exe
+::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/pt-BR/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ptb-x86.exe
+::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/pt-PT/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ptg-x86.exe
+::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/ro/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-rom-x86.exe
+::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/ru/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-rus-x86.exe
+::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/sl/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-slv-x86.exe
+::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/sv-SE/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-sve-x86.exe
+::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win32/tr/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-trk-x86.exe
 
-:: Firefox 52.5.3esr 64Bit
-::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/ar/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ara-amd64.exe
-::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/ca/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-cat-amd64.exe
-::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/cs/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-csy-amd64.exe
-::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/zh-TW/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-cht-amd64.exe
-::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/zh-CN/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-chs-amd64.exe
-::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/da/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-dan-amd64.exe
-::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/de/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-deu-amd64.exe
-::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/el/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ell-amd64.exe
-::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/en-US/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-enu-amd64.exe
-::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/en-GB/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-eng-amd64.exe
-::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/es-ES/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-esn-amd64.exe
-::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/es-AR/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ess-amd64.exe
-::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/fi/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-fin-amd64.exe
-::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/fr/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-fra-amd64.exe
-::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/he/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-heb-amd64.exe
-::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/hu/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-hun-amd64.exe
-::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/it/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ita-amd64.exe
-::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/ja/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-jpn-amd64.exe
-::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/ko/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-kor-amd64.exe
-::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/mk/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-mki-amd64.exe
-::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/nl/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-nld-amd64.exe
-::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/nb-NO/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-nor-amd64.exe
-::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/pl/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-plk-amd64.exe
-::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/pt-BR/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ptb-amd64.exe
-::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/pt-PT/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-ptg-amd64.exe
-::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/ro/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-rom-amd64.exe
-::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/ru/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-rus-amd64.exe
-::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/sl/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-slv-amd64.exe
-::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/sv-SE/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-sve-amd64.exe
-::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/52.5.3esr/win64/tr/Firefox%20Setup%2052.5.3esr.exe|packages/mozilla/firefox-5253esr-trk-amd64.exe
+:: Firefox 52.6.0esr 64Bit
+::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/ar/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ara-amd64.exe
+::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/ca/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-cat-amd64.exe
+::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/cs/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-csy-amd64.exe
+::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/zh-TW/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-cht-amd64.exe
+::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/zh-CN/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-chs-amd64.exe
+::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/da/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-dan-amd64.exe
+::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/de/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-deu-amd64.exe
+::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/el/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ell-amd64.exe
+::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/en-US/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-enu-amd64.exe
+::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/en-GB/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-eng-amd64.exe
+::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/es-ES/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-esn-amd64.exe
+::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/es-AR/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ess-amd64.exe
+::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/fi/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-fin-amd64.exe
+::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/fr/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-fra-amd64.exe
+::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/he/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-heb-amd64.exe
+::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/hu/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-hun-amd64.exe
+::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/it/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ita-amd64.exe
+::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/ja/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-jpn-amd64.exe
+::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/ko/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-kor-amd64.exe
+::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/mk/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-mki-amd64.exe
+::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/nl/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-nld-amd64.exe
+::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/nb-NO/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-nor-amd64.exe
+::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/pl/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-plk-amd64.exe
+::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/pt-BR/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ptb-amd64.exe
+::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/pt-PT/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-ptg-amd64.exe
+::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/ro/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-rom-amd64.exe
+::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/ru/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-rus-amd64.exe
+::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/sl/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-slv-amd64.exe
+::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/sv-SE/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-sve-amd64.exe
+::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/52.6.0esr/win64/tr/Firefox%20Setup%2052.6.0esr.exe|packages/mozilla/firefox-5260esr-trk-amd64.exe
 
 
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-5253esr-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira"
+todo.pl "%Z%\packages\mozilla\firefox-5260esr-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira"
