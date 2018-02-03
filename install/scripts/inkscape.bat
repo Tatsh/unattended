16f3b06061d4efc39b6be4d6096ac1bbc3d29e71
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-02-03 16:10:34 +0000

    Inkscape 0.92.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4229 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
 
-:: URL|All|https://media.inkscape.org/dl/resources/file/Inkscape-0.92.1.msi|packages/inkscape/inkscape-0.92.1-x86.msi
-:: URL|All|https://media.inkscape.org/dl/resources/file/Inkscape-0.92.1-x64.msi|packages/inkscape/inkscape-0.92.1-AMD64.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.2-x86.msi|packages/inkscape/inkscape-0.92.2-x86.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.2-x64.msi|packages/inkscape/inkscape-0.92.2-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92.1-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92.2-%PROCESSOR_ARCHITECTURE%.msi"
