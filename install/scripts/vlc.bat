12d0809217d38c425971c3507cc85fc82f1ae207
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-03-09 14:11:04 +0000

    VLC v3.0.1

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.8/win32/vlc-2.2.8-win32.exe|packages/vlc/vlc-2.2.8-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/2.2.8/win64/vlc-2.2.8-win64.exe|packages/vlc/vlc-2.2.8-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.1/win32/vlc-3.0.1-win32.exe|packages/vlc/vlc-3.0.1-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.1/win64/vlc-3.0.1-win64.exe|packages/vlc/vlc-3.0.1-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-2.2.8-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-3.0.1-%PROCESSOR_ARCHITECTURE%.exe /S"
