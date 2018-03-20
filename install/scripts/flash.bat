7a458dddb6d07655b6e31fd2251ba1d82e1cbc6f
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-03-20 07:57:03 +0000

    Flash Player v29.0.0.113

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 28.0.0.161
+:: OPTIONAL: Install Flash 29.0.0.113
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/28.0.0.161/install_flash_player.exe|packages/flash/install_flash_player_28.0.0.161.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/28.0.0.161/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_28.0.0.161.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/28.0.0.161/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_28.0.0.161.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.113/install_flash_player.exe|packages/flash/install_flash_player_29.0.0.113.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.113/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_29.0.0.113.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.113/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_29.0.0.113.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl "%Z%\packages\flash\install_flash_player_ax_28.0.0.161.exe -install"
+todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_29.0.0.113.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_28.0.0.161.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_29.0.0.113.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_28.0.0.161.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_29.0.0.113.exe -install"
