b8712e3862fb30edbd70eaf632f94ff49d51be15
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-03-20 07:58:33 +0000

    .NET v4.7.1

--- install/scripts/dotnet-win7sp1.bat
+++ install/scripts/dotnet-win7sp1.bat
@@ -1,5 +1,5 @@
 :: INSTALL :: Microsoft .NET Package 
 :: MORE INFO: http://msdn.microsoft.com/en-us/library/bb822049.aspx
-:: URL|ALL|https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe|updates/common/dotnetfx462-full-x86-x64.exe
+:: URL|ALL|https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe|updates/common/dotnetfx471-full-x86-x64.exe
 
-todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx462-full-x86-x64.exe /norestart /passive"
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx471-full-x86-x64.exe /norestart /passive"
