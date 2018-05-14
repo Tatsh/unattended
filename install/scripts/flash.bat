400ca1ca155a7825ab1927c1d1b9f8152c1af259
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-05-14 20:10:16 +0000

    flash_player_29.0.0.171
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4252 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 29.0.0.113
+:: OPTIONAL: Install Flash 29.0.0.171
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.113/install_flash_player.exe|packages/flash/install_flash_player_29.0.0.113.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.113/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_29.0.0.113.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.113/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_29.0.0.113.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.171/install_flash_player.exe|packages/flash/install_flash_player_29.0.0.171.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.171/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_29.0.0.171.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/29.0.0.171/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_29.0.0.171.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_29.0.0.113.exe -install"
+todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_29.0.0.171.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_29.0.0.113.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_29.0.0.171.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_29.0.0.113.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_29.0.0.171.exe -install"
