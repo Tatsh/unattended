a6a8b3333892f02703cf5e90f20a4f0911c7fa68
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-05-21 15:16:13 +0000

    Libreoffice 5.4.7
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4254 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -12,11 +12,11 @@
 :: <http://www.libreoffice.org/download/libreoffice-still/>
 :: "LibreOffice Still is the stable version that has undergone more testing (over a longer time)
 :: It is usually recommended for more conservative use."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.6/win/x86/LibreOffice_5.4.6_Win_x86.msi|packages/libreoffice/libreoffice_5.4.6_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.6/win/x86_64/LibreOffice_5.4.6_Win_x64.msi|packages/libreoffice/libreoffice_5.4.6_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.7/win/x86/LibreOffice_5.4.7_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.7/win/x86_64/LibreOffice_5.4.7_Win_x64.msi|packages/libreoffice/libreoffice_5.4.7_win_AMD64_install.msi
 
 @echo off
 
 :: libreOffice 5.4.x is last one that works on windows XP
 :: See bug https://bugs.documentfoundation.org/show_bug.cgi?id=112928
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.4.6_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.4.7_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
