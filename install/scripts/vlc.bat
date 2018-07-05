5235d87b7b1bc8b762c1f8e88a0fdb6f3d4ce335
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-07-05 07:08:30 +0000

    VLC Media Player v3.0.3
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4260 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.1/win32/vlc-3.0.1-win32.exe|packages/vlc/vlc-3.0.1-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.1/win64/vlc-3.0.1-win64.exe|packages/vlc/vlc-3.0.1-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.3/win32/vlc-3.0.3-win32.exe|packages/vlc/vlc-3.0.3-x86.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.3/win64/vlc-3.0.3-win64.exe|packages/vlc/vlc-3.0.3-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-3.0.1-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\vlc\vlc-3.0.3-%PROCESSOR_ARCHITECTURE%.exe /S"
