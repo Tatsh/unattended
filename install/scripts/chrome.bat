3b74c8a89b7c91e4c5d2cab9b2c942f9716b6b90
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-07-05 07:02:54 +0000

    Google Chrome v67.0.3396.99

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install Google Chrome For Work
 :: HOME: https://enterprise.google.com/chrome/chrome-browser/
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-65.0.3325.162-x86.msi
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-65.0.3325.162-AMD64.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-67.0.3396.99-x86.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-67.0.3396.99-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-65.0.3325.162-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-67.0.3396.99-%PROCESSOR_ARCHITECTURE%.msi\" /qb REBOOT=ReallySuppress"
