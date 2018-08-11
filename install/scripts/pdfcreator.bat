9868c2b7379b222809f3f895f6a120d6bf8fcea5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-08-11 09:38:21 +0000

    PDFCreator 3.2.2. Install 2.5.3 if XP

--- install/scripts/pdfcreator.bat
+++ install/scripts/pdfcreator.bat
@@ -2,18 +2,19 @@
 :: HOME: http://download.pdfforge.org/download/pdfcreator
 
 @echo off
-:: URL|ALL|http://blue.download.pdfforge.org/pdfcreator/3.1.1/PDFCreator-3_1_1-Setup.exe|packages/pdfcreator/pdfcreator-3.1.1.setup.exe
+:: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/3.2.2/PDFCreator-3_2_2-Setup.exe|packages/pdfcreator/pdfcreator-3.2.2.setup.exe
+:: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/2.5.3/PDFCreator-2_5_3-Setup.exe|packages/pdfcreator/pdfcreator-2.5.3.setup.exe
 
-:install
 :: No need for a desktop shortcut.
 todo.pl "unlink.pl special:AllUsersDesktop\"\PDFCreator.lnk\"" 
 
 :: Avoid auto update dialog
 todo.pl ".ignore-err 1 reg add \"HKLM\SOFTWARE\PDFCreator\Program\" /v \"UpdateInterval\" /t REG_SZ /d \"0\" /f"
 
+::If this system is XP then use 2.5.3
+set VERSION=3.2.2
+if "%WINVER%" == "winxpsp3" set VERSION=2.5.3
+
 :: Install PDFCreator
 :: To set default language, add /LANG=(language name)
-todo.pl "%Z%\packages\pdfcreator\pdfcreator-3.1.1.setup.exe /LOG=%SystemDrive%\netinst\logs\pdfcreator.txt /SILENT /COMPONENTS=\"program\" /NORESTART"
-
-::DotNet requierement
-todo.pl dotnet.bat
+todo.pl "%Z%\packages\pdfcreator\pdfcreator-%VERSION%.setup.exe /LOG=%SystemDrive%\netinst\logs\pdfcreator.txt /SILENT /COMPONENTS=\"program\" /NORESTART"
