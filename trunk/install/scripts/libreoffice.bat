a2d7a1c51c60538a24c8667e4844a87e8a41c0b3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-08-11 08:55:08 +0000

    LibreOffice 6.1.0 and 6.0.6 Enable 5.4.x only for XP

--- trunk/install/scripts/libreoffice.bat
+++ trunk/install/scripts/libreoffice.bat
@@ -5,18 +5,24 @@
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.0.5/win/x86/LibreOffice_6.0.5_Win_x86.msi|packages/libreoffice/libreoffice_6.0.5_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.0.5/win/x86_64/LibreOffice_6.0.5_Win_x64.msi|packages/libreoffice/libreoffice_6.0.5_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.1.0/win/x86/LibreOffice_6.1.0_Win_x86.msi|packages/libreoffice/libreoffice_6.1.0_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.1.0/win/x86_64/LibreOffice_6.1.0_Win_x64.msi|packages/libreoffice/libreoffice_6.1.0_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
 :: "LibreOffice Still is the stable version that has undergone more testing (over a longer time)
 :: It is usually recommended for more conservative use."
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.0.6/win/x86/LibreOffice_6.0.6_Win_x86.msi|packages/libreoffice/libreoffice_6.0.6_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.0.6/win/x86_64/LibreOffice_6.0.6_Win_x64.msi|packages/libreoffice/libreoffice_6.0.6_win_AMD64_install.msi
+
+:: libreOffice 5.4.x is last release that works on windows XP
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.7/win/x86/LibreOffice_5.4.7_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.7/win/x86_64/LibreOffice_5.4.7_Win_x64.msi|packages/libreoffice/libreoffice_5.4.7_win_AMD64_install.msi
-
+:: See bug https://bugs.documentfoundation.org/show_bug.cgi?id=112928
 @echo off
 
-:: libreOffice 5.4.x is last one that works on windows XP
-:: See bug https://bugs.documentfoundation.org/show_bug.cgi?id=112928
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_5.4.7_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
+:: As default use the Still version but if this system is XP then use 5.4.x
+set VERSION=6.0.6
+if "%WINVER%" == "winxpsp3" set VERSION=5.4.7
+
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\libreoffice.txt /i %Z%\packages\libreoffice\libreoffice_%VERSION%_win_%PROCESSOR_ARCHITECTURE%_install.msi /norestart /passive"
