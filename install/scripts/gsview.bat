3db949973da3dbc339622e33cea41b160ea27491
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-09-06 06:05:27 +0000

    Ghostscript 9.24

--- install/scripts/gsview.bat
+++ install/scripts/gsview.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install Ghostscript and GSview
 :: HOME: http://pages.cs.wisc.edu/~ghost/
-:: URL|ALL|https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w32.exe|packages/ghostscript/gs922-x86.exe
-:: URL|ALL|https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe|packages/ghostscript/gs922-AMD64.exe
+:: URL|ALL|https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs924/gs924w32.exe|packages/ghostscript/gs924-x86.exe
+:: URL|ALL|https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs924/gs924w64.exe|packages/ghostscript/gs924-AMD64.exe
 :: URL|ALL|http://pages.cs.wisc.edu/~ghost/gsview/download/gsv50w32.exe|packages/ghostscript/gsv50w32.exe
 
 :: Then manually extract to gs and gv folders in the same path
@@ -9,7 +9,7 @@ todo.pl "%TEMP%\gv\setup.exe \"%ProgramFiles%\""
 todo.pl "\"%ProgramFiles%\7-Zip\7z\" x -o\"%TEMP%\"\gv %Z%\packages\ghostscript\gsv50w32.exe"
 
 :: some apps need ghostscript to be added on the path
-:: set path=%path%;%ProgramFiles%\gs\gs9.19\bin
+:: set path=%path%;%ProgramFiles%\gs\gs9.23\bin
 :: reg ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%path%" /f
 
-todo.pl "%Z%\packages\ghostscript\gs922-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\ghostscript\gs924-%PROCESSOR_ARCHITECTURE%.exe /S"
