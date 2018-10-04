1b7efad8c7d78ccb315da5b8f74c95af8fc61df2
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-10-04 14:43:20 +0000

    fix archive link for older versions

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -16,8 +16,8 @@
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/6.0.6/win/x86_64/LibreOffice_6.0.6_Win_x64.msi|packages/libreoffice/libreoffice_6.0.6_win_AMD64_install.msi
 
 :: libreOffice 5.4.x is last release that works on windows XP
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.7/win/x86/LibreOffice_5.4.7_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/5.4.7/win/x86_64/LibreOffice_5.4.7_Win_x64.msi|packages/libreoffice/libreoffice_5.4.7_win_AMD64_install.msi
+::URL|ALL|http://downloadarchive.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86/LibreOffice_5.4.7.2_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
+::URL|ALL|http://downloadarchive.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86_64/LibreOffice_5.4.7.2_Win_x64.msi|packages/libreoffice/libreoffice_5.4.7_win_AMD64_install.msi
 :: See bug https://bugs.documentfoundation.org/show_bug.cgi?id=112928
 @echo off
 
