0e1277604f50add08f9b0fc01a0d4851886f8ecd
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-10-14 17:07:32 +0000

    PDFCreator 3.3.0

--- install/scripts/pdfcreator.bat
+++ install/scripts/pdfcreator.bat
@@ -2,7 +2,7 @@
 :: HOME: http://download.pdfforge.org/download/pdfcreator
 
 @echo off
-:: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/3.2.2/PDFCreator-3_2_2-Setup.exe|packages/pdfcreator/pdfcreator-3.2.2.setup.exe
+:: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/3.3.0/PDFCreator-3_3_0-Setup.exe|packages/pdfcreator/pdfcreator-3.3.0.setup.exe
 :: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/2.5.3/PDFCreator-2_5_3-Setup.exe|packages/pdfcreator/pdfcreator-2.5.3.setup.exe
 
 :: No need for a desktop shortcut.
@@ -12,7 +12,7 @@ todo.pl "unlink.pl special:AllUsersDesktop\"\PDFCreator.lnk\""
 todo.pl ".ignore-err 1 reg add \"HKLM\SOFTWARE\PDFCreator\Program\" /v \"UpdateInterval\" /t REG_SZ /d \"0\" /f"
 
 ::If this system is XP then use 2.5.3
-set VERSION=3.2.2
+set VERSION=3.3.0
 if "%WINVER%" == "winxpsp3" set VERSION=2.5.3
 
 :: Install PDFCreator
