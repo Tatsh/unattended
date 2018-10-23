953fa5f20ff19e469d033b6a08c7487bdc23a0c6
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-10-23 09:28:12 +0000

    Microsoft .NET Framework v4.7.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4292 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/dotnet-win7sp1.bat
+++ install/scripts/dotnet-win7sp1.bat
@@ -1,5 +1,5 @@
 :: INSTALL :: Microsoft .NET Package 
 :: MORE INFO: http://msdn.microsoft.com/en-us/library/bb822049.aspx
-:: URL|ALL|https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe|updates/common/dotnetfx471-full-x86-x64.exe
+:: URL|ALL|https://download.microsoft.com/download/6/E/4/6E48E8AB-DC00-419E-9704-06DD46E5F81D/NDP472-KB4054530-x86-x64-AllOS-ENU.exe|updates/common/dotnetfx472-full-x86-x64.exe
 
-todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx471-full-x86-x64.exe /norestart /passive"
+todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx472-full-x86-x64.exe /norestart /passive"
