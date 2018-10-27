ee04efcb787c0b0f931faaeb96305b8cf876c51e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-10-27 22:34:44 +0000

    VLC 3.0.4

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,8 +1,9 @@
 :: OPTIONAL:  Install VideoLAN - VLC media player
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.4/win32/vlc-3.0.4-win32.exe|packages/vlc/vlc-3.0.4-x86.exe
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.4/win64/vlc-3.0.4-win64.exe|packages/vlc/vlc-3.0.4-AMD64.exe
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.4/win32/vlc-3.0.4-win32.msi|packages/vlc/vlc-3.0.4-x86.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.4/win64/vlc-3.0.4-win64.msi|packages/vlc/vlc-3.0.4-AMD64.msi
 
 @Echo off
 
-todo.pl "%Z%\packages\vlc\vlc-3.0.4-%PROCESSOR_ARCHITECTURE%.exe /S"
+
+todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.4-%PROCESSOR_ARCHITECTURE%.msi"
