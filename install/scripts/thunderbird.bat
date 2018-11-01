8a8e250ab8073870f12439ba8f46424676895510
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-11-01 10:18:05 +0000

    Thunderbird 60.3.0. Add 64bits support
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4296 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,43 +1,63 @@
-:: OPTIONAL: Install Thunderbird 60.2.1
+:: OPTIONAL: Install Thunderbird 60.3.0
 :: HOME: http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/?C=M;O=D
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 ::
-:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/ca/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-cat.exe
-:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/cs/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-csy.exe
-:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/da/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-dan.exe
-:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/de/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-deu.exe
-:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/el/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-ell.exe
-:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/en-GB/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-eng.exe
-:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/en-US/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-enu.exe
-:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/es-ES/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-esn.exe
-:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/es-AR/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-ess.exe
-:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/fi/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-fin.exe
-:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/fr/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-fra.exe
-:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/he/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-heb.exe
-:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/hu/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-hun.exe
-:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/it/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-ita.exe
-:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/ja/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-jpn.exe
-:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/ko/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-kor.exe
-:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/nl/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-nld.exe
-:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/nb-NO/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-nor.exe
-:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/pl/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-plk.exe
-:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/pt-BR/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-ptb.exe
-:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/ru/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-rus.exe
-:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/sl/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-slv.exe
-:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/sv-SE/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-sve.exe
-:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.2.1/win32/tr/Thunderbird%20Setup%2060.2.1.exe|packages/mozilla/thunderbird-6021-trk.exe
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/ca/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-cat-x86.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/cs/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-csy-x86.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/da/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-dan-x86.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/de/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-deu-x86.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/el/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ell-x86.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/en-GB/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-eng-x86.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/en-US/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-enu-x86.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/es-ES/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-esn-x86.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/es-AR/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ess-x86.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/fi/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-fin-x86.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/fr/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-fra-x86.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/he/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-heb-x86.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/hu/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-hun-x86.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/it/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ita-x86.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/ja/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-jpn-x86.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/ko/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-kor-x86.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/nl/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-nld-x86.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/nb-NO/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-nor-x86.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/pl/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-plk-x86.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/pt-BR/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ptb-x86.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/ru/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-rus-x86.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/sl/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-slv-x86.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/sv-SE/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-sve-x86.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win32/tr/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-trk-x86.exe
+
+:: URL|CAT|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/ca/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-cat-amd64.exe
+:: URL|CSY|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/cs/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-csy-amd64.exe
+:: URL|DAN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/da/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-dan-amd64.exe
+:: URL|DEU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/de/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-deu-amd64.exe
+:: URL|ELL|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/el/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ell-amd64.exe
+:: URL|ENG|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/en-GB/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-eng-amd64.exe
+:: URL|ENU|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/en-US/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-enu-amd64.exe
+:: URL|ESN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/es-ES/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-esn-amd64.exe
+:: URL|ESS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/es-AR/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ess-amd64.exe
+:: URL|FIN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/fi/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-fin-amd64.exe
+:: URL|FRA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/fr/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-fra-amd64.exe
+:: URL|HEB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/he/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-heb-amd64.exe
+:: URL|HUN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/hu/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-hun-amd64.exe
+:: URL|ITA|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/it/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ita-amd64.exe
+:: URL|JPN|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/ja/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-jpn-amd64.exe
+:: URL|KOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/ko/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-kor-amd64.exe
+:: URL|NLD|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/nl/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-nld-amd64.exe
+:: URL|NOR|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/nb-NO/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-nor-amd64.exe
+:: URL|PLK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/pl/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-plk-amd64.exe
+:: URL|PTB|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/pt-BR/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-ptb-amd64.exe
+:: URL|RUS|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/ru/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-rus-amd64.exe
+:: URL|SLV|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/sl/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-slv-amd64.exe
+:: URL|SVE|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/sv-SE/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-sve-amd64.exe
+:: URL|TRK|http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/60.3.0/win64/tr/Thunderbird%20Setup%2060.3.0.exe|packages/mozilla/thunderbird-6030-trk-amd64.exe
 
 @echo off
 
-:: Delete the bundled Lightning add-on
-::IF %PROCESSOR_ARCHITECTURE%==AMD64 todo.pl "rmdir /S /Q \"%PROGRAMFILES(x86)%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\""
-::IF %PROCESSOR_ARCHITECTURE%==x86 todo.pl "rmdir /S /Q \"%PROGRAMFILES%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\""
-
 :: Move Lightning add-on to globally installed extensions folder
-IF %PROCESSOR_ARCHITECTURE%==AMD64 todo.pl "move /Y \"%PROGRAMFILES(x86)%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\" \"%PROGRAMFILES(x86)%\Mozilla Thunderbird\extensions\""
-IF %PROCESSOR_ARCHITECTURE%==x86 todo.pl "move /Y \"%PROGRAMFILES%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}\" \"%PROGRAMFILES%\Mozilla Thunderbird\extensions\\""
+todo.pl "move /Y \"%PROGRAMFILES%\Mozilla Thunderbird\distribution\extensions\{e2fda1a4-762b-4020-b5ad-a41df1933103}.xpi\" \"%PROGRAMFILES%\Mozilla Thunderbird\extensions\""
 
 :: Add Thunderbird as Default Email Client
 todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
-todo.pl "%Z%\packages\mozilla\thunderbird-6021-%WINLANG%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\thunderbird-6030-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
