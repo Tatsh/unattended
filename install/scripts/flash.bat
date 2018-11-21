ce6a72486d0e5eeda813e7ebcbdc253517b664b5
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-11-21 12:59:37 +0000

    Flash Player v31.0.0.153
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4303 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 31.0.0.122
+:: OPTIONAL: Install Flash 31.0.0.153
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.122/install_flash_player.exe|packages/flash/install_flash_player_31.0.0.122.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.122/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_31.0.0.122.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.122/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_31.0.0.122.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.153/install_flash_player.exe|packages/flash/install_flash_player_31.0.0.153.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.153/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_31.0.0.153.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.153/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_31.0.0.153.exe
 ::
 @Echo off
 
@@ -12,10 +12,10 @@
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_31.0.0.122.exe -install"
+todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_31.0.0.153.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_31.0.0.122.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_31.0.0.153.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_31.0.0.122.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_31.0.0.153.exe -install"
