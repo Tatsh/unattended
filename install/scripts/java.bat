b83b962b8d0136fec9412743fcc22f6f8c595985
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2018-12-11 09:40:57 +0000

    Java 8 p 151 is the last one supported in windows xp
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4306 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -1,6 +1,7 @@
 :: OPTIONAL: Install Sun Java Virtual Machine
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227550_e758a0de34e24606bca991d704f6dcbf|packages/jre/jre-8u151-windows-x86.exe
 ::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=235725_2787e4a523244c269598db4e85c51e0c|packages/jre/jre-8u191-windows-x86.exe
 ::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=235727_2787e4a523244c269598db4e85c51e0c|packages/jre/jre-8u191-windows-amd64.exe
 
@@ -10,6 +11,12 @@
 @Echo off
 :: You can use WEB_JAVA_SECURITY_LEVEL=M 
 :: <http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jre-installer-options.html>
+
 :: Some Apps and websites does not work if you are using 64bit
-:: to enable 64 change this jre-xuxx-windows-%PROCESSOR_ARCHITECTURE%.exe
-todo.pl "%Z%\packages\jre\jre-8u191-windows-x86.exe /s /L %SystemDrive%\netinst\logs\jre-8u171.log REBOOT=ReallySuppress SPONSORS=Disable AUTO_UPDATE=Disable"
+:: to enable 64 change the VERSION variable.
+set VERSION=8u191-windows-x86
+
+::If system is XP then use version 8u151
+if "%WINVER%" == "winxpsp3" set VERSION=8u151-windows-x86
+
+todo.pl "%Z%\packages\jre\jre-%VERSION%.exe /s /L %SystemDrive%\netinst\logs\jre-%VERSION%.log REBOOT=ReallySuppress SPONSORS=Disable AUTO_UPDATE=Disable"
