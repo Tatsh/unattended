10fede670a314d65859fbb0992c5ee34f29a27e7
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-01-02 14:41:41 +0000

    Opera v57.0.3098.106

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/54.0.2952.64/win/Opera_54.0.2952.64_Setup.exe|packages/opera/opera_54.0.2952.64_setup.exe
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/57.0.3098.106/win/Opera_57.0.3098.106_Setup.exe|packages/opera/opera_57.0.3098.106_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_54.0.2952.64_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_57.0.3098.106_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
