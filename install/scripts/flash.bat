c6b4e3ef30ce35d57d9f58854a6dbe5894d3b020
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-01-02 13:48:37 +0000

    Flash Player v32.0.0.101

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 31.0.0.153
+:: OPTIONAL: Install Flash 32.0.0.101
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.153/install_flash_player.exe|packages/flash/install_flash_player_31.0.0.153.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.153/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_31.0.0.153.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/31.0.0.153/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_31.0.0.153.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.101/install_flash_player.exe|packages/flash/install_flash_player_32.0.0.101.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.101/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_32.0.0.101.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.101/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_32.0.0.101.exe
 ::
 @Echo off
 
@@ -19,10 +19,10 @@ todo.pl "sc delete AdobeFlashPlayerUpdateSvc /f"
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_31.0.0.153.exe -install"
+todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_32.0.0.101.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_31.0.0.153.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_32.0.0.101.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_31.0.0.153.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_32.0.0.101.exe -install"
