9bf9fcb7fada727529c6ece4b50e4d469b47beda
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-01-29 09:30:05 +0000

    Opera v58.0.3135.47
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4320 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/57.0.3098.106/win/Opera_57.0.3098.106_Setup.exe|packages/opera/opera_57.0.3098.106_setup.exe
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/58.0.3135.47/win/Opera_58.0.3135.47_Setup.exe|packages/opera/opera_58.0.3135.47_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_57.0.3098.106_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_58.0.3135.47_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
