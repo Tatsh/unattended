730fd8620ae824e36207f60767b5da529ebc0126
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-02-08 23:34:33 +0000

    inkscape-0.92.4

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,8 +1,8 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
 
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.3-x86.msi|packages/inkscape/inkscape-0.92.3-x86.msi
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.3-x64.msi|packages/inkscape/inkscape-0.92.3-AMD64.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.4-x86.msi|packages/inkscape/inkscape-0.92.4-x86.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.4-x64.msi|packages/inkscape/inkscape-0.92.4-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92.3-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92.4-%PROCESSOR_ARCHITECTURE%.msi"
