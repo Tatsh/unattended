fabface87dc73545915c3d19332d5faf79239ffc
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-03-11 00:39:00 +0000

    7-zip 19.00

--- install/scripts/7-zip.bat
+++ install/scripts/7-zip.bat
@@ -1,7 +1,7 @@
 :: OPTIONAL: Install 7-Zip
 :: HOME: http://www.7-zip.org/
-:: URL|ALL|http://www.7-zip.org/a/7z1806.msi|packages/sevenzip/7z1806-x86.msi
-:: URL|ALL|http://www.7-zip.org/a/7z1806-x64.msi|packages/sevenzip/7z1806-AMD64.msi
+:: URL|ALL|http://www.7-zip.org/a/7z1900.msi|packages/sevenzip/7z1900-x86.msi
+:: URL|ALL|http://www.7-zip.org/a/7z1900-x64.msi|packages/sevenzip/7z1900-AMD64.msi
 @Echo off
 
 if exist "%ProgramFiles%\7-ZIP\7z.exe" goto done
@@ -19,6 +19,6 @@ FOR %%j IN (%Extn%) DO (
 )
 ENDLOCAL
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\7zip.txt /i %Z%\packages\sevenzip\7z1806-%PROCESSOR_ARCHITECTURE%.msi REBOOT=ReallySuppress"
+todo.pl "msiexec /qn /i %Z%\packages\sevenzip\7z1900-%PROCESSOR_ARCHITECTURE%.msi REBOOT=ReallySuppress"
 
 :done
