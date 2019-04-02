9e6fd64e35ce5aee62650a7956f1648187d2de20
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-04-02 00:44:36 +0000

    Add a couple of links about howto use config xml
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4340 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/office2007.bat
+++ install/scripts/office2007.bat
@@ -7,5 +7,6 @@
 :: URL|ESN|http://download.microsoft.com/download/E/1/D/E1D71BF9-2D60-40D2-A473-0B06E63F113D/office2007sp3-kb2526086-fullfile-es-es.exe|updates/office2007/office2007sp3-kb2526086-fullfile-esn.exe
 todo.pl ".reboot-on 194 %Z%\updates\office2007\office2007sp3-kb2526086-fullfile-%WINLANG%.exe /quiet /norestart"
 
-
+:: https://docs.microsoft.com/en-us/previous-versions/office/office-2010/cc179006(v=office.14)
+:: https://blogs.technet.microsoft.com/odsupport/2009/06/10/customize-office-2007-by-using-the-config-xml/
 todo.pl "%Z%\packages\office2007\setup.exe /config %Z%\site\office2007.xml"
