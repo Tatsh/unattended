9ed4212c8cd66cee9a2361fc6ccbfcc2d3cefd55
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-04-13 17:24:47 +0000

    Firefox 66.0.3

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -2,69 +2,69 @@
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 65.0.2 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/ar/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/ca/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/cs/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/zh-TW/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/zh-CN/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/da/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/de/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/el/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/en-US/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/en-GB/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/es-ES/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/es-AR/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/fi/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/fr/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/he/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/hu/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/it/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/ja/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/ko/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/mk/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/nl/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/nb-NO/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/pl/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/pt-BR/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/pt-PT/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/ro/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/ru/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/sl/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/sv-SE/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win32/tr/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-trk-x86.exe
-:: Firefox 65.0.2 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/ar/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/ca/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/cs/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/zh-TW/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/zh-CN/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/da/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/de/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/el/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/en-US/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/en-GB/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/es-ES/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/es-AR/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/fi/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/fr/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/he/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/hu/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/it/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/ja/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/ko/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/mk/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/nl/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/nb-NO/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/pl/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/pt-BR/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/pt-PT/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/ro/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/ru/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/sl/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/sv-SE/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/65.0.2/win64/tr/Firefox%20Setup%2065.0.2.exe|packages/mozilla/firefox-6502-trk-AMD64.exe
+:: Firefox 66.0.3 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/ar/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/ca/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/cs/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/zh-TW/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/zh-CN/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/da/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/de/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/el/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/en-US/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/en-GB/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/es-ES/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/es-AR/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/fi/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/fr/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/he/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/hu/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/it/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/ja/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/ko/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/mk/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/nl/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/nb-NO/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/pl/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/pt-BR/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/pt-PT/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/ro/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/ru/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/sl/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/sv-SE/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win32/tr/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-trk-x86.exe
+:: Firefox 66.0.3 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/ar/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/ca/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/cs/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/zh-TW/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/zh-CN/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/da/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/de/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/el/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/en-US/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/en-GB/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/es-ES/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/es-AR/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/fi/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/fr/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/he/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/hu/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/it/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/ja/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/ko/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/mk/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/nl/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/nb-NO/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/pl/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/pt-BR/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/pt-PT/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/ro/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/ru/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/sl/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/sv-SE/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/66.0.3/win64/tr/Firefox%20Setup%2066.0.3.exe|packages/mozilla/firefox-6603-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-6502-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-6603-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
