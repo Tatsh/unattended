c1e327148518840549917eacb0d48e7034c153ca
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-04-18 16:12:23 +0000

    gimp-2.10.10
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4347 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/gimp.bat
+++ install/scripts/gimp.bat
@@ -1,11 +1,11 @@
 ::OPTIONAL: Install GIMP (2.10)
 ::HOME: http://download.gimp.org/pub/gimp/
-::URL|ALL|http://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.8-setup.exe|packages/gimp/gimp-2.10.8-x86-setup.exe
+::URL|ALL|http://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.10-setup.exe|packages/gimp/gimp-2.10.10-x86-setup.exe
 ::URL|ALL|http://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.0-x64-setup.exe|packages/gimp/gimp-2.10.0-amd64-setup.exe
 @echo off
 
 :: Create a link on the desktop
 todo.pl "shortcut.pl  \"%ProgramFiles%\GIMP 2\bin\gimp-2.10.exe\" special:AllUsersDesktop\Gimp2"
 
-:: Run the installers
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.10.8-x86-setup.exe SP- /SILENT /NORESTART"
+:: Run the installers. Avoid amd64 for now.
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.10.10-x86-setup.exe SP- /SILENT /NORESTART"
