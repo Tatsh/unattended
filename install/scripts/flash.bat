8dbdf3777572509ccec17567d9d7a66cfe78ffe4
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-06-07 11:06:57 +0000

    Delete  NPAPI Notifier task and update flash version

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,16 +1,17 @@
-:: OPTIONAL: Install Flash 32.0.0.114
+:: OPTIONAL: Install Flash 32.0.0.192
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.114/install_flash_player.exe|packages/flash/install_flash_player_32.0.0.114.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.114/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_32.0.0.114.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.114/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_32.0.0.114.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.192/install_flash_player.exe|packages/flash/install_flash_player_32.0.0.192.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.192/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_32.0.0.192.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.192/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_32.0.0.192.exe
 ::
 @Echo off
 
 :: Delete Tasks autoupdate:
 todo.pl "SchTasks /Delete /TN \"Adobe Flash Player Updater\" /f"
 todo.pl "SchTasks /Delete /TN \"Adobe Flash Player PPAPI Notifier\" /f"
+todo.pl "SchTasks /Delete /TN \"Adobe Flash Player NPAPI Notifier\" /f"
 
 :: Remove Flash autoupdate service:
 todo.pl "sc delete AdobeFlashPlayerUpdateSvc /f"
@@ -19,10 +20,10 @@ todo.pl "sc delete AdobeFlashPlayerUpdateSvc /f"
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_32.0.0.114.exe -install"
+todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_32.0.0.192.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_32.0.0.114.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_32.0.0.192.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_32.0.0.114.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_32.0.0.192.exe -install"
