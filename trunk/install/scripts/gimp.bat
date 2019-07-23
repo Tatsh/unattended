ed396c9a95035ebfc7c7f9b89858f81f127bf793
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-07-23 07:50:20 +0000

    gimp-2.10.12 second setup file

--- trunk/install/scripts/gimp.bat
+++ trunk/install/scripts/gimp.bat
@@ -1,6 +1,6 @@
 ::OPTIONAL: Install GIMP (2.10)
 ::HOME: http://download.gimp.org/pub/gimp/
-::URL|ALL|http://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.10-setup.exe|packages/gimp/gimp-2.10.10-x86-setup.exe
+::URL|ALL|http://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.12-setup-2.exe|packages/gimp/gimp-2.10.12-x86-setup-2.exe
 ::URL|ALL|http://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.0-x64-setup.exe|packages/gimp/gimp-2.10.0-amd64-setup.exe
 @echo off
 
@@ -8,4 +8,4 @@
 todo.pl "shortcut.pl  \"%ProgramFiles%\GIMP 2\bin\gimp-2.10.exe\" special:AllUsersDesktop\Gimp2"
 
 :: Run the installers. Avoid amd64 for now.
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.10.10-x86-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.10.12-x86-setup-2.exe SP- /SILENT /NORESTART"
