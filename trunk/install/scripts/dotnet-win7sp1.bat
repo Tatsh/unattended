0046162e603c4b5405c713a0f0e40cbaba8c6e6a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-09-01 12:29:57 +0000

    dotnet win7sp1 4.8

--- trunk/install/scripts/dotnet-win7sp1.bat
+++ trunk/install/scripts/dotnet-win7sp1.bat
@@ -1,5 +1,5 @@
 :: INSTALL :: Microsoft .NET Package 
 :: MORE INFO: http://msdn.microsoft.com/en-us/library/bb822049.aspx
-:: URL|ALL|https://download.microsoft.com/download/6/E/4/6E48E8AB-DC00-419E-9704-06DD46E5F81D/NDP472-KB4054530-x86-x64-AllOS-ENU.exe|updates/common/dotnetfx472-full-x86-x64.exe
+:: URL|ALL|https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe|updates/common/ndp48-x86-x64-allos-enu.exe
 
-todo.pl ".reboot-on 194 %Z%\updates\common\dotnetfx472-full-x86-x64.exe /norestart /passive"
+todo.pl ".reboot-on 194 %Z%\updates\common\ndp48-x86-x64-allos-enu.exe /norestart /passive"
