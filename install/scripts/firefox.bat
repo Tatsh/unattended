5d90a722bbbb6fe1fe053fcdcfd61f778e22b6c3
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2019-12-27 08:27:42 +0000

    Firefox 71.0

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,70 +1,70 @@
-:: OPTIONAL: Install Firefox 70.0.1
+:: OPTIONAL: Install Firefox 71.0
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 70.0.1 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/ar/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/ca/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/cs/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/zh-TW/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/zh-CN/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/da/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/de/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/el/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/en-US/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/en-GB/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/es-ES/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/es-AR/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/fi/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/fr/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/he/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/hu/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/it/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/ja/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/ko/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/mk/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/nl/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/nb-NO/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/pl/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/pt-BR/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/pt-PT/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/ro/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/ru/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/sl/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/sv-SE/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win32/tr/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-trk-x86.exe
-:: Firefox 70.0.1 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/ar/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/ca/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/cs/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/zh-TW/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/zh-CN/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/da/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/de/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/el/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/en-US/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/en-GB/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/es-ES/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/es-AR/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/fi/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/fr/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/he/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/hu/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/it/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/ja/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/ko/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/mk/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/nl/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/nb-NO/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/pl/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/pt-BR/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/pt-PT/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/ro/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/ru/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/sl/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/sv-SE/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/70.0.1/win64/tr/Firefox%20Setup%2070.0.1.exe|packages/mozilla/firefox-7001-trk-AMD64.exe
+:: Firefox 71.0 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/ar/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/ca/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/cs/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/zh-TW/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/zh-CN/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/da/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/de/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/el/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/en-US/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/en-GB/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/es-ES/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/es-AR/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/fi/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/fr/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/he/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/hu/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/it/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/ja/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/ko/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/mk/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/nl/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/nb-NO/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/pl/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/pt-BR/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/pt-PT/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/ro/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/ru/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/sl/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/sv-SE/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/71.0/win32/tr/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-trk-x86.exe
+:: Firefox 71.0 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/ar/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/ca/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/cs/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/zh-TW/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/zh-CN/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/da/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/de/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/el/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/en-US/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/en-GB/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/es-ES/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/es-AR/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/fi/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/fr/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/he/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/hu/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/it/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/ja/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/ko/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/mk/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/nl/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/nb-NO/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/pl/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/pt-BR/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/pt-PT/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/ro/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/ru/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/sl/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/sv-SE/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/71.0/win64/tr/Firefox%20Setup%2071.0.exe|packages/mozilla/firefox-710-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-7001-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-710-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
