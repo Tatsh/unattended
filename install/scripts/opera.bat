cf81d8ca5d487cee1b7fdf5e82715654c77feeac
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-01-02 11:12:58 +0000

    Opera v65.0.3467.42

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/58.0.3135.47/win/Opera_58.0.3135.47_Setup.exe|packages/opera/opera_58.0.3135.47_setup.exe
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/65.0.3467.42/win/Opera_65.0.3467.42_Setup.exe|packages/opera/opera_65.0.3467.42_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_58.0.3135.47_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_65.0.3467.42_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
