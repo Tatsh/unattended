228124225fd3931710dde53815c07af1f213b4d8
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-01-27 09:32:21 +0000

    Firefox v72.0.2

--- trunk/install/scripts/firefox.bat
+++ trunk/install/scripts/firefox.bat
@@ -1,70 +1,70 @@
-:: OPTIONAL: Install Firefox 72.0.1
+:: OPTIONAL: Install Firefox 72.0.2
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 72.0.1 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/ar/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/ca/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/cs/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/zh-TW/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/zh-CN/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/da/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/de/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/el/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/en-US/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/en-GB/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/es-ES/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/es-AR/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/fi/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/fr/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/he/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/hu/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/it/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/ja/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/ko/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/mk/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/nl/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/nb-NO/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/pl/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/pt-BR/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/pt-PT/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/ro/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/ru/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/sl/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/sv-SE/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win32/tr/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-trk-x86.exe
-:: Firefox 72.0.1 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/ar/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/ca/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/cs/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/zh-TW/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/zh-CN/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/da/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/de/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/el/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/en-US/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/en-GB/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/es-ES/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/es-AR/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/fi/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/fr/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/he/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/hu/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/it/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/ja/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/ko/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/mk/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/nl/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/nb-NO/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/pl/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/pt-BR/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/pt-PT/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/ro/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/ru/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/sl/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/sv-SE/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/72.0.1/win64/tr/Firefox%20Setup%2072.0.1.exe|packages/mozilla/firefox-7201-trk-AMD64.exe
+:: Firefox 72.0.2 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/ar/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/ca/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/cs/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/zh-TW/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/zh-CN/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/da/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/de/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/el/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/en-US/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/en-GB/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/es-ES/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/es-AR/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/fi/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/fr/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/he/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/hu/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/it/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/ja/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/ko/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/mk/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/nl/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/nb-NO/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/pl/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/pt-BR/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/pt-PT/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/ro/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/ru/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/sl/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/sv-SE/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win32/tr/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-trk-x86.exe
+:: Firefox 72.0.2 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/ar/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/ca/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/cs/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/zh-TW/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/zh-CN/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/da/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/de/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/el/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/en-US/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/en-GB/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/es-ES/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/es-AR/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/fi/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/fr/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/he/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/hu/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/it/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/ja/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/ko/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/mk/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/nl/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/nb-NO/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/pl/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/pt-BR/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/pt-PT/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/ro/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/ru/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/sl/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/sv-SE/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/72.0.2/win64/tr/Firefox%20Setup%2072.0.2.exe|packages/mozilla/firefox-7202-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-7201-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-7202-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
