5802c82ff0d8e09d49ad976dce9ef206e8435e89
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-05-09 00:36:20 +0000

    After 18yrs Inkscape release 1.0

--- trunk/install/scripts/inkscape.bat
+++ trunk/install/scripts/inkscape.bat
@@ -1,8 +1,9 @@
 :: OPTIONAL:  Install Inkscape and Open Clip Art 
 :: HOME: http://www.inkscape.org/
 
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.5-x86.msi|packages/inkscape/inkscape-0.92.5-x86.msi
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-0.92.5-x64.msi|packages/inkscape/inkscape-0.92.5-AMD64.msi
+
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.0-x86.msi|packages/inkscape/inkscape-1.0-x86.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.0-x64.msi|packages/inkscape/inkscape-1.0-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-0.92.5-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.0-%PROCESSOR_ARCHITECTURE%.msi"
