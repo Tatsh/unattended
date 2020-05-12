e64919eec67a1ea3ab5a7e34f4e48236f91c792b
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-05-12 22:29:03 +0000

    scribus-1.5.5

--- trunk/install/scripts/scribus.bat
+++ trunk/install/scripts/scribus.bat
@@ -1,14 +1,14 @@
 :: OPTIONAL:  Install Scribus
 :: HOME: http://www.scribus.net/
 
-:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.8-windows.exe|packages/scribus/scribus-1.4.8-windows-x86.exe
-:: URL|All|http://downloads.sourceforge.net/sourceforge/scribus/scribus-1.4.8-windows-x64.exe|packages/scribus/scribus-1.4.8-windows-AMD64.exe
+:: URL|All|http://downloads.sourceforge.net/scribus/scribus-1.5.5-windows.exe|packages/scribus/scribus-1.5.5-windows-x86.exe
+:: URL|All|http://downloads.sourceforge.net/scribus/scribus-1.5.5-windows-x64.exe|packages/scribus/scribus-1.5.5-windows-AMD64.exe
 @Echo off
 
 
 
 :: todo.pl "shortcut.pl \"%ProgramFiles%\"\Scribus\scribus.exe special:AllUsersDesktop\Scribus"
-todo.pl "%Z%\packages\scribus\scribus-1.4.8-windows-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\scribus\scribus-1.5.5-windows-%PROCESSOR_ARCHITECTURE%.exe /S"
 
 :: Install gsview first
 todo.pl gsview.bat
