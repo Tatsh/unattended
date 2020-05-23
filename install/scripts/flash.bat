52fd4dac9fbc7339e6888d9178e2163b6833ede0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-05-23 16:26:03 +0000

    Flash 32.0.0.371
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4427 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/flash.bat
+++ install/scripts/flash.bat
@@ -1,10 +1,10 @@
-:: OPTIONAL: Install Flash 32.0.0.192
+:: OPTIONAL: Install Flash 32.0.0.371
 :: HOME: http://get.adobe.com/flashplayer/otherversions/
 :: http://www.adobe.com/software/flash/about/
 :: http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.192/install_flash_player.exe|packages/flash/install_flash_player_32.0.0.192.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.192/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_32.0.0.192.exe
-:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.192/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_32.0.0.192.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.371/install_flash_player.exe|packages/flash/install_flash_player_32.0.0.371.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.371/install_flash_player_ax.exe|packages/flash/install_flash_player_ax_32.0.0.371.exe
+:: URL|ALL|http://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.371/install_flash_player_ppapi.exe|packages/flash/install_flash_player_ppapi_32.0.0.371.exe
 ::
 @Echo off
 
@@ -20,10 +20,10 @@ todo.pl "sc delete AdobeFlashPlayerUpdateSvc /f"
 todo.pl "echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg"
 
 :: ActiveX version for Internet Explorer
-todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_32.0.0.192.exe -install"
+todo.pl ".ignore-err 30 %Z%\packages\flash\install_flash_player_ax_32.0.0.371.exe -install"
 
 :: Version for Firefox NAPI
-todo.pl "%Z%\packages\flash\install_flash_player_32.0.0.192.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_32.0.0.371.exe -install"
 
 :: Version for Opera and Chromium
-todo.pl "%Z%\packages\flash\install_flash_player_ppapi_32.0.0.192.exe -install"
+todo.pl "%Z%\packages\flash\install_flash_player_ppapi_32.0.0.371.exe -install"
