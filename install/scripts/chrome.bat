e5622ffb3c0f6a6a28aef6c5d759ebcf17139929
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-06-21 11:27:05 +0000

    Google Chrome For Work 83.0.4103.106

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL:  Install Google Chrome For Work
-:: HOME: https://enterprise.google.com/chrome/chrome-browser/
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-79.0.3945.130-x86.msi
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-79.0.3945.130-AMD64.msi
+:: OPTIONAL:  Install Google Chrome For Work 83.0.4103.106
+:: HOME: https://enterprise.google.com/chrome/chrome-browser/ 
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-83.0.4103.106-x86.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-83.0.4103.106-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-79.0.3945.130-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-83.0.4103.106-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
