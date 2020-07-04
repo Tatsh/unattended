3f206009099aab13e1e70ed8a833966f37e679e5
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-07-04 07:03:02 +0000

    VideoLAN - VLC media player 3.0.11

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL:  VideoLAN - VLC media player 3.0.10
+:: OPTIONAL:  VideoLAN - VLC media player 3.0.11
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.10/win32/vlc-3.0.10-win32.msi|packages/vlc/vlc-3.0.10-x86.msi
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.10/win64/vlc-3.0.10-win64.msi|packages/vlc/vlc-3.0.10-AMD64.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.11/win32/vlc-3.0.11-win32.msi|packages/vlc/vlc-3.0.11-x86.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.11/win64/vlc-3.0.11-win64.msi|packages/vlc/vlc-3.0.11-AMD64.msi
 
 @Echo off
 
 
-todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.10-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.11-%PROCESSOR_ARCHITECTURE%.msi"
