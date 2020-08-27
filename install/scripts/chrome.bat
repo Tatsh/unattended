6d5ace25322d59aab790c87e1a54c335a936e817
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-08-27 13:35:54 +0000

    Chrome v85.0.4183.83
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4441 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL:  Install Google Chrome For Work 83.0.4103.106
+:: OPTIONAL:  Install Google Chrome For Work 85.0.4183.83
 :: HOME: https://enterprise.google.com/chrome/chrome-browser/ 
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-83.0.4103.106-x86.msi
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-83.0.4103.106-AMD64.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-85.0.4183.83-x86.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-85.0.4183.83-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-83.0.4103.106-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-85.0.4183.83-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
