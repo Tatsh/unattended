7941d24cf21dc39e0618e34994bd1c0d9f8771b0
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-11-02 15:16:43 +0000

    Chrome v86.0.4240.111

--- trunk/install/scripts/chrome.bat
+++ trunk/install/scripts/chrome.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL:  Install Google Chrome For Work 85.0.4183.83
+:: OPTIONAL:  Install Google Chrome For Work 86.0.4240.111
 :: HOME: https://enterprise.google.com/chrome/chrome-browser/ 
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-85.0.4183.83-x86.msi
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-85.0.4183.83-AMD64.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-86.0.4240.111-x86.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-86.0.4240.111-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-85.0.4183.83-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-86.0.4240.111-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
