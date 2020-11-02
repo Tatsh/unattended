7ff81a6c1884bd7cc6bd3a27f4bc448f13a7f947
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-11-02 15:14:20 +0000

    Firefox v82.0.2

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,70 +1,70 @@
-:: OPTIONAL: Install Firefox 80.0
+:: OPTIONAL: Install Firefox 82.0.2
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 80.0 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/ar/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/ca/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/cs/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/zh-TW/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/zh-CN/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/da/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/de/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/el/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/en-US/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/en-GB/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/es-ES/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/es-AR/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/fi/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/fr/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/he/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/hu/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/it/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/ja/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/ko/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/mk/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/nl/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/nb-NO/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/pl/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/pt-BR/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/pt-PT/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/ro/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/ru/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/sl/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/sv-SE/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/80.0/win32/tr/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-trk-x86.exe
-:: Firefox 80.0 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/ar/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/ca/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/cs/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/zh-TW/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/zh-CN/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/da/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/de/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/el/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/en-US/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/en-GB/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/es-ES/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/es-AR/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/fi/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/fr/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/he/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/hu/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/it/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/ja/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/ko/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/mk/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/nl/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/nb-NO/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/pl/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/pt-BR/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/pt-PT/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/ro/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/ru/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/sl/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/sv-SE/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/80.0/win64/tr/Firefox%20Setup%2080.0.exe|packages/mozilla/firefox-8000-trk-AMD64.exe
+:: Firefox 82.0.2 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/ar/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/ca/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/cs/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/zh-TW/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/zh-CN/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/da/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/de/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/el/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/en-US/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/en-GB/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/es-ES/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/es-AR/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/fi/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/fr/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/he/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/hu/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/it/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/ja/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/ko/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/mk/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/nl/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/nb-NO/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/pl/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/pt-BR/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/pt-PT/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/ro/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/ru/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/sl/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/sv-SE/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win32/tr/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-trk-x86.exe
+:: Firefox 82.0.2 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/ar/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/ca/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/cs/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/zh-TW/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/zh-CN/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/da/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/de/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/el/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/en-US/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/en-GB/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/es-ES/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/es-AR/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/fi/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/fr/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/he/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/hu/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/it/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/ja/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/ko/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/mk/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/nl/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/nb-NO/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/pl/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/pt-BR/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/pt-PT/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/ro/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/ru/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/sl/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/sv-SE/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/82.0.2/win64/tr/Firefox%20Setup%2082.0.2.exe|packages/mozilla/firefox-8202-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-8000-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-8202-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
