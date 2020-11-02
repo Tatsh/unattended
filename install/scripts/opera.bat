e5dea740136efe9f921aab9137ed4e9530b49aff
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-11-02 15:23:53 +0000

    Opera v72.0.3815.186

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
 :: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/65.0.3467.42/win/Opera_65.0.3467.42_Setup.exe|packages/opera/opera_65.0.3467.42_setup.exe
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/72.0.3815.186/win/Opera_72.0.3815.186_Setup.exe|packages/opera/opera_72.0.3815.186_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_65.0.3467.42_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_72.0.3815.186_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
