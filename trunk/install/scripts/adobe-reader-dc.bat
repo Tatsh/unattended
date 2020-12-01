fa9a578a7fce477fbacd959b4d5923bcc0847de7
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2020-12-01 14:11:42 +0000

    Adobe Reader Document Cloud (DC) 2001320064

--- trunk/install/scripts/adobe-reader-dc.bat
+++ trunk/install/scripts/adobe-reader-dc.bat
@@ -1,4 +1,4 @@
-:: OPTIONAL: Install Adobe Reader Document Cloud (DC) 2001220048
+:: OPTIONAL: Install Adobe Reader Document Cloud (DC) 2001320064
 @Echo Off
 
 :: Download Adobe Reader Document Cloud (DC) full version
@@ -27,7 +27,7 @@
 :: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_tr_TR.msi|packages/adobereader/acrordrdc1500720033_trk.msi
 
 :: Download the newest patch
-:: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/2001220048/AcroRdrDCUpd2001220048.msp|packages/adobereader/AcroRdrDCUpd2001220048.msp
+:: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/2001320064/AcroRdrDCUpd2001320064.msp|packages/adobereader/AcroRdrDCUpd2001320064.msp
 
 :: Download Adobe Reader Language Support
 :: URL|ALL|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/AcroRdrSD1500720033_all_DC.msi|packages/adobereader/misc/AdbeRdrSD1500720033_all.msi
@@ -55,4 +55,4 @@ todo.pl ".ignore-err 1 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion
 todo.pl ".ignore-err 1 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v \"Adobe Reader Speed Launcher\""
 
 :: This is the actual installer
-todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\adobereaderdc.txt /i \"%Z%\packages\adobereader\acrordrdc1500720033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\AcroRdrDCUpd2000920074.msp\" REBOOT=ReallySuppress"
+todo.pl ".ignore-err 194 msiexec /qn /l* %SystemDrive%\netinst\logs\adobereaderdc.txt /i \"%Z%\packages\adobereader\acrordrdc1500720033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\AcroRdrDCUpd2001320064.msp\" REBOOT=ReallySuppress"
