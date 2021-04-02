232c88981c954d6765d57db79d581a54e5acd922
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-04-02 08:58:28 +0000

    Scribus 1.5.6.1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4483 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/scribus.bat
+++ install/scripts/scribus.bat
@@ -1,14 +1,14 @@
-:: OPTIONAL:  Install Scribus
+:: OPTIONAL:  Install Scribus 1.5.6.1
 :: HOME: http://www.scribus.net/
 
-:: URL|All|http://downloads.sourceforge.net/scribus/scribus-1.5.5-windows.exe|packages/scribus/scribus-1.5.5-windows-x86.exe
-:: URL|All|http://downloads.sourceforge.net/scribus/scribus-1.5.5-windows-x64.exe|packages/scribus/scribus-1.5.5-windows-AMD64.exe
+:: URL|All|http://downloads.sourceforge.net/scribus/scribus-1.5.6.1-windows.exe|packages/scribus/scribus-1.5.6.1-windows-x86.exe
+:: URL|All|http://downloads.sourceforge.net/scribus/scribus-1.5.6.1-windows-x64.exe|packages/scribus/scribus-1.5.6.1-windows-AMD64.exe
 @Echo off
 
 
 
 :: todo.pl "shortcut.pl \"%ProgramFiles%\"\Scribus\scribus.exe special:AllUsersDesktop\Scribus"
-todo.pl "%Z%\packages\scribus\scribus-1.5.5-windows-%PROCESSOR_ARCHITECTURE%.exe /S"
+todo.pl "%Z%\packages\scribus\scribus-1.5.6.1-windows-%PROCESSOR_ARCHITECTURE%.exe /S"
 
 :: Install gsview first
 todo.pl gsview.bat
