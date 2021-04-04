5a22a34db83acdc4363d092384c270b65919dac0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-04-04 10:26:10 +0000

    GIMP (2.10.24)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4484 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/gimp.bat
+++ install/scripts/gimp.bat
@@ -1,6 +1,6 @@
-::OPTIONAL: Install GIMP (2.10.22)
+::OPTIONAL: Install GIMP (2.10.24)
 ::HOME: http://download.gimp.org/pub/gimp/
-::URL|ALL|https://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.22-setup.exe|packages/gimp/gimp-2.10.22-x86-setup.exe
+::URL|ALL|https://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.24-setup-2.exe|packages/gimp/gimp-2.10.24-x86-setup.exe
 ::URL|ALL|https://download.gimp.org/pub/gimp/v2.10/windows/gimp-2.10.0-x64-setup.exe|packages/gimp/gimp-2.10.0-amd64-setup.exe
 @echo off
 
@@ -8,4 +8,4 @@
 todo.pl "shortcut.pl  \"%ProgramFiles%\GIMP 2\bin\gimp-2.10.exe\" special:AllUsersDesktop\Gimp2"
 
 :: Run the installers. Avoid amd64 for now.
-todo.pl "start /wait %Z%\packages\gimp\gimp-2.10.22-x86-setup.exe SP- /SILENT /NORESTART"
+todo.pl "start /wait %Z%\packages\gimp\gimp-2.10.24-x86-setup.exe SP- /SILENT /NORESTART"
