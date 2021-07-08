94f02fa47f0c6ac94e554aa28838cec2303b09ba
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-07-08 11:41:04 +0000

    Firefox v89.0.2
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4500 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,71 +1,71 @@
-:: OPTIONAL: Install Firefox 89.0
+:: OPTIONAL: Install Firefox 89.0.2
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: http://download-origin.cdn.mozilla.net/pub/firefox/releases/?C=M;O=D
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 89.0 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/ar/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/ca/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/cs/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/zh-TW/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/zh-CN/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/da/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/de/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/el/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/en-US/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/en-GB/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/es-ES/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/es-AR/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/fi/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/fr/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/he/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/hu/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/it/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/ja/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/ko/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/mk/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/nl/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/nb-NO/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/pl/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/pt-BR/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/pt-PT/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/ro/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/ru/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/sl/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/sv-SE/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/89.0/win32/tr/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-trk-x86.exe
-:: Firefox 89.0 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/ar/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/ca/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/cs/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/zh-TW/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/zh-CN/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/da/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/de/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/el/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/en-US/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/en-GB/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/es-ES/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/es-AR/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/fi/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/fr/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/he/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/hu/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/it/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/ja/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/ko/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/mk/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/nl/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/nb-NO/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/pl/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/pt-BR/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/pt-PT/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/ro/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/ru/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/sl/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/sv-SE/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/89.0/win64/tr/Firefox%20Setup%2089.0.exe|packages/mozilla/firefox-8900-trk-AMD64.exe
+:: Firefox 89.0.2 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/ar/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/ca/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/cs/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/zh-TW/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/zh-CN/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/da/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/de/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/el/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/en-US/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/en-GB/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/es-ES/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/es-AR/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/fi/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/fr/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/he/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/hu/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/it/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/ja/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/ko/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/mk/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/nl/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/nb-NO/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/pl/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/pt-BR/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/pt-PT/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/ro/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/ru/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/sl/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/sv-SE/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win32/tr/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-trk-x86.exe
+:: Firefox 89.0.2 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/ar/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/ca/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/cs/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/zh-TW/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/zh-CN/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/da/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/de/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/el/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/en-US/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/en-GB/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/es-ES/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/es-AR/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/fi/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/fr/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/he/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/hu/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/it/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/ja/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/ko/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/mk/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/nl/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/nb-NO/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/pl/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/pt-BR/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/pt-PT/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/ro/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/ru/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/sl/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/sv-SE/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/89.0.2/win64/tr/Firefox%20Setup%2089.0.2.exe|packages/mozilla/firefox-8902-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-8900-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-8902-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
