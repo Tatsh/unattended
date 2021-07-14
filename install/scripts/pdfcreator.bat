ee44281341ba7bd31ce61eb55b146f7a0e94e95e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-07-14 17:26:13 +0000

    PdfCreator 4.3.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4504 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/pdfcreator.bat
+++ install/scripts/pdfcreator.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL: Install PDFCreator (PDF Printer) 4.2.0
+:: OPTIONAL: Install PDFCreator (PDF Printer) 4.3.0
 :: HOME: http://download.pdfforge.org/download/pdfcreator
 
 @echo off
-:: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/4.2.0/PDFCreator-4_2_0-Setup.exe|packages/pdfcreator/pdfcreator-4.2.0.setup.exe
+:: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/4.3.0/PDFCreator-4_3_0-Setup.exe|packages/pdfcreator/pdfcreator-4.3.0.setup.exe
 :: URL|ALL|http://azure.download.pdfforge.org/pdfcreator/2.5.3/PDFCreator-2_5_3-Setup.exe|packages/pdfcreator/pdfcreator-2.5.3.setup.exe
 
 :: No need for a desktop shortcut.
@@ -12,7 +12,7 @@ todo.pl "unlink.pl special:AllUsersDesktop\"\PDFCreator.lnk\""
 todo.pl ".ignore-err 1 reg add \"HKLM\SOFTWARE\PDFCreator\Program\" /v \"UpdateInterval\" /t REG_SZ /d \"0\" /f"
 
 ::If this system is XP then use 2.5.3
-set VERSION=4.2.0
+set VERSION=4.3.0
 if "%WINVER%" == "winxpsp3" set VERSION=2.5.3
 
 :: Install PDFCreator
