bf25b7b7e33582124e09f80e95d2f5019e559950
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-09-01 06:56:20 +0000

    Libreoffice 7.2.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4507 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -7,13 +7,15 @@
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.1.5/win/x86/LibreOffice_7.1.5_Win_x86.msi|packages/libreoffice/libreoffice_7.1.5_win_x86_install.msi
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.1.5/win/x86_64/LibreOffice_7.1.5_Win_x64.msi|packages/libreoffice/libreoffice_7.1.5_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.2.0/win/x86/LibreOffice_7.2.0_Win_x86.msi|packages/libreoffice/libreoffice_7.2.0_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.2.0/win/x86_64/LibreOffice_7.2.0_Win_x64.msi|packages/libreoffice/libreoffice_7.2.0_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
 :: "LibreOffice Still is the stable version that has undergone more testing (over a longer time)
 :: It is usually recommended for more conservative use."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.0.6/win/x86/LibreOffice_7.0.6_Win_x86.msi|packages/libreoffice/libreoffice_7.0.6_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.0.6/win/x86_64/LibreOffice_7.0.6_Win_x64.msi|packages/libreoffice/libreoffice_7.0.6_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.1.5/win/x86/LibreOffice_7.1.5_Win_x86.msi|packages/libreoffice/libreoffice_7.1.5_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.1.5/win/x86_64/LibreOffice_7.1.5_Win_x64.msi|packages/libreoffice/libreoffice_7.1.5_win_AMD64_install.msi
 
 :: libreOffice 5.4.x is last release that works on windows XP
 ::URL|ALL|http://downloadarchive.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86/LibreOffice_5.4.7.2_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
@@ -22,7 +24,7 @@
 @echo off
 
 :: As default use the Still version but if this system is XP then use 5.4.x
-set VERSION=7.0.6
+set VERSION=7.1.5
 if "%WINVER%" == "winxpsp3" set VERSION=5.4.7
 
 todo.pl ".ignore-err 194 msiexec /qn /norestart /passive /i %Z%\packages\libreoffice\libreoffice_%VERSION%_win_%PROCESSOR_ARCHITECTURE%_install.msi"
