fdea0343d7fc43df25ddc609b0eab750eec839ed
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-10-11 21:54:27 +0000

    Inkscape 1.1.1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4519 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL:  Install Inkscape 1.1
+:: OPTIONAL:  Install Inkscape 1.1.1
 :: HOME: http://www.inkscape.org/
-:: URL|All|https://inkscape.org/gallery/item/26938/inkscape-1.1-x86.msi|packages/inkscape/inkscape-1.1-x86.msi
-:: URL|All|https://inkscape.org/gallery/item/26939/inkscape-1.1-x64.msi|packages/inkscape/inkscape-1.1-AMD64.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.1.1_2021-09-20_3bf5ae0d25-x86.msi|packages/inkscape/inkscape-1.1.1-x86.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.1.1_2021-09-20_3bf5ae0d25-x64.msi|packages/inkscape/inkscape-1.1.1-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.1-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.1.1-%PROCESSOR_ARCHITECTURE%.msi"
