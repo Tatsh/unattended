ec4b51775fab06b84bf9bbca5ccc5ed8d312ff4d
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2021-10-11 22:04:35 +0000

    Update definitions 1.317.1843.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4520 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/mse.bat
+++ install/scripts/mse.bat
@@ -5,8 +5,8 @@
 :: URL|ESN|http://mse.dlservice.microsoft.com/download/2/A/C/2AC6E889-9EBB-4E06-A011-1AA9DAEDDB69/eses/x86/mseinstall.exe|packages/mse/mseinstall-x86.exe
 :: URL|ESN|http://mse.dlservice.microsoft.com/download/2/A/C/2AC6E889-9EBB-4E06-A011-1AA9DAEDDB69/eses/amd64/mseinstall.exe|packages/mse/mseinstall-amd64.exe
 :: DEFINITIONS: https://www.microsoft.com/en-us/wdsi/defenderupdates
-:: URL|ALL|http://definitionupdates.microsoft.com/download/DefinitionUpdates/VersionedSignatures/AM/1.317.1843.0/x86/mpam-fe.exe|packages/mse/mpam-fe-x86-1.317.1843.0.exe
-:: URL|ALL|http://definitionupdates.microsoft.com/download/DefinitionUpdates/VersionedSignatures/AM/1.317.1843.0/amd64/mpam-fe.exe|packages/mse/mpam-fe-amd64-1.317.1843.0.exe
+:: URL|ALL|http://definitionupdates.microsoft.com/download/DefinitionUpdates/VersionedSignatures/AM/1.351.225.0/1.1.18600.4/x86/mpam-fe.exe|packages/mse/mpam-fe-x86-1.351.225.0.exe
+:: URL|ALL|http://definitionupdates.microsoft.com/download/DefinitionUpdates/VersionedSignatures/AM/1.351.225.0/1.1.18600.4/amd64/mpam-fe.exe|packages/mse/mpam-fe-amd64-1.351.225.0.exe
 :: URL|ALL|http://definitionupdates.microsoft.com/download/DefinitionUpdates/NRI/x86/nis_full.exe|packages/mse/nis_full-x86.exe
 :: URL|ALL|http://definitionupdates.microsoft.com/download/DefinitionUpdates/NRI/amd64/nis_full.exe|packages/mse/nis_full-amd64.exe
 
@@ -14,7 +14,7 @@
 ::todo.pl "\"%PROGRAMFILES%\Microsoft Security Client\msseces.exe\" /update"
 
 :: INSTALL updates
-todo.pl "%Z%\packages\mse\mpam-fe-%PROCESSOR_ARCHITECTURE%-1.317.1843.0.exe /s"
+todo.pl "%Z%\packages\mse\mpam-fe-%PROCESSOR_ARCHITECTURE%-1.349.1004.0.exe /s"
 
 todo.pl ".reboot"
 
