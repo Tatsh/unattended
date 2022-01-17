d4112deec21536222b6ab50a42619de35e75a272
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-01-17 23:18:26 +0000

    Google Chrome 97.0.4692.71
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4539 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/chrome.bat
+++ install/scripts/chrome.bat
@@ -1,8 +1,8 @@
-:: OPTIONAL:  Install Google Chrome For Work 91.0.4472.124
-:: HOME: https://enterprise.google.com/chrome/chrome-browser/ 
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-91.0.4472.124-x86.msi
-:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-91.0.4472.124-AMD64.msi
+:: OPTIONAL:  Install Google Chrome For Work 97.0.4692.71
+:: HOME: https://chromeenterprise.google/browser/download/
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi|packages/chrome/chrome_enterprise-97.0.4692.71-x86.msi
+:: URL|All|https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi|packages/chrome/chrome_enterprise-97.0.4692.71-AMD64.msi
 
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-91.0.4472.124-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\google_chrome.txt /i \"%Z%\packages\chrome\chrome_enterprise-97.0.4692.71-%PROCESSOR_ARCHITECTURE%.msi\" REBOOT=ReallySuppress"
