1065d7a6afdbabbd2c7fd601f704b102cd813b02
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-02-21 08:33:22 +0000

    Thunderbird 91.6.1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4546 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,4 +1,4 @@
-:: OPTIONAL: Install Thunderbird 91.5.0
+:: OPTIONAL: Install Thunderbird 91.6.1
 :: HOME: http://download-origin.cdn.mozilla.net/pub/thunderbird/releases/?C=M;O=D
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 ::
@@ -27,55 +27,55 @@
 :: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/52.9.1/win32/sv-SE/Thunderbird%20Setup%2052.9.1.exe|packages/mozilla/thunderbird-5291-sve-x86.exe
 :: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/52.9.1/win32/tr/Thunderbird%20Setup%2052.9.1.exe|packages/mozilla/thunderbird-5291-trk-x86.exe
 
-:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/ca/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-cat-x86.exe
-:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/cs/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-csy-x86.exe
-:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/da/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-dan-x86.exe
-:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/de/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-deu-x86.exe
-:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/el/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ell-x86.exe
-:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/en-GB/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-eng-x86.exe
-:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/en-US/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-enu-x86.exe
-:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/es-ES/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-esn-x86.exe
-:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/es-AR/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ess-x86.exe
-:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/fi/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-fin-x86.exe
-:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/fr/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-fra-x86.exe
-:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/he/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-heb-x86.exe
-:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/hu/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-hun-x86.exe
-:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/it/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ita-x86.exe
-:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/ja/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-jpn-x86.exe
-:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/ko/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-kor-x86.exe
-:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/nl/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-nld-x86.exe
-:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/nb-NO/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-nor-x86.exe
-:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/pl/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-plk-x86.exe
-:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/pt-BR/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ptb-x86.exe
-:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/ru/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-rus-x86.exe
-:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/sl/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-slv-x86.exe
-:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/sv-SE/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-sve-x86.exe
-:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win32/tr/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-trk-x86.exe
+:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/ca/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-cat-x86.exe
+:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/cs/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-csy-x86.exe
+:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/da/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-dan-x86.exe
+:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/de/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-deu-x86.exe
+:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/el/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ell-x86.exe
+:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/en-GB/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-eng-x86.exe
+:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/en-US/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-enu-x86.exe
+:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/es-ES/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-esn-x86.exe
+:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/es-AR/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ess-x86.exe
+:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/fi/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-fin-x86.exe
+:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/fr/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-fra-x86.exe
+:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/he/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-heb-x86.exe
+:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/hu/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-hun-x86.exe
+:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/it/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ita-x86.exe
+:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/ja/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-jpn-x86.exe
+:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/ko/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-kor-x86.exe
+:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/nl/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-nld-x86.exe
+:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/nb-NO/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-nor-x86.exe
+:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/pl/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-plk-x86.exe
+:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/pt-BR/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ptb-x86.exe
+:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/ru/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-rus-x86.exe
+:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/sl/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-slv-x86.exe
+:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/sv-SE/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-sve-x86.exe
+:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win32/tr/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-trk-x86.exe
 
-:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/ca/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-cat-amd64.exe
-:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/cs/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-csy-amd64.exe
-:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/da/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-dan-amd64.exe
-:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/de/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-deu-amd64.exe
-:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/el/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ell-amd64.exe
-:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/en-GB/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-eng-amd64.exe
-:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/en-US/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-enu-amd64.exe
-:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/es-ES/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-esn-amd64.exe
-:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/es-AR/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ess-amd64.exe
-:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/fi/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-fin-amd64.exe
-:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/fr/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-fra-amd64.exe
-:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/he/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-heb-amd64.exe
-:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/hu/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-hun-amd64.exe
-:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/it/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ita-amd64.exe
-:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/ja/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-jpn-amd64.exe
-:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/ko/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-kor-amd64.exe
-:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/nl/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-nld-amd64.exe
-:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/nb-NO/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-nor-amd64.exe
-:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/pl/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-plk-amd64.exe
-:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/pt-BR/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-ptb-amd64.exe
-:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/ru/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-rus-amd64.exe
-:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/sl/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-slv-amd64.exe
-:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/sv-SE/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-sve-amd64.exe
-:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/91.5.0/win64/tr/Thunderbird%20Setup%2091.5.0.exe|packages/mozilla/thunderbird-9150-trk-amd64.exe
+:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/ca/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-cat-amd64.exe
+:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/cs/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-csy-amd64.exe
+:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/da/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-dan-amd64.exe
+:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/de/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-deu-amd64.exe
+:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/el/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ell-amd64.exe
+:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/en-GB/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-eng-amd64.exe
+:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/en-US/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-enu-amd64.exe
+:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/es-ES/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-esn-amd64.exe
+:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/es-AR/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ess-amd64.exe
+:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/fi/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-fin-amd64.exe
+:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/fr/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-fra-amd64.exe
+:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/he/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-heb-amd64.exe
+:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/hu/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-hun-amd64.exe
+:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/it/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ita-amd64.exe
+:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/ja/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-jpn-amd64.exe
+:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/ko/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-kor-amd64.exe
+:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/nl/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-nld-amd64.exe
+:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/nb-NO/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-nor-amd64.exe
+:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/pl/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-plk-amd64.exe
+:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/pt-BR/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-ptb-amd64.exe
+:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/ru/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-rus-amd64.exe
+:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/sl/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-slv-amd64.exe
+:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/sv-SE/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-sve-amd64.exe
+:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/91.6.1/win64/tr/Thunderbird%20Setup%2091.6.1.exe|packages/mozilla/thunderbird-9161-trk-amd64.exe
 
 @echo off
 
@@ -83,7 +83,7 @@
 todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
 :: if system is XP then use 52.9.1
-set VERSION=9150
+set VERSION=9161
 if "%WINVER%" == "winxpsp3" set VERSION=5291
 
 todo.pl "%Z%\packages\mozilla\thunderbird-%VERSION%-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
