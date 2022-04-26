54361476717386397dcd42ff9c33995d299b8d47
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-04-26 09:50:31 +0000

    VLC media player 3.0.17
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4553 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL:  VideoLAN - VLC media player 3.0.16
+:: OPTIONAL:  VideoLAN - VLC media player 3.0.17
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.16/win32/vlc-3.0.16-win32.msi|packages/vlc/vlc-3.0.16-x86.msi
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.16/win64/vlc-3.0.16-win64.msi|packages/vlc/vlc-3.0.16-AMD64.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.17/win32/vlc-3.0.17-win32.msi|packages/vlc/vlc-3.0.17-x86.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.17/win64/vlc-3.0.17-win64.msi|packages/vlc/vlc-3.0.17-AMD64.msi
 
 @Echo off
 
 
-todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.16-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.17-%PROCESSOR_ARCHITECTURE%.msi"
