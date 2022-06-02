8731cfd328c494283c343ebe612a023ebdaa994f
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-06-02 11:32:31 +0000

    Microsoft Windows Malicious Software Removal Tool v5.101
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4561 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/win_malicious-software-removal-tool.bat
+++ install/scripts/win_malicious-software-removal-tool.bat
@@ -1,9 +1,9 @@
-: Install the Microsoft Windows Malicious Software Removal Tool v5.100 (KB890830)
+: Install the Microsoft Windows Malicious Software Removal Tool v5.101 (KB890830)
 @Echo off
 
 :: <http://support.microsoft.com/kb/890830>
 :: https://www.catalog.update.microsoft.com/Search.aspx?q=KB890830
-:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V5.100.exe|updates/common/windows-kb890830-x86-v5.100.exe
-:: URL|ALL|http://download.microsoft.com/download/2/C/5/2C563B99-54D9-4D85-A82B-45D3CD2F53CE/Windows-KB890830-x64-V5.100.exe|updates/common/windows-kb890830-amd64-v5.100.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.100.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V5.101.exe|updates/common/windows-kb890830-x86-v5.101.exe
+:: URL|ALL|http://download.microsoft.com/download/2/C/5/2C563B99-54D9-4D85-A82B-45D3CD2F53CE/Windows-KB890830-x64-V5.101.exe|updates/common/windows-kb890830-amd64-v5.101.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.101.exe /Q"
 
