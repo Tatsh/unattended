a4c23f0a2ab0012fcb8a9d7abb040a33f17a30cf
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-06-02 11:32:10 +0000

    VLC v3.0.17.4
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4560 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/vlc.bat
+++ install/scripts/vlc.bat
@@ -1,9 +1,9 @@
-:: OPTIONAL:  VideoLAN - VLC media player 3.0.17
+:: OPTIONAL:  VideoLAN - VLC media player 3.0.17.4
 :: HOME: http://www.videolan.org/
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.17/win32/vlc-3.0.17-win32.msi|packages/vlc/vlc-3.0.17-x86.msi
-:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.17/win64/vlc-3.0.17-win64.msi|packages/vlc/vlc-3.0.17-AMD64.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win32/vlc-3.0.17.4-win32.msi|packages/vlc/vlc-3.0.17.4-x86.msi
+:: URL|All|http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.msi|packages/vlc/vlc-3.0.17.4-AMD64.msi
 
 @Echo off
 
 
-todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.17-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qn /norestart /i %Z%\packages\vlc\vlc-3.0.17.4-%PROCESSOR_ARCHITECTURE%.msi"
