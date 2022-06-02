74a7509a19a88fe73c66e9e9a8157fdd6dbcea7c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-06-02 11:31:55 +0000

    Opera v86.0.4363.23
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4559 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install Opera Browser
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/72.0.3815.186/win/Opera_72.0.3815.186_Setup.exe|packages/opera/opera_72.0.3815.186_setup.exe
+:: OPTIONAL: Install Opera Browser 86.0.4363.23
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/86.0.4363.23/win/Opera_86.0.4363.23_Setup.exe|packages/opera/opera_86.0.4363.23_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_72.0.3815.186_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_86.0.4363.23_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
