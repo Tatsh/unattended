95716cca9a21d60ed7d2cae5642e8e3c0d9f48a2
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-06-23 10:16:19 +0000

    Firefox 101.0.1
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4567 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,71 +1,71 @@
-:: OPTIONAL: Install Firefox 98.0.2
+:: OPTIONAL: Install Firefox 101.0.1
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: http://download-origin.cdn.mozilla.net/pub/firefox/releases/?C=M;O=D
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 98.0.2 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/ar/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/ca/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/cs/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/zh-TW/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/zh-CN/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/da/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/de/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/el/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/en-US/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/en-GB/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/es-ES/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/es-AR/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/fi/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/fr/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/he/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/hu/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/it/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/ja/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/ko/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/mk/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/nl/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/nb-NO/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/pl/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/pt-BR/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/pt-PT/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/ro/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/ru/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/sl/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/sv-SE/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win32/tr/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-trk-x86.exe
-:: Firefox 98.0.2 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/ar/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/ca/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/cs/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/zh-TW/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/zh-CN/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/da/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/de/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/el/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/en-US/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/en-GB/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/es-ES/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/es-AR/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/fi/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/fr/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/he/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/hu/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/it/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/ja/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/ko/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/mk/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/nl/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/nb-NO/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/pl/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/pt-BR/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/pt-PT/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/ro/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/ru/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/sl/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/sv-SE/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/98.0.2/win64/tr/Firefox%20Setup%2098.0.2.exe|packages/mozilla/firefox-9802-trk-AMD64.exe
+:: Firefox 101.0.1 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/ar/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/ca/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/cs/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/zh-TW/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/zh-CN/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/da/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/de/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/el/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/en-US/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/en-GB/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/es-ES/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/es-AR/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/fi/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/fr/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/he/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/hu/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/it/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/ja/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/ko/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/mk/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/nl/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/nb-NO/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/pl/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/pt-BR/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/pt-PT/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/ro/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/ru/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/sl/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/sv-SE/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win32/tr/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-trk-x86.exe
+:: Firefox 101.0.1 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/ar/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/ca/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/cs/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/zh-TW/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/zh-CN/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/da/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/de/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/el/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/en-US/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/en-GB/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/es-ES/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/es-AR/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/fi/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/fr/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/he/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/hu/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/it/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/ja/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/ko/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/mk/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/nl/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/nb-NO/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/pl/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/pt-BR/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/pt-PT/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/ro/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/ru/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/sl/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/sv-SE/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/101.0.1/win64/tr/Firefox%20Setup%20101.0.1.exe|packages/mozilla/firefox-10101-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-9802-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-10101-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
