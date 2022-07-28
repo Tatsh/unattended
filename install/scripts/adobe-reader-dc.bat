6b1452241c19d53fe95f76de4e08b2be89acd73d
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-07-28 07:25:39 +0000

    Adobe Reader DC 2022.001.20169 with updated Font & Language packs (2022.001.20085)
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4571 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/adobe-reader-dc.bat
+++ install/scripts/adobe-reader-dc.bat
@@ -1,4 +1,4 @@
-:: OPTIONAL: Install Adobe Reader Document Cloud (DC) 2100520048
+:: OPTIONAL: Install Adobe Reader Document Cloud (DC) 2200120169
 @Echo Off
 :: Note: Newest patches are no longer available on the FTP, link to patches can be found here:
 :: https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/
@@ -35,20 +35,20 @@
 :: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/acrordrdc1500720033_tr_TR.msi|packages/adobereader/acrordrdc1500720033_trk.msi
 
 :: Download the newest patch
-:: URL|ALL|https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2100720099/AcroRdrDCUpd2100720099.msp|packages/adobereader/AcroRdrDCUpd2100720099.msp
+:: URL|ALL|https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2200120169/AcroRdrDCUpd2200120169.msp|packages/adobereader/AcroRdrDCUpd2200120169.msp
 
 :: Extra language packs, more info:
 :: https://helpx.adobe.com/acrobat/kb/windows-font-packs-32-bit-reader.html
 :: Download Spelling dictionary pack
-:: URL|ALL|https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/AcroRdrSD1900820071_all_DC.msi|packages/adobereader/misc/AdbeRdrSD1900820071_all.msi
+:: URL|ALL|https://ardownload3.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/AcroRdrSD2200120085_all_DC.msi|packages/adobereader/misc/AcroRdrSD2200120085_all_DC.msi
 :: Download Font Pack
-:: URL|ALL|https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/FontPack2100120135_XtdAlf_Lang_DC.msi|packages/adobereader/misc/FontPack2100120135_XtdAlf_Lang_DC.msi
+:: URL|ALL|https://ardownload3.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/FontPack2200120085_XtdAlf_Lang_DC.msi|packages/adobereader/misc/FontPack2200120085_XtdAlf_Lang_DC.msi
 
 :: Uncomment the todo.pl lines if you want to install these language packs.
 :: Install Spelling dictionary pack
-:: todo.pl "msiexec /qb /i %Z%\packages\adobereader\misc\AdbeRdrSD1900820071_all.msi"
+:: todo.pl "msiexec /qb /i %Z%\packages\adobereader\misc\AcroRdrSD2200120085_all_DC.msi"
 :: Install Extended Asian Language Fonts Pack
-:: todo.pl "msiexec /qb /i %Z%\packages\adobereader\misc\FontPack2100120135_XtdAlf_Lang_DC.msi"
+:: todo.pl "msiexec /qb /i %Z%\packages\adobereader\misc\FontPack2200120085_XtdAlf_Lang_DC.msi"
 
 :: There is no need for a link on the desktop. This is a document-centric application.
 todo.pl "unlink.pl special:AllUsersDesktop\"\Acrobat Reader DC.lnk\"" 
@@ -56,4 +56,4 @@ todo.pl "unlink.pl special:AllUsersDesktop\"\Acrobat Reader DC.lnk\""
 todo.pl ".ignore-err 1 reg add \"HKLM\SOFTWARE\Adobe\Acrobat Reader\DC\AdobeViewer\" /f /v \"EULA\" /t REG_DWORD /d 1"
 
 :: This is the actual installer
-todo.pl ".ignore-err 194 msiexec /qb /l* %SystemDrive%\netinst\logs\adobereaderdc.txt /i \"%Z%\packages\adobereader\acrordrdc1500720033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\AcroRdrDCUpd2100720099.msp\" REBOOT=ReallySuppress"
+todo.pl ".ignore-err 194 msiexec /qb /l* %SystemDrive%\netinst\logs\adobereaderdc.txt /i \"%Z%\packages\adobereader\acrordrdc1500720033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\AcroRdrDCUpd2200120169.msp\" REBOOT=ReallySuppress"
