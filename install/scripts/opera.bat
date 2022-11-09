be7fb00750bd5a0e3536419cd62ea52f34319c97
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-11-09 08:55:09 +0000

    Opera v90.0.4480.107
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4589 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/opera.bat
+++ install/scripts/opera.bat
@@ -1,5 +1,5 @@
-:: OPTIONAL: Install Opera Browser 89.0.4447.48
-:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/89.0.4447.48/win/Opera_89.0.4447.48_Setup.exe|packages/opera/opera_89.0.4447.48_setup.exe
+:: OPTIONAL: Install Opera Browser 90.0.4480.107
+:: URL|ALL|https://ftp.opera.com/pub/opera/desktop/90.0.4480.107/win/Opera_90.0.4480.107_Setup.exe|packages/opera/opera_90.0.4480.107_setup.exe
 @Echo off
 
-todo.pl "%Z%\packages\opera\opera_89.0.4447.48_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
+todo.pl "%Z%\packages\opera\opera_90.0.4480.107_setup.exe /launcher /install /runimmediately /launchopera=0 /singleprofile=0 /copyonly=0 /allusers=1 /setdefaultbrowser=0 /startmenushortcut=1 /desktopshortcut=0 /quicklaunchshortcut=0 /pintotaskbar=0"
