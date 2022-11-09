d418117b2f55f41142c7f58b9274af1e205e6d8c
mythrix <mythrix@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-11-09 08:54:53 +0000

    Firefox v106.0.5
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4588 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox.bat
+++ install/scripts/firefox.bat
@@ -1,71 +1,71 @@
-:: OPTIONAL: Install Firefox 103.0
+:: OPTIONAL: Install Firefox 106.0.5
 :: HOME: https://www.mozilla.org/en-US/firefox/all/
 :: http://download-origin.cdn.mozilla.net/pub/firefox/releases/?C=M;O=D
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
-:: Firefox 103.0 32-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/ar/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ara-x86.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/ca/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-cat-x86.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/cs/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-csy-x86.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/zh-TW/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-cht-x86.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/zh-CN/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-chs-x86.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/da/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-dan-x86.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/de/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-deu-x86.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/el/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ell-x86.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/en-US/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-enu-x86.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/en-GB/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-eng-x86.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/es-ES/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-esn-x86.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/es-AR/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ess-x86.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/fi/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-fin-x86.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/fr/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-fra-x86.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/he/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-heb-x86.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/hu/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-hun-x86.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/it/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ita-x86.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/ja/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-jpn-x86.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/ko/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-kor-x86.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/mk/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-mki-x86.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/nl/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-nld-x86.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/nb-NO/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-nor-x86.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/pl/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-plk-x86.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/pt-BR/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ptb-x86.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/pt-PT/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ptg-x86.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/ro/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-rom-x86.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/ru/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-rus-x86.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/sl/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-slv-x86.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/sv-SE/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-sve-x86.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/103.0/win32/tr/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-trk-x86.exe
-:: Firefox 103.0 64-bit
-::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/ar/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ara-AMD64.exe
-::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/ca/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-cat-AMD64.exe
-::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/cs/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-csy-AMD64.exe
-::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/zh-TW/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-cht-AMD64.exe
-::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/zh-CN/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-chs-AMD64.exe
-::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/da/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-dan-AMD64.exe
-::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/de/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-deu-AMD64.exe
-::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/el/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ell-AMD64.exe
-::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/en-US/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-enu-AMD64.exe
-::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/en-GB/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-eng-AMD64.exe
-::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/es-ES/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-esn-AMD64.exe
-::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/es-AR/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ess-AMD64.exe
-::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/fi/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-fin-AMD64.exe
-::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/fr/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-fra-AMD64.exe
-::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/he/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-heb-AMD64.exe
-::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/hu/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-hun-AMD64.exe
-::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/it/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ita-AMD64.exe
-::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/ja/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-jpn-AMD64.exe
-::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/ko/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-kor-AMD64.exe
-::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/mk/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-mki-AMD64.exe
-::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/nl/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-nld-AMD64.exe
-::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/nb-NO/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-nor-AMD64.exe
-::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/pl/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-plk-AMD64.exe
-::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/pt-BR/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ptb-AMD64.exe
-::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/pt-PT/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-ptg-AMD64.exe
-::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/ro/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-rom-AMD64.exe
-::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/ru/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-rus-AMD64.exe
-::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/sl/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-slv-AMD64.exe
-::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/sv-SE/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-sve-AMD64.exe
-::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/103.0/win64/tr/Firefox%20Setup%20103.0.exe|packages/mozilla/firefox-10300-trk-AMD64.exe
+:: Firefox 106.0.5 32-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/ar/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ara-x86.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/ca/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-cat-x86.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/cs/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-csy-x86.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/zh-TW/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-cht-x86.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/zh-CN/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-chs-x86.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/da/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-dan-x86.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/de/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-deu-x86.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/el/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ell-x86.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/en-US/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-enu-x86.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/en-GB/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-eng-x86.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/es-ES/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-esn-x86.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/es-AR/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ess-x86.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/fi/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-fin-x86.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/fr/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-fra-x86.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/he/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-heb-x86.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/hu/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-hun-x86.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/it/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ita-x86.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/ja/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-jpn-x86.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/ko/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-kor-x86.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/mk/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-mki-x86.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/nl/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-nld-x86.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/nb-NO/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-nor-x86.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/pl/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-plk-x86.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/pt-BR/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ptb-x86.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/pt-PT/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ptg-x86.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/ro/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-rom-x86.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/ru/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-rus-x86.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/sl/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-slv-x86.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/sv-SE/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-sve-x86.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win32/tr/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-trk-x86.exe
+:: Firefox 106.0.5 64-bit
+::URL|ARA|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/ar/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ara-AMD64.exe
+::URL|CAT|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/ca/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-cat-AMD64.exe
+::URL|CSY|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/cs/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-csy-AMD64.exe
+::URL|CHT|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/zh-TW/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-cht-AMD64.exe
+::URL|CHS|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/zh-CN/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-chs-AMD64.exe
+::URL|DAN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/da/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-dan-AMD64.exe
+::URL|DEU|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/de/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-deu-AMD64.exe
+::URL|ELL|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/el/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ell-AMD64.exe
+::URL|ENU|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/en-US/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-enu-AMD64.exe
+::URL|ENG|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/en-GB/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-eng-AMD64.exe
+::URL|ESN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/es-ES/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-esn-AMD64.exe
+::URL|ESS|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/es-AR/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ess-AMD64.exe
+::URL|FIN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/fi/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-fin-AMD64.exe
+::URL|FRA|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/fr/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-fra-AMD64.exe
+::URL|HEB|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/he/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-heb-AMD64.exe
+::URL|HUN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/hu/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-hun-AMD64.exe
+::URL|ITA|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/it/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ita-AMD64.exe
+::URL|JPN|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/ja/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-jpn-AMD64.exe
+::URL|KOR|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/ko/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-kor-AMD64.exe
+::URL|MKI|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/mk/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-mki-AMD64.exe
+::URL|NLD|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/nl/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-nld-AMD64.exe
+::URL|NOR|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/nb-NO/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-nor-AMD64.exe
+::URL|PLK|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/pl/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-plk-AMD64.exe
+::URL|PTB|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/pt-BR/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ptb-AMD64.exe
+::URL|PTG|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/pt-PT/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-ptg-AMD64.exe
+::URL|ROM|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/ro/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-rom-AMD64.exe
+::URL|RUS|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/ru/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-rus-AMD64.exe
+::URL|SLV|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/sl/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-slv-AMD64.exe
+::URL|SVE|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/sv-SE/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-sve-AMD64.exe
+::URL|TRK|http://releases.mozilla.org/pub/firefox/releases/106.0.5/win64/tr/Firefox%20Setup%20106.0.5.exe|packages/mozilla/firefox-10605-trk-AMD64.exe
 
 @Echo off
 
-todo.pl "%Z%\packages\mozilla\firefox-10300-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
+todo.pl "%Z%\packages\mozilla\firefox-10605-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
