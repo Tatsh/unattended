154061998f16bf26f81c99a6b922ae1b28385343
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2022-12-19 00:22:13 +0000

    Thunderbird 102.6.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4598 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/thunderbird.bat
+++ install/scripts/thunderbird.bat
@@ -1,4 +1,4 @@
-:: OPTIONAL: Install Thunderbird 102.4.2
+:: OPTIONAL: Install Thunderbird 102.6.0
 :: HOME: http://download-origin.cdn.mozilla.net/pub/thunderbird/releases/?C=M;O=D
 :: Installation options: https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 ::
@@ -27,55 +27,55 @@
 :: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/52.9.1/win32/sv-SE/Thunderbird%20Setup%2052.9.1.exe|packages/mozilla/thunderbird-5291-sve-x86.exe
 :: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/52.9.1/win32/tr/Thunderbird%20Setup%2052.9.1.exe|packages/mozilla/thunderbird-5291-trk-x86.exe
 
-:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/ca/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-cat-x86.exe
-:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/cs/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-csy-x86.exe
-:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/da/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-dan-x86.exe
-:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/de/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-deu-x86.exe
-:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/el/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ell-x86.exe
-:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/en-GB/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-eng-x86.exe
-:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/en-US/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-enu-x86.exe
-:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/es-ES/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-esn-x86.exe
-:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/es-AR/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ess-x86.exe
-:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/fi/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-fin-x86.exe
-:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/fr/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-fra-x86.exe
-:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/he/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-heb-x86.exe
-:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/hu/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-hun-x86.exe
-:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/it/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ita-x86.exe
-:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/ja/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-jpn-x86.exe
-:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/ko/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-kor-x86.exe
-:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/nl/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-nld-x86.exe
-:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/nb-NO/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-nor-x86.exe
-:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/pl/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-plk-x86.exe
-:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/pt-BR/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ptb-x86.exe
-:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/ru/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-rus-x86.exe
-:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/sl/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-slv-x86.exe
-:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/sv-SE/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-sve-x86.exe
-:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win32/tr/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-trk-x86.exe
+:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/ca/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-cat-x86.exe
+:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/cs/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-csy-x86.exe
+:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/da/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-dan-x86.exe
+:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/de/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-deu-x86.exe
+:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/el/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ell-x86.exe
+:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/en-GB/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-eng-x86.exe
+:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/en-US/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-enu-x86.exe
+:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/es-ES/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-esn-x86.exe
+:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/es-AR/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ess-x86.exe
+:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/fi/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-fin-x86.exe
+:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/fr/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-fra-x86.exe
+:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/he/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-heb-x86.exe
+:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/hu/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-hun-x86.exe
+:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/it/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ita-x86.exe
+:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/ja/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-jpn-x86.exe
+:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/ko/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-kor-x86.exe
+:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/nl/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-nld-x86.exe
+:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/nb-NO/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-nor-x86.exe
+:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/pl/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-plk-x86.exe
+:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/pt-BR/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ptb-x86.exe
+:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/ru/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-rus-x86.exe
+:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/sl/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-slv-x86.exe
+:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/sv-SE/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-sve-x86.exe
+:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win32/tr/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-trk-x86.exe
 
-:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/ca/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-cat-amd64.exe
-:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/cs/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-csy-amd64.exe
-:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/da/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-dan-amd64.exe
-:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/de/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-deu-amd64.exe
-:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/el/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ell-amd64.exe
-:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/en-GB/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-eng-amd64.exe
-:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/en-US/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-enu-amd64.exe
-:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/es-ES/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-esn-amd64.exe
-:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/es-AR/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ess-amd64.exe
-:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/fi/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-fin-amd64.exe
-:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/fr/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-fra-amd64.exe
-:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/he/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-heb-amd64.exe
-:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/hu/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-hun-amd64.exe
-:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/it/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ita-amd64.exe
-:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/ja/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-jpn-amd64.exe
-:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/ko/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-kor-amd64.exe
-:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/nl/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-nld-amd64.exe
-:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/nb-NO/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-nor-amd64.exe
-:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/pl/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-plk-amd64.exe
-:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/pt-BR/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-ptb-amd64.exe
-:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/ru/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-rus-amd64.exe
-:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/sl/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-slv-amd64.exe
-:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/sv-SE/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-sve-amd64.exe
-:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/102.4.2/win64/tr/Thunderbird%20Setup%20102.4.2.exe|packages/mozilla/thunderbird-10242-trk-amd64.exe
+:: URL|CAT|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/ca/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-cat-amd64.exe
+:: URL|CSY|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/cs/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-csy-amd64.exe
+:: URL|DAN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/da/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-dan-amd64.exe
+:: URL|DEU|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/de/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-deu-amd64.exe
+:: URL|ELL|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/el/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ell-amd64.exe
+:: URL|ENG|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/en-GB/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-eng-amd64.exe
+:: URL|ENU|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/en-US/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-enu-amd64.exe
+:: URL|ESN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/es-ES/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-esn-amd64.exe
+:: URL|ESS|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/es-AR/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ess-amd64.exe
+:: URL|FIN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/fi/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-fin-amd64.exe
+:: URL|FRA|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/fr/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-fra-amd64.exe
+:: URL|HEB|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/he/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-heb-amd64.exe
+:: URL|HUN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/hu/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-hun-amd64.exe
+:: URL|ITA|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/it/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ita-amd64.exe
+:: URL|JPN|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/ja/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-jpn-amd64.exe
+:: URL|KOR|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/ko/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-kor-amd64.exe
+:: URL|NLD|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/nl/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-nld-amd64.exe
+:: URL|NOR|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/nb-NO/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-nor-amd64.exe
+:: URL|PLK|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/pl/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-plk-amd64.exe
+:: URL|PTB|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/pt-BR/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-ptb-amd64.exe
+:: URL|RUS|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/ru/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-rus-amd64.exe
+:: URL|SLV|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/sl/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-slv-amd64.exe
+:: URL|SVE|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/sv-SE/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-sve-amd64.exe
+:: URL|TRK|http://releases.mozilla.org/pub/thunderbird/releases/102.6.0/win64/tr/Thunderbird%20Setup%20102.6.0.exe|packages/mozilla/thunderbird-10260-trk-amd64.exe
 
 @echo off
 
@@ -83,7 +83,7 @@
 todo.pl "reg add HKLM\Software\Clients\Mail /ve /f /d \"Mozilla Thunderbird\""
 
 :: if system is XP then use 52.9.1
-set VERSION=10242
+set VERSION=10260
 if "%WINVER%" == "winxpsp3" set VERSION=5291
 
 todo.pl "%Z%\packages\mozilla\thunderbird-%VERSION%-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe /S /DesktopShortcut=false /PreventRebootRequired=true /MaintenanceService=false"
