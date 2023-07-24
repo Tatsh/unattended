97b522ece2372e8a59b0ffc5796290c42c3045f9
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2023-07-24 08:20:22 +0000

    Inkscape 1.3
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4633 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,8 +1,7 @@
-:: OPTIONAL:  Install Inkscape 1.2.2
+:: OPTIONAL:  Install Inkscape 1.3
 :: HOME: http://www.inkscape.org/
-
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.2.2_2022-12-01_b0a8486541-x86.msi|packages/inkscape/inkscape-1.2.2-x86.msi
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.2.2_2022-12-01_b0a8486541-x64.msi|packages/inkscape/inkscape-1.2.2-AMD64.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.3_2023-07-21_0e150ed6c4-x86.msi|packages/inkscape/inkscape-1.3-x86.msi
+:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.3_2023-07-21_0e150ed6c4-x64.msi|packages/inkscape/inkscape-1.3-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.2.2-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.3-%PROCESSOR_ARCHITECTURE%.msi"
