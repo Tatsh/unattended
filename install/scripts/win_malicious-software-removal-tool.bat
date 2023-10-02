83ca0febf725f3ba92c2483d96faa9b72e082e49
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2023-10-02 07:13:03 +0000

    Windows Malicious Software Removal Tool v5.117
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4643 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/win_malicious-software-removal-tool.bat
+++ install/scripts/win_malicious-software-removal-tool.bat
@@ -3,7 +3,7 @@
 
 :: <http://support.microsoft.com/kb/890830>
 :: https://www.catalog.update.microsoft.com/Search.aspx?q=KB890830
-:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V5.113.exe|updates/common/windows-kb890830-x86-v5.113.exe
-:: URL|ALL|http://download.microsoft.com/download/2/C/5/2C563B99-54D9-4D85-A82B-45D3CD2F53CE/Windows-KB890830-x64-V5.113.exe|updates/common/windows-kb890830-amd64-v5.113.exe
-todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.113.exe /Q"
+:: URL|ALL|http://download.microsoft.com/download/4/A/A/4AA524C6-239D-47FF-860B-5B397199CBF8/Windows-KB890830-V5.117.exe|updates/common/windows-kb890830-x86-v5.117.exe
+:: URL|ALL|http://download.microsoft.com/download/2/C/5/2C563B99-54D9-4D85-A82B-45D3CD2F53CE/Windows-KB890830-x64-V5.117.exe|updates/common/windows-kb890830-amd64-v5.117.exe
+todo.pl ".reboot-on 194 %Z%\updates\common\windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.117.exe /Q"
 
