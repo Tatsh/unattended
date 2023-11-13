ae912f68478160a68758fa0f0ca88254e42ae789
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2023-11-13 08:07:41 +0000

    Java JRE 8 Update 391
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4649 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -2,10 +2,10 @@
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
 ::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227550_e758a0de34e24606bca991d704f6dcbf|packages/jre/jre-8u151-windows-x86.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=248772_8c876547113c4e4aab3c868e9e0ec572|packages/jre/jre-8u381-windows-x86.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=248774_8c876547113c4e4aab3c868e9e0ec572|packages/jre/jre-8u381-windows-amd64.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=248761_8c876547113c4e4aab3c868e9e0ec572|packages/jre/jre-8u381-linux-i586.tar.gz
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=248763_8c876547113c4e4aab3c868e9e0ec572|packages/jre/jre-8u381-linux-x64.tar.gz
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=249201_b291ca3e0c8548b5a51d5a5f50063037|packages/jre/jre-8u391-windows-x86.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=249203_b291ca3e0c8548b5a51d5a5f50063037|packages/jre/jre-8u391-windows-amd64.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=249190_b291ca3e0c8548b5a51d5a5f50063037|packages/jre/jre-8u391-linux-i586.tar.gz
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=249192_b291ca3e0c8548b5a51d5a5f50063037|packages/jre/jre-8u391-linux-x64.tar.gz
 
 :: JRE version 7 is no longer available for public download, need to login and "agree" to download it:
 :: <http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html>
@@ -16,7 +16,7 @@
 
 :: Some Apps and websites does not work if you are using 64bit
 :: to enable 64 change the VERSION variable.
-set VERSION=8u381-windows-x86
+set VERSION=8u391-windows-x86
 
 ::If system is XP then use version 8u151
 if "%WINVER%" == "winxpsp3" set VERSION=8u151-windows-x86
