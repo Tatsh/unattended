78b0f6de8b09f75fdfe4ba750a10cb4298f64aa9
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2024-05-02 17:48:04 +0000

    LibreOffice 24.2.3
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4660 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -5,8 +5,8 @@
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.2.2/win/x86/LibreOffice_24.2.2_Win_x86.msi|packages/libreoffice/libreoffice_24.2.2_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.2.2/win/x86_64/LibreOffice_24.2.2_Win_x86-64.msi|packages/libreoffice/libreoffice_24.2.2_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.2.3/win/x86/LibreOffice_24.2.3_Win_x86.msi|packages/libreoffice/libreoffice_24.2.3_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.2.3/win/x86_64/LibreOffice_24.2.3_Win_x86-64.msi|packages/libreoffice/libreoffice_24.2.3_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
@@ -16,8 +16,8 @@
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/7.6.6/win/x86_64/LibreOffice_7.6.6_Win_x86-64.msi|packages/libreoffice/libreoffice_7.6.6_win_AMD64_install.msi
 
 :: libreOffice 5.4.x is last release that works on windows XP
-::URL|ALL|http://downloadarchive.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86/LibreOffice_5.4.7.2_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
-::URL|ALL|http://downloadarchive.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86_64/LibreOffice_5.4.7.2_Win_x64.msi|packages/libreoffice/libreoffice_5.4.7_win_AMD64_install.msi
+::URL|ALL|http://archive-na.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86/LibreOffice_5.4.7.2_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
+::URL|ALL|http://archive-na.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86_64/LibreOffice_5.4.7.2_Win_x64.msi|packages/libreoffice/libreoffice_5.4.7_win_AMD64_install.msi
 :: See bug https://bugs.documentfoundation.org/show_bug.cgi?id=112928
 @echo off
 
