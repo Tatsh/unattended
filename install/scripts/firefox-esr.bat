245f6c022822ef3c5de79f800b92292c8574f0d1
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2024-08-07 06:45:27 +0000

    Firefox 115.14.0es
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4674 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/firefox-esr.bat
+++ install/scripts/firefox-esr.bat
@@ -1,68 +1,68 @@
 :: OPTIONAL: Install Firefox Extended Release Support (ESR)
 ::HOME: http://www.mozilla.org/en-US/firefox/organizations/all.html
-:: Firefox 115.11.0esr 32Bit
-::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/ar/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ara-x86.exe
-::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/ca/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-cat-x86.exe
-::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/cs/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-csy-x86.exe
-::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/zh-TW/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-cht-x86.exe
-::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/zh-CN/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-chs-x86.exe
-::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/da/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-dan-x86.exe
-::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/de/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-deu-x86.exe
-::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/el/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ell-x86.exe
-::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/en-US/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-enu-x86.exe
-::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/en-GB/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-eng-x86.exe
-::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/es-ES/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-esn-x86.exe
-::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/es-AR/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ess-x86.exe
-::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/fi/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-fin-x86.exe
-::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/fr/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-fra-x86.exe
-::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/he/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-heb-x86.exe
-::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/hu/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-hun-x86.exe
-::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/it/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ita-x86.exe
-::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/ja/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-jpn-x86.exe
-::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/ko/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-kor-x86.exe
-::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/mk/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-mki-x86.exe
-::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/nl/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-nld-x86.exe
-::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/nb-NO/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-nor-x86.exe
-::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/pl/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-plk-x86.exe
-::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/pt-BR/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ptb-x86.exe
-::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/pt-PT/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ptg-x86.exe
-::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/ro/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-rom-x86.exe
-::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/ru/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-rus-x86.exe
-::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/sl/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-slv-x86.exe
-::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/sv-SE/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-sve-x86.exe
-::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win32/tr/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-trk-x86.exe
+:: Firefox 115.14.0esr 32Bit
+::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/ar/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ara-x86.exe
+::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/ca/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-cat-x86.exe
+::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/cs/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-csy-x86.exe
+::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/zh-TW/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-cht-x86.exe
+::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/zh-CN/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-chs-x86.exe
+::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/da/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-dan-x86.exe
+::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/de/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-deu-x86.exe
+::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/el/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ell-x86.exe
+::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/en-US/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-enu-x86.exe
+::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/en-GB/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-eng-x86.exe
+::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/es-ES/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-esn-x86.exe
+::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/es-AR/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ess-x86.exe
+::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/fi/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-fin-x86.exe
+::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/fr/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-fra-x86.exe
+::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/he/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-heb-x86.exe
+::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/hu/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-hun-x86.exe
+::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/it/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ita-x86.exe
+::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/ja/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-jpn-x86.exe
+::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/ko/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-kor-x86.exe
+::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/mk/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-mki-x86.exe
+::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/nl/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-nld-x86.exe
+::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/nb-NO/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-nor-x86.exe
+::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/pl/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-plk-x86.exe
+::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/pt-BR/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ptb-x86.exe
+::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/pt-PT/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ptg-x86.exe
+::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/ro/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-rom-x86.exe
+::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/ru/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-rus-x86.exe
+::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/sl/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-slv-x86.exe
+::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/sv-SE/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-sve-x86.exe
+::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win32/tr/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-trk-x86.exe
 
-:: Firefox 115.11.0esr 64Bit
-::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/ar/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ara-amd64.exe
-::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/ca/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-cat-amd64.exe
-::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/cs/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-csy-amd64.exe
-::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/zh-TW/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-cht-amd64.exe
-::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/zh-CN/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-chs-amd64.exe
-::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/da/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-dan-amd64.exe
-::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/de/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-deu-amd64.exe
-::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/el/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ell-amd64.exe
-::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/en-US/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-enu-amd64.exe
-::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/en-GB/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-eng-amd64.exe
-::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/es-ES/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-esn-amd64.exe
-::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/es-AR/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ess-amd64.exe
-::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/fi/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-fin-amd64.exe
-::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/fr/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-fra-amd64.exe
-::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/he/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-heb-amd64.exe
-::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/hu/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-hun-amd64.exe
-::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/it/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ita-amd64.exe
-::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/ja/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-jpn-amd64.exe
-::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/ko/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-kor-amd64.exe
-::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/mk/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-mki-amd64.exe
-::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/nl/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-nld-amd64.exe
-::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/nb-NO/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-nor-amd64.exe
-::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/pl/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-plk-amd64.exe
-::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/pt-BR/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ptb-amd64.exe
-::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/pt-PT/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-ptg-amd64.exe
-::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/ro/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-rom-amd64.exe
-::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/ru/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-rus-amd64.exe
-::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/sl/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-slv-amd64.exe
-::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/sv-SE/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-sve-amd64.exe
-::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/115.11.0esr/win64/tr/Firefox%20Setup%20115.11.0esr.exe|packages/mozilla/firefox-11511esr-trk-amd64.exe
+:: Firefox 115.14.0esr 64Bit
+::URL|ARA|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/ar/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ara-amd64.exe
+::URL|CAT|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/ca/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-cat-amd64.exe
+::URL|CSY|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/cs/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-csy-amd64.exe
+::URL|CHT|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/zh-TW/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-cht-amd64.exe
+::URL|CHS|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/zh-CN/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-chs-amd64.exe
+::URL|DAN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/da/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-dan-amd64.exe
+::URL|DEU|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/de/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-deu-amd64.exe
+::URL|ELL|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/el/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ell-amd64.exe
+::URL|ENU|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/en-US/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-enu-amd64.exe
+::URL|ENG|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/en-GB/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-eng-amd64.exe
+::URL|ESN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/es-ES/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-esn-amd64.exe
+::URL|ESS|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/es-AR/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ess-amd64.exe
+::URL|FIN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/fi/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-fin-amd64.exe
+::URL|FRA|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/fr/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-fra-amd64.exe
+::URL|HEB|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/he/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-heb-amd64.exe
+::URL|HUN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/hu/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-hun-amd64.exe
+::URL|ITA|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/it/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ita-amd64.exe
+::URL|JPN|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/ja/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-jpn-amd64.exe
+::URL|KOR|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/ko/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-kor-amd64.exe
+::URL|MKI|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/mk/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-mki-amd64.exe
+::URL|NLD|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/nl/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-nld-amd64.exe
+::URL|NOR|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/nb-NO/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-nor-amd64.exe
+::URL|PLK|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/pl/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-plk-amd64.exe
+::URL|PTB|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/pt-BR/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ptb-amd64.exe
+::URL|PTG|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/pt-PT/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-ptg-amd64.exe
+::URL|ROM|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/ro/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-rom-amd64.exe
+::URL|RUS|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/ru/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-rus-amd64.exe
+::URL|SLV|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/sl/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-slv-amd64.exe
+::URL|SVE|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/sv-SE/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-sve-amd64.exe
+::URL|TRK|http://ftp.mozilla.org/pub/firefox/releases/115.14.0esr/win64/tr/Firefox%20Setup%20115.14.0esr.exe|packages/mozilla/firefox-11514esr-trk-amd64.exe
 
 
 :: Firefox 52.9.0esr 32Bit For Windows XP
@@ -103,7 +103,7 @@
 @Echo off
 
 :: If system is XP then use 52.9.0esr
-set VERSION=11511esr
+set VERSION=11514esr
 if "%WINVER%" == "winxpsp3" set VERSION=5290esr
 
 todo.pl "%Z%\packages\mozilla\firefox-%VERSION%-%WINLANG%-%PROCESSOR_ARCHITECTURE%.exe -ms -ira /PreventRebootRequired=true /MaintenanceService=false"
