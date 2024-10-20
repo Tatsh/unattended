89b972c00b19d4da0dbd4f081fe1002d53b95c8a
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2024-10-20 22:00:13 +0000

    Java Virtual Machine Version 8 Update 431
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4682 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/java.bat
+++ install/scripts/java.bat
@@ -1,11 +1,11 @@
-:: OPTIONAL: Install Java Virtual Machine Version 8 Update 421
+:: OPTIONAL: Install Java Virtual Machine Version 8 Update 431
 :: <http://www.java.com/en/download/manual.jsp>
 :: <http://www.oracle.com/technetwork/java/javase/silent-136552.html>
 ::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227550_e758a0de34e24606bca991d704f6dcbf|packages/jre/jre-8u151-windows-x86.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=250127_d8aa705069af427f9b83e66b34f5e380|packages/jre/jre-8u421-windows-x86.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=250129_d8aa705069af427f9b83e66b34f5e380|packages/jre/jre-8u421-windows-amd64.exe
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=250116_d8aa705069af427f9b83e66b34f5e380|packages/jre/jre-8u421-linux-i586.tar.gz
-::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=250117_d8aa705069af427f9b83e66b34f5e380|packages/jre/jre-8u421-linux-x64.tar.gz
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=251407_0d8f12bc927a4e2c9f8568ca567db4ee|packages/jre/jre-8u431-windows-x86.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=251408_0d8f12bc927a4e2c9f8568ca567db4ee|packages/jre/jre-8u431-windows-amd64.exe
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=251396_0d8f12bc927a4e2c9f8568ca567db4ee|packages/jre/jre-8u431-linux-i586.tar.gz
+::URL|ALL|http://javadl.oracle.com/webapps/download/AutoDL?BundleId=251398_0d8f12bc927a4e2c9f8568ca567db4ee|packages/jre/jre-8u431-linux-x64.tar.gz
 
 :: JRE version 7 is no longer available for public download, need to login and "agree" to download it:
 :: <http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html>
@@ -16,7 +16,7 @@
 
 :: Some Apps and websites does not work if you are using 64bit
 :: to enable 64 change the VERSION variable.
-set VERSION=8u421-windows-x86
+set VERSION=8u431-windows-x86
 
 ::If system is XP then use version 8u151
 if "%WINVER%" == "winxpsp3" set VERSION=8u151-windows-x86
