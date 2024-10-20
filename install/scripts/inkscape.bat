113253506923d7cb51c928fb8d5b7df255ba91d0
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2024-10-20 23:24:37 +0000

    Inkscape 1.4.0
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4683 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/inkscape.bat
+++ install/scripts/inkscape.bat
@@ -1,7 +1,7 @@
-:: OPTIONAL:  Install Inkscape 1.3.2
+:: OPTIONAL:  Install Inkscape 1.4.0
 :: HOME: http://www.inkscape.org/
 :: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.3.2_2023-11-25_091e20ef0f-x86.msi|packages/inkscape/inkscape-1.3.2-x86.msi
-:: URL|All|https://media.inkscape.org/dl/resources/file/inkscape-1.3.2_2023-11-25_091e20e-x64.msi|packages/inkscape/inkscape-1.3.2-AMD64.msi
+:: URL|All|http://inkscape.org/gallery/item/53697/inkscape-1.4_2024-10-11_86a8ad7-x64.msi|packages/inkscape/inkscape-1.4.0-AMD64.msi
 @Echo off
 
-todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.3.2-%PROCESSOR_ARCHITECTURE%.msi"
+todo.pl "msiexec /qb /l* %SystemDrive%\netinst\logs\inkscape.txt /i  %Z%\packages\inkscape\inkscape-1.4.0-%PROCESSOR_ARCHITECTURE%.msi"
