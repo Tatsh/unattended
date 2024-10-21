5ff60e3c8a0da0093d31eb3305fa8c100e77a6ef
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2024-10-21 00:56:00 +0000

    Adobe Reader DC 24.003.20180
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4684 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/adobe-reader-dc.bat
+++ install/scripts/adobe-reader-dc.bat
@@ -33,14 +33,14 @@
 :: URL|TRK|ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_tr_TR.msi|packages/adobereader/acrordrdc_15.007.20033_trk.msi
 
 :: Download the newest patch
-:: URL|ALL|https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300320284/AcroRdrDCUpd2300320284.msp|packages/adobereader/acrordrdcupd_23.003.20284.msp
+:: URL|ALL|https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2400320180/AcroRdrDCUpd2400320180.msp|packages/adobereader/acrordrdcupd_24.003.20180.msp
 
 :: Extra language packs, more info:
 :: https://helpx.adobe.com/acrobat/kb/windows-font-packs-32-bit-reader.html
 :: Download Spelling dictionary pack
-:: URL|ALL|https://ardownload3.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/AcroRdrSD2200120085_all_DC.msi|packages/adobereader/misc/AcroRdrSD_22.001.20085_all_DC.msi
+:: URL|ALL|http://ardownload3.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/AcroRdrSD2200120085_all_DC.msi|packages/adobereader/misc/AcroRdrSD_22.001.20085_all_DC.msi
 :: Download Font Pack
-:: URL|ALL|https://ardownload3.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/FontPack2200120085_XtdAlf_Lang_DC.msi|packages/adobereader/misc/FontPack_22.001.20085_XtdAlf_Lang_DC.msi
+:: URL|ALL|http://ardownload3.adobe.com/pub/adobe/reader/win/AcrobatDC/misc/FontPack2200120085_XtdAlf_Lang_DC.msi|packages/adobereader/misc/FontPack_22.001.20085_XtdAlf_Lang_DC.msi
 
 :: Uncomment the todo.pl lines if you want to install these language packs.
 :: Install Spelling dictionary pack
@@ -54,4 +54,4 @@ todo.pl "unlink.pl special:AllUsersDesktop\"\Acrobat Reader DC.lnk\""
 todo.pl ".ignore-err 1 reg add \"HKLM\SOFTWARE\Adobe\Acrobat Reader\DC\AdobeViewer\" /f /v \"EULA\" /t REG_DWORD /d 1"
 
 :: This is the actual installer
-todo.pl ".ignore-err 194 msiexec /qb /l* %SystemDrive%\netinst\logs\adobereaderdc.txt /i \"%Z%\packages\adobereader\acrordrdc_15.007.20033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\acrordrdcupd_23.003.20284.msp\" REBOOT=ReallySuppress"
+todo.pl ".ignore-err 194 msiexec /qb /l* %SystemDrive%\netinst\logs\adobereaderdc.txt /i \"%Z%\packages\adobereader\acrordrdc_15.007.20033_%WINLANG%.msi\" PATCH=\"%Z%\packages\adobereader\acrordrdcupd_24.003.20180.msp\" REBOOT=ReallySuppress"
