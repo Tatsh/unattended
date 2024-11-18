ca365fc692017a36250c773cb2134f7054aabd0e
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2024-11-18 23:19:05 +0000

    LibreOffice 24.8.3
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4687 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -1,12 +1,12 @@
 ::OPTIONAL: Install LibreOffice
 ::HOME: http://www.libreoffice.org/
-:: LibreOffice Fresh 24.8.2
+:: LibreOffice Fresh 24.8.3
 :: <http://www.libreoffice.org/download/libreoffice-fresh/>
 :: "LibreOffice Fresh is the stable version with the most recent features.
 :: DEPLOYMENT INFO https://wiki.documentfoundation.org/Deployment_and_Migration
 :: Users interested in taking advantage of our most innovative features should download and use our fresh version."
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.8.2/win/x86/LibreOffice_24.8.2_Win_x86.msi|packages/libreoffice/libreoffice_24.8.2_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.8.2/win/x86_64/LibreOffice_24.8.2_Win_x86-64.msi|packages/libreoffice/libreoffice_24.8.2_win_AMD64_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.8.3/win/x86/LibreOffice_24.8.3_Win_x86.msi|packages/libreoffice/libreoffice_24.8.3_win_x86_install.msi
+::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.8.3/win/x86_64/LibreOffice_24.8.3_Win_x86-64.msi|packages/libreoffice/libreoffice_24.8.3_win_AMD64_install.msi
 
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
