3437e554eac7426f4e24a9911d4e150c04dd92fd
jjp3 <jjp3@3671eaaa-a6c4-423f-a734-8bc38373c25a>
2025-08-21 12:28:47 +0000

    LibreOffice 25.2.x works on win7/8
    git-svn-id: https://svn.code.sf.net/p/unattended/code/trunk@4705 3671eaaa-a6c4-423f-a734-8bc38373c25a

--- install/scripts/libreoffice.bat
+++ install/scripts/libreoffice.bat
@@ -11,12 +11,9 @@
 :: LibreOffice Still
 :: <http://www.libreoffice.org/download/libreoffice-still/>
 :: "LibreOffice Still is the stable version that has undergone more testing (over a longer time)
+:: libreOffice 25.2.x is last release that works on windows 7 and 8/8.1
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/25.2.5/win/x86/LibreOffice_25.2.5_Win_x86.msi|packages/libreoffice/libreoffice_25.2.5_win_x86_install.msi
 ::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/25.2.5/win/x86_64/LibreOffice_25.2.5_Win_x86-64.msi|packages/libreoffice/libreoffice_25.2.5_win_AMD64_install.msi
-
-:: libreOffice 24.8.x is last release that works on windows 7 and 8/8.1
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.8.7/win/x86/LibreOffice_24.8.7_Win_x86.msi|packages/libreoffice/libreoffice_24.8.7_win_x86_install.msi
-::URL|ALL|http://download.documentfoundation.org/libreoffice/stable/24.8.7/win/x86_64/LibreOffice_24.8.7_Win_x86-64.msi|packages/libreoffice/libreoffice_24.8.7_win_AMD64_install.msi
 
 :: libreOffice 5.4.x is last release that works on windows XP
 ::URL|ALL|http://archive-na.documentfoundation.org/libreoffice/old/5.4.7.2/win/x86/LibreOffice_5.4.7.2_Win_x86.msi|packages/libreoffice/libreoffice_5.4.7_win_x86_install.msi
@@ -25,8 +22,8 @@
 @echo off
 
 :: Old systems use diferent versions.
-set VERSION=24.8.7
+set VERSION=25.2.5
 if "%WINVER%" == "winxpsp3" set VERSION=5.4.7
-if "%WINVER%" == "win7" set VERSION=24.8.7
+if "%WINVER%" == "win7" set VERSION=25.2.5
 
 todo.pl ".ignore-err 194 msiexec /qn /norestart /passive /i %Z%\packages\libreoffice\libreoffice_%VERSION%_win_%PROCESSOR_ARCHITECTURE%_install.msi"
